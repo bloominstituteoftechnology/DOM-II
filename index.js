427fa8f629e1ca76ebbca974e0a0d6bd8f00f554
hrehman000@citymail.cuny.edu <hrehman000@citymail.cuny.edu>
2018-02-27 15:51:44 -0500

    initial commit

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 // Outward function
+const redBlock = document.getElementsByClassName("block--red")[0];
+const blueBlock = document.getElementsByClassName("block--blue")[0];
+const greenBlock = document.getElementsByClassName("block--green")[0];
+const pinkBlock = document.getElementsByClassName("block--pink")[0];
+const grayBlock = document.getElementsByClassName("block--gray")[0];
 
+function rocket () {
+    grayBlock.addEventListener("click", (event) => {
+        toTop(event.target);
+    });
+}
+
+function toTop(arg) {
+    arg.style.order = -1;
+}
+ 
+rocket();
 // init function
-
-
-
