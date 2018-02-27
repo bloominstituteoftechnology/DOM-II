a42bafe0e955472ee561a407f9ae3f553057818a
DragonRydrz <DragonRydrz@me.com>
2018-02-27 16:56:55 -0500

    much progress on traveling

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-document.querySelector(".blocks").addEventListener("click", (event) => {
-  document.querySelector(".blocks").removeChild(event.target);
-  document.querySelector(".blocks").prepend(event.target); });
+// ############################# ROCKETS ################################
+
+// document.querySelector(".blocks").addEventListener("click", (event) => {
+//   document.querySelector(".blocks").removeChild(event.target);
+//   document.querySelector(".blocks").prepend(event.target); });
+
+// ############################ TRAVELERS ###############################
+
+const container = document.querySelector(".blocks");
+
+container.addEventListener('mousedown', moveRight);
+
+
+function moveRight(event) {
+
+  if (container.addEventListener) container.addEventListener('mouseup', moveLeft);
+  if (event.target === container) return null;
+  let margin = 2;
+  console.log(event);
+  const rightInterval = setInterval(() => {
+    event.target.style.marginLeft = `${++margin}%`;
+    if (margin === 80) clearInterval(rightInterval);
+  },100);
+
+  function moveLeft() {
+    if (event.target === container) return null;
+    clearInterval(rightInterval);
+    console.log(event);
+    const leftInterval = setInterval(() => {
+      event.target.style.marginLeft = `${--margin}%`;
+      if (margin <= 2) clearInterval(leftInterval);
+    },100);
+  }
+
+
+}
+
