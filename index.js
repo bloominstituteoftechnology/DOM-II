b675160ec9a223d605c236c6d63a701c590afd46
joshuacoyne <josh.j.coyne@gmail.com>
2018-02-27 17:21:41 -0500

    DOM II - TRAVELERS - WIP

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 
 const blocks = document.querySelector('.blocks');
-let lowestOrder = 0;
-blocks.addEventListener('click', function(e){
-      lowestOrder--;
-      e.target.style.order  = lowestOrder;
-});
+// let lowestOrder = 0;
+// blocks.addEventListener('click', function(e){
+//       lowestOrder--;
+//       e.target.style.order  = lowestOrder;
+// });
 
+blocks.addEventListener('mousedown', moveToRight);
+
+function moveToRight(e) {
+      console.log(this);
+      console.log(e.target);
+      e.target.style.right="0px";
+
+}
+
+
+// when mouse click down send element to the right
+// when mouse is released start setInterval function making
+// mouse travel left
