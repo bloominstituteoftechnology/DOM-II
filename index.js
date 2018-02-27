1e3f746fe0059e6b0e2c4ac3c5e729284b0cbbb7
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-02-27 17:44:12 -0500

    Jacob S. Henry, Nour Hassan, Walter S. Woodward WIP

---
+++ index.js
@@ -0,0 +1,7 @@
+const blocks = document.querySelector(".blocks");
+const red = document.querySelector(".block--red");
+const blue = document.querySelector(".block--blue");
+const green = document.querySelector(".block--green");
+const pink = document.querySelector(".block--pink");
+const gray = document.querySelector(".block--gray");
+
