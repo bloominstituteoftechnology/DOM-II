7112b0aa906ecbfd7887e85dcf68d390c5677fd7
Justin Kaseman <justinkaseman@live.com>
2018-02-27 14:57:12 -0800

    Finish travelers (see previous commit for rockets)

--- index.js
+++ index.js
@@ -1,10 +1,46 @@
 const allBlocks = document.querySelectorAll(".block");
 
 allBlocks.forEach(function(x){
-    x.addEventListener("click", moveTop);
+    x.addEventListener("mousedown", moveRight);
 });
 
 function moveTop() {
-    console.log(event.target.parentNode);
     event.target.parentNode.insertBefore(event.target, event.target.parentNode.firstChild);
+}
+
+function moveRight(event) {
+    let box = event.target
+    let pad = window.getComputedStyle(box, null).getPropertyValue('padding-left');
+    pad = +pad.substring(0, pad.length - 2);
+    let move = setInterval(frame, 10);
+    function frame() {
+        if (pad == 350) {
+            clearInterval(move);
+        } else {
+            pad = pad + 5;
+            box.style.padding = `0 0 0 ${pad}px`;
+        }
+    }
+    box.addEventListener("mouseup", function() {
+        clearInterval(move);
+        moveLeft(event);
+    });
+}
+
+function moveLeft(event) {
+    let box = event.target
+    let pad = window.getComputedStyle(box, null).getPropertyValue('padding-left');
+    pad = +pad.substring(0, pad.length - 2);
+    let move = setInterval(frame, 10);
+    function frame() {
+        if (pad == 0) {
+            clearInterval(move);
+        } else {
+            pad = pad - 5;
+            box.style.padding = `0 0 0 ${pad}px`;
+        }
+    }
+    box.addEventListener("mousedown", function() {
+        clearInterval(move);
+    });
 }
