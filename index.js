b3ed1da6c73289abbf8b772761886237922f71d7
Sagdi Formanov <forsy@Sagdis-MacBook-Pro.local>
2018-02-27 13:42:01 -0800

    completed rockets

--- index.js
+++ index.js
@@ -1,6 +1,75 @@
 const blocks = document.querySelector(".blocks");
 
-blocks.addEventListener("click", () => {
-  console.log("it worked");
+const red = document.querySelector(".block--red");
+const green = document.querySelector(".block--green");
+const blue = document.querySelector(".block--blue");
+const gray = document.querySelector(".block--gray");
+const pink = document.querySelector(".block--pink");
+const empty = document.querySelector(".empty");
+
+blocks.addEventListener("click", (event) => {
+  // console.log("it worked", event);
+  
+  if (event.target.id === "red") {
+    console.log("red");
+    empty.style.backgroundColor = "red";
+    empty.style.display = "inline-block";
+    red.style.display = "none";
+    
+    green.style.display = "inline-block";
+    blue.style.display = "inline-block";
+    gray.style.display = "inline-block";
+    pink.style.display = "inline-block";
+  }
+  if (event.target.id === "green") {
+    console.log("green");
+    empty.style.backgroundColor = "green";
+    empty.style.display = "inline-block";
+    green.style.display = "none";
+    
+    red.style.display = "inline-block";
+    blue.style.display = "inline-block";
+    gray.style.display = "inline-block";
+    pink.style.display = "inline-block";
+  }
+   if (event.target.id === "blue") {
+    console.log("blue");
+    empty.style.backgroundColor = "blue";
+    empty.style.display = "inline-block";
+    blue.style.display = "none";
+    
+    green.style.display = "inline-block";
+    red.style.display = "inline-block";
+    gray.style.display = "inline-block";
+    pink.style.display = "inline-block";
+  }
+ if (event.target.id === "gray") {
+    console.log("gray");
+    empty.style.backgroundColor = "gray";
+    empty.style.display = "inline-block";
+    gray.style.display = "none";
+    
+    green.style.display = "inline-block";
+    blue.style.display = "inline-block";
+    red.style.display = "inline-block";
+    pink.style.display = "inline-block";
+  }
+ if (event.target.id === "pink") {
+    console.log("pink");
+    empty.style.backgroundColor = "pink";
+    empty.style.display = "inline-block";
+    pink.style.display = "none";
+    
+    green.style.display = "inline-block";
+    blue.style.display = "inline-block";
+    gray.style.display = "inline-block";
+    red.style.display = "inline-block";
+  }
+  
 });
+
+
+
+//---
+
 
