8e911534b737c643393cd89d9437e5cb698e4e75
unknown <adfaris@gmail.com>
2018-02-27 18:09:33 -0500

    Solved rocket part of the project and started working on travelers

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-// const blockRed = document.getElementsByClassName("block--red");
-// const blockblue = document.getElementsByClassName("block--blue");
-// const blockgreen = document.getElementsByClassName("block--green");
-// const blockpink = document.getElementsByClassName("block--pink");
-// const blockgray = document.getElementsByClassName("block--gray");
-
-
-
-
 // document.querySelector(".blocks").addEventListener("click", (event) => {
 //     document.querySelector(".blocks").removeChild(event.target);
 //     document.querySelector(".blocks").prepend(event.target);   
@@ -19,31 +10,3 @@ parent.addEventListener("click", (event) => {
 parent.removeChild(event.target);
 parent.prepend(event.target); 
 });
-
-// theParent = document.getElementById("theParent");
-// theKid = document.createElement("div");
-// theKid.innerHTML = 'Are we there yet?';
-
-// // append theKid to the end of theParent
-// theParent.appendChild(theKid);
-
-// // prepend theKid to the beginning of theParent
-// theParent.insertBefore(theKid, theParent.firstChild);
-
-
-
-// blockblue.addEventListener("click", (event) => {
-//   event.color = "blue";
-// });
-
-// blockgreen.addEventListener("click", () => {
-//   parent.prepend(blockgreen);
-// });
-
-// blockpink.addEventListener("click", (event) =>{
-//     event.color = "pink";
-// });
-
-// blockgray.addEventListener("click", (event) =>{
-//     event.color = "gray";
-// });
