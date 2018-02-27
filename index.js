fe51284061424a32c7eda23d33c747db660d8a55
Khepf <jeremy1975@gmail.com>
2018-02-27 16:48:49 -0500

    JEREMY rocket part works

--- index.js
+++ index.js
@@ -5,42 +5,64 @@ const greenBlock = document.querySelector(".block--green");
 const pinkBlock = document.querySelector(".block--pink");
 const grayBlock = document.querySelector(".block--gray");
 
-// When an element is clicked, assign it the lowest order so it goes to top
-// have pre-defined orders of 1-5. When element is clicked, it is assigned the order of 1
-// How to make it so the other elements are incremented to 2,3,4,5?
-
-// what if? Instead of incrementing the one we want, we decrement the other four?
-
-// divName.style.color='red';
-// style.order='num';
-// num--;
-
-let redNum = 0;
-let blueNum = 0;
-let greenNum = 0;
-let pinkNum = 0;
-let grayNum = 0;
-
 redBlock.addEventListener("click", (event) => {
-  // redBlock.classList.add("blockToTop");
-  redNum--;
-  redBlock.style.order="redNum";
+   redBlock.classList.add("blockToTop");
+   redOtherBoxes();
 });
 
 blueBlock.addEventListener("click", (event) => {
-  // blueBlock.classList.add("blockToTop");
-  blueNum--;
-  blueBlock.style.order="blueNum"; 
+   blueBlock.classList.add("blockToTop");
+   blueOtherBoxes();
+  
 });
 
 greenBlock.addEventListener("click", (event) => {
   greenBlock.classList.add("blockToTop");
+  greenOtherBoxes();
 });
 
 pinkBlock.addEventListener("click", (event) => {
-  pinkBlock.classList.add("blockToTop"); 
+  pinkBlock.classList.add("blockToTop");
+  pinkOtherBoxes();
 });
 
 grayBlock.addEventListener("click", (event) => {
   grayBlock.classList.add("blockToTop");
+  grayOtherBoxes();
 });
+
+let redOtherBoxes= () => {
+  blueBlock.classList.remove("blockToTop");
+  greenBlock.classList.remove("blockToTop");
+  pinkBlock.classList.remove("blockToTop");
+  grayBlock.classList.remove("blockToTop");
+}
+
+let blueOtherBoxes = () => {
+  redBlock.classList.remove("blockToTop");
+  greenBlock.classList.remove("blockToTop");
+  pinkBlock.classList.remove("blockToTop");
+  grayBlock.classList.remove("blockToTop");
+}
+
+let greenOtherBoxes = () => {
+  redBlock.classList.remove("blockToTop");
+  blueBlock.classList.remove("blockToTop");
+  pinkBlock.classList.remove("blockToTop");
+  grayBlock.classList.remove("blockToTop");
+}
+
+let pinkOtherBoxes = () => {
+  redBlock.classList.remove("blockToTop");
+  blueBlock.classList.remove("blockToTop");
+  greenBlock.classList.remove("blockToTop");
+  grayBlock.classList.remove("blockToTop"); 
+}
+
+let grayOtherBoxes = () => {
+  redBlock.classList.remove("blockToTop");
+  blueBlock.classList.remove("blockToTop");
+  greenBlock.classList.remove("blockToTop");
+  pinkBlock.classList.remove("blockToTop");
+}
+
