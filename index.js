b91cebad17f6eb9bebfd014f048d9592b363bfe5
noxasaxon <saxon.h@outlook.com>
2018-02-27 13:08:26 -0800

    boxes shift to right when mousedown

---
+++ index.js
@@ -0,0 +1,21 @@
+//const box1 = document.getElementById("box1");
+//const box2 = document.getElementById("box2");
+//const box3 = document.getElementById("box3");
+
+const allBlocks = document.getElementsByClassName("blocks");
+const block = document.getElementsByClassName("block");
+const blockArray = Array.from(block);
+
+
+//shift block to right when clicked down
+Array.from(block).forEach(function(element) {
+    //console.log(allBlocks);
+    //element.addEventListener('mousedown', shiftRight(blockArray[blockArray.indexOf(element)]));
+    //element.addEventListener('mousedown', shiftRight(element));
+    element.addEventListener('mousedown', function () {
+        this.style.marginLeft = "30px";
+    });
+    element.addEventListener('mouseup', function () {
+        this.style.marginLeft = "10px";
+    })
+});
