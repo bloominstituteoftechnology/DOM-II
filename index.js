a46818f2e026559121c67ac98a6009acf785fa22
zackhitch <zackhitch@users.noreply.github.com>
2018-02-27 12:48:42 -0800

    Completed Part I - Rockets - Zack Hitchcock

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-const red = document.getElementsByClassName('block--red');
-const blue = document.getElementsByClassName('block--blue');
-const green = document.getElementsByClassName('block--green');
-const pink = document.getElementsByClassName('block--pink');
-const gray = document.getElementsByClassName('block--gray');
+let blocksDiv = document.querySelector('.blocks');
 
+const red = document.getElementsByClassName('block--red')[0];
+const blue = document.getElementsByClassName('block--blue')[0];
+const green = document.getElementsByClassName('block--green')[0];
+const pink = document.getElementsByClassName('block--pink')[0];
+const gray = document.getElementsByClassName('block--gray')[0];
+
+let firstBlock = document.getElementsByClassName('block')[0];
+
+red.addEventListener('click', (event) => {
+    blocksDiv.insertBefore(red, firstBlock);
+    firstBlock = document.getElementsByClassName('block')[0];
+});
+
+blue.addEventListener('click', (event) => {
+    blocksDiv.insertBefore(blue, firstBlock);
+    firstBlock = document.getElementsByClassName('block')[0];
+});
+
+green.addEventListener('click', (event) => {
+    blocksDiv.insertBefore(green, firstBlock);
+    firstBlock = document.getElementsByClassName('block')[0];
+});
+
+pink.addEventListener('click', (event) => {
+    blocksDiv.insertBefore(pink, firstBlock);
+    firstBlock = document.getElementsByClassName('block')[0];
+});
+
+gray.addEventListener('click', (event) => {
+    blocksDiv.insertBefore(gray, firstBlock);
+    firstBlock = document.getElementsByClassName('block')[0];
+});
