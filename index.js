fb816e6366ab7666e0fe61939d3029a21159a279
Shaun Kelly <Shaun@redskelly.local>
2018-02-27 16:57:08 -0500

    Set up index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+const redBox = document.getElementsByClassName('block--red');
+const blueBox = document.getElementsByClassName('block--blue');
+const greenBox = document.getElementsByClassName('block--green');
+const pinkBox = document.getElementsByClassName('block--pink');
+const grayBox = document.getElementsByClassName('block--gray');
+
+// function that moves the element to the top
+    // if object is clicked, move to top
+
+// pass elements through function
+
+// possible setup: 
+document.getElementByClassName('block--red') = function() {};
+// -or-
+document.getElementById('block--red') = function() {}; // would need to add id in index.html
