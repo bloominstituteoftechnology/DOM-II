67bd3b1dc6e4e81b5a6340a2197ab26c95b9e493
unknown <ruiz.james12@yahoo.com>
2018-02-27 15:10:07 -0800

    Part 1: Rockets COMPLETED

---
+++ index.js
@@ -0,0 +1,45 @@
+const box1 = document.getElementsByClassName("block--red")[0];
+const box2 = document.getElementsByClassName("block--blue")[0];
+const box3 = document.getElementsByClassName("block--green")[0];
+const box4 = document.getElementsByClassName("block--pink")[0];
+const box5 = document.getElementsByClassName("block--gray")[0];
+
+box1.addEventListener('click', (event) => {
+    box1.style.order = -1;
+    box2.style.order = 0;
+    box3.style.order = 0;
+    box4.style.order = 0;
+    box5.style.order = 0;
+})
+
+box2.addEventListener('click', (event) => {
+    box1.style.order = 0;
+    box2.style.order = -1;
+    box3.style.order = 0;
+    box4.style.order = 0;
+    box5.style.order = 0;
+})
+
+box3.addEventListener('click', (event) => {
+    box1.style.order = 0;
+    box2.style.order = 0;
+    box3.style.order = -1;
+    box4.style.order = 0;
+    box5.style.order = 0;
+})
+
+box4.addEventListener('click', (event) => {
+    box1.style.order = 0;
+    box2.style.order = 0;
+    box3.style.order = 0;
+    box4.style.order = -1;
+    box5.style.order = 0;
+})
+
+box5.addEventListener('click', (event) => {
+    box1.style.order = 0;
+    box2.style.order = 0;
+    box3.style.order = 0;
+    box4.style.order = 0;
+    box5.style.order = -1;
+})
