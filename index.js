8493065c8693018bbc470998d30f4f278ee482fc
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-02-27 15:17:21 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const redBlock = document.getElementsByClassName("block block--red");
+const blueBlock = document.getElementsByClassName("block block--blue");
+const greenBlock = document.getElementsByClassName("block block--green");
+const pinkBlock = document.getElementsByClassName("block block--pink");
+const grayBlock = document.getElementsByClassName("block block--gray");
