a14ef288e28786d89476163c6068acc0c82c924f
Randy Wilson <phoenixrr2113@gmail.com>
2018-02-27 12:45:23 -0800

    all done with rockets

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const redBox = document.getElementsByClassName("block--red");
-const blueBox = document.getElementsByClassName("block block--blue");
-const greenBox = document.getElementsByClassName("block block--green");
-const pinkBox = document.getElementsByClassName("block block--pink");
-const grayBox = document.getElementsByClassName("block block--gray");
-
-
-document.redBox.addEventListener("click", function() {
-  redBox.setAttribute("order: -1");
-  console.log('clicked');
-});
-// blueBox.addEventListener("click", (e) => {
-//   if (event.value === "block block--red") {
-//     blueBox.style = "order: 1";
-//   }
-// })
-// redBox.addEventListener("click", (e) => {
-//   if (event.value === "block block--red") {
-//     redBox.style = "order: 1";
-//   }
-// })
-// redBox.addEventListener("click", (e) => {
-//   if (event.value === "block block--red") {
-//     redBox.style = "order: 1";
-//   }
-// })
+document.querySelector(".blocks").addEventListener("click", (event) => {
+  document.querySelector(".blocks").removeChild(event.target);
+  document.querySelector(".blocks").prepend(event.target); });
