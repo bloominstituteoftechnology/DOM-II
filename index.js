d0c9db3daf2dd43206ece010f59650aff7f3c97f
astro11 <aquila.strother@asu.edu>
2018-02-27 18:37:37 -0500

    updated index.js to reflect travelers WIP, added PNGs to match colors

--- index.js
+++ index.js
@@ -1,8 +1,56 @@
-var list = document.getElementById("blocks").childNodes;
-
-for (var i = 0; i < list.length; i++) {
+/* Rockets
+let list = document.getElementById("blocks").childNodes;
+for (let i = 0; i < list.length; i++) {
 list[i].addEventListener("click",
 function() {
 blocks.insertBefore(this, blocks.childNodes[0])
 });
 }
+*/
+
+// Travelers - work in progress
+
+let redRocket = document.querySelector('.block--red');
+redRocket.addEventListener("mousedown", function() {
+	let redTravel = redRocket.animate([{
+		transform: 'translate(0)'
+	}, {
+		transform: 'translate(800px, 100px)'
+	}], 800);
+});
+
+let blueRocket = document.querySelector('.block--blue');
+blueRocket.addEventListener("mousedown", function() {
+	let blueTravel = blueRocket.animate([{
+		transform: 'translate(0)'
+	}, {
+		transform: 'translate(800px, 100px)'
+	}], 800);
+});
+
+let greenRocket = document.querySelector('.block--green');
+greenRocket.addEventListener("mousedown", function() {
+	let greenTravel = greenRocket.animate([{
+		transform: 'translate(0)'
+	}, {
+		transform: 'translate(800px, 100px)'
+	}], 800);
+});
+
+let pinkRocket = document.querySelector('.block--pink');
+pinkRocket.addEventListener("mousedown", function() {
+	let pinkTravel = pinkRocket.animate([{
+		transform: 'translate(0)'
+	}, {
+		transform: 'translate(800px, 100px)'
+	}], 800);
+});
+
+let grayRocket = document.querySelector('.block--gray');
+grayRocket.addEventListener("mousedown", function() {
+	let grayTravel = grayRocket.animate([{
+		transform: 'translate(0)'
+	}, {
+		transform: 'translate(800px, 100px)'
+	}], 800);
+});
