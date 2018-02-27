283788c63292bd4430fff0de6971a4ca9190becd
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-02-27 15:10:08 -0600

    attempting to get event listener to work, but no luck yet

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
+const blocks = document.getElementsByClassName("blocks");
 const redBlock = document.getElementsByClassName("block block--red");
 const blueBlock = document.getElementsByClassName("block block--blue");
 const greenBlock = document.getElementsByClassName("block block--green");
 const pinkBlock = document.getElementsByClassName("block block--pink");
 const grayBlock = document.getElementsByClassName("block block--gray");
 
+blocks.addEventListener("click", (event) => {
+    if(event.target.className === red) {
+        console.log('red');
+    }
+});
+
 redBlock.addEventListener("click", (event) => {
+  event.color = 'red';
    
 });
 
 blueBlock.addEventListener("click", (event) => {
-if(event.target.class === blueBlock) {
-    console.log('blue box');
-    }
+    event.color = 'blue';
 });
 
 greenBlock.addEventListener("click", (event) => {
-
+    event.color = 'green';
 });
 
 pinkBlock.addEventListener("click", (event) => {
-
+    event.color = 'pink';
 });
 
 grayBlock.addEventListener("click", (event) => {
-
+    event.color = 'gray';
 });
