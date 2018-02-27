1bc87ef6afb3fa4db4b53c71fb1dbdd0dbb37091
hrehman000@citymail.cuny.edu <hrehman000@citymail.cuny.edu>
2018-02-27 16:22:29 -0500

    rockets completed

--- index.js
+++ index.js
@@ -4,16 +4,40 @@ const blueBlock = document.getElementsByClassName("block--blue")[0];
 const greenBlock = document.getElementsByClassName("block--green")[0];
 const pinkBlock = document.getElementsByClassName("block--pink")[0];
 const grayBlock = document.getElementsByClassName("block--gray")[0];
+const blocks = document.getElementsByClassName("blocks")[0];
 
 function rocket () {
     grayBlock.addEventListener("click", (event) => {
         toTop(event.target);
     });
+
+    redBlock.addEventListener("click", (event) => {
+        toTop(event.target);
+    });
+
+    blueBlock.addEventListener("click", (event) => {
+        toTop(event.target);
+    });
+
+    greenBlock.addEventListener("click", (event) => {
+        toTop(event.target);
+    });
+
+    pinkBlock.addEventListener("click", (event) => {
+        toTop(event.target);
+    });
 }
 
+ function init () {
+     Array.from(blocks.children).forEach(block => {
+         block.style.order = 0;
+     });
+ }
+
 function toTop(arg) {
-    arg.style.order = -1;
+    init();
+    arg.style.order--;
 }
  
-rocket();
+const timerInterval = setInterval (rocket(),10);
 // init function
