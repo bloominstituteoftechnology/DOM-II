a6cacfb7571b9f3041eb2232f76567a0e7a72cc2
zackhitch <zackhitch@users.noreply.github.com>
2018-02-27 12:25:46 -0800

    Started work on DOM-II - Zack Hitchcock

---
+++ index.js
@@ -0,0 +1,6 @@
+const red = document.getElementsByClassName('block--red');
+const blue = document.getElementsByClassName('block--blue');
+const green = document.getElementsByClassName('block--green');
+const pink = document.getElementsByClassName('block--pink');
+const gray = document.getElementsByClassName('block--gray');
+
