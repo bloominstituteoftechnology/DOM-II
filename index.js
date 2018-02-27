d7932e14af4c7c73cf3b9bc325d10f6795cd48a1
ubuntu <irishseanvaldivia@gmail.com>
2018-02-27 16:30:39 -0600

    Created my variables!

---
+++ index.js
@@ -0,0 +1,6 @@
+let redBlock = document.getElementsByClassName('block--red')[0];
+let blueBlock = document.getElementsByClassName('block--blue')[0];
+let greenBlock = document.getElementsByClassName('block--green')[0];
+let pinkBlock = document.getElementsByClassName('block--pink')[0];
+let grayBlock = document.getElementsByClassName('block--gray')[0];
+
