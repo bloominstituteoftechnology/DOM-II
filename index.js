de5333b653b1d00c625bd9bc537d0073efb39354
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-02-27 14:35:58 -0600

    added eventlistener method to each variable

--- index.js
+++ index.js
@@ -3,3 +3,25 @@ const blueBlock = document.getElementsByClassName("block block--blue");
 const greenBlock = document.getElementsByClassName("block block--green");
 const pinkBlock = document.getElementsByClassName("block block--pink");
 const grayBlock = document.getElementsByClassName("block block--gray");
+
+redBlock.addEventListener("click", (event) => {
+   
+});
+
+blueBlock.addEventListener("click", (event) => {
+if(event.target.class === blueBlock) {
+    console.log('blue box');
+    }
+});
+
+greenBlock.addEventListener("click", (event) => {
+
+});
+
+pinkBlock.addEventListener("click", (event) => {
+
+});
+
+grayBlock.addEventListener("click", (event) => {
+
+});
