affbfe0574e747e32b5705b6ad1fae473847fcf9
unknown <adfaris@gmail.com>
2018-02-27 17:48:22 -0500

    Solved rocket part of the project

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
-const blockRed = document.getElementsByClassName("block--red")
-const blockblue = document.getElementsByClassName("block--blue")
-const blockgreen = document.getElementsByClassName("block--green")
-const blockpink = document.getElementsByClassName("block--pink")
-const blockgray = document.getElementsByClassName("block--gray")
+// const blockRed = document.getElementsByClassName("block--red");
+// const blockblue = document.getElementsByClassName("block--blue");
+// const blockgreen = document.getElementsByClassName("block--green");
+// const blockpink = document.getElementsByClassName("block--pink");
+// const blockgray = document.getElementsByClassName("block--gray");
 
 
 
-blockRed.addEventListener("click", (event) => {
-    event.color === "red"
+
+// document.querySelector(".blocks").addEventListener("click", (event) => {
+//     document.querySelector(".blocks").removeChild(event.target);
+//     document.querySelector(".blocks").prepend(event.target);   
+// });
+
+const parent = document.querySelector(".blocks");
+
+
+parent.addEventListener("click", (event) => {
+parent.removeChild(event.target);
+parent.prepend(event.target); 
 });
 
-blockblue.addEventListener("click", (event) => {
-  event.color = "blue";
-});
+// theParent = document.getElementById("theParent");
+// theKid = document.createElement("div");
+// theKid.innerHTML = 'Are we there yet?';
 
-blockgreen.addEventListener("click", (event) => {
-  event.color = "blue";
-});
+// // append theKid to the end of theParent
+// theParent.appendChild(theKid);
 
-blockpink.addEventListener("click", (event) =>{
-    event.color = "pink";
-});
+// // prepend theKid to the beginning of theParent
+// theParent.insertBefore(theKid, theParent.firstChild);
 
-blockgray.addEventListener("click", (event) =>{
-    event.color = "gray";
-});
+
+
+// blockblue.addEventListener("click", (event) => {
+//   event.color = "blue";
+// });
+
+// blockgreen.addEventListener("click", () => {
+//   parent.prepend(blockgreen);
+// });
+
+// blockpink.addEventListener("click", (event) =>{
+//     event.color = "pink";
+// });
+
+// blockgray.addEventListener("click", (event) =>{
+//     event.color = "gray";
+// });
