8846cb19b768805f034ee577c5ae30f35fee56b4
unknown <jacobiswinning@gmail.com>
2018-02-27 16:46:12 -0700

    added function

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-const blocks = document.querySelector(".blocks");
-const red = document.querySelector(".block--red");
-const blue = document.querySelector(".block--blue");
-const green = document.querySelector(".block--green");
-const pink = document.querySelector(".block--pink");
-const gray = document.querySelector(".block--gray");
 
+const red = document.getElementsByClassName(".block--red"[0]);
+const blue = document.getElementsByClassName(".block--blue"[0]);
+const green = document.getElementsByClassName(".block--green"[0]);
+const pink = document.getElementsByClassName(".block--pink"[0]);
+const gray = document.getElementsByClassName(".block--gray"[0]);
+
+let count = -1;
+const bubble = (element) => {
+    element.target.style.order = count;
+    count -= 1;
+}
+red.addEventListener('click', bubble);
+blue.addEventListener('click', bubble);
+green.addEventListener('click', bubble);
+pink.addEventListener('click', bubble);
+gray.addEventListener('click', bubble);
