68ae0bd23316dce064a2fb3c9b56e77e7c5ce653
unknown <jacobiswinning@gmail.com>
2018-02-27 17:03:26 -0700

    fixed

---
+++ index.js
@@ -0,0 +1,16 @@
+const red = document.getElementsByClassName(".block--red"[0]);
+const blue = document.getElementsByClassName(".block--blue"[0]);
+const green = document.getElementsByClassName(".block--green"[0]);
+const pink = document.getElementsByClassName(".block--pink"[0]);
+const gray = document.getElementsByClassName(".block--gray"[0]);
+
+let count = -1;
+const bubble = (element) => {
+    element.target.style.order = count;
+    count -= 1;
+}
+red.addEventListener('click', bubble);
+blue.addEventListener('click', bubble);
+green.addEventListener('click', bubble);
+pink.addEventListener('click', bubble);
+gray.addEventListener('click', bubble);
