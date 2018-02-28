bfc544d11927a4771a2366f2d7d271796af548c0
William <wtpwinberg@gmail.com>
2018-02-27 17:19:49 -0800

    Polish

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const pink = document.getElementById("pink");
 const gray = document.getElementById("gray");
 let i = 0;
 let mover;
-<<<<<<< HEAD
 
 // RED
 red.addEventListener("mousedown", () => {
@@ -74,36 +73,6 @@ gray.addEventListener("mousedown", () => {
 
     gray.style.marginLeft = `${i}px`;
   }, 100);
-=======
-red.addEventListener("mousedown", () => {
-mover = window.setInterval(function () {
-  
-  i = i + 10;
-  console.log("this is i: ", i);
-
-  red.style.marginLeft = `${i}px`;
-  }, 250);
-});
-
-  red.addEventListener("mouseup", () => {
-    window.clearInterval(mover);
-    red.style.marginLeft = '10px';
-    i = 10;
-});
-
-
-pink.addEventListener("mousedown", (event) => {
-  pink.id = 'toRight';
-});
-blue.addEventListener("mousedown", (event) => {
-  blue.id = 'toRight';
-});
-green.addEventListener("click", (event) => {
-  green.id = 'toRight';
-});
-gray.addEventListener("click", (event) => {
-  gray.id = 'toRight';
->>>>>>> 3fcbf0467d54f04b0079480f4f957c272b667cb0
 });
 
 gray.addEventListener("mouseup", () => {
