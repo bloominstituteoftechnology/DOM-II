1387aa1dea82a12e218c0e9923f0c6403edc29e6
unknown <ruiz.james12@yahoo.com>
2018-02-27 17:52:24 -0800

    Part 1 Revised

--- index.js
+++ index.js
@@ -7,40 +7,39 @@ const box5 = document.getElementsByClassName("block--gray")[0];
 box1.addEventListener('click', (event) => {
     box1.style.order = -1;
     box2.style.order = 0;
-    box3.style.order = 0;
-    box4.style.order = 0;
-    box5.style.order = 0;
+    box3.style.order = 1;
+    box4.style.order = 2;
+    box5.style.order = 3;
 })
 
 box2.addEventListener('click', (event) => {
-    box1.style.order = 0;
+    box1.style.order = 3;
     box2.style.order = -1;
     box3.style.order = 0;
-    box4.style.order = 0;
-    box5.style.order = 0;
+    box4.style.order = 1;
+    box5.style.order = 2;
 })
 
 box3.addEventListener('click', (event) => {
-    box1.style.order = 0;
-    box2.style.order = 0;
+    box1.style.order = 2;
+    box2.style.order = 3;
     box3.style.order = -1;
     box4.style.order = 0;
-    box5.style.order = 0;
+    box5.style.order = 1;
 })
 
 box4.addEventListener('click', (event) => {
-    box1.style.order = 0;
-    box2.style.order = 0;
-    box3.style.order = 0;
+    box1.style.order = 1;
+    box2.style.order = 2;
+    box3.style.order = 3;
     box4.style.order = -1;
     box5.style.order = 0;
 })
 
 box5.addEventListener('click', (event) => {
     box1.style.order = 0;
-    box2.style.order = 0;
-    box3.style.order = 0;
-    box4.style.order = 0;
+    box2.style.order = 1;
+    box3.style.order = 2;
+    box4.style.order = 3;
     box5.style.order = -1;
 })
-
