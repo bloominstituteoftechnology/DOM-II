7c6b8152c93cfcf799d252f409885e7327d401d8
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2018-02-27 19:59:38 -0600

    Completed Travelers

--- index.js
+++ index.js
@@ -1,26 +1,79 @@
-const container = document.querySelector(".blocks");
 
-container.addEventListener('mousedown', moveRight);
 
-function moveRight(event) {
-    if (container.addEventListener) 
-    container.addEventListener('mouseup', moveLeft);
-    if (event.target=== container) return null;
+// Travelers 
+const red = document.getElementsByClassName('block block--red');
+const blue = document.getElementsByClassName('block block--blue');
+const green = document.getElementsByClassName('block block--green');
+const pink = document.getElementsByClassName('block block--pink');
+const gray = document.getElementsByClassName('block block--gray');
 
-    let margin = 2;
-    console.log(event);
-    const rightInterval = setInterval(() => {
-        event.target.style.marginLeft = '$(++margin)%';
-        if (margin === 80) clearInterval(rightInterval);
-    }, 100);
+let moveMarginLeftRed = 10;
+let moveMarginLeftBlue = 10;
+let moveMarginLeftGreen = 10;
+let moveMarginLeftPink = 10;
+let moveMarginLeftGray = 10;
 
-    function moveLeft() {
-        if (event.target === container) return null;
-        clearInterval(rightInterval);
-        console.log(event);
-        const leftInterval = setInterval(() => {
-            event.target.style.marginLeft = '$(--margin%)%';
-            if (margin <= 2) clearInterval(leftInterval);
-        }, 100);
+let moveRight;
+let moveLeft;
+
+red[0].addEventListener('mousedown', () => {
+  clearInterval(moveLeft);
+  moveRight = setInterval(function() {
+    moveMarginLeftRed += 10;
+    red[0].style.marginLeft = moveMarginLeftRed.toString() + '%';
+  }, 10);
+});
+blue[0].addEventListener('mousedown', () => {
+  clearInterval(moveLeft);
+  moveRight = setInterval(function() {
+    moveMarginLeftBlue += 10;
+    blue[0].style.marginLeft = moveMarginLeftBlue.toString() + '%';
+  }, 10);
+});
+green[0].addEventListener('mousedown', () => {
+  clearInterval(moveLeft);
+  moveRight = setInterval(function() {
+    moveMarginLeftGreen += 10;
+    green[0].style.marginLeft = moveMarginLeftGreen.toString() + '%';
+  }, 10);
+});
+pink[0].addEventListener('mousedown', () => {
+  clearInterval(moveLeft);
+  moveRight = setInterval(function() {
+    moveMarginLeftPink += 10;
+    pink[0].style.marginLeft = moveMarginLeftPink.toString() + '%';
+  }, 10);
+});
+gray[0].addEventListener('mousedown', () => {
+  clearInterval(moveLeft);
+  moveRight = setInterval(function() {
+    moveMarginLeftGray += 10;
+    gray[0].style.marginLeft = moveMarginLeftGray.toString() + '%';
+  }, 10);
+});
+
+window.addEventListener('mouseup', () => {
+  moveLeft = setInterval(function() {
+    clearInterval(moveRight);
+    if (moveMarginLeftRed >= 10) {
+      moveMarginLeftRed -= 1;
+      red[0].style.marginLeft = moveMarginLeftRed.toString() + '%';
     }
-}
+    if (moveMarginLeftBlue >= 10) {
+      moveMarginLeftBlue -= 1;
+      blue[0].style.marginLeft = moveMarginLeftBlue.toString() + '%';
+    }
+    if (moveMarginLeftGreen >= 10) {
+      moveMarginLeftGreen -= 1;
+      green[0].style.marginLeft = moveMarginLeftGreen.toString() + '%';
+    }
+    if (moveMarginLeftPink >= 10) {
+      moveMarginLeftPink -= 1;
+      pink[0].style.marginLeft = moveMarginLeftPink.toString() + '%';
+    }
+    if (moveMarginLeftGray >= 10) {
+      moveMarginLeftGray -= 1;
+      gray[0].style.marginLeft = moveMarginLeftGray.toString() + '%';
+    }
+  }, 10);
+});
