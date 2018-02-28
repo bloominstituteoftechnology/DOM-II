a8dda2494c828d98d2ff231f56e83f7658a471ec
unknown <jacobiswinning@gmail.com>
2018-02-27 17:31:52 -0700

    fixed

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-const red = document.getElementsByClassName(".block--red"[0]);
-const blue = document.getElementsByClassName(".block--blue"[0]);
-const green = document.getElementsByClassName(".block--green"[0]);
-const pink = document.getElementsByClassName(".block--pink"[0]);
-const gray = document.getElementsByClassName(".block--gray"[0]);
+let red = document.getElementsByClassName('block--red')[0];
+let blue = document.getElementsByClassName('block--blue')[0];
+let green = document.getElementsByClassName('block--green')[0];
+let pink = document.getElementsByClassName('block--pink')[0];
+let gray = document.getElementsByClassName('block--gray')[0];
 
+let count = -1;
 
-const bubble = (element) => {
-    element.target.style.order = -1 ;
-    
+function bubble(red) {
+    red.target.style.order = count;
+    count -= 1;
 }
-red.addEventListener('click', bubble());
-blue.addEventListener('click', bubble());
-green.addEventListener('click', bubble());
-pink.addEventListener('click', bubble());
-gray.addEventListener('click', bubble());
+
+red.addEventListener('click', bubble);
+blue.addEventListener('click', bubble);
+green.addEventListener('click', bubble);
+pink.addEventListener('click', bubble);
+gray.addEventListener('click', bubble);
