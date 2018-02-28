29c4c57d0c9148516dc04fbaf6ee52808099bf46
Shaun Kelly <Shaun@redskelly.local>
2018-02-27 19:46:09 -0500

    Rockets complete.

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
-const redBox = document.getElementsByClassName('block--red');
-const blueBox = document.getElementsByClassName('block--blue');
-const greenBox = document.getElementsByClassName('block--green');
-const pinkBox = document.getElementsByClassName('block--pink');
-const grayBox = document.getElementsByClassName('block--gray');
+const block = document.querySelectorAll(".block");
 
-// function that moves the element to the top
-    // if object is clicked, move to top
-
-// pass elements through function
-
-// possible setup: 
-document.getElementByClassName('block--red') = function() {};
-// -or-
-document.getElementById('block--red') = function() {}; // would need to add id in index.html
+block.forEach((elements) => {
+    elements.addEventListener("click", () => {        
+        const blocks = elements.parentNode;
+        blocks.removeChild(elements); // removes
+        blocks.prepend(elements); // adds element to start of list
+    });
+});
