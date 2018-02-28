a31fc7bbc64627982117ea250747eeb8435f35e1
WalterSWoodward <walter.sekope.woodward@gmail.com>
2018-02-27 20:36:58 -0500

    Completes Travelers

--- index.js
+++ index.js
@@ -4,15 +4,39 @@ let green = document.getElementsByClassName('block--green')[0];
 let pink = document.getElementsByClassName('block--pink')[0];
 let gray = document.getElementsByClassName('block--gray')[0];
 
-let count = 0;
+// let count = 0;
 
-function bubble(element) {
-    element.target.style.order = count;
-    count--;
+// function bubble(element) {
+//     element.target.style.order = count;
+//     count--;
+// }
+
+// red.addEventListener('click', bubble);
+// blue.addEventListener('click', bubble);
+// green.addEventListener('click', bubble);
+// pink.addEventListener('click', bubble);
+// gray.addEventListener('click', bubble);
+
+
+function travel(element) {
+    element.target.style.marginLeft = '50px';
 }
 
-red.addEventListener('click', bubble);
-blue.addEventListener('click', bubble);
-green.addEventListener('click', bubble);
-pink.addEventListener('click', bubble);
-gray.addEventListener('click', bubble);
+function travelLeft(element) {
+    element.target.style.marginLeft = '10px';
+}
+
+red.addEventListener('mousedown', travel);
+red.addEventListener('mouseup', travelLeft);
+
+blue.addEventListener('mousedown', travel);
+blue.addEventListener('mouseup', travelLeft);
+
+green.addEventListener('mousedown', travel);
+green.addEventListener('mouseup', travelLeft);
+
+pink.addEventListener('mousedown', travel);
+pink.addEventListener('mouseup', travelLeft);
+
+gray.addEventListener('mousedown', travel);
+gray.addEventListener('mouseup', travelLeft);
