d396da3ea06384d0a92b40f3ca8a18904fdd30e1
Sumayyah <sumayyah@zubairs-air.attlocal.net>
2018-02-27 16:43:39 -0800

    rockets finished

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
 const blocks = document.querySelectorAll(".block");
-// const red = document.querySelector(".block--red");
-// const blue = document.querySelector(".block--blue");
-// const green = document.querySelector(".block--green");
-// const pink = document.querySelector(".block--pink");
-// const gray = document.querySelector(".block--gray");
+blocks.forEach((block) =>  {
+    block.addEventListener("mousedown", () => {
+        block.style.transform = "translate(100px)"
+    });
+    block.addEventListener("mouseup", () => {
+        block.style.transform = "translate(0px)"
+    });
+    block.addEventListener("click", () => {
+        const parent = block.parentNode;
+        parent.removeChild(block);
+        parent.prepend(block);
+    });
+});
 
-// blocks.forEach((block) => {
-//   block.addEventListener("click", () => {
-//     console.log(block);
-//     const parent = block.parentNode;
-//     parent.removeChild(block);
-//     parent.prepend(block);
-//   });
-// });
-//take each element of blocks and name it block
-// can be called whatever
-
-for (let i = 0; i < blocks.length; i++) {
-    blocks[i].addEventListener("click", () => {
-        console.log(blocks[i]);
-        const parent = blocks[i].parentNode;
-        parent.removeChild(blocks[i]);
-        parent.prepend(blocks[i]);
-      });
-}
+// for (let i = 0; i < blocks.length; i++) {
+//     blocks[i].addEventListener("mousedown", () => {
+//         console.log(blocks[i]);
+//         blocks[i].style.transform = "translate(100px)"
+//     });
+//     blocks[i].addEventListener("mouseup", () => {
+//         console.log(blocks[i]);
+//         blocks[i].style.transform = "translate(0px)"
+//     });
+//     blocks[i].addEventListener("click", () => {
+//         console.log(blocks[i]);
+//         const parent = blocks[i].parentNode;
+//         parent.removeChild(blocks[i]);
+//         parent.prepend(blocks[i]);
+//     });
+// }
