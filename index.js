cdc1228bbeb24e4248c86a98dcd09a8abd2d8ced
Abraham Andujo <foxtara00@gmail.com>
2018-02-27 17:08:49 -0800

    Rocket

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 let block = document.getElementsByClassName("block");
 
 Array.from(block).forEach(function(element) {
-    element.addEventListener('mousedown', function () {
-        this.style.marginLeft = "30px";
-    });
-    element.addEventListener('mouseup', function () {
-        this.style.marginLeft = "10px";
-    })
-    element.addEventListener('mouseout', function () {
-        this.style.marginLeft = "10px";
+    element.addEventListener('click', function () {
+        element.parentElement.prepend(element);
     })
 });
