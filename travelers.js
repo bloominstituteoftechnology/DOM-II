1ddc975e5a9de299b657f26ef88e00b025dbd82e
Drew Moody <drewmoody@Drews-MBP.fios-router.home>
2018-02-27 20:11:53 -0500

    updated Travelers

--- travelers.js
+++ travelers.js
@@ -5,64 +5,47 @@ const greenBlock = document.querySelector(".block--green");
 const pinkBlock = document.querySelector(".block--pink");
 const grayBlock = document.querySelector(".block--gray");
 
-
+console.log(blocks.childNodes[1]);
 // const redBlockAppend = document.getElementsByClassName("blocks")[0].appendChild(redBlock);
 
-let hasBeenClicked = false;
-
 redBlock.addEventListener("mousedown", (event) => {
-  console.log(event);
-  redBlock.style.marginLeft = "50px";
-  hasBeenClicked = true;
+    
+    redBlock.style.marginLeft = '50px' // '50px'
+    console.log(parseInt(redBlock.style.marginLeft));
  });
  
  blueBlock.addEventListener("mousedown", (event) => {
-    blueBlock.classList.add("blockDownClick");
+    blueBlock.style.marginLeft = "50px";
  });
  
  greenBlock.addEventListener("mousedown", (event) => {
-   greenBlock.classList.add("blockDownClick");
+    greenBlock.style.marginLeft = "50px";
  });
  
  pinkBlock.addEventListener("mousedown", (event) => {
-   pinkBlock.classList.add("blockDownClick");
+    pinkBlock.style.marginLeft = "50px";
  });
  
  grayBlock.addEventListener("mousedown", (event) => {
-   grayBlock.classList.add("blockDownClick");
+    grayBlock.style.marginLeft = "50px";
  });
 
 redBlock.addEventListener("mouseup", (event) => {
-  // redBlock.classList.remove("blockDownClick");
-  if(hasBeenClicked) {
-    const twoSecsInterval = window.setTimeout(() => {
-      console.log('hiUP');
-      redBlock.style.marginLeft = "10px";
-    
-    }, 2000);
-  }
+    redBlock.style.marginLeft = "10px";
 });
  
  blueBlock.addEventListener("mouseup", (event) => {
-  const twoSecsInterval = window.setTimeout(() => {
-		redBlock.classList.remove("blockDownClick");
-    }, 2000);
+    blueBlock.style.marginLeft = "10px";
  });
  
  greenBlock.addEventListener("mouseup", (event) => {
-  const twoSecsInterval = window.setInterval(() => {
-		redBlock.classList.remove("blockDownClick");
-    }, 2000);
+    greenBlock.style.marginLeft = "10px";
  });
  
  pinkBlock.addEventListener("mouseup", (event) => {
-  const twoSecsInterval = window.setInterval(() => {
-		redBlock.classList.remove("blockDownClick");
-    }, 2000);
+    pinkBlock.style.marginLeft = "10px";
  });
  
  grayBlock.addEventListener("mouseup", (event) => {
-  const twoSecsInterval = window.setInterval(() => {
-		redBlock.classList.remove("blockDownClick");
-    }, 2000);
+    grayBlock.style.marginLeft = "10px";
  });
