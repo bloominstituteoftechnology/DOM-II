cdb7c9b5b2968b583a1b45d5578c4651de271ac3
ubuntu <irishseanvaldivia@gmail.com>
2018-02-28 11:13:21 -0600

    x

--- index.js
+++ index.js
@@ -1,53 +1,16 @@
-const redBlock = document.querySelector(".block--red");
-const blueBlock = document.querySelector(".block--blue");
-const greenBlock = document.querySelector(".block--green");
-const pinkBlock = document.querySelector(".block--pink");
-const grayBlock = document.querySelector(".block--gray");
+/* ——— https://codepen.io/lefrenk/pen/YedpoY ——— */
+const blocks = document.querySelectorAll(".block");
 
-
-grayBlock.addEventListener("click", (event) =>{
-    grayBlock.style.order = "1";
-    redBlock.style.order = "5";
-    pinkBlock.style.order = "4";
-    greenBlock.style.order = "3";
-    blueBlock.style.order = "2";
-});
-
-pinkBlock.addEventListener("click", (event) =>{
-    grayBlock.style.order = "4";
-    redBlock.style.order = "5";
-    pinkBlock.style.order = "1";
-    greenBlock.style.order = "3";
-    blueBlock.style.order = "2";
-});
-
-greenBlock.addEventListener("click", (event) =>{
-    grayBlock.style.order = "3";
-    redBlock.style.order = "5";
-    pinkBlock.style.order = "4";
-    greenBlock.style.order = "1";
-    blueBlock.style.order = "2";
-});
-
-pinkBlock.addEventListener("click", (event) =>{
-    grayBlock.style.order = "4";
-    redBlock.style.order = "5";
-    pinkBlock.style.order = "1";
-    greenBlock.style.order = "3";
-    blueBlock.style.order = "2";
-});
-
-redBlock.addEventListener("click", (event) =>{
-    grayBlock.style.order = "5";
-    redBlock.style.order = "1";
-    pinkBlock.style.order = "4";
-    greenBlock.style.order = "3";
-    blueBlock.style.order = "2";
-});
-blueBlock.addEventListener("click", (event) =>{
-    grayBlock.style.order = "5";
-    redBlock.style.order = "2";
-    pinkBlock.style.order = "4";
-    greenBlock.style.order = "3";
-    blueBlock.style.order = "1";
+blocks.forEach(block => {
+  block.addEventListener("click", () => {
+    const parent = block.parentNode;
+    parent.removeChild(block);
+    parent.prepend(block);
+  });
+  block.addEventListener("mousedown", () => {
+    block.style.transform = "translate(75px)"
+  });
+  block.addEventListener("mouseup", () => {
+    block.style.transform = "translate(0px)"
+  });
 });
