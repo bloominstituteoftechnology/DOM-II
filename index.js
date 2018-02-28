6364e32a6d2de1bd8fe3c80e167551f8c22e6d6c
Sagdi Formanov <forsy@Sagdis-MacBook-Pro.local>
2018-02-27 16:19:37 -0800

    got travelers working

--- index.js
+++ index.js
@@ -1,105 +1,77 @@
 const blocks = document.querySelector(".blocks");
-
 const red = document.querySelector(".block--red");
 const green = document.querySelector(".block--green");
 const blue = document.querySelector(".block--blue");
 const gray = document.querySelector(".block--gray");
 const pink = document.querySelector(".block--pink");
-const empty = document.querySelector(".empty");
+
+function endMove(param) {
+  clearInterval(param);
+}
 
 red.addEventListener("mousedown", (event) => {
   console.log("red is pressed");
-  let margin = 30;
-  const right = window.setInterval(() => {
-    red.style.marginLeft = `${margin++}px`;
-    //red.style.marginLeft = `${margin + 30}px`;
+  let margin = 10;
+  const right = setInterval(() => {
+    event.target.style.marginLeft = `${margin++}px`;
+    if (margin === 40) endMove(right);
   }, 10);
 });
 
-
 red.addEventListener("mouseup", (event) => {
   console.log("red is released");
+  endMove(event);
   red.style.marginLeft = "10px";
-  clearInterval(red);
 });
 
+green.addEventListener("mousedown", (event) => {
+  let margin = 10;
+  const right = setInterval(() => {
+    event.target.style.marginLeft = `${margin++}px`;
+    if (margin === 40) endMove(right);
+  }, 10);
+});
 
+green.addEventListener("mouseup", (event) => {
+  endMove(event);
+  green.style.marginLeft = "10px";
+});
 
+blue.addEventListener("mousedown", (event) => {
+  let margin = 10;
+  const right = setInterval(() => {
+    event.target.style.marginLeft = `${margin++}px`;
+    if (margin === 40) endMove(right);
+  }, 10);
+});
 
+blue.addEventListener("mouseup", (event) => {
+  endMove(event);
+  blue.style.marginLeft = "10px";
+});
 
+gray.addEventListener("mousedown", (event) => {
+  let margin = 10;
+  const right = setInterval(() => {
+    event.target.style.marginLeft = `${margin++}px`;
+    if (margin === 40) endMove(right);
+  }, 10);
+});
 
+gray.addEventListener("mouseup", (event) => {
+  endMove(event);
+  gray.style.marginLeft = "10px";
+});
 
+pink.addEventListener("mousedown", (event) => {
+  let margin = 10;
+  const right = setInterval(() => {
+    event.target.style.marginLeft = `${margin++}px`;
+    if (margin === 40) endMove(right);
+  }, 10);
+});
 
-
-
-
-
-
-
-
-// blocks.addEventListener("click", (event) => {
-//   // console.log("it worked", event);
-// 
-//   if (event.target.id === "red") {
-//     console.log("red");
-//     empty.style.backgroundColor = "red";
-//     empty.style.display = "inline-block";
-//     red.style.display = "none";
-// 
-//     green.style.display = "inline-block";
-//     blue.style.display = "inline-block";
-//     gray.style.display = "inline-block";
-//     pink.style.display = "inline-block";
-//   }
-//   if (event.target.id === "green") {
-//     console.log("green");
-//     empty.style.backgroundColor = "green";
-//     empty.style.display = "inline-block";
-//     green.style.display = "none";
-// 
-//     red.style.display = "inline-block";
-//     blue.style.display = "inline-block";
-//     gray.style.display = "inline-block";
-//     pink.style.display = "inline-block";
-//   }
-//    if (event.target.id === "blue") {
-//     console.log("blue");
-//     empty.style.backgroundColor = "blue";
-//     empty.style.display = "inline-block";
-//     blue.style.display = "none";
-// 
-//     green.style.display = "inline-block";
-//     red.style.display = "inline-block";
-//     gray.style.display = "inline-block";
-//     pink.style.display = "inline-block";
-//   }
-//  if (event.target.id === "gray") {
-//     console.log("gray");
-//     empty.style.backgroundColor = "gray";
-//     empty.style.display = "inline-block";
-//     gray.style.display = "none";
-// 
-//     green.style.display = "inline-block";
-//     blue.style.display = "inline-block";
-//     red.style.display = "inline-block";
-//     pink.style.display = "inline-block";
-//   }
-//  if (event.target.id === "pink") {
-//     console.log("pink");
-//     empty.style.backgroundColor = "pink";
-//     empty.style.display = "inline-block";
-//     pink.style.display = "none";
-// 
-//     green.style.display = "inline-block";
-//     blue.style.display = "inline-block";
-//     gray.style.display = "inline-block";
-//     red.style.display = "inline-block";
-//   }
-// 
-// });
-
-
-
-// some comment
-
-
+pink.addEventListener("mouseup", (event) => {
+  endMove(event);
+  pink.style.marginLeft = "10px";
+});
