ba123b3f515801b49eb5df7fd18be4595920f901
Cesar Mejia Leiva <cmejia@gmail.com>
2018-03-04 19:48:24 -0800

    rockets; cascade implementation complete; cleaned up comments

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ for (let i = 0; i < block.length; i++) {
   boxClass[i] = boxClassList[1]; 
 }
 
-let oldboxClass = boxClass;
-let clickedBox = 0;
-
 // addEventListener to each box 
 // Clicked box moves to top of boxClass array
 // other boxes cascade down
@@ -20,12 +17,15 @@ let clickedBox = 0;
 for (let i = 0; i < block.length; i++) {
   block[i].addEventListener("click", () => {
 
+    // old class order removed
     for (let j = 0; j < block.length; j++) {
       block[j].classList.remove(boxClass[j]);
     }
+
     boxClass.splice(0, 0, boxClass[i]);
     boxClass.splice(i + 1, 1);
 
+    // new class order added
     for (let j = 0; j < block.length; j++) {
       block[j].classList.add(boxClass[j]);
     }
