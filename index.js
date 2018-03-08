456474159aa10224c7076804514dcd7b58a5b1af
Alexander <alexander.botello@g.austincc.edu>
2018-03-07 21:57:03 -0600

    travelers complete

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const blocks = document.querySelectorAll(".block");
 blocks.forEach((element) => {
-    element.addEventListener("click", () => {
-        const parent = element.parentNode;
-        parent.removeChild(element);
-        parent.prepend(element);
+    element.addEventListener("mousedown", () => {
+        element.style.transform = "translateX(200px)";
+    });
+
+    element.addEventListener("mouseup", () => {
+        element.style.transform = "translateX(0px)";
     });
 });
