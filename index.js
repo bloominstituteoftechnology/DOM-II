348e4e345fac4144964b3cb16fa61738513f6f99
Bhavik Ravani <ravanibhavik@gmail.com>
2018-03-07 23:03:52 -0500

    Completed Task 1 - Rockets

---
+++ index.js
@@ -0,0 +1,14 @@
+const blocks = document.querySelector(".blocks").childNodes;
+console.log(blocks);
+let orderValue = -1;
+
+function moveToTop(event) {
+  console.log(event.target.classList[1]);
+  event.target.style.order = orderValue;
+  orderValue -= 1;
+}
+
+blocks.forEach((block) => {
+  console.log(block);
+  block.addEventListener('click', moveToTop);
+});
