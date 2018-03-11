76e0b1879e010e0a7eafafbf694b5b9c9f6eedc7
Bhavik Ravani <ravanibhavik@gmail.com>
2018-03-11 19:13:10 -0400

    Completed Task 2 - travelers

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const blocks = document.querySelectorAll(".block");
-console.log(blocks);
 let orderValue = -1;
 
 function moveToTop(event) {
-  console.log(event.target.classList[1]);
   event.target.style.order = orderValue;
   orderValue -= 1;
 }
 
+// Move to Right using CSS property
+function moveToRight(event) {
+  event.target.style.transition = "margin-left 2s linear 1s";
+  event.target.style.marginLeft = "788px";
+}
+
+// Object to keep track of element interval id.
+const elementIntervalDict = {};
+
+// Funtion will start setInterval if block is on far left corner.
+// Clear interval otherwise.
+const traveler = (event) => {
+  console.log(elementIntervalDict);
+  if (event.target.style.marginLeft === "") {
+    let intervalId = setInterval(moveToRight.bind(this, event.target), 1000);
+    elementIntervalDict[event.target.classList[1]] = intervalId;
+  } else {
+    event.target.style.marginLeft = "";
+    clearInterval(elementIntervalDict[event.target.classList[1]]);
+  }
+}
+
+// Funtion to move block till it reaches right corner or user clicks block again.
+function moveToRight(element) {
+  if (element.style.marginLeft === "") {
+    element.style.marginLeft = "10px"
+  } else {
+    currentMargin = parseInt(element.style.marginLeft);
+    if (currentMargin < 788) { 
+      element.style.marginLeft = currentMargin + 10 + "px";
+    }
+  }
+}
+
+// Associate click event listener with each block.
 blocks.forEach((block) => {
-  console.log(block);
   block.addEventListener('click', moveToTop);
+  block.addEventListener('click', moveToRight);
+  block.addEventListener('click', traveler);
 });
