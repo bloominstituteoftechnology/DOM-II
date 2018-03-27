b908d3bbbd7e54d71a8fae8d05799733a7aea2b7
Cruise Brantley <cruise.brantley@gmail.com>
2018-03-27 16:41:16 -0500

    updated rockets commit with queryselector to remove array syntax

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
+const redBlock = document.querySelector('.block--red');
+const blueBlock = document.querySelector('.block--blue');
+const greenBlock = document.querySelector('.block--green');
+const pinkBlock = document.querySelector('.block--pink');
+const grayBlock = document.querySelector('.block--gray');
+
 // Rockets commit
-const redBlock = document.getElementsByClassName('block--red');
-const blueBlock = document.getElementsByClassName('block--blue');
-const greenBlock = document.getElementsByClassName('block--green');
-const pinkBlock = document.getElementsByClassName('block--pink');
-const grayBlock = document.getElementsByClassName('block--gray');
 let count = 0;
 
 const blockTop = (event) => {
     event.target.style.order = count--;
 }
 
-blueBlock[0].addEventListener('click', blockTop);
-redBlock[0].addEventListener('click', blockTop);
-greenBlock[0].addEventListener('click', blockTop);
-pinkBlock[0].addEventListener('click', blockTop);
-grayBlock[0].addEventListener('click', blockTop);
+blueBlock.addEventListener('click', blockTop);
+redBlock.addEventListener('click', blockTop);
+greenBlock.addEventListener('click', blockTop);
+pinkBlock.addEventListener('click', blockTop);
+grayBlock.addEventListener('click', blockTop);
+
+/*
+// Travelers commit
+
+const goRight = (event) => {
+
+}
+
+blueBlock.addEventListener('mousedown', goRight);
+redBlock.addEventListener('mousedown', goRight);
+greenBlock.addEventListener('mousedown', goRight);
+pinkBlock.addEventListener('mousedown', goRight);
+grayBlock.addEventListener('mousedown', goRight);
+*/
