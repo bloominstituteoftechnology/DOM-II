11dd8584d8ce21d659c617ae984195d8e9273137
cgoez <goez2001@gmail.com>
2018-03-27 15:32:17 -0700

    box travel right on mouse down

--- index.js
+++ index.js
@@ -7,37 +7,47 @@ const pinkBlock = document.querySelector('.block--pink')
 const grayBlock = document.querySelector('.block--gray')
 
 // event handler rockets
-
-const rockets = (event) => {
-    // event.stopPropagation(); // not sure if needed yet
+const rockets = (event) => {    
     const parent = event.target.parentNode;
-    parent.removeChild(event.target);
-    // event.target.style.order = -1;
+    parent.removeChild(event.target);    
     parent.prepend(event.target);
+}
+
+// STRETCH GOALS
+// travelers
+
+// const travelRed = (event) => {
+//     // let traveler = event.target;
+//     // traveler = event.currentTarget;       
+//     redBlock.style.marginLeft += '1px';    
+//     const travelers = setInterval(travelRed, 10);      
+// }
+
+const travelEvent = (e) => {
+    let distance = 0
+    const moveRight = () => {
+      distance += 10
+      e.target.style.marginLeft = `${distance}px`;
+    }
+    setInterval(moveRight, 10);    
+  }
+
+const travelBack = (event) => {
+    // event.target.style.marginLeft = '10px';
+    clearInterval(travelRight);
+    e.target.style.transform = 'translateX(10px)';
 }
 
 // event listener
 blocks.forEach((element) => {
     element.addEventListener('click', rockets); 
+    element.addEventListener('mousedown', travelEvent);
+    element.addEventListener('mouseup', travelBack);
+
+    // redBlock.addEventListener('mousedown', travelRed);
+    // blueBlock.addEventListener('mousedown', travelBlue);
+    // blueBlock.addEventListener('mouseup', travelBack);
+    // greenBlock.addEventListener('mouseup', travelBack);
+    // pinkBlock.addEventListener('mouseup', travelBack);
+    // grayBlock.addEventListener('mouseup', travelBack);   
 })
-
-
-// alt attempt
-
-// // rockets, go to top when clicked
-
-// const rockets = (event) => {
-//     // event.stopPropagation(); // not sure if needed yet
-//     const parent = event.target.parentNode;
-//     parent.removeChild(event.target);
-//     // event.target.style.order = -1;
-//     parent.prepend(event.target);
-// }
-
-// rockets event listeners
-
-// redBlock.addEventListener('click', rockets);
-// blueBlock.addEventListener('click', rockets);
-// greenBlock.addEventListener('click', rockets);
-// pinkBlock.addEventListener('click', rockets);
-// grayBlock.addEventListener('click', rockets);
