4021dc728e924e5cab04ff3b09eb9ed71895c678
Jonathan Greene <greeneselectronics@gmail.com>
2018-03-27 16:02:17 -0600

    added in movement to the right and then back to the left.

--- index.js
+++ index.js
@@ -34,3 +34,82 @@ function moveGrayBlock() {
   //  const newboxs = grayBox[0].style.color = 'red';
   //   console.log(newbox);
   }
+
+  //////////////////////////////////////////////////////////////
+  // this seperates the moving up to the moving right 
+
+  function moveGrayRight() {
+    console.log('This is moving  right!')
+    let grayBox = document.getElementsByClassName('block--gray');
+   const  newbox = grayBox[0].style.marginLeft = '700%';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function movePinkRight() {
+    console.log('This is moving  right!')
+    let grayBox = document.getElementsByClassName('block--pink');
+   const  newbox = grayBox[0].style.marginLeft = '700%';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function moveGreenRight() {
+    console.log('This is moving  right!')
+    let grayBox = document.getElementsByClassName('block--green');
+   const  newbox = grayBox[0].style.marginLeft = '700%';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function moveBlueRight() {
+    console.log('This is moving  right!')
+    let grayBox = document.getElementsByClassName('block--blue');
+   const  newbox = grayBox[0].style.marginLeft = '700%';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function moveRedRight() {
+    console.log('This is moving  right!')
+    let grayBox = document.getElementsByClassName('block--red');
+   const  newbox = grayBox[0].style.marginLeft = '700%';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // This is for moving back!
+
+  function moveGrayBack() {
+    console.log('This is moving  right!')
+    let grayBox = document.getElementsByClassName('block--gray');
+   const  newbox = grayBox[0].style.marginLeft = '0';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function movePinkBack() {
+    console.log('This is moving  right!')
+    let grayBox = document.getElementsByClassName('block--pink');
+   const  newbox = grayBox[0].style.marginLeft = '0';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function moveGreenBack() {
+    console.log('This is moving  right!')
+    let grayBox = document.getElementsByClassName('block--green');
+   const  newbox = grayBox[0].style.marginLeft = '0';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function moveBlueBack() {
+    console.log('This is moving  right!')
+    let grayBox = document.getElementsByClassName('block--blue');
+   const  newbox = grayBox[0].style.marginLeft = '0';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function moveRedBack() {
+    console.log('This is moving  right!')
+    let grayBox = document.getElementsByClassName('block--red');
+   const  newbox = grayBox[0].style.marginLeft = '0';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
