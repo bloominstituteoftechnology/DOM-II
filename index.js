5b5e895b515ca20e1b2af442552b331eab1fce6a
David Finley <dfinley2917@gmail.com>
2018-03-27 18:59:27 -0400

    Rocket completed

---
+++ index.js
@@ -0,0 +1,12 @@
+const blocks = document.querySelector('.blocks');
+const ourFunction = (event) => {
+    event.stopPropagation();
+    let parent = event.target.parentNode;
+    parent.removeChild(event.target);
+    parent.prepend(event.target);
+};
+
+for (i = 0; i < blocks.children.length; i++) {
+blocks.children[i].addEventListener('click', ourFunction);
+}
+
