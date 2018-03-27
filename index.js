31f4ec448fbed6390dc20ab90c9d6f636dbcf0a7
totsuya <supertatortots@gmail.com>
2018-03-27 15:33:02 -0400

    added variables

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 const allBlocks = document.querySelector(".blocks");
-const redBlock = document.getElementById("redBlock")
+const redBlock = document.getElementById("redBlock");
+const blueBlock = document.getElementById("blueBlock");
+const greenBlock = document.getElementById("greenBlock");
+const pinkBlock = document.getElementById("pinkBlock");
+const grayBlock = document.getElementById("grayBlock");
