9ae0d65f22142db377b757447b0b6759f9baee6c
nickbrenn <nbren@tutanota.com>
2018-03-27 18:58:41 -0400

    Finished travelers mostly

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
-const changecolor = (event) => {
-    event.target.style.backgroundColor = 'pink';
+// rockets code
+
+// let lowest = 0;
+
+// const blocks = document.querySelector('.blocks');
+
+// const rocket = (event) => {
+//     lowest--;
+//     event.target.style.order = lowest;
+// }
+
+// blocks.addEventListener('click', rocket);
+
+
+
+
+
+// traveller code
+
+const block = document.querySelectorAll('.block');
+// let offset = document.querySelectorAll('.block').style.width;
+
+let moveBy = window.innerWidth - 100;
+
+const moveRight = (event) => {
+    event.target.style.transform = 'translate('+moveBy+'%)';
 }
 
-let lowest = 0;
-
-const blocks = document.querySelector('.blocks');
-
-const rocket = (event) => {
-    lowest--;
-    event.target.style.order = lowest;
+const moveLeft = (event) => {
+    event.target.style.transform = 'translate(0px)';
 }
 
-blocks.addEventListener('click', rocket);
+for (let i = 0; i < block.length; i++) {
+    block[i].style.transitionDuration = '6s';
+    block[i].addEventListener('mousedown', moveRight);
+    block[i].addEventListener('mouseenter', moveRight);
+    block[i].addEventListener('mouseenter', moveLeft);
+    block[i].addEventListener('mouseleave', moveLeft);
+    block[i].addEventListener('mouseup', moveLeft);
+}
