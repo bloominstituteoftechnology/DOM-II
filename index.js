0e7178402625cd7f6111e0e63e14580b6b2f15c2
EricGomez101 <eric_gomez_1@hotmail.com>
2018-03-27 15:19:00 -0700

    travelers assignment complete

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 const blockContainer = document.getElementsByClassName('blocks')[0];
 
 window.onload = function() {
-  blockContainer.addEventListener('click', (event) => {
+
+  // rocket assignment
+   // blockContainer.addEventListener('click', (event) => {
+   //   if (event.target.parentNode.className === "blocks") {
+   //     event.target.parentNode.insertBefore(event.target, event.target.parentNode.childNodes[0]);
+   //   }
+   // });
+  let x;
+
+  function moveRight(event) {
+    x = setInterval(function() {
+      if (event.target.style.marginLeft === undefined || event.target.style.marginLeft === null || parseInt(event.target.style.marginLeft) === 0 || event.target.style.marginLeft === "") {
+        event.target.style.marginLeft = "0px";
+      }
+      event.target.style.marginLeft = parseInt(event.target.style.marginLeft) + 1 + "px";
+    }, 10);
+  }
+  function reset(event) {
+    x = setInterval(function() {
+      event.target.style.marginLeft = parseInt(event.target.style.marginLeft) - 1 + "px";
+      if (parseInt(event.target.style.marginLeft) === 0) {
+        clearInterval(x);
+      }
+    }, 10);
+  }
+  blockContainer.addEventListener('mousedown', (event) => {
+    clearInterval(x);
     if (event.target.parentNode.className === "blocks") {
-      console.log(event.target.offsetLeft);
-      event.target.offsetLeft += 100;
-      console.log(event.target.offsetLeft);
-      event.target.parentNode.insertBefore(event.target, event.target.parentNode.childNodes[0]);
+      moveRight(event);
+    }
+  });
+  blockContainer.addEventListener('mouseup', (event) => {
+    if (event.target.parentNode.className === "blocks"){
+      clearInterval(x);
+      reset(event);
     }
   });
 }
