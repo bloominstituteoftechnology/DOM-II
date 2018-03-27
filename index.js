b17ad847fbd68a2bfe7812fbd528041199f0fd28
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-03-27 14:13:50 -0500

    completed rockets

---
+++ index.js
@@ -0,0 +1,15 @@
+// gives an array-like object containing all elements that have a .block
+const block = document.querySelectorAll('.block');
+
+// iterate  over each item in array and attach addEventListener method to each item
+block.forEach((item) => {
+item.addEventListener('click', (event) => {
+    // access parent div 'blocks'
+    const parent = item.parentElement;
+    // remove clicked 'block' from parent
+    parent.removeChild(item);
+
+    // prepend attaches any item or in this case 'block' to the parent as the parent's first child element
+    parent.prepend(item);
+});
+});
