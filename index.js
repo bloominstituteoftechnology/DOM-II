640d2cce05dae1aba4e3aa6b059e2551271007b1
Justin Hong <jhong2009@gmail.com>
2018-03-27 12:42:43 -0700

    set up move() event handler and added comments for rockets

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const green = document.querySelector('.block--green');
 const pink = document.querySelector('.block--pink');
 const gray = document.querySelector('.block--gray');
 
+// holds current value of flex order at the time of a 'click' event
+let order = 0;
 // event
-const move = {
-	
+const move = () => {
+	// shift a box to the start of the HTML collection
+	// decrement order: (-) orders will move to the top of the main-axis
+	// default order is 0 for all blocks
+	order--;
+	event.target.style.order = order.toString();
 }
 
 // event listeners
