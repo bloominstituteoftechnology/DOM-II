13164f222bed783dd153a1c89d713133d6dc7cbb
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-03-27 15:48:28 -0500

    completed travelers. Deleted an extra parenthesis that was causing issue

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
+// ################################ ROCKETS #################################
+
 // gives an array-like object containing all elements that have a .block
 const block = document.querySelectorAll('.block');
 
 // iterate  over each item in array and attach addEventListener method to each item
 block.forEach((item) => {
 item.addEventListener('click', (event) => {
+    
     // access parent div 'blocks'
     const parent = item.parentElement;
     // remove clicked 'block' from parent
-    parent.removeChild(item);
+    // parent.removeChild(item);
 
     // prepend attaches any item or in this case 'block' to the parent as the parent's first child element
     parent.prepend(item);
 });
 });
+
+// ################################ TRAVELERS #################################
+   
+
+block.forEach((item) => {
+    item.addEventListener('mousedown', (event) => {
+        event.target.style.position = 'relative';
+        event.target.style.transform = 'translate(600px)';       
+});
+
+item.addEventListener('mouseout', (event) => {
+    event.target.style.position = 'relative';
+    event.target.style.transform = 'translate(0)'; 
+});
+});
+
+
+
+   
+
