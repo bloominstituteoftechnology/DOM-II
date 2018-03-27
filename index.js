4ec7142b25b3cc6d7bf4cf0e0e2eca8807d2b3a8
totsuya <supertatortots@gmail.com>
2018-03-27 15:43:46 -0400

    added child nodes and vaariable for order counting

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-const allBlocks = document.querySelector(".blocks");
+const allBlocks = document.querySelector(".blocks").childNodes; //includes all the child blocks
 const redBlock = document.getElementById("redBlock");
 const blueBlock = document.getElementById("blueBlock");
 const greenBlock = document.getElementById("greenBlock");
 const pinkBlock = document.getElementById("pinkBlock");
 const grayBlock = document.getElementById("grayBlock");
+
+let orderCounter = -1 //acts as a counter for the order
