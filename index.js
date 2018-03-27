fd4e3388d275aede9c2daa1cab190cd8ee240bff
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2018-03-27 13:46:58 -0700

    got blocks to move right on mouse down

--- index.js
+++ index.js
@@ -14,19 +14,24 @@ childrenArr.map(child => {
   child.addEventListener('click', toTop)
 })
 
+let mouse = false
 // While a mouse is clicked down on a box, it should move to the right
+// A mouse down that occurs during a transition back to the original position should send it traveling to the right agian from its current position
 // TRAVELERS
 const moveToRight = node => {
-  let curLeft = parseInt(node.style.left) || 0
-  curLeft = `${curLeft + 1}px`
-  node.style.marginLeft = curLeft
+  if (mouse) {
+    setInterval(() => {
+      (node.style.marginLeft = `${(parseInt(node.style.marginLeft)|| 0) +10}px`)
+    }, 100)
+  }
 }
 
 const moveRight = e => {
+  mouse = true
   let childToMove = e.target
   // move right every 100ms
-  setInterval(moveToRight(childToMove), 100)
-  stopPropagation() 
+  moveToRight(childToMove)
+  e.stopPropagation() 
 }
 
 childrenArr.map(child => child.addEventListener('mousedown', moveRight)) 
