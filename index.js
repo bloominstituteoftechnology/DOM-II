6ce1d908e6abc2c67ef4fdb6c9731d99fd71b533
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-03-27 18:55:03 -0400

    Robert Hogan, Paul Fang. Assignment completed.

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const greenBlock = document.querySelector('.block--green');
 const pinkBlock = document.querySelector('.block--pink');
 const grayBlock = document.querySelector('.block--gray');
 
-let position = 0; // may require double-click/ x2 double-clicks
+// ROCKETS ASSIGNMENT BELOW
+
+let position = 0;// may require double-click/ x2 double-clicks
 const moveToTop = (event) => {
     event.target.style.order = position;
     position--;
@@ -16,3 +18,35 @@ const moveToTop = (event) => {
   pinkBlock.addEventListener('click', moveToTop);
   grayBlock.addEventListener('click', moveToTop);
   redBlock.addEventListener('click', moveToTop);
+
+// TRAVELERS ASSIGNMENT BELOW
+
+const moveToRight = (event) => {
+    event.stopPropagation();
+    event.target.style.marginLeft = '1300px';
+}
+
+
+const moveToRight = (event) => {
+    let margin = 0;
+    const increment = () => {
+        margin++;
+        event.target.style.marginLeft = `${margin}px`
+    }
+    const incrementMl = setInterval(increment, 5);
+}
+
+
+redBlock.addEventListener('mousedown', moveToRight);
+blueBlock.addEventListener('mousedown', moveToRight);
+greenBlock.addEventListener('mousedown', moveToRight);
+pinkBlock.addEventListener('mousedown', moveToRight);
+grayBlock.addEventListener('mousedown', moveToRight);
+
+redBlock.addEventListener('mouseup', moveBack);
+blueBlock.addEventListener('mouseup', moveBack);
+greenBlock.addEventListener('mouseup', moveBack);
+pinkBlock.addEventListener('mouseup', moveBack);
+grayBlock.addEventListener('mouseup', moveBack);
+
+
