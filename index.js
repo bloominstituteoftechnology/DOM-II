a821be5c20dbfd29c4cd8a27c526fda1b6bd39fa
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-03-27 15:22:01 -0500

    rockets works fine, travelers is wip

--- index.js
+++ index.js
@@ -20,3 +20,43 @@
          this.style.marginLeft = "10px";
      });
  })
+
+
+ 
+
+//  // ################################ ROCKETS #################################
+
+// // gives an array-like object containing all elements that have a .block
+// const block = document.querySelectorAll('.block');
+
+// // iterate  over each item in array and attach addEventListener method to each item
+// block.forEach((item) => {
+// item.addEventListener('click', (event) => {
+    
+//     // access parent div 'blocks'
+//     const parent = item.parentElement;
+//     // remove clicked 'block' from parent
+//     // parent.removeChild(item);
+
+//     // prepend attaches any item or in this case 'block' to the parent as the parent's first child element
+//     parent.prepend(item);
+// });
+// });
+
+// // ################################ TRAVELERS #################################
+   
+
+// block.forEach((item) => {
+//     item.addEventListener('mousedown', (event) => {
+        
+//         event.target.style.position = 'relative';
+//         event.target.style.transform = 'translate(400px)';
+        
+        
+// });
+
+//     item.addEventListener('mouseout'), (event) => {
+//         event.target.style.position = 'relative';
+//         event.target.style.transform = 'translate(-400px)';
+//     }
+// });
