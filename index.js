0998d583057c9622f36953ddad4f59132f3bc765
K Haag <kh@Ks-MacBook-Pro.local>
2018-03-27 14:54:24 -0700

    DOM-II Push4?

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const nodeList = document.getElementsByClassName('blocks')[0]
 let timer;
-nodeList.addEventListener('click', (event) => {
+// nodeList.addEventListener('click', (event) => {
+//     if (event.target.parentNode.className === "blocks") {
+//       event.target.parentNode.insertBefore(event.target, event.target.parentNode.childNodes[0]);
+//     }
+// });
+
+
+nodeList.addEventListener('mousedown', (event) => {
     if (event.target.parentNode.className === "blocks") {
-      event.target.parentNode.insertBefore(event.target, event.target.parentNode.childNodes[0]);
+        timer = window.setInterval(function(){
+        if(event.target.style.marginLeft === undefined || event.target.style.marginLeft === '' || event.target.style.marginLeft === 0 ) {
+            event.target.style.marginLeft = '0px'}
+        console.log('working')
+        event.target.style.marginLeft = parseInt(event.target.style.marginLeft) + 10 + 'px';
+        },1000)  
     }
 });
-let counter =0;
-nodeList.addEventListener('mousedown', (event) => {
-    timer = window.setInterval(function(){
+
+nodeList.addEventListener('mouseup', (event) => {
+    clearInterval(timer);
     if (event.target.parentNode.className === "blocks") {
+        timer = window.setInterval(function(){
+        if(event.target.style.marginLeft !== 0 ) {
+            event.target.style.marginLeft !== '0px'}
         console.log('working')
-counter ++
-counter +='px'
-        event.target.style.marginRight ="";
-
-
-   
+        event.target.style.marginLeft = parseInt(event.target.style.marginLeft) + -10 + 'px';
+        if(event.target.style.marginLeft === 0) { clearInterval(timer); }
+        },1000)  
     }
-
-},10)
-
-
 });
 
