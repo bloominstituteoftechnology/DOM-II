8389dfbc564ae66f538273ace1819cb7c816da27
Jonathan Greene <greeneselectronics@gmail.com>
2018-03-27 16:09:20 -0600

    Fixed the padding so it looks nicer

--- index.js
+++ index.js
@@ -81,35 +81,35 @@ function moveGrayBlock() {
   function moveGrayBack() {
     console.log('This is moving  right!')
     let grayBox = document.getElementsByClassName('block--gray');
-   const  newbox = grayBox[0].style.marginLeft = '0';
+   const  newbox = grayBox[0].style.marginLeft = '10px';
   //  const newboxs = grayBox[0].style.color = 'red';
   //   console.log(newbox);
   }
   function movePinkBack() {
     console.log('This is moving  right!')
     let grayBox = document.getElementsByClassName('block--pink');
-   const  newbox = grayBox[0].style.marginLeft = '0';
+   const  newbox = grayBox[0].style.marginLeft = '10px';
   //  const newboxs = grayBox[0].style.color = 'red';
   //   console.log(newbox);
   }
   function moveGreenBack() {
     console.log('This is moving  right!')
     let grayBox = document.getElementsByClassName('block--green');
-   const  newbox = grayBox[0].style.marginLeft = '0';
+   const  newbox = grayBox[0].style.marginLeft = '10px';
   //  const newboxs = grayBox[0].style.color = 'red';
   //   console.log(newbox);
   }
   function moveBlueBack() {
     console.log('This is moving  right!')
     let grayBox = document.getElementsByClassName('block--blue');
-   const  newbox = grayBox[0].style.marginLeft = '0';
+   const  newbox = grayBox[0].style.marginLeft = '10px';
   //  const newboxs = grayBox[0].style.color = 'red';
   //   console.log(newbox);
   }
   function moveRedBack() {
     console.log('This is moving  right!')
     let grayBox = document.getElementsByClassName('block--red');
-   const  newbox = grayBox[0].style.marginLeft = '0';
+   const  newbox = grayBox[0].style.marginLeft = '10px';
   //  const newboxs = grayBox[0].style.color = 'red';
   //   console.log(newbox);
   }
