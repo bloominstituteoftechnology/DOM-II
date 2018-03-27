9dda516b440e35370f606c53d9b686d55d73f57a
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-03-27 12:56:03 -0700

    Rocket Blocks w/ comments

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
-const rocketBlocks = document.querySelectorAll('.block');
+const rocketBlocks = document.querySelectorAll('.block'); // using querySelectorAll to select all block classes from the parent node blocks
 
-rocketBlocks.forEach((element) => {
-	element.addEventListener('click', () => {
-		const block = element.parentNode;
-		block.prepend(element);		
+rocketBlocks.forEach((element) => { // using forEach so that 'click' is executed for each element
+	element.addEventListener('click', () => { // added the EventListener with click and...
+		const block = element.parentNode; // ...the argument that when a block is clicked...
+		block.prepend(element);		// it will go to the top of the parent node, using prepend from yesterday
 	});
 });
