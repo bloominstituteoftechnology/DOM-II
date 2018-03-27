6e3273794d677c655c79c4f9786a3be45e063999
Robert Hogan <roberthogan@Roberts-MacBook-Pro.local>
2018-03-27 16:39:00 -0400

    Robert Hogan, Paul Fang. Color swap, rocket progress.

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-//initial commit. 
-const test = 'test';
+const blocks = document.querySelector('.blocks');
+const redBlock = document.querySelector('.block--red');
+const blueBlock = document.querySelector('.block--blue');
+const greenBlock = document.querySelector('.block--green');
+const pinkBlock = document.querySelector('.block--pink');
+const grayBlock = document.querySelector('.block--gray');
+
+const makeRed = (event) => {
+    event.target.style.backgroundColor = 'red';
+  }
+
+  const swapColor = (event) => {
+      event.target.style.backgroundColor = '';
+  }
+
+  blueBlock.addEventListener('click', makeRed);
+  greenBlock.addEventListener('click', makeRed);
+  pinkBlock.addEventListener('click', makeRed);
+  grayBlock.addEventListener('click', makeRed);
