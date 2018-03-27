9e9814180890d5f321d171421a8323eb755c31ec
mgchmiel <michaelchmielniak@yahoo.com>
2018-03-27 16:19:25 -0400

    TRAVELERS

--- index.js
+++ index.js
@@ -8,15 +8,15 @@
 //     });
 // });
  //Traveler
- const blocks = document.getElementsByClassName("blocks");
- const block = document.getElementsByClassName("block");
- const blockArr = Array.from(block);
+//  const blocks = document.getElementsByClassName("blocks");
+//  const block = document.getElementsByClassName("block");
+//  const blockArr = Array.from(block);
  
- blockArr.forEach(function(obj){
-     obj.addEventListener('mousedown', function () {
-         this.style.marginLeft = "800px";
-     });
-     obj.addEventListener('mouseout', function (){
-         this.style.marginLeft = "10px";
-     });
- })
+//  blockArr.forEach(function(obj){
+//      obj.addEventListener('mousedown', function () {
+//          this.style.marginLeft = "800px";
+//      });
+//      obj.addEventListener('mouseout', function (){
+//          this.style.marginLeft = "10px";
+//      });
+//  })
