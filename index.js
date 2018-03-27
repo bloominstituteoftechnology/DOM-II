963a5b91f574f49b8d9718d0230b3957f46388db
Abraham Bueno <abueno@localhost.localdomain>
2018-03-27 17:02:13 -0500

    Trying to make mousedown and mouseup to work properly

--- index.js
+++ index.js
@@ -8,10 +8,14 @@ let pblock = document.querySelector(".block--pink")
 pblock.style.border = '2px solid blue';
 let gyblock = document.querySelector(".block--gray")
 gyblock.style.border = '2px solid blue';
+let bcontainer = document.querySelector(".blocks")
+bcontainer.style.border = '2px solid blue';
+bcontainer.style.width = "100%";
 
 let count = 0;
 const orderB = (event) => {
-event.target.style.order = --count;
+    event.stopPropagation();
+    event.target.style.order = --count;
 }
 
 bblock.addEventListener('click', orderB)
@@ -20,3 +24,26 @@ gblock.addEventListener('click', orderB)
 pblock.addEventListener('click', orderB)
 gyblock.addEventListener('click', orderB)
 bblock.addEventListener('click', orderB)
+
+const orderMouse = (event) => {
+    event.stopPropagation();
+    event.target.style.alignSelf = "flex-end";
+    // event.target.style.backgroundColor = "black";
+}
+const orderReturn = (event) => {
+    event.stopPropagation();
+    event.target.style.alignSelf = "flex-start";
+    // event.target.style.backgroundColor = "white";
+}
+rblock.addEventListener('mousedown', orderMouse)
+bblock.addEventListener('mousedown', orderMouse)
+gblock.addEventListener('mousedown', orderMouse)
+pblock.addEventListener('mousedown', orderMouse)
+gyblock.addEventListener('mousedown', orderMouse)
+
+rblock.addEventListener('mouseup', orderReturn)
+bblock.addEventListener('mouseup', orderReturn)
+gblock.addEventListener('mouseup', orderReturn)
+pblock.addEventListener('mouseup', orderReturn)
+gyblock.addEventListener('mouseup', orderReturn)
+// bblock.addEventListener('mousedown', orderMouse)
