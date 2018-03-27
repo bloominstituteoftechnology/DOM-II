d0aa232f8a5bd3b2f40e1446465bf7cc6eea23cb
Jonathan Greene <greeneselectronics@gmail.com>
2018-03-27 15:20:50 -0600

    Made it so each time a box is clicked it moves it to the top of the stack.

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
-function onTheMove() {
-    var elem = document.getElementById("block block--gray");   
-    var pos = 0;
-    var id = setInterval(frame, 5);
-    function frame() {
-      if (pos == 350) {
-        clearInterval(id);
-      } else {
-        pos++; 
-        elem.style.top = pos + 'px'; 
-        elem.style.bottom = pos + 'px'; 
-      }
-    }
+function moveGrayBlock() {
+    console.log('This is working!')
+    let grayBox = document.getElementsByClassName('block--gray');
+   const  newbox = grayBox[0].style.order = '-1';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+
+  function movePinkBlock() {
+    console.log('This is working!')
+    let grayBox = document.getElementsByClassName('block--pink');
+   const  newbox = grayBox[0].style.order = '-1';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function moveGreenBlock() {
+    console.log('This is working!')
+    let grayBox = document.getElementsByClassName('block--green');
+   const  newbox = grayBox[0].style.order = '-1';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function moveBlueBlock() {
+    console.log('This is working!')
+    let grayBox = document.getElementsByClassName('block--blue');
+   const  newbox = grayBox[0].style.order = '-1';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
+  }
+  function moveRedBlock() {
+    console.log('This is working!')
+    let grayBox = document.getElementsByClassName('block--red');
+   const  newbox = grayBox[0].style.order = '-1';
+  //  const newboxs = grayBox[0].style.color = 'red';
+  //   console.log(newbox);
   }
