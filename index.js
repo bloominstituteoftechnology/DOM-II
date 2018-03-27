7e819cb4f993569e1395d6de77e3a9c09e889de5
Kaitlyn Flynn <kaitlynflynn@gmail.com>
2018-03-27 12:39:44 -0700

    Made blocks move to top

---
+++ index.js
@@ -0,0 +1,16 @@
+//When a block is clicked, it should go to the top of the stack
+
+const blocks = document.querySelector('.blocks');
+const childrenArr = [...blocks.childNodes]
+
+const toTop = (event) => {
+    let childToMove = blocks.removeChild(event.target);
+    blocks.insertBefore(childToMove, blocks.firstChild);
+    event.stopPropagation();  
+}
+
+// add event listener to each child
+childrenArr.map( child => {
+    child.addEventListener('click', toTop)
+})
+
