9598d3488619f063cc3fa9a4beb7b7cb4548e0f4
Devon Bacon <devon.bacon@gmail.com>
2018-03-27 09:55:27 -0700

    Added event listeners to move elements with block class to right

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 // Refernces to DOM elements
 const blockDiv = document.querySelector('.blocks');
 const blockList = document.querySelectorAll('.block');
+let lastBlockClicked;
 
 // Give event listener to all elements in blockList
-for (let i = 0; i < Array.from(blockList).length; i++) {
+for (let i = 0; i < blockList.length; i++) {
+  // Move to top of stack on click
   blockList[i].addEventListener('click', function() {
     this.remove();
     blockDiv.prepend(this);
+  });
+  // Move to right on mouse down
+  blockList[i].addEventListener('mousedown', function() {
+    this.classList.add('move-right');
+    lastBlockClicked = this;
   });
 }
