ec6560221cff1941bd37881894d450ed82775a01
JacobLeonlyerla <umasour@yahoo.com>
2018-03-27 12:35:04 -0700

    set up basic structor for first task

---
+++ index.js
@@ -0,0 +1,19 @@
+const nodeList = document.querySelectorAll('.blocks')
+const redBox = document.getElementsByClassName('.block--red')
+const blueBox = document.getElementsByClassName('.block--blue')
+const greenBox = document.getElementsByClassName('.block--green')
+const pinkBox = document.getElementsByClassName('.block--pink')
+const grayBox = document.getElementsByClassName('.block--gray')
+
+
+
+const moveTop = (event) =>{
+event.stopPropagation();
+
+for (let i = 0; i < blocks.length; i++) {
+
+    
+}
+
+}
+nodeList.addEventListener('click', moveTop);
