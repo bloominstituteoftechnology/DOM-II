0c66a05b7fe4867a1607dae8417d086772bade51
Justin Hong <jhong2009@gmail.com>
2018-03-27 12:26:42 -0700

    set up even listeners and handlers for rockets

--- index.js
+++ index.js
@@ -4,3 +4,23 @@ const blue = document.querySelector('.block--blue');
 const green = document.querySelector('.block--green');
 const pink = document.querySelector('.block--pink');
 const gray = document.querySelector('.block--gray');
+
+// event
+const move = {
+	
+}
+
+// event listeners
+red.addEventListener('click', move);
+blue.addEventListener('click', move);
+green.addEventListener('click', move);
+pink.addEventListener('click', move);
+gray.addEventListener('click', move);
+
+// rockets
+	// When a block is clicked, it should go to the top of the stack
+
+
+
+
+// travelers
