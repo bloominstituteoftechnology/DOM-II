dd3e16378ad24faa5c39a8ee8e74202dfc3cd628
Nedu Robert <nedumrobert@gmail.com>
2018-03-27 20:11:39 +0100

    travelers

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const blocks = document.querySelectorAll('.block');
 let order = -1
 
-function topOfStack(e) {
-    e.target.style.order = order;
-    order -=1;
+// function topOfStack(e) {
+//     e.target.style.order = order;
+//     order -=1;
+// }
+
+function rightOfStack(e) {
+    e.target.style.marginLeft = '500px';
 }
 
-
+function originalPosition(e) {
+    e.target.style.marginLeft = '10px';
+}
 
 blocks.forEach(block => {
-    block.addEventListener('click', topOfStack);
+    // block.addEventListener('click', topOfStack);
+    block.addEventListener('mousedown', rightOfStack);
+    block.addEventListener('mouseout', originalPosition);
 });
