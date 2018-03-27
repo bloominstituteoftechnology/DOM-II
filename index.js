c4fb2098d088edc0da5b6d16fe14a96a5cb0c445
totsuya <supertatortots@gmail.com>
2018-03-27 16:33:27 -0400

    got rockets working

--- index.js
+++ index.js
@@ -5,4 +5,25 @@ const greenBlock = document.getElementById("greenBlock");
 const pinkBlock = document.getElementById("pinkBlock");
 const grayBlock = document.getElementById("grayBlock");
 
-let orderCounter = -1 //acts as a counter for the order
+// sets order for each block
+function orderSet() {
+  document.getElementById("redBlock").style.order = "0";
+  document.getElementById("blueBlock").style.order = "0";
+  document.getElementById("greenBlock").style.order = "0";
+  document.getElementById("pinkBlock").style.order = "0";
+  document.getElementById("grayBlock").style.order = "0";
+};
+
+// makes it so whatever block you click jumps to the top (super archaic method)
+let count = 0;
+const changeOrder = (event) => {
+   // event.stopPropagation();
+   event.target.style.order = count--;
+}
+
+// when you click on blocks, it changes order!
+redBlock.addEventListener('click', changeOrder);
+blueBlock.addEventListener('click', changeOrder);
+greenBlock.addEventListener('click', changeOrder);
+pinkBlock.addEventListener('click', changeOrder);
+grayBlock.addEventListener('click', changeOrder);
