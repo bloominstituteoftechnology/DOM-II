fec6065d483998d92952a97a7ed15a7d1d8ac6c7
Devon Bacon <devon.bacon@gmail.com>
2018-03-27 09:29:55 -0700

    Added event listeners for elements with block class

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Refernces to DOM elements
 const blockDiv = document.querySelector('.blocks');
 const blockList = document.querySelectorAll('.block');
 
+// Give event listener to all elements in blockList
+for (let i = 0; i < Array.from(blockList).length; i++) {
+  blockList[i].addEventListener('click', function() {
+    this.remove();
+    blockDiv.prepend(this);
+  });
+}
