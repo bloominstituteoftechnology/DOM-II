3ff604737d9774ba98c59cf8dd80204024acf064
JulianAlexander <julian.pineyro@gmail.com>
2018-03-28 01:37:32 +0300

    Added blocks going to the right

--- index.js
+++ index.js
@@ -11,6 +11,41 @@ const rockets = (event) => {
     parent.prepend(event.target);
 }
 
+// STRETCH GOALS
+// travelers
+
+// const travelRed = (event) => {
+//     // let traveler = event.target;
+//     // traveler = event.currentTarget;       
+//     redBlock.style.marginLeft += '1px';    
+//     const travelers = setInterval(travelRed, 10);      
+// }
+
+const travelEvent = (e) => {
+    let distance = 0
+    const moveRight = () => {
+      distance += 10
+      e.target.style.marginLeft = `${distance}px`;
+    }
+    setInterval(moveRight, 10);    
+  }
+
+const travelBack = (event) => {
+    // event.target.style.marginLeft = '10px';
+    clearInterval(travelRight);
+    e.target.style.transform = 'translateX(10px)';
+}
+
+// event listener
 blocks.forEach((element) => {
     element.addEventListener('click', rockets); 
+    element.addEventListener('mousedown', travelEvent);
+    element.addEventListener('mouseup', travelBack);
+
+    // redBlock.addEventListener('mousedown', travelRed);
+    // blueBlock.addEventListener('mousedown', travelBlue);
+    // blueBlock.addEventListener('mouseup', travelBack);
+    // greenBlock.addEventListener('mouseup', travelBack);
+    // pinkBlock.addEventListener('mouseup', travelBack);
+    // grayBlock.addEventListener('mouseup', travelBack);   
 })
