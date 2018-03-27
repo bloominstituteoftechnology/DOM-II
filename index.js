cb3b881c21b70f9b33101ef510244d521a601112
Justin M <jekm321@gmail.com>
2018-03-27 15:49:28 -0400

    changed to better suiting function name, added comments

--- index.js
+++ index.js
@@ -5,18 +5,21 @@ const blue = document.querySelector('.block--blue');
 const green = document.querySelector('.block--green');
 const pink = document.querySelector('.block--pink');
 const gray = document.querySelector('.block--gray');
+// all the varialbes i could possibly need and more
 
 const first = (e) => {
     event.stopPropagation();
-    reduce();
+    reset();
     e.target.style.order -= 1;
 }
+// the function used to make a  box move to the top
 
-const reduce = () => {
+const reset = () => {
     for (let i = 0; i < allBlocks.length; i++) {
         allBlocks[i].style.order = 1;
     }
 }
+//the function that makes all the other boxes go back to original order
 
 red.addEventListener('click', first);
 blue.addEventListener('click', first);
