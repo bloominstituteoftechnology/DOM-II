ab6dd4dc09364e7d736a64e0b090ef3ee272c9d6
2940cristian <2940cristian@gmail.com>
2018-03-27 21:30:41 -0700

    added for loop, deleted commented code, mouse up/down complete

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-let red = document.querySelector(".block--red")
-let blue = document.querySelector(".block--blue")
-let green = document.querySelector(".block--green")
-let pink = document.querySelector(".block--pink")
-let gray = document.querySelector(".block--gray")
-let blocks = document.querySelector('.blocks');
-
-let order = -1;
-
-// function blockClicked(e) {
-//     order--
-//     e.target.style.order = order;
-// };
+let blockLoop = document.getElementsByClassName("block")
 
 function mouseDown(e) {
-    e.target.style.position = "relative";
+    e.target.style.position = "relative"; //pushes box to right on mousedown
     e.target.style.left = "15rem";
 };
 
 function mouseUp(e) {
-    e.target.classList.add("mouseRelease");
-    console.log('test')
+    e.target.classList.add("mouseRelease"); //releasing on block causes it to transition to 
+    console.log('test')                    //the left and be shot back to the right
 };
 
-// blocks.addEventListener('click', blockClicked);
-red.addEventListener('mouseup', mouseUp);
-red.addEventListener('mousedown', mouseDown);
-
-blue.addEventListener('mouseup', mouseUp);
-blue.addEventListener('mousedown', mouseDown);
-
-green.addEventListener('mouseup', mouseUp);
-green.addEventListener('mousedown', mouseDown);
-
-pink.addEventListener('mouseup', mouseUp);
-pink.addEventListener('mousedown', mouseDown);
-
-gray.addEventListener('mouseup', mouseUp);
-gray.addEventListener('mousedown', mouseDown);
+for(let i = 0; i<blockLoop.length; i++) {
+    blockLoop[i].addEventListener('mouseup', mouseUp); //looping through each index/block to give it an event listener
+    blockLoop[i].addEventListener('mousedown', mouseDown);
+}
