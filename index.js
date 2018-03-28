a11cf425f53f8cf21f94904e90ed6e6b75b5f2f0
Justin M <jekm321@gmail.com>
2018-03-28 12:24:25 -0400

    changed method of changing order, now it is not only cleaner, there is now a slight animation upon order change, also changed method of applying .addEventListerner

--- index.js
+++ index.js
@@ -8,19 +8,18 @@ const gray = document.querySelector('.block--gray');
 const body = document.querySelector('#container');
 // all the varialbes i could possibly need and more
 
-// const first = (e) => {
-//     e.stopPropagation();
-//     reset();
-//     e.target.style.order -= 1;
-// };
-// // the function used to make a  box move to the top
+let order = -1;
 
-// const reset = () => {
-//     for (let i = 0; i < allBlocks.length; i++) {
-//         allBlocks[i].style.order = 1;
-//     }
-// };
-// // the function that makes all the other boxes go back to original order
+const first = (e) => {
+    const child = e.target;
+    child.style.order = order;
+    order--;
+};
+// the function used to make a  box move to the top
+
+for (i = 0; i < blocks.children.length; i++) {
+    blocks.children[i].addEventListener('click', first);
+}
 
 // red.addEventListener('click', first);
 // blue.addEventListener('click', first);
@@ -31,57 +30,57 @@ const body = document.querySelector('#container');
 // // -------- Part 1 is above ---------
 // // -------- Part 2 is below ---------
 
-let interval;
-let lastTarg;
+// let interval;
+// let lastTarg;
 
-const begin = (e) => {
-    lastTarg = e;
-    clearInterval(interval);
-    interval = window.setInterval(increment, 150, e);
-}
-
-const specEnd = lastTarg => {
-    clearInterval(interval);
-    interval = window.setInterval(decrement, 150, lastTarg);
-}
-
-// const end = (e) => {
+// const begin = (e) => {
+//     lastTarg = e;
 //     clearInterval(interval);
-//     interval = window.setInterval(decrement, 75, e);
+//     interval = window.setInterval(increment, 150, e);
 // }
 
-const increment = (targ) => {
-    let targLength = targ.target.style.marginLeft.length;
-    let value;
+// const specEnd = lastTarg => {
+//     clearInterval(interval);
+//     interval = window.setInterval(decrement, 150, lastTarg);
+// }
 
-    if (targLength > 0) {
-        let value = parseInt(targ.target.style.marginLeft);
-        if (value <300) value += 20;
-        targ.target.style.marginLeft = String(value) + 'px';
-    } else targ.target.style.marginLeft = '10px';
-}
+// // const end = (e) => {
+// //     clearInterval(interval);
+// //     interval = window.setInterval(decrement, 75, e);
+// // }
 
-const decrement = (targ) => {
-    let value = parseInt(targ.target.style.marginLeft);
-    value -= 20;
-    if (value < 10) {
-        targ.target.style.marginLeft = '';
-        clearInterval(interval);
-        return;
-    }
-    targ.target.style.marginLeft = (String(value) + "px");
-}
+// const increment = (targ) => {
+//     let targLength = targ.target.style.marginLeft.length;
+//     let value;
 
-red.addEventListener('mousedown', begin);
-blue.addEventListener('mousedown', begin);
-green.addEventListener('mousedown', begin);
-pink.addEventListener('mousedown', begin);
-gray.addEventListener('mousedown', begin);
+//     if (targLength > 0) {
+//         let value = parseInt(targ.target.style.marginLeft);
+//         if (value <300) value += 20;
+//         targ.target.style.marginLeft = String(value) + 'px';
+//     } else targ.target.style.marginLeft = '10px';
+// }
 
-body.addEventListener('mouseup', specEnd);
+// const decrement = (targ) => {
+//     let value = parseInt(targ.target.style.marginLeft);
+//     value -= 20;
+//     if (value < 10) {
+//         targ.target.style.marginLeft = '';
+//         clearInterval(interval);
+//         return;
+//     }
+//     targ.target.style.marginLeft = (String(value) + "px");
+// }
 
-// red.addEventListener('mouseup', end);
-// blue.addEventListener('mouseup', end);
-// green.addEventListener('mouseup', end);
-// pink.addEventListener('mouseup', end);
-// gray.addEventListener('mouseup', end);
+// red.addEventListener('mousedown', begin);
+// blue.addEventListener('mousedown', begin);
+// green.addEventListener('mousedown', begin);
+// pink.addEventListener('mousedown', begin);
+// gray.addEventListener('mousedown', begin);
+
+// body.addEventListener('mouseup', specEnd);
+
+// // red.addEventListener('mouseup', end);
+// // blue.addEventListener('mouseup', end);
+// // green.addEventListener('mouseup', end);
+// // pink.addEventListener('mouseup', end);
+// // gray.addEventListener('mouseup', end);
