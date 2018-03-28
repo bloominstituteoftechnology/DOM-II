f0873ac51796b7d6aa8cd324ac1214b1e2e6932f
odax <nr10@albion.edu>
2018-03-28 15:27:57 -0400

    started working on 2nd half

--- index.js
+++ index.js
@@ -26,3 +26,18 @@
 // for (i = 0; i < blocks.children.length; i++) {
 //      blocks.children[i].addEventListener('click', ourFunction);
 //      }
+
+//my goal is to get a box to move on it's own right at refresh, the box i want moved is this: <div class="block block--red"></div>
+// will try using an interval to constantly apply function to move.
+
+    // const redBlock = document.querySelector('.block--red');
+
+    // function move () {
+    // redBlock.style.marginLeft += 20px;
+    // }
+
+// let int = setInterval(move,1000);
+
+//after i complete that I want to create a class that will move on it's own when applied
+
+//after i complete that I need to use element.classList.toggle('class) to give it the class when clicked, and take away the class when unclicked
