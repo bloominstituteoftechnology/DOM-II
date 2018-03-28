7ff2b6f5aa91dd17596f909e3b45f51c65019244
David A. Morales <you@example.com>
2018-03-28 12:26:33 -0400

    unfinished travelers

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const blue = document.querySelector('.block--blue');
 const green = document.querySelector('.block--green');
 const pink = document.querySelector('.block--pink');
 const gray = document.querySelector('.block--gray');
+const blocks = document.querySelector('block');
 
 // Rockets
 
@@ -19,3 +20,24 @@ green.addEventListener('click', go);
 pink.addEventListener('click', go);
 gray.addEventListener('click', go);
 
+// Travelers
+
+const travelerEvent = (e) => {
+    let distance = 0
+
+    const moveRight = () => {
+      distance += 10
+      e.target.style.marginLeft = `${distance}px`;
+    }
+    setInterval(moveRight, 10);
+  }
+  
+  for(let i = 0; i < blocks.children.length; i++) {
+    blocks.children[i].addEventListener('click', travelerEvent);
+  }
+
+red.addEventListener('mousedown', moveRight);
+blue.addEventListener('mousedown', moveRight);
+green.addEventListener('mousedown', moveRight);
+pink.addEventListener('mousedown', moveRight);
+gray.addEventListener('mousedown', moveRight);
