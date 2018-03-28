fbff602b840b8be7dfd649132a37aae2538ce0ec
SyCoDe <SyCoDe@gmail.com>
2018-03-28 07:50:46 -0600

    finished 1st requirement in travelers

--- index.js
+++ index.js
@@ -1,18 +1,37 @@
-const parent = document.getElementsByClassName('blocks')[0]; //grabs the parent node
-
+// const parent = document.getElementsByClassName('blocks')[0]; //grabs the parent node
+// let blocks = Array.from(document.querySelectorAll('.block'));
 let order = -5;
+const blocks = document.querySelectorAll(".block"); //creates parent node as array
+let orderValue = -1;
 
-const moveOrder = (event) => { //callback function used to move the target box to the top, above all other boxes
-    order--;
+const rockets = (event) => { //callback function used to move the target box to the top, above all other boxes
     event.target.style.order = order;
+    order--;
 }
 
-const moveRight = (event) => { //callback function to move box to the right on mouse click held down
-    event.target.style.marginLeft = '100px';
+const travelers = (event) => { //callback function to move box to the right on mouse click held down
+    // event.target.style.transition = "margin-left 2s linear 1s";
+    event.target.style.marginLeft = "100px";
 }
 
-for (let i = 0; i < parent.children.length; i++) { // loop to add eventlistener to each box
-    parent.children[i].addEventListener('click', moveOrder); 
-    console.log(parent.children[i]); //.addEventListener('click', moveOrder);
-    // parent.children[i].addEventListener('mousedown', moveRight);
+const combo = (e) => { //testing different ideas
+    rockets(e);
+    travelers(e);
+    e.target.style.order = "initial"
+}
+
+const reset = (e) => {  //WIP: reset rockets & travelers
+    event.target.style.marginRight = "100px";
+}
+
+// blocks.forEach(block => { //addEventListeners for rockets & travelers
+//     block.addEventListener('click', rockets); 
+//     block.addEventListener('mousedown', travelers);
+//     block.addEventListener('mouseup', reset);
+// });
+
+for (let i=0; i<blocks.length; i++){ //addEventListeners for rockets & travelers
+    blocks[i].addEventListener('click', rockets); 
+    blocks[i].addEventListener('mousedown', travelers);
+    blocks[i].addEventListener('mouseup', reset);
 }
