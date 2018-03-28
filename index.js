e7561340c9a38435895e36fcdfee1f4250405a70
David A. Morales <you@example.com>
2018-03-27 20:16:57 -0400

    block variables

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const red = document.querySelector('.block--red');
+const blue = document.querySelector('.block--blue');
+const green = document.querySelector('.block--green');
+const pink = document.querySelector('.block--pink');
+const gray = document.querySelector('.block--gray');
+
+// Rockets
