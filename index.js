9844452ebf3edecc0dc7ee44b88aacd547090fa2
David A. Morales <you@example.com>
2018-03-27 21:13:08 -0400

    rockets

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const pink = document.querySelector('.block--pink');
 const gray = document.querySelector('.block--gray');
 
 // Rockets
+
+let order = 0;
+const go = () => {
+    order: (0);
+    order--;
+    event.target.style.order = order.toString();
+}
+
+red.addEventListener('click', go);
+blue.addEventListener('click', go);
+green.addEventListener('click', go);
+pink.addEventListener('click', go);
+gray.addEventListener('click', go);
+
