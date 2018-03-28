bdc2eb07bbb8d88be572795273775f1a79775290
imsdhk <jappleleaf@gmail.com>
2018-03-28 10:32:35 -0400

    fixed traveler function

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 const blocks = document.querySelectorAll(".block");
 
-const rocket = (e) => {
-    let parent = e.target.parentNode;
-    let current = e.target;
-    parent.removeChild(current);
-    parent.prepend(current);
+const rocketEvent = (event) => {
+    const child = event.target;
+    const parent = event.target.parentNode
+    parent.removeChild(event.target);
+    parent.prepend(event.target);
 }
 
-const travelRight = (e) => {
-    e.target.style.transform = "translateX(100px)";
-}
-const travelBack = (e) => {
-    e.target.style.transform = "translateX(0px)";
+// const travelerEvent = (event) => {
+//     event.target.style.transform = `translateX(200px)`;
+// };
+
+const travelerEvent = (e) => {
+    let distance = 0
+    const moveRight = () => {
+        distance += 10
+        e.target.style.marginLeft = `${distance}px`;
+    }
+    setInterval(moveRight, 10);
 }
 
-blocks.forEach(element => {
-    element.addEventListener("click", rocket); 
-    element.addEventListener("mouseenter", travelRight);
-    element.addEventListener("mouseleave", travelBack);
-});
+blocks.forEach((element) => {
+    element.addEventListener('click', rocketEvent);
+    element.addEventListener('mousedown', travelerEvent);
+})
