2f0b1d3b5d7db0efa6029f5830fa62d53d93ec59
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-04-03 00:04:18 -0600

    DOM-II

--- index.js
+++ index.js
@@ -1,36 +1,31 @@
-const blocks = document.querySelector(".blocks");
-const blockRed = document.getElementById("block block--red");
-const blockBlue = document.getElementById("block block--blue");
-const blockGreen = document.getElementById("block block--green");
-const blockPink = document.getElementById("block block--pink");
-const blockGray = document.getElementById("block block--gray");
+const blocks = document.querySelector('.blocks');
 
-const toTheTop = (event) => {
-      event.stopPropagation();
-      event.target.style.backgroundColor = 'red';
+//  let order = -1;
+//const rocketEvent = (event) => {
+//  const child = event.target;
+// child.style.order = order;
+// order--;
+//}
+
+//for (let i = 0; i < blocks.children.length; i++) {
+//    blocks.children[i].addEventListener('click', rocketEvent);
+//}
+
+const travelerEvent = (event) => {
+    let distance = 0;
+
+    const moveRight = () => {
+        distance += 10;
+        event.target.style.marginLeft = distance + 'px';
+    }
+    setInterval(moveRight, 10);
+}
+for (let i = 0; i < blocks.children.length; i++) {
+    blocks.children[i].addEventListener('click', travelerEvent);
 }
 
-      blockRed.addEventListener('click', toTheTop);
-      blockBlue.addEventListener('click', totheTop);
-      blockGreen.addEventListener('click', toTheTop);
-      blockPink.addEventListener('click', toTheTop);
-      blockGray.addEventListener('click', toTheTop);
-
-
-const moveRight = (event) => {
-      event.stopPropagation();
-      event.target.blocks.style.right = '100px';
-}
-
-      blockRed.addEventListener('click', moveRight);
-      blockBlue.addEventListener('click', moveRight);
-      blockGreen.addEventListener('click', moveRight);
-      blockPink.addEventListener('click', moveRight);
-      blockGray.addEventListener('click', moveRight);
 
 
 
 
 
-
-//window.setInterval {};
