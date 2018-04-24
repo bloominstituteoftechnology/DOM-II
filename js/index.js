15eff1e4077d24adc0031e1d8b91e48fd1c5e3fe
Zach Campbell <beazlebubbles@gmail.com>
2018-04-24 15:41:25 -0600

    Added navLinks nodelist to get to nav links added click listeners for each link moved color change to a window click listener added placeholder callbacks in listeners

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const startButton = document.getElementById("start-button");
 const ctaText = document.getElementById("big-text");
+const navLinks = document.querySelectorAll(".nav");
 const colors = ["red", "blue", "orange", "green", "purple", "yellow", "aqua", "pink"]
 
 startButton.addEventListener("click", () => TweenLite.to(ctaText, 1, {color:colors[Math.floor(Math.random() * colors.length)]}));
