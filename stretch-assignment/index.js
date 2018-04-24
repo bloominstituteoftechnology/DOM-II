3dcdb4ca32bf04e2c07113877e6b17b4401da94e
Brandon Benefield <bsquared18@gmail.com>
2018-04-24 13:47:22 -0400

    index.js: add mousedown event to move blocks

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,9 +1,20 @@
 const blocks = document.querySelectorAll('.block');
 
 for (let i = 0; i < blocks.length; i++) {
-  let blockContainer = document.querySelector('.blocks');
+  blocks[i].style.position = 'relative';
+  blocks[i].style.left = '0';
+  blocks[i].style.transition = 'left 1s';
   
   blocks[i].addEventListener('mouseup', function() {
+    let blockContainer = document.querySelector('.blocks');
     blockContainer.prepend(this);
+  });
+
+  blocks[i].addEventListener('mousedown', function() {
+    this.style.left = '100vw';
+  });
+
+  document.addEventListener('mouseup', function () {
+    blocks[i].style.left = '0';
   });
 }
