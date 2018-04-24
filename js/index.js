67a69148d819027fcb5827d3e1c874d0dc48ada7
udykas <udykas@gmail.com>
2018-04-24 17:24:09 -0400

    Add images and modified  html, css and js

---
+++ js/index.js
@@ -0,0 +1,31 @@
+let logo = document.querySelector('#logo-img');
+
+// let button = document.querySelector('.btn');
+
+// console.log(button);
+// console.log(logo);
+
+// button.addEventListener('click', (event) => {
+//     console.log('You pressed the button!');
+// });
+
+// let mainHeader = document.querySelector('.dom-text');
+
+
+
+// console.log(button);
+// console.log(mainHeader.innerHtml);
+
+
+// mainHeader.addEventListener('click', (event) => {
+//     event.currentTarget.classList.add("go-green");
+//     console.log('You clicked the header!');
+// });
+
+window.addEventListener('contextmenu', () => {
+    console.log("context!")
+});
+
+window.addEventListener('wheel', () => {
+    console.log("wheelin' and dealin'!")
+});
