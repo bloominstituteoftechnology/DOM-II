5d6875311bacb7d12148b9febdf9d2b6f209b688
Matthias Bakken <matthias.bakken@gmail.com>
2018-04-24 14:03:53 +0700

    created rockets and then set event listeners to all blocks

--- index.js
+++ index.js
@@ -6,3 +6,21 @@ for (let i = 0; i < document.getElementsByClassName('blocks')[0].children.length
 };
 // console.log(blockClsList);
 // console.log(blockClsList.length);
+
+// =======================  Rockets  ==========================
+
+// When a block is clicked, it should go to the top of the stack
+// holds current value of flex order at the time of a 'click' event
+let order = 0;
+// event
+const move = () => {
+    // decrement order: (-) orders will move to the top of the main-axis
+    // default order is 0 for all blocks
+	order--;
+	event.target.style.order = order.toString();
+}
+
+// creates event listener on click for all blocks
+for (let i = 0; i < blockClsList.length; i++) {
+	document.querySelector('.'+blockClsList[i]).addEventListener('click', move);
+}
