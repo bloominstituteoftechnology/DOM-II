c9951a066eb7336e359006f4528bce71518deb86
Brandon Benefield <bsquared18@gmail.com>
2018-04-24 12:35:19 -0400

    index.js: replace "click" event with "mouseup"

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -3,7 +3,7 @@ const blocks = document.querySelectorAll('.block');
 for (let i = 0; i < blocks.length; i++) {
   let blockContainer = document.querySelector('.blocks');
   
-  blocks[i].addEventListener('click', function() {
+  blocks[i].addEventListener('mouseup', function() {
     blockContainer.prepend(this);
   });
 }
