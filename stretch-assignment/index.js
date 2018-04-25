8dbf111d4a2ba62dd10dfc4780fbdbbc2b8cb623
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-04-25 09:10:20 -0400

    Stretch Rockets - V1.1 Calculate the math to animate moving each block to the top

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,22 +1,8 @@
 let blockSelector = document.getElementsByClassName("block");
 let blocksSelector = document.getElementsByClassName("blocks");
 
-
-
-
-
-moveUpDelete = function(e) {
-    var newBlock = document.createElement("div");
-    newBlock.setAttribute("class", e.target.getAttribute("class"));
-    blocksSelector[0].prepend(newBlock);
-    blockSelector[0].addEventListener('click', (e) => {
-        moveUp(e);
-    });
-    e.target.remove();
-
-};
-
-createEvents = function () {
+//Creates Click Event for each block
+createEvents = () => {
     for(let i =0; i < blockSelector.length ; i++) {
         blockSelector[i].addEventListener('click', (e) => {
             moveUp(e);
@@ -24,11 +10,25 @@ createEvents = function () {
     };
 }
 
+//Creates a new block on top and deletes the block from the Dom
+moveUpDelete = (e) => {
+    var newBlock = document.createElement("div");
+    newBlock.setAttribute("class", e.target.getAttribute("class"));
+    blocksSelector[0].prepend(newBlock);
+    blockSelector[0].addEventListener('click', (e) => {
+        moveUp(e);
+    });
+    e.target.remove();
+};
+
+moveUp = (e) => {
+    var element = e.target;
+    var position = element.getBoundingClientRect();
+    var x = position.left;
+    var y = 73.75 - position.top;
+    var myAnimation = new TweenLite.to(e.target, 1, {ease: Bounce.easeOut, y: y, onComplete:moveUpDelete, onCompleteParams:[e]});
+}
+
 createEvents();
 
-moveUp = function(e) {
-    // TweenLite.to(e.target, 2.5, { ease: Bounce.easeOut, y: -400 });
-    var myAnimation = new TweenLite.to(e.target, 1, {ease: Bounce.easeOut, y: -490, onComplete:moveUpDelete, onCompleteParams:[e]});
-
-}
 
