f225ed8fe6330a9b09e24c4378bb60d7dc5b5d52
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-04-28 07:39:26 +0800

    Implemented Project Manager's Suggestions
    1. Cleaned my code by separating declarations with one line of space each.

--- js/index.js
+++ js/index.js
@@ -51,10 +51,10 @@ logoImg.addEventListener('dblclick', () => {
 // IMAGES
 
 let headerImage = document.getElementById("cta-img");
-headerImage.setAttribute("src", "img/header-img.png")
+headerImage.setAttribute("src", "img/header-img.png");
 
-let middleImage = document.getElementById("middle-img");
-middleImage.setAttribute("src", "img/mid-page-accent.jpg")
+middleImage = document.getElementById("middle-img");
+middleImage.setAttribute("src", "img/mid-page-accent.jpg");
 
 // Event Listener: Double Click
 
@@ -70,15 +70,20 @@ middleImage.addEventListener('dblclick', () => {
 
 let services = document.getElementsByTagName("a")[0];
 services.innerHTML = "Services";
-let product = document.getElementsByTagName("a")[1];
+
+product = document.getElementsByTagName("a")[1];
 product.innerHTML = "Product";
-let vision = document.getElementsByTagName("a")[2];
+
+vision = document.getElementsByTagName("a")[2];
 vision.innerHTML = "Vision";
-let features = document.getElementsByTagName("a")[3];
+
+features = document.getElementsByTagName("a")[3];
 features.innerHTML = "Features";
-let about = document.getElementsByTagName("a")[4];
+
+about = document.getElementsByTagName("a")[4];
 about.innerHTML = "About"
-let contact = document.getElementsByTagName("a")[5];
+
+contact = document.getElementsByTagName("a")[5];
 contact.innerHTML = "Contact";
 
 // Event Listener: Mouseover
@@ -113,7 +118,7 @@ contact.addEventListener('mouseover', () => {
 let ctaText = document.getElementsByTagName("h1")[0];
 ctaText.innerHTML = "DOM Is Awesome";
 
-let button = document.getElementsByTagName("button")[0];
+button = document.getElementsByTagName("button")[0];
 button.innerHTML = "Get Started";
 
 // Event Listener: Double Click
@@ -133,27 +138,32 @@ button.addEventListener('click', () => {
 
 let featuresH4 = document.getElementsByTagName('h4')[0];
 featuresH4.innerHTML = "Features";
-let featureContent = document.getElementsByTagName('p')[0];
+
+featureContent = document.getElementsByTagName('p')[0];
 featureContent.innerHTML = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
 let aboutH4 = document.getElementsByTagName('h4')[1];
 aboutH4.innerHTML = "About";
-let aboutContent = document.getElementsByTagName('p')[1];
+
+aboutContent = document.getElementsByTagName('p')[1];
 aboutContent.innerHTML = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
 let servicesH4 = document.getElementsByTagName('h4')[2];
 servicesH4.innerHTML = "Services";
-let servicesContent = document.getElementsByTagName('p')[2];
+
+servicesContent = document.getElementsByTagName('p')[2];
 servicesContent.innerHTML = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
 let productH4 = document.getElementsByTagName('h4')[3];
 productH4.innerHTML = "Product"
-let productContent = document.getElementsByTagName('p')[3];
+
+productContent = document.getElementsByTagName('p')[3];
 productContent.innerHTML = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
 let visionH4 = document.getElementsByTagName('h4')[4];
 visionH4.innerHTML = "Vision";
-let visionContent = document.getElementsByTagName('p')[4];
+
+visionContent = document.getElementsByTagName('p')[4];
 visionContent.innerHTML = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis."
 
 // Event Listener: Mouse Over
@@ -161,6 +171,7 @@ visionContent.innerHTML = "Vision content elementum magna eros, ac posuere elvit
 featuresH4.addEventListener('mouseover', () => {
   featuresH4.style.backgroundColor = "red";
 });
+
 featureContent.addEventListener('mouseover', () => {
   featureContent.style.backgroundColor = "red";
 });
@@ -168,6 +179,7 @@ featureContent.addEventListener('mouseover', () => {
 aboutH4.addEventListener('mouseover', () => {
   aboutH4.style.backgroundColor = "orange";
 });
+
 aboutContent.addEventListener('mouseover', () => {
   aboutContent.style.backgroundColor = "orange";
 });
@@ -175,6 +187,7 @@ aboutContent.addEventListener('mouseover', () => {
 servicesH4.addEventListener('mouseover', () => {
   servicesH4.style.backgroundColor = "yellow";
 });
+
 servicesContent.addEventListener('mouseover', () => {
   servicesContent.style.backgroundColor = "yellow";
 });
@@ -182,6 +195,7 @@ servicesContent.addEventListener('mouseover', () => {
 productH4.addEventListener('mouseover', () => {
   productH4.style.backgroundColor = "green";
 });
+
 productContent.addEventListener('mouseover', () => {
   productContent.style.backgroundColor = "green";
 });
@@ -189,6 +203,7 @@ productContent.addEventListener('mouseover', () => {
 visionH4.addEventListener('mouseover', () => {
   visionH4.style.backgroundColor = "blue";
 });
+
 visionContent.addEventListener('mouseover', () => {
   visionContent.style.backgroundColor = "blue";
 });
@@ -197,11 +212,14 @@ visionContent.addEventListener('mouseover', () => {
 
 let contactH4 = document.getElementsByTagName('h4')[5];
 contactH4.innerHTML = "Contact";
-let address = document.getElementsByTagName('p')[5];
+
+address = document.getElementsByTagName('p')[5];
 address.innerHTML = "123 Way 456 Street Somewhere, USA";
-let phone = document.getElementsByTagName('p')[6];
+
+phone = document.getElementsByTagName('p')[6];
 phone.innerHTML = "1 (888) 888-8888";
-let email = document.getElementsByTagName('p')[7];
+
+email = document.getElementsByTagName('p')[7];
 email.innerHTML = "sales@greatidea.io";
 
 // FOOTER
