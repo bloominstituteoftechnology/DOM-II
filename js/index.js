349adbe448d5b4e1c86775cee33e8cdccd700e25
david <davidlayman85@gmail.com>
2018-05-29 15:55:03 -0400

    david layman DOM-II

---
+++ js/index.js
@@ -0,0 +1,89 @@
+const siteContent = {
+  "nav": {
+    "nav-item-1": "Services",
+    "nav-item-2": "Product",
+    "nav-item-3": "Vision",
+    "nav-item-4": "Features",
+    "nav-item-5": "About",
+    "nav-item-6": "Contact",
+    "img-src": "img/logo.png"
+  },
+  "cta": {
+    "h1": "DOM Is Awesome",
+    "button": "Get Started",
+    "img-src": "img/header-img.png"
+  },
+  "main-content": {
+    "features-h4":"Features",
+    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-h4":"About",
+    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "middle-img-src": "img/mid-page-accent.jpg",
+    "services-h4":"Services",
+    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-h4":"Product",
+    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-h4":"Vision",
+    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+  },
+  "contact": {
+    "contact-h4" : "Contact",
+    "address" : "123 Way 456 Street Somewhere, USA",
+    "phone" : "1 (888) 888-8888",
+    "email" : "sales@greatidea.io",
+  },
+  "footer": {
+    "copyright" : "Copyright Great Idea! 2018"
+  },
+};
+
+// Example: Update the img src for the logo
+let logo = document.getElementById("logo-img");
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+
+let navBar = document.querySelectorAll("nav a");
+navBar[0].innerHTML="Services";
+navBar[1].innerHTML="Product";
+navBar[2].innerHTML="Vision";
+navBar[3].innerHTML="Features";
+navBar[4].innerHTML="About";
+navBar[5].innerHTML="Contact";
+
+document.querySelector("h1").innerHTML = "DOM Is Awesome";
+document.querySelector("button").innerHTML = "Get Started";
+
+document.getElementById("cta-img").src="img/header-img.png";
+
+let mainContent = document.querySelectorAll(".main-content h4");
+mainContent[0].innerHTML="Features";
+mainContent[1].innerHTML="About";
+mainContent[2].innerHTML="Services";
+mainContent[3].innerHTML="Product";
+mainContent[4].innerHTML="Vision";
+
+let mainContentP = document.querySelectorAll(".main-content p");
+mainContentP[0].innerHTML="Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[1].innerHTML="About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[2].innerHTML="Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[3].innerHTML="Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+mainContentP[4].innerHTML="Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+
+document.getElementById("middle-img").src="img/mid-page-accent.jpg";
+
+document.querySelector(".contact h4").innerHTML="Contact";
+
+let contactP =document.querySelectorAll(".contact p");
+contactP[0].innerHTML="123 Way 456 Street Somewhere, USA";
+contactP[1].innerHTML="1 (888) 888-8888";
+contactP[2].innerHTML="sales@greatidea.io";
+
+document.querySelector("footer").innerHTML="Copyright Great Idea! 2018";
+
+
+//event listners
+
+document.getElementById("cta-img").addEventListener("mouseover", ()=>{ document.getElementById("cta-img").style.border="5px solid red";});
+document.getElementById("cta-img").addEventListener("mouseout", ()=>{ document.getElementById("cta-img").style.border="0px solid red";});
+document.getElementById("cta-img").addEventListener("dblclick", ()=>{ document.getElementById("cta-img").style.border="8px solid green";});
+document.querySelector("h1").addEventListener("mouseenter", ()=>{ document.querySelector("h1").style.background="blue";});
+document.querySelector("h1").addEventListener("mouseleave", ()=>{ document.querySelector("h1").style.background="white";});
