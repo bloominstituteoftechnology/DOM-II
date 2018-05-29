7864e9dcb8a39b89edbd8cebe4e0ba3d56a99474
Luis <luis@Luiss-MacBook-Air.local>
2018-05-29 12:30:58 -0700

    used queryselector to select div's by classes

---
+++ stretch-assignment/index.js
@@ -0,0 +1,5 @@
+let redBox = document.querySelector('.block--red')
+let blueBox = document.querySelector('.block--blue')
+let greenBox = document.querySelector('.block--green')
+let pinkBox = document.querySelector('.block--pink')
+let grayBox = document.querySelector('.block--gray')
