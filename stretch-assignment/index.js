b22e345b4ddb3842a44bf5b9aff4893878f6a27c
Sam Khaled <samkhaled@outlook.com>
2018-05-29 18:51:07 -0400

    worked on the stretch-assignment = was interesting

---
+++ stretch-assignment/index.js
@@ -0,0 +1,20 @@
+const blocks = document.querySelectorAll('.block');
+
+for (let i = 0; i < blocks.length; i++) {
+  blocks[i].style.position = 'relative';
+  blocks[i].style.left = '0';
+  blocks[i].style.transition = 'left 1s';
+  
+  blocks[i].addEventListener('mouseup', function() {
+    let blockContainer = document.querySelector('.blocks');
+    blockContainer.prepend(this);
+  });
+
+  blocks[i].addEventListener('mousedown', function() {
+    this.style.left = '93vw';
+  });
+
+  document.addEventListener('mouseup', function () {
+    blocks[i].style.left = '0';
+  });
+}
