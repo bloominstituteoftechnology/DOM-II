fec6cec538407b1c3716bd51619310bad3638fad
sliw <slioweng@gmail.com>
2018-07-11 06:13:36 +0800

    add stretch goal 1

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -3,15 +3,22 @@ const boxList = document.querySelectorAll('.block');
 
 boxList.forEach(function(box) {
   box.addEventListener('click', putInFront);
-  box.addEventListener('click', goToRight);
+  // box.addEventListener('click', goToRight);
 })
 
 // rockets
 let order = -1;
 
 function putInFront(event) {
-  event.target.style.order = `${order}`;
-  order = order - 1;
+
+  event.target.style.transform = `translate(0, -${event.target.offsetHeight * 2}px)`;
+  event.target.style.transitionDuration = '2s';
+  
+  setTimeout(function() {
+    event.target.style.transform = `translate(0, 0px)`;
+    event.target.style.order = `${order}`;
+    order = order - 1;
+  }, 200)
 }
 
 function goToRight(event) {
