5ff9104a07ea7152f2039e9823b7b45595ba6485
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-10 16:01:51 -0700

    add animation for rocket going up

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -2,7 +2,9 @@ let blockParent = document.querySelectorAll('.block');
 blockParent.forEach((block, index) => {
 	block.style.order = index;
 	console.log(blockParent[index].style.order);
-	block.addEventListener('click', ()=>{
+	block.addEventListener('click', (event)=>{
+		event.target.style.transform = `translate(0, -${event.target.offsetHeight + 100} px)`;
+		event.target.style.transitionDuration = '1.5s';
 		block.style.order = 0;
 		incrementElse(blockParent, index);
 	})
