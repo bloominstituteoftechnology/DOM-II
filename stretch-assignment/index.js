8d54d7f75c7d6be95a8f7a13e37ade61b6a8f544
Arthur Bates <aabatesj@gmail.com>
2018-07-10 15:06:38 -0400

    Arthur Bates
    First PR

---
+++ stretch-assignment/index.js
@@ -0,0 +1,6 @@
+let navElements = document.getElementsByClassName("blocks");
+let navElements = document.getElementsByClassName("block block--red");
+let navElements = document.getElementsByClassName("block block--blue");
+let navElements = document.getElementsByClassName("block block--green");
+let navElements = document.getElementsByClassName("block block--pink");
+let navElements = document.getElementsByClassName("block block--gray");
