03ecaf7e7b70c5d234226ce6394076cc6462e341
johnoro <jjcummins102@gmail.com>
2018-07-10 18:51:02 -0400

    Added functionality for travelers and completed stretch.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,11 +1,38 @@
 /* index.js for the stretch assignment */
 
-// Selectors
-
 let blocks = document.querySelectorAll('.blocks .block');
 
 blocks.forEach(block => {
+    // click block to move it to the top (of its column)
     block.addEventListener('click', e => {
         document.querySelector('.blocks').prepend(e.target);
+    });
+    
+    let i = 0;
+
+    // move right on left click
+    block.addEventListener('mousedown', e => {
+        const moveRight = setInterval(() => {
+            i++;
+            e.target.style['margin-left'] = `${i}px`;
+            if (i > 500) clearInterval(moveRight);
+        }, 10);
+
+        e.target.addEventListener('mouseup', () => {
+            clearInterval(moveRight);
+        });
+    });
+
+    // return left on release of left click
+    block.addEventListener('mouseup', e => { 
+        const moveLeft = setInterval(() => {
+            i--;
+            e.target.style['margin-left'] = `${i}px`;
+            if (i < 11) clearInterval(moveLeft);
+        }, 10);
+
+        e.target.addEventListener('mousedown', () => {
+            clearInterval(moveRight);
+        });
     });
 });
