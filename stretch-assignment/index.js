3bc7e2636c9bcdbc295a2ad4d656f020f3cda40b
Arthur Bates <aabatesj@gmail.com>
2018-07-10 17:53:12 -0400

    added DOM-I files
    and added more events to remaining boxes from DOM-II

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -10,11 +10,25 @@ let grayBlock = document.querySelector(".block--gray");
 
 
 //Events
+redBlock.innerHTML = "Click me!";
 redBlock.addEventListener("click", function () { redBlock.style.background = "lightblue" });
 
-blueBlock.addEventListener("click", changeText);
-function changeText() {
-    blueBlock.textContent = "Here is some text for the blue box"
-};
+blueBlock.innerHTML = "Click me!";
+blueBlock.style.color = "white";
+blueBlock.addEventListener("click", function () {
+    blueBlock.style.background = "orange";
+    blueBlock.textContent = "Here is some text for the now orange box";
+});
 
+greenBlock.innerHTML = "Hover here!";
 greenBlock.addEventListener("mouseover", function () { greenBlock.style.border = "3px solid red" });
+
+pinkBlock.innerHTML = "Hover here!";
+pinkBlock.addEventListener("mouseenter", function () { pinkBlock.textContent = "Don't leave!"; });
+pinkBlock.addEventListener("mouseleave", function () { alert("Goodbye :..("); });
+
+grayBlock.innerHTML = "Wanna know a secret?";
+grayBlock.addEventListener("mouseover", function () {
+    grayBlock.style.background = "white"
+    grayBlock.textContent = "I've been working on the wrong file the whole time.";
+});
