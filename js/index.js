29fe40e3bc4a304101564f288b7296bbad6171d1
bri79319 <bri79319@gmail.com>
2018-07-10 17:05:00 -0400

    more changes

--- js/index.js
+++ js/index.js
@@ -93,6 +93,11 @@ buttonText.addEventListener('click', (event) => {
 
 let circleImage = document.getElementById("cta-img");
 circleImage.setAttribute('src', siteContent["cta"]["img-src"]);
+circleImage.addEventListener('click', () => {
+
+  CustomEase.create("hop", "M0,0.005 C0,0.005 0.056,0.445 0.175,0.445 0.294,0.445 0.332,0 0.332,0 0.332,0 0.414,1 0.671,1 0.991,1 1,0 1,0");
+  TweenLite.to("#cta-img", 2, { ease: "hop", scale:1.5, rotation:30 });
+});
 
 let topContentHeader = document.querySelectorAll(".top-content h4");
 topContentHeader[0].innerHTML = siteContent["main-content"]["features-h4"];
