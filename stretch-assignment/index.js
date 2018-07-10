1269aac0f434d9f294d1752c545896a664244650
sliw <slioweng@gmail.com>
2018-07-11 06:23:20 +0800

    add stretch goal 2

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -3,14 +3,14 @@ const boxList = document.querySelectorAll('.block');
 
 boxList.forEach(function(box) {
   box.addEventListener('click', putInFront);
-  // box.addEventListener('click', goToRight);
+  box.addEventListener('mousedown', goToRight);
+  box.addEventListener('mouseleave', reset);
 })
 
 // rockets
 let order = -1;
 
 function putInFront(event) {
-
   event.target.style.transform = `translate(0, -${event.target.offsetHeight * 2}px)`;
   event.target.style.transitionDuration = '2s';
   
@@ -22,12 +22,18 @@ function putInFront(event) {
 }
 
 function goToRight(event) {
-  let infinteVal = 0;
+  // let infinteVal = 0;
 
-  setInterval(function() {
-    infinteVal += 10;
-    event.target.style.transform = `translate(${infinteVal}px, 0)`;
-  }, 100)
+  // setInterval(function() {
+  //   infinteVal += 10;
+  //    event.target.style.transform = `translate(${infinteVal}px, 0)`;
+  // }, 100)
   
+  event.target.style.transform = `translate(600px, 0)`
   event.target.style.transitionDuration = '3s';
+}
+
+function reset(event) {
+  event.target.style.transform = `translate(0, 0)`
+  event.target.style.transitionDuration = '1s';
 }
