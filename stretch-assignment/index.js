283f46a39424c8cdccfe4daf01708188d9199a9d
Will Kwon <kwontaewoongah@gmail.com>
2018-07-10 16:12:40 -0700

    Update index.js

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,6 +1,20 @@
 const blocks=document.querySelectorAll('.blocks .block');
 let index=0;
-order=(e)=>{e.target.style.order=index;index-=1;};
+let interval;
+let size=10;
+let lastClicked;
+rockets=(e)=>{e.target.style.order=index;index-=1;};
+travelers=(e)=>{
+    lastClicked=e.target;
+    interval=setInterval(()=>{size+=2;e.target.style.marginLeft=size+'px';},10);
+}
+reset=()=>{
+    clearInterval(interval);
+    size=10;
+    lastClicked.style.marginLeft=size+'px';
+}
 for (let i=0; i<blocks.length; i++) {
-    blocks[i].addEventListener('click',order);
+    blocks[i].addEventListener('click',rockets);
+    blocks[i].addEventListener('mousedown',travelers);
+    blocks[i].addEventListener('mouseleave', reset);
 }
