399f2bf3bd11a8bd1ced94f7bde9c8b2c0dd2789
Grant Reighard <grantreighard@Grants-iMac.local>
2018-07-10 18:08:03 -0400

    Working through stretch project still, just need to animate the ordering of boxes.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,7 +1,25 @@
 let blocks = document.querySelectorAll(".block");
 
+let first = -1;
 blocks.forEach(el => el.addEventListener("click", event => {
-    let tmp = blocks[0].className;
-    blocks[0].className = event.target.className;
-    event.target.className = tmp;
+    // let tmp = blocks[0].getBoundingClientRect().top;
+    // let tmp2 = event.target.getBoundingClientRect().top;
+    // TweenMax.to(el, 10, {y:tmp-tmp2});
+    // TweenMax.to(blocks[0], 2, {y:tmp2-tmp});
+
+    el.style.order = --first;
+    
 }));
+
+blocks.forEach(el => el.addEventListener("mousedown", event => {
+    TweenMax.to(el, 15, {x:2000, repeat:-1});
+}));
+
+blocks.forEach(el => el.addEventListener("mouseup", event => {
+    TweenMax.to(el, 1, {x:0});
+}));
+
+blocks.forEach(el => el.addEventListener("mouseleave", event => {
+    TweenMax.to(el, 1, {x:0});
+}));
+
