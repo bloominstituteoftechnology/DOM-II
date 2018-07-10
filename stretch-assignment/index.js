0b2e7c7221902cccbd56e51629514129fedf214b
Arthur Bates <aabatesj@gmail.com>
2018-07-10 15:10:12 -0400

    variables
    set selectors by class name for blocks

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,6 +1,8 @@
-let navElements = document.getElementsByClassName("blocks");
-let navElements = document.getElementsByClassName("block block--red");
-let navElements = document.getElementsByClassName("block block--blue");
-let navElements = document.getElementsByClassName("block block--green");
-let navElements = document.getElementsByClassName("block block--pink");
-let navElements = document.getElementsByClassName("block block--gray");
+// element.addEventListener( event, function, useCapture);
+
+let blocks = document.getElementsByClassName("blocks");
+let redBlock = document.getElementsByClassName("block block--red");
+let blueBlock = document.getElementsByClassName("block block--blue");
+let greenBlock = document.getElementsByClassName("block block--green");
+let pinkBlock = document.getElementsByClassName("block block--pink");
+let grayBlock = document.getElementsByClassName("block block--gray");
