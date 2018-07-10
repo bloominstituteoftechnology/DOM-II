1c3e57c1883f4d46b2520435d62ef32904e96fb0
sliw <slioweng@gmail.com>
2018-07-11 06:07:03 +0800

    implement travelers

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -3,6 +3,7 @@ const boxList = document.querySelectorAll('.block');
 
 boxList.forEach(function(box) {
   box.addEventListener('click', putInFront);
+  box.addEventListener('click', goToRight);
 })
 
 // rockets
@@ -11,4 +12,15 @@ let order = -1;
 function putInFront(event) {
   event.target.style.order = `${order}`;
   order = order - 1;
+}
+
+function goToRight(event) {
+  let infinteVal = 0;
+
+  setInterval(function() {
+    infinteVal += 10;
+    event.target.style.transform = `translate(${infinteVal}px, 0)`;
+  }, 100)
+  
+  event.target.style.transitionDuration = '3s';
 }
