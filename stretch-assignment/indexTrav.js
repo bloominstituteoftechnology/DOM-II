77940ba09171f9f0aac425a98ad36d0bc201362c
Jun Hyuk Kim <junhyukkeem@gmail.com>
2018-07-10 14:58:04 -0700

    complete stretch goals for travelers
    add limit on distance
    
    add travel left functionality
    
    add continue functionality when mousedown is triggered during left motion

--- stretch-assignment/indexTrav.js
+++ stretch-assignment/indexTrav.js
@@ -1,12 +1,28 @@
 let blockParent = document.querySelectorAll('.block');
 blockParent.forEach((block, index) => {
 	let increment = 0;
-	block.addEventListener('mousedown', ()=>{
-		setInterval(()=>{
+	block.addEventListener('mousedown', (event)=>{
+		let increase = setInterval(()=>{
 			increment += 10;
-			console.log(increment);
 			block.style['margin-left'] = `${increment}px`
+			if (increment > 400){
+				clearInterval(increase);
+			}
 		}, 100)
-	})
+		block.addEventListener('mouseup', ()=>{
+			clearInterval(increase);
+		});
+	});
+	block.addEventListener('mouseup', (event)=>{
+		let decrease = setInterval(()=>{
+			increment -= 10;
+			block.style['margin-left'] = `${increment}px`
+			if (increment < 20){
+				clearInterval(decrease);
+			}
+		}, 100)
+		block.addEventListener('mousedown', ()=>{
+			clearInterval(decrease);
+		});
+	});
 });
-
