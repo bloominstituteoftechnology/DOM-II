b833ee0c6d18bed7d606b36600d7dce2724aa652
Arthur Bates <aabatesj@gmail.com>
2018-07-10 15:35:03 -0400

    second event
    click event to add text inside blue box

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,5 +1,6 @@
 // element.addEventListener( event, function, useCapture);
 
+//selectors
 let blocks = document.querySelector(".blocks");
 let redBlock = document.querySelector(".block--red");
 let blueBlock = document.querySelector(".block--blue");
@@ -7,4 +8,10 @@ let greenBlock = document.querySelector(".block--green");
 let pinkBlock = document.querySelector(".block--pink");
 let grayBlock = document.querySelector(".block--gray");
 
+
+//Events
 redBlock.addEventListener("click", function () { redBlock.style.background = "lightblue" });
+blueBlock.addEventListener("click", changeText);
+function changeText() {
+    blueBlock.textContent = "Here is some text for the blue box"
+};
