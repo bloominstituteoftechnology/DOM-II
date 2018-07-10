28d0ea7a1a99e9077afbbf82a139fb4d42218935
Will Kwon <kwontaewoongah@gmail.com>
2018-07-10 16:58:16 -0700

    Finished rockets and travellers

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -2,19 +2,21 @@ const blocks=document.querySelectorAll('.blocks .block');
 let index=0;
 let interval;
 let size=10;
-let lastClicked;
+
 rockets=(e)=>{e.target.style.order=index;index-=1;};
+
 travelers=(e)=>{
-    lastClicked=e.target;
-    interval=setInterval(()=>{size+=2;e.target.style.marginLeft=size+'px';},10);
+    interval=setInterval(()=>{size+=5;e.target.style.marginLeft=size+'px';},100);
+    
 }
-reset=()=>{
-    clearInterval(interval);
+reset=(e)=>{
     size=10;
-    lastClicked.style.marginLeft=size+'px';
+    e.target.style.marginLeft=size+'px';
+    clearInterval(interval);
 }
 for (let i=0; i<blocks.length; i++) {
     blocks[i].addEventListener('click',rockets);
-    blocks[i].addEventListener('mousedown',travelers);
-    blocks[i].addEventListener('mouseleave', reset);
+    blocks[i].addEventListener('mouseover',travelers);
+    blocks[i].addEventListener('mouseleave',reset);
 }
+
