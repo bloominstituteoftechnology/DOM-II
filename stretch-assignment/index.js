4d31549492cfb9fe348574db1f833af7a4bead6d
dasgma <dasgrigoma@gmail.com>
2018-07-10 22:09:37 -0700

    OMG. I think i have killed the dinosaur with moving boxes to the right.

---
+++ stretch-assignment/index.js
@@ -0,0 +1,57 @@
+       
+   
+   //Moving boxes to the right side of the screen when pressing right mouse click
+   //Goes back to start position after releasing the left mouse button
+   
+   let redBox = TweenMax.to(".block--red", 5, {x:600, paused: true});
+
+   let blockRed = document.querySelector(".block--red");
+   blockRed.onmousedown = () => {
+       redBox.play();
+   }
+   blockRed.onmouseup = () => {
+       redBox.reverse();
+   }
+
+   let blueBox = TweenMax.to(".block--blue", 5, {x:600, paused: true});
+
+   let blockBlue = document.querySelector(".block--blue");
+   blockBlue.onmousedown = () => {
+       blueBox.play();
+   }
+   blockBlue.onmouseup = () => {
+       blueBox.reverse();
+   }
+
+   let greenBox = TweenMax.to(".block--green", 5, {x:600, paused: true});
+
+   let blockGreen = document.querySelector(".block--green");
+   blockGreen.onmousedown = () => {
+       greenBox.play();
+   }
+   blockGreen.onmouseup = () => {
+       greenBox.reverse();
+   }
+
+   let pinkBox = TweenMax.to(".block--pink", 5, {x:600, paused: true});
+
+   let blockPink = document.querySelector(".block--pink");
+   blockPink.onmousedown = () => {
+       pinkBox.play();
+   }
+   blockPink.onmouseup = () => {
+       pinkBox.reverse();
+   }
+
+   let grayBox = TweenMax.to(".block--gray", 5, {x:600, paused: true});
+
+   let blockGray = document.querySelector(".block--gray");
+   blockGray.onmousedown = () => {
+       grayBox.play();
+   }
+   blockGray.onmouseup = () => {
+       grayBox.reverse();
+   }
+
+
+   
