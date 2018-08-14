d6b91d562e86f322946b78c471776cbf338f6778
m-j-hacker <mjhacker@gmail.com>
2018-08-14 15:27:50 -0500

    MOAR event listeners

--- js/index.js
+++ js/index.js
@@ -2,10 +2,13 @@
 console.log("Test!");
 
 // variables to point to parts of DOM being changed
+const mainPage = document.querySelector(".home");
 const navigation = document.querySelector(".nav");
 const logoHeading = document.querySelector(".logo-heading")
-const header = document.querySelector("header");
+const headerIntro = document.querySelector(".intro");
 const topImg = document.querySelector(".img-content")
+
+//Multiple items to cache
 const textContent = document.querySelectorAll(".text-content");
 const allH4Text = document.querySelectorAll("h4");
 // Event Listeners
@@ -14,6 +17,14 @@ navigation.addEventListener('mouseover', (event) => {
   event.stopPropagation();
 });
 logoHeading.addEventListener('click', (event) => {
-  event.currentTarget.parentNode.style.color = "blue";
+  event.currentTarget.parentNode.style.color = "red";
   console.log("Ouch! That hurt!");
+  console.log("Wait a minute, am I bleeding??")
+});
+headerIntro.addEventListener('mouseover', (event) => {
+  event.currentTarget.style.width = "110%";
+  headerIntro.addEventListener('mouseleave', (event) => {
+    event.currentTarget.style.width = "100%";
+    event.stopPropagation();
+  });
 });
