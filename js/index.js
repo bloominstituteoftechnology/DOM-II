038636366745015122a729808d4b9e6da53063e1
Caleb Kirkwood <calebkirkwood@calebs-mbp.attlocal.net>
2018-08-14 18:30:22 -0400

    Added 4 event listeners, working on a drag and drop

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
-// Your code goes here
+const images = document.querySelectorAll('img');
+// images fade in on load
+images.forEach((item) => {
+  item.addEventListener('load', () => {
+    TweenMax.to('img', 3, { opacity: 1 });
+  });
+});
+// resize images on mouse scroll
+images.forEach((item) => {
+  item.addEventListener('wheel', (event) => {
+    let newWidth = event.target.width;
+    newWidth += 10 * event.deltaY;
+    item.width = newWidth;
+    event.preventDefault();
+  });
+});
+// resize a tags on mouseover
+const allATags = document.querySelectorAll('a');
+allATags.forEach((item) => {
+  item.addEventListener('mouseover', () => {
+    item.style.fontWeight = 900;
+    TweenMax.to(item, 0.2, { fontSize: 20 });
+  });
+  item.addEventListener('mouseleave', () => {
+    item.style.fontWeight = 100;
+    TweenMax.to(item, 0.2, { fontSize: 16 });
+  });
+});
+
+// const mainNav = document.querySelector('.main-navigation');
+// mainNav.addEventListener(
+//   'drag',
+//   (event) => {
+//     // console.log(event);
+//   },
+//   false
+// );
+// mainNav.addEventListener(
+//   'drop',
+//   (event) => {
+//     event.preventDefault();
+//     // x = event.offsetX;
+//     console.log(test);
+//   },
+//   false
+// );
