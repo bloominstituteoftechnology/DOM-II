001e2bbd3ac791dd480ad81d245651bd659be133
Max <maxkajiwara@gmail.com>
2018-08-14 12:57:10 -0700

    Planned events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
-// Your code goes here
+/*[ Element selectors ]*/
+
+ const homeContainer = document
+
+
+ /*[ Event listeners ]*/
+
+ //Load page
+
+
+ //Click the bus
+
+
+ //Scroll the page
+
+
+ //Press any key
+
+
+ //Resize the window
+
+
+ //Double-click the copyright footer
+
+
+ //Mouseover each 'Sign Me Up!' button ~(turn each green)
+
+
+ //Highlight some text in the second paragraph
+
+
+ //Drag the first photo of a boat
+
+
+ //Drop it somewhere (here will work:)
+
+ 
