d4a2c7914fc429e34f539fff9d67c3a912203a58
m-j-hacker <mjhacker@gmail.com>
2018-08-14 15:04:37 -0500

    Working on Event listeners

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@ const logoHeading = document.querySelector(".logo-heading")
 const header = document.querySelector("header");
 const topImg = document.querySelector(".img-content")
 const textContent = document.querySelectorAll(".text-content");
+const allH4Text = document.querySelectorAll("h4");
+// Event Listeners
+navigation.addEventListener('mouseover', (event) => {
+  console.log("We sensed a mouse dancing above a link.");
+  event.stopPropagation();
+});
+logoHeading.addEventListener('click', (event) => {
+  event.currentTarget.parentNode.style.color = "blue";
+  console.log("Ouch! That hurt!");
+});
