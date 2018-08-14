c4922f1a8956561816e5ebd279c44c82e5ba4c84
Matthew Sokoloff <matthewsokoloff@Matthews-MacBook-Pro.local>
2018-08-14 17:56:30 -0500

    I played with it more than I created to try to understand how things worked, I currently have 3 eventsListeners out of 10.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+let logo = document.querySelector('.logo-heading')
+let nav = document.querySelectorAll('.nav a');
+let button = document.querySelectorAll('.btn')
+
+logo.addEventListener('dblclick', () => {
+    alert('This is NOT clickable!')
+});
+
+nav.forEach((a) => {
+    a.addEventListener('wheel', () => {
+        a.style.fontSize = '2rem';
+    });
+});
+
+
+button.forEach((btn) => {
+    btn.addEventListener('mouseover', () => {
+        btn.style.background = 'black';
+    });
+});
