8a9e5aea01a652d421d1bb336dc8d6d0cba4bc33
Michael Agard <mdagard@gmail.com>
2018-08-14 15:43:06 -0400

    added janky fade out effect on fun bus image scroll

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+// * [ ] `mouseover`
+// * [ ] `keydown`
+// * [ ] `wheel`
+// * [ ] `drag / drop`
+// * [ ] `load`
+// * [ ] `focus`
+// * [ ] `resize`
+// * [x] `scroll`
+// * [ ] `select`
+// * [ ] `dblclick`
+// .stopPropagation();
+let opacity = 1;
+const mainNav = document.querySelector('.nav-container .nav');
+const funBusImage = document.querySelector('.intro img');
+funBusImage.style.display = 'auto';
+// janky fade out effect on funBusImage scroll
+window.addEventListener('scroll', (event) => {
+    opacity = opacity - .3;    
+    funBusImage.style.opacity = `${opacity}`;
+    if (opacity <= 0) {funBusImage.style.display = 'none'}
+})
+
+window.addEventListener('resize', (event) => {
+    funBusImage.style.opacity = 1;
+})
