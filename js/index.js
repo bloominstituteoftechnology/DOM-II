72dd93f8f59dc8d02e314041b9c680440237f5e1
alejandrok93 <alejandrok93@gmail.com>
2018-08-14 15:11:25 -0700

    added more events

--- js/index.js
+++ js/index.js
@@ -7,6 +7,8 @@ let headers = document.querySelector(".intro h2");
 let input = document.querySelector(".input");
 let navItems = document.querySelectorAll(".nav-link");
 let go = document.querySelector(".text-content h2");
+let images = document.querySelectorAll("img");
+console.log(images);
 let fontColor = "#212529";
 
 //Add event: make logo larger on mouseover and addHover
@@ -22,16 +24,19 @@ header.addEventListener("mouseover", function(event) {
 
 //Add event: keydown to input field
 input.addEventListener("keydown", event => {
-  headers.innerText = "STOP TYPING!!!";
+  headers.innerText = "STOP TYPING!!";
 
-  //   headers.forEach(item => {
-  //     item.innerText = "";
-  //   });
+  headers.forEach(item => {
+    item.innerText = "";
+  });
 });
 
-//Add event : mouseenter + mouse leave
+//Add event : mouseenter + mouse leave , prevent click
 navItems.forEach(function(item) {
   addHover(item);
+  item.addEventListener("click", function(event) {
+    event.preventDefault();
+  });
 });
 function addHover(item) {
   item.addEventListener("mouseenter", function(event) {
@@ -46,4 +51,35 @@ function addHover(item) {
 go.addEventListener("click", function(event) {
   let img = document.querySelector(".img-content");
   img.classList.toggle("rotate");
+});
+
+//Add event: opacity on images whe mouse over
+images.forEach(function(img) {
+  img.addEventListener("mouseenter", function(item) {
+    img.style.opacity = ".3";
+  });
+
+  img.addEventListener("mouseleave", function(item) {
+    img.style.opacity = "1";
+  });
+});
+
+// Add event: scroll
+let counter = 0;
+window.addEventListener("scroll", function(event) {
+  console.log("scroll!");
+  counter++;
+  if (counter > 200) {
+    alert("You've scrolled too much!!");
+  }
+});
+
+//Add event: resize
+window.addEventListener("resize", function(event) {
+  alert("Is this better?!");
+});
+
+//Add event: load
+window.addEventListener("load", function(event) {
+  console.log("It loads!");
 });
