f70ce9d7e3b0a3a53f56915c67138e752d905a08
Clint Kunz <clintkunz@gmail.com>
2018-08-14 14:06:56 -0600

    added mouseover event to logo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+//mouseover
+const logo = document.querySelector(".logo-heading");
+logo.addEventListener('mouseover', () => {
+    logo.style.fontSize= '5rem'; 
+})
+//keydown
+
+//wheel
+
+//drag/drop
+
+//load
+
+//focus
+
+//resize
+
+//scroll
+
+//select
+
+//dblclick
