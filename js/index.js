80b17b298cd360f8207c9b99f0e1637c934873a5
BrianARuff <thelonewolf198927@gmail.com>
2018-08-14 16:35:35 -0400

    mouseenter mouseleave events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const aTagArray = Array.from(document.querySelectorAll('a'));
+
+// *** loop over a tags and apply color changes on specified events ***
+aTagArray.map(a => {
+    // *** MOUSE ENTER ***
+    a.addEventListener('mouseenter', function (e) {
+        e.target.style.color = "green";
+    });
+
+    // ** MOUSE LEAVE
+    a.addEventListener('mouseleave', function (e) {
+        e.target.style.color = "black";
+    });
+});
+
