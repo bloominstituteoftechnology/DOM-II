05643b328c5aac688ab80dfbf632bfef3b3b7fe2
m-j-hacker <mjhacker@gmail.com>
2018-08-14 14:50:15 -0500

    Added some selectors to index.js

--- js/index.js
+++ js/index.js
@@ -1,2 +1,9 @@
 // Your code goes here
 console.log("Test!");
+
+// variables to point to parts of DOM being changed
+const navigation = document.querySelector(".nav");
+const logoHeading = document.querySelector(".logo-heading")
+const header = document.querySelector("header");
+const topImg = document.querySelector(".img-content")
+const textContent = document.querySelectorAll(".text-content");
