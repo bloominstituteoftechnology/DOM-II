d9ebb654979299b4e8b903ad6efdae8fa3de8fab
Cameron Ray <cameronray@Camerons-MacBook-Pro.local>
2018-08-14 16:20:15 -0700

    Commit before Standup

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+const parentContainer = document.querySelector('.home');
+const navBox = document.querySelector('nav');
+const navLinks = document.querySelectorAll('nav a');
+const textContent = document.querySelector('.text-content');
+
+
+navBox.addEventListener('mouseover', (event) => {
+    console.log('Hello');
+    navBox.style.backgroundColor = 'red';
+    navLinks.style.color = 'green';
+});
+
+textContent.addEventListener('click', (event) => {
+    console.log('Hello');
+    textContent.style.color = 'purple';
+})
