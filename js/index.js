b8519a11fed96ee03e317d433ee7bf05928beeb9
Michael Agard <mdagard@gmail.com>
2018-08-14 16:53:17 -0400

    added mouseover events

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Your code goes here
-// * [ ] `mouseover`
+// * [x] `mouseover`
 // * [ ] `keydown`
 // * [ ] `wheel`
 // * [ ] `drag / drop`
@@ -10,9 +10,13 @@
 // * [ ] `select`
 // * [ ] `dblclick`
 // .stopPropagation();
+let firstPageLoad = true;
 let opacity = 1;
+let width = 0;
 const mainNav = document.querySelector('.nav-container .nav');
 const funBusImage = document.querySelector('.intro img');
+const universalImgSelect = document.querySelectorAll('img');
+const body = document.querySelector('body');
 funBusImage.style.display = 'auto';
 // janky fade out effect on funBusImage scroll
 window.addEventListener('scroll', (event) => {
@@ -25,17 +29,27 @@ window.addEventListener('resize', (event) => {
     funBusImage.style.opacity = 1;
 })
 
-// window.addEventListener('load', (event) => {
-//     alert('Welcome to my Fun Bus Page!');
-//     alert('I hope you enjoy your stay here on the Fun Bus!')
-//     alert('The Fun Bus strives to be a wild ride!')
-//     alert('I wish all my content was delivered to me through alert messages in my browser.')
-//     alert('Content control would be a thing of the past!')
-//     alert('Just think about it, you could just click a simple button to get more information to consume.')
-//     alert(`Honestly, you wouldn't need to think about much else.`)
-//     alert('Just mindlessly clicking... clicking... clicking...')
-//     alert('How about we change up the boxes a little bit. You know what to do.')
-//     confirm(`Sometimes options can be fun! Who knew "cancel" could be so enticing! Go ahead and click whatever you'd like. You've earned it.`);
-//     alert(`Not that it makes much of a choice. It's all about the illusion of choice, am I right? The idea of power is intoxicating.`)
-//     alert(`I want to get off Fun Bus's wild ride`)
-// })
+universalImgSelect[1].addEventListener('mouseover', (event) => {
+    width === 50 ? alert(`That's too far!`) : width += 5;
+    universalImgSelect[1].style.borderRadius = `${width}%`;
+})
+
+window.addEventListener('load', (event) => {
+    // window.alert('Welcome to my Fun Bus Page!');
+    // alert('I hope you enjoy your stay here on the Fun Bus!')
+    // alert('The Fun Bus strives to be a wild ride!')
+    // alert('I wish all my content was delivered to me through alert messages in my browser.')
+    // alert('Content control would be a thing of the past!')
+    // alert('Just think about it, you could just click a simple button to get more information to consume.')
+    // alert(`Honestly, you wouldn't need to think about much else.`)
+    // alert('Just mindlessly clicking... clicking... clicking...')
+    // alert('How about we change up the boxes a little bit. You know what to do.')
+    // confirm(`Sometimes options can be fun! Who knew "cancel" could be so enticing! Go ahead and click whatever you'd like. You've earned it.`);
+    // alert(`Not that it makes much of a choice. It's all about the illusion of choice, am I right? The idea of power is intoxicating.`)
+    // alert(`I want to get off Fun Bus's wild ride`);
+    // body.classList.remove('blur');
+})
+
+window.addEventListener('focus', (event) => {
+    
+})
