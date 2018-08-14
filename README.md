71cddabdd674fa80e6694a370a99462b595bbb47
Joseph Chretien-Golden <joechre@gmail.com>
2018-08-14 18:14:37 -0500

    6:15

--- README.md
+++ README.md
@@ -33,8 +33,8 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 ## Task 3: Create Unique Event Listeners
 
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
-	* [ ] `keydown`
+	* [ ] `mouseover`// that makes the funbus bigger
+	* [ ] `keydown` 
 	* [ ] `wheel`
 	* [ ] `drag / drop`
 	* [ ] `load`
@@ -42,7 +42,7 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 	* [ ] `resize`
 	* [ ] `scroll`
 	* [ ] `select`
-	* [ ] `dblclick`
+	* [ ] `dblclick`top image goes to magic school bus site
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,87 @@
 // Your code goes here
+const toSchoolSite = document.querySelector('.top-img');
+const enlarge = document.querySelector('.logo-heading');
+const goNow = document.querySelector('.lets-go');
+const button = document.querySelector('.btn2');
+const pic2 = document.querySelector('.img-content');
+const adv = document.querySelector('.adv');
+const pickText = document.querySelector('.pick-text');
+const welcome = document.querySelector('.welcome');
+const fun = document.querySelector('.fun');
+
+
+
+
+// Step 2: Add an Event Listener to the reference
+toSchoolSite.addEventListener('dblclick', (event) => { 
+    // Step 3: Do something with the event
+    //event.preventDefault();
+    
+    event.currentTarget.parentNode.parentNode.parentNode.style.backgroundColor = "#FCF0E8";
+    console.log("hello!!!")
+  });
+
+  enlarge.addEventListener('mouseover', (event) => { 
+    
+    event.currentTarget.parentNode.parentNode.style.backgroundColor = "#B4E6F8";
+    
+  });
+
+  goNow.addEventListener('click', (event) => { 
+    console.log("hello?")
+    event.target.style.color = "#11ADE8";
+    
+  });
+
+  button.addEventListener('click', (event) => { 
+    console.log("hello**")
+    event.target.style.color = "#11ADE8";
+    
+  });
+  pic2.addEventListener('click', (event) => { 
+    console.log("hello^^")
+    pic2.classList.toggle('hide-this');
+    //hide??
+  });
+  enlarge.addEventListener('mouseup', (event) => { 
+    console.log("resize")
+    enlarge.classList.toggle('hide-this');
+   
+  });
+
+  adv.addEventListener('dblclick', (event) => { 
+    console.log("golden")
+    event.target.style.color = "#E8C04A";
+    
+  });
+  pickText.addEventListener('dblclick', (event) => { 
+    console.log("pink")
+    event.target.style.color = "#F599DD";
+    
+  });
+  document.addEventListener('keydown', (event) => { 
+    console.log("razzmataz")
+    welcome.style.color = "#D7145C";
+    //button
+  });
+  button.addEventListener('dblclick', (event) => { 
+    console.log("heller")
+    event.target.style.color = "red";
+    
+  });
+
+  document.addEventListener('keypress', (event) => { 
+    console.log("hello^^")
+    fun.classList.toggle('hide-this');
+    //hide??
+  });
+
+
+  
+
+
+
+
+//buttondown?
+
+
