df699e10c550a7773fb91679fe4ecb3c4a570297
IlanL75 <anatoly.leytman@gmail.com>
2018-08-14 15:49:15 -0700

    will be completed later

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+let h = document.querySelector('h1');
+h.addEventListener('mouseover',(event) => {
+	h.style.color = "gold";
+});
+h.addEventListener('mouseout', (event) => {
+	h.style.color = "black";
+});
+
+
+
