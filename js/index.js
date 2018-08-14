0b4ac4d52495460abfe6b6d3ab05152972fc9aa8
decagondev <decagondevelopment@gmail.com>
2018-08-14 20:20:24 +0100

    added some element selector hooks

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// setup some hooks for the elements
+const mainNav = document.querySelector(".main-navigation");
+const mainNavTags = document.querySelectorAll("a");
+const introImg = document.querySelector(".intro-img");
+const funImg = document.querySelector(".fun-img");
+const adventureHeading = document.querySelector(".adventure");
+const freakyClick = document.querySelector(".freaky-click");
+const imgDst = document.querySelector(".img-dst");
+
+
+
