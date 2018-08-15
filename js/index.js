fbcd4e9af812b48706296216709cf081d29a510e
jeanfern5 <jeanfern555@gmail.com>
2018-08-15 15:15:46 -0500

    Added a few more event listeners.

--- js/index.js
+++ js/index.js
@@ -1,10 +1,17 @@
 // Your code goes here
 let logo = document.querySelector('.logo-heading')
 let nav = document.querySelectorAll('.nav a');
-let button = document.querySelectorAll('.btn')
+let button = document.querySelectorAll('.btn');
+let home = document.querySelector('.home');
+let mainNav = document.querySelector('.main-navigation');
+let footer = document.querySelector('.footer');
+let ctaTitle = document.querySelector('.intro h2');
+let funBus = document.querySelector('.fun-bus');
+const parentContainer = document.querySelector('.home');
+
 
 logo.addEventListener('dblclick', () => {
-    alert('This is NOT clickable!')
+    alert('Calm down!')
 });
 
 nav.forEach((a) => {
@@ -13,9 +20,26 @@ nav.forEach((a) => {
     });
 });
 
-
 button.forEach((btn) => {
     btn.addEventListener('mouseover', () => {
         btn.style.background = 'black';
     });
 });
+
+window.addEventListener('keydown', (e) => {
+    const keyName = e.key;
+    alert('key: ' + keyName);
+});
+
+window.addEventListener('scroll', () => {
+    home.style.background = '#FFC0CB'
+});
+window.addEventListener('scroll', () => {
+    footer.style.background = '#FFC0CB'
+});
+
+
+mainNav.addEventListener('onresize', () => {
+    mainNav.style.background = 'yellow';
+  });
+
