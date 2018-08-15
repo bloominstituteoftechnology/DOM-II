01d2ef41cb60b839dc2fad3f54a794daa818e385
Brock Maraee Rohloff <00AgentB@gmail.com>
2018-08-14 19:00:24 -0600

    ACTUALLY finished project (added form and two event listeners that used it.

--- js/index.js
+++ js/index.js
@@ -17,7 +17,6 @@ for(i = 0; i < navAnchors.length; i++) {
 }
 
 // Logo
-
 const logoHeading = document.querySelector(".logo-heading");
 
 logoHeading.addEventListener("dblclick", e => {
@@ -33,7 +32,7 @@ window.addEventListener("load", e => {
 // Nav Bar Shrink
 const navBar = document.querySelector("header")
 window.addEventListener("scroll", e => {
-if (Math.round(window.scrollY) < 1) {
+if (Math.round(window.scrollY) < 90) {
     TweenMax.to(navBar, .25, {height: "90px"});
 } else {
     TweenMax.to(navBar, .25, {height: "60px"});
@@ -55,19 +54,39 @@ const modal = document.querySelector('#myModal');
 for(i = 0; i < travelButtons.length; i++) {
     let currentButton = travelButtons[i];
     currentButton.addEventListener("click", e => {
-        modal.style.display = "block";
+        TweenMax.to(modal, .25, {opacity: 1} );
+        modal.style.display = "flex";
     });
-}
 
-window.addEventListener("click", e => {
-    e.stopPropagation();
-    if (e.target == modal) {
-        modal.style.display = "none";
-    }
-});
+    window.addEventListener("click", e => {
+        e.stopPropagation();
+        if (e.target == modal) {
+            TweenMax.to(modal, .25, {opacity: 0, onComplete: () => {modal.style.display = "none"}});
+        }
+    });
+    
+}
 
 // Bus Image
 const busImage = document.querySelector(".home .content-section img");
 busImage.addEventListener("wheel", e => {
-    TweenMax.fromTo(busImage, 1, {opacity: 1}, {opacity: .5, repeat: 1, yoyo: true, ease:Sine.easeInOut})
+    TweenMax.fromTo(busImage, 1, {opacity: 1}, {opacity: .5, repeat: 1, yoyo: true, ease:Sine.easeInOut});
 });
+
+// Home Input Bounce
+ const homeInputs = document.querySelectorAll(".findoutmore input");
+ for (i = 0; i < homeInputs.length; i++){
+     let currentInput = homeInputs[i];
+     currentInput.addEventListener("change", e => {
+         TweenMax.fromTo(currentInput, .1, {y:0}, {y:5, repeat: 1, yoyo: true, ease:Sine.easeInOut});
+         currentInput.value="";
+         alert("INVALID ENTRY!");
+     },false);
+ }
+
+ // Clear Textarea on Focus
+ const formTArea = document.querySelector("textarea");
+    formTArea.addEventListener("focus", e => {
+    formTArea.value="";
+ });
+ 
