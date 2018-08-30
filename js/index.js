9922542f891a2631cae034e01f4c08c034029b0f
TheGameFreak720 <luis.alvarez@commithub.com>
2018-08-29 23:41:58 -0400

    Finished first event mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const button = document.querySelectorAll('.btn');
+
+const buttonColorHandler = function() {
+    this.style.backgroundColor = 'white';
+    this.style.color = '#17A2B8';
+}
+
+button.forEach(button => {
+    button.addEventListener('mouseover', buttonColorHandler);
+});
