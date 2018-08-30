fecadf4c51cc37ed5d14559248942cc78bdb4584
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-08-30 16:23:38 -0400

    Employed preventDefault, currently adding unique events.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,53 @@
 // Your code goes here
+/*
+
+
+mouseenter
+
+dblclick
+
+
+
+click
+
+*/
+/*Default prevention.*/
+let navLinks = document.querySelectorAll('.nav-link');
+let adventureImg = document.querySelector('.img-content img');
+console.log(navLinks);
+
+navLinks.forEach(item => {
+    item.addEventListener('click', function(event){
+        event.preventDefault();
+        console.log(event);
+    })
+})
+
+navLinks.forEach(item => {
+    item.addEventListener('mouseenter', function(){
+       if ( item.style.fontStyle === ''){
+           item.style.fontStyle = 'italic' ;
+       }
+
+    })
+
+    item.addEventListener('click', function(){
+        item.style.fontStyle = 'bold';
+    })
+})
+
+let fontColor = function(){
+    this.style.color = 'purple'
+}
+
+navLinks.forEach(item => {
+    item.addEventListener('click', fontColor);
+})
+
+
+adventureImg.addEventListener('dblclick', function(){
+    
+    adventureImg.setAttribute('src', 'https://cdn.pixabay.com/photo/2016/11/29/09/49/adventure-1868817_1280.jpg');
+})
+
+console.log(adventureImg);
