d62da5582120e6040dd1009136c4250633c22136
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-08-29 21:10:22 -0700

    Added mouseenter event to header bus image

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
-let funBusH1 = document.querySelector('h1');
+let funBusH1 = document.querySelector('.logo-heading');
 let funner = function () {
-    funBusH1.innerHTML = 'Fun Bus - The Funnest!';
+    if (funBusH1.innerHTML === 'Fun Bus') {
+        funBusH1.innerHTML = 'The Funnest Bus!'
+    }
+    else {
+        funBusH1.innerHTML = 'Fun Bus';
+    }
 }
 funBusH1.addEventListener('click', funner);
+
+let busImage = document.querySelector('.intro img');
+let addBorder = function () {
+    if (busImage.style.borderBottom === '') {
+        busImage.style.borderBottom = '2px dashed #C0C0C0'; 
+    }
+    else {
+        busImage.style.borderBottom = '';
+    }
+}
+busImage.addEventListener('mouseenter', addBorder);
