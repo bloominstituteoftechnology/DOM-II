bd44542c349429b108aec8702dd5ecbfe1016fe2
BrandonMoll <brandonmoll85@gmail.com>
2018-08-29 22:33:37 -0500

    made links turn green when you click them

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+const links = document.querySelectorAll('a');
+console.log(links);
+const changeColorHandler = function(event) {
+    if (this.style.color === '') {
+    this.style.color = 'green';
+    } else {
+        this.style.color = '';
+    }
+};
+
+links.forEach(function (item) {
+    item.addEventListener('click', changeColorHandler)
+});
