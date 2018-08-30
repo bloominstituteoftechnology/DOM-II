61fe1389fa831777de5de9e7d8cbe19fba4c9e95
Wesley Meeks <wesmeeks88@gmail.com>
2018-08-29 22:06:03 -0600

    entered in two eventlisteners to the nav, one effecting the logo of the nav the other effecting the nav anchors.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+//change color of logo, font size and give it a background.
+let logo = document.querySelector('.logo-heading');
+
+  logo.addEventListener('mousemove', (event)=>{
+       logo.style.backgroundColor ='blanchedalmond';
+       logo.style.fontSize='60px';
+       logo.style.color ='limegreen';
+     });
+
+// underline nav items and color limegreen
+let navItems = document.querySelectorAll('nav a');
+navItems.forEach(item =>{
+    item.addEventListener('mouseenter', (event)=>{
+        console.log("mouse enter occured", navItems);
+        item.style.color= 'limegreen';
+        item.style.textDecoration='underline';
+      });
+    });
