f75214b28bb0ce6a77d16ddb9c01981f6406df16
Eric Fleming <efleming111@yahoo.com>
2018-08-30 10:39:34 -0700

    Add drag and drop coupons to index.js

--- js/index.js
+++ js/index.js
@@ -66,4 +66,121 @@ function pageScrollHandler(event){
 
 window.addEventListener('scroll', pageScrollHandler);
 
-//
+// Add drag and drop coupon
+const contentPick = document.querySelector('.content-pick');
+contentPick.style.flexWrap = 'wrap';
+
+const coupons = document.createElement('div');
+coupons.className = 'coupons';
+contentPick.prepend(coupons);
+
+const twentyfivePerOff = document.createElement('div');
+const freeMeals = document.createElement('div');
+
+twentyfivePerOff.draggable = true;
+freeMeals.draggable = true;
+
+twentyfivePerOff.className = 'coupon twenty-five';
+freeMeals.className = 'coupon free-meals';
+
+coupons.appendChild(twentyfivePerOff);
+coupons.appendChild(freeMeals);
+
+const twentyFiveHeader = document.createElement('h4');
+const freeMealsHeader = document.createElement('h4');
+twentyFiveHeader.style.fontSize = '4.0rem';
+freeMealsHeader.style.fontSize = '4.0rem';
+twentyFiveHeader.style.textAlign = 'center';
+freeMealsHeader.style.textAlign = 'center';
+twentyFiveHeader.style.paddingTop = '35px';
+freeMealsHeader.style.paddingTop = '10px';
+const dragNDrop1 = document.createElement('p');
+const dragNDrop2 = document.createElement('p');
+dragNDrop1.style.fontSize = '1.0rem';
+dragNDrop2.style.fontSize = '1.0rem';
+dragNDrop1.style.textAlign = 'center';
+dragNDrop2.style.textAlign = 'center';
+
+twentyFiveHeader.innerHTML = '25% off';
+freeMealsHeader.innerHTML = 'Meals Included';
+dragNDrop1.innerHTML = 'Drag coupon onto a destination button';
+dragNDrop2.innerHTML = 'Drag coupon onto a destination button';
+
+twentyfivePerOff.appendChild(twentyFiveHeader);
+twentyfivePerOff.appendChild(dragNDrop1);
+freeMeals.appendChild(freeMealsHeader);
+freeMeals.appendChild(dragNDrop2);
+
+let draggedElement;
+const sun = document.getElementsByClassName('destination')[0].children[2];
+const mountain = document.getElementsByClassName('destination')[1].children[2];
+const island = document.getElementsByClassName('destination')[2].children[2];
+
+
+document.addEventListener('dragstart', function(event){
+    draggedElement = event.target;
+});
+
+document.addEventListener('dragover', function(event){
+    event.preventDefault();
+})
+
+document.addEventListener('dragenter', function(event){
+    if(event.target === sun){
+        event.target.parentNode.style.backgroundColor = '#FEEEA3';
+    }
+
+    else if(event.target === mountain){
+        event.target.parentNode.style.backgroundColor = '#FEEEA3';
+    }
+
+    else if(event.target === island){
+        event.target.parentNode.style.backgroundColor = '#FEEEA3';
+    }
+});
+
+document.addEventListener('dragleave', function(event){
+    if(event.target === sun){
+        event.target.parentNode.style.backgroundColor = '';
+    }
+
+    else if(event.target === mountain){
+        event.target.parentNode.style.backgroundColor = '';
+    }
+
+    else if(event.target === island){
+        event.target.parentNode.style.backgroundColor = '';
+    }
+});
+
+document.addEventListener('drop', function(event){
+    if(event.target === sun){
+        event.target.parentNode.style.backgroundColor = '';
+        if(draggedElement === twentyfivePerOff){
+            event.target.innerHTML = '25% off!!!'
+        }
+        else if(draggedElement === freeMeals){
+            event.target.innerHTML = 'Free Meals!!!'
+        }
+    }
+
+    else if(event.target === mountain){
+        event.target.parentNode.style.backgroundColor = '';
+        if(draggedElement === twentyfivePerOff){
+            event.target.innerHTML = '25% off!!!'
+        }
+        else if(draggedElement === freeMeals){
+            event.target.innerHTML = 'Free Meals!!!'
+        }
+    }
+
+    else if(event.target === island){
+        event.target.parentNode.style.backgroundColor = '';
+        if(draggedElement === twentyfivePerOff){
+            event.target.innerHTML = '25% off!!!'
+        }
+        else if(draggedElement === freeMeals){
+            event.target.innerHTML = 'Free Meals!!!'
+        }
+    }
+});
