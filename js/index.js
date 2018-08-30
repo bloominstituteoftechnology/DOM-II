42df4108ff3de3aa27915d3fa166c588a2bdaad8
hraya <hraya@lssu.edu>
2018-08-29 23:14:59 -0500

    inital commit, created on eventListener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+let mainHeader = document.querySelector(".intro h2");
+mainHeader.addEventListener("mouseover", function(event){
+    event.target.style.color = "green"
+})
+
+
+
