0c034d081965bcbc56be7554f5287f7ef6a0e3e4
paulapivat <paul.apivat@gmail.com>
2018-08-30 10:55:50 +0700

    add mouseover to main header picture

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 // Your code goes here
 
+
 //mouseover
+document.getElementById('headerpic').style.filter = "sepia(100%)";
+
+let headerpic = document.getElementById('headerpic');
+headerpic.addEventListener("mouseover", function(event) {
+    //this = event.target
+    event.target.style.filter = "sepia(0%)";
+    // reset color so once mouseover, effect takes place
+    setTimeout(function(){
+        event.target.style.filter = "sepia(100%)";
+    }, 500);
+})
 
 //keydown
 
