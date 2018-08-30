a9343cea954f07c848589987bc674da4fc603537
BrendonKootz <BrendonKootz@gmail.com>
2018-08-30 10:48:39 -0500

    Forked&cloned | first event listener added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+
+
+/* FIRST EXAMPLE */
+// On-click of 'Blog' button change text from 'Blog' to 'Whoa Dude'
+const GrabNav = document.getElementsByClassName("nav-link");
+console.log(GrabNav);
+
+GrabNav[2].addEventListener("click", function(){
+	let WhoaDude = "Whoa Dude";
+	GrabNav[2].innerHTML = WhoaDude;
+	}
+	,false);
+
+/* SECOND EXAMPLE */
+
+/* THIRD EXAMPLE */
+
+/* FOURTH EXAMPLE */
+
+/* 5TH EXAMPLE */
+
+/* SIXTH EXAMPLE */
+
+/* SEVENTH EXAMPLE */
+
+/* EIGHTH EXAMPLE */
+
+/* NINTH EXAMPLE */
+
+/* TENTH EXAMPLE */
+
