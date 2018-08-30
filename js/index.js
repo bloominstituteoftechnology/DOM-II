7831f80bcb177eaf186521e5aceb321876316a3e
Ken Barlow <kenbrlow@gmail.com>
2018-08-30 14:26:15 -0700

    Added a mouseover event for all elements of type class "destination"

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+var els = document.getElementsByClassName("destination");
+
+Array.prototype.forEach.call(els, function(el) {
+    // Do stuff here
+    el.addEventListener('mouseover', logElement);
+});
+
+
+function logElement() {
+    console.log('hello');
+}
