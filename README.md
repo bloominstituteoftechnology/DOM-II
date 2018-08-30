addd276b1c1a87fdceae35bfca9df79a50419ce4
cferrell2 <cferrell12182@gmail.com>
2018-08-30 19:52:22 -0400

    add wheel,drag/drop, load event handling

--- README.md
+++ README.md
@@ -35,9 +35,9 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use:
 	* [x] `mouseover`
 	* [x] `keydown`
-	* [ ] `wheel`
-	* [ ] `drag / drop`
-	* [ ] `load`
+	* [x] `wheel`
+	* [x] `drag / drop`
+	* [x] `load`
 	* [ ] `focus`
 	* [ ] `resize`
 	* [ ] `scroll`
--- js/index.js
+++ js/index.js
@@ -1,31 +1,101 @@
 // Your code goes here
 
 //****Mouseover  ********** */
-const funBusImg = document.querySelector('.intro img');
+// const funBusImg = document.querySelector('.intro img');
 
-// cb - Mouseover event handler
-//Temporarily hide bus image
-function hideFunBusImg(event){
-    event.target.style.display = 'none';
+// // cb - Mouseover event handler
+// //Temporarily hide bus image
+// function hideFunBusImg(event){
+//     event.target.style.display = 'none';
 
-    setTimeout(function(){
-        event.target.style.display = '';
-    }, 1000);
+//     setTimeout(function(){
+//         event.target.style.display = '';
+//     }, 1000);
 
-}
-//Add Event listeners for funBusImg
-funBusImg.addEventListener('mouseover', hideFunBusImg);
+// }
+// //Add Event listeners for funBusImg
+// funBusImg.addEventListener('mouseover', hideFunBusImg);
 
 
 //************* Keydown Event**************/
-//change footer text on keydown
-let footer = document.querySelector('.footer');
-let footerText = ''
-//keydown event handler
-function changeFooterText(event){
-    footerText += event.key;
-    footer.textContent = footerText;
-}
+// //change footer text on keydown
+// let footer = document.querySelector('.footer');
+// let footerText = ''
+// //keydown event handler
+// function changeFooterText(event){
+//     footerText += event.key;
+//     footer.textContent = footerText;
+// }
 
-document.addEventListener('keydown', changeFooterText);
+// document.addEventListener('keydown', changeFooterText);
 
+
+/********************************************
+ * wheel event
+ *****************************************/
+
+ //wheel event handler
+// let dynamicFontSize = '1.6';
+
+// function changeFont(event){
+//     console.log(event.deltaY);
+//     console.log(document.querySelector('body').style.fontSize);
+//     if (event.deltaY > 0) {  //wheel direction down
+//         dynamicFontSize = parseFloat(dynamicFontSize) + 0.2;
+//         dynamicFontSize = (dynamicFontSize.toString() + 'em');
+//         document.querySelector('.logo-heading').style.fontSize = dynamicFontSize;
+//     }
+//     if (event.deltaY < 0){  //wheel direction up
+//         dynamicFontSize = parseFloat(dynamicFontSize) - 0.2;
+//         dynamicFontSize = (dynamicFontSize.toString() + 'em');
+//         document.querySelector('.logo-heading').style.fontSize = dynamicFontSize;
+//     }
+// }
+// document.addEventListener('wheel', changeFont);
+
+
+/*********************************************************
+ * DRAG / DROP EVENT
+ ****************************************************** */
+
+//make image draggable
+//  const letsGoImg = document.querySelector('.img-content img');
+// letsGoImg.draggable = true;
+// letsGoImg.style.cursor = 'pointer';
+
+
+// //create functionality for drop container
+// const dropContainer = document.querySelector('.text-content');
+
+// //event handler for drop container
+// function dragDrop(){
+//     dropContainer.appendChild(letsGoImg);
+// }
+
+// //Add event listener for drop container
+// dropContainer.addEventListener('drop', dragDrop);
+
+// // prevent default to allow drop
+// dropContainer.addEventListener("dragover", function (event) {
+//     event.preventDefault();
+// });
+
+
+/********************************************************
+ * LOAD EVENT
+ *****************************************************/
+
+ //Give message saying that resources have finished loading
+ window.addEventListener('load', function(event){
+    //grab header
+    const header = document.querySelector('.main-navigation');
+    const infopara = document.createElement('p');
+    infopara.textContent = 'All Resources have finished loading!';
+    infopara.style.color = '#ff1111';
+    header.prepend(infopara);
+
+    setTimeout(() => {
+        infopara.style.display = 'none';
+    }, 3000);
+
+ });
