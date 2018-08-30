aac23660ed6e99fb4682f9de150db2c7d6f726dd
Bruce Cabanayan <bcabanayan@Bruces-MacBook-Pro.local>
2018-08-29 20:57:12 -0700

    Added click event to Fun Bus header

--- js/index.js
+++ js/index.js
@@ -1,1 +1,5 @@
-// Your code goes here
+let funBusH1 = document.querySelector('h1');
+let funner = function () {
+    funBusH1.innerHTML = 'Fun Bus - The Funnest!';
+}
+funBusH1.addEventListener('click', funner);
