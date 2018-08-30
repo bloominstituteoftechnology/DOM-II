3f12d4e168c1a7e841d633064a31e22e88ada1b3
stevekutz <stkutz@gmail.com>
2018-08-30 00:07:21 -0500

    added 7 more via drag & drop functionality, will do some more to see other event behavior

--- js/index.js
+++ js/index.js
@@ -31,3 +31,60 @@ document.addEventListener('keypress', event => {
   alert(`someone pressed the ${keyPressed} key`);
 });
 
+// ***** alert indicating mouse wheel spun
+document.addEventListener('wheel', () => {
+  alert(`someone spun the mouse wheel`);
+});
+
+// Some dragable code from MDN
+document.addEventListener("drag", function( event ) {
+
+}, false);
+
+document.addEventListener("dragstart", function( event ) {
+  // store a ref. on the dragged elem
+  dragged = event.target;
+  // make it half transparent
+  event.target.style.opacity = .5;
+  event.target.style.zIndex = 2;
+}, false);
+
+document.addEventListener("dragend", function( event ) {
+  // reset the transparency
+  event.target.style.opacity = "";
+}, false);
+
+/* events fired on the drop targets */
+document.addEventListener("dragover", function( event ) {
+  // prevent default to allow drop
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function( event ) {
+  // highlight potential drop target when the draggable element enters it
+  if ( event.target.className == "dropzone" ) {
+    event.target.style.border = "5px solid blue";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function( event ) {
+  // reset background of potential drop target when the draggable element leaves it
+  if ( event.target.className == "dropzone" ) {
+    event.target.style.background = "";
+
+  }
+
+}, false);
+
+document.addEventListener("drop", function( event ) {
+  // prevent default action (open as link for some elements)
+  event.preventDefault();
+  // move dragged elem to the selected drop target
+  if ( event.target.className == "dropzone" ) {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+
+}, false);
