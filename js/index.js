acdc91c9eb1a5fa8f9ae332fc6066e74c857ae8a
cferrell2 <cferrell12182@gmail.com>
2018-08-30 00:54:46 -0400

    add mouseover and keydown event handling

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+
+//****Mouseover  ********** */
+const funBusImg = document.querySelector('.intro img');
+
+// cb - Mouseover event handler
+//Temporarily hide bus image
+function hideFunBusImg(event){
+    event.target.style.display = 'none';
+
+    setTimeout(function(){
+        event.target.style.display = '';
+    }, 1000);
+
+}
+//Add Event listeners for funBusImg
+funBusImg.addEventListener('mouseover', hideFunBusImg);
+
+
+//************* Keydown Event**************/
+//change footer text on keydown
+let footer = document.querySelector('.footer');
+let footerText = ''
+//keydown event handler
+function changeFooterText(event){
+    footerText += event.key;
+    footer.textContent = footerText;
+}
+
+document.addEventListener('keydown', changeFooterText);
+
