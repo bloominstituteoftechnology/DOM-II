a618f29beb8e366a3a455bd3e1abe4cdcaa9360d
jbierly1 <james.bierly@gmail.com>
2018-08-30 23:35:34 -0400

    started assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+let logo = document.querySelector('.logo-heading');
+
+logo.addEventListener('mouseover', function() {
+	logo.style.color = 'orange';
+});
+
+logo.addEventListener('mouseleave', function() {
+	logo.style.color = '#212529';
+});
+
+let navitem = document.querySelector('.nav-link');
+
+navitem.addEventListener('focus', function() {
+	navitem.style.color = 'purpl';
+});
