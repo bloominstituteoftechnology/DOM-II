7675864d1dbfe6d90f740ba9f5b2d0a4d23961ca
Julie Jonak <juliejonak10@gmail.com>
2018-08-30 21:14:58 -0500

    Working on a pop up box for load

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 // Your code goes here
 
-const funBusRainbow = document.querySelector('h1');
 
+//**Rainbow Fun Bus Animation on mouse over 1/10 **
+const funBusRainbow = document.querySelector('h1');
 
 funBusRainbow.addEventListener('mouseover', function(){
 
@@ -13,11 +14,13 @@ funBusRainbow.addEventListener('mouseover', function(){
     )
     }
 );
-
 // funBusRainbow.addEventListener('click', function(){
 //     this.style.color = '';
 // })
+//can we toggle on/off animations? Could get it to toggle on but not off
 
+
+//** Nav items change colors on click 2/10**
 //how can I for loop this
 const nav1 = document.querySelectorAll('.nav a')[0];
 const nav2 = document.querySelectorAll('.nav a')[1];
@@ -66,26 +69,31 @@ nav4.onclick = function(event){
     }
 }
 //can I make it click through the rainbow with other else if?
-//can we toggle on/off animations? Could get it to toggle on but not off
 
 
+
+
+//**Turns red when page is re-sized 3/10**
 const welcome = document.querySelectorAll('.intro p')[0];
 window.addEventListener('resize', function(){
     welcome.style = 'color: red';
 })
 
+
+// **Text gets larger when double clicked 4/10**
 const welcome2 = document.querySelectorAll('.text-content p')[1];
 welcome2.addEventListener('dblclick', function(){
     welcome2.style = 'font-size: 2.0rem';
 })
 
+//**Turns blue when the page is scrolled  down 5/10**
 const text = document.querySelectorAll('.content-section p')[0];
 window.addEventListener('scroll', function(){
     text.style = 'color: blue';
 })
 
 
-
+//** All three bottom divs turn aqua on mouseover */
 const bottomDiv1 = document.querySelectorAll('.destination')[0];
 const bottomDiv2 = document.querySelectorAll('.destination')[1];
 const bottomDiv3 = document.querySelectorAll('.destination')[2];
@@ -112,9 +120,19 @@ bottomDiv3.addEventListener('mouseover', function(){
 
 
 
+//prepend to .home after .intro class, before .content-section claass (4 children, 
+// this adds a fifth) --> .insertBefore(NAME, parent.second-child) to put before 2nd child
+// pop up form so would like it to have z-index: 1
 
-
-
+const parentDiv = document.querySelector('.home');
+const box = document.createElement('section');
+parentDiv.prepend(box);
+box.style = 'z-index: 1;'
+box.classList.add('popup');
+box.style = 'height: 300px; width: 80%; margin: 0 auto;';
+box.style.background = 'red';
+box.innerHTML = 'I am a box';
+// box.style.display = 'none'
 
 
 
