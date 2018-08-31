91547fc577d7dd7807c322619989648cdef6d716
ShardeDeLane <rhcprocker@yahoo.com>
2018-08-31 19:56:39 -0400

    playing around with event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+alert('Welcome To The Fun Bus');
+
+let banner = document.querySelector('.intro img');
+let header = document.getElementsByClassName('intro');
+let headerTitle = document.querySelector('.main-navigation');
+
+banner.addEventListener('mouseenter', change);
+banner.addEventListener('mouseleave', original);
+headerTitle.addEventListener('mouseover', headerBackground);
+
+function change(){
+  if(banner.src='../img/fun-bus.jpg'){
+    banner.src = '../img/thor-banner.jpg';
+  }
+}
+
+function original(){
+  if(banner.src='../img/thor-banner.jpg'){
+    banner.src = '../img/fun-bus.jpg';
+  }
+}
+
+function headerBackground(){
+  headerTitle.style.backgroundColor = 'rgb(48, 186, 30)';
+}
+
+
+function changeHeader(){}
