8433dda97d7b91831222ed30e3aace82107e2147
Denise Escobar <descobar1490@gmail.com>
2018-08-30 22:31:07 -0500

    fixed and added another listener

--- js/index.js
+++ js/index.js
@@ -3,14 +3,24 @@ let changeColor = (event) => {
     event.target.style.color = "red";
 }
 
-let changeSize = (event) => {
-    event.target.style.fontSize = "4rem";
+let bottomBorder = (event) => {
+    event.target.style.borderBottom = "1px solid black";
 }
+
+let makeBold = (event) => {
+    event.target.style.fontWeight= "bold";
+}
+
 let logoTxt = document.getElementsByClassName("logo-heading");
 logoTxt[0].addEventListener("mouseenter", changeColor );
 
 
 let links = document.getElementsByClassName("nav-link");
 for(i=0; i<links.length; i++){
-    links[i].addEventListener("mousemove", changeSize);
+    links[i].addEventListener("mousemove", bottomBorder);
+}
+
+let allH2 = document.getElementsByTagName("h2");
+for(i=0; i<links.length; i++){
+    allH2[i].addEventListener("copy", makeBold);
 }
