ea6b162a337347a1dceb59154a7888b5eb62fc0a
Alex Ostrander <alexanderostrander23@gmail.com>
2018-08-30 23:14:52 -0400

    Added resize selector

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
 const aTag = document.querySelectorAll(".nav-link");
-
 aTag.forEach(tag => {
-    tag.addEventListener('click', function(event) { event.preventDefault() });
+    tag.addEventListener('click', function (event) {
+        event.preventDefault()
+    });
+});
+const underLine = function (event) {
+    event.target.style.textDecoration = 'line-through';
+}
+
+aTag.forEach(under => {
+    under.addEventListener('click', underLine);
 });
 
 const para = document.querySelectorAll('p');
-const textColorChange = function(event) {
+const textColorChange = function (event) {
     event.target.style.color = 'green';
 }
 
@@ -14,7 +22,7 @@ para.forEach(text => {
 });
 
 const headerOne = document.querySelectorAll('h1');
-const headerColorChange = function(event) {
+const headerColorChange = function (event) {
     event.target.style.color = 'dodgerblue';
 }
 
@@ -31,3 +39,28 @@ const headerFour = document.querySelectorAll('h4');
 headerFour.forEach(head => {
     head.addEventListener('mousemove', headerColorChange);
 });
+
+const images = document.querySelectorAll('img');
+
+const imgRemove = function(event) {
+    if(event.target.style.display === ''){
+    event.target.style.display = 'none';
+    } else {        
+        event.target.style.display = '';
+    }
+}
+
+images.forEach(img => {
+    img.addEventListener('mousedown', imgRemove);
+});
+
+
+aTag.forEach(color => {
+    color.addEventListener('click', textColorChange);
+});
+
+window.addEventListener('resize', function(event) {
+    alert ("This has been resized.");
+  });
+
+
