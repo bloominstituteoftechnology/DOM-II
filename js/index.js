bdf8b9d4bc381fd01944a27b73ef387183477fe9
Jesse Anderson <jesseanderson@Jesses-MacBook-Pro.local>
2018-08-30 22:56:17 -0500

    First bits of progress on index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+// 1. click
+let logo = document.querySelector('.logo-heading');
+logo.addEventListener('click', function() {
+  this.style.color = 'red';
+});
+
+// 2. mousedown
+logo.addEventListener('mousedown', function() {
+  this.style.color = 'black';
+});
+
+// 3. keydown
+let body = document.querySelector('body');
+body.addEventListener('keydown', function() {
+  this.style.backgroundColor = 'skyblue';
+});
+
+// 4.
