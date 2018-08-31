bbf92e2868de407f41d97a0668297e361e9c1f40
Griffin <griffinshuth@gmail.com>
2018-08-30 20:57:55 -0600

    started on getting my index.js done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const logo = document.querySelector('.logo-heading')
+logo.addEventListener('mouseover', function (){
+    logo.style.color =  'Blue';
+    logo.style.fontSize = '5.6rem';
+});
+const nav1 = document.querySelectorAll('.nav a')
+nav1.forEach(function(element){
+    element.addEventListener('click', function (event){
+    let tag = event.target.style.backgroundColor = 'yellow';
+    
+    
+})
+});
+const intro = document.querySelector('.intro')
+intro.addEventListener('')
