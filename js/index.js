f916faeec0c43abcac4931564a9c9d29ef72dd42
Paul DeYonghe <p.deyonghe@gmail.com>
2018-08-30 21:11:59 -0500

    first commit - halfway done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.querySelector("h1").addEventListener("click", function(){ alert("Clicked!"); });
+document.querySelector("nav a").addEventListener("mouseover", function(){ alert("Home!")});
+document.querySelector("header img").addEventListener("dblclick", function(){ alert("This is a bus!")});
+document.querySelector("header img").addEventListener("mouseout", function(){document.querySelector("h1").style.color = "blue"; });
+document.querySelector(".img-content img").addEventListener("wheel", function(){document.querySelector("header img").style.display = "none"});
