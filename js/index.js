56a5dcb4cd96b9d8e26cbdbde2b7c22d8878b548
Ken Barlow <kenbrlow@gmail.com>
2018-08-31 06:21:26 -0700

    Added remaining event listeners, now going to customize the event types.

--- js/index.js
+++ js/index.js
@@ -4,9 +4,6 @@
 
 // todo - remove the red colors from the functions
 
-// todo - adjust event listeners for 2 - 10
-
-
 // 1
 var itemOne = document.querySelectorAll('header .container h1')[0].addEventListener('mouseover', itemOneFunc);
 
@@ -48,5 +45,55 @@ for (var i = 0; i < itemFive.length; i++) {
 }
 
 function itemFiveFunc(item) {
-    this.style.border = '1px solid green';
+    this.style.fontWeight = 'bold';
+}
+
+// 6
+var itemSix = document.querySelectorAll('footer')[0].addEventListener('mouseover', itemSixFunc);
+
+function itemSixFunc(item) {
+    this.style.border = '1px solid red';
+}
+
+// 7
+var itemSeven = document.querySelectorAll('.destination .btn')
+
+for (var i = 0; i < itemSeven.length; i++) {
+    itemSeven[i].addEventListener('mouseover', itemSevenFunc);
+}
+
+function itemSevenFunc(item) {
+    this.style.border = '1px solid red';
+}
+
+// 8
+var itemEight = document.querySelectorAll('.content-destination')[0].addEventListener('mouseover', itemEightFunc);
+
+function itemEightFunc(item) {
+    this.style.border = '1px solid red';
+}
+
+
+// 9
+var itemNine = document.querySelectorAll('h2')
+
+for (var i = 0; i < itemNine.length; i++) {
+    itemNine[i].addEventListener('mouseover', itemNineFunc);
+}
+
+function itemNineFunc(item) {
+    this.style.border = '1px solid red';
+    this.style.fontWeight = 'bold';
+}
+
+// 10
+var itemTen = document.querySelectorAll('p')
+
+for (var i = 0; i < itemTen.length; i++) {
+    itemTen[i].addEventListener('mouseover', itemTenFunc);
+}
+
+function itemTenFunc(item) {
+    this.style.border = '1px solid red';
+    this.style.fontWeight = 'bold';
 }
