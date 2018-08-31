3efa6320552c8916ec6be7ef92dc32cb90b450b2
Levi Appenfelder <levi.appenfelder@gmail.com>
2018-08-30 21:21:49 -0600

    Draft

--- js/index.js
+++ js/index.js
@@ -11,10 +11,9 @@ mainH4.forEach(function(button) {
  const midImage = document.querySelectorAll(`div img`)
 
  midImage.forEach(function(button) {
-    button.addEventListener('mouseover',function() {event.target.style.width = '80%'})
-    button.addEventListener('mouseout',function() {event.target.style.width = '100%'})})
+    button.addEventListener('mouseenter',function() {event.target.style.width = '80%'})})
 
-    const paragraph = document.querySelector(`.intro p`)
+    const paragraphs = document.querySelectorAll(`p`)
     
     let count = 1.6;
 
@@ -22,9 +21,15 @@ mainH4.forEach(function(button) {
         count = count + 0.5;
         return count; 
     } ;
-        paragraph.addEventListener('click',function() {
-            event.target.style.fontSize = `${size()}rem`})
-            
+        paragraphs.forEach(function(button) {button.addEventListener('click',function() {
+            if (count >5.1) { count = 1.1;}
+            event.target.style.fontSize = `${size()}rem`})})
+
+        paragraphs.forEach(function(button) {button.addEventListener('dblclick',function() {
+            event.target.style.fontSize = `1.6rem`;
+            })})
+
+    
 
 
 
