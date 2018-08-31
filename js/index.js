7d6c4e2c3cc2e73545b79d9fb4c08a5735a75f14
Eric Whitcomb <eric.whitcomb@gmail.com>
2018-08-30 22:20:44 -0600

    Working on Task 3: Created 10 unique event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,65 @@
 // Your code goes here
+
+// view the console to see events
+
+// mouse over the logo
+let logo = document.querySelector(".logo-heading");
+logo.addEventListener("mouseover", function() {
+    console.log("Mouse over logo");
+});
+
+// type on the keyboard
+window.addEventListener("keydown", function(e) {
+    console.log(`${e.key} key was pressed`);
+});
+
+// rotate the wheel on the mouse
+window.addEventListener("wheel", function() {
+    console.log("You are turning the wheel on the mouse");
+});
+
+// when dragging and dropping the copyright
+let copyright = document.querySelector("footer p");
+copyright.addEventListener("dragstart", function(e) {
+    console.log("You started dragging the copyright");
+});
+copyright.addEventListener("dragend", function(e) {
+    console.log("You dropped the copyright");
+});
+
+// happens once page has loaded
+window.addEventListener("load", function() {
+    console.log("Page has been loaded");
+});
+
+// when links get focus
+let links = document.querySelectorAll(".nav-link");
+links.forEach(function(link) {
+    link.addEventListener("focus", function() {
+        console.log("Link received focus");
+    });
+});
+
+// when the window is resized
+window.addEventListener("resize", function() {
+    console.log("Window has been resized");
+})
+
+// when the window is scrolled
+window.addEventListener("scroll", function(e) {
+    console.log("The window has been scrolled");
+});
+
+// when copying any text
+document.addEventListener("copy", function(e) {
+    console.log("Text has been copied");
+    e.preventDefault();
+});
+
+// double click fun bus image
+let img = document.querySelector(".intro img");
+img.addEventListener("dblclick", function() {
+    console.log("Fun Bus Image double clicked");
+});
+
+
