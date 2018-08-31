4ce63e6ed2195bc9c010ec55a025c3634bf00bff
Courtney Buratto <you@example.com>
2018-08-30 22:33:25 -0600

    added click event listener, and one event handler

--- js/index.js
+++ js/index.js
@@ -9,17 +9,37 @@
 	// * [ ] `scroll`
 	// * [ ] `select`
     // * [x] `dblclick`
+    // * [x] 'click'    
+//reference
+const head = document.querySelector('.logo-heading');
+const allATags = document.querySelectorAll('.nav-link');
+const para = document.querySelectorAll('p');
+const allH2Tags = document.querySelectorAll('h2');
+
     
-    //selectors
-    const doubleFun = document.querySelector('.logo-heading');
-    const wheelie = document.querySelectorAll('h2');
+//handlers
+const aClickHandler = function(event) {
+    this.style.color = "aqua";
+}
+
     
+//addEventListener
+head.addEventListener('dblclick', (event) => {
+    head.style.color = 'purple';
+});//1 Fun Bus logo turns purple when double clicked
+
+allATags.forEach( nav => {
+    nav.addEventListener('click', aClickHandler)
+}); //2 a tags turn aqua when clicked once
+
+
+
+
+
+
     
-    
-    //addEventListeners
-    doubleFun.addEventListener('dblclick', (event) => {
-        this.style.color = 'purple';
-    });//1
+
+
 
     
     
