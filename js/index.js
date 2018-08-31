357743d8ac2fb7009ff3decd0ed02d5f0a8f53a8
Mitchell Robles <mitchellr92@live.com>
2018-08-30 19:17:01 -0700

    created some simple event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+const img = document.querySelector('.intro img');
+img.addEventListener('dblclick', function () {
+    img.style.hieght = '50px';
+    img.style.width = '25px';
+});
+
+const footer = document.querySelector('footer');
+footer.addEventListener('mouseover', function () {
+    footer.style.backgroundColor = 'Hotpink';
+});
+
+const img2 = document.querySelector('.img-content img');
+img2.addEventListener('click', function () {
+    img2.style.height = '300px';
+    img2.style.width = '25px';
+});
+
+const intro = document.querySelector('body');
+intro.addEventListener('keyup', function () {
+    intro.style.backgroundColor = 'Limegreen';
+});
+
+const adventureAwaits = document.querySelector('.inverse-content .text-content');
+adventureAwaits.addEventListener('wheel', function () {
+    adventureAwaits.style.display = 'flex';
+});
+
+
