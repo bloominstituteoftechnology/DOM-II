37517d0d46f1e312fad71e30b7ac9e199b411fb6
Joe Mercado <jryel1970@gmail.com>
2018-08-30 23:53:34 -0400

    first commit. still need to finish assignment.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 // Your code goes here
+let topImg = document.getElementsByTagName('img')[0];
+
+topImg.addEventListener('mouseenter', function(event){
+	event.target.style.height = '350px';
+  event.target.style.width = '500px';
+});
+
+document.addEventListener('keydown', function(event){
+  const keyName = event.key;
+  alert('keydown event\n\n' + 'key: ' + keyName);
+});
+
+let middleImg = document.getElementsByTagName('img')[1];
+
+middleImg.addEventListener("dblclick", function(e) {
+	e.target.style.height = '350px';
+  e.target.style.width = '500px';
+});
+
+window.addEventListener("resize", resizeFunction);
+
+var x = 0;
+function resizeFunction() {
+    var txt = x += 1;
+    document.getElementsByTagName('p')[0].innerHTML = 'The window is being resized ' + txt + ' times.';
+}
+
+window.addEventListener("load", loadFunction);
+
+function loadFunction() {
+    document.getElementsByTagName("h2")[0].innerHTML = "Navbar is loaded";
+}
+
+document.getElementsByTagName("p")[8].addEventListener("wheel", myFunction);
+
+function myFunction() {
+    this.style.fontSize = "35px";
+}
