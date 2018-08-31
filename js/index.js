3dc5b21250eae8c9d85dd79acfb3552c054de263
Tommy Collison <tommy@collison.ie>
2018-08-30 17:48:31 -0700

    ok, figured out how to do it properly

--- js/index.js
+++ js/index.js
@@ -4,3 +4,18 @@
 document.getElementsByClassName("logo-heading")[0].onmouseover = function() { 
     this.style.color = "red";
 }; 
+
+document.querySelectorAll('.container h2').onmouseover = function() { 
+    this.style.color = "red";
+};
+
+// starts here
+const h2Aqua = document.querySelectorAll('.container h2');
+const h2AquaFunction = function() {
+    this.style.color = "blue";
+}
+
+h2Aqua.forEach(function(element) {
+    element.addEventListener('dblclick', h2AquaFunction);
+})
+// ends here
