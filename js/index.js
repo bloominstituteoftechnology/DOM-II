2658fd662ade9d409def639100270247bd2b59d1
Glenn-David Daniel <d2rd@outlook.com>
2018-08-30 22:10:40 -0700

    Organizing elements to refactor.  Added id's to elements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+/*
+img sources
+  <img id="funbus" src="img/fun-bus.jpg" alt="bus in the sand">
+  <img id="adventure" src="img/adventure.jpg" alt="Let's go on an adventure!"> 
+  <img id="fun" src="img/fun.jpg" class="img-fluid rounded" alt="Lets have fun!">
+// <img src="img/destination.jpg" alt="Second slide">
+
+headers
+<h2>Welcome To Fun Bus!</h2>  | 
+
+<h2 id="LetsGo" >Let's Go!</h2> | https://earth.google.com/web/
+
+<h2>Adventure Awaits</h2>  | 
+
+<h2>Pick Your Destination</h2> | https://www.kayak.com/news/the-10-hottest-destinations-of-2018/
+
+<h4 id="FunInSun">Fun In The Sun</h4>  |  https://www.kayak.com/news/summers-cheapest-travel-weekends/
+
+<h4>Mountain Excursion</h4>  | 
+
+<h4>Island Getaway</h4>   | 
+
+ EVENTS
+  select
+
+  3 options
+  Learn more (wikipedia)
+  Watch Videos
+  I want to go (priceline, kayak)
+
+  */
