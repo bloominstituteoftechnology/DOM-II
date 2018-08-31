837630909e8f6f989440b6cc463f110715bd300d
Marguel Gutierrez <marguel.gtz@gmail.com>
2018-08-30 19:47:22 -0500

    Animated navbar with mouseover method

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+let logo = document.querySelector(".logo-heading");
+
+let navItem = document.querySelectorAll("nav a");
+
+logo.addEventListener("mouseover", () => {
+  logo.style.fontSize = "50px";
+
+  logo.addEventListener("mouseleave", () => {
+    logo.style.fontSize = "40px";
+  });
+});
+
+for (let i = 0; i < navItem.length; i++) {
+  navItem[i].addEventListener("mouseover", () => {
+    navItem[i].style.fontSize = "24px";
+
+    navItem[i].addEventListener("mouseleave", () => {
+      navItem[i].style.fontSize = "16px";
+    });
+  });
+}
