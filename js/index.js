cd08d8b1ff12c3858fce46057436a99f06c44f22
Denise Escobar <descobar1490@gmail.com>
2018-08-30 22:21:10 -0500

    Added two event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+let changeColor = (event) => {
+    event.target.style.color = "red";
+}
+
+let changeSize = (event) => {
+    event.target.style.fontSize = "4rem";
+}
+let logoTxt = document.getElementsByClassName("logo-heading");
+logoTxt[0].addEventListener("mouseenter", changeColor );
+
+
+let links = document.getElementsByClassName("nav-link");
+for(i=0; i<links.length; i++){
+    links[i].addEventListener("mousemove", changeSize);
+}
