4904d407c151183ed397105e92ac4f540c2ff6c8
Courtney Buratto <you@example.com>
2018-08-30 20:22:14 -0600

    added dblclick event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+    // * [ ] `mouseover`
+	// * [ ] `keydown`
+	// * [ ] `wheel`
+	// * [ ] `drag / drop`
+	// * [ ] `load`
+	// * [ ] `focus`
+	// * [ ] `resize`
+	// * [ ] `scroll`
+	// * [ ] `select`
+    // * [x] `dblclick`
+    
+    //selectors
+    const doubleFun = document.querySelector('.logo-heading');
+    const wheelie = document.querySelectorAll('h2');
+    
+    
+    
+    //addEventListeners
+    doubleFun.addEventListener('dblclick', (event) => {
+        this.style.color = 'purple';
+    });//1
+
+    
+    
