9e1c9ce4b085961139e953c85e2343f2be6f0fbc
Cesar Aponte <cesar.aponte.1996.vazquez@gmail.com>
2018-08-31 00:00:47 -0400

    Added all events except focus and select. Have added text input fields for the two events and will continue at a later time

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+const funBusLogo = document.querySelector('.logo-heading');
+const navLinks = document.querySelectorAll('nav a');
+var links = []
+for(i=0;i<navLinks.length;i++){
+    links.push(navLinks[i]);
+}
+
+funBusLogo.addEventListener('mouseover', function(){funBusLogo.style.color= 'hotpink'});
+links[0].addEventListener('wheel', function(){links[0].style.color= 'aqua'});
+links[1].addEventListener('keydown', function(){links[1].style.color= "red"});
+links[2].addEventListener('drag', function(){links[2].style.color= "purple"});
+window.addEventListener('load', function(event){links[3].style.color= "green"});
+
+const busIMG = document.querySelector('img');
+
+window.addEventListener('resize', function(){busIMG.style.display= 'none'});
+window.addEventListener('scroll', function(){busIMG.style.display= 'initial'});
+
+const firstPara = document.querySelector('.rounded');
+
+firstPara.addEventListener('dblclick', function(event){firstPara.style.width= "250px"});
+firstPara.addEventListener('dblclick', function(event){firstPara.style.height= "200px"});
+firstPara.addEventListener('select', function(){fisrtPara.style.display= 'none'});
