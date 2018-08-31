d90eea026931a8562879fcaa4279590a4eadd5ee
Paul DeYonghe <p.deyonghe@gmail.com>
2018-08-30 21:51:27 -0500

    second commit - 7 of 10, a solid C

--- js/index.js
+++ js/index.js
@@ -4,3 +4,8 @@ document.querySelector("nav a").addEventListener("mouseover", function(){ alert(
 document.querySelector("header img").addEventListener("dblclick", function(){ alert("This is a bus!")});
 document.querySelector("header img").addEventListener("mouseout", function(){document.querySelector("h1").style.color = "blue"; });
 document.querySelector(".img-content img").addEventListener("wheel", function(){document.querySelector("header img").style.display = "none"});
+document.querySelector(".text-content h2").addEventListener("mouseup", function(){document.querySelector(".text-content").style.color = "rebeccapurple";});
+document.addEventListener("scroll", function(){ if (document.querySelector("h1").style.color === "blue"){
+                                                    document.querySelector("h1").style.color = "red";
+                                                  }
+                                                });
