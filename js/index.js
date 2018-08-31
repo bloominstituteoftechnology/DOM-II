749fc24e9e42432cf833e6ffcffd43d0d0e5c026
Alex Ostrander <alexanderostrander23@gmail.com>
2018-08-30 21:51:03 -0400

    Added color to text with mouseover and mousemove

--- js/index.js
+++ js/index.js
@@ -1,5 +1,33 @@
-const aTag = document.querySelectorAll(".nav-link")
+const aTag = document.querySelectorAll(".nav-link");
 
 aTag.forEach(tag => {
     tag.addEventListener('click', function(event) { event.preventDefault() });
+});
+
+const para = document.querySelectorAll('p');
+const textColorChange = function(event) {
+    event.target.style.color = 'green';
+}
+
+para.forEach(text => {
+    text.addEventListener('mouseover', textColorChange)
+});
+
+const headerOne = document.querySelectorAll('h1');
+const headerColorChange = function(event) {
+    event.target.style.color = 'dodgerblue';
+}
+
+headerOne.forEach(head => {
+    head.addEventListener('mousemove', headerColorChange);
+});
+
+const headerTwo = document.querySelectorAll('h2');
+headerTwo.forEach(head => {
+    head.addEventListener('mousemove', headerColorChange);
+});
+
+const headerFour = document.querySelectorAll('h4');
+headerFour.forEach(head => {
+    head.addEventListener('mousemove', headerColorChange);
 });
