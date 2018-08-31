5db980b8dc53ecf0be7571679342c0d09ef3e6f4
Ken Barlow <kenbrlow@gmail.com>
2018-08-31 06:27:16 -0700

    Finished adding different event listener types and some different actions

--- js/index.js
+++ js/index.js
@@ -1,9 +1,4 @@
 
-// todo - adjust each function to perform an action that 
-// benefits the current style of the website.
-
-// todo - remove the red colors from the functions
-
 // 1
 var itemOne = document.querySelectorAll('header .container h1')[0].addEventListener('mouseover', itemOneFunc);
 
@@ -12,14 +7,14 @@ function itemOneFunc(item) {
 }
 
 // 2
-var itemTwo = document.querySelectorAll('header .container nav')[0].addEventListener('mouseover', itemTwoFunc);
+var itemTwo = document.querySelectorAll('header .container nav')[0].addEventListener('keydown', itemTwoFunc);
 
 function itemTwoFunc(item) {
-    this.style.backgroundColor = 'red';
+    this.style.display = 'none';
 }
 
 // 3
-var itemThree = document.querySelectorAll('.container header img')[0].addEventListener('mouseover', itemThreeFunc);
+var itemThree = document.querySelectorAll('.container header img')[0].addEventListener('wheel', itemThreeFunc);
 
 function itemThreeFunc(item) {
     this.style.border = '1px solid red';
@@ -29,11 +24,11 @@ function itemThreeFunc(item) {
 var itemFour = document.querySelectorAll('.container .content-section .text-content h2')
 
 for (var i = 0;  i < itemFour.length; i++) {
-    itemFour[i].addEventListener('mouseover', itemFourFunc);
+    itemFour[i].addEventListener('drag', itemFourFunc);
 }
 
 function itemFourFunc(item) {
-    this.style.fontWeight = 'bold';
+    this.style.display = 'none';
 }
 
 
@@ -41,7 +36,7 @@ function itemFourFunc(item) {
 var itemFive = document.querySelectorAll('.destination h4')
 
 for (var i = 0; i < itemFive.length; i++) {
-    itemFive[i].addEventListener('mouseover', itemFiveFunc);
+    itemFive[i].addEventListener('load', itemFiveFunc);
 }
 
 function itemFiveFunc(item) {
@@ -49,7 +44,7 @@ function itemFiveFunc(item) {
 }
 
 // 6
-var itemSix = document.querySelectorAll('footer')[0].addEventListener('mouseover', itemSixFunc);
+var itemSix = document.querySelectorAll('footer')[0].addEventListener('focus', itemSixFunc);
 
 function itemSixFunc(item) {
     this.style.border = '1px solid red';
@@ -59,15 +54,15 @@ function itemSixFunc(item) {
 var itemSeven = document.querySelectorAll('.destination .btn')
 
 for (var i = 0; i < itemSeven.length; i++) {
-    itemSeven[i].addEventListener('mouseover', itemSevenFunc);
+    itemSeven[i].addEventListener('dblclick', itemSevenFunc);
 }
 
 function itemSevenFunc(item) {
-    this.style.border = '1px solid red';
+    this.style.display = 'none';
 }
 
 // 8
-var itemEight = document.querySelectorAll('.content-destination')[0].addEventListener('mouseover', itemEightFunc);
+var itemEight = document.querySelectorAll('.content-destination')[0].addEventListener('select', itemEightFunc);
 
 function itemEightFunc(item) {
     this.style.border = '1px solid red';
@@ -78,7 +73,7 @@ function itemEightFunc(item) {
 var itemNine = document.querySelectorAll('h2')
 
 for (var i = 0; i < itemNine.length; i++) {
-    itemNine[i].addEventListener('mouseover', itemNineFunc);
+    itemNine[i].addEventListener('scroll', itemNineFunc);
 }
 
 function itemNineFunc(item) {
@@ -90,10 +85,9 @@ function itemNineFunc(item) {
 var itemTen = document.querySelectorAll('p')
 
 for (var i = 0; i < itemTen.length; i++) {
-    itemTen[i].addEventListener('mouseover', itemTenFunc);
+    itemTen[i].addEventListener('click', itemTenFunc);
 }
 
 function itemTenFunc(item) {
-    this.style.border = '1px solid red';
-    this.style.fontWeight = 'bold';
+    this.style.display = 'none';
 }
