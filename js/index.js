807d683ccf5014c8c5159865d09768f85837f24c
FreeKeyBoy <jjhutton81@gmail.com>
2018-09-01 12:32:07 -0700

    Added a dabbing cactus double click event. Added a keypress event that inverts the colors on the HTML body. Added effect that enlarges nav links and makes them orange on mouse in, then reverts to normal on mouse out.

--- js/index.js
+++ js/index.js
@@ -2,18 +2,24 @@ const logo = document.querySelector('.logo-heading');
 const rickRoll = document.querySelector(".rickRoll");
 const buttonList = document.querySelectorAll('.btn');
 const adventurePic = document.querySelector(".img-content img");
+const navLinks = document.querySelectorAll("a");
+const bodCont = document.querySelector("body");
+const funImg = document.querySelector(".funImg");
 
 
-//new images for rickRoll
+//new images for effects
 const rick = document.createElement('img');
 rick.src = "img/rick.jpg"
 const roll = document.createElement('img');
-roll.scr = "img/roll.jpg"
+roll.src = "img/roll.jpg"
+const dab = document.createElement('img');
+dab.src = "img/dabbingCactus.jpg"
 
 //img event
+//rick roll on fun-bus img
 rickRoll.addEventListener("mouseover", mouseOverImage);
 
-function mouseOverImage () {
+function mouseOverImage() {
     if (rickRoll.src.includes("img/fun-bus.jpg")) {
         rickRoll.src = "img/rick.jpg"
     } else if (rickRoll.src.includes("img/rick.jpg")) {
@@ -22,14 +28,25 @@ function mouseOverImage () {
         rickRoll.src = "img/fun-bus.jpg"
     }
 };
-//needs debugging. Ask for advice on stand up
 
+//dabbing cactus img over fun img
+
+funImg.addEventListener("dblclick", dblClickImg);
+
+function dblClickImg() {
+    if (funImg.src.includes("img/fun.jpg")) {
+        funImg.src = "img/dabbingCactus.jpg"
+    } else if (funImg.src.includes("img/dabbingCactus.jpg")) {
+        funImg.src = "img/fun.jpg"
+    }
+};
+//end img events
 //button event
 buttonList.forEach(function(button) {
     button.addEventListener('click', goAway);
 });
 
-function goAway (event){
+function goAway(event){
     event.target.style.display = "none";
 };
 
@@ -37,11 +54,23 @@ function goAway (event){
 
 // needs debugging
 
-//nav 
+//logo effects
 logo.addEventListener("mouseover", function() {
     logo.style.border = '2px dashed black';
     logo.style.backgroundColor = 'gold';
     logo.style.padding = '10px';
+});
+
+//nav bar and links effects
+navLinks.forEach(function(element){
+    element.addEventListener('mouseover', function(event){
+        event.target.style.color = "orange";
+        event.target.style.fontSize = "3.3rem";
+    });
+    element.addEventListener('mouseout', function(event){
+        event.target.style.color = "";
+        event.target.style.fontSize = "";
+    })
 });
 
 //adventure pick effect
@@ -53,3 +82,18 @@ function noFade() {
 };
 adventurePic.addEventListener("mouseover", fade);
 adventurePic.addEventListener("mouseout", noFade);
+
+//body color change effect
+//text change effect
+function blackWhite() {
+    bodCont.style.backgroundColor = "black";
+    bodCont.style.color = "white";
+};
+function bodyDefault() {
+    bodCont.style.backgroundColor = "";
+    bodCont.style.color = "";
+};
+
+bodCont.addEventListener("keydown", blackWhite);
+bodCont.addEventListener("keyup", bodyDefault);
+
