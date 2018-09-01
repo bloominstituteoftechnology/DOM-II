fbd16aa847df811d9ad8b5e5ef7eda5a47996895
ShardeDeLane <rhcprocker@yahoo.com>
2018-08-31 23:12:46 -0400

    update

--- js/index.js
+++ js/index.js
@@ -1,10 +1,23 @@
 // Your code goes here
 alert('Welcome To The Fun Bus');
 
+// navigation title
 let title = document.getElementsByClassName('logo-heading');
 for (let i = 0; i < title.length; i++){
   title[i].addEventListener('mouseenter', changeTitle);
   title[i].addEventListener('mouseleave', originalTitle);
+}
+
+function changeTitle(){
+  title[0].innerHTML = 'Thor Ragnarok';
+  // navigation header background color changed
+  headerBackground[0].style.backgroundColor = 'rgb(31, 168, 23)';
+}
+
+function originalTitle(){
+  if(title[0].innerHTML = 'Thor'){
+      title[0].innerHTML = 'Fun Bus';
+  }
 }
 
 // navigation links
@@ -14,18 +27,51 @@ for(let i = 0; i < links.length; i++){
   links[i].addEventListener('mouseleave', originalNav);
 }
 
+function changeLinks(){
+  links[0].innerHTML = 'Thor';
+  links[1].innerHTML = 'Valhalla';
+  links[2].innerHTML = 'About';
+  links[3].innerHTML = 'Avengers';
+  title[0].innerHTML = 'Thor Ragnarok';
+  // navigation width changed
+  navigation[0].style.width = ('500px');
+}
+
+function originalNav(){
+  links[0].innerHTML = 'Home';
+  links[1].innerHTML = 'About Us';
+  links[2].innerHTML = 'Blog';
+  links[3].innerHTML = 'Contact';
+  //navigation changed back to original width
+  navigation[0].style.width = ('350px');
+  title[0].innerHTML = 'Fun Bus';
+}
+
+
+// navigation bar
 let navigation = document.getElementsByClassName('nav');
+// navigation header
 let headerBackground = document.getElementsByClassName('main-navigation');
 
 // fun bus banner
 let banner = document.querySelector('.container .intro img');
 banner.addEventListener('mouseover', changeBanner);
 
-// main h2 and paragraph
+function changeBanner(){
+  banner.src = '../img/thor-banner.jpg';
+}
+
+
+// main title and paragraph
 let mainTitle = document.querySelector('.home .intro h2');
 mainTitle.addEventListener('mouseover', newTitle);
 let mainText = document.querySelector('.home .intro p');
 mainText.addEventListener('mouseover', newTitle);
+
+function newTitle(){
+  mainTitle.innerHTML = 'Odin\'s tale';
+  mainText.innerHTML = 'Odin Allfather did not act without thought. Now as the sun shone over asgard and the buildings were illuminated by it’s rays, gleaming like gold, he thought long and hard. At the realms edge, the darkness of the cosmos spread out like a calm sea. Asgard was at peace, and all was ready for the momentous events to come. Standing in his chambers, Odin stared out at the realm he had ruled for so many years.';
+}
 
 // section images
 let imageContent = document.querySelectorAll('.content-section .img-content img');
@@ -40,7 +86,7 @@ function changeImage(){
   imageContent[1].src='../img/thor-green-bg.jpg';
 }
 
-// section header
+// section headers loop and events
 let textHeaders = document.querySelectorAll('.text-content h2');
 for (let i = 0; i < textHeaders.length; i++){
   textHeaders[i].addEventListener('mouseenter', mainHeaders);
@@ -51,7 +97,7 @@ function mainHeaders(){
   textHeaders[1].innerHTML = 'Avengers';
 }
 
-// section text
+// section text loop and events
 let sectionText = document.querySelectorAll('.text-content p');
 for(let i = 0; i < sectionText.length; i++){
   sectionText[i].addEventListener('mouseenter', textContent)
@@ -65,71 +111,40 @@ function textContent(){
 }
 
 
-function changeTitle(){
-  title[0].innerHTML = 'Thor Ragnarok';
-  headerBackground[0].style.backgroundColor = 'rgb(31, 168, 23)';
-}
-
-function originalTitle(){
-  if(title[0].innerHTML = 'Thor'){
-      title[0].innerHTML = 'Fun Bus';
-  }
-}
-
-function changeLinks(){
-  links[0].innerHTML = 'Thor Home';
-  links[1].innerHTML = 'Valhalla';
-  links[2].innerHTML = 'About Thor';
-  links[3].innerHTML = 'Contact Avengers';
-  title[0].innerHTML = 'Thor Ragnarok';
-  navigation[0].style.width = ('500px');
-}
-
-function originalNav(){
-  links[0].innerHTML = 'Home';
-  links[1].innerHTML = 'About Us';
-  links[2].innerHTML = 'Blog';
-  links[3].innerHTML = 'Contact';
-  navigation[0].style.width = ('350px');
-  title[0].innerHTML = 'Fun Bus';
-}
-
-function changeBanner(){
-  banner.src = '../img/thor-banner.jpg';
-}
-
-function newTitle(){
-  mainTitle.innerHTML = 'Odin\'s tale';
-  mainText.innerHTML = 'Odin Allfather did not act without thought. Now as the sun shone over asgard and the buildings were illuminated by it’s rays, gleaming like gold, he thought long and hard. At the realms edge, the darkness of the cosmos spread out like a calm sea. Asgard was at peace, and all was ready for the momentous events to come. Standing in his chambers, Odin stared out at the realm he had ruled for so many years.';
-}
-
-
-// function banner(){}
-// function bannerOriginal(){}
-// title[0].innerHTML = 'Thor Ragnarok';
-// let links = document.getElementsByClassName('nav-link');
-// console.log(links);
-// links[0].innerHTML = 'Thor Home';
-// links[1].innerHTML = 'Valhalla';
-// links[2].innerHTML = 'About Thor';
-// links[3].innerHTML = 'Contact Avengers';
-
-// let navigation = document.getElementsByClassName('nav');
-// navigation[0].style.width = ('500px');
+// function changeTitle(){
+//   title[0].innerHTML = 'Thor Ragnarok';
+//   headerBackground[0].style.backgroundColor = 'rgb(31, 168, 23)';
+// }
 //
-// let banner = document.querySelector('.intro img');
-// banner.src = '../img/thor-banner.jpg';
+// function originalTitle(){
+//   if(title[0].innerHTML = 'Thor'){
+//       title[0].innerHTML = 'Fun Bus';
+//   }
+// }
 //
-// let mainTitle = document.querySelector('.home .intro h2');
-// console.log(mainTitle);
-// mainTitle.innerHTML = 'Thor Ragnarok';
-// let mainText = document.querySelector('.home .intro p');
-// mainText.innerHTML = 'At last the great hall was filled with asgardians waiting for a glimpse of the new king. Attendants gave out golden glasses filled with the finest wine in the realm for the amazingly dressed guest’s when the horn sounded the start of the coronation Odin on the great throne that he would soon pass down to his oldest son Thor and Queen frigga on the balcony above and that’s when it happened Thor was walking down the isle and finally it was time. Without doubt he was a great warrior but a warror king, that was still a lesson to learn.';
+// function changeLinks(){
+//   links[0].innerHTML = 'Thor Home';
+//   links[1].innerHTML = 'Valhalla';
+//   links[2].innerHTML = 'About Thor';
+//   links[3].innerHTML = 'Contact Avengers';
+//   title[0].innerHTML = 'Thor Ragnarok';
+//   navigation[0].style.width = ('500px');
+// }
 //
-// let middleHeader= document.querySelector('.text-content h2');
-// console.log(middleHeader);
-// middleHeader.innerHTML = 'Odin';
-// let middleText = document.querySelectorAll('.text-content p');
-// console.log(middleText);
-// middleText[0].innerHTML = 'Despite the beauty before him, his mind was troubled and his expression drawn with worry and tension. As Allfather, Odin had battled great beasts , invaded foreign realms , destroyed strong enemies and kept the realm of asgard safe and peaceful. He had lost his brothers and father to war. For thousands of thousands of years he had carried the burden of his crown alone. It had worried him at times, energized him at others.';
-// middleText[1].innerHTML = 'When he had married his wife Frigga the burden had lifted as she was a strong partner and had a helpful ear.';
+// function originalNav(){
+//   links[0].innerHTML = 'Home';
+//   links[1].innerHTML = 'About Us';
+//   links[2].innerHTML = 'Blog';
+//   links[3].innerHTML = 'Contact';
+//   navigation[0].style.width = ('350px');
+//   title[0].innerHTML = 'Fun Bus';
+// }
+//
+// function changeBanner(){
+//   banner.src = '../img/thor-banner.jpg';
+// }
+
+// function newTitle(){
+//   mainTitle.innerHTML = 'Odin\'s tale';
+//   mainText.innerHTML = 'Odin Allfather did not act without thought. Now as the sun shone over asgard and the buildings were illuminated by it’s rays, gleaming like gold, he thought long and hard. At the realms edge, the darkness of the cosmos spread out like a calm sea. Asgard was at peace, and all was ready for the momentous events to come. Standing in his chambers, Odin stared out at the realm he had ruled for so many years.';
+// }
