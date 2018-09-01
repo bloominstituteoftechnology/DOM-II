140c849c7bd0b621a087b834df666445aacba8de
paulapivat <paul.apivat@gmail.com>
2018-09-02 00:03:08 +0700

    add instructions to index.js

---
+++ stretch-assignment/index.js
@@ -0,0 +1,32 @@
+// When a block is clicked, it should go to the top of the stack.
+
+
+
+
+
+// Animate the rockets, visually show them being transported to the top.
+
+
+
+
+
+
+// While a mouse is clicked down on a box, it should move to the right 
+// (It should coninuously move, there is no limit on how far it will go).
+// mousedown 
+
+
+
+
+
+
+
+
+
+// When the mouse button is released, it should travel back to its original position.
+// mouseup
+
+
+
+
+// A mouse down that occurs during a transition back to the original position should send it traveling to the right again from it's current position.
