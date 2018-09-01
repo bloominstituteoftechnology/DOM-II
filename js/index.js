b1f61fe8670764fdb680ae163f95c1d855fc1c63
Marguel Gutierrez <marguel.gtz@gmail.com>
2018-09-01 11:50:32 -0500

    Font size of the elements on the nav bar change increase when mouseover

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 // Your code goes here
 let logo = document.querySelector(".logo-heading");
 
-let navItem = document.querySelectorAll("nav a");
-
+// 1. Font size of the elements on the nav bar change increase when mouseover
 logo.addEventListener("mouseover", () => {
   logo.style.fontSize = "50px";
 
@@ -10,6 +9,8 @@ logo.addEventListener("mouseover", () => {
     logo.style.fontSize = "40px";
   });
 });
+
+let navItem = document.querySelectorAll("nav a");
 
 for (let i = 0; i < navItem.length; i++) {
   navItem[i].addEventListener("mouseover", () => {
@@ -20,3 +21,88 @@ for (let i = 0; i < navItem.length; i++) {
     });
   });
 }
+
+// 2. Bakground Change when pressing a key down
+let body = document.querySelector("body");
+
+body.addEventListener("keydown", () => {
+  body.style.background = "gray";
+});
+
+body.addEventListener("keyup", () => {
+  body.style.background = "white";
+});
+
+// 3. Paragraphs Background color
+
+let paragraph = document.querySelectorAll("p");
+
+paragraph.forEach(item => {
+  item.addEventListener("mousemove", function() {
+    this.parentNode.style.backgroundColor = "gray";
+    this.parentNode.style.borderRadius = "10px";
+  });
+});
+
+paragraph.forEach(item => {
+  item.addEventListener("mouseleave", function() {
+    this.parentNode.style.backgroundColor = "";
+  });
+});
+
+// 4. Clicking clicking on pictures makes them dissapear
+
+let imgs = document.querySelectorAll("img");
+
+imgs.forEach(item => {
+  item.addEventListener("click", function() {
+    this.parentNode.style.display = "none";
+  });
+});
+
+// 5. clicking on paragraphs makes its width 100%
+
+paragraph.forEach(item => {
+  item.addEventListener("click", function() {
+    this.parentNode.style.width = "100%";
+  });
+});
+
+// 6. Double click on a button changed its color
+
+let button = document.querySelectorAll(".btn");
+
+button.forEach(item => {
+  item.addEventListener("dblclick", function() {
+    this.style.backgroundColor = "red";
+  });
+});
+
+// 7. draggin imgs alert
+
+imgs.forEach(item => {
+  item.addEventListener("drag", function(event) {
+    alert("No dragging");
+  });
+});
+
+// 8. Logo turns Grennwhen double clicked
+
+logo.addEventListener("dblclick", function() {
+  logo.style.color = "green";
+});
+
+// 9. Nav Items turn red when clicked
+
+navItem.forEach(item => {
+  item.addEventListener("click", function() {
+    this.style.color = "red";
+  });
+});
+
+// 10. footer background color changes when clicked
+
+let footer = document.querySelector("footer");
+footer.addEventListener("click", function() {
+  footer.style.backgroundColor = "blue";
+});
