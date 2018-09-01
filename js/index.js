dff7ad563bedc61bb56c124211ef10222cc32627
hraya <hraya@lssu.edu>
2018-09-01 02:11:41 -0500

    Adding EventListeners
    added keyup and keydown events and begain a mouseenter event

--- js/index.js
+++ js/index.js
@@ -1,8 +1,31 @@
 // Your code goes here
-let mainHeader = document.querySelector(".intro h2");
-mainHeader.addEventListener("mouseover", function(event){
+let mainHeader = document.querySelector('.intro h2');
+mainHeader.addEventListener('mouseover', function(event){
     event.target.style.color = "green"
 })
 
+let busImg = document.querySelector('header img');
+busImg.addEventListener('wheel', function(){
+    busImg.style.width ='400px';
+},{passive:true})
 
+window.addEventListener('keydown', function(event){
+    if(event.key === 'a') {
+    let mapImg = document.querySelectorAll('img')[1];
+        mapImg.style.display = 'none';
+    }
+});
 
+window.addEventListener('keyup', function(event){
+    if(event.key === 'a') {
+        let cityImg = document.querySelectorAll('img')[2];
+        cityImg.style.display = 'none';
+    }
+})
+
+let contentSec = document.querySelector('.content-section');
+contentSec.addEventListener('mouseenter',)
+console.log(contentSec);
+
+let inverseSec = document.querySelector('.inverse-content');
+console.log(inverseSec);
