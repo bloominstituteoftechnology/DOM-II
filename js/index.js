19d5cf4fe9a21b3593874deb6b18ded18303a304
Courtney Buratto <you@example.com>
2018-09-02 15:36:54 -0600

    added Mouseover and Mouseout

--- js/index.js
+++ js/index.js
@@ -1,36 +1,48 @@
 // Your code goes here
-    // * [ ] `mouseover`
-	// * [ ] `keydown`
+    // * [x] `mouseover`
+    // * [x] 'mouseout'
+    // * [ ] `keydown`
 	// * [ ] `wheel`
 	// * [ ] `drag / drop`
 	// * [ ] `load`
 	// * [ ] `focus`
 	// * [ ] `resize`
 	// * [ ] `scroll`
-	// * [ ] `select`
+    // * [ ] `select`
     // * [x] `dblclick`
     // * [x] 'click'    
+
 //reference
 const head = document.querySelector('.logo-heading');
 const allATags = document.querySelectorAll('.nav-link');
 const para = document.querySelectorAll('p');
-const allH2Tags = document.querySelectorAll('h2');
 
-    
 //handlers
-const aClickHandler = function(event) {
+const aClickHandler = function(e) {
     this.style.color = "aqua";
 }
 
-    
-//addEventListener
-head.addEventListener('dblclick', (event) => {
+const pHandler = function(e) {
+     this.style.color = "orange";
+ }
+
+ const pHandlerOut = function(e) {
+     this.style.color = "black";
+ }
+
+//add 10 Event Listeners
+head.addEventListener('dblclick', (e) => {
     head.style.color = 'purple';
 });//1 Fun Bus logo turns purple when double clicked
 
 allATags.forEach( nav => {
     nav.addEventListener('click', aClickHandler)
-}); //2 a tags turn aqua when clicked once
+});//2 a tags turn aqua when clicked once
+
+para.forEach(graph => {
+    graph.addEventListener('mouseover', pHandler)
+    graph.addEventListener('mouseout', pHandlerOut)
+});//3 all <p> tags turn orange when the mouseover, turns to black when mouseout
 
 
 
