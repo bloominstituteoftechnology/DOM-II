5d20e6312cce6fcbdd56c045c33a48995febe4e3
Colin Dismuke <colin.dismuke@gmail.com>
2018-09-01 23:00:56 -0500

    Added comments.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -2,16 +2,22 @@ class Rocket {
     constructor(rocket) {
         this.item = rocket;
 
+        // Event listeners
         this.item.addEventListener('click', this.blastOff);
         this.item.addEventListener('mousedown', this.animate);
+
+        // TODO: The 'mouseup' event doesn't seem to work very well
+        // when the element has moved all the way across the screen.
         this.item.addEventListener('mouseup', this.animateBack);
     }
 
     // blastOff method
+    // The animation probably needs to happen using an additional CSS class
+    // that includes transition and transform properties
     blastOff() {
         console.log("Inside blastoff() " + event.currentTarget)
         if (event.currentTarget === redRocket) {
-            redRocket.style.order = "1";
+            redRocket.style.order = "1"; 
             blueRocket.style.order = "0";
             greenRocket.style.order = "0";
             pinkRocket.style.order = "0";
@@ -45,66 +51,50 @@ class Rocket {
         console.log('Blast off 🚀')
     }
 
-        // for (let i = 0; i < rockets.length; i++) {
-        //     if (rockets[i] === event.target) {
-        //         event.target.style.order = "1";
-        //     } else {
-        //         rockets[i].style.order = "0";
-        //     }
-        // }
+    // animate method
+    animate() {
+        let start = Date.now(); // Start time
+        let element = event.currentTarget;
 
-        animate() {
-            let start = Date.now(); // Start time
-            let element = event.currentTarget;
+        let timer = setInterval(function() {
+            // Elapsed time from the start
+            let elapsedTime = Date.now() - start;
+        
+            if (elapsedTime >= 1000) {
+            clearInterval(timer); // Animation finishes after 5 seconds
+            return;
+            }
+        
+            // Draw the animation
+            draw(elapsedTime, element);
+        
+        }, 10);
+        
+        // As elapsedTime increases, left increases from 0px to 5000px
+        function draw(elapsedTime, e) {
+            e.style.position = "relative";
+            e.style.left = elapsedTime + 'px';
+        } 
+    }
 
-            let timer = setInterval(function() {
-              // Elapsed time from the start
-              let elapsedTime = Date.now() - start;
-            
-              if (elapsedTime >= 5000) {
-                clearInterval(timer); // Animation finishes after 5 seconds
-                return;
-              }
-            
-              // Draw the animation
-              draw(elapsedTime, element);
-            
-            }, 10);
-            
-            // As elapsedTime increases, left increases from 0px to 5000px
-            function draw(elapsedTime, e) {
-                e.style.position = "relative";
-                e.style.left = elapsedTime + 'px';
-            } 
-        }
+    // animateBack method
+    // Should animate back to original position
+    animateBack() {
+        let element = event.currentTarget;
+        let bodyRect = document.body.getBoundingClientRect(),
+            elemRect = element.getBoundingClientRect(),
+            rocketOffset = elemRect.left - bodyRect.left;
 
-        // animateBack() {
-        //     let start = Date.now(); // Start time
-        //     let element = event.currentTarget;
-        //     let bodyRect = document.body.getBoundingClientRect(),
-        //         elemRect = element.getBoundingClientRect(),
-        //         offset = elemRect.left - bodyRect.left;
-
-        //     let timer = setInterval(function() {
-        //       // Elapsed time from the start
-        //       let elapsedTime = Date.now() - start;
-            
-        //       if (elapsedTime >= 5000) {
-        //         clearInterval(timer); // Animation finishes after 5 seconds
-        //         return;
-        //       }
-            
-        //       // Draw the animation
-        //       draw(elapsedTime, element);
-            
-        //     }, 10);
-            
-        //     // As elapsedTime increases, left increases from 0px to 5000px
-        //     function draw(elapsedTime, e) {
-        //         e.style.position = "relative";
-        //         e.style.left = elapsedTime + 'px';
-        //     } 
-        // }
+        setInterval(function() {
+            // Draw the animation
+            draw(rocketOffset, element);
+        }, 10);
+        
+        function draw(offset, e) {
+            e.style.position = "relative";
+            e.style.right = offset + 'px';
+        } 
+    }
 }
 
 
