9f94dd2bebcce78793668c32faa8face03a47770
Abdiel Fernandez <fernandezabdiel@gmail.com>
2018-09-03 12:06:48 -0400

    added a coouple more events to the page

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-//shawdow over images
+//shawdow-box over every image hovered
 const images =  document.querySelectorAll('img');
 
 
@@ -17,7 +17,7 @@ images.forEach(item => {
     item.addEventListener('mouseleave', shadowUnder);
 });
 
-//pick the first letter of each 2 and it will be selected
+//pick the first letter of each h2 and it will be selected
 const h2 = document.querySelectorAll('h2');
 document.addEventListener('keydown', (event) => {
     switch (event.key) {
@@ -38,6 +38,7 @@ document.addEventListener('keydown', (event) => {
     }
 })
 
+//any h2 that you double
 h2.forEach(item => {
     item.addEventListener('dblclick', (event) => {
     if (item.style.color === '') {
@@ -47,3 +48,17 @@ h2.forEach(item => {
     }
     })
 })
+
+//todo -- I need to check why this does not work
+
+// const destination = document.querySelectorAll('.destination');
+// const destinationParent = document.querySelector('.content-pick')
+
+// const blackAndWhite = (event) => {
+//     destinationParent.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+//     destination[currentTarget].style.backgroundColor = 'white';
+// }
+
+// destination.forEach(item => {
+//     item.addEventListener('mouseover', blackAndWhite)
+// });
