81ac131f66f9c4ee236cc3225c80f4a96b4b6a29
Alex Ostrander <alexanderostrander23@gmail.com>
2018-09-04 13:06:32 -0400

    Added drag and drop event listener

--- js/index.js
+++ js/index.js
@@ -71,3 +71,48 @@ window.addEventListener('scroll', function(event) {
     button[2].style.backgroundColor = "dodgerblue"
 });
 
+const fill = document.querySelector('.fill');
+const empties = document.querySelectorAll('.empty');
+
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+
+function dragStart() {
+    console.log('start');
+    this.className += ' hold';
+    setTimeout(() => this.className = 'invisible', 0);
+}
+
+function dragEnd() {
+    console.log('end');
+    this.className = 'fill';
+}
+
+function dragOver(e) {
+    console.log('over');
+    e.preventDefault();
+}
+
+function dragEnter(e) {
+    console.log('enter');
+    e.preventDefault();
+    this.className += ' hovered';
+}
+
+function dragLeave() {
+    console.log('leave');
+    this.className = 'empty';
+}
+
+function dragDrop() {
+    console.log('drop');
+    this.className = 'empty';
+    this.append(fill);
+}
