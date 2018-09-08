d8b79434fac97b7f365d0fd577a6b4a127ddacc2
erscriber <erscriber@gmail.com>
2018-09-08 08:34:57 -0400

    3 Event Listeners added

--- README.md
+++ README.md
@@ -33,11 +33,11 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 ## Task 3: Create Unique Event Listeners
 
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
+	* [ X ] `mouseover`
 	* [ ] `keydown`
-	* [ ] `wheel`
+	* [ X ] `wheel`
 	* [ ] `drag / drop`
-	* [ ] `load`
+	* [ X ] `load`
 	* [ ] `focus`
 	* [ ] `resize`
 	* [ ] `scroll`
--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+
+// Constants
+
+const navLogo = document.querySelector('.logo-heading');
+const container = document.querySelector('.container');
+const navMain = document.querySelector('.nav-container');
+const wholePage = document.querySelector('body');
+const navLinks = document.querySelectorAll('.nav-link');
+const topNav = document.querySelector('.main-navigation');
+const images = document.querySelectorAll('img');
+const paragraphs = document.querySelectorAll('p');
+let buttons = document.querySelectorAll('.btn');
+
+
+//MouseOver
+navLogo.addEventListener("mouseover", function(){
+	navLogo.style.border = '3px dashed hotpink';
+	navLogo.style.background = 'darkgray';
+	navLogo.style.transform = 'scale(2, 1)';
+	navLogo.style.padding = '10px';
+});
+
+
+//Wheel
+window.addEventListener("wheel", function(){
+	if (documetn.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+		navLogo.style.display = 'none';
+		navMain.style.justifyContent = 'center';
+	} else {
+		navLogo.style.cssText = "";
+		navMain.style.justifyContent = 'space-between';
+	}
+});
+
+
+//Load
+window.addEventListener("load", function(){
+	this.alert("Welcome to the Fun Bus!");
+});
