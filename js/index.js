a5ce0c577eb716cb9a8fc7fcc25081bdd2a19610
TRIF3X <thewizardofcrypto@gmail.com>
2018-09-18 15:23:27 -0500

    2 events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+let animateHeading = document.querySelector('.logo-heading');
+animateHeading.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor = 'cyan';
+});
+//-------------------------------------------------------------------------
+let animateBTN = document.querySelectorAll('.btn');
+for (let i = 0; i<animateBTN.length; i++) {
+    animateBTN[i].addEventListener('click', () => {
+    event.target.style.color = 'red'
+ })
+}
+//------------------------------------------------------------------------
