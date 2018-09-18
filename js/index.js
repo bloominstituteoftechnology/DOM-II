77d271507166c436595a049c9a7b4c7d486d9112
Lee Len Formento <leebatef@gmail.com>
2018-09-18 16:27:50 -0400

    Added mousemove, keydown

--- js/index.js
+++ js/index.js
@@ -9,6 +9,8 @@ const welcome = document.querySelector(".home h2");
 const letsGo = document.querySelector(".text-content h2");
 const destinationPic = document.querySelector(".pic-destination");
 const footer = document.querySelector(".footer");
+const welcomeP = document.querySelector(".home p");
+const wholeBody = document.querySelector("body");
 
 // Event listeners
 logo.addEventListener("mouseover", () => {
@@ -17,3 +19,12 @@ logo.addEventListener("mouseover", () => {
 logo.addEventListener("mouseleave", () => {
     logo.classList.toggle("grow")
 });
+welcome.addEventListener("mousemove", (event) => {
+    event.currentTarget.style.color = "red";
+});
+
+wholeBody.addEventListener("keydown", (event) => {
+    // event.currentTarget.style.backgroundColor = 
+    // event.currentTarget.style.backgroundColor = "red";
+    wholeBody.classList.toggle("color"); 
+  });
