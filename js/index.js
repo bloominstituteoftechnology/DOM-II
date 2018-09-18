7692725fc63a7927fe26497168afec6e021bb3b1
Brandon Strellis <bstrellis@gmail.com>
2018-09-18 18:19:44 -0400

    6 events done

--- js/index.js
+++ js/index.js
@@ -19,4 +19,27 @@ blogLink.addEventListener('copy', (event) => {
     console.log('Don\'t copy me! I barely earn a living as it is!');
 });
 
-// Event 4: 
+// Event 4: load ***********
+const contactLink = document.querySelector('.contact-link');
+contactLink.addEventListener('load', (event) => {
+    // do animation here?
+    console.log('your window has loaded!');
+});
+
+// Event 5: resize
+// can only be called on window
+// shouldn't be used for DOM manipulation? so another contrived example
+window.addEventListener('resize', (event) => {
+    document.querySelector('nav').style.background = 'darkorange';
+});
+
+// Event 6: focus
+homeLink.addEventListener('focus', (event) => {
+    document.querySelector('header img').src = 'https://wallpaperbrowse.com/media/images/POTD_chick_3597497k.jpg'
+});
+
+// Event 7: blur
+homeLink.addEventListener('blur', (event) => {
+
+});
+
