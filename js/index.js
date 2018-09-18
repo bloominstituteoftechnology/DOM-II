43b5f18b08f5a5a6620d5cfe35bec03fa6333723
mm2079 <vayne3252@gmail.com>
2018-09-18 18:31:59 -0400

    M. Malik

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
-// Your code goes here
+// mouseover
+let bColor = document.querySelector( ".container" );
+bColor.addEventListener( "mouseover", function( event ){
+    event.stopPropagation();
+    event.target.style.backgroundColor = "gray";
+    
+    setTimeout( function(){
+        event.target.style.backgroundColor = "";
+    }, 200 )
+});
+
+// keydown
+let keyDown = document.querySelector( "img" );
+document.addEventListener( "keydown", function( event ){
+    if( event.key == 'f' )
+    {
+        alert( "key down event");
+    }
+});
+
+// wheel
+document.addEventListener( "wheel", function( event ){
+    alert( "wheel event" );
+})
+
+// load
+
