7148d0bf9678f44a98f70b330d188cceb2a688cd
SavannahGreuel <smallidgesavannah@gmail.com>
2018-09-18 14:24:03 -0600

    initial commit + 2 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+
+// mouseover
+let highLightLink = document.querySelector('a');
+highLightLink.addEventListener('mouseover', (event) => {
+    
+    event.target.style.color = 'yellow';
+    event.preventDefault();
+
+
+    setTimeout (() => {
+        event.target.style.color = ''; 
+    }, 500);
+}, false);
+
+
+// keydown
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    alert(' hey, you pressed key: ' + keyName + '. Please press another key!');
+  });
+
+
+// wheel
+
+// drag / drop
+// load
+// focus
+// resize
+// scroll
+// select
+// dblclick
