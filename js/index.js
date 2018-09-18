cc1849c60b53efdad163b4ae0b02cfa3a00f8738
pedro <pedroluismontesinosrivera@gmail.com>
2018-09-18 16:24:37 -0400

    added logo animation

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const logo = document.querySelector(".logo-heading");
+const busImage = document.querySelector(".home .intro img");
+
+logo.addEventListener("mouseover", () =>{
+    logo.classList.add("logo-move");
+});
+logo.addEventListener("mouseleave", () => {
+    logo.classList.remove("logo-move");
+});
+
