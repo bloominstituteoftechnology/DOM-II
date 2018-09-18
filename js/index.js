2e2fb427240782ac03dc6fc1613688210acdb46a
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-18 14:10:12 -0500

    added mouseover on main image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+// Header image mouseover
+document.querySelector('.intro img').addEventListener('mouseover', (e) => {
+    e.target.style.border = '3px solid black';
+    setTimeout(() => {
+        e.target.style.border = '';
+    }, 1500)
+});
