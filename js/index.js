8bee7698be6bba7a532eeda218dd8328282cbdce
theryanclausen <theryanclausen@gmail.com>
2018-09-18 12:21:51 -0700

    default prevented

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
 // Your code goes here
+let navLinks = document.querySelectorAll('.main-navigation .nav-container nav .nav-link');
+
+navLinks.forEach(x => x.addEventListener('click', (e) => e.preventDefault()))
