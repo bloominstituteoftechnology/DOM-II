f4f386f0ec96f69126761f16c0e0686cf821b27b
Wanda <doubleuevans@gmail.com>
2018-09-18 14:44:32 -0500

    first commit and pull

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+// `mouseover`
+const boldLink = document.querySelectorAll('.nav-link');
+boldLink.addEventListener('mouseover', (event) => {
+  event.preventDefault();  
+  event.target.style.fontFamily = 'Verdana, serif';
+
+  setTimeout(function () {
+    event.target.style.fontFamily = "";
+  }, 500);
+}, false);
+// `keydown`
+// `wheel`
+// `drag / drop`
+// `load`
+// `focus`
+// `resize`
+// `scroll`
+// `select`
+// `dblclick`
