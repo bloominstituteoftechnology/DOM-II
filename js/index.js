d50ad74471e7ab7183d1eb2ddcf47dbf7f079f96
Justin <jdlow2@gmail.com>
2018-09-18 15:51:51 -0400

    initial commit. added first event listener.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+//
+// use wheel to change home container background color
+//
+function getRandomColor() {
+  const colors = [];
+  for (let i = 0; i < 3; i++) {
+    const randomColor = Math.floor(Math.random() * 256);
+    colors.push(randomColor);
+  }
+  return `rgb(${colors[0]}, ${colors[1]}, ${colors[2]})`;
+}
+const home = document.querySelector('.home');
+home.addEventListener('wheel', (event) => {
+  console.log('hello');
+  const randomColor = getRandomColor();
+  console.log(randomColor);
+  event.currentTarget.style.backgroundColor = `${randomColor}`;
+});
