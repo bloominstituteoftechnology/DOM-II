cb0dce712c1bbe12f8031f2bedba29ddfbc6ac1c
zacharyarney <zachary.arney@gmail.com>
2018-09-18 17:51:11 -0500

    finished 8 out of 10 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
-// Your code goes here
+const mainNav = document.querySelector('.main-navigation');
+const navItems = mainNav.querySelector('.nav');
+const title = mainNav.querySelector('h1');
+const home = document.querySelector('.home');
+document.addEventListener('keydown', () => {
+  navItems.classList.toggle('vis');
+});
+console.log(title.innerText);
+title.addEventListener('mouseleave', () => {
+  title.style['font-size'] = '12px';
+});
+
+document.addEventListener('scroll', () => {
+  home.style.background = 'gray';
+});
+
+window.addEventListener('resize', () => {
+  home.style.color = 'purple';
+});
+
+const pics = document.querySelectorAll('img');
+pics[0].addEventListener('click', () => {
+  pics[0].classList.toggle('invert');
+});
+
+pics[1].addEventListener('mouseover', () => {
+  pics[1].classList.toggle('sat');
+});
+
+pics[2].addEventListener('dblclick', () => {
+  pics[2].classList.toggle('cntrst');
+});
+
+pics[3].addEventListener('load', () => {
+  pics[3].classList.add('cntrst');
+});
