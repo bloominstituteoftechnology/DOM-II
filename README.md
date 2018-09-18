8d84b03dd63673949fbfa341aaa91973eaedc7ea
Jake Thomas <jmaxt12@gmail.com>
2018-09-18 16:53:44 -0600

    Lots of random thing happen when you do random thing ;P

--- README.md
+++ README.md
@@ -11,7 +11,7 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 ## Task 2: Set Up LESS Preprocessor
 
-* [ ] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
+<!-- * [ ] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
 
 * [ ] Open your terminal and navigate to your preprocessing project by using the `cd` command
 
@@ -19,7 +19,7 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 * [ ] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
 
-* [ ] Once you see the red screen, you can delete that style and you're ready to start on the next task
+* [ ] Once you see the red screen, you can delete that style and you're ready to start on the next task -->
 
 ## Task 3: Create Unique Event Listeners
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 // Your code goes here
+const headPic = document.querySelector('.intro img');
+const pic = document.querySelectorAll('img');
+const btn = document.querySelectorAll('.btn');
+const bottomPic = document.querySelector('.content-destination img'); 
+const body = document.querySelector('body');
+const home = document.querySelector('nav');
+
+headPic.addEventListener('click', (event) => {
+    event.target.style.display = 'none';
+})
+
+pic[1].addEventListener('mouseover', (event) => {
+    btn[2].style.border = '2px solid red'
+})
+
+pic[2].addEventListener('dblclick', (event) => {
+    event.target.style.border = '5px dashed purple'
+})
+
+bottomPic.addEventListener('dblclick', (event) => {
+    btn[1].style.width = '90px';
+    btn[0].style.width = '30px';
+    btn[2].style.width = '270px';
+})
+
+btn[0].addEventListener('click', (event) => {
+    console.log('Button 1');
+})
+btn[1].addEventListener('click', (event) => {
+    console.log('Button 2');
+})
+
+btn[2].addEventListener('click', (event) => {
+    console.log('Button 3');
+})
+
+home.addEventListener('mouseover', (event) =>{
+    body.style.display = 'none';
+    
+})
+
