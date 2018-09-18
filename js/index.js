657eb61e9ba95d1f9c84072d246d5321f67d1122
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-09-18 16:11:27 -0500

    working on stretch

--- js/index.js
+++ js/index.js
@@ -34,19 +34,79 @@ button2.addEventListener('click', (event) => {
 
 const button3 = button[2];
 
+button3.addEventListener('click', (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    let home = document.querySelector('.home');
+    home.classList.toggle('button3');
+})
+
 const button4 = button[3];
+
+button4.addEventListener('click', (event) => {
+    event.stopPropagation();
+    event.preventDefault();
+    let home = document.querySelector('.home');
+    home.classList.toggle('button4');
+})
 
 const image1 = document.querySelector('.intro img');
 
+image1.addEventListener('click', (event) => {
+    event.stopPropagation();
+    image1.classList.toggle('image1');
+})
 const image2 = document.querySelector('.img-content img');
 
+image2.addEventListener('click', (event) => {
+    event.stopPropagation();
+    image2.classList.toggle('image2');
+})
+
 const image3 = document.querySelector('.inverse-content img');
+
+image3.addEventListener('click', (event) => {
+    event.stopPropagation();
+    image3.classList.toggle('image3');
+})
 
 const footerButtons = document.querySelectorAll('.btn');
 
 const footerButton1 = footerButtons[0];
 
+footerButton1.addEventListener('click', (event) => {
+    event.stopPropagation();
+    const contentPick = document.querySelector('.content-pick');
+    contentPick.classList.toggle('footerButton1');
+})
+
 const footerButton2 = footerButtons[1];
+
+footerButton2.addEventListener('click', (event) => {
+    event.stopPropagation();
+    const contentPick = document.querySelector('.content-pick');
+    contentPick.classList.toggle('footerButton2');
+})
 
 const footerButton3 = footerButtons[2];
 
+footerButton3.addEventListener('click', (event) => {
+    event.stopPropagation();
+    const contentPick = document.querySelector('.content-pick');
+    contentPick.classList.toggle('footerButton3');
+})
+
+
+const bigOlButton = document.querySelector('.btn-big');
+bigOlButton.addEventListener('click', (event) => {
+    event.stopPropagation();
+    event.
+    const contentPick = document.querySelector('.content-pick');
+    const newDiv = document.createElement('div');
+    newDiv.style.height = "100px";
+    newDiv.style.width = "200px";
+    newDiv.style.margin = "50px auto"
+    newDiv.style.backgroundColor = 'rgb(100,230,100)';
+    contentPick.append(newDiv);
+
+})
