ae6a9182c45d6e4b4bfa1cf7b6c71412324c4e03
Joe Bugajski <yogurtmystery@gmail.com>
2018-09-18 17:59:31 -0500

    first push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+const funBusHeading = document.querySelector(".logo-heading");
+funBusHeading.addEventListener("mouseover", e => {
+    funBusHeading.style.color = "pink";
+})
