2792e9431620781c22047a9272cc67a2ff5b09be
mikaelacurrier <mikaela.currier@gmail.com>
2018-09-18 15:25:19 -0400

    Initial Commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+let funTitle = document.querySelector('h1');
+funTitle.addEventListener('mouseover', (e) => {
+    event.target.style.color = 'purple';
+})
+
+document.addEventListener('keydown', (e) => {
+    const aKey = event.key;
+    alert(`Whoa dude, you can\'t just go around pressing buttons like:\n ${aKey}\n`)
+})
