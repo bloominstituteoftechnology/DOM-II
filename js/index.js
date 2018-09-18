fde20dfce2ce7ab41e7b94a072d464d6dcc34388
Lee Len Formento <leebatef@gmail.com>
2018-09-18 14:51:15 -0400

    Added query selectors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+// References
+const logo = document.querySelector(".logo-heading"); //h1
+const navLink = document.querySelectorAll(".nav-link"); //navitems
+const funImg = document.querySelector(".fun-bus-pic"); //fun bus pic
+const welcome = document.querySelector(".home h2");
+const letsGo = document.querySelector(".text-content h2");
+const destinationPic = document.querySelector(".pic-destination");
+const footer = document.querySelector(".footer");
