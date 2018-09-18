d671ebbf16aac19cb0c4159ca039ba69d200b5c4
Wanda <doubleuevans@gmail.com>
2018-09-18 15:23:22 -0500

    added click eventlistenter

--- js/index.js
+++ js/index.js
@@ -1,15 +1,22 @@
 // Your code goes here
 // `mouseover`
-const boldLink = document.querySelectorAll('.nav-link');
+const boldLink = document.querySelector('.logo-heading');
 boldLink.addEventListener('mouseover', (event) => {
-  event.preventDefault();  
+  //event.preventDefault();  
   event.target.style.fontFamily = 'Verdana, serif';
 
   setTimeout(function () {
     event.target.style.fontFamily = "";
   }, 500);
 }, false);
+
 // `keydown`
+const navGone = document.querySelector('a');
+navGone.addEventListener('keydown', (event) => {
+  if(event.keyCode >= 65 && event.keyCode <= 90){
+    event.target.style.display = "none"
+  }
+});
 // `wheel`
 // `drag / drop`
 // `load`
@@ -18,3 +25,13 @@ boldLink.addEventListener('mouseover', (event) => {
 // `scroll`
 // `select`
 // `dblclick`
+
+//on click
+const midButton = document.querySelectorAll('.btn');
+midButton[1].addEventListener('click', (event) => {
+  event.target.style.display = 'none'
+
+  setTimeout(function () {
+    event.target.style.display = "";
+  }, 1000);
+})
