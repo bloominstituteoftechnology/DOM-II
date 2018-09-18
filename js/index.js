e05249ae027b1217a66ba02553dbd45cb842526e
Brandon Strellis <bstrellis@gmail.com>
2018-09-18 19:10:33 -0400

    everything done but stopPropagation

--- js/index.js
+++ js/index.js
@@ -20,8 +20,7 @@ blogLink.addEventListener('copy', (event) => {
 });
 
 // Event 4: load ***********
-const contactLink = document.querySelector('.contact-link');
-contactLink.addEventListener('load', (event) => {
+window.addEventListener('load', (event) => {
     // do animation here?
     console.log('your window has loaded!');
 });
@@ -30,16 +29,46 @@ contactLink.addEventListener('load', (event) => {
 // can only be called on window
 // shouldn't be used for DOM manipulation? so another contrived example
 window.addEventListener('resize', (event) => {
+    // event.preventDefault();
     document.querySelector('nav').style.background = 'darkorange';
+    // event.stopPropagation();
 });
 
 // Event 6: focus
-homeLink.addEventListener('focus', (event) => {
-    document.querySelector('header img').src = 'https://wallpaperbrowse.com/media/images/POTD_chick_3597497k.jpg'
+const headerImg = document.querySelector('header img');
+homeLink.addEventListener('focus', () => {
+    headerImg.src = 'https://wallpaperbrowse.com/media/images/POTD_chick_3597497k.jpg';
 });
 
 // Event 7: blur
-homeLink.addEventListener('blur', (event) => {
-
+homeLink.addEventListener('blur', () => {
+    headerImg.src = 'https://cdn.kuaza.com/wp-content/uploads/2017/08/romantik-kuslar-14-subat-sevgililer-gunu-en-guzel-resimler.jpg';
 });
+
+// Event 8: dblclick
+headerImg.addEventListener('dblclick', () => {
+    headerImg.style.visibility = 'hidden';
+});
+
+// Event 9: scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('.home').style.background = 'red';
+});
+
+// Event 10: beforeprint
+window.addEventListener('beforeprint', (event) => {
+    console.log('print it, man!');
+});
+
+// Event 11: stopPropagation to rule them all!
+const mainNav = document.querySelector('.main-navigation');
+mainNav.addEventListener('mouseover', (event) => {
+    event.target.style.border = 'solid';
+});
+
+// prevents default behavior of anchor tags
+document.querySelectorAll('nav a').addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
 
