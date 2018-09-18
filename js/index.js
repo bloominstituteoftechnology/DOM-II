d76d1a43b031ea62e05d57f1c79ef8e846d929b1
csdisciple <oleksstasyev@gmail.com>
2018-09-18 19:26:22 -0400

    Initial commit, started on the MVP, half way done!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+let funBus = document.querySelector('img');
+funBus.addEventListener('click', event => {
+    console.log('Fun bus was clicked!');
+});
+
+let mainNav = document.querySelectorAll('a');
+mainNav[0].addEventListener("mouseover", event => {
+        mainNav[0].style.borderBottom = "1px solid black";
+});
