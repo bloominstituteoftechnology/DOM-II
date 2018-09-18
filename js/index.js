378a04b7d4538a022e24365cee1e2ddb5a6777c7
Phil Hart <phil@Phils-iMac.local>
2018-09-18 13:27:55 -0600

    made event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+// Event Listeners
+const navLogo = document.querySelector('.logo-heading');
+const nav = document.querySelector('.nav');
+const headerIntro = document.queryselector('.intro');
+const button = document.querySelector('.btn;');
+const secondImage = document.querySelector('.img-content');
+const letsGo = document.querySelector('.text-content');
+const footer = document.querySelector('.footer');
+const destination1 = document.querySelector('.content-destination');
+const destination2 = document.querySelector('.destination');
+
+
