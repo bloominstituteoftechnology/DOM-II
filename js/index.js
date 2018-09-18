f7470bf267304829054a1851862497741435c8f4
pedro <pedroluismontesinosrivera@gmail.com>
2018-09-18 17:34:17 -0400

    mouse down and mouse up for images

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 const logo = document.querySelector(".logo-heading");
-const busImage = document.querySelector(".home .intro img");
+const images = document.querySelectorAll("img");
+const background = document.querySelector("body");
 
 logo.addEventListener("mouseover", () =>{
     logo.classList.add("logo-move");
@@ -9,3 +10,14 @@ logo.addEventListener("mouseleave", () => {
     logo.classList.remove("logo-move");
 });
 
+images.forEach((img) => {
+    img.addEventListener("mousedown", function() {
+        this.style.boxShadow = "10px 10px 5px grey";
+        this.style.transform = "scale(1.1)";
+    });
+
+    img.addEventListener("mouseup", function(){
+        this.style.boxShadow = "";
+        this.style.transform = "scale(1)";
+    });
+});
