8c1328459c202278e4ed787453d0b2a9bbef01a3
Adam <ajmboosh@gmail.com>
2018-09-18 16:06:57 -0500

    initial pull request + added click event for navlinks with stopPropagation, and mouseover event for bus image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+//wholeNav and navHeader given same listener event + result of changing their background to red
+//navHeader is nested inside wholeNav, so stopPropagation() keeps a click on it
+//from changing the whole nav background
+const wholeNav = document.querySelector(".nav-container");
+wholeNav.addEventListener("click", event => {
+  event.currentTarget.classList.toggle("change-red");
+});
+
+const navHeader = document.querySelector(".logo-heading");
+navHeader.addEventListener("click", event => {
+  event.currentTarget.classList.toggle("change-red");
+  event.stopPropagation();
+});
+
+const busImg = document.querySelector(".intro img");
+busImg.addEventListener("mouseover", event => {
+  TweenMax.to(".intro img", 2, {
+    opacity: 0.5
+  });
+});
