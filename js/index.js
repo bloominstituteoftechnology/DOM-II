4a97b7b9fddc4b6bb5f5ac90847bf92f1132baec
Brian G <brian.gregorius@gmail.com>
2018-09-18 15:40:11 -0400

    compeleted button click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+const myBtn = document.querySelector('.btn');
+const contentPick = document.querySelector('.content-pick');
+const myHeader = document.querySelectorAll('.home h2');
+
+console.log(myHeader);
+
+
+console.log(contentPick);
+contentPick.addEventListener('click', () => {
+    contentPick.classList.toggle('change-background');
+});
+
+// myBtn.addEventListener('click', () => {
+    // Step 3: Do something!
+//     console.log("still works");
+//   });
