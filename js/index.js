02071d61fc8a3620a6f3f7387e3416c214658f08
Gabriel Samaniego <selfdev.gs@gmail.com>
2018-09-18 14:27:21 -0700

    mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+// mouseover ---- Used on "Let's Go!" img
+// keydown ------
+// wheel -------- Used on nav
+// drag / drop --
+// load --------- Used on window
+// focus -------- Used on form
+// resize -------
+// scroll ------- Used on fixed icon - to animate
+// select -------
+// dblclick ---- Used on pick
+
+// =========== MOUSEOVER ==========
+let imgContent = document.querySelectorAll(".content-section .img-content img");
+imgContent[0].addEventListener("mouseover", function(e) {
+  e.target.style.opacity = ".6";
+  e.target.style.transition = ".4s";
+});
