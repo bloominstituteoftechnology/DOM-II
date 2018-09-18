6981d2cc064b6daa377ae0fa38b454833a5521f8
Hunter315 <hunter.mckay.smith@gmail.com>
2018-09-18 14:26:12 -0600

    added nav, h1, scroll events

--- js/index.js
+++ js/index.js
@@ -1,10 +1,33 @@
 // Your code goes here
-const myBtn = document.querySelector('.btn');
+// Variables
+
+const mainNav = document.querySelector('.main-navigation .nav-container');
+const funBusMain = document.querySelector('.logo-heading');
+const nav = document.querySelectorAll('.nav-link');
 
 
 
+//Function
 
-myBtn.addEventListener('click', (e) => {
-    alert('You signed up!')
-       
+funBusMain.addEventListener('click', () =>{
+    funBusMain.style.color = 'darkkhaki';
+});
+
+//Nav elements mouseover Grow and Color Change
+for (let i = 0; i < nav.length; i++){
+    nav[i].addEventListener('mouseover', () =>{
+        nav[i].style.fontSize = '1.65rem';
+        nav[i].style.color = 'lightseagreen';
     });
+}
+//Nav elements shrink back and color change
+for (let i = 0; i < nav.length; i++){
+    nav[i].addEventListener('mouseout', (event) =>{
+        nav[i].style.fontSize = '1.6rem';
+        nav[i].style.color = 'black';
+    });
+}
+
+window.addEventListener('scroll', function() {
+    document.body.style.backgroundColor = "aliceblue";
+});
