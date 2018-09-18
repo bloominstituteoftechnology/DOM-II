1f821acb9042693bacd97744119ec35aa3259da1
SeanOne <basement365@gmail.com>
2018-09-18 14:30:52 -0700

    still just workking through them

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+
+//lock the page
+const wholePage = document.querySelector('body');
+const funBus = document.querySelector('#funBus');
+const signUpButton = document.querySelector('.btn');
+const intro = document.querySelector('.intro');
+
+
+
+
+wholePage.addEventListener('wheel', (e) => {
+//   e.preventDefault();  
+})
+
+intro.addEventListener("mouseover", function( e ) {
+    e.target.style.color = 'red';
+});
+
+window.addEventListener('load', function( e ) {
+    console.log('All Done!');
+});
+
+signUpButton.addEventListener('dblclick', function( e ) {
+    e.target.parentNode.style.order = '1';
+})
+
