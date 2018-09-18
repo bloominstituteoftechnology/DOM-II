b4a9c68c885096027bd572712ac025bbb8e2f67b
samsisle <samko9522@gmail.com>
2018-09-18 15:47:07 -0700

    Update index.js main assignment [1]

--- js/index.js
+++ js/index.js
@@ -1,1 +1,51 @@
-// Your code goes here
+/*-- DOM selectors -----------------------------*/
+const header = document.querySelector('header');
+const logoHeading = document.querySelector('.logo-heading');
+const navItems = document.querySelectorAll('.nav-link');
+
+/*-- functions ---------------------------------*/
+const randomNum = num => Math.floor(Math.random() * num);
+
+const randomColor = () => `rgb(${randomNum(256)}, ${randomNum(256)}, ${randomNum(256)})`;
+const randomRadius = num => `${randomNum(num)} px`;
+
+const tacoNuke = duration => {
+	setTimeout(() => {
+		alert('🌮 + 💣 = TacoNuke! Your body blemishes slowly.');
+	}, duration);
+};
+
+/*-- event listeners ---------------------------*/
+logoHeading.addEventListener('mouseover', () => {
+	logoHeading.style.color = randomColor();
+});
+
+window.addEventListener('keydown', () => {
+	logoHeading.style.color = 'black';
+});
+
+window.addEventListener('wheel', () => {
+	header.style.background = '#FFEBCD';
+});
+
+window.addEventListener('load', () => tacoNuke(3000));
+
+navItems[0].addEventListener('focus', () => {
+	navItems[0].style.background = randomColor();
+});
+
+navItems[0].addEventListener('blur', () => {
+	navItems[0].style.background = 'none';
+});
+
+window.addEventListener('resize', () => {
+	navItems[1].style.background = randomColor();
+});
+
+window.addEventListener('scroll', () => {
+	navItems[2].style.background = randomColor();
+});
+
+navItems[3].addEventListener('dblclick', () => {
+  navItems[3].style.background = randomColor();
+});
