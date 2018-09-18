734b00db7cd2ee5163f3e21e943e08d989984af2
jaklundun <govi.escudero@gmail.com>
2018-09-18 15:31:22 -0700

    On click Fun in The Sun CTA changes font from red to blue

--- js/index.js
+++ js/index.js
@@ -1,2 +1,14 @@
 // Your code goes here
 //Initial commit 
+
+// * Create 10 unique event listeners 
+
+// 1. changes 'Fun in The Sun CTA font from red to blue'
+let grabButton = document.querySelector('.destination');
+
+let buttonCTA = document.querySelector('.btn');
+
+grabButton.addEventListener('click', () => {
+    buttonCTA.style.color = 'blue';
+});
+
