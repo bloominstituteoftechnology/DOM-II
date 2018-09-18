c408016d811682343e3a2b21be7d0c6d4bf69df8
MEBonham <programmer@mckaybonham.com>
2018-09-18 17:01:11 -0600

    Made paragraphs disappear on right-click

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ navBar.addEventListener("wheel", navBarChange);
 
 function navBarChange(e) {
     e.preventDefault();
-    console.log(e);
+    // console.log(e);
     navBarOpacity += e.deltaY > 0 ? .02 : -.02;
     navBarOpacity = Math.min(navBarOpacity, 1);
     navBarOpacity = Math.max(navBarOpacity, 0);
@@ -29,7 +29,7 @@ mainTitle.addEventListener("mousemove", dragMove);
 mainTitle.addEventListener("mouseup", stopDrag);
 function startDrag(e) {
     if (e.button === 0) {
-        console.log("startDrag");
+        // console.log("startDrag");
         dragging = true;
         mouseOriginX = e.screenX;
         mouseOriginY = e.screenY;
@@ -37,14 +37,14 @@ function startDrag(e) {
 }
 function dragMove(e) {
     if (dragging) {
-        console.log("dragMove");
+        // console.log("dragMove");
         mainTitle.style.left = `${anchorX + (e.screenX - mouseOriginX)}px`;
         mainTitle.style.top = `${anchorY + (e.screenY - mouseOriginY)}px`;
     }
 }
 function stopDrag(e) {
     if (e.button === 0 && dragging) {
-        console.log("stopDrag");
+        // console.log("stopDrag");
         anchorX += e.screenX - mouseOriginX;
         anchorY += e.screenY - mouseOriginY;
         dragging = false;
@@ -58,7 +58,7 @@ loadAlert.textContent = "Website fully loaded!";
 loadAlert.style.color = "darkred";
 loadAlert.style.fontWeight = "bold";
 window.addEventListener("load", function(e) {
-    console.log("Load event firing");
+    // console.log("Load event firing");
     foot.appendChild(loadAlert);
 });
 
@@ -66,11 +66,11 @@ window.addEventListener("load", function(e) {
 const navLinks = document.querySelectorAll(".nav-container nav a");
 navLinks.forEach(link => {
     link.addEventListener("focus", function(e) {
-        console.log("focus");
+        // console.log("focus");
         e.target.classList.toggle("lime-glow");
     });
     link.addEventListener("blur", function(e) {
-        console.log("blur");
+        // console.log("blur");
         e.target.classList.toggle("lime-glow");
     })
 });
@@ -78,14 +78,32 @@ navLinks.forEach(link => {
 // Make o's appear wherever you click
 const body = document.querySelector("body");
 body.addEventListener("click", function(e) {
-    console.log(e.clientX);
-    console.log(e.clientY);
+    // console.log(e.clientX);
+    // console.log(e.clientY);
     let newO = document.createElement("p");
     newO.textContent = "o";
     newO.style.fontWeight = "bold";
     newO.style.color = "purple";
     newO.style.position = "absolute";
-    newO.style.left = `${e.clientX}px`;
-    newO.style.top = `${e.clientY}px`;
+    newO.style.left = `${e.clientX - 5}px`;
+    newO.style.top = `${e.clientY - 12}px`;
     e.currentTarget.prepend(newO);
+});
+
+// Zap paragraphs with right mouse click
+const allPs = document.querySelectorAll("p");
+allPs.forEach(function(paragraph) {
+    paragraph.addEventListener("contextmenu", function(e) {
+        if (e.button === 2) {
+            e.preventDefault();
+            e.currentTarget.style.display = "none";
+        }
+    });
+    // paragraph.addEventListener("click", function(e) {
+    //     e.preventDefault();
+    //     console.log("registered click");
+    //     if (e.button === 2) {
+            
+    //     }
+    // });
 });
