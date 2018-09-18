150cf46a2e44dbd9a78823dde9ea7629419b136b
Yanrong Xu <xuyanrong@icloud.com>
2018-09-18 15:55:34 -0400

    create the reference

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+// Create the reference
+const navTag = document.querySelectorAll('.main-navigation .nav-link');  //4 element
+const headingTag = document.querySelectorAll('h2'); // 4 element
+const pTag = document.querySelectorAll('p');
