e43d4bda7e13f4bb9ccbd6dc726fabbb3758ade2
Daniel Ferrer <danfrrr13@gmail.com>
2018-09-18 15:38:59 -0400

    added load event listener to window object

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+window.addEventListener("load", (event) => {
+    TweenMax.from('.nav', 2, {
+        opacity: 0
+      });
+    event.preventDefault();
+});
+
+let mainNavItem = document.querySelectorAll('.nav a');
