dc6a207386ff73a3eb285308ae932c035e4bf53c
Yanrong Xu <xuyanrong@icloud.com>
2018-09-18 19:13:53 -0400

    Create unqiue event listeners

--- js/index.js
+++ js/index.js
@@ -1,4 +1,62 @@
 // Create the reference
-const navTag = document.querySelectorAll('.main-navigation .nav-link');  //4 element
-const headingTag = document.querySelectorAll('h2'); // 4 element
+const bodyTag = document.querySelector('body');
+const navTag = document.querySelectorAll('.main-navigation .nav-link'); 
+const headingTag = document.querySelectorAll('h2'); 
 const pTag = document.querySelectorAll('p');
+const btn = document.querySelectorAll('.btn');
+
+const header = document.querySelector('.main-navigation');
+
+// add event listener to the reference
+bodyTag.addEventListener('keydown', (e) => {
+    bodyTag.classList.toggle('invisibility');
+})
+
+navTag[0].addEventListener('mouseover',(e) => {
+    header.classList.toggle('changeBackground');
+})
+
+bodyTag.addEventListener('wheel', (e) => {
+    e.stopPropagation();
+    bodyTag.style.backgroundColor = 'lightgray';
+})
+
+pTag[0].addEventListener('mousemove', (e) => {
+    e.stopPropagation();
+    console.log('u moveover me');
+})
+
+window.addEventListener('load',(e) => {
+    console.log('website fully load');
+})
+
+btn[1].addEventListener('dblclick', (e) => {
+    e.stopPropagation();
+    btn[1].style.backgroundColor = 'blue';
+})
+
+window.addEventListener('scroll', (e) => {
+    e.stopPropagation();
+    console.log('u are scoll');
+})
+
+window.addEventListener('resize', (e) => {
+    e.stopPropagation();
+    console.log('u are resize');
+})
+
+btn[2].addEventListener('contextmenu', (e) => {
+    e.stopPropagation();
+    console.log('u right-click');
+})
+
+window.addEventListener('copy', (e) => {
+    e.stopPropagation();
+    console.log('just copy a item');
+})
+
+for (let i=0; i < navTag.length; i++) {
+    navTag[i].addEventListener('click', (e) => {
+        e.preventDefault();
+    })
+}
