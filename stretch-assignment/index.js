b1c7c627ce7ef15d7cca4295f38eae5d2af519e0
rytwalker <rytwalker@gmail.com>
2018-09-18 18:28:03 -0400

    Completed travellers

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,7 +1,31 @@
-const blocks = document.querySelectorAll('.block');
 const container = document.querySelector('.blocks');
 
-container.addEventListener('click', e => {
-  let topBlock = e.target;
-  container.prepend(topBlock);
+// container.addEventListener('click', e => {
+//   let topBlock = e.target;
+//   container.prepend(topBlock);
+// });
+
+const blocks = document.querySelectorAll('.block');
+
+blocks.forEach(block => {
+  // mousedown on block
+  block.style.cursor = 'pointer';
+  let initTime;
+  let moveRight;
+  block.addEventListener('mousedown', () => {
+    initTime = Date.now();
+    moveRight = setInterval(blockInterval, 10);
+  });
+
+  // mouseup on window
+  window.addEventListener('mouseup', () => {
+    clearInterval(moveRight);
+  });
+
+  // callback for setInterval()
+  function blockInterval() {
+    let counter = Math.floor((Date.now() - initTime) / 100);
+    console.log(counter);
+    block.style.transform = `translateX(${counter}px)`;
+  }
 });
