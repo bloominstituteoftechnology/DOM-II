072324e1b2d4440ee2b4e06d139347fdce344614
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-18 14:30:59 -0500

    added keydown event on main content images

--- js/index.js
+++ js/index.js
@@ -6,3 +6,15 @@ document.querySelector('.intro img').addEventListener('mouseover', (e) => {
         e.target.style.border = '';
     }, 1500)
 });
+
+// Remove main content images on keydown with 'a'
+const img = document.querySelectorAll('.img-content img');
+const mainContent = document.querySelectorAll('.content-section');
+window.addEventListener('keydown', (e) => {
+    if (e.keyCode == '65') {
+        // firstImg.style.border = '3px solid red';
+        img[0].style.display = 'none';
+        img[1].style.display = 'none';
+        mainContent[0].style.width = '100%';
+    }   
+});
