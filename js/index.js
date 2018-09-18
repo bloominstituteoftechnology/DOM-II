c658c9a7e321a23235714d8a1332152b457753f9
lola <lev@levs-air.lan>
2018-09-18 17:08:59 -0400

    Added event listeners for mouseover, wheel, & scroll

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+document.querySelector('.logo-heading').addEventListener('mouseover', (event) => { 
+    event.currentTarget.innerHTML = 'Fun Bus!!!'
+    event.currentTarget.style.color = 'orange';
+});
+
+const container = document.querySelector('html')
+document.addEventListener('wheel', () => {
+    container.style.backgroundColor = '#ffdae0';
+});
+
+document.addEventListener('scroll', () => {
+    container.style.backgroundColor = '#ffedcc';
+});
+
