b00395fa94ed58a905a8a0544d0608cbcafb61cf
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-09-18 15:24:41 -0500

    adding variables

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+const header = document.querySelector('.main-navigation');
+
+const logoHeading = document.querySelector('.logo-heading');
+
+const button = document.querySelectorAll('.nav-link');
+
+const button1 = button[0];
+
+const button2 = button[1];
+
+const button3 = button[2];
+
+const button4 = button[3];
+
+const image1 = document.querySelector('.intro img');
+
+const image2 = document.querySelector('.img-content img');
+
+const image3 = document.querySelector('.inverse-content img');
+
+const footerButtons = document.querySelectorAll('.btn');
+
+const footerButton1 = footerButtons[0];
+
+const footerButton2 = footerButtons[1];
+
+const footerButton3 = footerButtons[2];
+
+
+
+console.log(footerButton1);
+console.log(footerButton2);
+console.log(footerButton3);
