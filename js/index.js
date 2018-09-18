6064e363d8b95348da20e10a7bdc30ff39b79c1e
Riley-Brown <riley@riley.gg>
2018-09-18 17:12:54 -0400

    Added load event, mouseover and mouseleave events

--- js/index.js
+++ js/index.js
@@ -4,19 +4,23 @@ let img = document.querySelectorAll("img");
 let modal = document.querySelector(".image-modal");
 let modalImg = document.querySelector(".image-modal img");
 
-function modalClickOne() {
-  imgSpan[0].addEventListener("click", () => {
-    modal.style.display = "block";
-    // fade modal in
-    // $(".image-modal").fadeIn();
-    // set image src
-    modalImg.src = img[2].src;
-  });
-}
+imgSpan[0].addEventListener("click", () => {
+  modal.style.display = "block";
+  modalImg.src = img[3].src;
+});
 
-modalClickOne();
+imgSpan[1].addEventListener("click", () => {
+  modal.style.display = "block";
+  modalImg.src = img[4].src;
+});
 
-// close modal
+imgSpan[2].addEventListener("click", () => {
+  modal.style.display = "block";
+  modalImg.src = img[5].src;
+});
+
+// close modal functions
+// close modal on image modal span click
 function modalClose() {
   document.querySelector(".image-modal span").addEventListener("click", () => {
     modal.style.display = "none";
@@ -25,6 +29,7 @@ function modalClose() {
 
 modalClose();
 
+// close img modal on img modal click
 function modalCloseTwo() {
   modalImg.addEventListener("click", () => {
     modal.style.display = "none";
@@ -32,18 +37,39 @@ function modalCloseTwo() {
 }
 modalCloseTwo();
 
-function modalClickTwo() {
-  imgSpan[1].addEventListener("click", () => {
-    modal.style.display = "block";
-    modalImg.src = img[3].src;
-  });
-}
-modalClickTwo();
+// hide spinner on load event
+window.addEventListener("load", () => {
+  document.querySelector(".fullscreen-spinner").style.display = "none";
+});
 
-function modalClickThree() {
-  imgSpan[2].addEventListener("click", () => {
-    modal.style.display = "block";
-    modalImg.src = img[4].src;
-  });
-}
-modalClickThree();
+// mouse over img events
+
+imgSpan[0].addEventListener("mouseover", () => {
+  imgSpan[0].style.transform = "scale(1.1)";
+  imgSpan[0].style.transition = "500ms ease-in-out";
+});
+
+imgSpan[0].addEventListener("mouseleave", () => {
+  imgSpan[0].style.transform = "scale(1)";
+});
+
+// img two mouse over
+imgSpan[1].addEventListener("mouseover", () => {
+  imgSpan[1].style.transform = "scale(1.1)";
+  imgSpan[1].style.transition = "500ms ease-in-out";
+});
+
+imgSpan[1].addEventListener("mouseleave", () => {
+  imgSpan[1].style.transform = "scale(1)";
+});
+
+// img three mouse over
+
+imgSpan[2].addEventListener("mouseover", () => {
+  imgSpan[2].style.transform = "scale(1.1)";
+  imgSpan[2].style.transition = "500ms ease-in-out";
+});
+
+imgSpan[2].addEventListener("mouseleave", () => {
+  imgSpan[2].style.transform = "scale(1)";
+});
