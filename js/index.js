51facf5ebe246dfa089fba17f4d8949222ba8b16
Kat Fries <katjohnnson@Kats-MacBook-Air.local>
2018-09-18 18:21:37 -0400

    need to fix propagation

--- js/index.js
+++ js/index.js
@@ -8,7 +8,12 @@ const btns = document.querySelectorAll(".btn");
 
 const contentSections = document.querySelectorAll(".content-section");
 
+const contentSectionsImgs = document.querySelectorAll(".content-section img");
+
 const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+
+let timedEvent;
+let myTimer = setInterval(timedEvent, 500);
 
 h1.draggable = true;
 h1.addEventListener('drag', (e) => {
@@ -25,6 +30,10 @@ aTags.forEach(aTag => aTag.addEventListener('mouseover', (e) => {
   e.target.style.color = rainbow[Math.floor(Math.random()*rainbow.length)];
 }));
 
+aTags[2].addEventListener('click', (e) => {
+  e.stopPropagation();
+})
+
 
 jumbo.addEventListener('mouseover', (e) => {
   e.target.src = "img/wall-e-bus.jpg";
@@ -40,42 +49,27 @@ jumbo.addEventListener('mouseout', (e) => {
 
 contentSections.forEach(aTag => aTag.addEventListener('dblclick', (e) => {
   e.target.style.visibility = 'hidden';
+
+  setTimeout(function() {
+    e.target.style.visibility = 'visible';
+
+  }, 1500);
 }));
+
+const imgArr = ["img/adventure.jpg", "img/fun.jpg"]
+
+contentSectionsImgs.forEach(aTag => aTag.addEventListener('dblclick', (e) => {
+  e.target.src = "img/wall-e-bus.jpg";
+
+  setTimeout(function() {
+    e.target.src = imgArr[Math.floor(Math.random()*imgArr.length)]
+
+  }, 2500);
+}));
+
 
 btns.forEach(btn => btn.addEventListener('click', (e) => {
   e.target.innerText = "Thanks for signing up!"
 }))
 
-// btns[0].addEventListener('click', (e) => {
-//   e.target.innerText = "Thanks for signing up!"
-// }) 
 
-// btns[1].addEventListener('click', (e) => {
-//   e.target.innerText = "Thanks for signing up!"
-// }) 
-
-// btns[2].addEventListener('click', (e) => {
-//   e.target.innerText = "Thanks for signing up!"
-// }) 
-
-
-
-// * [ ] `mouseover`
-// * [ ] `keydown`
-// * [ ] `wheel`
-// * [ ] `drag / drop`
-// * [ ] `load`
-// * [ ] `focus`
-// * [ ] `resize`
-// * [ ] `scroll`
-// * [ ] `select`
-// * [ ] `dblclick`
-
-{/* <img src="img/fun-bus.jpg" alt="bus in the sand"> */}
-
-// let logo = document.getElementById("logo-img"); //
-// logo.setAttribute('src', siteContent["nav"]["img-src"])	logo.setAttribute('src', siteContent["nav"]["img-src"])
-
-{/* <div class="container home">
-<header class="intro">
-  <img src="img/fun-bus.jpg" alt="bus in the sand"> */}
