2e457ea5da6291fe85f2fed00ae752c9df974f42
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-09-18 12:48:42 -0700

    nested two events and used event.stopPropagation();

--- js/index.js
+++ js/index.js
@@ -6,21 +6,24 @@ const introImg = document.querySelector(".intro img");
 const imageContent = document.querySelectorAll(".img-content img");
 const btn = document.querySelectorAll(".btn");
 const destination = document.querySelectorAll(".destination");
-const destinationH2 = document.querySelectorAll(".content-destination h2");
+const destinationH2 = document.querySelectorAll(".destination h4");
 const navLink = document.querySelectorAll(".nav-link");
 const body = document.querySelector("body");
 const navContainer = document.querySelector(".nav-container");
+const nav = document.querySelector(".nav");
 
 navH1.addEventListener("mouseover", event => {
   event.currentTarget.style.color = "blue";
 });
 
-intro.addEventListener("mouseenter", event => {
+intro.addEventListener("click", event => {
   event.currentTarget.style.backgroundColor = "powderblue";
+  event.stopPropagation();
 });
 
-introH2.addEventListener("click", event => {
+introH2.addEventListener("mouseenter", event => {
   event.currentTarget.innerText = "Come Aboard The Fun Bus!";
+  event.stopPropagation();
 });
 
 introImg.addEventListener("dblclick", event => {
@@ -32,5 +35,55 @@ imageContent[1].addEventListener("click", event => {
 });
 
 imageContent[0].addEventListener("click", event => {
-    event.currentTarget.style.width = "200px";
-  });
+  event.currentTarget.style.width = "200px";
+});
+
+btn[0].addEventListener("mousedown", event => {
+  event.currentTarget.style.backgroundColor = "red";
+  event.currentTarget.style.color = "black";
+});
+
+btn[2].addEventListener("mousedown", event => {
+  event.currentTarget.style.backgroundColor = "blue";
+  event.currentTarget.style.color = "black";
+});
+
+btn[1].addEventListener("mousedown", event => {
+  event.currentTarget.style.backgroundColor = "white";
+  event.currentTarget.style.color = "black";
+});
+
+destination[0].addEventListener("wheel", event => {
+  event.currentTarget.style.backgroundColor = "green";
+});
+
+destination[1].addEventListener("wheel", event => {
+  event.currentTarget.style.backgroundColor = "red";
+});
+
+destination[2].addEventListener("wheel", event => {
+  event.currentTarget.style.backgroundColor = "blue";
+});
+
+destinationH2[0].addEventListener("contextmenu", event => {
+  event.currentTarget.innerText = "Dont forget your sun-tan lotion!";
+});
+
+destinationH2[1].addEventListener("contextmenu", event => {
+  event.currentTarget.innerText = "Have fun climbing!";
+});
+
+destinationH2[2].addEventListener("contextmenu", event => {
+  event.currentTarget.innerText = "Take a swim in the ocean!";
+});
+
+navLink[0].addEventListener("click", event => {
+  event.currentTarget.style.color = "blue";
+  event.preventDefault();
+
+  event.stopPropagation();
+});
+
+nav.addEventListener("click", event => {
+  event.currentTarget.style.backgroundColor = "blue";
+});
