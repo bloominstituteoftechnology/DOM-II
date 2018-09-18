0c9bcb267155e1ee9e2ed6baa3c1e881577e74de
SeanOne <basement365@gmail.com>
2018-09-18 15:17:12 -0700

    couple more event listeners done | still working on others

--- js/index.js
+++ js/index.js
@@ -4,25 +4,44 @@
 //lock the page
 const wholePage = document.querySelector('body');
 const funBus = document.querySelector('#funBus');
-const signUpButton = document.querySelector('.btn');
+const signUpButton = document.querySelectorAll('.btn');
 const intro = document.querySelector('.intro');
+const search = document.querySelector(".fa-search");
+const destHeader = document.querySelectorAll('.destination h4')
 
 
-
-
+//locks up the page | doesnt allow scrolling
 wholePage.addEventListener('wheel', (e) => {
 //   e.preventDefault();  
 })
 
+//changes the text in the intro section to red
 intro.addEventListener("mouseover", function( e ) {
     e.target.style.color = 'red';
 });
 
+
+//console logs to when the page is loaded
 window.addEventListener('load', function( e ) {
     console.log('All Done!');
 });
 
-signUpButton.addEventListener('dblclick', function( e ) {
-    e.target.parentNode.style.order = '1';
+//console log fat shamming message
+window.addEventListener('resize', function( e ) {
+    console.log('Can\'t you just love me the way I am')
+})
+
+//moves buttons parent elements towards the right
+signUpButton.forEach(e => {
+    e.addEventListener('dblclick', function( event ) {
+        event.target.parentNode.style.order = '1';
+    })
+})
+
+//sets button parent elements back to 0
+destHeader.forEach(e => {
+    e.addEventListener('mouseleave', function( event ) {
+        event.target.parentNode.style.order = '0';
+    })
 })
 
