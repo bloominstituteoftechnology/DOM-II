8dda7cfc40a803a51a1aaff974a115c92c4ada97
Axolotl <samuelmachat@gmail.com>
2018-09-18 16:13:06 -0400

    added color rotation for nav h1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+// nav h1 changes to a one of a rotating array of colors each time you hover
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+
+let h1Color = 0
+
+const navH1 = document.querySelector('.nav-container h1')
+
+navH1.addEventListener('mouseenter', () => {
+  navH1.style.color = colors[h1Color]
+})
+
+navH1.addEventListener('mouseleave', () => {
+  navH1.style.color = 'black'
+  h1Color = h1Color === colors.length - 1 ? 0 : h1Color + 1
+})
+
+
