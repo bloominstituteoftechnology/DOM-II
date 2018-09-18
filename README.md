39748badf8aae191a2130fe84685f64581267320
SabrinaGear <sabrinagear.ft@gmail.com>
2018-09-18 16:58:14 -0500

    query selectors and event listeners 'click' 'mouseenter' 'mouseleave' 'toggle'

--- README.md
+++ README.md
@@ -4,31 +4,8 @@
 
 Fun Bus wants you to make their site more interactive. They are relying on you to provide 10 unique events to enhance their site. Explore the many events available to you by using the [MDN events reference](https://developer.mozilla.org/en-US/docs/Web/Events).
 
-  
 
-## Task 1: Set Up The Project With Git
 
-  
-
-* [ ] Fork the project into your GitHub user account
-
-* [ ] Clone the forked project into a directory on your machine
-
-* [ ] You are now ready to build this project with your preferred IDE
-
-  
-
-## Task 2: Set Up LESS Preprocessor
-
-* [ ] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
-
-* [ ] Open your terminal and navigate to your preprocessing project by using the `cd` command
-
-* [ ] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
-
-* [ ] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
-
-* [ ] Once you see the red screen, you can delete that style and you're ready to start on the next task
 
 ## Task 3: Create Unique Event Listeners
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,71 @@
-// Your code goes here
+let siteHead = document.querySelector("h1");
+let h2All = document.querySelectorAll("h2");
+
+//mouseover ~ one
+siteHead.addEventListener('mouseover', () => {
+    siteHead.style.color = "hotPink";
+});
+//mouseout ~ two
+siteHead.addEventListener('mouseout', () => {
+    siteHead.style.color = "Black";
+})
+
+for (let i = 0; i < h2All.length; i++) {
+    h2All[i].addEventListener('mouseover', () => {
+        h2All[i].style.color = "hotPink";
+    });
+    h2All[i].addEventListener('mouseout', () => {
+        h2All[i].style.color = "Black";
+    })
+}
+
+//click and double-click ~ three and four
+const navItems = document.querySelectorAll("nav a");
+const colors = ['purple', 'green', 'blue', 'pink'];
+for (let i = 0; i < navItems.length; i++) {
+    navItems[i].addEventListener('click', () => {
+        navItems[i].style.color = colors[i];
+        navItems[i].style.fontWeight = 'bold';
+        navItems[i].style.fontSize = '2rem'
+        navItems[i].addEventListener('dblclick', () => {
+            navItems[i].style.fontWeight = "bold";
+            navItems[i].style.color = "black";
+        });
+    })
+}
+
+const contImages = document.querySelectorAll(".img-content");
+console.log(contImages);
+
+const body = document.querySelector('body');
+
+const smuButtons = document.querySelectorAll(".btn");
+for (i = 0; i < smuButtons.length; i++) {
+    smuButtons[i].addEventListener('click', () => {
+        body.classList.toggle('gradient');
+    })
+}
+
+
+//mouseenter ~ five
+const funBus = document.querySelector(".intro img");
+funBus.addEventListener('mouseenter', () => {
+        funBus.style.borderRadius = "50%"
+    })
+    //mouseleave ~ six
+funBus.addEventListener('mouseleave', () => {
+    funBus.style.borderRadius = "0"
+})
+
+//wheel ~ seven
+const siteText = document.querySelectorAll("p");
+for (let i = 0; i < siteText.length; i++) {
+    siteText[i].addEventListener('wheel', () => {
+        siteText[i].style.color = "lightblue"
+
+    });
+
+}
+
+
+siteHead.classList.toggle('change');
