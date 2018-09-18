29d9cf9e25c66a87d01bfedcda11dfb5710671f2
jaklundun <govi.escudero@gmail.com>
2018-09-18 16:01:01 -0700

    Completed 4 events. 6 to go

--- js/index.js
+++ js/index.js
@@ -3,12 +3,39 @@
 
 // * Create 10 unique event listeners 
 
-// 1. changes 'Fun in The Sun CTA font from red to blue'
+// 1. changes 'Fun in The Sun CTA font from red to blue' on dblclick
 let grabButton = document.querySelector('.destination');
 
 let buttonCTA = document.querySelector('.btn');
 
-grabButton.addEventListener('click', () => {
+grabButton.addEventListener('dblclick', () => {
     buttonCTA.style.color = 'blue';
 });
+
+// 2. on mousing over hero banner, the header section disapears
+let heroBannerSec = document.querySelector('.intro');
+
+heroBannerSec.addEventListener('mouseenter', () => {
+heroBannerSec.style.display = 'none';
+
+})
+
+//3. Right clicking on Fun Bus logo will make the header section reappear.
+let logo = document.querySelector('.logo-heading');
+
+logo.addEventListener('contextmenu', () => {
+    heroBannerSec.style.display = 'inline';
+    });
+
+// 4. Home Nav item has its font size blow up to 88px when the browser window is resized
+
+let homeNav = document.querySelector('.nav-link');
+
+window.addEventListener('resize', () => {
+    homeNav.style.fontSize = '88px';
+  });
+
+
+
+
 
