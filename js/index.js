d2ddbb5b7282b8f4b45ddd9cd8fd9f807cded18f
Katy Hollobaugh <khollobaugh@gmail.com>
2018-09-18 18:31:28 -0400

    stopped nav links refreshing

--- js/index.js
+++ js/index.js
@@ -1,55 +1,79 @@
 // Your code goes here
-// mouseover
 const titleText = document.querySelector('.logo-heading');
+const navBar = document.querySelector('.main-navigation .nav');
+const logIn = document.querySelector('.main-navigation .login');
+const buttons = document.querySelectorAll('.content-pick .btn');
+const myHeading = document.querySelector('.intro p');
+const headingSection = document.querySelector('.intro');
+const introImg = document.querySelector('.intro img');
+const homeLink = document.querySelector('#home-tag');
+const aboutLink = document.querySelector('#about-tag');
+const blogLink = document.querySelector('#blog-tag');
+const contactLink = document.querySelector('#contact-tag');
 
-  titleText.addEventListener('mouseover', (event) => {
+//prevent nav links refreshing
+homeLink.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+  aboutLink.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+  blogLink.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+  contactLink.addEventListener('click', (event) => {
+    event.preventDefault();
+  });
+
+
+// mouseover
+titleText.addEventListener('mouseover', (event) => {
     event.target.style.color = 'orange';
   });
 
 //keydown
-const navBar = document.querySelector('.main-navigation .nav');
 window.addEventListener('keydown', (event) => {
         navBar.style.color = 'magenta';
   });
 
-
 // drag / drop
-// const dragIt = document.querySelector('.content-pick .btn');
-
-//   dragIt.addEventListener('dragstart', (event) => {
-//         dragDrop.event.target.style.opacity = '.5';
-//   });
+buttons[0].addEventListener('dragstart', (event) => {
+    buttons[0].event.target.style.opacity = '.5';
+});
+buttons[1].addEventListener('dragstart', (event) => {
+    buttons[1].event.target.style.opacity = '.5';
+});
+buttons[2].addEventListener('dragstart', (event) => {
+    buttons[2].event.target.style.opacity = '.5';
+});
 
 // load
 window.addEventListener('load', (event) => {
     console.log('Fun Bus page is done loading!');
   });
 
-
 // focus
-const logIn = document.querySelector('.main-navigation .login');
-
-  logIn.addEventListener('focus', (event) => {
+logIn.addEventListener('focus', (event) => {
     event.target.style.background = 'gray';
     event.target.style.color = 'white';
   }, true);
 
-  //blur
-  logIn.addEventListener('blur', (event) => {
+//blur
+logIn.addEventListener('blur', (event) => {
       event.target.style.background = "";
   }, true);
 
 
 // copy
-  document.addEventListener('copy', (event) => {
+document.addEventListener('copy', (event) => {
     event.clipboardData.setData('text/plain', 'You copied some text from the Fun Bus website!');
     event.preventDefault();
   });
 
 
 // scroll
-  window.addEventListener('scroll', () => {
-    console.log('scrolled!');
+window.addEventListener('scroll', () => {
+          console.log('scrolled!');
   });
 
 // select **Not working**
@@ -61,9 +85,6 @@ const logIn = document.querySelector('.main-navigation .login');
 
 
 //click
-const myHeading = document.querySelector('.intro p');
-const headingSection = document.querySelector('.intro');
-
 headingSection.addEventListener('click', (event) => {
     event.target.style = 'background-color: magenta';
 });
@@ -73,11 +94,7 @@ myHeading.addEventListener('click', (event) => {
     event.stopPropagation();
 })
 
- 
-
 // dblclick
-const hideImg = document.querySelector('.intro img');
-
-  hideImg.addEventListener('dblclick', (event) => {
+introImg.addEventListener('dblclick', (event) => {
     event.currentTarget.classList.toggle('hide');
   });
