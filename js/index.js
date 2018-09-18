85b6fabe1be6434ecb3302dee5142da2651d07dc
unknown <quan.nguyen199712@gmail.com>
2018-09-18 15:25:30 -0400

    Initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
-// Your code goes here
+// Quan's code goes here
+const botBtn = document.querySelectorAll(".btn");
+for(let i=0; i<botBtn.length; i++){
+    botBtn[i].addEventListener('click', (event) => {
+        event.backgroundColor="white";
+      });
+}
