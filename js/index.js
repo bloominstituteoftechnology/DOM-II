58ce19cd76e9f0839fc210eb201b5b493b53f0af
Brandon Strellis <bstrellis@gmail.com>
2018-09-18 17:21:42 -0400

    first three events done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+// Event 1: mouseover
+const homeLink = document.querySelector('.home-link');
+homeLink.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'orange';
+});
+
+// Event 2: keydown
+// I think this is a pretty contrived example
+const aboutLink = document.querySelector('.about-link');
+aboutLink.addEventListener('keydown', (event) => {
+    event.target.innerText = 'keydown tripped!';
+});
+
+// Event 3: copy
+const blogLink = document.querySelector('.blog-link');
+blogLink.addEventListener('copy', (event) => {
+    console.log('Don\'t copy me! I barely earn a living as it is!');
+});
+
+// Event 4: 
