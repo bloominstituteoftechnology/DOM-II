20f836bc528795494d6c75b78ff110d217dbf41f
Chad Jemmett <chad.jemmett@gmail.com>
2018-09-18 16:46:37 -0600

    did 9 out of 10 of the requirments

--- js/index.js
+++ js/index.js
@@ -1,1 +1,72 @@
 // Your code goes here
+//
+const backGround = document.querySelector('.home');
+const funInSun = document.querySelectorAll('.destination');
+const buttonText = document.querySelector('.btn');
+const popUp = document.querySelector('.popup-ad');
+const welcomeText = document.querySelector('.container .intro p');
+const boatImage = document.querySelector('.content-destination img');
+const headerLinks = document.querySelectorAll('.nav a');
+const bodyColor = document.querySelector('.home');
+// console.log(headerLinks);
+// 1
+boatImage.addEventListener('drag', (event) => {
+  boatImage.style.borderRadius = '1000px';
+});
+// 2
+boatImage.addEventListener('dragend', (event) => {
+  boatImage.style.borderRadius = '50px';
+});
+// 3
+welcomeText.addEventListener('dblclick', (event) => {
+  welcomeText.style.background = 'yellow';
+});
+// 4
+popUp.addEventListener('click', (event) => {
+  console.log('here is the pop up ad');
+  popUp.classList.add('display-none');
+});
+// 5
+funInSun[0].addEventListener('mouseover', (event) => { 
+  backGround.style.background = 'yellow';
+  document.querySelector('.intro h2').style.color = 'black';
+});
+//6
+funInSun[1].addEventListener('mouseover', (event) => { 
+  // console.log('we are over the div') 
+  backGround.style.background = 'purple';
+  document.querySelector('.intro h2').style.color = 'orange';
+ console.log(event);
+});
+// 7
+funInSun[2].addEventListener('mouseover', (event) => { 
+  // console.log('we are over the div') 
+  backGround.style.background = 'teal';
+  document.querySelector('.intro h2').style.color = 'white';
+
+});
+// 8 
+ headerLinks.forEach((item) => {
+   item.addEventListener('mouseover', (event) => {
+     item.style.color = 'blue'
+   })
+ });
+// 9
+
+document.addEventListener('keydown', (event) => {
+  let text = document.querySelector('.container h1')
+    if(text.style.color === 'yellow') {
+      text.style.color = 'black';
+    } else {text.style.color = 'yellow';}
+});
+document.addEventListener('keydown', (event) => {
+  let text = document.querySelector('p')
+    if(text.style.color === 'yellow') {
+      text.style.color = 'black';
+    } else {text.style.color = 'yellow';}
+  console.log(event);
+});
+// console.log(event);
+
+
+
