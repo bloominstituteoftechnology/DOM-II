2ce8860d5de96bb67bcb6d3494db7b274baac19e
Daniel Ferrer <danfrrr13@gmail.com>
2018-09-18 16:30:54 -0400

    added an event listener to the p tags

--- js/index.js
+++ js/index.js
@@ -5,7 +5,6 @@ window.addEventListener("load", (event) => {
     TweenMax.from('.nav', 2, {
         opacity: 0
       });
-    event.preventDefault();
 });
 
 //let mainNavItem = document.querySelectorAll('.nav a');
@@ -25,3 +24,20 @@ for (let i = 0; i < images.length; i++){
     });
 }
 
+
+let h2 = document.querySelectorAll('h2');
+
+for (let i = 0; i < images.length; i++) {
+    h2[i].addEventListener('mouseenter', (event) => {
+        TweenMax.to(h2[i], 3, {x:100})
+    });
+}
+
+
+let p = document.querySelectorAll('p'); 
+
+for (let i = 0; i < p.length; i++) {
+    p[i].addEventListener('dblclick', (event) => {
+    p[i].style.fontWeight = 'bold';
+    });       
+}
