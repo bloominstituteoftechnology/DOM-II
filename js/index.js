f0df5b75f1a79794a8bba3e718764b5f8584d361
Lee Len Formento <leebatef@gmail.com>
2018-09-18 15:29:51 -0400

    Added mouseover and mouseleave on fun bus logo.

--- js/index.js
+++ js/index.js
@@ -2,9 +2,18 @@
 
 // References
 const logo = document.querySelector(".logo-heading"); //h1
+const navBar = document.querySelectorAll(".nav");
 const navLink = document.querySelectorAll(".nav-link"); //navitems
 const funImg = document.querySelector(".fun-bus-pic"); //fun bus pic
 const welcome = document.querySelector(".home h2");
 const letsGo = document.querySelector(".text-content h2");
 const destinationPic = document.querySelector(".pic-destination");
 const footer = document.querySelector(".footer");
+
+// Event listeners
+logo.addEventListener("mouseover", () => {
+    logo.classList.toggle("grow"); 
+});
+logo.addEventListener("mouseleave", () => {
+    logo.classList.toggle("grow")
+});
