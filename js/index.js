11f5f9f9fea1e9a4eeb3a070453d318a6b78892a
rytwalker <rytwalker@gmail.com>
2018-09-18 16:46:38 -0400

    Finished main assignment

--- js/index.js
+++ js/index.js
@@ -18,10 +18,11 @@ imgContent.forEach(img => {
 
 // 3. doubleclick
 const hoverBtns = document.querySelectorAll('.btn-hover');
+const adventureImg = document.querySelector('.adventure-img');
 
 hoverBtns.forEach(btn => {
   btn.addEventListener('dblclick', () => {
-    btn.href = '#destination';
+    adventureImg.src = 'https://picsum.photos/376/282';
   });
 });
 
@@ -30,7 +31,6 @@ const filterInput = document.querySelector('#filterInput');
 const container = document.querySelector('.container');
 const logoHeading = document.querySelector('.logo-heading');
 
-// if filterInput.value = text on page background color yellow
 filterInput.addEventListener('keyup', () => {
   // change logo text
   logoHeading.innerText = filterInput.value;
@@ -41,9 +41,56 @@ filterInput.addEventListener('keyup', () => {
 // remove text from filter off focus
 filterInput.addEventListener('blur', () => {
   filterInput.value = '';
+  mainNav.style.height = '90px';
 });
 
 // 6. resize
 window.addEventListener('resize', () => {
   document.body.classList.add('dark');
 });
+
+// 7. clicks
+
+// 7.1
+const contentPick = document.querySelector('.content-pick');
+contentPick.style.cursor = 'pointer';
+contentPick.addEventListener('click', e => {
+  // vanish itens in section
+  e.target.classList.add('vanish');
+});
+
+// 7.2
+
+const contentPickBtn = document.querySelector('.content-pick .btn');
+contentPickBtn.addEventListener('click', e => {
+  e.stopPropagation();
+  contentPickBtn.innerText = 'Signed Up!';
+});
+
+// 8. scroll
+window.addEventListener('scroll', () => {
+  document.querySelector('.intro h2').style.fontSize = '4rem';
+  setTimeout(() => {
+    document.querySelector('.intro h2').style.fontSize = '2.5rem';
+  }, 1000);
+});
+
+// 9. load
+window.addEventListener('load', () => {
+  // fade in site
+  document.querySelector('body').classList.add('show');
+});
+
+// 10
+const mainNav = document.querySelector('.main-navigation');
+
+filterInput.addEventListener('select', () => {
+  console.log('selected');
+  mainNav.style.height = '300px';
+});
+
+// preventDefault() on nav links
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(link =>
+  link.addEventListener('click', e => e.preventDefault())
+);
