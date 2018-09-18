f592d6134ef8c3b2e4632c0612c0c32c8cc0ab23
bummings <sup@fauxkno.com>
2018-09-18 18:15:39 -0400

    added 3 events!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-// Your code goes here
+let logoHeading = document.querySelector('.logo-heading');
+
+let navLink = document.querySelectorAll('.nav-link');
+
+let body = document.querySelector('body');
+window.addEventListener('resize', (e) => {
+  // for (let i = 0; i < navLink.length; i++) {
+  //   navLink[i].classList.add('navLink');
+  // }
+  body.classList.add('navLink');  
+})
+
+
+let headerImg = document.querySelector('.intro img');
+headerImg.addEventListener('mouseover', (e) => {
+  headerImg.classList.add('blackAndWhite');
+});
+
+
+
+// window.addEventListener("load", function(event) {
+//   this.alert('Here\'s an annoying pop-up!');
+// });
