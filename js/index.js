615cd916ce22f145c7be070ae3766bb3374d6d9a
Carlos Lantigua <clantigua954@gmail.com>
2018-09-18 18:19:30 -0400

    completed all base objectives

--- js/index.js
+++ js/index.js
@@ -11,70 +11,92 @@ const textContent = document.querySelectorAll('.text-content')[0];
 const newP = document.createElement('p');
 const input = document.createElement('input');
 const destination = document.querySelector('.bottom-stuff');
-
+const allImages = document.getElementsByTagName('img');
 let scrollYHome = 0;
 //event listeners
 
-headerH1.addEventListener('mouseenter', (e) => {
-  // mouse enters header
-  e.target.preventPropagation;
-  e.target.style.transition = '0.3s ease-in';
-  e.target.style.fontSize = '4.5rem';
-  console.log('mouse is in');
+window.addEventListener('load', () => {
+  alert('Welcome to the fun bus, please do not click the creepy bus button. A developer made it when he was mad and we do not know how to fix it!');
 
-  // mouse leaves header
-  headerH1.addEventListener('mouseleave', (e) => {
+
+  headerH1.addEventListener('mouseenter', (e) => {
+    // mouse enters header
     e.target.preventPropagation;
-    e.target.style.transition = '0.3s ease-out';
-    e.target.style.fontSize = '4rem';
-    console.log('mouse is out');
+    e.target.style.transition = '0.3s ease-in';
+    e.target.style.fontSize = '4.5rem';
+    console.log('mouse is in');
+
+    // mouse leaves header
+    headerH1.addEventListener('mouseleave', (e) => {
+      e.target.preventPropagation;
+      e.target.style.transition = '0.3s ease-out';
+      e.target.style.fontSize = '4rem';
+      console.log('mouse is out');
+    })
+  });
+
+
+  creepBusBtn.addEventListener('click', (e) => {
+    // headerH1.preventPropagation;
+    e.target.preventPropagation;
+    headerH1.style.transition = '0.3s ease-in';
+    headerH1.innerText = 'Not so Fun Bus!';
+    funBusImg.removeAttribute('src');
+    funBusImg.setAttribute('src', 'img/creepyBus.jpg');
+    body.style.backgroundColor = 'black';
+    body.style.color = 'white';
+    mainHeading.innerText = 'Welcome to the Creepy Bus!';
+    letsGoH2.innerText = 'Leave us a message';
+    yourMessage.innerText = '';
+    input.type = 'text';
+    input.className = 'myInput';
+    textContent.appendChild(input);
+    newP.className = 'newP';
+    textContent.appendChild(newP);
   })
-});
 
-creepBusBtn.addEventListener('click', (e) => {
-  // headerH1.preventPropagation;
-  e.target.preventPropagation;
-  headerH1.style.transition = '0.3s ease-in';
-  headerH1.innerText = 'Not so Fun Bus!';
-  funBusImg.removeAttribute('src');
-  funBusImg.setAttribute('src', 'img/creepyBus.jpg');
-  body.style.backgroundColor = 'black';
-  body.style.color = 'white';
-  mainHeading.innerText = 'Welcome to the Creepy Bus!';
-  letsGoH2.innerText = 'Leave us a message';
-  yourMessage.innerText = '';
-  input.type = 'text';
-  input.className = 'myInput';
-  textContent.appendChild(input);
-  newP.className = 'newP';
-  textContent.appendChild(newP);
+  input.addEventListener('keyup', (e) => {
+    e.target.preventPropagation;
+    newP.innerText = input.value;
+  })
+
+  input.addEventListener('focus', (e) => {
+    e.target.preventPropagation;
+    e.target.style.border = "4px solid red";
+  })
+
+  input.addEventListener('blur', (e) => {
+    e.target.preventPropagation;
+    e.target.style.border = "0";
+  })
+
+  window.addEventListener('scroll', (e) => {
+    scrollYHome = window.scrollY;
+
+    if (scrollYHome > 350) {
+      destination.classList.add('visible');
+      destination.classList.remove('bottom-stuff');
+    } else {
+      destination.classList.add('bottom-stuff');
+      destination.classList.remove('visible');
+    }
+    //console.log(scrollYHome);
+  })
+
+
 })
 
-input.addEventListener('keyup', (e) => {
-  e.target.preventPropagation;
-  newP.innerText = input.value;
-})
+for (let i = 0; i < allImages.length; i++) {
+  allImages[i].addEventListener('mouseover', (e) => {
+    e.target.preventPropagation;
+    e.target.style.transition = "0.5s ease-in";
+    e.target.style.transform = "skew(30deg, 20deg)";
+  })
+  allImages[i].addEventListener('mouseleave', (e) => {
+    e.target.preventPropagation;
+    e.target.style.transition = "0.5s ease-in";
+    e.target.style.transform = "skew(0deg, 0deg)";
+    e.target.style.zIndex = "-1";
+  })
+}
 
-input.addEventListener('focus', (e) => {
-  e.target.preventPropagation;
-  e.target.style.border = "4px solid red";
-})
-
-input.addEventListener('blur', (e) => {
-  e.target.preventPropagation;
-  e.target.style.border = "0";
-})
-
-window.addEventListener('scroll', (e) => {
-  scrollYHome = window.scrollY;
-
-  if (scrollYHome > 350) {
-    destination.classList.add('visible');
-    destination.classList.remove('bottom-stuff');
-  } else {
-    destination.classList.add('bottom-stuff');
-    destination.classList.remove('visible');
-    //destination.style.transition = '0.3s ease-out';
-  }
-  console.log(scrollYHome);
-})
