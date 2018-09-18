2b5941584ba6e33a26fbddc3d67da8dbe38eaa16
unknown <quan.nguyen199712@gmail.com>
2018-09-18 17:03:39 -0400

    5 down, 5 more to go

--- js/index.js
+++ js/index.js
@@ -1,7 +1,24 @@
 // Quan's code goes here
 const botBtn = document.querySelectorAll(".btn");
 for(let i=0; i<botBtn.length; i++){
-    botBtn[i].addEventListener('click', (event) => {
-        event.backgroundColor="white";
+    botBtn[i].addEventListener('mousedown', (event) => {
+        event.target.style.color='#17A2B8';
+        event.target.style.backgroundColor= 'white';
+      });
+      botBtn[i].addEventListener('mouseup', (event) => {
+        event.target.style.color='white';
+        event.target.style.backgroundColor= '#17A2B8';
       });
 }
+const logo= document.querySelector('.logo-heading');
+logo.addEventListener('mouseover', (event) =>{
+    event.target.style.color='red';
+})
+logo.addEventListener('mouseout', (event) =>{
+    event.target.style.color='black';
+})
+const busImg= document.querySelector('.intro img');
+busImg.addEventListener('click',(event)=>{
+    alert('Why clicked bus?');
+})
+
