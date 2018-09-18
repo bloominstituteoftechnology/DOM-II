174c2aee0ed9262e37ff588c5b56cfe73a1ad850
Katie Cleary <katiecleary@airmaster.lan>
2018-09-18 16:04:05 -0400

    mapped out events to use and approx functionality

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+/*
+1. onclick - image popout
+2. onscroll - email optin -popup
+3. submit - share for X popup
+4. mouse over - take a vacation quiz
+5. reset - reset quiz to start again or leave
+4. on copy - offer to use travel planner 
+5. on dbl click-more details popout 
+6. touch list - send data to UX team 
+9. on error - if image doesn't load image isnt shown
+10. on text select - mini-survey 
+*/
