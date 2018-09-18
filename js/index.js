9aec0273f732566f94c6f5676191748546e45892
David247365 <ds051128@gmail.com>
2018-09-18 14:56:40 -0500

    Added mouseover event on main image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+
+// mouseover
+// keydown
+// wheel
+// drag / drop
+// load
+// focus
+// resize
+// scroll
+// select
+// dblclick
+
+const mainImage = document.querySelector('img');
+
+mainImage.addEventListener('mouseover', (event) => {
+    event.target.style.filter = 'grayscale(100%)'
+})
