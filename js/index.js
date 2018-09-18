9f43bdab6d7950e9fba5e7b73a5f036c93bebbf4
bummings <sup@fauxkno.com>
2018-09-18 18:45:12 -0400

    added a really dumb Y-height calculator

--- js/index.js
+++ js/index.js
@@ -4,10 +4,7 @@ let navLink = document.querySelectorAll('.nav-link');
 
 let body = document.querySelector('body');
 window.addEventListener('resize', (e) => {
-  // for (let i = 0; i < navLink.length; i++) {
-  //   navLink[i].classList.add('navLink');
-  // }
-  body.classList.add('navLink');  
+  body.classList.add('uppercase');  
 })
 
 
@@ -17,7 +14,50 @@ headerImg.addEventListener('mouseover', (e) => {
 });
 
 
+function showCoords(evt){
+  console.log(
+    "screenX value: " + evt.screenX + "\n"
+    + "screenY value: " + evt.screenY + "\n"
+  );
+}
+
 
 // window.addEventListener("load", function(event) {
 //   this.alert('Here\'s an annoying pop-up!');
 // });
+
+
+
+  // for (let i = 0; i < navLink.length; i++) {
+  //   navLink[i].classList.add('navLink');
+  // }
+
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function doSomething(scroll_pos) {
+  // console.log(last_known_scroll_position);
+  let scrollY = last_known_scroll_position;
+  let scrollHeightTotal = document.body.scrollHeight;
+  let scrollBox = document.querySelector('.xycoords');
+  scrollBox.innerText = `Y: ${scrollY} / ${scrollHeightTotal}`;
+
+}
+
+window.addEventListener('scroll', function(e) {
+
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+     
+    ticking = true;
+
+  }
+  
+});
