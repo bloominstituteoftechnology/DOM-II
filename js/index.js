e42f5b18f76c9d789992274159e394a42b3c4a67
David247365 <ds051128@gmail.com>
2018-09-18 17:52:30 -0500

    added more events like mouseover, mouseleave, click, and dblclick

--- js/index.js
+++ js/index.js
@@ -1,36 +1,67 @@
 // Your code goes here
 
+// Selectors
 
-// mouseover
+const background = document.querySelector('body');
 
 const mainImage = document.querySelector('.container .intro img');
 
+const mainNavigation = document.querySelector('.main-navigation');
+
+const mainNavLogo = document.querySelector('.logo-heading');
+
+const mainNavText = document.querySelectorAll('.nav-link');
+
+const funInTheSun = document.querySelector('.destination h4')
+
+const video = document.createElement('video');
+video.style.maxWidth = '50%';
+video.style.marginTop = '20px';
+video.type = 'video/mp4';
+video.src = 'Exploring the Sahara Desert Morocco - Lonely Planet travel video.mp4';
+video.controls = 'true';
+
+const destination = document.querySelector('.content-destination');
+destination.prepend(video);
+
+const btn = document.querySelectorAll('.content-pick .btn');
+console.log(btn);
+
+const footer = document.querySelector('.footer');
+
+mainImage.style.filter = 'grayscale(100%)'
+// mainImage.style.zIndex = '-1'
+// mainImage.style.position = 'relative'
+
 mainImage.addEventListener('mouseover', (event) => {
+    event.target.style.filter = 'grayscale(0%)'
+});
+
+// mouseenter
+
+video.addEventListener('mouseenter', (event) => {
+    event.target.style.maxWidth = '100%'
+});
+
+//mouseleave
+
+video.addEventListener('mouseleave', (event) => {
+    event.target.style.maxWidth = '50%'
+});
+
+mainImage.addEventListener('mouseleave', (event) => {
     event.target.style.filter = 'grayscale(100%)'
 });
 
-
 // keydown
-
-const background = document.querySelector('html')
 
 background.addEventListener('keydown', (event) =>{
     event.target.style.backgroundImage = 'linear-gradient(#FF5F6D, #FFC371)';
+    event.stopPropagation();
+    event.preventDefault();
 });
 
-// wheel
-
-
-
-// drag / drop
-// load
-// focus
-// resize
 // scroll
-
-const mainNavigation = document.querySelector('.main-navigation');
-const mainNavLogo = document.querySelector('.logo-heading');
-const mainNavText = document.querySelectorAll('.nav-link');
 
 window.addEventListener('scroll', (event) => {
     mainNavigation.style.backgroundColor = 'black';
@@ -38,8 +69,27 @@ window.addEventListener('scroll', (event) => {
     mainNavLogo.style.color = 'white';
 });
 
-// select
+//  Click
+
+btn.forEach(item => item.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = '#F4E2D8';
+}));
+
 // dblclick
+
+btn.forEach(item => item.addEventListener('dblclick', (event) => {
+    event.target.style.backgroundColor = '#BA5370';
+}));
+
+// select
+
+footer.addEventListener('select' , (event) => {
+    event.target.alert('Something was selected!')
+});
+
+// load
+// focus
+// resize
 
 
 
