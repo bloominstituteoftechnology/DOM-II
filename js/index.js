9a5a75fe7cc553afa174dbd72c2ae93a84675d63
Daniel Ferrer <danfrrr13@gmail.com>
2018-09-18 16:08:01 -0400

    added an event listener to the images

--- js/index.js
+++ js/index.js
@@ -8,4 +8,17 @@ window.addEventListener("load", (event) => {
     event.preventDefault();
 });
 
-let mainNavItem = document.querySelectorAll('.nav a');
+//let mainNavItem = document.querySelectorAll('.nav a');
+
+let images = document.querySelectorAll('img');
+
+
+for (let i = 0; i < images.length; i++){
+    images[i].addEventListener('mouseover', (event) => {
+        // event.target.style.filter = 'grayscale(100%)';
+        //console.log(images);
+        // 
+    images[i].classList.toggle('grayscale');
+    });
+}
+
