666537d51bf4989153f8ef1ca81a3dcb590c6bfb
Brian G <brian.gregorius@gmail.com>
2018-09-18 17:00:06 -0400

    added keydown/keyup/click/mouseover

--- js/index.js
+++ js/index.js
@@ -3,16 +3,36 @@
 const myBtn = document.querySelector('.btn');
 const contentPick = document.querySelector('.content-pick');
 const myHeader = document.querySelectorAll('.home h2');
+const myNav = document.querySelector('body');
 
-console.log(myHeader);
+console.log(myNav);
 
-
-console.log(contentPick);
 contentPick.addEventListener('click', () => {
     contentPick.classList.toggle('change-background');
 });
+
 
 // myBtn.addEventListener('click', () => {
     // Step 3: Do something!
 //     console.log("still works");
 //   });
+
+myHeader.forEach(item => item.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'darkorange';
+
+    setTimeout(() => {
+        event.target.style.color = '';
+    }, 733);
+    }, false));
+
+myNav.addEventListener('keydown', (e) => {
+    if (event.key == 'g'){
+        document.body.style.background = 'darkblue';
+    }
+});
+myNav.addEventListener('keydown', e => {
+    if (e.key =='a'){
+        document.body.style.background = 'white';
+    }
+});
+
