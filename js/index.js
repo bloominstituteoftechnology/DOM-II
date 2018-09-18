f4b7603f1d22b8fd64e851758c591e123da879c4
Brad Mortensen <b.mort86@gmail.com>
2018-09-18 14:00:24 -0600

    adding event listeners to fun bus page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+const navItems = document.querySelectorAll('a');
+
+navItems.addEventListener('click', ()=>{
+    event.preventDefault();
+});
+
+
+const bottomButtons = document.querySelectorAll('btn');
+
+bottomButtons.addEventListener('click', (event) => {
+    event.target.innerText = 'GO!';
+});
+
+const destinationImg = document.querySelector('.content-destination .img');
+
+destinationImg.addEventListener('click', ()=>{
+    destinationImg.classList.toggle('img-source');
+});
