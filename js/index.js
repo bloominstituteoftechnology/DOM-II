2167376c5d666badfd459fa81636ec3823d6f7fb
David247365 <ds051128@gmail.com>
2018-09-18 15:14:00 -0500

    added keydown event to the html document

--- js/index.js
+++ js/index.js
@@ -2,8 +2,28 @@
 
 
 // mouseover
+
+const mainImage = document.querySelector('.container .intro img');
+
+mainImage.addEventListener('mouseover', (event) => {
+    event.target.style.filter = 'grayscale(100%)'
+});
+
+
 // keydown
+
+const backgroundColorChange = document.querySelector('html')
+
+backgroundColorChange.addEventListener('keydown', (event) =>{
+    event.target.style.backgroundImage = 'linear-gradient(#FF5F6D, #FFC371)';
+});
+
 // wheel
+
+backgroundColorChange.addEventListener('wheel', (event) =>{
+    event.target.style.backgroundImage = 'linear-gradient(#EECDA3, #EF629F)';
+});
+
 // drag / drop
 // load
 // focus
@@ -12,8 +32,5 @@
 // select
 // dblclick
 
-const mainImage = document.querySelector('img');
 
-mainImage.addEventListener('mouseover', (event) => {
-    event.target.style.filter = 'grayscale(100%)'
-})
+
