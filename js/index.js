dad959d5aca8c8f4102e02b315dc5f2cbd33495c
MEBonham <programmer@mckaybonham.com>
2018-09-18 15:33:32 -0600

    Got header opacity-scrolling, started on drag & drop

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+const navBar = document.querySelector(".main-navigation");
+let navBarOpacity = 0.2;
+navBar.style.backgroundColor = `rgba(71, 37, 14, ${navBarOpacity})`;
+navBar.addEventListener("wheel", navBarChange);
+
+function navBarChange(e) {
+    e.preventDefault();
+    console.log(e);
+    navBarOpacity += e.deltaY > 0 ? .02 : -.02;
+    navBarOpacity = Math.min(navBarOpacity, 1);
+    navBarOpacity = Math.max(navBarOpacity, 0);
+    navBar.style.backgroundColor = `rgba(71, 37, 14, ${navBarOpacity})`;
+}
