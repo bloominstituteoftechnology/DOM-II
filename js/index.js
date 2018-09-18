1dc09e3a9b07e9b414c2f90dd91e6d9aa1a72d1d
lola <lev@levs-air.lan>
2018-09-18 17:42:35 -0400

    Added form for more elements to play with; added event listeners for buttons and inputs

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.querySelector('.logo-heading').addEventListener('mouseover', (event) =>
     event.currentTarget.style.color = 'orange';
 });
 
-const container = document.querySelector('html')
+const container = document.querySelector('html');
 document.addEventListener('wheel', () => {
     container.style.backgroundColor = '#ffdae0';
 });
@@ -12,3 +12,12 @@ document.addEventListener('scroll', () => {
     container.style.backgroundColor = '#ffedcc';
 });
 
+const btns = document.querySelectorAll('.btn');
+btns.forEach((btn) => { btn.addEventListener('click', () => {
+    window.alert('FUN BUS IS READY TO DEPART!');
+})});
+
+const inputs = document.querySelectorAll('.input');
+inputs.forEach((input) => { input.addEventListener('focus', (event) => {
+    event.currentTarget.style.color = '#ee8033';
+})});
