9d7d8bc6ae5192999eab00b791a4e04281275a9a
Phil Hart <phil@Phils-iMac.local>
2018-09-18 16:24:46 -0600

    finished

--- js/index.js
+++ js/index.js
@@ -2,13 +2,71 @@
 
 // Event Listeners
 const navLogo = document.querySelector('.logo-heading');
-const nav = document.querySelector('.nav');
-const headerIntro = document.queryselector('.intro');
-const button = document.querySelector('.btn;');
+const nav = document.querySelector('.nav-link');
+const headerIntro = document.querySelector('.intro');
+const button = document.querySelectorAll('.btn');
 const secondImage = document.querySelector('.img-content');
 const letsGo = document.querySelector('.text-content');
 const footer = document.querySelector('.footer');
 const destination1 = document.querySelector('.content-destination');
 const destination2 = document.querySelector('.destination');
 
+  
 
+
+
+
+// Adding Event Listeners
+navLogo.addEventListener('mouseover', (event) => {
+    event.target.style.color = "red";
+
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+
+  
+  nav.addEventListener('click', (event) => {
+    TweenMax.to(".nav-link", 1, {
+      opacity: 0
+    });
+    nav.addEventListener('click', (event) => {
+        event.preventDefault();
+    })
+  
+});
+
+headerIntro.addEventListener('mouseover', (event) => {
+    TweenMax.to(".intro", 3, {
+      opacity: 0
+    });
+  
+});
+
+
+
+button[0].addEventListener('click', (event) => {
+    event.target.style.display = 'none';
+});
+button[1].addEventListener('dblclick', (event) => {
+    event.target.style.border = '6px dashed yellow'
+})
+button[2].addEventListener('mouseover', (event) => {
+    event.target.style.color = 'green'
+})
+secondImage.addEventListener('mouseover', (event) => {
+    event.target.style.display = 'none';
+});
+
+letsGo.addEventListener('mouseover', (event) => {
+    event.target.style.border = '5px solid blue';
+});
+
+destination1.addEventListener('mouseover', (event) => {
+    event.target.style.display = 'flex';
+    event.stopPropagation;
+})
+
+destination2.addEventListener('dblclick', (event) => {
+    event.target.style.color = 'red'
+})
