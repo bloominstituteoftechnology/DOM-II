22ffb78efa1f000e98fc0c0cec99f89a09a70770
Shawn Antonucci <shawnantonucci2018@gmail.com>
2018-09-18 11:39:29 -0700

    added selectors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const navH1 = document.querySelector('#logo-heading');
+const intro = document.querySelector('.intro');
+const introH2 = document.querySelector('.intro h2');
+const introImg = document.querySelector('intro img');
+const imageContent = document.querySelectorAll('.img-content');
+const btn = document.querySelectorAll('.btn');
+const destination = document.querySelectorAll('.destination');
+const destinationH2 = document.querySelectorAll('.content-destination h2');
+const navLink = document.querySelectorAll('.nav-link');
+const body = document.querySelector('body');
+const navContainer = document.querySelector('.nav-container');
+
