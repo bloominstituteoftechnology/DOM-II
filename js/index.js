2c084269cf415061d02fc0700a2e25d3c3d8498a
Rahul Desai <rd825@georgetown.edu>
2018-09-18 16:06:10 -0400

    Almost done with event listeners

--- js/index.js
+++ js/index.js
@@ -1,9 +1,10 @@
 
 // EVENT LISTENER #1 - mouseover
 let navLinks = document.querySelectorAll('a');
-navLinks.forEach(link => link.addEventListener("mouseover", function(){
+navLinks.forEach(link => link.addEventListener("mouseover", function(event){
     link.style.color = 'green';
     link.style.textDecoration = 'underline';
+    event.preventDefault();
 }));
 
 // EVENT LISTENER #2 - mouseout
@@ -22,7 +23,7 @@ navLinks.forEach(link => link.addEventListener("mouseout", function(){
 window.addEventListener("load", function(event) {
     setTimeout(function(){
         document.querySelector('.modal').style.display = 'none';
-    },100);
+    },1000);
 });   
 
 // EVENT LISTENER #5 - Keydown
@@ -35,6 +36,36 @@ window.addEventListener("keydown", function(event) {
 window.addEventListener("scroll", function(event) {
     document.querySelectorAll('img').forEach(image => {
         image.style.filter = "invert(100%)";
-        image.style.transition = '0.3s';
+        image.style.transition = '1s';
     });
 });   
+
+// EVENT LISTENER #7 - Transition End 
+window.addEventListener('transitionend', function(event) {
+    document.querySelectorAll('img').forEach(image => {
+        image.style.filter = "invert(0%)";
+        image.style.transition = '1s';
+    });
+});   
+
+// EVENT LISTENER #8 - Double Click
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('dblclick', function(event) {
+        event.target.innerHTML = 'Why are you <br> double-clicking?'
+        event.target.style.fontSize = '15px';
+        event.target.style.textAlign = 'center';
+    })    
+});
+
+// EVENT LISTENER #9 - copy
+window.addEventListener('copy', function(event) {
+    alert('Why are you copying our site?');
+});   
+
+
+// EVENT LISTENER #10 - click
+document.querySelectorAll('.btn').forEach(button => {
+    button.addEventListener('click', function(event) {
+        confirm(`Is ${event.target.parentNode.firstChild.nextSibling.innerText} the trip you want to go on?`);
+    })    
+});
