166288a49b89211d90f4f193cdc2f9f742179ab4
Steven O'Neill <oneillsteven957@gmail.com>
2018-09-18 15:41:12 -0500

    preventDefault nav items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let navLink = document.querySelectorAll('#link-tag');
+navLink.forEach((link) => {
+    link.addEventListener('click', (e) => { 
+    e.preventDefault();
+});
+});
