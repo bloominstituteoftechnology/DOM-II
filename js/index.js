1e17d4adbd44ca7c53019a692d3b616a6a354925
Edwin Torres <edwin.torres.jr2@gmail.com>
2018-09-18 18:48:55 -0400

    Update

--- js/index.js
+++ js/index.js
@@ -1,1 +1,68 @@
 // Your code goes here
+i=0;
+const bus = document.querySelector("img")
+const page = document.querySelector("html")
+const image = document.querySelectorAll("img")
+const btn = document.querySelectorAll(".btn")
+const all = document.querySelector("html")
+
+
+//click image to make it vanish
+bus.addEventListener('click', ()=> {
+    bus.style.visibility = "hidden"
+})
+
+
+
+//scroll to make bus visible
+page.addEventListener("wheel", () => {
+    bus.style.visibility = "visible"
+})
+
+//add fun to Fun Bus
+page.addEventListener("keydown", () => {
+  i++;
+  if (i === 1) {
+    page.style.color = "red"
+} else if (i === 2) {
+    page.style.color = "orange"
+} else if (i === 3) {
+    page.style.color = 'Yellow'
+} else if (i === 4) {
+    page.style.color = "green"
+} else if (i === 5) {
+    page.style.color = "blue" 
+} else if (i === 6) {
+    page.style.color = "indigo"
+} else if (i===7) {
+    page.style.color= "violet"
+} else {
+    i = 0
+}
+})
+
+//make image disapear
+image[1].addEventListener("drag", ()=>{
+    image[1].style.visibility = "hidden"
+})
+
+image[2].addEventListener("drag", ()=>{
+    image[2].style.visibility = "hidden"
+})
+
+image[3].addEventListener("drag", ()=>{
+    image[3].style.visibility = "hidden"
+})
+
+//gets your images back 
+btn[0].addEventListener("click", ()=> {
+    for(i=0; i<image.length; i++) {
+        image[i].style.visibility = "visible"
+    }
+})
+
+
+//changes text to black and resets rainbow
+html.addEventListener('click', ()=> {
+    
+})
