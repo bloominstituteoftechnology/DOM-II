75b3f62443388e27e4825e01aee55e3b0eb1163b
Steve Smodish <smodish@hotmail.com>
2018-09-18 18:35:59 -0400

    Trying folding navbar with no luck

--- js/index.js
+++ js/index.js
@@ -1,1 +1,72 @@
 // Your code goes here
+
+/****************
+ * 
+ * Main Nav
+ * 
+ ***************/
+
+let prevYoff = 0;
+const navBar = document.querySelector("#main-navigation");
+
+window.addEventListener('onscroll', (event) => {
+  let currentYoff = window.scrollY;
+  let getDown = currentYoff < prevYoff;
+  let navClasses = navBar.classList;
+
+  if (getDown && !navClasses.contains("scrolled")) {
+    navClasses.add("scrolled");
+  } else if (!getDown) {
+    navClasses.remove("scrolled");
+  }
+
+  prevYoff = currentYoff;
+
+  console.log(prevYoff);
+});
+
+const navItems = document.querySelectorAll('.nav-link');
+
+navItems.forEach(navItem => navItem.addEventListener('click', (event) => {
+  event.preventDefault();
+}));
+
+navItems.forEach(navItem => navItem.addEventListener('mouseover', (event) => {
+  navItem.style.color = "lightgrey";
+}));
+navItems.forEach(navItem => navItem.addEventListener('mouseout', (event) => {
+  navItem.style.color = "";
+}));
+
+/****************
+ * 
+ * content section
+ * 
+ ***************/
+
+/****************
+ * 
+ * inverse content 
+ * 
+ ***************/
+
+/****************
+ * 
+ * content destination
+ * 
+ ***************/
+
+/****************
+ * 
+ * content pick
+ * 
+ ***************/
+
+const contentPick = document.querySelectorAll('.destination');
+contentPick.forEach(pick => pick.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const picked = event.target.parentNode.children[0].innerText;
+
+  alert("You picked " + picked + "\nYour going to have a great time!");
+}));
