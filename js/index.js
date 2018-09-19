e82607bf9669fdf765424c574381531c420e4cb6
Corey Gumbs <cgumbs@Coreys-MBP-3.fios-router.home>
2018-09-19 11:03:53 -0400

    [chore] completed link hover event

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
 //Corey Gumbs
 //Sept 18, 2018
 // Your code goes here
+
+
+
+///Navigation
+let aLinks = document.querySelectorAll("a");
+aLinks.forEach((link) => {
+
+    link.addEventListener("mouseover", (event) => {
+        event.target.style.color = "#17A2B8";
+    });
+
+    link.addEventListener("mouseleave", (event) => {
+        event.target.style.color = "#212529";
+    });
+});
