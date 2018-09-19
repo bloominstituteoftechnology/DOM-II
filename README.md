5a2c6ad8de55c21c9ebd088fb7f3f4df75e03c71
Mindysueasaurus <mymindyyeager@gmail.com>
2018-09-18 23:33:45 -0500

    finished the last half of the events and cleaned up code

--- README.md
+++ README.md
@@ -37,17 +37,17 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 	* [x] `keydown`
 	* [x] `wheel`
 	* [x] `drag / drop`
-	* [ ] `load`
-	* [ ] `focus`
-	* [ ] `resize`
-	* [ ] `scroll`
-	* [ ] `select`
-	* [ ] `dblclick`
+	* [x] `load`
+	* [x] `focus`
+	* [x] `resize`
+	* [x] `scroll`
+	* [] `select`
+	* [x] `dblclick`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
-* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
-* [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
+* [x] Nest two similar events somewhere in the site and prevent the event propagation properly
+* [x] Stop the navigation from items from refreshing the page by using `preventDefault()`
 
 ## Stretch Task:
 
--- js/index.js
+++ js/index.js
@@ -1,41 +1,87 @@
 // Your code goes here
 
 
-//mouseover
-
+//mouseover-done
 let logoText = document.querySelector('h1');
 logoText.addEventListener('mouseover', function(event) {
-                    logoText.style.fontSize = '200';
-                    logoText.style.fontFamily = 'Roboto';
-                    logoText.style.color = 'pink';
-                    logoText.innerText = 'Mindy was Here!'
+    this.style.fontSize = '200';
+    this.style.fontFamily = 'Roboto';
+    this.style.color = 'pink';
+    this.innerText = 'Mindy was Here!'
 });
 
-//keydown
-
-window.addEventListener('keydown', (event) => {
-   
-       alert('Stop Pressing Keys Please!')
-    
+//keydown-done
+let bodyKey = document.querySelector('body')
+bodyKey.addEventListener('keydown', function(event) {
+    this.classList.toggle('changeColor')
 });
 
 //wheel
-const image = document.querySelector('.intro img')
-image.addEventListener('wheel' , () => {
-    image.style.display = "none";
+let imageIntro = document.querySelector(".intro img");
+imageIntro.addEventListener('wheel' , (event) => {
+    imageIntro.style.display = "none";
 });
 
-//drag/drop
+//drag/drop-done
 const imgAdventure = document.querySelector(".content-section img")
 imgAdventure.addEventListener('dragstart', (event) => {
-    //dragged = event.target;
     imgAdventure.style.opacity = 0.3;
 }, false);
 
 //load
+window.addEventListener('load', () => {
+    let textChange = document.querySelector('.home .intro p')
+    textChange.style.background = 'lightgray';
+});
 
-//focus
-//resize
-//scroll
-//select
+//focus/blur-done
+let welcomP = document.querySelector('.home .intro p')
+//welcomP.style.filter = "blur(2px)";
+welcomP.addEventListener("mouseover", (event) => {
+    event.target.style.filter = "blur(4px)";
+  })
+
+
+//resize-done
+let intro = document.querySelector(".home .intro");
+window.addEventListener("resize", event => {
+  intro.style.paddingBottom = "150px";
+});
+
+//scroll-done
+let contentText = document.querySelector('.home .content-section h2');
+window.addEventListener('scroll', event => {
+    contentText.style.transform = 'rotate(20deg)';
+    let body = document.querySelector('body')
+    body.style.color = 'pink';
+})
+
+//click
+let btn = document.querySelector('.content-pick');
+btn.addEventListener('click', function(event){
+    btn.style.fontFamily = 'georgia';
+    
+ })
+let newBtn = document.querySelector('.btn');
+ newBtn.addEventListener('click', function(event){
+    event.stopPropagation();
+    newBtn.innerText = "not today satan";
+    newBtn.style.width = '100%';
+    newBtn.style.background = 'gold';
+})
+
 //dblclick
+let dblBtn = document.querySelector('.home .content-pick .btn');
+dblBtn.addEventListener('dblclick', () => {
+    let btnNew = document.querySelector('.btn');
+    btnNew.style.color = 'turquoise';
+    btnNew.style.background = 'pink';
+})
+
+//stopping refresh
+let navRefresh = document.querySelectorAll('.nav a');
+navRefresh.forEach((link, i) => {
+    navRefresh[i].addEventListener('click', () => {
+        event.preventDefault();
+    })
+})
