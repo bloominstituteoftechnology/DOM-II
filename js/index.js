1021d1d58de446c5e096e11e23e801976775def8
JacobABrennan <jacobabrennan@gmail.com>
2018-09-19 14:22:21 -0400

    Event 2+3: dblclick, load

--- js/index.js
+++ js/index.js
@@ -8,3 +8,15 @@ document.body.addEventListener('dblclick', (event) => {
     setTimeout(() => {event.target.style.fontSize = 'unset';},1000);
     console.log('Current: '+event.currentTarget.tagName);
 });
+
+//-- 2: load, 3: click ---------------------------
+
+document.querySelectorAll('img').forEach(element => {
+    element.addEventListener('load', (event) => {
+        element.style.filter = 'invert(1)';
+        element.style.transition = 'filter 1s ease';
+    });
+    element.addEventListener('click', (event) => {
+        element.style.filter = 'unset';
+    });
+});
