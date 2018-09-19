464fbd6beef47e41fadc3135121d7fe1dabe86a4
Ryan Boris <neodynia@icloud.com>
2018-09-19 18:09:53 -0400

    Add ELs to buttons and navLinks

--- js/index.js
+++ js/index.js
@@ -8,24 +8,12 @@ btns.forEach(function (btn) {
     });
 });
 
-document.addEventListener('keydown', (event) => {
-    const keyName = event.key;
-    const prize1 = (Math.random()) * 100;
-    const prize = Math.round(prize1);
-    alert(`You pressed ${keyName} and your prize is ${prize} % off!`);
-});
-
-document.addEventListener('keyup', (event) => {
-    const keyName = event.key;
-    alert(`You pressed ${keyName} and your prize is ${prize} % off!`);
-});
-
-document.addEventListener('keyleft', (event) => {
-    const keyName = event.key;
-    alert(`You pressed ${keyName} and your prize is ${prize} % off!`);
-});
-
-document.addEventListener('keyright', (event) => {
-    const keyName = event.key;
-    alert(`You pressed ${keyName} and your prize is ${prize} % off!`);
+const navlink = document.querySelectorAll('a.nav-link');
+navlink.forEach(link => {
+    link.addEventListener("mouseenter", e => {
+        e.target.style.color = "red";
+    });
+    link.addEventListener("mouseleave", e => {
+        e.target.style.color = "black";
+    });
 });
