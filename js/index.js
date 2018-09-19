e64d255b3da41fd5e531f1a9aeeda1d5fe8465d7
Chad Jemmett <chad.jemmett@gmail.com>
2018-09-18 20:12:52 -0600

    now have an understanding of the things talked about in the lecture

--- js/index.js
+++ js/index.js
@@ -8,6 +8,34 @@ const welcomeText = document.querySelector('.container .intro p');
 const boatImage = document.querySelector('.content-destination img');
 const headerLinks = document.querySelectorAll('.nav a');
 const bodyColor = document.querySelector('.home');
+const funBus = document.querySelector('.intro h2');
+function hideThing(e) {
+  if(e.currentTarget.style.visibility === 'hidden') {
+    e.currentTarget.style.visibility = 'visible';
+  } else {
+    e.currentTarget.style.visibility = 'hidden';
+  }
+}
+
+headerLinks.forEach((item) => {
+  item.addEventListener('click', (e) => {
+    popUp.style.visibility = 'visible';
+  })
+});
+
+funBus.addEventListener('click', (e) => {
+  if( e.target.parentNode.style.background === 'gray') {
+    e.target.parentNode.style.background = 'blue';
+  } else {
+    e.target.parentNode.style.background = 'gray';
+  }
+});
+
+const pTags = document.querySelectorAll('p');
+pTags.forEach((item) => {
+  item.addEventListener('mouseover', hideThing);
+  item.addEventListener('mouseleave', hideThing);
+});
 // console.log(headerLinks);
 // 1
 boatImage.addEventListener('drag', (event) => {
@@ -36,7 +64,7 @@ funInSun[1].addEventListener('mouseover', (event) => {
   // console.log('we are over the div') 
   backGround.style.background = 'purple';
   document.querySelector('.intro h2').style.color = 'orange';
- console.log(event);
+  console.log(event.currentTarget);
 });
 // 7
 funInSun[2].addEventListener('mouseover', (event) => { 
@@ -59,14 +87,13 @@ document.addEventListener('keydown', (event) => {
       text.style.color = 'black';
     } else {text.style.color = 'yellow';}
 });
+
 document.addEventListener('keydown', (event) => {
   let text = document.querySelector('p')
-    if(text.style.color === 'yellow') {
-      text.style.color = 'black';
-    } else {text.style.color = 'yellow';}
-  console.log(event);
+    if(text.style.color === 'black') {
+      text.style.color = 'yellow';
+    } else {text.style.color = 'black';}
 });
-// console.log(event);
 
 
 
