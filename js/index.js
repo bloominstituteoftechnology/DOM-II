b9f7014c421bf0a7855c790f18405c1a96e493ed
JacobABrennan <jacobabrennan@gmail.com>
2018-09-19 14:21:58 -0400

    Event 1: Click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+
+
+//== Ten Random Events =========================================================
+
+//-- 1: dblclick ---------------------------------
+document.body.addEventListener('dblclick', (event) => {
+    event.target.style.fontSize = '4rem';
+    setTimeout(() => {event.target.style.fontSize = 'unset';},1000);
+    console.log('Current: '+event.currentTarget.tagName);
+});
