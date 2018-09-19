efa7f9418b7812b749e24347573ea4b53b84d0fa
Cwright0428 <cwright0428@yahoo.com>
2018-09-19 10:05:58 -0500

    more changes to index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+let navLink = document.querySelectorAll(".nav-link");
+// console.log(navLink)
+navLink.forEach(each => {
+    each.addEventListener('mouseover', (event) => {
+        each.style.color = "red";
+        // console.log('triggering mouse over:');
+    })
+});
+
+const intro = document.querySelector('.intro')
+// console.dir(intro)
+intro.addEventListener('click', (event) => {
+    intro.style.color = "green";
+    console.log(event.target)
+});
+
+const textContent = document.querySelectorAll(".text-content")
+console.log('.textContent:', textContent);
+textContent.addEventListener('mouseenter', (event) => {
+    textContent.style.transform = 'scale(12)';
+    console.log('triggering mouse enter:');
+});
