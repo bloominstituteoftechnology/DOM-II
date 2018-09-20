363554ce44de0d3619ae1b8e301c9c700a515f69
Ryan Boris <neodynia@icloud.com>
2018-09-19 20:21:48 -0400

    Add wheel, mouseenter, mouseleave ELs

--- js/index.js
+++ js/index.js
@@ -10,10 +10,26 @@ btns.forEach(function (btn) {
 
 const navlink = document.querySelectorAll('a.nav-link');
 navlink.forEach(link => {
-    link.addEventListener("mouseenter", e => {
-        e.target.style.color = "red";
+    link.addEventListener("click", e => e.preventDefault());
+    link.addEventListener("dblclick", e => {
+        e.preventDefault();
+        e.target.classList.toggle("red");
     });
-    link.addEventListener("mouseleave", e => {
-        e.target.style.color = "black";
+});
+
+const h2 = document.querySelectorAll('h2');
+h2.forEach(h => {
+    document.querySelector('body').addEventListener("wheel", e => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        h.style.color = `#${randomColor}`;
     });
+});
+
+const header = document.querySelector('.intro');
+header.addEventListener("mouseenter", e => {
+    e.target.classList.add("hide");
+});
+
+header.addEventListener("mouseleave", e => {
+    e.target.classList.remove("hide");
 });
