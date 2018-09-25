504f947b9a76485928e700dfef760c0679524b44
Scott Bren <smbren@ncsu.edu>
2018-09-25 17:00:45 -0400

    Added new events (4 unique)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
 // Your code goes here
+const home = document.querySelector('.home');
+const navItems = document.querySelector('nav');
+const sunFun = document.querySelector('h4');
+const btn = document.querySelector('.btn');
+
+document.addEventListener('keypress', (event) => {
+
+    const logKey = event.key;
+
+    console.log(logKey);
+
+});
+
+document.addEventListener('scroll', (event) => {
+
+    let scrollDistance = 0;
+
+    scrollDistance = scrollDistance++;
+
+    console.log(scrollDistance);
+
+});
+
+navItems.addEventListener('click', (event) => {
+
+    event.preventDefault();
+
+});
+
+btn.addEventListener('click', (event) => {
+
+    //event.stopPropagation();
+    alert('Adventure Time!');
+
+});
+
+sunFun.addEventListener('mouseover', (event) => {
+
+    sunFun.style.cursor = 'pointer';
+
+});
+
+
+sunFun.addEventListener('click', (event) => {
+
+    console.log('Fun In The Sun!');
+
+});
+
