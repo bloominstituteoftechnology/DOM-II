f1ed1b2da1d69a68bbe31b8de4e6b817eb5e563b
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-10-23 17:43:59 -0400

    attempting to use blur

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// Your code goes here
+//Your code goes here
 const aTags = document.querySelectorAll('a');
 const myNav = document.querySelector('.nav');
 myNav.addEventListener('animationstart', function() {});
@@ -8,8 +8,27 @@ myDest.addEventListener('click', function() {
     event.target.style.color = 'red';
 });
 
-const myBtn = document.querySelectorAll('.btn');
+const myBtn = document.querySelector('.btn');
 console.log(myBtn);
-myBtn.addEventListener('focus', function() {
-    event.target.style.backgroundColor = 'green';
+myBtn.addEventListener('select', function() {
+    alert('Selection changed!');
+}, true);
+
+const myFoot = document.querySelector('.footer');
+myFoot.addEventListener('mouseover', function(event) {
+    event.target.textContent = 'Get the Foot out!';
+});
+
+let dragImg = document.querySelector('.rounded');
+dragImg.addEventListener('dragstart', function(event) {
+    dragged = event.target;
+    event.target.style.opacity = .5;
+}, false);
+dragImg.addEventListener("dragend", function(event) {
+    event.target.style.opacity = "";
+}, false);
+
+const myHeadings = document.querySelectorAll('h4');
+myHeadings.addEventListener('blur', function(event) {
+    event.target.style.background = "";
 }, true);
