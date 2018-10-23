74e7470e6e11a8c2a3f031710ee7321fcd7926e3
Josh Z <jzieger10@gmail.com>
2018-10-23 15:21:18 -0400

    Initial commit. Finished some event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+const navH1 = document.querySelector('.logo-heading');
+const busImg = document.querySelector('.busImg');
+let fontSize = 4;
+
+busImg.addEventListener('mouseover', function(){
+    this.style.filter = 'hue-rotate(140deg)'
+})
+
+navH1.addEventListener('wheel', function(){
+    if (event.deltaY > 0 && fontSize >= 1) {
+        fontSize--;
+      } else {
+        fontSize++;
+      }
+     this.style.fontSize = `${fontSize}rem`;
+})
