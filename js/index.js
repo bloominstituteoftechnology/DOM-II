178a760c972895ca7d292fc3d349255a96e70ebc
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-10-23 14:20:54 -0500

    add mouseover event listener to all img tags

--- js/index.js
+++ js/index.js
@@ -5,4 +5,10 @@
   window.addEventListener('load', function(e) {
     TweenLite.to(headerImg, 2, {rotation:360, ease: Power4.easeInOut,transformOrigin:"500px 50px"});
   });
+
+  const allImg = document.querySelectorAll('img');
+  allImg.forEach(e => e.addEventListener('mouseover', function(e) {
+    TweenLite.to(this, .5, {autoAlpha:.5});
+    TweenLite.to(this, .5, {autoAlpha:1, delay:.5});
+  }));
 })();
