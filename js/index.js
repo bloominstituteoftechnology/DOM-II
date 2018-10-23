b1c840d7cd4344658d97a24e2077bee8c5627870
J A <jarata@gmail.com>
2018-10-23 14:55:32 -0700

    dblclick and mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+// `mouseover`
+const nav = document.querySelector('.nav')
+
+nav.addEventListener('mouseover', function(event) {
+  event.target.style.color = 'orange';
+  event.target.style.fontWeight = 900;
+  setTimeout(function() {
+    event.target.style.color = "";
+    event.target.style.fontWeight = '';
+  }, 500);
+}, false);
+
+// `keydown`
+// const home = document.querySelector('.home')
+//
+// mainNav.addEventListener('keydown', function(event) {
+//   event.style.backgroundColor = #B7D9E3;
+//   setTimeout(function() {
+//     event.style.backgroundColor = '';
+//   }, 200);
+// }, false);
+
+// `wheel`
+
+// * [ ] `drag / drop`
+// * [ ] `load`
+// * [ ] `focus`
+// * [ ] `resize`
+// * [ ] `scroll`
+// * [ ] `select`
+
+// `dblclick`
+const button = document.querySelector('.content-destination > img')
+
+button.addEventListener('dblclick', function(event) {
+  event.target.parentNode.style.display = "none";
+  setTimeout(function() {
+    event.target.parentNode.style.display = "";
+  }, 900)
+}, false)
