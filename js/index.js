b139a38e79c218bd88b840eac10e16feb2601a6e
Latifah President <latifahpresident@Latifahs-MacBook-Pro.local>
2018-10-23 18:14:13 -0500

    added event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+const haveFunImg = document.querySelector('.img-fluid')
+console.log(haveFunImg);
+haveFunImg.addEventListener('mouseover', function(event){
+    haveFunImg.classList.toggle('display-none')
+});
+
+const homeColor = document.querySelector('html');
+
+    homeColor.addEventListener('click', function(event){
+        event.target.style.background = 'lightyellow'
+    });
+
+
+const h1Tag = document.querySelector('h1');
+h1Tag.addEventListener('dblclick', function(event){
+    event.currentTarget.textContent = 'Adventure Awaits'
+});
+
+// const destination = document.querySelector('.content-destination');
+// destination.addEventListener('click', function(event){
+//     TweenMax.to(".content-destination", 5, {rotation:360, ease:Power4.easeOut}) 
+// })
+
+// const h2Tag = document.querySelector('h2');
+// h2Tag.addEventListener('copy', function(event){
+//     event.currentTarget.style.color = 'green'
+// })
+
+const h2Tag = document.addEventListener('copy', () => {
+    console.log("Copied to clipboard!");
+})
