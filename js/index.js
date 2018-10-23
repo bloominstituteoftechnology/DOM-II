c44b4ed8cb2c495a79bf3a30e4dadaecd72eb7e6
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2018-10-23 16:39:01 -0400

    Adding focus to my buttons to add a background color

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const aTags = document.querySelectorAll('a');
+const myNav = document.querySelector('.nav');
+myNav.addEventListener('animationstart', function() {});
+
+const myDest = document.querySelector('div.destination');
+myDest.addEventListener('click', function() {
+    event.target.style.color = 'red';
+});
+
+const myBtn = document.querySelectorAll('.btn');
+console.log(myBtn);
+myBtn.addEventListener('focus', function() {
+    event.target.style.backgroundColor = 'green';
+}, true);
