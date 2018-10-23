4d501034417000a7e76d0cfec63aeb8fb02e8168
dyiar <austin.hl.blake@gmail.com>
2018-10-23 13:44:17 -0600

    Started DOM-II project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+const navBar = document.querySelectorAll('.nav-link');
+
+navBar.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'red';
+});
