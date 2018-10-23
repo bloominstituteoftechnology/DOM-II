309ad7873d4b7cf7ce2afa5c85efca63dd685333
tryingtokeepup <kl.hawaii@gmail.com>
2018-10-23 13:47:49 -0700

    first check, making sure everything works like codepen

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+const myButton = document.querySelector('.btn'); //check if i can also put .destination 
+
+
+// myButton.addEventListener('click', banana);
+// function banana(){
+//   console.log('This was called back on click!');
+// }
+
+myButton.addEventListener('click', banana);
+
+function banana(){
+    console.log('quick banana check!');
+}
