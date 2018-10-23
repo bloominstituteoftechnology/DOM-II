7ad72f1d2a0370c1662f59d68913bd7cf3d1c138
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-10-23 17:53:05 -0500

    add drop and drag event to h4

--- js/index.js
+++ js/index.js
@@ -63,16 +63,75 @@
     TweenLite.to(allImg, 3, {bezier:{curviness:1.25, values:[{x:10, y:0}, {x:-10, y:0}, {x:10, y:0}, {x:-10, y:0}]}, ease:Power1.easeOut});
   })
 
-  // prevent default on form button
+  // prevent default and propagation on form
   const signUpForm = document.querySelector('.signup-form');
+  signUpForm.addEventListener('click', function(e) {
+    signUpForm.classList.toggle('hidden');
+  });
+
+  const formInput = document.querySelectorAll('.form__input');
+  formInput.forEach(e => e.addEventListener('click', function(e) {
+    e.stopPropagation();
+  }))
   document.querySelector('#submitBtn').addEventListener('click', function(e){
     e.preventDefault();
+    e.stopPropagation();
     signUpForm.classList.toggle('hidden')
   })
 
   // change input text when selected
   document.addEventListener("select", function(event) {
-    console.log('select');
     event.target.value = `You're being hacked by the Cat in The Hat`;
   })
+
+  document.addEventListener("drags", function(event) {
+    //event.target.textContent = `No dragging allowed`;
+    console.log(event);
+  })
+
+
+  let dragged;
+  document.querySelectorAll('p').forEach(e => e.setAttribute('draggable', false));
+
+  /* events fired on the draggable target */
+  document.addEventListener("drag", function( event ) {
+
+  }, false);
+
+  document.addEventListener("dragstart", function( event ) {
+    dragged = event.target;
+    event.target.style.opacity = .5;
+  }, false);
+
+  document.addEventListener("dragend", function( event ) {
+    event.target.style.opacity = "";
+  }, false);
+
+  document.addEventListener("dragover", function( event ) {
+      event.preventDefault();
+  }, false);
+
+  document.querySelector('.sign-up').addEventListener("dragenter", function( event ) {
+      // highlight potential drop target when the draggable element enters it
+      if ( Array.from(event.target.classList).includes("dropzone") ) {
+          event.target.style.backgroundColor = "yellow";
+      }
+
+  }, false);
+
+  document.addEventListener("dragleave", function( event ) {
+      // reset background of potential drop target when the draggable element leaves it
+      if ( Array.from(event.target.classList).includes("dropzone") ) {
+          event.target.style.background = "";
+      }
+
+  }, false);
+
+  document.addEventListener("drop", function( event ) {
+    event.preventDefault();
+    // move dragged elem to the selected drop target
+    if ( Array.from(event.target.classList).includes("dropzone") ) {
+        event.target.value = dragged.textContent.toUpperCase();
+    }
+  }, false);
 })();
