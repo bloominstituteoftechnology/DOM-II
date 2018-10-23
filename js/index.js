d97857ed37b093375d2916d48feb0ea4400db7cc
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-10-23 14:09:51 -0500

    add load event listener to window

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 (function() {
   console.log('JavaScript is running');
 
+  const headerImg = document.querySelector('img');
+  window.addEventListener('load', function(e) {
+    TweenLite.to(headerImg, 2, {rotation:360, ease: Power4.easeInOut,transformOrigin:"500px 50px"});
+  });
 })();
