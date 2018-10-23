dbebeba59455957337f7f2cf4c9ac2f76cc4bca7
Lidii <lidiiany92@gmail.com>
2018-10-23 18:23:14 -0400

    Added event listeners

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Your code goes here
 
+
+//intro mouseenter, keydown
 const busImg = document.querySelector('.intro img');
 const introSection = document.querySelector('.intro');
 const introHeading = document.querySelector('.intro h2');
@@ -15,6 +17,69 @@ document.addEventListener('keydown', function(event){
     //console.log(event);
     introHeading.style.color = '#756345';
     introHeading.style.fontSize = '5rem';
+});
+
+
+//text - content wheel event
+const textContent = document.querySelectorAll('.text-content');
+
+document.addEventListener('wheel', function(event){
+    //  textContent[0].style.background = '#fff9ef';
+    //  textContent[0].style.transition = 'all .3s ease-out';
+    //  textContent[0].style.transitionDelay = '3s';
+
+    //  textContent[1].style.background = '#fff9ef';
+    //  textContent[1].style.transition = 'all .3s ease-out';
+    //  textContent[1].style.transitionDelay = '3s';
+
+     for(let i = 0; i<textContent.length; i++){
+        textContent[i].style.background = '#fff9ef';
+        textContent[i].style.transition = 'all .3s ease-out';
+        textContent[i].style.transitionDelay = '3s'
+     }
+
+});
+
+//content-destination scroll, dblclick 
+
+const contentDestination = document.querySelector('.content-destination');
+const scrolledHeading = document.querySelector('.content-destination h2');
+const destination = document.querySelectorAll('.destination p');
+
+contentDestination.addEventListener('dblclick', function(event){
+    console.log(event);
+    event.target.style.background = `rgba(237, 201, 175, .4)`;
+});
+
+window.addEventListener('scroll', function(){
+    
+    for(let i = 0; i<textContent.length; i++){
+    destination[i].style.background = 'rgba(237, 201, 175, .4)';
+    }
+});
+
+
+//destination select 
+
+destination.addEventListener('select', function(event){
+        event.target.style.color = 'blue';
+});
+
+//copy
+// const footerP = document.querySelector('footer p');
+// window.addEventListener('copy', function(event){
+//     footerP.style.color = 'pink';
+// });
+
+document.addEventListener('copy', () => {
+    alert("Copied to clipboard!");
+});
+
+//resize
+window.addEventListener('resize', function(event) {
+
+    alert('Testing');
+    
 });
 
 
