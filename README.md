1a18169a7d77ba4b037da7ef4cf5a2507d36aeca
Addison-Stavlo <AddieStavlo@gmail.com>
2018-10-23 18:28:09 -0400

    project complete

--- README.md
+++ README.md
@@ -10,48 +10,48 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
   
 
-* [ ] Fork the project into your GitHub user account
+* [X] Fork the project into your GitHub user account
 
-* [ ] Clone the forked project into a directory on your machine
+* [X] Clone the forked project into a directory on your machine
 
-* [ ] You are now ready to build this project with your preferred IDE
+* [X] You are now ready to build this project with your preferred IDE
 
   
 
 ## Task 2: Set Up LESS Preprocessor
 
-* [ ] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
+* [X] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
 
-* [ ] Open your terminal and navigate to your preprocessing project by using the `cd` command
+* [X] Open your terminal and navigate to your preprocessing project by using the `cd` command
 
-* [ ] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
+* [X] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
 
-* [ ] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
+* [X] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
 
-* [ ] Once you see the red screen, you can delete that style and you're ready to start on the next task
+* [X] Once you see the red screen, you can delete that style and you're ready to start on the next task
 
 ## Task 3: Create Unique Event Listeners
 
-* [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
-	* [ ] `keydown`
-	* [ ] `wheel`
-	* [ ] `drag / drop`
-	* [ ] `load`
-	* [ ] `focus`
-	* [ ] `resize`
-	* [ ] `scroll`
-	* [ ] `select`
-	* [ ] `dblclick`
+* [X] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
+	* [X] `mouseover`
+	* [X] `keydown`
+	* [X] `wheel`
+	* [X] `drag / drop`
+	* [X] `load`
+	* [X] `focus`
+	* [X] `resize`
+	* [X] `scroll`
+	* [X] `select`
+	* [X] `dblclick`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
-* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
-* [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
+* [X] Nest two similar events somewhere in the site and prevent the event propagation properly
+* [X] Stop the navigation from items from refreshing the page by using `preventDefault()`
 
 ## Stretch Task:
 
-* [ ] Go look at [GSAP](https://greensock.com/) and implement the animations found in that library with your custom events.
+* [X] Go look at [GSAP](https://greensock.com/) and implement the animations found in that library with your custom events.
 
   
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,101 @@
 // Your code goes here
+
+// * [ ] `mouseover`------------------------------
+// set anchor text color on mouseover
+const anchors = Array.from(document.querySelectorAll('.nav a'));
+anchors.forEach(e => e.addEventListener('mouseover',(event) => {
+    event.currentTarget.style.color = '#17A2B8';
+}));
+// * [ ] `keydown`--------------------------------
+// change body color on keypress
+const body = document.querySelector('body');
+document.addEventListener('keydown', (event) => {
+    body.style.backgroundColor = '#FFEBCD';
+
+});
+// * [ ] `wheel`----------------------------------------
+// increase header text on scroll over
+const introHeader = document.querySelector('.intro h2');
+introHeader.addEventListener('wheel', (event)=> {
+    event.preventDefault();
+    event.currentTarget.style.fontSize = '10rem';
+});
+// * [ ] `drag / drop`----------------------------------
+const introImg = document.querySelector('.intro img');
+const goImg = document.querySelector('.img-content img');
+
+
+introImg.addEventListener('drag',(event) => {
+    event.currentTarget.style.width = '400px';
+})
+introImg.addEventListener('dragend',(event) => {
+    event.currentTarget.style.width = '800px';
+})
+introHeader.addEventListener('dragover',(event) => {
+    event.preventDefault();
+});
+introHeader.addEventListener('drop',(event) => {
+    body.style.backgroundColor = 'purple';
+});
+
+// * [ ] `load`------------------------------------------
+// on load of img, turn background red and make img small
+goImg.addEventListener('load', (event) => {
+    goImg.style.width = '20px';
+    body.style.backgroundColor = 'red';
+})
+// * [ ] `focus`-----------------------------------------
+// turn anchors white on focus
+anchors.forEach(e => e.addEventListener('focus',(event) => {
+    event.currentTarget.style.color = 'white';
+}));
+// * [ ] `resize`---------------------------------------
+// turn body blue on resize
+window.addEventListener('resize', i => {
+    body.style.backgroundColor = 'blue';
+})
+// * [ ] `scroll`--------------------------------------
+// turn body green on scroll
+window.addEventListener('scroll', i => {
+    body.style.backgroundColor = 'green';
+})
+// * [ ] `select`---------------------------------------
+// turn textbox background black when text selected
+const textArea = document.querySelector('textarea');
+textArea.addEventListener('select', e => {
+    e.currentTarget.style.backgroundColor = 'black';
+})
+// cut works here where select wont
+const headText = document.querySelector('.intro p')
+headText.addEventListener('cut', (event)=>{
+    event.currentTarget.style.color = 'red';
+    console.log('selected');
+});
+// * [ ] `dblclick`-----------------------------------
+// change head welcome size on doubleclick
+introHeader.addEventListener('dblclick',e=>{
+    event.currentTarget.style.fontSize = '5rem';
+})
+//-----------------------------------------------------
+// prevent nav items from loading on click.
+anchors.forEach(e => e.addEventListener('click', (event) => {
+    event.preventDefault();
+}));
+
+//nest 2------------------------------------------------
+const textBox = document.querySelector('.text-content');
+const innerP = document.querySelector('.text-content p');
+textBox.addEventListener('click',e => {
+    e.currentTarget.style.backgroundColor = 'pink';
+})
+innerP.addEventListener('click',e=>{
+    e.stopPropagation();
+    e.currentTarget.style.backgroundColor = 'orange';
+})
+
+
+
+//GSAP---------------------------------------------------
+TweenMax.to('nav', 4, {marginRight:100,ease:Elastic.easeOut});
+TweenMax.to(introImg, 3, { borderRadius:'50%', border:'3px solid black'});
+TweenMax.from('body', 2, {opacity:0});
