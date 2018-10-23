8a0c126f46ba3eb16d4276621b2c3f70e9a0df5d
Dennis Orbison <orbison.dennis@gmail.com>
2018-10-23 15:33:22 -0700

    made some changes

--- js/index.js
+++ js/index.js
@@ -7,36 +7,45 @@ const images = Array.from(document.querySelectorAll('img'));
 const header = document.querySelector('header');
 const links = Array.from(document.querySelectorAll('a'));
 const container = document.querySelector('.home');
+let currentBackground = 'white-background'
+const paragraph = Array.from(document.querySelectorAll('p'));
 
 
 console.log(links);
 
 // 1 ; 1 - 4
-let color = ['red', 'blue', 'yellow', 'white'];
+let color = ['red-background', 'blue-background', 'yellow-background', 'white-background'];
+let colorText = ['red-text', 'blue-text', 'yellow-text', 'white-text'];
 for (let i = 0; i < images.length; i++) {
     images[i].addEventListener('mouseenter', function (event) {
-        header.style.backgroundColor = color[i];
+        header.classList.remove(currentBackground);
+        currentBackground = color[i];
+        header.classList.add(currentBackground);
     });
 }
 
 // 2 ; 2 - 8
-let saveHome = document.querySelector('.home').nodeValue;
+let saveHome;
 //saveHome = saveHome.slice(0);
-console.log(saveHome, 'here')
 for (let i = 0; i < links.length; i++) {
     links[i].addEventListener('click', function (event) {
         event.preventDefault();
-        document.body.style.backgroundColor = 'white';
-        document.querySelector('.home').style.display = 'block';
-        console.log(event.target.childNodes[0].data)
-        document.querySelector('.home').innerHTML = `<header class="intro"><h2>You clicked ${event.target.childNodes[0].data} button</h2</header>`;
+        header.classList.remove(currentBackground);
+        currentBackground = color[i]
+        console.log(header)
+        header.classList.add(currentBackground);
+        setTimeout(() => {
+            header.classList.remove(currentBackground);
+        }, 1000)
     });
 }
 
 // 3 ; 9
 container.addEventListener('mouseleave', function (event) {
-    container.style.display = 'none';
     document.querySelector('.logo-heading').classList.add('blink');
+    header.classList.remove(currentBackground);
+    currentBackground = 'green-background';
+    header.classList.add(currentBackground);
 });
 
 // header.addEventListener('mouseleave', function (event) {
@@ -46,7 +55,27 @@ container.addEventListener('mouseleave', function (event) {
 
 // 4 ; 10
 header.querySelector('.logo-heading').addEventListener('dblclick', function (event) {
-    document.querySelector('.home').appendChild = saveHome;
-    container.style.display = 'block';
     document.querySelector('.logo-heading').classList.remove('blink');
 });
+
+console.log(paragraph)
+
+//5
+paragraph.map(function (element) {
+    element.addEventListener('copy', function (event) {
+        console.log('here')
+        document.body.style.backgroundColor = 'pink';
+    })
+})
+
+//6
+let i = 0;
+let bodyBackground = 'white-background';
+window.addEventListener('keydown', (event) => {
+    document.body.classList.remove(bodyBackground);
+    bodyBackground = colorText[i];
+    console.log(color[i])
+    document.body.classList.add(bodyBackground);
+    i++;
+    if (i >= 4) i = 0;
+})
