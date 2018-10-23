b959edc152ae1b29142286e701ff11215b0bb609
Trai Lynne <trailynne@outlook.com>
2018-10-23 14:33:51 -0600

    Navigation: Added 'dblclick' event handler to nav links Links turn red and a dynamic alert is given

--- js/index.js
+++ js/index.js
@@ -10,8 +10,22 @@ let navLinks = document.querySelectorAll('.nav-link');
 
 //  ** == Events == **
 navLinks.forEach(link => {
-    link.addEventListener('click', event => {
-        console.log(event);
+    link.addEventListener('dblclick', event => {
+        // Prevent Default
         event.preventDefault();
+
+        link.style.color = 'red';
+        alert(`Coming Soon !! Be on the look out for our ${event.target.innerText.toUpperCase()} page !!`);
+
+        // Testing Log
+        console.log('type:', event.type, '\n\rcurrent target:', event.currentTarget, '\n\rtarget:', event.target);
     });
 });
+
+
+
+
+// Testing Log
+/*
+console.log('type', event.type, 'current target', event.currentTarget, 'target', event.target);
+*/
