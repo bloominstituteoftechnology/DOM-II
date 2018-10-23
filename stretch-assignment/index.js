139570cd3c3b0afcb7b348ec7833c212d552b6e3
Holden B <holden.bucher.92@gmail.com>
2018-10-23 16:58:47 -0500

    get rockets to move to the right

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -8,3 +8,28 @@ blocks.addEventListener('click', (event) => {
     blocks.children[0].classList.toggle(`${topColor}`);
     blocks.children[0].classList.toggle(`${clickColor}`)    
 })
+var currentX = 0;
+var rocketLaunch = false;
+const block = document.querySelectorAll('.block');
+block.forEach((ele) => ele.style.position = 'relative');
+console.log(block[0].style);
+function launchRocket() {
+    if (rocketLaunch == true) {
+        currentX += 10;
+        event.target.style.left = currentX + 'px';
+        console.log(event.target.style.left);
+    }
+}
+function blastOff() {
+    rocketLaunch = true;
+    launchRocket();
+}
+function returnToEarth() {
+    rocketLaunch = false;
+}
+setInterval(function () {
+    launchRocket();
+}, 200);
+
+block.forEach((ele) => ele.addEventListener('mousedown', blastOff, false));
+block.forEach((ele) => ele.addEventListener('mouseup', returnToEarth, false));
