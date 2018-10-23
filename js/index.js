9ce204417580f00c47dfd59b584e36e19939da2b
Stephen Bondor <bondor@Stephens-MacBook-Pro.local>
2018-10-23 13:07:09 -0700

    Add 10 really stupid events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,77 @@
 // Your code goes here
+
+//1
+const myTitle = document.querySelector('.logo-heading');
+
+myTitle.addEventListener('mouseover', function(event) {
+    // only has to deal with event bubbling
+    event.stopPropagation();
+    event.target.style.fontSize = "70px";
+});
+
+
+
+const myHome = document.querySelectorAll('.nav-link');
+
+//2
+myHome[0].addEventListener('mouseleave', function(event) {
+    // only has toded deal with event bubbling
+   // event.stopPropagation();
+    event.target.style.color = "red";
+});
+
+//3
+myHome[1].addEventListener('wheel', function(event) {
+    // only has to deal with event bubbling
+    event.stopPropagation();
+    event.target.style.color = "blue";
+});
+
+//4
+myHome[2].addEventListener('drag', function(event) {
+    // only has to deal with event bubbling
+    event.stopPropagation();
+    event.target.style.color = "yellow";
+});
+
+//5
+myHome[3].addEventListener('mouseenter', function(event) {
+    // only has to deal with event bubbling
+    event.stopPropagation();
+    event.target.style.color = "green";
+});
+
+//6 
+const myImg = document.querySelectorAll('img')
+myImg[0].addEventListener('click', function(event) {
+    // only has to deal with event bubbling
+    event.stopPropagation();
+    event.target.style.border = "10px solid green";
+});
+
+//7
+myImg[1].addEventListener('dblclick', function(event) {
+    // only has to deal with event bubbling
+    event.stopPropagation();
+    event.target.style.border = "10px solid blue";
+});
+
+//8
+myImg[2].addEventListener('contextmenu', function(event) {
+    // only has to deal with event bubbling
+    event.stopPropagation();
+    event.target.style.border = "10px solid purple";
+});
+
+//9
+myImg[3].addEventListener('mouseover', function(event) {
+    // only has to deal with event bubbling
+    event.stopPropagation();
+    event.target.style.border = "10px solid orange";
+});
+
+const myEverything = document.querySelector('.home');
+myEverything.addEventListener('mousedown', function(event) {
+    event.stopPropagation();
+    event.target.style.backgroundColor = "red";
+})
