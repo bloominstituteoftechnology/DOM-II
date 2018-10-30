894927ceb246ce680222844d6887155e7c5e44f0
J A <jarata@gmail.com>
2018-10-30 10:26:12 -0700

    task 3 3/10

--- js/index.js
+++ js/index.js
@@ -4,9 +4,21 @@ const load = window.addEventListener('load', event => {
   alert('Fun Bus has been loaded!')
 })
 // `mouseover`
+const mouseOver = document.querySelector('nav');
+
+mouseOver.addEventListener('mouseover', event => {
+  event.target.style.color = 'orange';
+  event.target.style.fontWeight = 1000;
+  setTimeout(function() {
+    event.target.style.color = "";
+    event.target.style.fontWeight = "";
+  }, 1000)
+}, false)
 
 // `keydown`
-
+const keyDown = window.addEventListener('keydown', event => {
+  alert("Don't press me further!")
+})
 // `wheel`
 
 // `drag / drop`
