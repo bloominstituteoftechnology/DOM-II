c2276b4949c642b2297807a35f44106af6aec28e
chrisgins <christiansgina@gmail.com>
2018-10-30 12:48:58 -0600

    Event 1: change to halloween picture on dblclick to adventure-img

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-// Your code goes here
+// ==== Event 1: change to halloween picture on dblclick to adventure-img
+
+const btnHover = document.querySelectorAll('.btn-hover');
+const adventureImg = document.querySelectorAll('.adventure-img');
+
+btnHover.forEach((btn, i) => {
+  btn.addEventListener('dblclick', () => {
+    adventureImg[i].src = 'https://cdn.pixabay.com/photo/2018/10/16/10/15/halloween-3751095_1280.jpg';
+  });
+});
+
