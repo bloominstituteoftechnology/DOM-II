8a5fdc13a734ea69199fe5a13d7d046fbbbfc27a
Joseph Eastman <josephryan96@gmail.com>
2018-10-30 15:48:15 -0700

    MVP WIP

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+let funBus = document.querySelector('.intro img');
+
+funBus.addEventListener('mouseover', event => {
+    funBus.src = ['img/no-fun-bus.jpg'];
+});
+
+document.addEventListener('keydown', event => {
+    alert('THE FUN IS OVER');
+});
+
+document.addEventListener('wheel', event => {
+    alert('You\'re not moving this bus!')
+});
+
+let mapImage = document.querySelector('.img-content');
+
+mapImage.addEventListener('drag', event => {
+    alert('STOP IT, HOW WILL WE KNOW WHERE TO GO??');
+});
+
+document.addEventListener('load', event => {
+    
+});
+
+'focus';
