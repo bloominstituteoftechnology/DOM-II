fa4273e5aef0795ac09cf3edb46a1e1cb5210178
chrisgins <christiansgina@gmail.com>
2018-10-30 14:12:20 -0600

    Add Events 2 & 3 stopPropagation

--- js/index.js
+++ js/index.js
@@ -1,11 +1,28 @@
-// ==== Event 1: change to halloween picture on dblclick to adventure-img
 
-const btnHover = document.querySelectorAll('.btn-hover');
-const adventureImg = document.querySelectorAll('.adventure-img');
+//==== Event 1: change to halloween picture on dblclick to adventure-img
 
-btnHover.forEach((btn, i) => {
+let btnHover = document.querySelectorAll('.btn-hover');
+let adventure = document.querySelectorAll('.adventure-img');
+
+img-content.forEach((btn, i) => {
   btn.addEventListener('dblclick', () => {
-    adventureImg[i].src = 'https://cdn.pixabay.com/photo/2018/10/16/10/15/halloween-3751095_1280.jpg';
+    adventure[i].src = 'hhttps://cdn.pixabay.com/photo/2018/10/16/10/15/halloween-3751095_1280.jpg';
   });
 });
 
+//====Event 2: make items in section vanish + mickey mouse hand====
+let contentPick = document.querySelector('.content-pick');
+contentPick.style.cursor = 'pointer';
+contentPick.addEventListener('click', e => {
+  e.target.classList.add('vanish');
+});
+
+
+//====Event 3: Change sign-up btns to read 'Hi Ash!'====
+let btnText = document.querySelectorAll('.content-pick .btn');
+btnText.forEach(button => {
+  button.addEventListener('click', e => {
+    e.stopPropagation();
+    button.innerText = 'Hi Ash!';
+  });
+});
