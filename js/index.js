79db0dcae964ca1f19b5d2338102fdb7e0049a18
Rachel Jefferies <rachel_lynne_t@yahoo.com>
2018-10-30 19:07:29 -0400

    Working on wheel scroll

--- js/index.js
+++ js/index.js
@@ -12,15 +12,16 @@
     * [ ] `dblclick` */
     
 // mouseover
+//look at the css below. It selects the logo heading (not the image)
 
-const imgMouseOver = document.querySelector('.main-navigation  nav-container .logo-heading');
+const imgMouseOver = document.querySelector(".main-navigation .nav-container .logo-heading")
 //removed .container from in front of nav-container
 
-document.addEventListener("mouseover", function( event ) {
-    // console.log(event);
-    // TweenMax.to(".logo-heading", 5, {rotation:360, scale:0.5, x:50, ease:Power4.easeOut});
+// document.addEventListener("mouseover", function( event ) {
+//     console.log(event);
+//     TweenMax.to(".logo-heading", 5, {rotation:360, scale:0.5, x:50, ease:Power4.easeOut});
 
-});
+// });
 
 //keydown
 
@@ -32,6 +33,29 @@ document.addEventListener('keydown', (event) => {
   });
 
 //wheel
+
+// const wheelScroll = document.querySelector("window");
+
+// document.addEventListener('wheel', (event) => {
+//     const scrollName = event.deltaY
+//     console.log(event.target);
+//     console.log(event.target);
+//     alert(scrollName);
+//     event.currentTarget.style.background = "green";
+//   });
+
+const wheelScroll = window.addEventListener('wheel',function (event) {
+    event.target.style.
+});
+
+// home.addEventListener('click', function(event) {
+//   // whatever you are targeting with your mouse
+//   console.log(event.target);
+//   //event.currentTarget.style.color = "green";
+//   // whatever you added the event listener to
+//   console.log(event.currentTarget);
+//   //event.target.style.color = "green";
+// });
 
 
 //drag-drop
