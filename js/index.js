e3dea6a1d48ee186b5585f88c2ce9ed6b4fceb4e
J A <jarata@gmail.com>
2018-10-30 14:03:27 -0700

    task 3 6/10

--- js/index.js
+++ js/index.js
@@ -20,15 +20,40 @@ const keyDown = window.addEventListener('keydown', event => {
   alert("Don't press me further!")
 })
 // `wheel`
-
+const wheel = window.addEventListener('wheel', function(event) {
+  event.target.style.backgroundColor = "#FCE1CE";
+  setTimeout(() => {
+    event.target.style.backgroundColor = "";
+  }, 3000)
+})
 // `drag / drop`
 
 // `focus`
 
 // `resize`
 
-// `scroll`
+// `mousemove`
+const mouseMove = document.querySelectorAll('.btn')
 
+mouseMove.addEventListener('mousemove', function(event) {
+  event.target.style.backgroundColor = "red";
+  setTimeout(function() {
+    event.target.style.backgroundColor = "";
+  }, 3000)
+})
 // `select`
+const select = document.getElementByTagName('body')
+
+select.addEventListener('select', function() {
+  alert('test');
+})
 
 // `dblclick`
+const dblClick = document.querySelector('h1')
+
+dblClick.addEventListener('dblclick', function(event) {
+  event.target.parentNode.style.backgroundColor = "#BDDBE6";
+  setTimeout(function() {
+    event.target.parentNode.style.backgroundColor = "";
+  }, 3000)
+})
