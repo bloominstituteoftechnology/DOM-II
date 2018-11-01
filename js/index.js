89eae8aa7c8ad7ed84016d2f77a55f810cea92f1
edreeseg <edward.reeseg@gmail.com>
2018-11-01 12:51:08 -0400

    Base objectives complete
    Commit done manually through web page due to lack of Git Bash access.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,54 @@
 // Your code goes here
+const logo = document.querySelector('.logo-heading');
+logo.style.cursor = 'pointer';
+logo.addEventListener('click', () => logo.style.color = logo.style.color === 'blue' ? 'black' : 'blue');
+
+const headerPicture = document.querySelector('.intro img');
+headerPicture.addEventListener('mouseover', () => 
+	headerPicture.style.opacity = headerPicture.style.opacity === '0.7' ? '1.0' : '0.7');
+	
+const bodyPictures = document.querySelectorAll('.img-content img');
+for (let pic of bodyPictures){
+	pic.addEventListener('mouseover', () => pic.style.borderRadius = '100px');
+	pic.addEventListener('mouseout', () => pic.style.borderRadius = '10px');
+}
+
+const destinationPic = document.querySelector('.content-destination img');
+
+window.addEventListener('load', () => {
+	let destRect = destinationPic.getBoundingClientRect();
+	window.addEventListener('resize', () => destRect = destinationPic.getBoundingClientRect());
+
+	destinationPic.addEventListener('drag', (e) => {
+	destinationPic.style.position = 'relative';
+	let currentX = e.clientX - destRect.x;
+	destinationPic.style.left = `${currentX - (destRect.width / 2)}px`;
+});
+
+destinationPic.addEventListener('dragend', () => {
+	destinationPic.style.position = 'static';
+});
+});
+
+const navLinks = document.querySelectorAll('.nav-link');
+for (let link of navLinks){
+	link.addEventListener('click', (e) => e.preventDefault());
+	link.addEventListener('mouseover', () => link.style.fontSize = '1.8rem');
+	link.addEventListener('mouseout', () => link.style.fontSize = '1.6rem');
+}
+
+const footerButtons = document.querySelectorAll('.btn');
+const footerDivs = document.querySelectorAll('.destination');
+
+for (let button of footerButtons){
+	button.addEventListener('click', (e) => {
+		button.style.borderRadius =
+		button.style.borderRadius === '0px' ? '15px' : '0px'
+		e.stopPropagation();
+	});
+}
+
+for (let div of footerDivs){
+	div.addEventListener('click', () => div.style.backgroundColor =
+	div.style.backgroundColor === 'rgb(23, 162, 184)' ? '#FFFFFF' : '#17A2B8');
+}
