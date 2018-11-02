56d9f226b1c7c212f624e42899e27e475106332d
ThomasUtsey <tomutsey@yahoo.com>
2018-11-01 22:04:41 -0400

    Add Event Listeners MouseOver and Wheel
    MouseOver - When the first H2 in the page is passed over by the mouse icon all of the h2's on the page changes colors.
    
    Wheel - When the first Paragraph is wheeled over by the scroll wheel all paragraphs change colors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,44 @@
 // Your code goes here
+// `mouseover`
+// this will be used to change the color of text in the h2s if the mouse passes over the first h2 "Welcome To Fun Bus!".
+const h2Mouse = document.querySelector('.intro h2');
+const h2Target = document.querySelectorAll('h2');
+h2Mouse.addEventListener('mouseover', event => {
+  h2Target[0].style.color='red'
+  h2Target[1].style.color='green'
+  h2Target[2].style.color = 'blue'
+  h2Target[3].style.color = 'orange'}
+  );
+//`keydown`
+
+
+// `wheel`
+// when the first paragragh is wheeled over it changes the color of all text in the paragraphs
+const pWheel = document.querySelector('.intro p');
+const pTarget = document.querySelectorAll('p');
+pWheel.addEventListener('wheel', event => {
+  pTarget[0].style.color='green'
+  pTarget[1].style.color='orange'
+  pTarget[2].style.color = 'red'
+  pTarget[3].style.color = 'blue'
+  pTarget[4].style.color='green'
+  pTarget[5].style.color='orange'
+  pTarget[6].style.color = 'red'
+  pTarget[7].style.color = 'blue'
+  pTarget[8].style.color='green'
+  pTarget[9].style.color='orange'
+}
+  );
+//`drag / drop`
+
+//`load`
+
+//`focus`
+
+//`resize`
+
+// `scroll`
+
+//`select`
+
+//`dblclick`
