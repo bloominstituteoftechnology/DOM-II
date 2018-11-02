bc3631adfec66def4abdba0f1316e5fa293c4633
Bill Geist <wcgeist@gmail.com>
2018-11-02 00:03:56 -0400

    changes to nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+// [ ] `mouseover`
+// [ ] `keydown`
+// [ ] `wheel`
+// [ ] `drag / drop`
+// [ ] `load`
+// [ ] `focus`
+// [ ] `resize`
+// [ ] `scroll`
+// [ ] `select`
+// [ ] `dblclick`
+
+const myNavLogo = document.querySelector('.logo-heading');
+myNavLogo.addEventListener('click', function (event) {
+    event.target.classList.toggle('active');
+    
+})
+
+const myNavLinks = document.querySelector('.nav');
+myNavLinks.addEventListener('mouseover', function (event) {
+    event.target.classList.toggle('active');
+    
+})
+
+const welcomeHeader = document.querySelector('.intro h2');
+document.addEventListener('keydown', function (event) {
+    event.target.classList.toggle('active');
+    
+})
