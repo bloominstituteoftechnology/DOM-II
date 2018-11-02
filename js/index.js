e54f720689ac3383fff5d0ee9dd856b2d95ac5d9
Jason Cook <the.root.user@gmail.com>
2018-11-02 19:46:20 -0400

    Confirm LESS watch-compiler running

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const images = document.querySelectorAll('img');
+
+images.addEventListener('mouseover', function(event) {
+    event.target.style.display = 'none';
+    event.stopPropagation(); 
+})
