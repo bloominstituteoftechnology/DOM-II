944ea9b01a08db2936f59e2438badde0248912dc
Trip1701 <bj2336@email.vccs.edu>
2018-11-02 10:32:15 -0400

    Event2: Keydown handler added to window, secret message if correct sequence found

--- js/index.js
+++ js/index.js
@@ -3,7 +3,18 @@
 //Sign Up Thank You
 const signUp = document.querySelectorAll('.btn');
 
-let count = 0;
 signUp.forEach(btn => btn.addEventListener('click', function() {
-  alert('Thank You For Signing Up!')
+  alert('Thank You For Signing Up!');
 }));
+
+
+//Hello World Easter Egg
+const keyLog = [];
+
+window.addEventListener('keypress', function(event) {
+  keyLog.push(event.key);
+  console.log(keyLog.join(''))
+  if(keyLog.join('') === 'hello world') {
+    alert('Hello Back To You');
+  }
+})
