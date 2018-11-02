fa800ea55f4ef50469e37b8a025e57a845d1189a
Bill Geist <wcgeist@gmail.com>
2018-11-02 00:51:48 -0400

    drag

--- js/index.js
+++ js/index.js
@@ -1,30 +1,36 @@
 // Your code goes here
 
-// [ ] `mouseover`
-// [ ] `keydown`
-// [ ] `wheel`
-// [ ] `drag / drop`
-// [ ] `load`
-// [ ] `focus`
-// [ ] `resize`
-// [ ] `scroll`
-// [ ] `select`
-// [ ] `dblclick`
-
 const myNavLogo = document.querySelector('.logo-heading');
 myNavLogo.addEventListener('click', function (event) {
     event.target.classList.toggle('active');
-    
 })
 
 const myNavLinks = document.querySelector('.nav');
 myNavLinks.addEventListener('mouseover', function (event) {
     event.target.classList.toggle('active');
-    
+})
+myNavLinks.addEventListener('mouseout', function (event) {
+    event.target.classList.toggle('active');
 })
 
 const welcomeHeader = document.querySelector('.intro h2');
 document.addEventListener('keydown', function (event) {
-    event.target.classList.toggle('active');
-    
+    const keyName = event.key;
+    if (keyName !== 'Control') {
+        welcomeHeader.classList.toggle('active'); 
+    }
 })
+
+const welcomeText = document.querySelector('.intro p');
+console.log(welcomeText);
+welcomeText.addEventListener('dblclick', function (event) {
+   event.target.classList.toggle('active');
+})
+
+const introImg = document.querySelector('.intro img');
+introImg.addEventListener('drag', function (event) {
+    event.target.style.border = "solid green 3rem";
+ })
+ introImg.addEventListener('dblclick', function (event) {
+    event.target.style.border = "none";
+ })
