0298072e555c3998d77f3a4307241e618a094b49
brettmadrid <brettmadrid9@gmail.com>
2018-11-01 20:58:52 -0700

    added wheel event listener
    when mouse wheel is rotated over any text element on the page the text
    element is changed to red

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 // Your code goes here
 const logoHeading = document.querySelector('.logo-heading');
 const body = document.querySelector('body');
+//const paragraphs = document.querySelectorAll('p');
 
 body.addEventListener('keydown', function() {
     alert('Please select a menu item. Thank you!');
@@ -14,5 +15,8 @@ logoHeading.addEventListener('mouseleave', function(event) {
     event.target.style.fontSize = '4rem';
 });
 
-
+body.addEventListener('wheel', function(event) {
+    event.target.style.color = 'red';
+    event.stopPropagation();
+});
 
