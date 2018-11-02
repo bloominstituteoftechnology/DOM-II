d1e0185838007f3023c0d52b2aa1c9a9ca0b3ffc
Jamar Torres <Jamar.Torres@UNHQ.UN.ORG>
2018-11-02 19:04:30 -0400

    created three successful event handlers so far

--- js/index.js
+++ js/index.js
@@ -1,3 +1,38 @@
 // Your code goes here
 
-const destBtn = document.querySelector()
+const destBtn = document.querySelector('.btn')
+
+// event listener using event bubbling to select all instances of button
+ document.addEventListener('click', (event) => {
+ 	event.stopPropagation()
+	if (event.target.classList.contains('btn')){
+		event.target.style.color = 'rebeccapurple';
+	} 
+	console.log(event.target)
+ }, false);
+
+ const logoExpansion = document.querySelector('.logo-heading')
+
+// mouseover event
+  logoExpansion.addEventListener('mouseover', (event) => {
+ 	event.target.style.fontSize = '50px';
+ 	console.log(logoExpansion)
+ });
+
+  const expandNavigation = document.querySelector('.nav-link')
+
+
+  // document.addEventListener('mouseenter', (event) => {
+  // 	event.stopPropagation()
+  // 	if (event.target.classList.contains('.nav-link')){
+  // 		event.target.style.fontSize = '20px';
+  // 		event.target.style.color = 'silver';
+  // 	}
+  // 	console.log(event.target)
+  // }, false);
+
+  const busImg = document.querySelector('img')
+
+  busImg.addEventListener('dblclick', (event) => {
+  		event.target.style.opacity = 0.5;
+  })
