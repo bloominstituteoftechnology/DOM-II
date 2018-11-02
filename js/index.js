8b292279167ab726c2029591a5312869ae76c44b
CatsAreMLG <joshuadgon@gmail.com>
2018-11-01 22:46:40 -0500

    Added 2/10 event listeners

--- js/index.js
+++ js/index.js
@@ -8,3 +8,16 @@
 // `scroll`
 // `select`
 // `dblclick`
+let you = prompt("Enter Your Name: ");
+document.addEventListener("copy", e => {
+  console.log(`Pleases don't forge ${you}`);
+});
+
+let counter = 0;
+const logo = document.querySelector(".logo-heading");
+document.addEventListener("wheel", e => {
+  logo.style.position = "relative";
+  counter += e.deltaY / 20;
+  counter < 0 ? (counter = 0) : (counter = counter);
+  logo.style.top = counter + "px";
+});
