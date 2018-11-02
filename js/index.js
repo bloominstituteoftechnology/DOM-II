c4f0b86f8b7baa0d914acb1f6848049244e6f923
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-02 13:52:33 -0400

    add mouseover and keydown addListeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+
+// Short notattion for document object
+const doc = document;
+
+// Mouseover listener
+
+const header = doc.querySelector('.main-navigation');
+
+// Bolden up our header to make it more prominent
+
+header.addEventListener('mouseover', (event) => {
+    event.stopPropagation();
+    event.target.style.fontWeight = 'bold';
+});
+
+
+// Keydown listener
+
+// Remind people to pay attention and scroll
+
+doc.addEventListener('keydown', (event) => {
+    const keydown = event.key;
+    alert('Typing any keys while reading our page including: ' + keydown + ' is strictly prohibited. Yes, even ' + keydown + '.');
+});
+
+
+
+
+
+
+
+
+
