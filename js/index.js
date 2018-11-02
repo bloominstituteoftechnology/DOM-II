98a8524dc17f17dbe16b5f619ec54f616378150c
Trip1701 <bj2336@email.vccs.edu>
2018-11-02 10:13:18 -0400

    Event1: Click Alert Added to Sign Up Buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+//Sign Up Thank You
+const signUp = document.querySelectorAll('.btn');
+
+let count = 0;
+signUp.forEach(btn => btn.addEventListener('click', function() {
+  alert('Thank You For Signing Up!')
+}));
