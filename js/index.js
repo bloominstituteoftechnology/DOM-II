9949ae4b735ff328a852b31c2a4f052299829e4a
brettmadrid <brettmadrid9@gmail.com>
2018-11-01 20:43:37 -0700

    added mouseover, mouseleave, and keydown eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+const logoHeading = document.querySelector('.logo-heading');
+const body = document.querySelector('body');
+
+body.addEventListener('keydown', function() {
+    alert('Please select a menu item. Thank you!');
+});
+
+logoHeading.addEventListener('mouseover', function(event) {
+    event.target.style.fontSize = '5rem';
+});
+
+logoHeading.addEventListener('mouseleave', function(event) {
+    event.target.style.fontSize = '4rem';
+});
+
+
+
