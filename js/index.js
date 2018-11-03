eab156c71924999b5752804954a1d2a4d5f05b91
CatsAreMLG <joshuadgon@gmail.com>
2018-11-03 13:02:12 -0500

    10/10 event listeners

--- js/index.js
+++ js/index.js
@@ -1,15 +1,10 @@
-// `mouseover`
-// `keydown`
-// `wheel`
 // `drag / drop`
-// `load`
 // `focus`
 // `resize`
-// `scroll`
 // `select`
-// `dblclick`
 let you = prompt("Enter Your Name: ");
 let title = document.querySelector(".intro h2");
+let span = document.querySelector(".intro span");
 document.addEventListener("copy", e => {
   title.textContent = `Check what you actually copied lol`;
   e.clipboardData.setData("text/plain", "ùwú");
@@ -20,7 +15,7 @@ document.addEventListener("keydown", e => {
 });
 
 let counter = 0;
-const logo = document.querySelector(".logo-heading");
+let logo = document.querySelector(".logo-heading");
 document.addEventListener("wheel", e => {
   logo.style.position = "relative";
   counter += e.deltaY / 20;
@@ -34,4 +29,51 @@ adventure.addEventListener("click", e => {
 });
 adventure.addEventListener("mouseover", e => {
   title.textContent = "Wanna go here?";
+});
+window.addEventListener("load", function(event) {
+  title.textContent = "All resources finished loading!";
+});
+let imageC = document.querySelectorAll(".img-content");
+let images = document.querySelectorAll(".img-content img");
+let body = document.querySelector("body");
+for (let i = 0; i < imageC.length; i++) {
+  imageC[i].addEventListener("dblclick", e => {
+    imageC[i].style.position = "absolute";
+    imageC[i].style.display = "flex";
+    imageC[i].style.justifyContent = "center";
+    imageC[i].style.alignItems = "center";
+    imageC[i].style.top = `${window.scrollY}px`;
+    imageC[i].style.left = `${window.innerWidth / 2 - 420}px`;
+    imageC[i].style.height = "100vh";
+    imageC[i].style.width = "800px";
+    images[i].style.maxWidth = "200vw";
+    images[i].style.width = "100%";
+    body.style.overflow = "hidden";
+  });
+  document.addEventListener("click", e => {
+    imageC[i].style.position = "static";
+    imageC[i].style.display = "block";
+    imageC[i].style.justifyContent = "flex-start";
+    imageC[i].style.alignItems = "flex-start";
+    imageC[i].style.top = `0`;
+    imageC[i].style.left = `0`;
+    imageC[i].style.height = "auto";
+    imageC[i].style.width = "48%";
+    images[i].style.maxWidth = "100%";
+    images[i].style.width = "auto";
+    body.style.overflowY = "scroll";
+  });
+}
+let mouse;
+window.addEventListener("mousemove", e => {
+  span.textContent = `mouse coordinates: ${e.x}, ${e.y}`;
+  span.style.fontSize = "1.5rem";
+});
+window.addEventListener("resize", e => {
+  title.textContent = `window size: ${window.innerWidth}px`;
+});
+window.addEventListener("cut", e => {
+  title.textContent = "Ouch! You cut me!";
+  e.clipboardData.setData("text/plain", "ùwú");
+  e.preventDefault();
 });
