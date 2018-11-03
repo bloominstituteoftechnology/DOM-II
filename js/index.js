7f78079a59dac7bf4392f95b355bb5448b47f40c
jrizza88 <jetorres88@gmail.com>
2018-11-03 17:44:40 -0400

    completed the main event listeners

--- js/index.js
+++ js/index.js
@@ -1,38 +1,31 @@
 // Your code goes here
 
-const destBtn = document.querySelector('.btn')
 
-// event listener using event bubbling to select all instances of button
+//even listener to see all instances of html clicks
  document.addEventListener('click', (event) => {
- 	event.stopPropagation()
-	if (event.target.classList.contains('btn')){
-		event.target.style.color = 'rebeccapurple';
-	} 
 	console.log(event.target)
  }, false);
+
 
  const logoExpansion = document.querySelector('.logo-heading')
 
 // mouseover event
   logoExpansion.addEventListener('mouseover', (event) => {
- 	event.target.style.fontSize = '50px';
- 	console.log(logoExpansion)
+   // event.target.style.fontSize = '50px';
+   event.target.classList.toggle('toggleIcon')
+ 	// console.log(logoExpansion)
  });
 
   const navLink = document.querySelectorAll('.nav-link')
   console.log(navLink);
 
-  // mouseenter event 
+  // mousemove event 
   for (let i = 0; i < navLink.length; i++){
-   navLink[i].addEventListener('mouseenter', (event) => {
-      // event.stopPropagation()
-      // if (event.target.classList.contains('.nav-link')){
-        event.preventDefault();
-        event.target.style.fontSize = '20px';
-        event.target.style.color = 'silver';
-      // }
-      console.log(event.target)
-    // }, false);
+   navLink[i].addEventListener('mouseover', (event) => {
+    event.preventDefault();
+    console.log("prevent default")
+          event.target.classList.toggle('navEvent');
+
     });
   }
 
@@ -43,4 +36,41 @@ const destBtn = document.querySelector('.btn')
   		event.target.style.opacity = 0.5;
   })
 
-  
+  const destBtn = document.querySelectorAll('.btn')
+
+  for (let i = 0; i <destBtn.length; i++){
+  destBtn[i].addEventListener('click', (event) => {
+      // event.preventDefault();
+      // event.target.style.color = 'rebeccapurple';
+      event.target.classList.toggle('btn-toggle');
+    });
+   console.log(destBtn)
+   console.log(destBtn[i]);
+    //  console.log(event.target)
+  }
+
+  const footerElement = document.querySelector('footer');
+// mouseenter event
+  footerElement.addEventListener('mouseover', (event) => {
+    event.target.classList.toggle('background-color');
+    console.log(footerElement)
+  })
+
+
+
+  // let elements = document.querySelectorAll('.block')
+
+  // const travelerEvent = (event) => {
+  //   let distance = 0;
+
+  //   const moveRight = () => {
+  //     distance += 10;
+  //     event.target.style.borderLeft = `${distance}px solid white`
+  //   }
+
+  //   setInterval (moveRight, 10)
+  // }
+
+  // for (let i = 0; i <elements.length; i++){
+  //   elements[i].addEventListener('click', travelerEvent)
+  // }
