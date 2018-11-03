112c7e746a6fa0a9d035331fed0f32161fde8105
Eiosx <eiosxenzo@gmail.com>
2018-11-03 13:06:43 -0400

    Added 3 event listeners.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+const h1 = document.querySelector('.logo-heading');
+const body = document.querySelector('body');
+const sandBus = document.querySelector('.intro img');
+
+//Hovering cursor over 'Fun Bus' title toggles a palette of text colors
+h1.addEventListener('mouseover', function(event){
+    h1.classList.toggle('logo-colors');
+    //need to remove class before next mouseover
+})
+
+//Toogles website dark palette by pressing '-'
+body.addEventListener('keydown', function(event){
+    const keyName = event.key;
+    if(keyName === '-'){
+        body.classList.toggle('dark-mode');
+    }
+    //Need to fix colors
+})
+
+sandBus.addEventListener('wheel', function(event){
+    sandBus.classList.toggle('.vrooom');
+})
+
