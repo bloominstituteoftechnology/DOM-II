bd1eeded840a2dbaef82a305ecd7d1e482b0aebc
leila <nb.leila10@gmail.com>
2018-11-03 14:23:55 -0500

    Stretch project
    When the mouse button is released, it should travel back to its original position.
    When a block is clicked, it should go to the top of the stack.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,13 +1,21 @@
-const blocks = document.querySelectorAll('.block');
+const blocks = document.querySelector('.blocks');
 
-// Variable to keep track of order, so block goes to top
-let order = 0;
-
-blocks.forEach(block => {
-  // Setting the click event listener on each block
-  block.addEventListener('click', function(event) {
-    order --;
-    // Make block rise to top
-    event.target.style.order = order;
-  });
+blocks.addEventListener('click', function(event) {
+  if(event.target === blocks) return;
+  blocks.prepend(event.target);
 });
+
+blocks.addEventListener('mousedown', function(event) {
+  if(event.target === blocks) return;
+  let position = 0;
+  const move = setInterval(function() {
+    position += 10;
+    if(position < 1000) {
+      TweenMax.to(event.target, 1, {x:position});
+    }
+      blocks.addEventListener('mouseup', function() {
+      TweenMax.to(event.target, 1, {x:-10});
+      clearInterval(move);
+    });
+  }, 10);
+})
