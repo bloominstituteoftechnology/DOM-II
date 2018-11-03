e1473f202f4f3c2ac45c697df28a6894831e5367
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2018-11-03 12:24:43 -0500

    Added mouseenter, mousedown, and mouseup event listeners.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 // Your code goes here
+window.addEventListener("load", function(event) {
+  alert("The page has now loaded");
+  });
+
 const button1 = document.querySelector('#btn1');
 button1.addEventListener('click', function(event){
   event.target.style.color = 'black';
@@ -12,4 +16,18 @@ button2.addEventListener('mouseover', function(event){
 const button3 = document.querySelector('#btn3');
 button3.addEventListener('dblclick', function(event){
   event.target.style.borderColor = 'red';
+});
+
+const img1 = document.querySelector('#img1');
+img1.addEventListener('mouseenter', function(event){
+  event.target.style.border = '2px dashed green';
+});
+
+const img4 = document.querySelector('#img4');
+img4.addEventListener('mousedown', function(event){
+  event.target.style.opacity = '0';
+});
+
+img4.addEventListener('mouseup', function(event){
+  event.target.style.opacity = '1';
 });
