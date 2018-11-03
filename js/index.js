5efb831a778b47f9701e321b76d8846eaca9dbe6
Andrew <andrewryanogle@gmail.com>
2018-11-03 09:21:55 -0700

    added eventListeners used foreach to loop through the links and preventDefault, and used greenSock to add animations to the page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+const timeLine = new TimelineMax();
+window.addEventListener("load", function (event) {
+    timeLine.staggerFrom('p', 0.5, {
+        opacity: 0,
+        cycle: {
+            rotationX: [-90, 90]
+        }
+    }, 0.1)
+});
+
+//animate fun bus image
+const introImage = document.querySelector('.intro img');
+introImage.addEventListener('mouseover', function (event) {
+    TweenLite.to(introImage, 2, { width: "200px" });
+
+    introImage.addEventListener('mouseleave', function (event) {
+
+        TweenLite.to(introImage, 2, { width: "100%" });
+
+    });
+
+
+});
+
+//preventDefault on nav items
+const norefreshing = document.querySelectorAll('.nav');
+norefreshing.forEach((navItem, index)=>{
+    navItem.addEventListener('click',function(event){
+        event.preventDefault();
+        console.log('default prevented');
+    })
+});
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach((button,idx)=>{
+    button.addEventListener('focus',(event)=>{
+        button.style.display = 'none';
+    })
+})
