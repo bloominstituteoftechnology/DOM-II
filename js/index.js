874e333efb2b111c8b168b02b75457c7c51f9413
Xavier Puentes <xavier@Xaviers-MacBook-Pro.local>
2018-11-03 12:45:51 -0500

    Added copy event listener; implemented stopPropagation

--- js/index.js
+++ js/index.js
@@ -6,6 +6,12 @@ window.addEventListener("load", function(event) {
 const button1 = document.querySelector('#btn1');
 button1.addEventListener('click', function(event){
   event.target.style.color = 'black';
+  event.stopPropagation();
+});
+
+const divClick = document.querySelector('#divBorder');
+divClick.addEventListener('click', function(event){
+  event.currentTarget.style.border = '1px solid red';
 });
 
 const button2 = document.querySelector('#btn2');
@@ -25,7 +31,7 @@ img1.addEventListener('mouseenter', function(event){
 
 img1.addEventListener('mouseleave', function(event){
   event.target.style.border = 'none';
-})
+});
 
 const img4 = document.querySelector('#img4');
 img4.addEventListener('mousedown', function(event){
@@ -34,4 +40,9 @@ img4.addEventListener('mousedown', function(event){
 
 img4.addEventListener('mouseup', function(event){
   event.target.style.opacity = '1';
+});
+
+const p = document.querySelector('p');
+p.addEventListener('copy', function(event){
+  alert('Why are you copying my paragraph!?')
 });
