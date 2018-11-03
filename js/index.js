c015ade53050d885e01de9154279bef3b4fc3f35
rberriz <rberriz@icloud.com>
2018-11-03 13:00:07 -0400

    final

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
 // Your code goes here
+const body = document.querySelector('body');
+
+document.addEventListener('keypress', function (event) {
+    body.classList.toggle('colorWheelToggle');
+});
+
+body.addEventListener('copy', function(event){
+    event.target.style.visibility = 'hidden';
+});
+
+body.addEventListener('wheel', function(event){
+    event.target.style.backgroundColor = 'red';
+});
+
+body.addEventListener('dblclick', function(event){
+    event.target.style.backgroundColor = 'green';
+});
+
+body.addEventListener('resize', function(event){
+    event.target.style.backgroundColor = 'yellow';
+});
+
+
+
+
+
+const images = document.querySelector('img');
+
+images[0].addEventListener('mouseenter', function(event) {
+    const str = event.target.width - 250;
+    event.target.style.width = `${str}px`.toString()
+});
+
+images[0].addEventListener('mouseleave', function(event) {
+    const str = event.target.width + 250;
+    event.target.style.width = `${str}px`.toString()
+});
+
+const headers = document.querySelectorAll('h2');
+
+headers.forEach(header => {
+    header.addEventListener('drag', function() {
+        header.classList.add('changeColor');
+    });
+});
+
+const buttons = document.querySelectorAll('.btn');
+
+buttons.addEventListener('dblclick', function(event) {
+    event.target.style.color = 'blue';
+})
