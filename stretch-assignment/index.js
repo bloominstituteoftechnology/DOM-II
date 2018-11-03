7021ed8dc8b0590aa3ca75628833f25435bdb334
seanwu20 <seanwu20@gmail.com>
2018-11-03 15:49:03 -0400

    Blocks now stop when mouse is released

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,18 +1,21 @@
 const block = document.querySelectorAll('.block');
 
 
-
-
-
 for (let i = 0; i < block.length; i++) {
     function motionFormula() {
         block[i].style.left = parseInt(block[i].style.left) + 3 + "px";
     }
+
     block[i].style.position = 'relative';
     block[i].style.left = '0px';
-    block[i].addEventListener("mousedown", function(event) {
-        setInterval(motionFormula,50)
-    })
+    block[i].addEventListener("mousedown", function (event) {
+        const motion = setInterval(motionFormula, 50);
+        window.addEventListener('mouseup', function (event) {
+            clearInterval(motion);
+        })
+    });
 }
+
+
 
 
