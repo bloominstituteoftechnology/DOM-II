b65d04c7438a2753636a357ee6fde4eed45ba9e3
Andrew <andrewryanogle@gmail.com>
2018-11-03 10:55:08 -0700

    finished adding event listeners

--- js/index.js
+++ js/index.js
@@ -25,23 +25,37 @@ introImage.addEventListener('mouseover', function (event) {
 
 //preventDefault on nav items
 const norefreshing = document.querySelectorAll('.nav');
-norefreshing.forEach((navItem, index)=>{
-    navItem.addEventListener('click',function(event){
+norefreshing.forEach((navItem, index) => {
+    navItem.addEventListener('click', function (event) {
         event.preventDefault();
         console.log('default prevented');
     })
 });
 const destination = document.querySelectorAll('.destination')
-destination.forEach((item,index)=>{
-    item.addEventListener('click',(event)=>{
+destination.forEach((item, index) => {
+    item.addEventListener('click', (event) => {
         item.style.backgroundColor = 'green';
     })
 })
 
 const buttons = document.querySelectorAll('.btn');
-buttons.forEach((button,idx)=>{
-    button.addEventListener('click',(event)=>{
+buttons.forEach((button, idx) => {
+    button.addEventListener('click', (event) => {
         button.style.display = 'none';
         event.stopPropagation();
     })
+})
+const body = document.querySelector('body');
+document.addEventListener('scroll', (event) => {
+    TweenMax.fromTo('body', 0.15, { x: -20 }, { x: 20, repeat: 5, yoyo: true, ease: Sine.easeInOut, onComplete: function () { TweenMax.to(this.target, 1.5, { x: 0, ease: Elastic.easeOut }) } })
+})
+document.addEventListener('keydown',(event)=>{
+    body.style.backgroundColor = 'black'
+
+    document.addEventListener('keyup',(event)=>{
+        body.style.backgroundColor = "white"
+    })
+})
+window.addEventListener('resize',(event)=>{
+    alert(`Honey, I've shrunk the document!`)
 })
