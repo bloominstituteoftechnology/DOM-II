edfd271025680e2c7584a95ab6e381acdde84105
Alex Bezruchko <alex.bezruchko@gmail.com>
2018-11-02 21:43:36 -0400

    add drag/drop  listener

--- js/index.js
+++ js/index.js
@@ -25,6 +25,12 @@ header.addEventListener('mouseover', (event) => {
 // });
 
 
+
+// Wheel listener
+
+// Show how much pixels people are scrolling through
+
+
 const counterNav = doc.createElement('span');
 counterNav.innerHTML = 0;
 // counterNav.style.cssFloat = 'right';
@@ -41,10 +47,46 @@ doc.querySelector('.main-navigation').append(counterDescription);
 doc.querySelector('.main-navigation').append(counterNav);
 
 
+// Drag and Drop
+
+// Asign elements to fill and empties constants
+
+const fill = doc.querySelector('.fill');
+const empties = doc.querySelectorAll('.empty');
+
+// Add listeners to the filled div
+fill.addEventListener('dragstart', dragStart);
+fill.addEventListener('dragend', dragEnd);
+
+// Run loop through empty div and add listeners with callback functions 
+for(const empty of empties) {
+    empty.addEventListener('dragover', dragOver);
+    empty.addEventListener('dragenter', dragEnter);
+    empty.addEventListener('dragleave', dragLeave);
+    empty.addEventListener('drop', dragDrop);
+}
+function dragStart() {
+    this.className += ' hold';
+    setTimeout(() => (this.className = 'invisible'), 0);
+}
 
 
+function dragEnd() {
+    this.className = 'fill';
+}
 
-
-
-
+function dragOver(e) {
+    e.preventDefault();
+}
+function dragEnter(e) {
+    e.preventDefault();
+    this.className += ' hovered';
+}
+function dragLeave() {
+    this.className = 'empty';
+}
+function dragDrop() {
+    this.className = 'empty';
+    this.append(fill);
+}
 
