9a4c2dc9890239e145291a4cb416c3fef7319dcd
Bill Geist <wcgeist@gmail.com>
2018-11-03 14:29:21 -0400

    stoppropagation

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 document.addEventListener('copy', function (event) {
     alert('you cannot copy that');
     event.preventDefault();
-    
+
 })
 
 const myNavLogo = document.querySelector('.logo-heading');
@@ -27,42 +27,44 @@ myNavLinks.addEventListener('mouseout', function (event) {
 const welcomeDiv = document.querySelector('.intro');
 welcomeDiv.addEventListener('dblclick', function (event) {
     event.target.classList.toggle('active');
- })
+})
 
- const welcomeHeader = document.querySelector('.intro h2');
- document.addEventListener('keydown', function (event) {
-     const keyName = event.key;
-     if (keyName === 'f') {
-         welcomeHeader.classList.toggle('active'); 
-     }
- })
+const welcomeHeader = document.querySelector('.intro h2');
+document.addEventListener('keydown', function (event) {
+    const keyName = event.key;
+    if (keyName === 'f') {
+        welcomeHeader.classList.toggle('active');
+    }
+})
 
 const welcomeText = document.querySelector('.intro p');
 welcomeText.addEventListener('dblclick', function (event) {
-   event.target.classList.toggle('active');
-   event.stopPropagation();
+    event.target.classList.toggle('active');
+    event.stopPropagation();
 })
 
 
 const introImg = document.querySelector('.intro img');
 introImg.addEventListener('drag', function (event) {
     event.target.style.border = "solid green 3rem";
- })
- introImg.addEventListener('dblclick', function (event) {
+})
+introImg.addEventListener('dblclick', function (event) {
     event.target.style.border = "none";
- })
+})
 
- const destImage = document.querySelector('.content-destination img');
- destImage.addEventListener('mousedown', function (event) {
+const destImage = document.querySelector('.content-destination img');
+destImage.addEventListener('mousedown', function (event) {
     event.target.classList.toggle('active');
- })
- destImage.addEventListener('mouseup', function (event) {
+})
+destImage.addEventListener('mouseup', function (event) {
     event.target.classList.toggle('active');
- })
+})
 
- const foot = document.querySelector('.footer');
- foot.addEventListener('mouseenter', function (event) {
+const foot = document.querySelector('.footer');
+foot.addEventListener('mouseenter', function (event) {
     event.target.classList.toggle('active');
- })
+})
 
- 
+
+
+
