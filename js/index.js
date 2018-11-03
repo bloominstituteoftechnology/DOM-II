5ca8243370eb79ba95678ec540f4e6285515a2c9
Eiosx <eiosxenzo@gmail.com>
2018-11-03 14:28:20 -0400

    Almost done, need to fix a couple of events.

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 const h1 = document.querySelector('.logo-heading');
 const body = document.querySelector('body');
 const sandBus = document.querySelector('.intro img');
+const button = document.querySelectorAll('.btn');
+const container = document.querySelector('.container');
+const anchors = document.querySelectorAll('a');
 
 //Hovering cursor over 'Fun Bus' title toggles a palette of text colors
 h1.addEventListener('mouseover', function(event){
@@ -9,7 +12,7 @@ h1.addEventListener('mouseover', function(event){
     //need to remove class before next mouseover
 })
 
-//Toogles website dark palette by pressing '-'
+//Toggles website dark palette by pressing '-'
 body.addEventListener('keydown', function(event){
     const keyName = event.key;
     if(keyName === '-'){
@@ -18,11 +21,48 @@ body.addEventListener('keydown', function(event){
     //Need to fix colors
 })
 
-//Bus img moves side to side
+//Bus img disappears
 sandBus.addEventListener('wheel', function(event){
-    sandBus.classList.toggle('.vrooom');
-    //fix, class is being toggled, but style isn't being added
+    sandBus.classList.toggle('vrooom');
 })
+
+//drag
+body.addEventListener('dragstart', function(event){
+    alert(`Don't drag me bro!`)
+})
+
+//After loading the page a msg pops up
+window.addEventListener('load', function(event){
+    alert('Loading Done! Click the button to continue!');
+})
+
+//focus
+// button.addEventListener('click',function(event){
+//     button.focus();
+// })
+// button.addEventListener('focus', function(event){
+//     alert("Pysche!!!!")
+// })
+
+//resize
+window.addEventListener('resize', function(event){
+    alert('Hello there!');
+})
+
+//scroll
+window.addEventListener('scroll', function(event){
+    anchors.forEach(function(anchor){
+        anchor.style.color = "blue";
+    })
+})
+
+//select
+// anchors.addEventListener('select', function(event){
+//     anchors.forEach(function(anchor){
+//         anchor.style.color = "black";
+//     })
+// })
+
 
 //Dblclicked text changes colors except for h1
 body.addEventListener('dblclick', function(event){
@@ -38,3 +78,4 @@ body.addEventListener('dblclick', function(event){
       }
 }
 })
+
