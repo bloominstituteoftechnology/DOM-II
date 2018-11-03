c35d8bb0c08660496d6d1c7d89145b15757b6d20
Jason Cook <the.root.user@gmail.com>
2018-11-03 10:54:27 -0400

    Call unnecessary, since array and called array were the same.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
 // Your code goes here
 
 const navItems = document.querySelectorAll('.nav-link');
-navItems.forEach.call(navItems,function(e){e.addEventListener('click', event => event.preventDefault())})
+navItems.forEach(e => {e.addEventListener('click', event => event.preventDefault())});
+
 const images = document.querySelectorAll('img');
 
-images.forEach.call(images,function(e){e.addEventListener('mouseenter', event => event.target.style.opacity = '0.8')});
-images.forEach.call(images,function(e){e.addEventListener('mouseleave', event => event.target.style.opacity = 'initial')});
-
-images.forEach.call(images,function(e){e.addEventListener('dragstart', event => event.target.classList.add('animateMe') )});
-images.forEach.call(images,function(e){e.addEventListener('dragend', event => event.target.classList.remove('animateMe') )});
+images.forEach(e => {e.addEventListener('mouseenter', event => event.target.style.opacity = '0.8')});
+images.forEach(e => {e.addEventListener('mouseleave', event => event.target.style.opacity = 'initial')});
+images.forEach(e => {e.addEventListener('dragstart', event => event.target.classList.add('animateMe') )});
+images.forEach(e => {e.addEventListener('dragend', event => event.target.classList.remove('animateMe') )});
 
 
 const anchors = document.querySelectorAll('a');
 
 const target = document.querySelector('h1');
 
-anchors.forEach.call(anchors,function(e){e.addEventListener('mouseover', event => ((Math.floor(Math.random()*10) + 1) % 2 === 0)? target.style.transform = 'rotate(180deg)' : target.style.transform = 'rotate(360deg)')});
+anchors.forEach(e => {e.addEventListener('mouseover', event => ((Math.floor(Math.random()*10) + 1) % 2 === 0)? target.style.transform = 'rotate(180deg)' : target.style.transform = 'rotate(360deg)')});
 
 document.addEventListener('keydown', function(event) {
     if (event) {
@@ -52,12 +52,12 @@ buttons.forEach.call(buttons,function(e){e.addEventListener('mouseup', event => 
     }
 )})
 
-destinations.forEach.call(destinations,function(e){e.addEventListener('mousedown', event => {
+destinations.forEach(e => {e.addEventListener('mousedown', event => {
     event.target.style.backgroundColor = '#ffd800'
     }
 )})
 
-destinations.forEach.call(destinations,function(e){e.addEventListener('mouseup', event => {
+destinations.forEach(e => {e.addEventListener('mouseup', event => {
     event.target.style.backgroundColor = 'initial'
     }
 )})
