04af1937f2f7eef871a999e292eba440357c396a
edreeseg <edward.reeseg@gmail.com>
2018-11-03 13:35:42 -0400

    stretch assignment basics

---
+++ stretch-assignment/index.js
@@ -0,0 +1,23 @@
+const blockParent = document.querySelector('.blocks');
+const blocks = document.querySelectorAll('.block');
+blockParent.addEventListener('click', (e) => {
+    if (e.target === blockParent) return;
+        const temp = e.target;
+        blockParent.removeChild(e.target);
+        blockParent.prepend(temp);
+});
+
+blockParent.addEventListener('mousedown', (e) => {
+    const timer = setTimeout(() => {
+        let position = 0;
+        const move = setInterval(() => {
+            position += 10;
+            e.target.style.transform = `translateX(${position}px)`;
+            blockParent.addEventListener('mouseup', () => {
+                e.target.style.transform = '';
+                clearInterval(move);
+            });
+        }, 50);
+    }, 500);
+    blockParent.addEventListener('mouseup', () => clearTimeout(timer));
+});
