8f5d155a1ed2b537aad2a425b18048fa8d9d2e0e
Melvin Merlos <melvinamerlos@gmail.com>
2018-11-04 13:09:09 -0500

    update 1: 3 unique events completed, 4 attempted, last 7 in prog.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
 // Your code goes here
+
+// mouseover --- unique event 
+const nav = document.querySelector('.main-navigation');
+nav.addEventListener('mouseover', function (event) {
+    event.target.style = `background-color: grey`
+})
+
+// keydown --- unique event 
+const pageTitle = document.querySelector('.logo-heading');
+document.addEventListener('keydown', function (event) {
+    let keyName = event.key
+    pageTitle.textContent = `key pressed: ${keyName}`
+})
+
+// wheel --- unique event 
+const centerImg = document.querySelector('.img-content');
+centerImg.addEventListener('wheel', function (event) {
+    console.log(event)
+})
+
+// drag / drop --- unique event 
+
+
+// load --- unique event 
+
+
+// focus --- unique event 
+
+
+// resize --- unique event 
+
+
+// scroll --- unique event 
+
+
+// select --- unique event 
+
+
+// dblclick --- unique event 
+const html = document.querySelector('.html');
+document.addEventListener('dblclick', function (event) {
+    event.target.style = `background-color: grey`
+})
+
