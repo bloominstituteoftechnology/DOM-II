ecda13d594f82771554a4d157d06cdc1b371ef55
Taylor Belk <belktaylor12@gmail.com>
2018-11-03 20:56:33 -0500

    add the remainder of the eventListeners in index.js

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@
 //  Stop the navigation from items from refreshing the page by using preventDefault()
 
 
-//-----------------------MouseOver
+// 1 and 2-----------------------MouseOver/mouseleave
 const navA = document.querySelectorAll('.nav-link');
 
 for (let i = 0; i < navA.length; i++) {
@@ -22,20 +22,76 @@ for (let i = 0; i < navA.length; i++) {
         });
       };
 
-//-----------------------keydown
+//3 and 4-----------------------keydown/keyup
 
-//-----------------------wheel
+const bgColor = document.querySelector('body');
 
-//-----------------------drag / drop
+bgColor.addEventListener('keydown', function () {
+    bgColor.style.backgroundColor = "lightblue";
+}); 
 
-//-----------------------load
+bgColor.addEventListener('keyup', function() {
+    bgColor.style.backgroundColor = 'white';
+})
 
-//-----------------------focus
 
-//-----------------------resize
 
-//-----------------------scroll
+//5-----------------------wheel
 
-//-----------------------select
+const changeHeadings = document.querySelectorAll('h2');
 
-//-----------------------dblclick
+for (let i = 0; i < changeHeadings.length; i++) {
+    changeHeadings[i].addEventListener('wheel', function () {
+        changeHeadings[i].style.color = 'red';
+      });
+    };
+
+//6-----------------------load
+
+window.addEventListener('load', function () {
+    alert('The page has loaded correctly')
+});
+
+
+//7-----------------------resize
+    window.addEventListener('resize', function() {
+        document.querySelector('.logo-heading').style.color = 'red';
+    });
+
+//8----------------------MouseOver
+
+const imagesA = document.getElementsByTagName('img');
+console.log(imagesA);
+
+for(let i = 0; i < imagesA.length; i++) {
+    imagesA[i].addEventListener('mouseover', function () {
+        imagesA[i].style.opacity = '0.5'
+    })
+};
+
+for(let i = 0; i < imagesA.length; i++) {
+    imagesA[i].addEventListener('mouseleave', function () {
+        imagesA[i].style.opacity = '1'
+    })
+};
+
+//9 ------------------copy
+
+const pText = document.querySelectorAll('p');
+
+for(let i = 0; i < pText.length; i++) {
+    pText[i].addEventListener('copy', function () {
+        pText[i].textContent = 'HA CAN\'T COPY ME!!'
+    })
+};
+
+
+//10---------------click
+
+const clickMessage = document.querySelectorAll('.btn');
+
+for(let i = 0; i < clickMessage.length; i++) {
+    clickMessage[i].addEventListener('click', function () {
+        alert('HEY! Don\'t click me!') 
+    })
+};
