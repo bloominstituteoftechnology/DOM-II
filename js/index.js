43a3ecc09e5aa5acb025013c90c4880405a368e4
Alando Appling <alandoappling@gmail.com>
2018-11-05 07:26:26 -0800

    js file

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+let rotateImg = document.querySelectorAll('img');
+
+rotateImg.forEach(x => x.addEventListener('mouseenter', function(event){
+    x.style.boxShadow = "12px 12px 2px 1px rgba(0, 0, 255, .2)"
+}))
+
+rotateImg.forEach(x => x.addEventListener('mouseleave', function(event){
+    x.style.boxShadow = "0px 0px 0px 0px rgba(0,0,0,0)"
+}))
+
+
+let page = window;
+page.addEventListener('scroll', function(event){
+    page.style.background = "blue"
+})
