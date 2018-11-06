2639b4c2ff80fe8f859ddce21ca1c450a6c803f7
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2018-11-05 16:40:47 -0800

    Rick Espinoza first PR for DOM-II

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// Your code goes here
+//Drag and drop navbar
+const navBar = document.querySelector('.main-navigation');
+navBar.addEventListener('dragstart', () => {
+    if(navBar.style.height = "90px"){
+        navBar.style.height = '250px';
+    }
+    navBar.addEventListener('dragend', () => {
+        if(navBar.style.height = '250px'){
+            navBar.style.height = "90px";
+        }
+    });
+});
+
+
+//Change background color to blue on keydown
+const background = document.querySelector('body');
+background.addEventListener('keydown', () => {
+    background.style.backgroundColor = "blue";
+});
+
+//purple background on resize
+window.addEventListener('resize', () => {
+    background.style.backgroundColor = 'blue';
+    window.addEventListener('resize', () => {
+        if(background.style.backgroundColor === "blue")
+        background.style.backgroundColor = "white";
+    });
+});
