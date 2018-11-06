ceb85958a27a6adcd2bbadaad08140bc75e04266
mustafa inaya <moose.inaya@icloud.com>
2018-11-05 19:00:55 -0800

    Implement, Implemented One Event Listner.
    Currently working on implementing the 10 event lisnteres. aiming to finish the MVP.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+//`dblclick`
+
+let dblclickEvent = document.getElementsByClassName("btn")
+for (let i = 0; dblclickEvent.lemgth; i++){
+    dblclickEvent[i].addEventlistener('dblclick', callback)
+}
