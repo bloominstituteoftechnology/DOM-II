5e8473b276b5f37c7dc9355e1157e069f865fcf0
Andrew Yurlov <yurlovandrew@gmail.com>
2018-11-05 16:51:09 -0800

    completed assignment

--- js/index.js
+++ js/index.js
@@ -1,81 +1,68 @@
-const logo = document.querySelector('.logo-heading');
-const nav = document.querySelectorAll('.nav-link');
-const header = document.querySelector('.main-navigation');
-const img = document.querySelector('.intro img');
-const h1 = document.querySelector('.logo-heading');
-const container = document.querySelector('.container');
-const btn = document.querySelectorAll('.btn');
-
 // Event 1
-logo.addEventListener('mouseover', (event) => {
+const logo = document.querySelector('.logo-heading').addEventListener('mouseover', (event) => {
   event.target.style.color = 'dodgerblue';
 });
 
 // Event 2
+const nav = document.querySelectorAll('.nav-link');
 for (let i = 0; i < nav.length; i++) {
   nav[i].addEventListener('focus', () => {
-    nav[i].preventDefault();
     nav[i].classList.toggle('toggle');
+    nav[i].preventDefault();
   });
 }
 
 // Event 3
-header.addEventListener('keydown', (event) => {
-  function random() {
-    let letters = '0123456789ABCDEF';
-    let color = '#';
-
-    for (let i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
-
+window.addEventListener('keydown', (event) => {
   const keyName = event.key;
   if (keyName === 'Enter') {
-    event.target.style.color = random();
+    document.querySelector('.nav').style.backgroundColor = 'red';
   }
 });
 
 // Event 4
-img.addEventListener('mouseenter', () => {
+document.querySelector('.intro img').addEventListener('mouseenter', () => {
   img.classList.toggle('imgToggle');
 });
 
 // Event 5
-h1.addEventListener('wheel', (event) => {
-  event.target.style.fontSize = '10px';
+window.addEventListener('scroll', () => {
+  const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+  if (scrolled === scrollable) {
+    alert('You reached the bottom');
+  }
 });
 
 // Event 6
-container.addEventListener('load', (event) => {
-  event.target.style.color = 'grey';
+window.addEventListener('load', () => {
+  alert('Website has loaded');
 });
 
 // Event 7
-nav.forEach(nav => {
+document.querySelectorAll('.nav-link').forEach(nav => {
   nav.addEventListener('dblclick', (event) => {
     event.target.style.color = 'red';
   });
 });
 
 // Event 8
-btn.forEach(b => {
+document.querySelectorAll('.btn').forEach(b => {
   b.addEventListener('click', (event) => {
-    event.target.style.color = 'green';
+    event.target.style.color = 'white';
   });
 });
 
 // Event 9
-btn.forEach(b => {
-  b.addEventListener('focusin', (event) => {
-    event.target.style.color = 'magenta';
+document.querySelectorAll('.btn').forEach(b => {
+  b.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'black';
   });
 });
 
 // Event 10
-btn.forEach(b => {
-  b.addEventListener('focusout', (event) => {
-    event.target.style.color = 'white';
+document.querySelectorAll('.btn').forEach(b => {
+  b.addEventListener('mouseout', (event) => {
+    event.target.style.backgroundColor = 'dodgerblue';
   });
 });
