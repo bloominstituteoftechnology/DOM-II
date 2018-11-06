a11ce70d099c261b766d4302b0752388aa5b424a
combatpacifist <35746304+combatpacifist@users.noreply.github.com>
2018-11-05 17:06:31 -0800

    added more mouse events

--- js/index.js
+++ js/index.js
@@ -14,15 +14,35 @@ navBar.addEventListener('dragstart', () => {
 
 //Change background color to blue on keydown
 const background = document.querySelector('body');
-background.addEventListener('keydown', () => {
-    background.style.backgroundColor = "blue";
+    background.addEventListener('keydown', () => {
+        background.style.backgroundColor = "blue";
 });
 
 //purple background on resize
 window.addEventListener('resize', () => {
-    background.style.backgroundColor = 'blue';
-    window.addEventListener('resize', () => {
-        if(background.style.backgroundColor === "blue")
-        background.style.backgroundColor = "white";
+    background.style.backgroundColor = 'purple';
+        window.addEventListener('resize', () => {
+            if(background.style.backgroundColor === "purple")
+                background.style.backgroundColor = "white";
+    });
+});
+
+
+//Expand banner on mouseover
+const banner = document.querySelector('.intro img');
+    banner.addEventListener('mouseover', () => {
+        banner.style.height = "20%";
+    
+//Bring back Back default banner
+banner.addEventListener('mouseleave', () => {
+    banner.style.height = "auto";
+    });
+});
+//Hide banner on double click
+banner.addEventListener('dblclick', () => {
+    banner.style.display = "none";
+//but show up on keypress pr scroll
+        window.addEventListener('scroll', () => {
+            banner.style.display = "block";
     });
 });
