278a6c670833f2e8fa3e69ca4bff35a23341309a
mustafa inaya <moose.inaya@icloud.com>
2018-11-06 17:39:33 -0800

    IMPLEMENT. Implemented Event Listeners Mixed With Animation.
    I am half way through implementing the 10 events, have not yet gotten through green sock.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,58 @@
 
 //`dblclick`
 
-let dblclickEvent = document.getElementsByClassName("btn")
-for (let i = 0; dblclickEvent.lemgth; i++){
-    dblclickEvent[i].addEventlistener('dblclick', callback)
-}
+let dblclickEvent = document.querySelector(".btn")
+    dblclickEvent.addEventListener('dblclick', function(event){
+        event.stopPropagation()
+        event.target.style.color = "green"
+        console.log(event.target)
+    })
+    
+// mouseover 
+
+    let mouseoverEvent = document.querySelector(".intro")
+    mouseoverEvent.addEventListener('mouseover', function(event){
+        event.stopPropagation()
+        event.target.style.color = "red"
+        console.log(event.target)
+    })
+
+// keydown 
+
+let keydownEvent = document.getElementsByClassName("text-content")[0]
+keydownEvent.addEventListener('keydown', function (event){
+    event.stopPropagation()
+    event.currentTarget.style.backgroundColor = "green"
+    console.log(event.currentTarget)
+})
+
+// wheel
+
+let containerHome = document.getElementsByClassName("container home")[0]
+let fontsize = 6
+containerHome.addEventListener('wheel', function (event){
+    if (event.deltaY < 0 ){
+        fontsize++
+    }
+    else{
+        fontsize--;
+    }
+    event.currentTarget.style.fontsize = `${fontsize}rem`
+})
+
+// `load`
+
+
+// nesting two events
+
+let myClass = document.getElementsByClassName("content-destination")[0]
+myClass.getElementsByTagName("h2")[0].addEventListener('click', function(event){
+    event.stopPropagation()
+    event.currentTarget.style.backgroundColor = "grey"
+})
+
+
+
+
+
+
