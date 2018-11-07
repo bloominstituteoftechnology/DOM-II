c1fc611bd89ff62b9cc9503b991109380034e368
Alando Appling <alandoappling@gmail.com>
2018-11-06 19:37:11 -0800

    Finished MVP

--- js/index.js
+++ js/index.js
@@ -2,15 +2,50 @@
 let rotateImg = document.querySelectorAll('img');
 
 rotateImg.forEach(x => x.addEventListener('mouseenter', function(event){
-    x.style.boxShadow = "12px 12px 2px 1px rgba(0, 0, 255, .2)"
+    x.style.boxShadow = "12px 12px 2px 1px black"
 }))
 
 rotateImg.forEach(x => x.addEventListener('mouseleave', function(event){
-    x.style.boxShadow = "0px 0px 0px 0px rgba(0,0,0,0)"
+    x.style.boxShadow = "0px 0px 0px 0px black"
 }))
 
+window.addEventListener('wheel', function(event){
+    let pageBody = document.getElementsByTagName('body')
+    pageBody[0].style.background = "blue"
+});
 
-let page = window;
-page.addEventListener('scroll', function(event){
-    page.style.background = "blue"
-})
+let paragraphs = document.querySelectorAll('p')
+for(let i = 0; i < paragraphs.length; i++){
+    paragraphs[i].addEventListener('mouseover',function(){
+        paragraphs[i].style.fontSize = "30px"
+        paragraphs[i].style.color = "white"
+    });
+    paragraphs[i].addEventListener('mouseout', function(){
+        paragraphs[i].style.fontSize = "25px"
+    })
+}
+
+window.addEventListener("load", function(event) {
+    alert("All resources finished loading!");
+});
+
+let navLinks = document.querySelectorAll('a');
+
+navLinks.forEach(x => x.addEventListener('dblclick', function(){
+    x.style.color = "blue"
+}))
+
+let sectionBorder = document.querySelectorAll('section');
+sectionBorder.forEach(x => x.addEventListener('mouseover', function(){
+    x.style.borderBottomColor = "black"
+}));
+
+let selectedText = document.querySelectorAll('h2')
+selectedText.forEach(x => x.addEventListener('mouseup', function(){
+    alert("Stop That!")
+}))
+
+let imgGreenSock = document.querySelectorAll("img");
+imgGreenSock.forEach(x => x.addEventListener('load', function(){
+    TweenLite.to(x, 2, {left:"542px", backgroundColor:"black", borderBottomColor:"#90e500", color:"white"});
+}));
