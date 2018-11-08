d3d46ac3e17fa9fcd2cdb739dc180cc191e3818f
ltann <leeroyleeroy18@gmail.com>
2018-11-07 21:45:27 -0800

    finished event listener- wheel

--- js/index.js
+++ js/index.js
@@ -4,12 +4,20 @@
 const logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener('mouseover', (event) => {
     console.log('change color')
-    logoHeading.classList.toggle('change-color')
+    logoHeading.classList.toggle('change-color');
 });
 
 //Keydown
-document.querySelector('body').addEventListener('keydown', myFunction)
+document.querySelector('body').addEventListener('keydown', myKeydownFunc);
 
-function myFunction() {
-    alert('UH-OH, you pressed a button!')
+function myKeydownFunc() {
+    alert('UH-OH, you pressed a button!');
+}
+
+
+//Wheel
+document.querySelector('.home').addEventListener('wheel', myWheelFunc);
+
+function myWheelFunc() {
+    this.style.color = 'dodgerblue';
 }
