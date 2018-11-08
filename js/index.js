cc2c3fb28544b2f5b3e8b2782f1f93de17974ada
ltann <leeroyleeroy18@gmail.com>
2018-11-07 20:43:52 -0800

    finished event listener- mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+//Mouseover
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', (event) => {
+    console.log('change color')
+    logoHeading.classList.toggle('change-color')
+});
+
+//Keydown
