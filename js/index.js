5ed2d948ac760a36ddcb21f5aa4f6f9eab1e5bcc
Andrew <acarmstrong98@gmail.com>
2018-11-10 14:06:00 -0500

    implemented listeners for click, mouseOver, wheel, and load!

--- js/index.js
+++ js/index.js
@@ -1,1 +1,42 @@
 // Your code goes here
+
+//click
+const logo = document.querySelector('.logo-heading');
+logo.style.cursor = 'pointer';
+logo.addEventListener('click', () => logo.style.color = logo.style.color === '#F9CD81' ? 'black': '#F9CD81');
+
+//mouseOver
+const headerPicture = document.querySelector('.intro img');
+    headerPicture.addEventListener('mouseover', () => headerPicture.style.borderRadius = '100px');
+    headerPicture.addEventListener('mouseout', () => headerPicture.style.borderRadius = '20px');
+	
+//keyDown
+
+
+//wheel
+document.querySelector('.home').addEventListener('wheel', wheel);
+ function wheel() {
+    this.style.color = 'teal';
+}
+
+//drag/drop
+
+
+//load
+window.addEventListener('load', function(event) {
+    alert('What are you waiting for?! Start your vaca today!');
+ })
+
+//focus
+
+
+//resize
+
+
+//scroll
+
+
+//select
+
+
+//doubleClick
