ea486cbaf64a037599b71a0c8de0dba8c4901bff
derekjones-io <42226382+derekjones-io@users.noreply.github.com>
2018-11-13 13:22:46 -0500

    Add keydown and click functionality
    Keydown will change background image of body to sky blue, while
    clicking on the 'Sign Me Up!' buttons will change the destination
    image to one reflecting the header of the button. Clicking on the
    changed destination button will return it back to the original image.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-// Your code goes here
+// 1. keydown
+let body = document.querySelector('body');
+body.addEventListener('keydown', function() {
+	this.style.backgroundColor = 'skyblue';
+});
+
+// 2. click
+let destinationPhoto = document.querySelector('.content-destination img');
+let btn = document.querySelectorAll('.btn');
+console.log(btn);
+
+btn[0].addEventListener('click', function() {
+	destinationPhoto.src = 'img/fun-in-the-sun.jpg';
+	destinationPhoto.style.width = '600px';
+	destinationPhoto.style.height = '200px';
+	event.stopPropagation();
+});
+
+btn[1].addEventListener('click', function() {
+	destinationPhoto.src = 'img/mountain-excursion.jpg';
+	destinationPhoto.style.width = '600px';
+	destinationPhoto.style.height = '200px';
+	event.stopPropagation();
+});
+
+btn[2].addEventListener('click', function() {
+	destinationPhoto.src = 'img/island-getaway.jpg';
+	destinationPhoto.style.width = '600px';
+	destinationPhoto.style.height = '200px';
+	event.stopPropagation();
+});
+
+body.addEventListener('click', function() {
+	destinationPhoto.src = 'img/destination.jpg';
+});
