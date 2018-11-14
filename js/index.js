4653549ce591ba219baa326df3349e581d5bde50
amadison17 <angelmadison2015@gmail.com>
2018-11-13 19:29:45 -0500

    index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+document.getElementsByClassName(".nav-link");
+document.getElementsByClassName(".nav-link").addEventListener("mouseover", mouseOver);
+document.getElementsByClassName(".nav-link").addEventListener("mouseout", mouseOut);
+function mouseOver() {
+    document.getElementsByClassName(".nav-link").style.color = "orange";
+}
+function mouseOut() {
+    document.getElementsByClassName(".nav-link").style.color = "blue";
+}
+
+
+
+
+
+
+
+
