cd99f553a5ab4d5c014facc16bcd8751617d8eb7
derekjones-io <42226382+derekjones-io@users.noreply.github.com>
2018-11-13 19:24:36 -0500

    Add wheel functionality for random background colors

--- js/index.js
+++ js/index.js
@@ -1,35 +1,42 @@
-// 1. keydown
+//variables
 let body = document.querySelector('body');
+let destinationImg = document.querySelector('.content-destination img');
+let btn = document.querySelectorAll('.btn');
+
+// 1. keydown
 body.addEventListener('keydown', function() {
 	this.style.backgroundColor = 'skyblue';
 });
 
 // 2. click
-let destinationPhoto = document.querySelector('.content-destination img');
-let btn = document.querySelectorAll('.btn');
-console.log(btn);
-
 btn[0].addEventListener('click', function() {
-	destinationPhoto.src = 'img/fun-in-the-sun.jpg';
-	destinationPhoto.style.width = '600px';
-	destinationPhoto.style.height = '200px';
+	destinationImg.src = 'img/fun-in-the-sun.jpg';
+	destinationImg.style.width = '600px';
+	destinationImg.style.height = '200px';
 	event.stopPropagation();
 });
 
 btn[1].addEventListener('click', function() {
-	destinationPhoto.src = 'img/mountain-excursion.jpg';
-	destinationPhoto.style.width = '600px';
-	destinationPhoto.style.height = '200px';
+	destinationImg.src = 'img/mountain-excursion.jpg';
+	destinationImg.style.width = '600px';
+	destinationImg.style.height = '200px';
 	event.stopPropagation();
 });
 
 btn[2].addEventListener('click', function() {
-	destinationPhoto.src = 'img/island-getaway.jpg';
-	destinationPhoto.style.width = '600px';
-	destinationPhoto.style.height = '200px';
+	destinationImg.src = 'img/island-getaway.jpg';
+	destinationImg.style.width = '600px';
+	destinationImg.style.height = '200px';
 	event.stopPropagation();
 });
 
 body.addEventListener('click', function() {
-	destinationPhoto.src = 'img/destination.jpg';
+	destinationImg.src = 'img/destination.jpg';
+});
+
+// 3. wheel
+body.addEventListener('wheel', function() {
+	const colors = ['skyblue', 'goldenrod', 'yellow', 'dodgerblue'];
+	let random = Math.floor(Math.random() * 4);
+	this.style.backgroundColor = colors[random];
 });
