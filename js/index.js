36fac8e733e17478323dd9685460a8812e1e2091
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2018-12-04 17:10:49 -0500

    Added fourth event listener (img skew)

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const logoHeading = document.querySelector(".logo-heading");
 const introImg = document.querySelector("header img");
 const buttons = document.querySelectorAll(".btn");
 const contentPick = document.querySelector(".content-pick");
+const allImgs = document.querySelectorAll("img");
+
 
 window.addEventListener("load", function(e) {
     console.log("All resources finished loading!");
@@ -26,9 +28,18 @@ buttons.forEach((btn) => {
 buttons.forEach((btn) => {
     btn.addEventListener("click", function(e) {
         contentPick.style.flexWrap = "wrap";
-        
-        const newP = document.createElement("p");
-        newP.textContent = "You picked Fun in The Sun!";
-        contentPick.appendChild(newP);
+
+        const newH1 = document.createElement("h1");
+        newH1.textContent = "You picked a great option!";
+        newH1.style.padding = "30px 0";
+        newH1.style.margin = "0 auto";
+        newH1.style.textAlign = "center";
+        contentPick.appendChild(newH1);
+    })
+})
+
+allImgs.forEach((img) => {
+    img.addEventListener("dblclick", function(e) {
+        e.target.style.transform = "skewX(-15deg)";
     })
 })
