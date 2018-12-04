ea7de595fdd30ffec9670a6c3d7596d22714888d
Nathan Thomas <nwthomas@me.com>
2018-12-04 12:39:49 -0800

    Add .border() mixing and event to logo

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 // ============= Variables
+const headerLogo = document.querySelector(".logo-heading");
 
 // ============= Functions
 
 // ============= Event Listeners
+headerLogo.addEventListener("mouseover", function() {
+  headerLogo.style.fontSize = "7rem";
+  headerLogo.style.fontWeight = "500";
+  headerLogo.style.opacity = 0.5;
+});
