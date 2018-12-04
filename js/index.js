a957c78a6755f393124c1687ef72ab96a0433c95
RachelKolk <rach.m.obrien@gmail.com>
2018-12-04 16:20:24 -0500

    working on links with event listeners having issues

--- js/index.js
+++ js/index.js
@@ -5,15 +5,21 @@ pageHeader.addEventListener('mouseenter', function(e) {
 })
 
 
-// const links = document.querySelectorAll('a');
-// links.addEventListener('mouseover', function(e) {
-//     e.stopPropagation();
-//     for (let i = 0; i < links.length; i++) {
-//         links[i].style.color = 'white';
-//     }
-// })
+const links = document.querySelectorAll('nav-links');
+links.removeEventListener('mouseenter', function(e) {
+    pageHeader.style.backgroundColor = '#17A2B8';
+})
+links.addEventListener('mouseover', function(e) {
+    e.stopPropagation();
+    // for (let i = 0; i < links.length; i++) {
+    //     links[i].style.color = 'white';
+    // }
+    links[0].style.color = 'white';
+})
+
 
 const busPic = document.querySelector('.bus-pic');
 busPic.addEventListener('click', function(e) {
     busPic.src = 'img/photo-1464851707681-f9d5fdaccea8.jpeg'
 })
+
