b0851417a1c9bcdd1a668b98b4be443e02152bb5
cori-peck <cori.peck@ymail.com>
2018-12-04 15:14:55 -0600

    Completed One Event Listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+// 1. Mouse Move = Random Nav Colors
+const navAnima = document.querySelector('body');
+const navItems = document.querySelectorAll('a');
+
+navAnima.addEventListener('mousemove', random);
+function random() {
+    navItems.forEach(item => item.style.color = getColor());
+}
+function getColor() {
+    const charactrs = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += charactrs[Math.floor(Math.random() * 16)];
+    } 
+    return color;
+}
+
+
+//2. 
