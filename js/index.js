01dc5c4c2e0cdd9ee9ff24fe4d7497d91037fccb
Sheila <s.hopper01110@gmail.com>
2018-12-04 17:52:38 -0500

    Resizing

--- js/index.js
+++ js/index.js
@@ -5,9 +5,10 @@
 const navLinks = document.querySelector('.nav');
 
 
+
 // =============== MOUSEOVER ============ //
 
-navLinks.addEventListener('mouseover', function(e) {
+navLinks.addEventListener('mouseover', (e) => {
     e.target.style.color= "#411f9e";
     setTimeout(function() {
         e.target.style.color = "";
@@ -28,22 +29,43 @@ document.addEventListener('keydown', (e) => {
 
 
 
-// ================ DRAG/DROP ========= //
+// ================ DRAG START ========= //
+
 
 document.addEventListener("dragstart", (e) => {
-     dragged = e.target;
-   console.log ('Drag me')
+    dragged = e.target;
+    console.log ('You dragged me!')
     
-    e.target.style.opacity = .5;
+e.target.style.opacity = .5;
 }, false);
 
 // ================ LOAD =============== //
 
+window.addEventListener("load", (e) => {
+    console.log("Loading -> complete.");
+});
 
+
+// ================ FOCUS ============== //
+
+const introPic = document.querySelector(".intro img");
+introPic.addEventListener('focus', (e) =>  {
+      e.target.style.background = "black";    
+    }, true);
+    introPic.addEventListener("blur", (e) => {
+          e.target.style.background = "";    
+        }, true);
+        
 
 
 
 // ================ RESIZE ============== //
+
+
+
+
+
+
 
 
 
