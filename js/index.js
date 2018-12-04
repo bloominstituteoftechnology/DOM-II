c1c9676aaccebb6e25c75adfbf3d311a205938cf
Brandon <Blent1050@yahoo.com>
2018-12-04 15:39:17 -0500

    Added greensock animations

--- js/index.js
+++ js/index.js
@@ -15,15 +15,34 @@ navTags[2].addEventListener('dblclick', (event) => {
     navTags[2].classList.toggle("makeRed");
 });
 
+
+
+
+
+//Greensock
 const buttons = document.querySelectorAll('.btn');
 
+TweenMax.staggerFrom(".btn", 1, {
+    x: -300,
+    opacity: 0
+}, -0.2);
+
 buttons.forEach((element) => {
-    element.addEventListener('click',(event) => {
-        TweenMax.to(element, 2, {
-            backgroundColor: 'red'
+    element.addEventListener('mouseenter',(event) => {
+        TweenMax.to(element, 1, {
+            scale: 1.1,
+            backgroundColor: 'rgb(45, 97, 105)',
+            color: 'white'
+            
         });
     });
-
+    element.addEventListener('mouseout',(event) => {
+        console.log('mouse exited!');
+        TweenMax.to(element, 1, {
+            scale:1,
+            backgroundColor: '#17A2B8'
+        });
+    });
 });
 
 
