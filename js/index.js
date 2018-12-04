166e1b529eae622eff0fdfa2b9b2f1ddcdf619d6
Matt Basile <matthewbasile2@gmail.com>
2018-12-04 15:40:50 -0500

    Added color on mouseover and mouseout to NavLinks. LINES: index.js 1-15.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+// #1 MouseOver Dynamic Nav Tags
+const navLinks = document.querySelectorAll('a');
+console.log(navLinks);
+
+navLinks.forEach((navLink)=>{
+    navLink.addEventListener('mouseover', ()=>{
+        navLink.style.color="blue";
+    }
+)});
+navLinks.forEach((navLink)=>{
+    navLink.addEventListener('mouseout', ()=>{
+        navLink.style.color="black";
+    }
+)});
