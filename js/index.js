7a4f829502e127e9c5cfde7fc54dd5ef1e5b664d
croy <roy.christie95@gmail.com>
2018-12-04 16:09:06 -0500

    Added click and double click event listeners to h2s

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Your code goes here
 
+// Button Animations
+
 buttonAnim = Array.from(document.querySelectorAll(".btn"));
 
 buttonAnim[0].addEventListener("mouseover", function(event){
@@ -11,7 +13,7 @@ buttonAnim[0].addEventListener("mouseout", function(event){
     event.target.textContent = "Sign Me Up!";
   })
 
-  buttonAnim[1].addEventListener("mouseover", function(event){
+buttonAnim[1].addEventListener("mouseover", function(event){
     event.target.textContent = "Click For Fun!";
     event.target.style.textAlign = "center";  
   });
@@ -20,7 +22,7 @@ buttonAnim[1].addEventListener("mouseout", function(event){
     event.target.textContent = "Sign Me Up!";
   })
 
-  buttonAnim[2].addEventListener("mouseover", function(event){
+buttonAnim[2].addEventListener("mouseover", function(event){
     event.target.textContent = "Click For Fun!";
     event.target.style.textAlign = "center";  
   });
@@ -28,3 +30,32 @@ buttonAnim[1].addEventListener("mouseout", function(event){
 buttonAnim[2].addEventListener("mouseout", function(event){
     event.target.textContent = "Sign Me Up!";
   })
+
+  // Header Color Change
+
+const headerTwos = Array.from(document.querySelectorAll("h2"));
+headerTwos[0].addEventListener("click", function(event){
+    event.target.style.color = "red";
+});
+headerTwos[1].addEventListener("click", function(event){
+    event.target.style.color = "red";
+});
+headerTwos[2].addEventListener("click", function(event){
+    event.target.style.color = "red";
+});
+headerTwos[3].addEventListener("click", function(event){
+    event.target.style.color = "red";
+});
+
+headerTwos[0].addEventListener("dblclick", function(event){
+    event.target.style.color = "black";
+});
+headerTwos[1].addEventListener("dblclick", function(event){
+    event.target.style.color = "black";
+});
+headerTwos[2].addEventListener("dblclick", function(event){
+    event.target.style.color = "black";
+});
+headerTwos[3].addEventListener("dblclick", function(event){
+    event.target.style.color = "black";
+});
