2d7d8c25ff9e8cfecd43efc0863f1a205575245c
joepound <14357797+joepound@users.noreply.github.com>
2018-12-04 13:47:18 -0700

    Added a loading screen (load event)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+// Clearing up elements for loading screen
+// ---------------------------------------
+
+let containers = Array.from(document.querySelectorAll("body > *")).slice(1);
+containers.forEach(element => element.style.display ="none");
+
+document.body.style.backgroundColor = "#201E1E";
+
+// When the page finally loads
+window.addEventListener("load", function() {
+  let loadScreen = document.querySelector("#loading-container");
+  document.body.removeChild(loadScreen);
+  document.body.removeAttribute("style");
+  containers.forEach(element => element.removeAttribute("style"));
+});
