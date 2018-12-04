9deb73e7cca9f6eacfe158aea5fab37c2e6f1c1d
Olivia Osborn <olioz95@gmail.com>
2018-12-04 16:19:42 -0500

    stopped nav items from refreshing page, and made two event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+
+//Stop the navigation from items from refreshing the page by using `preventDefault()`
+const aTags = document.querySelectorAll('a')
+for (i=0; i<aTags.length; i++) {
+    aTags[0].addEventListener("click", function(e) {
+        e.preventDefault()
+    })
+}
+
+//make logo turn red with mouseover 
+const logo = document.querySelector(".logo-heading")
+logo.addEventListener("mouseover", function(e) {
+    logo.style.color = "red";
+})
+
+//
+// logo.style.fontSize = logoSize
+logo.addEventListener("click", function(e) {
+     logo.style.fontSize = "10rem"
+})
