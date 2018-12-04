c59606e554d480d370353f7bdb77d72226f90e47
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2018-12-04 16:15:56 -0600

    Added more event listeners. Added dblclick, scroll, resize and click events

--- js/index.js
+++ js/index.js
@@ -20,8 +20,53 @@ const home = document.querySelector(".home");
 
 document.addEventListener("keydown", (e) => {
     const keyName = e.key;
-    if(keyName){
+    if(keyName != "Escape"){
         home.style.opacity = ".5";
+    } else { //resets the opacity back to default with the esc key
+        home.style.opacity = "";
     }
 });
- 
+
+// Click event listener
+
+const bus = document.querySelector("img");
+
+//Adds text whenever the bus img is clicked on
+bus.addEventListener("click", (e) => {
+    e.stopPropagation();
+
+    const text = document.createElement("h2");
+    text.textContent = "Wow! Now try pressing any key! Can also press escape to cancel the effect!";
+
+    const header = document.querySelector(".intro");
+    header.appendChild(text);
+}, { once: true });
+
+
+// Scroll event listener
+
+const images = document.querySelectorAll("img")
+//Scrolling brings back the images
+window.addEventListener("scroll", (e) => {
+    images.forEach((currentvalue) => {
+        currentvalue.style.display = "";
+    });
+});
+
+
+// Resize event listener
+
+//Resizing makes all images display none
+window.addEventListener("resize", (e) => {
+    images.forEach((currentvalue) => {
+        currentvalue.style.display = "none";
+    });
+});
+
+//Double click event listener 
+
+const letsGo = document.querySelector(".content-section .text-content h2");
+
+letsGo.addEventListener("dblclick", (e) => {
+    e.target.style.cssText = "font-size : 10rem";
+}); 
