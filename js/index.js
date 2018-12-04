106a7da19c37de3f7fbd2e70339ee556f0e73704
ChelseaTolnai <ChelseaTolnai@gmail.com>
2018-12-04 17:19:01 -0500

    Task 3: Added additional event listeners, 8 in total so far.

--- js/index.js
+++ js/index.js
@@ -4,19 +4,19 @@ let navAnchors = document.querySelectorAll('.nav-link');
 let nav = document.querySelector('.main-navigation');
 
 navAnchors.forEach(function(index, i){
-    navAnchors[i].addEventListener('mouseover', function(e) {
+    navAnchors[i].addEventListener('mouseenter', function(e) {
         e.target.style.color = '#fcc100';
         e.target.style.textShadow= '1px 1px 1px black';
         e.target.style.textDecoration = 'underline';
     });
 
-    navAnchors[i].addEventListener('mouseout', function(e) {
+    navAnchors[i].addEventListener('mouseleave', function(e) {
         e.target.style.color = '';
         e.target.style.textShadow= '';
         e.target.style.textDecoration = 'none';
     });
 
-    navAnchors[i].addEventListener("click", function(e){
+    navAnchors[i].addEventListener('click', function(e){
         e.preventDefault();
     });
 });
@@ -31,6 +31,35 @@ window.addEventListener('scroll', function(e) {
 
 
 // ===== MAIN CONTENT =====
+
+// === Intro Section ===
+
+let intro = document.querySelector('.intro');
+
+intro.addEventListener('mouseover', function(e){
+    e.target.style.fontWeight = '900';
+})
+
+intro.addEventListener('mouseout', function(e){
+    e.target.style.fontWeight = '';
+})
+
+
+// === Destination Section ===
+
+let button = document.querySelectorAll('.btn');
+
+button.forEach(function(index, i){
+    button[i].addEventListener('click', function(e) {
+        e.target.textContent = 'You\'re Signed Up!';
+    });
+});
+
+button.forEach(function(index, i){
+    button[i].addEventListener('dblclick', function(e) {
+        e.target.textContent = 'You\'re NOT Signed Up!';
+    });
+});
 
 const destination = document.querySelectorAll('.destination');
 
@@ -51,18 +80,3 @@ dir2.style.fontSize = '1.2em';
 destination[1].append(dir0);
 destination[1].append(dir1);
 destination[1].append(dir2);
-
-
-let button = document.querySelectorAll('.btn');
-
-button.forEach(function(index, i){
-    button[i].addEventListener('click', function(e) {
-        e.target.textContent = 'You\'re Signed Up!';
-    });
-});
-
-button.forEach(function(index, i){
-    button[i].addEventListener('dblclick', function(e) {
-        e.target.textContent = 'You\'re NOT Signed Up!';
-    });
-});
