1472f75329a938a4c0263f14ad0e1be806f6c4d1
samjeffcoat <samjeffcoat@gmail.com>
2018-12-04 15:57:16 -0600

    added some clicks and double clicks

--- js/index.js
+++ js/index.js
@@ -1,1 +1,58 @@
 // Your code goes here
+//Mouseover
+
+const aTag =document.querySelectorAll("a");
+const img = document.querySelectorAll(".intro");
+const h1 = document.querySelector("h1");
+
+h1.addEventListener('dblclick', function (e) {
+    e.preventDefault();
+    e.target.style.color ='purple';
+});
+
+aTag[0].addEventListener('click', function (e) {
+    e.preventDefault();
+    e.target.style.color='blue';
+    console.log ("Home Fited");
+});
+
+
+
+aTag[1].addEventListener('click', function (e) {
+    e.preventDefault();
+    e.target.style.color= 'red';
+    console.log ("did this work?");
+})
+
+aTag[2].addEventListener('click', function (e) {
+    e.preventDefault();
+    e.target.style.color= 'green';
+    console.log ("did this work?");
+})
+aTag[3].addEventListener('click', function (e) {
+    e.preventDefault();
+    e.target.style.color= 'orange';
+    console.log ("did this work?");
+})
+
+
+
+
+
+
+
+
+
+
+/*
+
+ keydown
+ wheel
+ drag / drop
+ load
+ focus
+ resize
+ scroll
+ select
+ dblclick
+ */
