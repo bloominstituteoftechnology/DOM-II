e322d0dff8f14526782b175a4946163060f01441
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2018-12-04 15:02:05 -0600

    Added an eventlistener for if a key is pressed

--- js/index.js
+++ js/index.js
@@ -6,11 +6,22 @@
 const nav = document.querySelectorAll(".nav-link");
 
 nav.forEach((currentvalue) => {
-    currentvalue.addEventListener('mouseover', function(e) {
+    currentvalue.addEventListener("mouseover", (e) => {
         currentvalue.style.color = "red";
     });
-    currentvalue.addEventListener('mouseout', function(e) {
+    currentvalue.addEventListener("mouseout", (e) => {
         currentvalue.style.color = "#212529";
     });
 });
 
+// Keydown event listener 
+
+const home = document.querySelector(".home");
+
+document.addEventListener("keydown", (e) => {
+    const keyName = e.key;
+    if(keyName){
+        home.style.opacity = ".5";
+    }
+});
+ 
