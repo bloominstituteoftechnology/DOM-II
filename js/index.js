23040fdb78ec99daa34a8049699a863816acba17
cecil john tantay <cjbtantay@gmail.com>
2018-12-04 14:44:23 -0800

    added img opacity and some logic for logo color clicker

--- js/index.js
+++ js/index.js
@@ -1,2 +1,59 @@
 // Your code goes here
 
+// import {TweenMax, Power2, TimelineLite} from "gsap/TweenMax";
+
+
+//=====- querySelector declerations
+const contactNav = document.querySelectorAll(".nav-link");
+const destination = document.querySelectorAll(".destination");
+const btn = document.querySelectorAll("btn");
+const mainNavigation = document.querySelector("main-navigation")
+const img = document.querySelectorAll("img");
+const nav = document.querySelectorAll(".nav")
+
+//=====- new elements
+// const burgerArrow = document.createElement("span");
+// burgerArrow.textContent = "&#8964;";
+// mainNavigation.appendChild(burgerArrow);
+
+// if 
+mainNavigation.addEventListener("click", ()=>{
+  counter = 0;
+  counter++;
+  return ()=>{
+    if (count % 3 === 0 ){
+
+    } else if (count % 5 === 0){
+      
+    } else if (count % 3 === 0 && i % 5=== 0){
+      
+    } else {
+     
+    }
+  }
+})
+
+img[1].addEventListener("mouseover", ()=>{
+  img[1].style.opacity = ".5"
+})
+img[1].addEventListener("mouseout", ()=>{
+  img[1].style.opacity = "1"
+})
+
+img[2].addEventListener("mouseover", ()=>{
+  img[2].style.opacity = ".5"
+})
+img[2].addEventListener("mouseout", ()=>{
+  img[2].style.opacity = "1"
+})
+
+img[3].addEventListener("mouseover", ()=>{
+  img[3].style.opacity = ".5"
+})
+img[3].addEventListener("mouseout", ()=>{
+  img[3].style.opacity = "1"
+})
+
+
+// TweenMax.to(".container img")
+
