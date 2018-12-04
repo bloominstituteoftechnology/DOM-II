d11d84253c2777a4e53ce69bd32c268b993818e8
RachelKolk <rach.m.obrien@gmail.com>
2018-12-04 16:00:02 -0500

    2nd event listener done

--- js/index.js
+++ js/index.js
@@ -3,7 +3,17 @@ const pageHeader = document.querySelector('.main-navigation');
 pageHeader.addEventListener('mouseenter', function(e) {
     pageHeader.style.backgroundColor = '#17A2B8';
 })
-pageHeader.addEventListener('mouseleave', function(e){
-    e.stopPropagation();
-    pageHeader.style.backgroundColor = 'none';
+
+
+// const links = document.querySelectorAll('a');
+// links.addEventListener('mouseover', function(e) {
+//     e.stopPropagation();
+//     for (let i = 0; i < links.length; i++) {
+//         links[i].style.color = 'white';
+//     }
+// })
+
+const busPic = document.querySelector('.bus-pic');
+busPic.addEventListener('click', function(e) {
+    busPic.src = 'img/photo-1464851707681-f9d5fdaccea8.jpeg'
 })
