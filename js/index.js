dab0bbfaa0df598e52eee272b032c23cf6a539fc
RachelKolk <rach.m.obrien@gmail.com>
2018-12-04 16:59:57 -0500

    completed 4th event listener

--- js/index.js
+++ js/index.js
@@ -9,26 +9,23 @@ pageHeader.addEventListener('mouseenter', function(e) {
 
 
 const links = document.querySelectorAll('nav-links');
-// pageHeader.removeEventListener('mouseenter', function(e) {
-//     pageHeader.style.backgroundColor = '#17A2B8';
-// })
-// links.addEventListener('mouseover', function(e) {
-//     e.stopPropagation();
-//     // for (let i = 0; i < links.length; i++) {
-//     //     links[i].style.color = 'white';
-//     // }
-//     links[0].style.color = 'white';
-// })
-// links[0].addEventListener('mouseover', function(e) {
-//     e.preventDefault();
+for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener('mouseover', function(e) {
+        links[i].style.color = 'white';
+    })
+}
+pageHeader.removeEventListener('mouseenter', function(e) {
+    pageHeader.style.backgroundColor = '#17A2B8';
+})
 
-// })
 
 //==========  page event listeners
 const home = document.querySelector('.home');
 home.addEventListener('wheel', function(e) {
-    console.log('Wheeeee!');
+    console.log("Wheeeee! We're scrolling, now.");
 })
+
+
 
 //========== Main content event listeners
 const busPic = document.querySelector('.bus-pic');
@@ -36,3 +33,11 @@ busPic.addEventListener('click', function(e) {
     busPic.src = 'img/photo-1464851707681-f9d5fdaccea8.jpeg'
 })
 
+
+
+const buttons = document.querySelectorAll('.btn');
+for (let i=0; i<buttons.length; i++) {
+    buttons[i].addEventListener('mouseover', function(e) {
+        buttons[i].style.backgroundColor = '#FCB74A';
+    })
+}
