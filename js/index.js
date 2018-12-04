138fc02c327bd0e0dd26790b16eee6780d117faf
Henry Neal <henry.neal0@gmail.com>
2018-12-04 17:41:52 -0500

    was stuck on getting code to work for about 2 hours, but slowly moving along

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+const navBox = document.querySelectorAll('.nav a')
+// console.log(navBox)
+for (let i = 0; i < navBox.length; i++){
+    navBox[i].addEventListener('mouseover', function(){
+
+        navBox[i].classList.add('nav-link')
+    });
+    
+    navBox[i].addEventListener('mouseout', function(){
+
+        navBox[i].classList.remove('nav-link')
+    }); 
+}
+
+
+// const funBus = document.querySelectorAll('.logo-heading')
+
+//     funBus.addEventListener('dblclick', funciton(){
+
+//     funBus.classList.add('funBusNew')
+// })
+
