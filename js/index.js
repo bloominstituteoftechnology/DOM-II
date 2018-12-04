9c17ab4cc9470b08b9cfb137bab25a0fba33ffe1
yannafaith <yannafaith97@gmail.com>
2018-12-04 15:00:17 -0700

    added 2 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+
+const funBusHeader = document.querySelector('h1')
+funBusHeader.addEventListener('click', e => {funBusHeader.style.color = 'blue';})
+
+const h2 = document.querySelector('h2');
+h2.addEventListener('mouseover', e => {h2.style.color = 'orange'});
+
+
