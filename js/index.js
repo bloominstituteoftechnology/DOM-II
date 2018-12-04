3b5024b35c13dbdb73f89ec7e4c3abc50c7a3cd9
ills5771 <ilyayelizarov1@gmail.com>
2018-12-04 12:52:24 -0800

    Added first 3 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+//1. Mouseover changes a tag colors
+const navMouseOver = document.querySelectorAll('.nav a');
+
+navMouseOver[0].addEventListener("mouseover", function(event) {
+    event.target.style.color = "green";
+})
+navMouseOver[1].addEventListener("mouseover", function(event) {
+    event.target.style.color = "red";
+})
+navMouseOver[2].addEventListener("mouseover", function(event) {
+    event.target.style.color = "purple";
+})
+navMouseOver[3].addEventListener("mouseover", function(event) {
+    event.target.style.color = "blue";
+})
+
+//2. Keydown
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    console.log('keydown event \n\n' + 'key' + keyName);
+});
+
+//3. Wheel 
+const wheelData = document.querySelector('.home');
+
+wheelData.addEventListener("wheel", function(event) {
+    event.target.style.fontSize = "4.2rem";
+    // console.log(`I'm wheeling!`);
+});
+
+//4. Drag / Drop 
