6f035f3b21232877de7dec8aceec8f8f763b8148
leiannel <leilouis112@gmail.com>
2018-12-04 18:58:05 -0500

    updated index.js

--- js/index.js
+++ js/index.js
@@ -20,15 +20,15 @@ function getColor() {
 
 const audio = document.querySelector('audio')
 document.addEventListener('keydown', function(){
-    console.log('btn was pressed')
+    audio.play()
 })
 
 
 // LOAD=====================================================
 
-// window.addEventListener('load', function(){
-//     alert('Hey, this is the .load() method! Click ok to see that your page has loaded!');
-// })
+window.addEventListener('load', function(){
+    alert('Hey, this is the .load() method! Click ok to see that your page has loaded!');
+})
 
 // RESIZE======================================================
 const links = document.querySelectorAll('.nav-link');
@@ -48,10 +48,48 @@ document.addEventListener('scroll', function(){
     })
 })
 
-// FOCUS====================================================
+// FOCUS & SELECT============================================
 
-const h4 = document.getElementsByTagName('h4')
-
-for(let i=0; i < h4.length; i++){
-    h4[i].focus()
+const input = document.getElementById('myText');
+const btn = document.querySelector('.inputBtn');
+const focusMethod = function() {
+    input.focus()
+    input.select()
 }
+
+// DRAG & DROP============================================
+heading.addEventListener('dragstart', dragStart);
+heading.addEventListener('dragend', dragEnd);
+
+const empty = document.querySelector('.empty');
+
+empty.addEventListener('dragover', dragOver);
+empty.addEventListener('drop', dragDrop);
+function dragStart(){
+ this.classList.add('dragging');
+
+}
+
+function dragEnd(){
+    this.classList.remove('dragging');
+}
+function dragOver(e){
+    e.preventDefault();
+    console.log('over')
+}
+function dragDrop(){
+    this.append(heading)
+}
+
+// WHEEL================================================
+
+empty.addEventListener('wheel', function(){
+    console.log('hi')
+})
+
+// DBLCLICK============================================
+
+const img = document.querySelector('img')
+img.addEventListener('dblclick', function(){
+    img.classList.toggle('border')
+})
