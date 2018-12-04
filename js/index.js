9e97a69f306a47e33bf2fd9a873676196eae9ba3
Sheila <s.hopper01110@gmail.com>
2018-12-04 17:01:19 -0500

    DOM II SF

--- js/index.js
+++ js/index.js
@@ -1,1 +1,61 @@
-// Your code goes here
+// CODE: //
+
+// ============== CONSTANTS ============ //
+
+const navLinks = document.querySelector('.nav');
+
+
+// =============== MOUSEOVER ============ //
+
+navLinks.addEventListener('mouseover', function(e) {
+    e.target.style.color= "#411f9e";
+    setTimeout(function() {
+        e.target.style.color = "";
+      }, 400);
+    }, false);
+
+
+// ================ KEYDOWN ============= //
+
+document.addEventListener('keydown', (e) => {
+    const keyDown = e.key;
+    console.log('Hey! I am working.')
+    return('keydown e\o\o' + 'key: ' + keyDown);
+});
+
+
+// ================WHEEL =============== //
+
+
+
+// ================ DRAG/DROP ========= //
+
+document.addEventListener("dragstart", (e) => {
+     dragged = e.target;
+   console.log ('Drag me')
+    
+    e.target.style.opacity = .5;
+}, false);
+
+// ================ LOAD =============== //
+
+
+
+
+
+// ================ RESIZE ============== //
+
+
+
+
+// ================ SCROLL ============== //
+
+
+
+
+// ================ SELECT ============== //
+
+
+
+
+// ================ DBLCLICK ============== //
