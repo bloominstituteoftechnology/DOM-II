81d8bbeb009f17fa005103f1920fe5d249455132
Daniel Weinman <namniewd@gmail.com>
2018-12-04 17:38:29 -0500

    blocks move right indefinitely when mousedown event occurs

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,6 +1,10 @@
-let lowest = 0;
 const groupOfBlocks = document.querySelector('.blocks');
 const individualBlocks = document.querySelectorAll('.block');
+
 individualBlocks.forEach((item) => item.addEventListener('click', function(e) {
     groupOfBlocks.prepend(item);
 }))
+
+individualBlocks.forEach((item) => item.addEventListener('mousedown', function(e) {
+    TweenMax.to(item, 5, {xPercent: 1200});
+}));
