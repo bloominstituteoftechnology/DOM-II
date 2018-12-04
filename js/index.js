bad1bd5c076ea9a5adc998d1ce45e907f0a3744d
David Flack <davidscottflack@gmail.com>
2018-12-04 16:03:00 -0600

    Add img event listener

--- js/index.js
+++ js/index.js
@@ -18,7 +18,10 @@ home.addEventListener('click', function(e) {
 });
 
 home.addEventListener('mouseover', function(e) {
-    e.target.classList.toggle('josh-class');
+    e.target.classList.add('josh-class');
+})
+home.addEventListener('mouseout', function(e) {
+    e.target.classList.remove('josh-class');
 })
 
 const introHeading = document.querySelector('.intro h2');
@@ -27,3 +30,23 @@ document.addEventListener('keydown', (event) => {
     const keyName = event.key;
     introHeading.textContent += keyName;
   });
+const paragraphs = document.querySelectorAll('p');
+for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].addEventListener('mouseover', function() {
+        paragraphs[i].classList.add('color-changer');
+    })
+}
+for (let i = 0; i < paragraphs.length; i++) {
+    paragraphs[i].addEventListener('mouseout', function() {
+        paragraphs[i].classList.remove('color-changer');
+    })
+}
+
+const busImg = document.querySelector('.intro img'); 
+busImg.addEventListener('mouseover', function() {
+    busImg.classList.add('cross-out');
+})
+ 
+busImg.addEventListener('mouseout', function() {
+    busImg.classList.remove('cross-out');
+})
