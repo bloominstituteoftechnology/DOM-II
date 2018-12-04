4bda3c95729082d9ec9e305fb4d2f346683d886f
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2018-12-04 16:56:05 -0600

    Added 10 event listeners to be used on the fun bus web page

--- js/index.js
+++ js/index.js
@@ -8,9 +8,9 @@ const nav = document.querySelectorAll(".nav-link");
 nav.forEach((currentvalue) => {
     currentvalue.addEventListener("mouseover", (e) => {
         currentvalue.style.color = "red";
-    });
-    currentvalue.addEventListener("mouseout", (e) => {
-        currentvalue.style.color = "#212529";
+        setTimeout(function() {
+            e.target.style.color = "";
+          }, 500);
     });
 });
 
@@ -20,10 +20,11 @@ const home = document.querySelector(".home");
 
 document.addEventListener("keydown", (e) => {
     const keyName = e.key;
-    if(keyName != "Escape"){
+    if(keyName){
         home.style.opacity = ".5";
-    } else { //resets the opacity back to default with the esc key
-        home.style.opacity = "";
+        setTimeout(function() {
+            home.style.opacity = "";
+          }, 500);
     }
 });
 
@@ -36,7 +37,7 @@ bus.addEventListener("click", (e) => {
     e.stopPropagation();
 
     const text = document.createElement("h2");
-    text.textContent = "Wow! Now try pressing any key! Can also press escape to cancel the effect!";
+    text.textContent = "Wow! Now try pressing any key!";
 
     const header = document.querySelector(".intro");
     header.appendChild(text);
@@ -70,3 +71,39 @@ const letsGo = document.querySelector(".content-section .text-content h2");
 letsGo.addEventListener("dblclick", (e) => {
     e.target.style.cssText = "font-size : 10rem";
 }); 
+
+
+//Mousemove event listener
+
+window.addEventListener("mousemove", (e) => {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    home.style.color = `${color}`;
+});
+
+//Mouseover Rotate bus imgevent listener
+
+bus.addEventListener("mouseover", (e) => {
+    e.target.style.transform = "rotate(30deg)";
+    setTimeout(function() {
+        e.target.style.transform = "";
+      }, 1000);
+});
+
+//click move event listener
+//moves any element in the home container move to the right by 100px
+
+home.addEventListener("click", (e) => {
+    e.target.style.transform = "translateX(100px)";
+});
+
+//Click event listener for the Fun Bus logo heading
+
+const logoHeading = document.querySelector(".logo-heading");
+
+logoHeading.addEventListener("click", (e) => {
+    e.target.style.cssText = "font-size: 1rem";
+});
