17787a4927f0c1b120a820d4972aef9250a19df1
Brandon <Blent1050@yahoo.com>
2018-12-04 16:10:54 -0500

    MVP goals and stretch goals complete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 
 const navTags = document.querySelectorAll('nav a');
-
+const logo = document.querySelector('h1');
 navTags[0].addEventListener('mouseover', (event) => {
     navTags[0].classList.toggle("makeRed");
 });
@@ -15,10 +15,21 @@ navTags[2].addEventListener('dblclick', (event) => {
     navTags[2].classList.toggle("makeRed");
 });
 
+navTags[3].addEventListener('mousedown', (event) => {
+    navTags[3].classList.toggle("makeRed");
+});
 
+TweenMax.from(logo, 1.5, {
+    y: -100,
+    ease: Back.easeOut.config(1.7)
+});
 
-
-
+window.addEventListener('load', (event) => {
+    alert('Website has been loaded');
+});
+window.addEventListener('rezise', (event) => {
+    console.log('The window has been resized')
+});
 //Greensock
 const buttons = document.querySelectorAll('.btn');
 
@@ -33,7 +44,6 @@ buttons.forEach((element) => {
             scale: 1.1,
             backgroundColor: 'rgb(45, 97, 105)',
             color: 'white'
-            
         });
     });
     element.addEventListener('mouseout',(event) => {
@@ -45,5 +55,34 @@ buttons.forEach((element) => {
     });
 });
 
+let animateHTML = function() {
+    let elems;
+    let windowHeight;
+    function init() {
+      elems = document.querySelectorAll('.hidden');
+      windowHeight = window.innerHeight;
+      addEventHandlers();
+      checkPosition();
+    }
+    function addEventHandlers() {
+      window.addEventListener('scroll', checkPosition);
+      window.addEventListener('resize', init);
+    }
+    function checkPosition() {
+      for (var i = 0; i < elems.length; i++) {
+        let positionFromTop = elems[i].getBoundingClientRect().top;
+        if (positionFromTop - windowHeight <= -170) {
+            TweenMax.to(elems[i], 2, {
+
+                opacity: 1
+            });
+        }
+      }
+    }
+    return {
+      init: init
+    };
+  };
+  animateHTML().init();
 
 
