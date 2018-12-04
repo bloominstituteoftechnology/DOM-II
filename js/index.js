7c44aef9736da25a8d362718f76e486d15d0e6f0
jeff15113 <itsmejeffery@gmail.com>
2018-12-04 18:47:33 -0500

    added mouse and click event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+// Global Variables
+let currentOffset = 0;
+const busIcon = document.querySelector(".bus-icon");
+
+// Get page virtical offset and apply to bus-icon
+window.addEventListener("scroll", event => {
+  currentOffset = window.scrollY;
+  busIcon.style.top = `${busIcon.style.top + currentOffset}px`;
+});
+
+// Bus icon follows mouse
+window.addEventListener("mousemove", event => {
+  busIcon.style.left = `${event.clientX + 1}px`;
+  busIcon.style.top = `${event.clientY + currentOffset + 1}px`;
+});
+
+// Add visable border around clicked items
+const container = document.querySelector(".home");
+container.addEventListener("click", event => {
+  event.target.style.border = "red solid 1px";
+});
