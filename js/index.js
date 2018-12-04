30d76ca013e8f4e7a6aba0f3b42f333ff6272604
KROHNICK <business.kanekperez@gmail.com>
2018-12-04 12:43:21 -0800

    Added mouseover, dblclick and scroll

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
 // Your code goes here
+
+// Buttons (MouseOver & DoubleClick) //
+const myButton1 = document.getElementsByClassName('btn')[0];
+//console.log(myButton1);
+myButton1.addEventListener('mouseover', function (e) {
+    e.stopPropagation();
+    myButton1.style.color = 'black';
+    myButton1.style.backgroundColor = 'yellow';
+});
+const myButton2 = document.getElementsByClassName('btn')[1];
+//console.log(myButton2);
+myButton2.addEventListener('dblclick', function (e) {
+    myButton2.style.color = 'yellow';
+    myButton2.style.backgroundColor = 'black';
+});
+const myButton3 = document.getElementsByClassName('btn')[2];
+//console.log(myButton3);
+myButton3.addEventListener('mouseover', function (e) {
+    myButton3.style.color = 'black';
+    myButton3.style.backgroundColor = 'yellow';
+});
+
+// Nav Links (Scroll) //
+function randomColors() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+const navLinks = document.querySelectorAll('a');
+//console.log(navLinks);
+window.addEventListener('scroll', function (e) {
+    navLinks.forEach(item => item.style.color = randomColors());
+    e.stopPropagation();
+});
