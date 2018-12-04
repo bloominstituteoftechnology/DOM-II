756a47c85974f180c52978d308752f604e5f9ee9
Nathan Thomas <nwthomas@me.com>
2018-12-04 12:54:30 -0800

    Update index.html with scroll event

--- js/index.js
+++ js/index.js
@@ -1,5 +1,12 @@
 // ============= Variables
+const html = document.querySelector("html");
 const headerLogo = document.querySelector(".logo-heading");
+const nav = document.querySelector(".main-navigation");
+
+const intro = document.querySelector(".intro");
+const invContent = document.querySelector(".inverse-content");
+const pickContent = document.querySelector(".content-pick");
+const mainContainer = document.getElementsByClassName("container");
 
 // ============= Functions
 
@@ -8,4 +15,9 @@ headerLogo.addEventListener("mouseover", function() {
   headerLogo.style.fontSize = "7rem";
   headerLogo.style.fontWeight = "500";
   headerLogo.style.opacity = 0.5;
+});
+
+html.addEventListener("wheel", function(event) {
+  html.style.background = "lightgray";
+  nav.style.background = "lightgray";
 });
