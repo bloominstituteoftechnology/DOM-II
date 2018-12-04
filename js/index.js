99b104a37556efc075c5b07f2a65b6fe51c0ad01
ltrii <lelandtr@gmail.com>
2018-12-04 11:33:47 -0800

    getting a feel for things b4 lunch

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const destBtn = document.querySelector('.destination .btn');
+
+destBtn.addEventListener('click', (event) => {
+    destBtn.style.animation = 'innout 1s ease-in-out 0s 1 alternate';
+})
