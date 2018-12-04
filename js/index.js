c8b7dd9c4abe2edf7100fd30178e5474df53b100
Matt Basile <matthewbasile2@gmail.com>
2018-12-04 17:53:25 -0500

    Added animation to our body upon loading. Added an animationstart and animation end listener that hides and reveals our navbar in accordance to our loading animation. LINES: 59-74.

--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 // Your code goes here
 // #1 MouseOver Dynamic Nav Tags
 const navLinks = document.querySelectorAll('a');
-console.log(navLinks);
-
 navLinks.forEach((navLink)=>{
     navLink.addEventListener('mouseover', ()=>{
         navLink.style.color="blue";
@@ -19,13 +17,12 @@ navLinks.forEach((navLink)=>{
         event.preventDefault();
     })
 });
-
 // #2 Wheel - bus Tilts
 const bus = document.querySelector('.fun-bus');
 bus.addEventListener('wheel', ()=>{bus.style.transform = "rotate(7deg)"});
 // // #3 Doublick - colorful words.
+const paras = document.querySelectorAll('section p');
 window.addEventListener('keydown', ()=>{
-    const paras = document.querySelectorAll('section p');
     paras.forEach((p)=>{
       p.style.color=getRandomColor();
     });
@@ -39,25 +36,38 @@ function getRandomColor() {
     return color
   }
 // #4 Enlage Headers on scroll
+const htags = document.querySelectorAll('.container h2')
 document.addEventListener('scroll', ()=>{
-    const htags = document.querySelectorAll('.container h2')
     htags.forEach((h)=>{
         h.style.fontSize = "5rem";
     })
 });
 // #5 Form Popup on Sign Me Up Button
 const btns = document.querySelectorAll('.btn');
-console.log(btns);
 btns.forEach((btn)=>{
     btn.addEventListener('click',()=>{
         prompt("Enter Your Email");
     })
 });
-
 //#6 On window Resize Images spin.
 const images = document.querySelectorAll('img');
 window.addEventListener('resize', ()=>{
     images.forEach((img)=>{
         img.classList.add("rotate-center")
     })
+});
+// #7 On load main content Animates
+const home = document.querySelector('.home');
+console.log(home);
+window.addEventListener('load', ()=>{
+    
+    home.classList.add("scaleUp")
+} )
+const navigation = document.querySelector('.main-navigation');
+//#8 Hide Nav On Animation 
+home.addEventListener('animationstart',  ()=>{
+    navigation.style.display="none";
+});
+home.addEventListener('animationend',  ()=>{
+    navigation.style.display="flex";
 });
