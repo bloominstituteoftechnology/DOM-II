a1d39bbc2252b5de5aa5a3ce9679f681c8e1a26b
kjohna <43828763+kjohna@users.noreply.github.com>
2018-12-04 14:14:47 -0700

    Task 3: mouseover animation for Fun Bus logo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+const logoHeading = document.querySelector(".logo-heading");
+logoHeading.addEventListener("mouseover", function (){
+  logoHeading.classList.toggle("animate-wobble");
+  console.log("logoHeading mouseover");
+});
