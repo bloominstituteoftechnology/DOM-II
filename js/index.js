01fb21b35e11f02fa9beb841424373639e012e38
JamesB <jblazer1@yahoo.com>
2018-12-04 14:41:35 -0800

    Add click, mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+
+const logHead = document.querySelector('h1');
+logHead.addEventListener('click', function(){
+    logHead.style.border = '2px dashed hotpink';
+});
+
+const adventure = document.querySelector('.img-content img');
+adventure.addEventListener('mouseover', function(){
+    adventure.style.opacity = '0';
+});
+
+const blog = document.querySelector('a');
+blog[3].addEventListener('click', function(e){
+  e.preventDefault();
+});
+
+
