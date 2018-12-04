cb12b0f358541873f81f25cd3312bc24a18229cc
Samsoucy <newf930@outlook.com>
2018-12-04 18:08:13 -0500

    mouseover, click, dblclick

--- js/index.js
+++ js/index.js
@@ -1,15 +1,32 @@
 // Your code goes here
-const button = document.querySelector(".btn");
-
-button.addEventListener("mouseover", function(e){
-    e.target.style.backgroundColor = "pink";
+const button = document.querySelectorAll(".btn");
+button.forEach(function(button){
+    button.addEventListener("mouseover", function(){
+        button.style.backgroundColor = "pink";
+        
+    }) 
+    button.addEventListener("mouseover", function(e){
+    e.preventDefault();
+    })
 });
 
+const logo = document.querySelector(".logo-heading")
+logo.addEventListener("click", function(e){
+    logo.style.color = "blue";
+})
+
+const navigation = document.querySelector(".nav");
+    document.addEventListener("dblclick", function(e){
+         e.target.style.opacity = .2;
+    });
+
+   
 
 
-function myFunction(x) {
-    x.classList.toggle("fa-thumbs-down");
-}
+// function myFunction(x) {
+//     x.classList.toggle("fa-thumbs-down");
+// })
+
 
 
 
