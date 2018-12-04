96534927fcf3d4c147b51cf412c4d48ce4078622
samjeffcoat <samjeffcoat@gmail.com>
2018-12-04 17:12:58 -0600

    added some more functionality to page

--- js/index.js
+++ js/index.js
@@ -3,13 +3,22 @@
 
 const aTag =document.querySelectorAll("a");
 const img = document.querySelectorAll(".intro");
+const h2= document.querySelector(".content-destination");
 const h1 = document.querySelector("h1");
+const btn=document.querySelectorAll(".btn");
+const dest = document.querySelector(".destination");
 
+
+//mouseover- when mousover content-destination text will turn red
+h2.addEventListener("mouseover", function (e) {
+    h2.style.color= "red";
+})
+// dblclick when you double click fun bus the text will turn purple
 h1.addEventListener('dblclick', function (e) {
     e.preventDefault();
     e.target.style.color ='purple';
 });
-
+//when you click on home it will turn blue and wont open another page
 aTag[0].addEventListener('click', function (e) {
     e.preventDefault();
     e.target.style.color='blue';
@@ -17,18 +26,20 @@ aTag[0].addEventListener('click', function (e) {
 });
 
 
-
+//when you click on about us it will turn red and wont open another page
 aTag[1].addEventListener('click', function (e) {
     e.preventDefault();
     e.target.style.color= 'red';
     console.log ("did this work?");
 })
 
+//when you click on blog it will turn green and wont open another page
 aTag[2].addEventListener('click', function (e) {
     e.preventDefault();
     e.target.style.color= 'green';
     console.log ("did this work?");
 })
+//when you click on contact it will turn orange and wont open another page
 aTag[3].addEventListener('click', function (e) {
     e.preventDefault();
     e.target.style.color= 'orange';
@@ -37,7 +48,23 @@ aTag[3].addEventListener('click', function (e) {
 
 
 
+btn[0].addEventListener('focus', function (e) {
+    e.preventDefault();
+    e.target.style.background= 'yellow';
+    console.log("button working");
+}, true);
 
+btn[1].addEventListener('select', function (e) {
+    e.preventDefault();
+    e.target.style.color= 'black';
+    console.log("button working");
+})
+//When you move the mouse over the button the text will turn red
+btn[2].addEventListener('mousemove', function (e) {
+    e.preventDefault();
+    e.target.style.color= 'red';
+    console.log("button working");
+})
 
 
 
