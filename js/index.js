06962cbedf7f38ee5bb5c93ceab200568a6f0aa3
croy <roy.christie95@gmail.com>
2018-12-04 15:43:17 -0500

    Added event listeners to website buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+buttonAnim = Array.from(document.querySelectorAll(".btn"));
+
+buttonAnim[0].addEventListener("mouseover", function(event){
+    event.target.textContent = "Click For Fun!";
+    event.target.style.textAlign = "center";  
+  });
+
+buttonAnim[0].addEventListener("mouseout", function(event){
+    event.target.textContent = "Sign Me Up!";
+  })
+
+  buttonAnim[1].addEventListener("mouseover", function(event){
+    event.target.textContent = "Click For Fun!";
+    event.target.style.textAlign = "center";  
+  });
+
+buttonAnim[1].addEventListener("mouseout", function(event){
+    event.target.textContent = "Sign Me Up!";
+  })
+
+  buttonAnim[2].addEventListener("mouseover", function(event){
+    event.target.textContent = "Click For Fun!";
+    event.target.style.textAlign = "center";  
+  });
+
+buttonAnim[2].addEventListener("mouseout", function(event){
+    event.target.textContent = "Sign Me Up!";
+  })
