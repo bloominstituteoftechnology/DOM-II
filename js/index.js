4c2e33d2a42c0596900c27563bdfa644dd42b380
ChelseaTolnai <ChelseaTolnai@gmail.com>
2018-12-04 16:22:46 -0500

    Task 3: Added fifth event listenter.

--- js/index.js
+++ js/index.js
@@ -24,16 +24,31 @@ window.addEventListener('scroll', function(e) {
 
     setTimeout(function(e) {
         nav.style.backgroundColor = '';
-    }, 1500);
+    }, 1000);
 });
 
 
 // ===== MAIN CONTENT =====
+
+var elem = document.querySelectorAll('h2');
+
+elem.forEach(function(index, i){
+    elem[i].addEventListener('select', function() {
+        alert('Selection changed!');
+    });
+});
+
 
 let button = document.querySelectorAll('.btn');
 
 button.forEach(function(index, i){
     button[i].addEventListener('click', function(e) {
         e.target.textContent = 'You\'re Signed Up!';
+    });
+});
+
+button.forEach(function(index, i){
+    button[i].addEventListener('dblclick', function(e) {
+        e.target.textContent = 'You\'re NOT Signed Up!';
     });
 });
