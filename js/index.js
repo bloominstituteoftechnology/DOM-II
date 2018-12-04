ba5bd97302a475f75e9bb1995d8e4c6a99513d8c
rmejia-tajc <rmejia.tajc@gmail.com>
2018-12-04 14:25:02 -0700

    add mouseover and keydown event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+const yellowbus = document.querySelector(".intro img");
+
+yellowbus.addEventListener("mouseover", function(event) {
+    yellowbus.style.border="1px solid blue";
+})
+
+const home = document.querySelector("body");
+
+home.addEventListener('keydown', function(event) {
+    event.stopPropagation();
+    home.classList.toggle("goblue");
+  });
