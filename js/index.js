e39416e805f9347968ce6569df0eca0b28c397ee
Mike Landers <landers.mike@gmail.com>
2018-12-04 15:58:48 -0800

    Added all 10 events 	* [x] `mouseover` 	* [x] `click` 	* [x] `keydown` 	* [x] `mouseleave` 	* [x] `mouseenter` 	* [x] `load` 	* [x] `focus` 	* [x] `scroll` 	* [x] `select` 	* [x] `dblclick`

--- js/index.js
+++ js/index.js
@@ -15,13 +15,36 @@ element.addEventListener('mouseout', function() {
     });
 })
 
-// Click
+// Click - on images
 const img = document.querySelectorAll("img");
 img.forEach(function(element){
-    element.addEventListener('click', function(){
-        element.classList.toggle('dim')
-    })
+    element.addEventListener('click', function(e){
+        console.log('image was clicked')
+        e.stopPropagation();
+        setTimeout(function(){
+            element.classList.toggle('dim');
+        },3000)
+    },)
 })
+
+// Click - on window
+window.addEventListener('click', function(){
+    console.log('window was clicked')
+})
+
+// Click - on contact link in menu
+const intro = document.querySelector('.intro');
+const section = document.querySelectorAll('section');
+console.log(section)
+
+navA[3].addEventListener('click', function(e){
+    form.classList.toggle('hide');
+    form.style.paddingTop = '100px';
+    intro.classList.toggle('hide');
+    section.forEach(function(element){
+        element.classList.toggle('hide');
+})})
+
 
 // Load
 const logo = document.querySelector(".logo-heading");
@@ -29,7 +52,10 @@ const headerBar = document.querySelector(".main-navigation");
 
 window.addEventListener("load", function(event) {
     logo.style.color = 'yellow';
-    headerBar.style.backgroundColor = 'black';
+    setTimeout(function(){
+        headerBar.style.backgroundColor = 'black';
+    },1000);
+    
     navA.forEach(function(element){ 
             element.style.color = 'yellow';
          });
@@ -37,7 +63,7 @@ window.addEventListener("load", function(event) {
     console.log("All resources finished loading!");
   });
 
-// Double Click
+// Double Click - Sign Me Up buttons
 const btn = document.querySelectorAll(".btn");
 const form = document.querySelector("form")
 btn.forEach(function(element){ 
@@ -45,3 +71,42 @@ btn.forEach(function(element){
         form.classList.toggle('hide')
      });
   })
+
+  //Scroll - down page to move logo image
+  let logoImg = document.querySelector('.logoImg')
+  window.addEventListener('scroll', function(){
+        logoImg.style.marginLeft = '100px';
+        console.log('test');
+  })
+
+  //focus - in contact form
+form.addEventListener("focus", function( event ) {
+  event.target.style.background = "pink";    
+}, true);
+
+//keydown - anywhere on page
+document.addEventListener('keydown', (event) => {
+    const keyName = event.key;
+    alert('keydown event\n\n' + 'key: ' + keyName);
+  });
+
+//Prevent Default
+navA[0].addEventListener('click', function(e){
+e.preventDefault();
+})
+
+// Select - text within the subject field of the form
+let subject = document.querySelector('#subject');
+subject.addEventListener('select', function() {
+    console.log('Selection changed!');
+  }, false);
+
+  //mouseleave - intro section
+  intro.addEventListener('mouseleave', function(){
+      intro.style.border = '1px solid red'
+  })
+
+   //mouseenter - intro section
+   intro.addEventListener('mouseenter', function(){
+    intro.style.border = '2px solid green'
+})
