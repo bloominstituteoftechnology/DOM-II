4f97209d52c7218c4ee6d78281c126e7d421fe8d
David Flack <davidscottflack@gmail.com>
2018-12-04 16:23:57 -0600

    Add links event listener

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
 // Your code goes here
 const buttons = document.querySelectorAll('.btn')
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener('click', function() {
-        TweenLite.to(buttons[i], 1, { ease: CustomEase.create("custom", "M0,0.018 C0.138,0.222 0.739,0.302 1,0"), y: -500 }); 
-    });
-}
+
 const home = document.querySelector('.home');
 const navLinks = document.querySelectorAll('.nav a');
-// navLinks[2].addEventListener('click', function () {
-//     // home.addEventListener('click', function(e) {
-//     //     e.target.style.display = 'none';
-//     })
-// })
+
+for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('mouseover', function() {
+        navLinks[i].style.transform = 'scale(1.2)';
+    })
+    navLinks[i].addEventListener('mouseout', function() {
+        navLinks[i].style.transform = 'scale(1)';
+    })
+}
 
 home.addEventListener('click', function(e) {
     e.target.classList.toggle('font-change');
