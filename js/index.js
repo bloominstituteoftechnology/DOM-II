62d6a63a8eb0924c4ef7e3af5bc7979f6626bca7
Estevan <estevan.rodriguez10@yahoo.com>
2018-12-04 12:58:10 -0800

    created an event listener on home button. when clicked changes color to green

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 const aButtons = document.querySelectorAll('a');
 
-aButtons.forEach(function(e){
-e.preventDefault();
-});
+// aButtons.forEach(function(e){
+// e.preventDefault()
+//  });
+
+ aButtons[0].addEventListener('click',function(e){
+   
+    e.target.style.color = 'green'
+console.log('Home was clicked');
+ });
