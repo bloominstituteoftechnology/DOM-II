498a423d18dad4d2ec073e38a97038c3e00513fe
Samsoucy <newf930@outlook.com>
2018-12-04 17:23:32 -0500

    task 3

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+const button = document.querySelector(".btn");
+
+button.addEventListener("mouseover", function(e){
+    e.target.style.backgroundColor = "pink";
+});
+
+
+
+function myFunction(x) {
+    x.classList.toggle("fa-thumbs-down");
+}
+
+
+
+
+
