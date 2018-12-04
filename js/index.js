00c78824e813a7081c6c23af73d1fa2cd6bbe639
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2018-12-04 14:39:36 -0600

    Init commit. Added an event listener for the navigation anchors using mouseover event type

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+
+//Mouse over event listener for Navigation Anchor tags
+
+const nav = document.querySelectorAll(".nav-link");
+
+nav.forEach((currentvalue) => {
+    currentvalue.addEventListener('mouseover', function(e) {
+        currentvalue.style.color = "red";
+    });
+    currentvalue.addEventListener('mouseout', function(e) {
+        currentvalue.style.color = "#212529";
+    });
+});
+
