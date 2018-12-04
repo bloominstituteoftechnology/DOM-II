ad89354a2459e35467f718288f9c257452cb352f
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2018-12-04 16:38:15 -0500

    Added second event listener, attempted to remove

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 const logoHeading = document.querySelector(".logo-heading");
 const introImg = document.querySelector("header img");
+const buttons = document.querySelectorAll(".btn");
 
-logoHeading.addEventListener('mouseover', function(e) {
+logoHeading.addEventListener("mouseover", function() {
     introImg.style.display = "initial";
 })
+
+buttons.forEach((btn) => {
+    btn.addEventListener("mouseover", function(e) {
+        e.target.style.transform = "scale(1.2,1.2)";
+    })
+    // btn.removeEventListener("mouseover", function(e) {
+    //     e.target.style.transform = "scale(1.2,1.2)";
+    // })
+});
