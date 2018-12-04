08da45c865cd73e93edfeaab3257c36a807e508a
Nate Boyette <nateboyette@icloud.com>
2018-12-04 13:49:14 -0800

    Added scale animation to nav on mouseover.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+const navItems = document.querySelectorAll('a');
+console.log(navItems);
+
+
+
+navItems.forEach((item) => {
+    item.addEventListener('mouseover', (e) => {
+        e.stopPropagation();
+        item.classList.toggle('scale-up-center');
+    })
+});
+
+
+
+
+
+
+
