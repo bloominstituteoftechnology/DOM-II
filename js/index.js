cb8b4751278b2f8f32ce6af406a699b2478a0e8b
Raine Wallace <rainebeaudubs@gmail.com>
2018-12-04 16:12:08 -0500

    Slowly, but successfully, making changes to nav

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 // Your code goes here
+const aTag =
+document.querySelectorAll('a');
+
+const heading = 
+document.querySelector('.logo-heading');
+
+aTag[0].addEventListener('click', function(e) {
+    e.preventDefault();
+    e.target.style.color = 'red';
+    console.log('Oh, hey, you clicked me!')
+});
+
+aTag[1].addEventListener('click', function(e) {
+    e.preventDefault();
+    e.target.style.color = 'orange';
+    console.log('Oh, yeah.')
+});
+
+aTag[2].addEventListener('click', function(e) {
+    e.preventDefault();
+    e.target.style.color = 'yellow';
+    console.log('I don\'t actually know how to put these together, that\'s why it looks like this.')
+});
+
+aTag[3].addEventListener('click', function(e) {
+    e.preventDefault();
+    e.target.style.color = 'green';
+    console.log('Lol colors.')
+});
+
+// heading[0].addEventListener('click', function(e) {
+//     e.preventDefault();
+//     e.target.style.color = 'red';
+// });
