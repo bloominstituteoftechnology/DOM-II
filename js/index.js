9bb9525ee27e5375462484e26a8b0c7d67789c4b
Brandon <Blent1050@yahoo.com>
2018-12-04 15:21:26 -0500

    Implemented 3 event listeners on fun bus

--- js/index.js
+++ js/index.js
@@ -1,2 +1,30 @@
 // Your code goes here
-g
+
+const navTags = document.querySelectorAll('nav a');
+
+navTags[0].addEventListener('mouseover', (event) => {
+    navTags[0].classList.toggle("makeRed");
+});
+
+navTags[1].addEventListener('click', (event) => {
+    navTags[1].classList.toggle("makeRed");
+    event.preventDefault();
+});
+
+navTags[2].addEventListener('dblclick', (event) => {
+    navTags[2].classList.toggle("makeRed");
+});
+
+const buttons = document.querySelectorAll('.btn');
+
+buttons.forEach((element) => {
+    element.addEventListener('click',(event) => {
+        TweenMax.to(element, 2, {
+            backgroundColor: 'red'
+        });
+    });
+
+});
+
+
+
