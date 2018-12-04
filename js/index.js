c966cfc442c873fbd00f9241c636e0bdded88024
ChelseaTolnai <ChelseaTolnai@gmail.com>
2018-12-04 15:02:38 -0500

    Task 3: Added two event listeners to nav anchors.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,19 @@
 // Your code goes here
 
+let navAnchors = document.querySelectorAll('.nav-link');
+
+navAnchors.forEach(function(index, i){
+    navAnchors[i].addEventListener('mouseover', function(e) {
+        e.target.style.color = '#fcc100';
+        e.target.style.textDecoration = 'underline';
+    });
+});
+
+navAnchors.forEach(function(index, i){
+    navAnchors[i].addEventListener('mouseout', function(e) {
+        e.target.style.color = '';
+        e.target.style.textDecoration = 'none';
+    });
+});
+
+
