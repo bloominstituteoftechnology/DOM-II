a9c8f969cecb4c8572140be107c74ae74dfc2cf9
cori-peck <cori.peck@ymail.com>
2018-12-04 17:53:33 -0600

    Completed to 8 event listeners

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 // 1. Mouse Move = Random Nav Colors
-const navAnima = document.querySelector('body');
-const navItems = document.querySelectorAll('a');
+const navAnima = document.querySelector('.main-navigation');
+const navItems = document.querySelectorAll('.nav a');
+const picFade = document.querySelector('#pic3');
+const funButton = document.querySelector('#btnfun');
+const mtnButton = document.querySelector('#btnmountain');
+const islandButton = document.querySelector('#btnisland');
 
 navAnima.addEventListener('mousemove', random);
 function random() {
@@ -16,4 +20,37 @@ function getColor() {
 }
 
 
-//2. 
+//2. Fade Out
+picFade.addEventListener('mouseover', function(e) {
+    e.preventDefault();
+    e.target.style.opacity = '0.5';
+});
+
+//3. Fade In
+picFade.addEventListener('mouseout', function(e) {
+    e.target.style.opacity = '1';
+});
+
+
+//4. Alert on Click
+funButton.addEventListener('click', function(e) {
+    e.preventDefault();
+    alert('Let\'s Have Fun!');
+});
+
+//5. Disable on Click
+mtnButton.addEventListener('click', function(e) {
+    e.preventDefault();
+});
+
+//6. Alert on Double Click
+mtnButton.addEventListener('dblclick', function(e) {
+    e.preventDefault();
+    alert('To the Top!');
+});
+
+//7. Alert on Right Click
+islandButton.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+    alert('Go "Right" to the Island!');
+});
