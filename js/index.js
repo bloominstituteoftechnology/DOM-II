24928efbb65091cf9969bf624ffcb6e97b493185
ltrii <lelandtr@gmail.com>
2018-12-04 12:12:42 -0800

    additional research

--- js/index.js
+++ js/index.js
@@ -3,4 +3,26 @@ const destBtn = document.querySelector('.destination .btn');
 
 destBtn.addEventListener('click', (event) => {
     destBtn.style.animation = 'innout 1s ease-in-out 0s 1 alternate';
-})
+});
+
+const navLinks = document.querySelector('a');
+
+navLinks.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'green';
+});
+
+const logoHeading = document.querySelector('.logo-heading');
+
+logoHeading.addEventListener('dblclick', (event) => {
+    event.target.style.fontFamily = 'Arial';
+});
+
+
+//scrolling event?
+document.addEventListener("scroll", function() {
+
+
+  
+  })
+
+
