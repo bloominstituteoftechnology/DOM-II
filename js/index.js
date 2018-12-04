3822a24220841fbd2618eca185908e1ac44fae21
ltrii <lelandtr@gmail.com>
2018-12-04 14:18:54 -0800

    More eventlisteners added

--- js/index.js
+++ js/index.js
@@ -1,28 +1,82 @@
 // Your code goes here
-const destBtn = document.querySelector('.destination .btn');
+const destBtn = document.querySelectorAll('.destination .btn');
 
-destBtn.addEventListener('click', (event) => {
-    destBtn.style.animation = 'innout 1s ease-in-out 0s 1 alternate';
+for (let i = 0; i < destBtn.length; i++){
+    destBtn[i].addEventListener('mousedown', (event) => {
+        event.target.style.animation = 'innout 1s ease-in-out 0s 1 alternate';
+    });
+}
+
+
+const navLinks = document.querySelectorAll('a');
+
+for (let i = 0; i < navLinks.length; i++){
+    navLinks[i].addEventListener('mouseover', (event) => {
+        event.target.style.color = 'green';
+    });
+}
+
+const imgapp = document.querySelectorAll('img');
+
+for (let i = 0; i < imgapp.length; i++){
+    imgapp[i].addEventListener('mouseup', (event) => {
+        event.target.style.animation = 'imgapp 1s linear 0s 1 forwards';
+    });
+}
+
+// const h2fun = document.querySelectorAll('p');
+
+// for (let i = 0; i < h2fun.length; i++){
+
+//     h2fun[i].addEventListener('mouseover', (event) => {
+//         event.target.fontSize = '3em';
+//     });
+
+//     h2fun[i].addEventListener('mouseleave', (event) => {
+//         event.target.fontSize = '1.2em';
+//     });
+// }
+
+
+const funText = document.querySelector('.logo-heading');
+
+funText.addEventListener('dblclick', (event) => {
+    event.target.style.fontFamily = 'Courier';
 });
 
-const navLinks = document.querySelector('a');
 
-navLinks.addEventListener('mouseover', (event) => {
-    event.target.style.color = 'green';
-});
+let scrollcount = 0;
 
-const logoHeading = document.querySelector('.logo-heading');
-
-logoHeading.addEventListener('dblclick', (event) => {
-    event.target.style.fontFamily = 'Arial';
-});
-
-
-//scrolling event?
 document.addEventListener("scroll", function() {
 
-
+    console.log("Now we are scrolling!");
+    scrollcount = scrollcount + 1;
   
-  })
+});
+
+const headimg = document.querySelector('.intro img');
+
+
+//mouseup
+headimg.addEventListener("mouseup", (event) => {
+
+    event.target.href = 'img/leaves.jpg';
+  
+})
+
+const mainp = document.querySelector('footer');
+
+mainp.addEventListener("click", (event) => {
+    event.target.innerHTML = "Blah blah blah blah blah"
+})
+
+const destdiv = document.querySelectorAll('.destination');
+
+for (let i = 0; i < destdiv.length; i++){
+    destdiv[i].addEventListener('mouseover', (event) => {
+        event.target.style.animation = 'destcolor .3s linear 0s 1 forwards';
+    });
+}
+
 
 
