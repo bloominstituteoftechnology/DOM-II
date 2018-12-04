a786e85a8d82423f4d5ccf4563760d41207ba1ae
MichaelNey <ney.quentin.michael@gmail.com>
2018-12-04 14:30:54 -0800

    Added some comments in stretch assignment code

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -5,15 +5,20 @@ let pink = document.querySelector('.block--pink');
 let gray = document.querySelector('.block--gray');
 let blocks = document.querySelectorAll('.block');
 
+//Assign a variable to the current lowest ordered element
 let neg = 0;
+
 blocks.forEach(b => {
     b.style.marginLeft = '0px';
     let interval;
+
+    //On click, set the order to a lower number of the lowest ordered element, to make it appear above/before the rest.
     b.addEventListener('click', (e) => {
         neg -= 1;
         TweenMax.to(e.target, 0.25, {order: `${neg}`});
     });
 
+    //On mousedown, start an interval executing every 10ms that adds 1px left margin (To visually push it to the right)
     b.addEventListener('mousedown', (e) => {
         e.target.dataset.flying = 'true';
         interval = setInterval(function() {
@@ -22,6 +27,8 @@ blocks.forEach(b => {
             TweenMax.to(e.target, 0.001, {marginLeft: `${newMargin}px`});
         }, 10);
     });
+
+    //On mouseup/mouseleave remove the interval that is moving the rocket/box to the right.
     b.addEventListener('mouseup', (e) => {
         e.target.dataset.flying = 'false';
         clearInterval(interval);
