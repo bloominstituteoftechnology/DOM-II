ce17efe29246cc4b45e59eb0d04a532d3b26f603
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2018-12-04 16:15:11 -0500

    Added first event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+const logoHeading = document.querySelector(".logo-heading");
+const introImg = document.querySelector("header img");
+
+logoHeading.addEventListener('mouseover', function(e) {
+    introImg.style.display = "initial";
+})
