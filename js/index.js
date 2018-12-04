8f7bd934ba444b7597e7f919bb893cd7a0a1bf39
byronholmes2018 <lshopeful2018@mgmail.com>
2018-12-04 17:20:26 -0600

    Finished first task of MVP, adding 10 event listeners, modifying less as needed.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,64 @@
-// Your code goes here
+// Your code goes here'
+
+//On mousing over the nav links, change color
+
+document.querySelectorAll('nav a').forEach(function(link){link.addEventListener("mouseover", function(e){
+    e.target.style.color = "red"; setTimeout(function(){e.target.style.color ="";}, 500);}, false)
+});
+
+//Anytime you right click...
+
+document.body.addEventListener("contextmenu", function(e){
+  alert("Whatcha doin' there...?");
+});
+
+
+//When copying text page, alerts user with request for credit
+
+document.querySelectorAll('p').forEach(function(text){text.addEventListener("copy", function(e){
+  alert("Be sure to give us credit for that!");
+})});
+
+//When you press mouse button down on image, it scales up to 1.05 times its normal size...
+
+document.querySelectorAll('img').forEach(function(img){img.addEventListener("mousedown", function(e){
+  img.className = "zoom";
+})});
+
+//...then when you release the button, it goes back to normal.
+
+document.querySelectorAll('img').forEach(function(img){img.addEventListener("mouseup", function(e){
+  img.className = "";
+})});
+
+//As you scroll...
+
+var counter = 0;
+window.addEventListener('scroll', function(e){counter++;if(counter%60===0){
+  alert("You've gone a ways...");}
+});
+
+//Double click the images for a surprise...
+
+document.querySelectorAll('img').forEach(function(img){img.addEventListener("dblclick", function(e){
+  img.className = "spin";
+})});
+
+//A friendly reminder after the above surprise plays out...
+
+document.querySelectorAll('img').forEach(function(img){img.addEventListener("animationend", function(e){
+  setTimeout(function(){alert("It's FUUUNNN!");
+}, 100);
+})});
+
+//This event lets page fade in on load
+
+window.addEventListener("load", function(e){
+  document.body.className = "";
+});
+
+//This event lets page reset the body's class name so it will fade in on reload
+
+window.addEventListener("reset", function(e){
+  document.body.className = "fade";
+})
