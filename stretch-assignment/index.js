2c11d1d28973e01c80d159a295707d7fc9d2f132
Matt Basile <matthewbasile2@gmail.com>
2018-12-04 20:13:51 -0500

    Played around with stretch more but wasn't as successful as I wanted. Worth playing around more with when there is not new material to review.

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,10 +1,33 @@
-const blockList = document.querySelector(".blocks");
+const blockList = document.querySelector('.blocks');
 const blocks = document.querySelectorAll('.block');
-const blockArr = Array.from(blocks);
+const blockOne = document.querySelector('.block-1');
 
 blocks.forEach((block)=>{
     block.addEventListener('click', ()=>{
         blockList.prepend(block);
+        // console.log(blocks);
     })
 })
 
+let mouse =false;
+let currentX = 0;
+
+blocks.forEach((block)=>{
+    block.addEventListener('mousedown', ()=>{
+        blockList.style.position="absolute";
+        block.classList.add="move";
+        console.log('down')
+    });
+    block.addEventListener('mouseup', ()=>{
+        console.log('we up');
+    }
+    );
+    
+})
+
+function updatePos(block) {
+    mouse  = true;
+    currentX = currentX+10;
+    block.style.translate = `${currentX + 'px'}, 0`;
+    console.log(block.style.translate);
+}
