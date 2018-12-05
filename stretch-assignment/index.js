f89bec680f001ae1049486d44068eec155ca5f3c
KieranVieira <kieranvieira@live.com>
2018-12-04 20:00:47 -0500

    Finished stretch question, has a little bug but working for the most part

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -2,45 +2,32 @@ const block = document.querySelectorAll(".block");
 
 let lastOrder = 0;
 let xIndex = {
-    "block--red": 0,
-    "block--blue": 0,
-    "block--green": 0,
-    "block--pink": 0,
-    "block--gray": 0
+  "block--red": 0,
+  "block--blue": 0,
+  "block--green": 0,
+  "block--pink": 0,
+  "block--gray": 0
 }
+let interval;
 
-// block.forEach((item) => {
-//     item.addEventListener('click', () => {
-//         lastOrder -= 1;
-//         item.style.order = lastOrder
-//     })
-
-//     item.addEventListener('mousedown', () => {
-//         let count = 0;
-//         while(item.onmousedown){
-//             count++;
-//         }
-//         xIndex[item.classList[1]]++;
-//         console.log(count)
-//     })
-// })
-
-for(let i = 0; i <= block.length; i++){
-    block[i].addEventListener('click', function(){
-        lastOrder -= 1;
-        block[i].style.order = `${lastOrder}`;
-    })
-    block[i].addEventListener('mousedown', function(){
-        window.setInterval(function(){
-            xIndex[block[i].classList[1]]++;
-            TweenMax.to(`.${block[i].classList[1]}`, 0.5, {
-                x:xIndex[block[i].classList[1]],
-            })
-        },10)
-        console.log("holding it down");
-    })
-    block[i].addEventListener('mouseup', function(){
-        window.clearInterval(interval);
-        console.log("released");
-    })
+for (let i = 0; i <= block.length; i++) {
+  block[i].addEventListener('click', function () {
+    lastOrder -= 1;
+    block[i].style.order = `${lastOrder}`;
+  })
+  block[i].addEventListener('mousedown', function () {
+    interval = window.setInterval(function () {
+      xIndex[block[i].classList[1]]++;
+      console.log(xIndex)
+      TweenMax.to(`.${block[i].classList[1]}`, 0.5, {
+        x:xIndex[block[i].classList[1]],
+      })
+    }, 10)
+    interval;
+    console.log("holding it down");
+  })
+  window.addEventListener('mouseup', function () {
+    window.clearInterval(interval);
+    console.log("released");
+  })
 }
