bb3c2e718a3c227435925c274ed195eb35fff9df
Nate Boyette <nateboyette@icloud.com>
2018-12-04 22:57:13 -0800

    Added a nested event with .stopPropagation(). Completed main objectives.

--- js/index.js
+++ js/index.js
@@ -1,29 +1,26 @@
 // Your code goes here
 
 const navItems = document.querySelectorAll('a');
-console.log(navItems);
-
-
 
 navItems.forEach((item) => {
     item.addEventListener('click', (e) => {
         e.preventDefault();
         e.stopPropagation();
         e.target.classList.toggle('scale-up-center');
-
     })
 });
 
-
 const busImg = document.querySelector('.intro img');
+busImg.addEventListener('mouseover', (e) => {
+    busImg.classList.toggle('fade-out-bck');
+});
 
 busImg.addEventListener('mouseover', (e) => {
     busImg.classList.toggle('fade-out-bck');
-})
+});
 
 
 const headers = document.querySelectorAll('h2');
-console.log(headers);
 
 headers.forEach((item) => {
     window.addEventListener('scroll', () =>{
@@ -32,9 +29,7 @@ headers.forEach((item) => {
 })
 
 
-
 const img = document.querySelectorAll('img');
-console.log(img);
 
 img.forEach((item) => {
     item.addEventListener('dblclick', (e) => {
@@ -45,27 +40,40 @@ img.forEach((item) => {
 
 
 const logo = document.querySelector('.logo-heading');
-console.log(logo);
+
 
 window.addEventListener('load', () => {
     logo.classList.toggle('focus-in-expand')
 })
-
 
 window.addEventListener('copy', () => {
     alert(`You just copied some text`)
 })
 
 
+const destItems = document.querySelectorAll('.destination');
+
+destItems.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        alert('You pressed a destination item');
+    })
+})
 
 const buttons = document.querySelectorAll('.btn');
-console.log(buttons);
+
 
 buttons.forEach((btn) => {
     btn.addEventListener('mouseenter', (e) => {
         e.target.classList.toggle('vibrate-1');
     });
 
+})
+
+buttons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        e.stopPropagation();
+        alert('You pressed a button');
+    })
 })
 
 
@@ -88,7 +96,7 @@ pTags.forEach((item) => {
     item.addEventListener('mousemove', (e) => {
         e.target.classList.toggle('blink-1');
     })
-})
+});
 
 
 
