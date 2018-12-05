02b6b82622f57f1c0a0ae86d6ad214ca682f7dbd
cecil john tantay <cjbtantay@gmail.com>
2018-12-04 23:56:00 -0800

    come changes

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,5 +1,6 @@
 // block nodelist
 const block = document.querySelectorAll(".block");
+const img = document.querySelectorAll("img");
 // individial elements
 const redBlock = document.querySelector(".block--red");
 const blueBlock = document.querySelector(".block--blue");
@@ -7,8 +8,6 @@ const greenBlock = document.querySelector(".block--green");
 const pinkBlock = document.querySelector(".block--pink");
 const grayBlock = document.querySelector(".block--gray");
 const body = document.querySelector("body");
-const img = document.querySelectorAll("img");
-
 let negCounter = 0;
 let intervalId;
 
@@ -21,27 +20,27 @@ body.prepend(h1Element);
 // create img tag for rocket.png
 const redRocket = document.createElement("img");
 redRocket.src = "./rocket.png";
-redRocket.alt = "just a rocket"
+redRocket.alt = "just a rocket";
 redRocket.style.height = "50px";
 
 const blueRocket = document.createElement("img");
 blueRocket.src = "./rocket.png";
-blueRocket.alt = "just a rocket"
+blueRocket.alt = "just a rocket";
 blueRocket.style.height = "50px";
 
 const greenRocket = document.createElement("img");
 greenRocket.src = "./rocket.png";
-greenRocket.alt = "just a rocket"
+greenRocket.alt = "just a rocket";
 greenRocket.style.height = "50px";
 
 const pinkRocket = document.createElement("img");
 pinkRocket.src = "./rocket.png";
-pinkRocket.alt = "just a rocket"
+pinkRocket.alt = "just a rocket";
 pinkRocket.style.height = "50px";
 
 const grayRocket = document.createElement("img");
 grayRocket.src = "./rocket.png";
-grayRocket.alt = "just a rocket"
+grayRocket.alt = "just a rocket";
 grayRocket.style.height = "50px";
 
 redBlock.appendChild(redRocket);
@@ -49,8 +48,6 @@ blueBlock.appendChild(blueRocket);
 greenBlock.appendChild(greenRocket);
 pinkBlock.appendChild(pinkRocket);
 grayBlock.appendChild(grayRocket);
-
-
 
 block.forEach((item, i) => {
   // when clicked, place lowest number to the top position
@@ -62,22 +59,29 @@ block.forEach((item, i) => {
 
   // Launching ONLY IN THE FIRST POSITION
 
-  if (item.style.order <= negCounter){
+  if (item.style.order <= negCounter) {
     // when mouse is clicked and held down, move block from left to right infinitely
-  item.addEventListener("mousedown", e => {
-    intervalId = setInterval(()=> {
-      img[i]
-      console.log('hello')
-    }, 1000)
-  })
 
-  // when mouse is released, block moves from right back to original position
-  item.addEventListener("mouseup", e=> {
-    clearInterval(intervalId);
-  })
+    item.addEventListener("click", e => {
+      intervalId = setInterval(() => {
+        // set current rocket. First child due to img being prepended to e
+        TweenMax.to(e.target.firstChild, 1, { y: `20px`});
+      }, 1000);
+    });
+    item.addEventListener("mousedown", e => {
+      intervalId = setInterval(() => {
+        // current'blocks first child is its img tag... wowza. I'm learning a lot. I want it's img tag to fly!
 
+        TweenMax.to(e.target.firstChild, 1, { x: `2000px` });
+        console.log("hello");
+      }, 1000);
+    });
+
+    // when mouse is released, block moves from right back to original position
+    item.addEventListener("mouseup", e => {
+      clearInterval(intervalId);
+    });
   }
-  
 });
 
 let travelerLaunch = () => {
