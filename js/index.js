93e3c8e3c33e1942a9e49a954a10c086dd2ff7cd
rosilioroman <rosilioroman@gmail.com>
2018-12-04 17:31:32 -0700

    fixed preventDefault() and stopPropagation(). Added comments on functionality that I want to add on the .main-navigation section

--- js/index.js
+++ js/index.js
@@ -20,7 +20,14 @@ window.addEventListener("load", function(e) { //====> when the page loads
     });
 });
 
-//Navbar Links
+
+//Navbar
+//I want to add a click event on .main-navigation that will make the navbar not "sticky"
+// let navSystem = document.querySelector(".main-navigation");
+// navSystem.addEventListener("click", function() {
+//     navSystem.style.position = "static";
+// });
+
 let navLinks = document.querySelectorAll(".nav-link");
 for(let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener("mouseover", function() { //when the user mouses over link
@@ -31,6 +38,12 @@ for(let i = 0; i < navLinks.length; i++) {
     navLinks[i].addEventListener("mouseout", function() { //when the user mouses out of link
         navLinks[i].style.color = "#212529"; //revert to original color
         navLinks[i].style.fontWeight = "normal"; //revert to normal
+    });
+
+    //Stop links from refreshing the page by using preventDefault()
+    navLinks[i].addEventListener("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation(); //the click event here won't trigger click on .main-navigation
     });
 }
 
@@ -53,6 +66,9 @@ for(let i = 0; i < headings.length; i++) {
     headings[i].addEventListener("dblclick", function(){
         headings[i].style.animation = "scale-up-bottom 0.4s cubic-bezier(0.390, 0.575, 0.565, 1.000) both";
     });
+    headings[i].addEventListener("animationend", function() {
+        headings[i].style.animation = "none";
+    });
 }
 
 
@@ -62,8 +78,10 @@ for(let j = 0; j < buttons.length; j++) { //iterate over the NodeList
     buttons[j].addEventListener('mouseover', function(){ //when user mouses over
         buttons[j].style.animation = "heartbeat 1s ease-in-out 2 both"; //add animation
     });
+    buttons[j].addEventList
 
     buttons[j].addEventListener('click', function() {
         alert("Email us at reservations@funbus.com!");
-    })
+    });
 }
+
