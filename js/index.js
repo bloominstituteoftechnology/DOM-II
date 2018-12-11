503d315aa48d2e80d3f04d209ab3fd5c6231b750
Preprocessing - I <karenly1961@gmail.com>
2018-12-11 16:34:29 -0500

    ..

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+const menuHome = document.querySelector("a");
+menuHome.style.color = "red";
+
+const funBus = document.querySelector(".main-navigation .logo-heading");
+funBus.style.color = "green";
+
+const welcome = document.querySelector(".intro h2");
+welcome.style.color = "deeppink";
+
+const allH2s = document.querySelectorAll(".content-section h2");
+allH2s[0].style.color = "cyan";
+allH2s[1].style.color = "orange";
+
+const adventurePara = document.querySelector(".content-section p");
+adventurePara.style.color = "brown";
+
+const pickDestination = document.querySelector(".content-destination h2");
+pickDestination.style.color = "purple";
+
+const buttons = document.querySelectorAll(".destination .btn")
+buttons[0].style.color = "maroon"
+buttons[1].style.color = "navy";
+buttons[2].style.color = "olive";
+
+const footer = document.querySelector("footer p")
+footer.style.color = "pink";
