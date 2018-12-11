0c408d8ec231c943d19670cfcf7146305395839c
nickkruger <ngkruger@gmail.com>
2018-12-11 14:24:18 -0700

    mouseover complete

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
-// Your code goes here
+
+//mouseover
+
+const funBusTitle = document.querySelector('.logo-heading');
+funBusTitle.addEventListener(
+    "mouseover",
+        event => {
+            event.target.style.color = "red";
+        setTimeout(function() {
+            event.target.style.color = "";
+        }, 500);
+    },
+    false
+    );
+
+//keydown
+
+//wheel
+
+//drag/drop
+
+//load
+
+//focus
+
+//resize
+
+//scroll
+
+//select
+
+//dblclick
+
