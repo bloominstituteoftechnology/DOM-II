b425aac7f4b80dbdd9d2f87386d4b91bf49734f4
elisamartin <elisamv93@gmail.com>
2019-01-29 16:55:14 +0100

    Finish MVP

--- js/index.js
+++ js/index.js
@@ -1,45 +1,39 @@
 const header = document.querySelector('.main-navigation');
-const heading = document.querySelector('h1');
+const heading = document.querySelector('.main-navigation h1');
 const imgs = document.querySelector('.img-content');
 const someText = document.querySelector('body');
 const navItems = document.querySelectorAll('nav a');
+const buttons = document.querySelectorAll('.btn');
 
 window.addEventListener('cut', () => {
 	alert('Hell NO');
 });
-
 window.addEventListener('load', () => {
 	alert('All loaded!');
 });
-
 window.addEventListener('resize', () => {
 	header.style.backgroundColor = 'deeppink';
 });
-
 window.addEventListener('scroll', () => {
 	console.log('Scrooolling');
 });
-
 window.addEventListener('keydown', () => {
 	someText.style.color = 'green';
 });
-
 window.addEventListener('keyup', () => {
 	someText.style.color = '';
 });
-
 heading.addEventListener('mouseover', () => {
 	heading.style.color = 'cornflowerblue';
 });
-
 heading.addEventListener('mouseout', () => {
 	heading.style.color = '';
 });
-
-header.addEventListener('dblclick', () => {
-	header.style.backgroundColor = 'palevioletred';
+heading.addEventListener('dblclick', (event) => {
+	event.stopPropagation();
+	heading.style.backgroundColor = 'palevioletred';
+	console.log('hey!');
 });
-
 imgs.addEventListener('dragstart', () => {
 	alert('What are you doing?');
 });
