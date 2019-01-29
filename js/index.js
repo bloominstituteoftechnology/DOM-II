a9b9f56ad6ae7e12c2930f73a9d4a7b50ea0775e
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-01-29 14:35:32 +0000

    Implement "mouseover" Event Listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+(function mouseoverEvent() {
+  const navLinks = document.querySelectorAll('a.nav-link');
+
+  function mouseOverCallback(eventObject) {
+    const { target } = eventObject;
+    target.classList.toggle('hover');
+  }
+
+  for (let i = 0; i < navLinks.length; i++) {
+    navLinks[i].addEventListener('mouseover', mouseOverCallback);
+  }
+})();
