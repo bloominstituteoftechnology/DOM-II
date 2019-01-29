691c57d1b6dd5aa4f1e6798c0132ae456cfcf67e
mattshardman <mattshardman@gmail.com>
2019-01-29 13:37:18 +0000

    add scroll event listener than causes menu to drop down

--- js/index.js
+++ js/index.js
@@ -1,13 +1,29 @@
 // Your code goes here
 
 const body = document.querySelector('body');
+const header = document.querySelector('.nav2');
 
 function rand(input) {
   return Math.floor(Math.random() * (input + 1));
 }
 
+// change background to random colour on 'b' key press
 document.addEventListener('keydown', (e) => {
   if (e.which === 66) {
     body.style.background = `rgb(${rand(255)}, ${rand(255)}, ${rand(255)})`;
+  }
+});
+
+// add drop down menu
+document.addEventListener('scroll', (e) => {
+  if (window.scrollY > 300) {
+    header.style.position = 'fixed';
+    header.style.transform = 'translateY(0%)';
+    return null;
+  }
+
+  if (window.scrollY < 300) {
+    header.style.transform = 'translateY(-100%)';
+    return null;
   }
 });
