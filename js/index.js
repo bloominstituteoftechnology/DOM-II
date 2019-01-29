2f18fda5401f314a5a4181eb807e95b62f776e29
elisamartin <elisamv93@gmail.com>
2019-01-29 16:13:23 +0100

    Tasks 1-2-3

--- js/index.js
+++ js/index.js
@@ -1,1 +1,54 @@
-// Your code goes here
+const header = document.querySelector('.main-navigation');
+const heading = document.querySelector('h1');
+const imgs = document.querySelector('.img-content');
+const someText = document.querySelector('body');
+const navItems = document.querySelectorAll('nav a');
+
+window.addEventListener('cut', () => {
+	alert('Hell NO');
+});
+
+window.addEventListener('load', () => {
+	alert('All loaded!');
+});
+
+window.addEventListener('resize', () => {
+	header.style.backgroundColor = 'deeppink';
+});
+
+window.addEventListener('scroll', () => {
+	console.log('Scrooolling');
+});
+
+window.addEventListener('keydown', () => {
+	someText.style.color = 'green';
+});
+
+window.addEventListener('keyup', () => {
+	someText.style.color = '';
+});
+
+heading.addEventListener('mouseover', () => {
+	heading.style.color = 'cornflowerblue';
+});
+
+heading.addEventListener('mouseout', () => {
+	heading.style.color = '';
+});
+
+header.addEventListener('dblclick', () => {
+	header.style.backgroundColor = 'palevioletred';
+});
+
+imgs.addEventListener('dragstart', () => {
+	alert('What are you doing?');
+});
+
+// Prevent Default
+
+navItems.forEach((link) =>
+	link.addEventListener('click', () => {
+		link.preventDefault;
+		console.log('Default prevented');
+	})
+);
