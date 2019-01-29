393ef93921ab27a9bb1a1af3d8c22e9fb488acc7
ddot727 <ddot727@gmail.com>
2019-01-29 17:07:38 -0500

    added events that pull up pop ups when sign up buttons are double clicked

--- js/index.js
+++ js/index.js
@@ -1,18 +1,34 @@
 // Your code goes here
-const ssnInfo = document.querySelector(".social-security");
 
+//Keylogger
+const ssnInfo = document.querySelector(".social-security");
 ssnInfo.addEventListener('keydown', e =>{
     console.log(`${e.code}`);
 });
 
+//Warning (Click Sumbit)
 const submitButton = document.querySelector(".form-submit");
 submitButton.addEventListener('click', e =>{
     window.alert("Stop silly!");
     e.preventDefault();
 });
  
-// const ssnInfo = document.querySelector(".social-security");
-
+//Pop-Ups
+const signUpButton1 = document.querySelector(".btn1");
+signUpButton1.addEventListener('dblclick', e =>{
+    window.alert("{Insert Annoying ad here}");
+    e.preventDefault();
+});
+const signUpButton2 = document.querySelector(".btn2");
+signUpButton2.addEventListener('dblclick', e =>{
+    window.alert("Meet Hot Singles In Your Area");
+    e.preventDefault();
+});
+const signUpButton3 = document.querySelector(".btn3");
+signUpButton3.addEventListener('dblclick', e =>{
+    window.alert("Free Air! Click Here!");
+    e.preventDefault();
+});
 
 // const ssnInfo = document.querySelector(".social-security");
 
