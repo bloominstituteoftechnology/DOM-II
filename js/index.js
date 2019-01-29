429921f22ba6bde8c1aafde7b153dbcd6f786951
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-01-29 17:24:33 -0600

    Added Scroll Listener

--- js/index.js
+++ js/index.js
@@ -2,6 +2,34 @@
 
 // Navigation Header
 const navigationHeader = document.getElementsByClassName('logo-heading');
+
+// Changing color when mousing over header
 navigationHeader[0].addEventListener('mouseover', e => {
     event.target.style.color = "purple";
 });
+
+// Changing to orange on click
+navigationHeader[0].addEventListener('click', e => {
+    event.target.style.color = "orange";
+});
+
+
+// Changing to green when mousing out
+navigationHeader[0].addEventListener('mouseout', e => {
+    event.target.style.color = "green";
+});
+
+// Adding Scroll Event Listener
+
+window.addEventListener('scroll', () => {
+    const scrolled = window.scrollY;
+    const scrollable = 1310;
+    if(scrolled === scrollable){
+        alert(`DUN DUN DUN YOU'VE REACHED THE BOTTOM!`);    
+    }
+});
+
+
+
+
+
