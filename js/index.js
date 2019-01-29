5d912169751d615f89f8e10db45ce5207e2839d5
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-01-29 15:52:59 -0600

    Added Event Listener to navHeader

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+// Navigation Header
+const navigationHeader = document.getElementsByClassName('logo-heading');
+navigationHeader[0].addEventListener('mouseover', e => {
+    event.target.style.color = "purple";
+});
