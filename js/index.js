9cf8a2d09b3539584ac33d08a0a66bdf67f72c9e
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-01-29 18:01:26 -0500

    moved event triggers to another file

--- js/index.js
+++ js/index.js
@@ -7,32 +7,44 @@ document.querySelectorAll('nav a').forEach(function(link){link.addEventListener(
 
 
 // keydown
-const blinkRed = document.querySelector('html');
-blinkRed.addEventListener("keydown", event => { 
-            event.target.style.background = "red";
+const root = document.querySelector('html');
+root.addEventListener('keydown', event => { 
+            event.target.style.background = 'red';
         setTimeout(function() {
             event.target.style.background = "";
         }, 500);
         });
-        
 
 // wheel
+window.addEventListener('wheel', (e) => {
+    console.log('Wheelin');
+  });
 
 
 // drag / drop
-
+window.addEventListener('drag', (e) => {
+    console.log('drag');
+  });
 
 // load
-
+window.addEventListener('load', (e) => {
+    console.log('load');
+  });
 
 // focus
-
+window.addEventListener('focus', (e) => {
+    console.log('focus');
+  });
 
 // resize
-
+window.addEventListener('resize', (e) => {
+    console.log('resize');
+  });
 
 // scroll
-
+window.addEventListener('scroll', (e) => {
+    console.log('scroll');
+  });
 
 // select
 document.querySelectorAll('img').forEach(function(img){img.addEventListener("mousedown", function(e){
---
+++ js/test.js
@@ -0,0 +1,50 @@
+// Tests
+// mouseover
+window.addEventListener('mouseover', (e) => {
+    console.log('mouseover');
+  });
+
+// keydown
+window.addEventListener('keydown', (e) => {
+    console.log('keydown');
+  });
+
+// wheel
+window.addEventListener('wheel', (e) => {
+    console.log('wheel');
+  });
+
+// drag / drop
+window.addEventListener('drag', (e) => {
+    console.log('drag');
+  });
+
+// load
+window.addEventListener('load', (e) => {
+    console.log('load');
+  });
+
+// focus
+window.addEventListener('focus', (e) => {
+    console.log('focus');
+  });
+
+// resize
+window.addEventListener('resize', (e) => {
+    console.log('resize');
+  });
+
+// scroll
+window.addEventListener('scroll', (e) => {
+    console.log('scroll');
+  });
+
+// select
+window.addEventListener('select', (e) => {
+    console.log('select');
+  });
+
+// dblclick
+window.addEventListener('dblclick', (e) => {
+    console.log('dblclick');
+  });
