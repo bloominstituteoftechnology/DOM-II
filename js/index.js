576709a006e39b7aa9db19c7133d6ee1b8f1b22a
Thomas Folbrecht <tfolbrecht@gmail.com>
2019-01-29 17:32:09 -0500

    4/10

--- js/index.js
+++ js/index.js
@@ -8,10 +8,7 @@ document.querySelectorAll('nav a').forEach(function(link){link.addEventListener(
 
 // keydown
 const blinkRed = document.querySelector('html');
-
-blinkRed.addEventListener(
-    "keydown", 
-        event => { 
+blinkRed.addEventListener("keydown", event => { 
             event.target.style.background = "red";
         setTimeout(function() {
             event.target.style.background = "";
@@ -46,25 +43,3 @@ document.querySelectorAll('img').forEach(function(img){img.addEventListener("mou
 document.querySelectorAll('img').forEach(function(img){img.addEventListener("dblclick", function(e){
     img.className = "spin";
 })});
-
-
-
-//Anytime you right click...
-
-document.body.addEventListener("contextmenu", function(e){
-  alert("Right Click");
-});
-
-
-//When copying text page, alerts user with request for credit
-
-// document.querySelectorAll('p').forEach(function(text){text.addEventListener("copy", function(e){
-//     alert("Please acknowledge our copytradepatent on these words");
-//   })});
-  
-//When you press mouse button down on image, it scales up to 1.05 times its normal size...
-
-
-//This event lets page fade in on load
-
-
