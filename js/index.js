1b7c7a2b668f61b922593a9e9f063c826277996b
ddot727 <ddot727@gmail.com>
2019-01-29 16:38:21 -0500

    added event that logs each keystroke in SSN box

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 // Your code goes here
+const ssnInfo = document.querySelector(".social-security");
+
+ssnInfo.addEventListener('keydown', e =>{
+    console.log(`${e.code}`);
+});
+
+const submitButton = document.querySelector(".form-submit");
+submitButton.addEventListener('click', e =>{
+    window.alert("Stop silly!");
+    e.preventDefault();
+});
+
+// const ssnInfo = document.querySelector(".social-security");
+
+
+// const ssnInfo = document.querySelector(".social-security");
+
+
+// const ssnInfo = document.querySelector(".social-security");
+
+
+// const ssnInfo = document.querySelector(".social-security");
+
+
+// const ssnInfo = document.querySelector(".social-security");
+
+
+// const ssnInfo = document.querySelector(".social-security");
+
+
+// const ssnInfo = document.querySelector(".social-security");
+
+
+// const ssnInfo = document.querySelector(".social-security");
