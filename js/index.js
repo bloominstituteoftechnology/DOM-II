4ce519fddc4c8a5db2c8a65557ad20d2c99bf3e2
Wesley White <wwhite@Wesleys-MacBook-Pro.local>
2019-02-02 09:08:54 -0800

    nav talks with a click

--- js/index.js
+++ js/index.js
@@ -7,7 +7,16 @@ let banana = document.querySelector('img')
 let anchors = document.querySelector('.nav, a')
 let btn = document.querySelectorAll('.btn') 
 
+
 // 1. Turns banner into a banana for Chance with a 'mouseover'//
 banana.addEventListener('mouseover', (object) =>{
     banana.setAttribute('src', 'img/banana.jpg')
 })
+
+// 2. nav anchors talk with a 'click'//  
+anchors.addEventListener('click', (object) => {
+    alert("Ouch!! why did you poke me?")
+})
+
+// 3. 
+
