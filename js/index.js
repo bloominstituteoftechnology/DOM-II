77d8811f45daa242660640f771dc0018399a43c8
kaiserawu <kaiser.a.wu@gmail.com>
2019-02-02 09:30:45 -0800

    Added hover effect on nav buttons using event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+let nav = document.querySelector('nav');
+let navButtons = nav.querySelectorAll('a');
+
+console.log(navButtons);
+for (let i = 0; i < navButtons.length; i++) {
+    navButtons[i].addEventListener('mouseenter', (event) => {
+        event.target.style.backgroundColor = 'lightgrey';
+    })
+    navButtons[i].addEventListener('mouseleave', (event) => {
+        event.target.style.backgroundColor = 'white';
+    })
+}
