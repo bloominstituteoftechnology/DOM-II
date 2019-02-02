af7ea6bcf87dfaed1d60445992910a814c3e956e
Wesley White <wwhite@Wesleys-MacBook-Pro.local>
2019-02-02 08:48:21 -0800

    added some variables

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+// Variables //
+let body = document.querySelector('body')
+let banana = document.querySelector('img')
+let anchors = document.querySelector('.nav, a')
+let btn = document.querySelectorAll('.btn') 
