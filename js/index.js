66018d704c6a28d71ec86d8e9d9b22d7e584b89d
Wesley White <wwhite@Wesleys-MacBook-Pro.local>
2019-02-02 08:59:54 -0800

    added a banana for Chance

--- js/index.js
+++ js/index.js
@@ -6,3 +6,8 @@ let body = document.querySelector('body')
 let banana = document.querySelector('img')
 let anchors = document.querySelector('.nav, a')
 let btn = document.querySelectorAll('.btn') 
+
+// 1. Turns banner into a banana for Chance with a 'mouseover'//
+banana.addEventListener('mouseover', (object) =>{
+    banana.setAttribute('src', 'img/banana.jpg')
+})
