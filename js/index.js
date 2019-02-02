74ce7620dac771b70731fd8a8e3bb4e02ec17345
max-keller427 <max.keller427@gmail.com>
2019-02-02 13:54:53 -0700

    7/10 events

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 // Your code goes here
-
+// Selectors //////////////////////
 let h2 = document.querySelectorAll('h2'); 
+const body = document.querySelector('body');
+let p = document.querySelectorAll('p');
+const img = document.querySelectorAll('img');
+const nav = document.querySelector('.main-navigation');
+
+
+
+// 1 ///////////////////////////// 'click'
+
 h2.forEach((index, i) => h2[i].addEventListener('click', (event =>
     alert (`You clicked an h2`)))
 )
 
-let p = document.querySelectorAll('p');
+// 2 & 3 ///////////////////////////// 'mouseover' & 'mouseleave'
+
 p.forEach((index, i) => {
    p[i].addEventListener('mouseover', (event => p[i].style.backgroundColor = 'yellow'))
    p[i].addEventListener('mouseleave', (event => p[i].style.backgroundColor = 'white'))
 })
-
-const body = document.querySelector('body');
 
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
@@ -22,7 +30,25 @@ function getRandomColor() {
     return color;
   }
 
+// 4 ///////////////////////////// 'keydown'
+
 document.addEventListener('keydown', (event =>  body.style.backgroundColor = getRandomColor() ));
 
+// 5 ///////////////////////////// 'dblclick'
 
+document.addEventListener('dblclick', (event => body.style.backgroundColor = 'white')); 
+
+// 6 ///////////////////////////// 'select'
+
+window.addEventListener('select', (event => {
+    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+    selection.style.color = getRandomColor();
+}))
+
+// 7 ///////////////////////////// 'scroll'
+
+window.addEventListener('scroll', (event =>
+    nav.style.backgroundColor = getRandomColor()));
+
+// 8 /////////////////////////////
 
