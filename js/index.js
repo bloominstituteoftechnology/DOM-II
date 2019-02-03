80a215644db778dc15701876da1fda1a247cceef
Snigdha Regmi <sregmi@NORD.LOCAL>
2019-02-02 18:45:07 -0700

    mvp done

--- js/index.js
+++ js/index.js
@@ -4,37 +4,26 @@
 //     img.target.style.color = "orange"
 //  })
 
-//select the elements
-
 //1. copy
 let intro = document.querySelector(".intro h2");
 intro.addEventListener("copy", (event) => {
   alert("Intro is copied");
 });
 
-
 //2. mouseover
 let buttons = document.querySelectorAll(".btn");
 buttons[0].addEventListener("mouseover", (event) => {
+  event.stopPropagation();
   event.target.style.backgroundColor = "purple";
 });
 buttons[1].addEventListener("mouseover", (event) => {
+  event.stopPropagation();
   event.target.style.backgroundColor = "purple";
 });
 buttons[2].addEventListener("mouseover", (event) => {
+  event.stopPropagation();
   event.target.style.backgroundColor = "purple";
 });
-
-buttons[0].addEventListener("mouseleave", (event) => {
-  event.target.style.backgroundColor = "blue ";
-});
-buttons[1].addEventListener("mouseleave", (event) => {
-  event.target.style.backgroundColor = "blue";
-});
-buttons[2].addEventListener("mouseleave", (event) => {
-  event.target.style.backgroundColor = "blue";
-});
-
 
 //3. click
 let busSand = document.querySelector(".intro img");
@@ -42,32 +31,31 @@ busSand.addEventListener("click", (event) => {
   alert("Welcome To Fun Bus!");
 });
 
-
 //4. remove
 let logoHeading = document.querySelector(".logo-heading");
 logoHeading.addEventListener("mouseleave", (event) => {
+  event.stopPropagation();
   event.target.remove("letsGo");
 });
-logoHeading.addEventListener("mouseover", (event) => {
-  event.target.add("letsGo");
-});
-
 
 //5. contextmenu
 let navLinks = document.querySelectorAll(".nav a");
 navLinks[0].addEventListener("contextmenu", (event) => {
+  event.preventDefault(); //Stop the navigation from items from refreshing the page
   event.target.style.color = "red";
 });
 navLinks[1].addEventListener("contextmenu", (event) => {
+  event.preventDefault();
   event.target.style.color = "red";
 });
 navLinks[2].addEventListener("contextmenu", (event) => {
+  event.preventDefault();
   event.target.style.color = "red";
 });
 navLinks[3].addEventListener("contextmenu", (event) => {
+  event.preventDefault();
   event.target.style.color = "red";
 });
-
 
 //6. wheel
 let footer = document.querySelector("footer");
@@ -75,36 +63,33 @@ footer.addEventListener("wheel", (event) => {
   event.target.style.backgroundColor = "yellow";
 });
 
-
 //7. doubleclick
-let letsGo = document.querySelector(".img-content img");
+let letsGo = document.querySelector(".content-section h2");
 letsGo.addEventListener("dblclick", (event) => {
-  event.preventDefault();
-  alert("Join is for the fun ride");
+  event.stopPropagation();
+  event.target.style.opacity = 0.5;
 });
 
+//9 keydown
+let container = document.querySelector("body");
+container.addEventListener("keydown", (event) => {
+  event.target.style.backgroundColor = "gray";
+});
 
 //8 mousemove
 let destination = document.querySelectorAll(".destination h4");
 destination[0].addEventListener("mousemove", (event) => {
-  event.target.style.color = "orange";
+  event.target.style.fontFamily = "Verdana";
 });
 destination[1].addEventListener("mousemove", (event) => {
-  event.target.style.color = "orange";
+  event.target.style.fontFamily = "Verdana";
 });
 destination[2].addEventListener("mousemove", (event) => {
-  event.target.style.color = "orange";
+  event.target.style.fontFamily = "Verdana";
 });
 
-
-//9 drag
-let pickYour = document.querySelector(".content-destination h2");
-pickYour.addEventListener("drag", (event) => {
-  alert("Pick your destination is being dragged");
+//10 mouseenter
+let para = document.querySelector(".content-destination p");
+para.addEventListener("mouseenter", (event) => {
+  event.target.style.fontWeight = "Bolder";
 });
-
-
-//10 animation
-// destinations.addEventListener("webkitAnimationStart", (event) => {
-//     event.target.style.backgroundColor = "red";
-//   });
