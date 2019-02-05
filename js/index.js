10cf61a2c8dd5fd7c9cf09d94ea64b986a6cf217
Marc Thiongo <marcthuo@gmail.com>
2019-02-05 18:57:56 -0500

    added changes JS

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
 // Your code goes here
-gf
+
+const logoHeading = document.querySelector(".logo-heading");
+logoHeading.addEventListener('keydown', function(event) {
+  console.log(`this is the blank key: ${event.code}`)
+})
+
+const nav = document.querySelector(".nav");
+nav.addEventListener("mouseover", function( event ) {   
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+})
+
+
+
+const containerBtn = document.querySelector('.container')
+containerBtn.addEventListener('click', function(event) {
+
+//  target you are clicking on
+console.log(`this is my target: ${event.target}`) 
+  
+// target that you added the event listener to.
+console.log(`this is my current target: ${event.currentTarget}`)
+
+    event.stopPropagation();
+})
