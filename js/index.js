4c78ad0fbd9a54cb469117501ee6ae25f7adb23f
Marc Thiongo <marcthuo@gmail.com>
2019-02-05 23:24:43 -0500

    added changes JS

--- js/index.js
+++ js/index.js
@@ -1,26 +1,42 @@
 // Your code goes here
 
 const logoHeading = document.querySelector(".logo-heading");
-logoHeading.addEventListener('keydown', function(event) {
-  console.log(`this is the blank key: ${event.code}`)
+logoHeading.addEventListener('keydown', e => {
+  console.log(`this is the blank key: ${e.code}`)
 })
 
 const nav = document.querySelector(".nav");
-nav.addEventListener("mouseover", function( event ) {   
+nav.addEventListener("mouseover", e => {   
     // highlight the mouseover target
-    event.target.style.color = "orange";
+    e.target.style.color = "orange";
 })
 
+const textContent = document.querySelector('.text-content');
+// dblclick to make underlined
+textContent.addEventListener('dblclick', e => {
+  e.target.style.textDecoration = "underline";
+});
 
+// function logSelection(e) {
+//     const destination = document.querySelector('.destination, p');
+//     const selection = e.target.value.substring(e.target.selectionStart, e.target.selectionEnd);
+//     destination.textContent = alert(`You selected: ${selection}`);
+//   }
 
-const containerBtn = document.querySelector('.container')
-containerBtn.addEventListener('click', function(event) {
+const div = document.querySelector('btn');
+
+div.addEventListener('Sign Me Up!', e => {
+div.innerHTML = `Sign Me Up count: ${e.detail}`;
+});
+
+const mainNavigation = document.querySelector('.main-navigation')
+mainNavigation.addEventListener('click', e => {
 
 //  target you are clicking on
-console.log(`this is my target: ${event.target}`) 
+console.log(`this is my target: ${e.target}`) 
   
 // target that you added the event listener to.
-console.log(`this is my current target: ${event.currentTarget}`)
+console.log(`this is my current target: ${e.currentTarget}`)
 
-    event.stopPropagation();
+    e.stopPropagation();
 })
