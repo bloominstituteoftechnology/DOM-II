74828152f41557962892176b17ac5c6be8f6b602
Nathan <s6nculve@gmail.com>
2019-02-07 11:35:15 -0800

    Updated function(event) in window.addEventListener. Project Complete.

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 
 // When the doc loads this alert will appear explaining what you can do on the site.
   window.addEventListener("load", function(event) {
-    alert("On this site, you may do the following: Hover over the Fun Bus logo to double its size, drag the intro img to increase its size, double click the nav links to change their background color to red, press any keyboard key to make the header disappear, hover over the header to make the header reappear, hover over a expedition title to change its color to MediumSeaGreen, Double click a sign up button to prompt an input box and then an alert, Right click the footer to change its background color to MediumTurquoise.");});
+    alert("On this site, you may do the following: Hover over the Fun Bus logo to double its size, drag the intro img to increase its size, double click the nav links to change their background color to red, press any keyboard key to make the header disappear, hover over the header to make the header reappear, hover over a expedition title to change its color to MediumSeaGreen, hover over a Adventure Awaits title to change its color to Maroon, Double click a sign up button to prompt an input box and then an alert, Right click the footer to change its background color to MediumTurquoise.");});
 
 // Hover over the Fun Bus logo to double its size.
   const navLogoHeading = document.querySelector('.logo-heading');
