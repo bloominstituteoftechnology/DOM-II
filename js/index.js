c7885d9a26db392d2ed95d3e52e5e73112cf394c
ntsum <natsukajiwaram@gmail.com>
2019-03-05 14:05:25 -0500

    Stopped all of the nav links from refreshing

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const noRefresh = document.querySelector('.nav');
+noRefresh.addEventListener('click', function(event){
+ console.log(`I was clicked hehe`);
+event.preventDefault();
+});
