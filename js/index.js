fa3b2c63dff53c468f90554883686aa170c3c8ba
MAllen07 <mrsmallen07@gmail.com>
2019-03-12 19:05:22 -0400

    created mousover on the nanav and the footer changing colors, created a background change on click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 // Your code goes here
+
+//mouseover//
+const nav = document.querySelectorAll('a');
+const navHover = function(e){
+    e.target.style.color = 'orange';
+    e.preventDefault();
+}
+const navHoverNot = function(e){
+    e.target.style.color = '';
+}
+nav.forEach (item => {item.addEventListener('mouseover', navHover)});
+nav.forEach (item => {item.addEventListener('mouseleave', navHoverNot)});
+
+
+//bottom button//
+const button = document.querySelectorAll('.btn');
+const buttonClicked = function(e) {
+    e.target.style.backgroundColor = 'grey';
+    e.target.style.color = 'blue';
+}
+button.forEach(item => {item.addEventListener('click', buttonClicked)});
+
+
+//background color scroll //
+const body = document.querySelector('body')
+body.addEventListener('keyup', function() {
+    body.style.background = 'grey';
+})
+
+//footer//
+const footerText = document.querySelector('.footer p');
+footerText.addEventListener('mouseover', function() {
+    footerText.style.color = 'green';
+})
+
