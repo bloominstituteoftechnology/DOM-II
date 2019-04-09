74918f78e0ff27811441e5b82c8de1c008ce3a06
alyssa-hatfield <47366310+xCthaeh@users.noreply.github.com>
2019-04-09 13:10:56 -0400

    Fun Bus text transformed when scrolling up and down

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+//Fun Bus text transformed when scrolling up and down
+const titleH1 = document.querySelector('h1');
+
+window.addEventListener('wheel', e => {
+  if(e.wheelDelta === -120) {
+    titleH1.textContent = 'Weeeeeeeee';
+  } else {
+    titleH1.textContent = 'Woooooooooo';
+  }
+});
+
