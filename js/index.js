be621ef70ddf8e5eb48e8647d39c038a6c203b28
BrannanC <brannanconrad@gmail.com>
2019-04-09 17:58:16 -0500

    Rocket bus

--- js/index.js
+++ js/index.js
@@ -1,84 +1,84 @@
 // Your code goes here
-// let obj = {
-//     scale: 1.0,
-//     px: 4,
-//     hash: 25,
-//     shw: `4px 4px 1px #250000`
-// }
+let obj = {
+    scale: 1.0,
+    px: 4,
+    hash: 25,
+    shw: `4px 4px 1px #250000`
+}
 
-// const nav = document.querySelector('.nav-container');
-// navItems = nav.querySelectorAll('a');
-// // ********* 1
-// navItems.forEach(a => {
-//     a.addEventListener('mouseleave', function(){
-//         obj.scale += 0.1;
-//         a.style.transform = `scale(${obj.scale})`;
-//     })
-// });
+const nav = document.querySelector('.nav-container');
+navItems = nav.querySelectorAll('a');
+// ********* 1
+navItems.forEach(a => {
+    a.addEventListener('mouseleave', function(){
+        obj.scale += 0.1;
+        a.style.transform = `scale(${obj.scale})`;
+    })
+});
 
-// const body = document.querySelector('body');
-// // ********* 2
-// body.addEventListener('click', function(e){
-//     e.target.classList.toggle('darkMode');
-// })
+const body = document.querySelector('body');
+// ********* 2
+body.addEventListener('click', function(e){
+    e.target.classList.toggle('darkMode');
+})
 
-// // ********* 3
-// body.addEventListener('wheel', function(e){
-//     e.target.style.boxShadow = obj.shw;
-//     obj.px += 1;
-//     if(obj.hash <= 97){
-//       obj.hash += 2;
-//     }  
-//     obj.shw = obj.shw + `, ${obj.px}px ${obj.px}px 1px #${obj.hash}0000`;
-//   })
+// ********* 3
+body.addEventListener('wheel', function(e){
+    e.target.style.boxShadow = obj.shw;
+    obj.px += 1;
+    if(obj.hash <= 97){
+      obj.hash += 2;
+    }  
+    obj.shw = obj.shw + `, ${obj.px}px ${obj.px}px 1px #${obj.hash}0000`;
+  })
 
-//   const imgs = document.querySelectorAll('img');
-// // ********* 4
-//   imgs.forEach(x => {
-//       x.addEventListener('mouseover', function(){
-//         x.style.transformOrigin = `bottom left`;
-//         x.style.transform = `scale(1.1)`;
-//       })
-//   })
-// // ********* 5
-//   imgs.forEach(x => {
-//     x.addEventListener('mouseleave', function(){
-//       x.style.transformOrigin = `bottom left`;
-//       x.style.transform = `scale(0.9)`;
-//     })
-// })
-// // ********* 6
-// window.addEventListener('resize', function(){
-//     body.style.background = '#' + Math.floor(Math.random()*16777215).toString(16);
-// })
-// // ********* 7
-// window.addEventListener('dblclick', function(){
-//     document.location.reload();
-// })
+  const imgs = document.querySelectorAll('img');
+// ********* 4
+  imgs.forEach(x => {
+      x.addEventListener('mouseover', function(){
+        x.style.transformOrigin = `bottom left`;
+        x.style.transform = `scale(1.1)`;
+      })
+  })
+// ********* 5
+  imgs.forEach(x => {
+    x.addEventListener('mouseleave', function(){
+      x.style.transformOrigin = `bottom left`;
+      x.style.transform = `scale(0.9)`;
+    })
+})
+// ********* 6
+window.addEventListener('resize', function(){
+    body.style.background = '#' + Math.floor(Math.random()*16777215).toString(16);
+})
+// ********* 7
+window.addEventListener('dblclick', function(){
+    document.location.reload();
+})
 
-// // ********* 8
-// const home = document.querySelector('.home');
-// home.addEventListener('copy', function(){
-//     alert("Woa, hey there coyboy, don't copy me");
-// })
+// ********* 8
+const home = document.querySelector('.home');
+home.addEventListener('copy', function(){
+    alert("Woa, hey there coyboy, don't copy me");
+})
 
-// // ********* 9
-// home.addEventListener('contextmenu', function(e){
-//     e.target.style.color = 'red';
-// })
+// ********* 9
+home.addEventListener('contextmenu', function(e){
+    e.target.style.color = 'red';
+})
 
-// // ********* 10 - stopImmediatePropagation
-// imgs.forEach(x => x.addEventListener('dblclick', function(e){
-//     x.style.display = 'none';
-//     e.stopImmediatePropagation();
-// }))
+// ********* 10 - stopImmediatePropagation
+imgs.forEach(x => x.addEventListener('dblclick', function(e){
+    x.style.display = 'none';
+    e.stopImmediatePropagation();
+}))
 
-// // ********* preventDefault
-// navItems.forEach(a => {
-//     a.addEventListener('click', function(e){
-//         e.preventDefault();
-//     })
-// });
+// ********* preventDefault
+navItems.forEach(a => {
+    a.addEventListener('click', function(e){
+        e.preventDefault();
+    })
+});
 
 // *********** GSAP
 
@@ -120,4 +120,40 @@ GSAPimgs.forEach(x => {
             scale: 1,
         });
     }) 
+})
+
+const audio = document.querySelector('audio');
+window.addEventListener('click', () => {
+    audio.currentTime = 0;
+    audio.play();
+})
+
+// audio.addEventListener('ended', () => {
+//     audio.play();
+// })
+
+const the_fun_bus = document.querySelector('.the-fun-bus');
+audio.addEventListener('play', () => {
+    TweenMax.fromTo(the_fun_bus, 9, {
+        display: 'block',
+        transformPerspective:600, 
+        rotationX: 10,
+        rotationZ: 10,
+        rotationY: 30,
+        x: '-50px',
+        y: '-250',
+    },{
+        transformPerspective:600, 
+        display: 'block',
+        rotationX: -10,
+        rotationZ: -10,
+        rotationY: -30,
+        ease: Elastic.easeOut.config(1, 0.3),
+        x: '50px',
+        y: '-1800px',
+    });
+})
+
+audio.addEventListener('ended', () => {
+    the_fun_bus.style.display = 'none';
 })
