0acc5d5b835d1791affd23d8b81aac30df368a09
JustSpokenCodes <JustSpokenCodes@users.noreply.github.com>
2019-04-13 09:36:43 -0700

    this site will be a thing of the past

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+const foot = document.querySelector('.footer')
+foot.addEventListener('mouseover', function(e){
+    e.target.style.display = 'none'
+})
+
+const bigLetter = document.querySelector('h1')
+bigLetter.addEventListener('keydown', function(e){
+    e.target.fontSize = '8rem'
+})
+
+const para = document.querySelector('p')
+para.addEventListener('wheel', function(e){
+    para.lineHeight = '3'
+})
+
+const content = document.querySelector('.content-section')
+content.addEventListener()
