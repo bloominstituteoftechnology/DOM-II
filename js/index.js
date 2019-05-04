8ee1fbb2966e138c5da150ac4c6c2094d67eb1f3
suvash1 <suvash1@gmail.com>
2019-05-04 13:15:57 -0400

    added two event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+let header = document.querySelector('header');
+let logoHeading = document.querySelector('.logo-heading');
+
+//console.log(header);
+
+//
+header.addEventListener("dblclick", function(){
+    //console.log("this worked");
+    header.style.backgroundColor = "red";
+});
+
+//
+logoHeading.addEventListener("hover", function(){
+    console.log("Scrolling works");
+});
+
+
+
