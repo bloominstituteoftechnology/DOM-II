aced6602f719c37e1034ed6f8113c4b5439fe3fb
Tara Sherman <taradelin@gmail.com>
2019-05-04 10:27:16 -0400

    mouseenter: pointer disappears when hovering over bus image

--- js/index.js
+++ js/index.js
@@ -2,10 +2,18 @@
 
 // Selectors
 let navItems = document.querySelectorAll('.nav-link');
+let introImg = document.querySelector('.intro img');
+
 
 //doubleclick: changes color of nav items
 navItems.forEach(event => {
     event.addEventListener('dblclick', (event) => {
         event.target.style.color = 'purple'; 
     });
+});
+
+// mouseenter: pointer disappears when hovering over bus image
+introImg.addEventListener('mouseenter', (event) => {
+    event.target.style.cursor = 'none';
+    event.stopPropagation;
 });
