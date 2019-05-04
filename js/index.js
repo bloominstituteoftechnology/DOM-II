398bd479309bf6004f44822865d736a995190f96
Sir-Williams <sirjohnwilliams@live.com>
2019-05-04 14:17:21 -0700

    made constructors for mouse over, key down, wheel drag & drop, load and resize

--- js/index.js
+++ js/index.js
@@ -1,1 +1,108 @@
 // Your code goes here
+
+//Mouse Over / changes anchor colors
+const navAnchors = document.querySelector('.nav');
+
+navAnchors.addEventListener('mouseover', (event) =>{
+    navAnchors.style.backgroundColor = 'gold';
+    console.log('gold nav bar')
+    event.stopPropagation();
+});
+
+//Key Down / Press any key for a secret message for any plagerizers
+const pageBody = document.querySelector('body');
+const noPlagerismFont = document.querySelector('.footer p');
+
+pageBody.addEventListener('keydown', (event) => {
+    noPlagerismFont.textContent = 'Copyright Fun Bus 2018. https://www.copyright.gov/help/faq/definitions.html';
+    console.log('alerted plagerizers')
+    event.stopPropagation();
+});
+
+//Wheel / make font size smaller for h1 and changes first 'p' color to black
+const title = document.querySelector('h1');
+
+pageBody.addEventListener('wheel', (event) =>{
+    title.style.fontSize = '22px';
+    console.log('font size changed')
+
+    loadFontColor.style.color = 'black';
+
+    event.stopPropagation();
+});
+
+//Drag & Drop / drags the second image
+const dragImage = document.querySelector('.content-section img')
+
+dragImage.addEventListener('drag', (event) => {
+    console.log('draggaing')
+
+    event.stopPropagation();
+}, false);
+
+dragImage.addEventListener("dragstart", (event) => {
+    dragged = event.target;
+    event.target.style.opacity = .5;
+
+    console.log('drag start')
+
+    event.stopPropagation();
+  }, false);
+
+  dragImage.addEventListener("dragend", (event) => {
+    event.target.style.opacity = '3%';
+
+    console.log('drag end')
+
+    event.stopPropagation();
+  }, false);
+
+//Load / 
+const loadFontColor = document.querySelector('p')
+
+window.addEventListener('load', (event) => {
+    console.log('text is loaded as white')
+    loadFontColor.style.color = 'white';
+    
+    event.stopPropagation();
+});
+
+//Focus / changes input background to grey and text to green
+const name = document.querySelector('input')
+name.addEventListener('focus', (event) => {
+    event.target.style.background = 'lightgrey';
+    event.target.style.color = 'green';
+    
+    console.log('input changed to corrected colors')
+
+    event.stopPropagation();
+});
+
+//Resize / 
+const lastImage = document.querySelector('.content-destination img')
+const heightOutput = document.querySelector('#height');
+const widthOutput = document.querySelector('#width');
+
+window.addEventListener('resize', (event) => {
+    console.log(heightOutput, widthOutput)
+
+    event.stopPropagation();
+});
+
+//Scroll / 
+const scroll = document
+
+
+
+//Select / 
+
+
+//Double Click / 
+
+
+
+
+
+
+
+
