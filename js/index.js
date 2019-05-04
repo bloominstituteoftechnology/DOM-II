4958df9019bb5ab2ae34e31735e7b7678850cbac
Tara Sherman <taradelin@gmail.com>
2019-05-04 10:28:51 -0400

    mouseover/mouseleave: text expands & then shrinks

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@
 // Selectors
 let navItems = document.querySelectorAll('.nav-link');
 let introImg = document.querySelector('.intro img');
+let logoHeading = document.querySelector('.logo-heading');
 
 
 //doubleclick: changes color of nav items
@@ -15,5 +16,15 @@ navItems.forEach(event => {
 // mouseenter: pointer disappears when hovering over bus image
 introImg.addEventListener('mouseenter', (event) => {
     event.target.style.cursor = 'none';
+    event.stopPropagation;
+});
+
+// mouseover/mouseleave: text expands and then shrinks
+logoHeading.addEventListener('mouseover', (event) => {
+    event.target.style.fontSize = '6rem';
+    event.stopPropagation
+});
+logoHeading.addEventListener('mouseleave', (event) => {
+    event.target.style.fontSize = '4rem';
     event.stopPropagation;
 });
