c7d0bcc42ea4616bd8d58dfe0581f67ae2ea91ba
Tara Sherman <taradelin@gmail.com>
2019-05-04 10:25:06 -0400

    doubleclick: changes color of nav items

--- js/index.js
+++ js/index.js
@@ -1,2 +1,11 @@
 // initial commit
 
+// Selectors
+let navItems = document.querySelectorAll('.nav-link');
+
+//doubleclick: changes color of nav items
+navItems.forEach(event => {
+    event.addEventListener('dblclick', (event) => {
+        event.target.style.color = 'purple'; 
+    });
+});
