6ffbca1a1772c285db1449ce7b662d426451afae
suvash1 <suvash1@gmail.com>
2019-05-06 19:29:33 -0400

    still need some more work, need to nest the events and prevent propagation

--- js/index.js
+++ js/index.js
@@ -3,7 +3,16 @@ let header = document.querySelector('header');
 let logoHeading = document.querySelector('.logo-heading');
 let btn1 = document.querySelector('.destination .btn');
 
-console.log(btn1);
+let intro =document.querySelector(".intro");
+
+let headerImg = document.querySelector(".intro img");
+
+let headerTxt = document.querySelector(".intro p");
+
+//
+
+headerTxt.addEventListener("select", function(){
+headerTxt.style.color = "orange"});
 
 //
 header.addEventListener("dblclick", function(){
@@ -20,6 +29,14 @@ logoHeading.addEventListener("mouseover", function(){
 
 btn1.addEventListener("wheel",function(){
     btn1.style.width = "400px";
+})
+
+intro.addEventListener("mouseover", function(){
+    intro.style.backgroundColor = "blue";
+})
+
+headerImg.addEventListener("drag",function(){
+    headerImg.style.border = "22px solid orange";
 })
 
 
