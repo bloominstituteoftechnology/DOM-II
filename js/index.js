25d8e4d4ac2359de8fe2381a0d1d55535b864399
Amber Pittman <amber@amberpittman.com>
2019-05-10 22:53:14 -0400

    Created 2 events - dblclick & mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+
+// DBLCLICK
+const logo = document.querySelector('.logo-heading');
+console.log(logo);
+logo.addEventListener('dblclick', function(e) {
+  console.log("The Logo was double clicked!");
+});
+
+// MOUSEOVER
+const busImg = document.querySelector(".busImg");
+console.log(busImg);
+busImg.addEventListener("mouseover", function(e) {
+  console.log("Mouse went over the Bus Image!");
+  busImg.style.width = "80%";
+});
