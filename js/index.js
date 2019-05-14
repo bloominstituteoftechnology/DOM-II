13d12fcc239ed067a45310523d9f0a92067fd7c6
Yusuf Abdulkarim <haywhyze@gmail.com>
2019-05-14 12:54:31 +0100

    Initial Commit - add mouseover event for nav-links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+const eventListener = (element, event, cb) => {
+  element.addEventListener(event, cb);
+}
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink.forEach(e => eventListener(e, 'mouseover', () => {
+  e.classList.add('red');
+}))
+
+navLink.forEach(e => eventListener(e, 'mouseout', () => {
+  e.classList.remove('red');
+}))
+
