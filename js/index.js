ba5935464b964f2e472fe2180f37f15b0728e392
Davindar <davdavdav4@gmail.com>
2019-05-21 23:53:46 +0100

    two mouse events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+//This expands the size of the image, it you click it twice
+const card = document.querySelector('.intro');
+
+card.addEventListener('dblclick', function (e) {
+  card.classList.toggle('large');
+});
+
+//
+let test = document.querySelector("nav");
+
+test.addEventListener("mouseover", function( event ) {   
+    event.target.style.color = "orange";
+});
