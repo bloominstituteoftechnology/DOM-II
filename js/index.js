fa96e50d3cc8dc80e93be1c9d4e5b653ff2fab06
Aaron Johnson <aaron.johnson1031@gmail.com>
2019-06-08 13:58:22 -0400

    added first two event listeners, click, mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+//Variable Declarations
+const home = document.querySelector(".home");
+const heroImg = document.querySelector(".container .intro img");
+
+const btns = document
+  .querySelectorAll(".btn")
+  .forEach(elem => elem.addEventListener("click", playJingle, true));
+
+const happyAudio = new Audio("audio/happy.mp3");
+
+const modal = document.createElement("div");
+modal.classList.add("modal");
+home.prepend(modal);
+
+//Functions
+function moveHero(e) {
+  e.target.style.right = 0;
+  console.log("heroMoved");
+  e.target.removeEventListener("mouseover", moveHero, true);
+}
+
+function playJingle(e) {
+  happyAudio.play();
+}
+
+//Event Listeners
+heroImg.addEventListener("mouseover", moveHero, true);
+
+// ***Unique Listeners***
+// click
+// mouseover
+//resize
