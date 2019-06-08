46c947b4c775cb71b80e37e085f81b57811564cc
Brian <lawmen218@yahoo.com>
2019-06-07 23:38:26 -0400

    working on it

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
 // Your code goes here
+// mouseover
+// keydown
+// wheel
+// drag / drop
+// load
+// focus
+// resize
+// scroll
+// select
+// dblclick
+
+
+const navLink = document.querySelector('.nav');
+navLink.addEventListener("dblclick", (event) => {
+    event.target.style.backgroundColor = "green";
+});
+
+const funBus = document.querySelector('.logo-heading');
+funBus.addEventListener("mouseover", (event) => {
+    funBus.style.fontSize = "5rem";
+});
+
+const keyUsed = document.querySelector("body");
+keyUsed.addEventListener("keydown", (event) => {
+    console.log("key was pressed!")
+});
+
+const wheelZoom = document.querySelector(".img-content");
+wheelZoom.addEventListener("wheel", (event) => {
+    console.log("this is zooming in")
+});
+
+const dragEm = document.querySelector(".content-destination.img");
+console.log(dragEm);
