030c9e4383d1b56188edd7f61fc9b5ded85dbead
Aaron Johnson <aaron.johnson1031@gmail.com>
2019-06-08 15:17:51 -0400

    added scroll and resize functions

--- js/index.js
+++ js/index.js
@@ -22,7 +22,6 @@ myInput.textContent =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut aliquam at urna eget finibus. Fusce facilisis nulla posuere libero volutpat, eu eleifend odio venenatis. Etiam vestibulum sapien libero, in porttitor est scelerisque nec. Etiam eget pharetra ante. In id sollicitudin nulla. Aliquam ullamcorper sagittis dolor at ultricies. Sed maximus risus at massa fermentum vehicula. Pellentesque malesuada venenatis orci, aliquet vehicula quam tincidunt at. Ut non orci vitae magna laoreet pharetra. Nam rhoncus nibh eu turpis ullamcorper aliquam. Nulla ornare nec nisi eget lobortis. Morbi condimentum mattis mi, sed placerat mauris euismod eu. Praesent varius metus sit amet tortor iaculis, at accumsan metus molestie. Proin dignissim laoreet tellus, nec faucibus ante. Sed purus ex, bibendum quis gravida a, euismod quis erat. Aenean nec aliquam dui, sed congue metus. Cras sollicitudin laoreet vulputate. Aliquam bibendum sodales dui, a semper diam interdum ut. Aliquam aliquam volutpat libero. Vivamus viverra consectetur leo in tincidunt. Curabitur fermentum elit eu scelerisque pharetra. Integer tincidunt convallis condimentum. Praesent aliquam elit eu lectus suscipit luctus sed sit amet metus. Maecenas sem libero, tincidunt id pretium sed, aliquet nec urna. Cras euismod odio odio, ac rhoncus lorem varius eget. Donec velit metus, imperdiet sit amet turpis eget, euismod molestie justo. Aenean pellentesque velit vel ex iaculis, ut sagittis dui sodales. Morbi pellentesque erat non ex dapibus condimentum. Donec congue sem non libero viverra, et egestas orci semper. Phasellus facilisis feugiat ligula et vehicula. Phasellus consectetur magna diam, in efficitur elit efficitur in. Integer sit amet quam a metus vulputate varius. Sed porttitor eros et nunc cursus bibendum. Sed augue sapien, rhoncus nec congue vitae, euismod eget elit. Etiam tortor turpis, luctus id gravida ac, dignissim eu neque. Nullam malesuada luctus vulputate. Pellentesque semper ultricies lobortis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In aliquam convallis tellus, vitae venenatis erat cursus at. Pellentesque vitae facilisis sapien. Phasellus vel vulputate quam. Quisque pretium maximus urna non pretium. Quisque consequat lorem tristique est imperdiet, sit amet volutpat turpis cursus. Proin blandit metus quis volutpat tristique. Fusce aliquam sem ipsum, quis sagittis metus elementum vel. Duis ut dolor ligula. Proin posuere est id turpis faucibus, nec viverra purus accumsan. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc ornare molestie semper. Morbi vel dui a odio efficitur lobortis id in lectus. Nulla facilisi. Nullam quis odio luctus, condimentum neque a, cursus magna. Sed porta id odio vel fermentum. Quisque semper ex interdum orci tincidunt, scelerisque consequat sapien finibus. Proin ac tristique nunc. Aenean gravida consequat nunc, vel tristique lorem pellentesque et. Pellentesque non quam turpis. Donec gravida quis libero gravida placerat. Aenean id ex at odio ultrices lobortis.";
 
 home.appendChild(myInput);
-
 //Functions
 function moveHero(e) {
   e.target.style.right = 0;
@@ -55,12 +54,26 @@ function logSelection(e) {
   console.log(`You selected: ${selection}`);
 }
 
+function logYOffset() {
+  console.log(`Page Y-Offset ${window.pageYOffset}`);
+}
+
+function logPageDimensions() {
+  console.log(
+    `Window Width: ${window.innerWidth}, Window Height: ${window.innerHeight}`
+  );
+}
+
 //Event Listeners
 heroImg.addEventListener("mouseover", moveHero, true);
 document.addEventListener("keydown", displayMessage, true);
 myInput.onselect = logSelection;
+document.addEventListener("scroll", logYOffset, true);
+window.onresize = logPageDimensions;
 // ***Unique Listeners***
 // click
 // mouseover
 // keydown
 // select
+// scroll
+// resize
