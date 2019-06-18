2844ce6acd815dc9f842b259792db22880980a6c
noahfranco <noahfra@gmail.com>
2019-06-18 16:42:44 -0400

    finished mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+// ----- Mouseover -----
+let content = document.querySelector(" h2"); 
+
+content.addEventListener("mouseenter", function(event){
+    event.target,style.color = "blue"; 
+setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+
+content.addEventListener("mouseover", function(event){
+    event.target.style.color = "green"
+    setTimeout(function() {
+        event.target.style.color = "";
+      }, 500);
+    }, false);
+
+// ----- KeyDown -----
+
+
+
+
+    
