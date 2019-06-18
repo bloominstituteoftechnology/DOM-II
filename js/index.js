7bdbe7a75a8fc089d4e1e99f9066f3fd8ee05bf7
dangolbeeker <scottb305@outlook.com>
2019-06-18 17:19:30 -0400

    mvp

--- js/index.js
+++ js/index.js
@@ -1,25 +1,34 @@
 // Your code goes here
 const navTags = document.querySelectorAll('nav a');
 
+const logo = document.querySelector('h1');
 navTags[0].addEventListener('mouseover', (event) => {
     navTags[0].classList.toggle("makeRed");
 });
 
 navTags[1].addEventListener('click', (event) => {
     navTags[1].classList.toggle("makeRed");
+    event.preventDefault();
 });
+
 
 navTags[2].addEventListener('dblclick', (event) => {
     navTags[2].classList.toggle("makeRed");
 });
 
-const buttons = document.querySelectorAll('.btn');
-
-
-buttons.forEach((element) => {
-    element.addEventListener('click', (event) => {
-        TweenMax.to(element, 2 {
-            backgroundColor: 'red'
-        });
-    });
+navTags[3].addEventListener('mousedown', (event) => {
+    navTags[3].classList.toggle("makeRed");
 });
+
+TweenMax.from(logo, 1.5, {
+    y: -100,
+    ease: Back.easeOut.config(1.7)
+});
+
+window.addEventListener('load', (event) => {
+    alert('Website has been loaded');
+});
+window.addEventListener('rezise', (event) => {
+    console.log('The window has been resized')
+});
+
