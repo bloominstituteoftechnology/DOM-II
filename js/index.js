70cfcf3163356496aeb38942ddc9b5fb8c31e3b4
Suparada Saitalae <ann.suparada@gmail.com>
2019-06-18 17:46:45 -0500

    focus, drag/drop,

--- js/index.js
+++ js/index.js
@@ -15,23 +15,63 @@ wheelDestination.addEventListener("wheel", (e) => {e.style.fontSize = "35px";
 
 
 //
-const signUpBtn = document.querySelector('.btn');
-signUpBtn.addEventListener('dblclick', () => {
-	
-});
+const letsGo = document.querySelector('.text-content h2');
+console.log(letsGo)
+letsGo.addEventListener.innerHTML = "YAYYYY!";
 
-
+//load
 window.addEventListener('load', (event) => {
     console.log('page is fully loaded');
 })
 
-
+//focus
 const password = document.querySelector('input[type="password"]');
-
 password.addEventListener('focus', (event) => {
   event.target.style.background = 'pink';    
 });
-
 password.addEventListener('blur', (event) => {
   event.target.style.background = '';    
 });
+
+//drag / drop
+var dragged;
+document.addEventListener("drag", function(event) {
+}, false);
+
+document.addEventListener("dragstart", function(event) {
+  dragged = event.target;
+  event.target.style.opacity = .5;
+}, false);
+
+document.addEventListener("dragend", function(event) {
+  event.target.style.opacity = "";
+}, false);
+
+document.addEventListener("dragover", function(event) {
+  event.preventDefault();
+}, false);
+
+document.addEventListener("dragenter", function(event) {
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "purple";
+  }
+
+}, false);
+
+document.addEventListener("dragleave", function(event) {
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+  }
+
+}, false);
+
+document.addEventListener("drop", function(event) {
+  event.preventDefault();
+  if (event.target.className == "dropzone") {
+    event.target.style.background = "";
+    dragged.parentNode.removeChild( dragged );
+    event.target.appendChild( dragged );
+  }
+}, false);
+
+//
