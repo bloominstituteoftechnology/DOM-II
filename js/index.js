771116baee249894aa74d7a6c7898fd69923f719
johnschneider1 <johnlawschneider@gmail.com>
2019-06-18 14:15:01 -0500

    setting up daily project starting with selectors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+// Using your index.js file, create 10 unique event listeners. using your creativity to make the Fun Bus site more interactive. Here are some unique events you could try to use:
+//  mouseover
+//  keydown
+//  wheel
+//  drag / drop
+//  load
+//  focus
+//  resize
+//  scroll
+//  select
+//  dblclick
+
+// selectors
+
+const busImg = document.querySelector(".intro img");
+const header = document.querySelector("header");
