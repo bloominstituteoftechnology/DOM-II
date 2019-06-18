777a02a8e74d3c42eb9db9acf52c614be43ef033
dangolbeeker <scottb305@outlook.com>
2019-06-18 16:03:24 -0400

    added 3 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const navTags = document.querySelectorAll('nav a');
+
+navTags[0].addEventListener('mouseover', (event) => {
+    navTags[0].classList.toggle("makeRed");
+});
+
+navTags[1].addEventListener('click', (event) => {
+    navTags[1].classList.toggle("makeRed");
+});
+
+navTags[2].addEventListener('dblclick', (event) => {
+    navTags[2].classList.toggle("makeRed");
+});
+
+const buttons = document.querySelectorAll('.btn');
+
+
+buttons.forEach((element) => {
+    element.addEventListener('click', (event) => {
+        TweenMax.to(element, 2 {
+            backgroundColor: 'red'
+        });
+    });
+});
