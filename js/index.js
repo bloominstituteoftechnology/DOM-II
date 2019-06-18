84a31949291340169a27a5a9c66a0a61731dc950
daniela-arias-ordonez <daniela.arias2000@gmail.com>
2019-06-18 17:34:34 -0500

    Added new event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+const adventure = document.getElementById("adventure");
+const destination = document.getElementById("destination");
+const funbus = document.getElementById("funbus");
+const fun = document.getElementById("fun");
+
+
+adventure.onmouseover = function (){
+    alert("Have fun!");
+}
+
+window.onload = function (){
+    alert("Choose a destination!");    
+}
+// funbus.onwheel = function (){
+    
+// }
+// fun.ondrag / drop = function (){
+    
+// }
+// focus
+// resize
+// scroll
+// select
+// dblclick
