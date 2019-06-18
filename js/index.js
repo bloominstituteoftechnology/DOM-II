f2cd732e991c049f32d34af5880ec156dd9d5ebe
Kelly M <moreira.kd@outlook.com>
2019-06-18 15:54:07 -0400

    3 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+
+// One
+
+const nav = document.querySelectorAll('.nav-link');
+nav.forEach((ele) => ele.addEventListener('mouseover', (event) => event.target.style.color = 'red'));
+// nav.forEach((ele) => ele.addEventListener('mouseover', (event) => event.target.style.fontsize = '5rem'));
+
+
+// Two
+
+const para = document.querySelectorAll('p');
+para.forEach((eleP) => eleP.addEventListener('click', (event) => event.target.style.color = 'blue'));
+
+
+//Three
+
+const btns = document.querySelectorAll('.btn');
+btns.forEach((eleBtn) => eleBtn.addEventListener('mouseover', (event) => event.target.style.color = 'orange'));
