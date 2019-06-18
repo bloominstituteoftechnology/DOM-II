217bb54ad4741232c20acdb43468ed40c25221d9
johnschneider1 <johnlawschneider@gmail.com>
2019-06-18 15:56:55 -0500

    eventlisten in progress

--- js/index.js
+++ js/index.js
@@ -14,12 +14,51 @@
 // selectors
 
 const logoHeading = document.querySelector("h1");
+console.log(logoHeading);
 const navLinks = document.querySelectorAll("nav a");
+console.log(navLinks);
 const busImg = document.querySelector(".intro img");
-const logo = document.querySelector(".logo.heading");
+console.log(busImg);
+
 const titles = document.querySelectorAll("h2");
+console.log(titles);
 const mapImg = document.querySelector(".img-content img");
+console.log(mapImg);
 const buttons = document.querySelectorAll(".btn");
+console.log(buttons);
 const secondTitles = document.querySelectorAll("h4");
+console.log(secondTitles);
 const footer = document.querySelector(".footer p");
+console.log(footer);
 const allText = document.querySelectorAll("p");
+console.log(allText);
+
+// even listeners
+
+buttons.forEach(function(currentValue, index) {
+  currentValue.style.color = "green";
+  buttons[0].addEventListener(
+    "mouseover",
+    function(event) {
+      console.log(`buttons fired!`);
+      event.target.style.color = "purple";
+      setTimeout(function() {
+        event.target.style.color = "";
+      }, 500);
+    },
+    false
+  );
+  buttons[1].addEventListener();
+});
+
+busImg.addEventListener("click", function() {
+  busImg.style.display = "none";
+});
+
+mapImg.addEventListener("mouseover", function() {
+  mapImg.style.display = "";
+});
+
+logoHeading.addEventListener("blur", event => {
+  event.target.style.background = "";
+});
