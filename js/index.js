f5ca152e3c11093ffff12605f74c20bd1300e2ef
noahfranco <noahfra@gmail.com>
2019-06-18 18:10:37 -0400

    finished click element

--- js/index.js
+++ js/index.js
@@ -2,23 +2,27 @@
 // ----- Mouseover -----
 let content = document.querySelector(" h2"); 
 
-content.addEventListener("mouseenter", function(event){
-    event.target,style.color = "blue"; 
-setTimeout(function() {
-    event.target.style.color = "";
-  }, 500);
-}, false);
-
-
 content.addEventListener("mouseover", function(event){
     event.target.style.color = "green"
     setTimeout(function() {
         event.target.style.color = "";
-      }, 500);
+      }, 2000);
     }, false);
 
-// ----- KeyDown -----
 
+// ----- Wheel -----
+const theWeel = document.querySelector(".intro img"); 
+theWeel.addEventListener("wheel", index => { 
+    theWeel.
+    
+})
+
+
+// ----- Click -----
+const pickDestination = document.querySelector(".content-destination h2"); 
+pickDestination.addEventListener("click", event => {
+    pickDestination.textContent = "What am I doing with my life?"
+}); 
 
 
 
