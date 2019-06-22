4685a67dbb58292c3b5339976b3ad395f4e55174
Nick Dailey <n.dailey000@icloud.com>
2019-06-21 22:17:06 -0500

    final for the night

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', function(){
+    logoHeading.style.fontSize = '2rem'; 
+});
+
+
+const btn = document.querySelector('.btn');
+btn.addEventListener('dblclick', function(){
+    btn.style.backgroundColor = 'black'; 
+});
+
+
+const intro = document.querySelector('.intro');
+intro.addEventListener('wheel', function(){
+    window.scrollTo(0, 1200);
+});
+
+
+const footer = document.querySelector('.footer p');
+footer.addEventListener('click', function(){
+  footer.style.fontSize = '20rem';
+});
+
+
+const introImg = document.querySelector('.intro img');
+introImg.addEventListener('contextmenu', function(){
+    introImg.style.display = 'none';
+});
+
