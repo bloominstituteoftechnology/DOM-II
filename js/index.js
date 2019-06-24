9c85f7b7350d889415882732a2c15836c9ed894d
3Dia <tmgantt85@gmail.com>
2019-06-23 23:22:39 -0400

    a

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+
+let mouseOver = document.querySelector('.container nav-container');
+
+mouseOver.addEventListener("mouseover", function(event){
+   event.target.style.color = "pink";
+   setTimeout(function(){
+       event.target.style.color="";
+   }, 500 );
+}, false);
