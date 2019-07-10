924664408ce50da421b489fa765ccdc6093f3565
Kelly M <moreira.kd@outlook.com>
2019-07-10 15:18:10 -0400

    edits from yesterday

--- js/index.js
+++ js/index.js
@@ -6,7 +6,21 @@
 const nav = document.querySelectorAll('.nav-link');
 nav.forEach((e) => e.addEventListener('mouseover', (event) => event.target.style.fontFamily = 'lobster'));
 nav.forEach((e) => e.addEventListener('mouseout', (event) => event.target.style.fontFamily = ''));
-nav.forEach((e) => e.addEventListener('click', (event) => event.target.style.textDecoration = 'line-through'));
+nav.forEach((e) => e.addEventListener('click', (event) => {
+    event.target.style.textDecoration = 'line-through'
+    event.stopPropagation();
+}));
+
+
+//stop propagation
+// changed window. to thirdBorder. to stopPropagating on everything
+
+const thirdBorder = document.querySelector('.content-destination');
+thirdBorder.addEventListener('click', (event) => {
+     event.target.style.border = '1rem dotted blue'
+     event.stopPropagation();
+    
+});
 
 
 //4 -load
@@ -43,66 +57,87 @@ window.addEventListener('keydown', function(e){
 });
 
 
+// 8 -scroll
 
-// 4
+window.addEventListener('scroll', () =>{
 
-const para = document.querySelectorAll('p');
-para.forEach((e) => e.addEventListener('load', (event) => event.target.style.fontFamily = 'Satisfy'));
-
-
-
-
-//Three
-
-const btns = document.querySelectorAll('.btn');
-btns.forEach((eleBtn) => eleBtn.addEventListener('mouseover', (event) => event.target.style.color = 'orange'));
-
-
-
-
-
-//Five
-
-const firstBorder = document.querySelector('.content-section');
-window.addEventListener('load', () => {
-    firstBorder.style.border = '2rem dashed purple';
-});
-
-
-//Six
-
-const secondBorder = document.querySelector('.inverse-content');
-window.addEventListener('load', () => {
-    secondBorder.style.border = '1rem solid grey';
-});
-
-
-//Seven
-
-const thirdBorder = document.querySelector('.content-destination');
-window.addEventListener('click', () => {
-    thirdBorder.style.border = '1rem dotted blue';
+    // console.log('Scrolled!');
     
-});
+    const scrolled = window.scrollY;
+
+    console.log(scrolled);
+})
+
+
+// 9-10 -mousedown + mouseup
+
+
+const button = document.querySelectorAll('.btn');
+button.forEach((eleBtn) => eleBtn.addEventListener('mousedown', (e) => event.target.style.background = 'black'));
+button.forEach((eleBtn) => eleBtn.addEventListener('mouseup', (e) => event.target.style.background = 'navy'));
 
 
 
-//Eight
 
 
-const h1El = document.querySelector('h1');
-window.addEventListener('load', () => {
-    h1El.style.color = 'pink';
-});
 
 
-//Nine
 
 
-const h2El = document.querySelector('h2');
-window.addEventListener('load', () => {
-    h2El.style.color = 'green';
-});
+
+
+// // 4
+
+// const para = document.querySelectorAll('p');
+// para.forEach((e) => e.addEventListener('load', (event) => event.target.style.fontFamily = 'Satisfy'));
+
+
+
+
+// //Three
+
+// const btns = document.querySelectorAll('.btn');
+// btns.forEach((eleBtn) => eleBtn.addEventListener('mouseover', (event) => event.target.style.color = 'orange'));
+
+
+
+
+
+// //Five
+
+// const firstBorder = document.querySelector('.content-section');
+// window.addEventListener('load', () => {
+//     firstBorder.style.border = '2rem dashed purple';
+// });
+
+
+// //Six
+
+// const secondBorder = document.querySelector('.inverse-content');
+// window.addEventListener('load', () => {
+//     secondBorder.style.border = '1rem solid grey';
+// });
+
+
+
+
+
+// //Eight
+
+
+// const h1El = document.querySelector('h1');
+// window.addEventListener('load', () => {
+//     h1El.style.color = 'pink';
+// });
+
+
+// //Nine
+
+
+// const h2El = document.querySelector('h2');
+// window.addEventListener('load', () => {
+//     h2El.style.color = 'green';
+// });
 
 
 
