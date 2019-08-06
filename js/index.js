7bb8363e176e7c25d539129cf1077cb3cabeb8f5
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-08-06 14:17:14 -0700

    made the first two images change when clicked and hovered over

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
 // Your code goes here
+const headerImg = document.querySelectorAll("img")[0];
+const nextImg = document.querySelectorAll("img")[1];
+
+const makeOutline = function() {
+    this.style.border = "3px solid red"
+};
+const makeSmall = function() {
+    this.style.width = "300px"
+};
+
+headerImg.addEventListener("click", makeSmall);
+headerImg.addEventListener("mouseover", makeOutline);
+headerImg.adddEventListener("onkeydown")
+
+nextImg.addEventListener("click", makeSmall);
+nextImg.addEventListener("mouseover", makeOutline);
+
+
+// ----------------------------------------------------------------------
+
