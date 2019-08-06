5e6e3b9dd7618dfd52a46d561a22ffe04488c693
Roger McConkie <rogermcconkie@Rogers-MacBook-Pro-3.local>
2019-08-06 17:47:08 -0600

    questions done, just need prevent default

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-// #1 (this also includes my example of Propogation)
+// #1    `click` (this also includes my example of Propogation)
 const sunDiv = document.querySelector('.destination');
 sunDiv.addEventListener('click', function(e){
     console.log(`Clicking has fired this function: ${e}`)
@@ -13,37 +13,44 @@ smuButton.addEventListener('click', function(e){
     e.stopPropagation();
 })
 
-//#2    `mouseover` logo in the nav
+//#2    `mouseenter` 
 const logoH = document.querySelector('.logo-heading');
 logoH.addEventListener('mouseenter', (e)=>{
     e.target.style.color = "green";
 })
+
+//#3      `mouseleave`
 logoH.addEventListener('mouseleave', (e)=>{
     e.target.style.color = "black";
 })
 
-//#3	`keydown`
+//#4	`keydown`
 const bodyA =document.querySelector('body');
 bodyA.addEventListener('keydown', function(event){
-    console.log(`${event.code} was pressed.`);
+    console.log(`${event.code} was pressed down.`);
+})
+//#5	`keyup`
+const bodyb =document.querySelector('body');
+bodyb.addEventListener('keyup', function(event){
+    console.log(`${event.code} was pressed released.`);
 })
 
-//#4	`drag / drop`
-
-//#5	`wheel`
+//#6	`wheel`
 const pyDest =document.querySelector('.content-destination h2')
 pyDest.addEventListener('wheel', function(event){
     event.target.fontSize = '28px'
 })
-//#6	`focus`
+//#7	`focus`
 const letsGo = document.querySelector('a')
 letsGo.addEventListener('focus', (event)=> {
-    event.target.style.backgroundColor= 'pink';
+    event.target.style.backgroundColor= 'grey';
 })
-//#7	`resize`
 
-//#8	`scroll`
-
+//#8	`mousedown`
+const allheader = document.querySelector('header');
+allheader.addEventListener('mousedown', function(event){
+    event.target.style.backgroundColor = "red";
+})
 //#9	`select`
 function logSelection(event) {
     const log = document.getElementById('log');
@@ -59,3 +66,4 @@ const head2 = document.querySelector('header h2');
 head2.addEventListener('dblclick', function(event){
     event.target.style.color = "red";
 })
+
