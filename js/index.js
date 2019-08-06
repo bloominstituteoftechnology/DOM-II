3808fe2af8af7c9818d9601389b23313c3bf06fa
Angela Flowers <me@angela.flowers>
2019-08-06 18:59:40 -0400

    added four listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+const buttons = document.querySelectorAll(".btn");
+buttons.forEach(btn => {
+    btn.addEventListener('click', (event) => {
+        alert("Thank you!!!");
+    });
+});
+
+const logo = document.querySelector(".logo-heading");
+logo.addEventListener('mouseover', (event) => {
+    logo.style.display = "none";
+});
+
+const pics = document.querySelectorAll('img');
+pics.forEach(img => {
+    img.addEventListener('keypress', (event) => {
+        img.style.opacity = "0.5";
+    });
+});
+
+const bus = document.querySelector('img');
+
+console.log(pics);
+console.log(bus);
