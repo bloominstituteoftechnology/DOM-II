8143a5d9c047ae706c103c0fae7169156986c2be
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-08-06 15:58:17 -0700

    added drag and scroll

--- js/index.js
+++ js/index.js
@@ -1,29 +1,52 @@
 // mouseover
 const logo = document.querySelector('.logo-heading');
-logo.addEventListener('mouseover', () => {
+logo.addEventListener('mouseover', (e) => {
     logo.textContent = "Lets Go!";
 })
 
-//wheel
-// const nav = document.querySelector('.nav a');
-// nav.forEach((a) => {
-//     a.addEventListener('wheel', () => {
-//         a.style.fontSize = '2rem';
-//     });
-// });
+// keydown
+window.addEventListener('keydown', (e) => {
+    console.log(`This is my keypress: ${e.code}`);
+  });
 
+// wheel
+const mainNav = document.querySelector('.main-navigation');
+console.log(mainNav);
+mainNav.addEventListener('wheel', (e) => {
+    mainNav.style.backgroundColor = "green";
+})
 
+// dbclick
 
+const pictures = document.querySelectorAll('img');
 
-//scroll
+for(i = 0; i < pictures.length; i ++){
+    pictures[i].addEventListener('dblclick', (e) => {
+        e.target.style.filter = "invert(100%)";
+    });
+};
+
+// drag / drop
+
+for(i = 0; i < pictures.length; i ++){
+    pictures[i].addEventListener('drag', (e) => {
+        e.target.setAttribute('src', "https://i.ibb.co/k58Sx7D/images.jpg")
+    })
+}
+
+//load
+
+//focus
+
+//resize
+
+// scroll
+
 const home = document.querySelector('.home');
 window.addEventListener('scroll', () => {
     home.style.background = '#E62739'
 });
 
-//keydown
+// select
 
 
-window.addEventListener('keydown', (e) => {
-  console.log(`This is my keypress: ${e.code}`);
-});
