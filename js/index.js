b33043943190ae19c9cd9c492906da38e732771c
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-06 14:02:46 -0500

    completed first few events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+let logo = document.querySelector('.logo-heading');
+
+let black = logo.addEventListener('mouseover', (event) => {
+    event.target.style.backgroundColor ='black';
+});
+let removeBlack = logo.addEventListener('mouseout', (event) => {
+    event.target.style.backgroundColor ='white';
+})
+/////////////////////
+
+let 
+.addEventListener('keydown', (event) => {});
+
+
+//element.addEventListener('wheel', (event) => {//Handle event});
+//element.addEventListener('drag / drop', (event) => {//Handle event});
+//element.addEventListener('load', (event) => {//Handle event});
+//element.addEventListener('focus', (event) => {//Handle event});
+//element.addEventListener('resize', (event) => {//Handle event});
+//element.addEventListener('scroll', (event) => {//Handle event});
+//element.addEventListener('select', (event) => {//Handle event});
+//element.addEventListener('dblclick', (event) => {//Handle event});
