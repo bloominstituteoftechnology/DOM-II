fddb6c92f20006f8dd3e2595666815610aaae6de
Gurpreet Dhaliwal <gdhaliwal22@gmail.com>
2019-08-06 14:56:10 -0700

    completed mouseover scroll and keydown

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
-// Your code goes here
+// mouseover
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseover', () => {
+    logo.textContent = "Lets Go!";
+})
+
+//wheel
+// const nav = document.querySelector('.nav a');
+// nav.forEach((a) => {
+//     a.addEventListener('wheel', () => {
+//         a.style.fontSize = '2rem';
+//     });
+// });
+
+
+
+
+//scroll
+const home = document.querySelector('.home');
+window.addEventListener('scroll', () => {
+    home.style.background = '#E62739'
+});
+
+//keydown
+
+
+window.addEventListener('keydown', (e) => {
+  console.log(`This is my keypress: ${e.code}`);
+});
