31e69b5847d42e22ac0649326ffd696e59da262f
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-06 14:31:37 -0500

    completed wheel event

--- js/index.js
+++ js/index.js
@@ -6,14 +6,34 @@ let black = logo.addEventListener('mouseover', (event) => {
 });
 let removeBlack = logo.addEventListener('mouseout', (event) => {
     event.target.style.backgroundColor ='white';
-})
+});
 /////////////////////
 
-let 
-.addEventListener('keydown', (event) => {});
+let nav= document.querySelectorAll('nav a')[0];
+console.log(nav);
+nav.addEventListener('keydown', (event) => {
+    event.target.style.backgroundColor ='green';
+});
+nav.addEventListener('keyup', (event) => {
+    event.target.style.backgroundColor ='white';
+});
+
+let scale = 1;
+let nav1 = document.querySelectorAll('nav a')[1];
+nav1.addEventListener('wheel', function zoom (event) {
+      event.preventDefault();
+
+  scale += event.deltaY * -0.01;
+
+  // Restrict scale
+  scale = Math.min(Math.max(.125, scale), 4);
+
+  // Apply scale transform
+  nav1.style.transform = `scale(${scale})`;
+
+});
 
 
-//element.addEventListener('wheel', (event) => {//Handle event});
 //element.addEventListener('drag / drop', (event) => {//Handle event});
 //element.addEventListener('load', (event) => {//Handle event});
 //element.addEventListener('focus', (event) => {//Handle event});
