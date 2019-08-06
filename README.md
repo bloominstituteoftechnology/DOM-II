d14a85f380ed5582baffa4010e5070890b0a49b5
Tyler Gilchrist <tyler668@gmail.com>
2019-08-06 16:39:40 -0400

    Added some events

--- README.md
+++ README.md
@@ -48,7 +48,7 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
 * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
-* [ ] Stop the navigation from items from refreshing the page by using `preventDefault()`
+* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
 
 ## Stretch Task:
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,58 @@
 // Your code goes here
+
+// (1) Logo Anim ===
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseover', (e) => {
+    TweenMax.to(e.target, 1, { rotation: 360 });
+});
+logo.addEventListener('mouseleave', (e) => {
+    TweenMax.to(e.target, 1, { rotation: -360 });
+});
+
+
+// (2) Body color change on keydown ===
+const body = document.querySelector('body');
+
+body.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+        body.style.backgroundColor = '#' + (0x1000000 + (Math.random()) * 0xffffff).toString(16).substr(1, 6);
+        //Random color hex
+    }
+});
+
+// (3) Hover nav link behavior ===
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(a => {
+    a.addEventListener('mouseover', (e) => {
+        e.target.style.color = "lightgreen";
+        e.preventDefault();
+    });
+    a.addEventListener('mouseleave', (e) => {
+        e.target.style.color = "black";
+        e.preventDefault();
+    });
+});
+
+// (4) Preventing nested elements from bubbling using stopPropagation ===
+const intro = document.querySelector('.intro');
+const introImg = document.querySelector(".intro img");
+
+intro.addEventListener('click', (e) =>{
+    alert("outer");
+});
+
+introImg.addEventListener('click', (e) =>{
+    alert("inner");
+    e.stopPropagation();
+});
+
+// (5) On key press log which key was pressed
+document.addEventListener('keydown', logKey);
+function logKey(e) {
+    console.log(` ${e.code}`);
+}
+
+
+
+
+
