598fe21f525665465b8204053000079cd80c8bae
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-08-06 16:18:27 -0500

    worked through drag and drop, fucus, unfocus, working on resize

--- js/index.js
+++ js/index.js
@@ -18,6 +18,8 @@ nav.addEventListener('keyup', (event) => {
     event.target.style.backgroundColor ='white';
 });
 
+//////////////////////
+
 let scale = 1;
 let nav1 = document.querySelectorAll('nav a')[1];
 nav1.addEventListener('wheel', function zoom (event) {
@@ -33,10 +35,93 @@ nav1.addEventListener('wheel', function zoom (event) {
 
 });
 
+/////////////
+document.addEventListener("dragstart", function(event) {
+    // The dataTransfer.setData() method sets the data type and the value of the dragged data
+    event.dataTransfer.setData("Text", event.target.id);
+  
+    // Output some text when starting to drag the p element
+    document.getElementById("demo").innerHTML = "Started to drag the h element.";
+  
+    // Change the opacity of the draggable element
+    event.target.style.opacity = "0.4";
+  });
+  
+  // While dragging the p element, change the color of the output text
+  document.addEventListener("drag", function(event) {
+    document.getElementById("demo").style.color = "red";
+  });
+  
+  // Output some text when finished dragging the p element and reset the opacity
+  document.addEventListener("dragend", function(event) {
+    document.getElementById("demo").innerHTML = "Finished dragging the p element.";
+    event.target.style.opacity = "1";
+  });
+  
+  
+  /* ----------------- Events fired on the drop target ----------------- */
+  
+  // When the draggable p element enters the droptarget, change the DIVS's border style
+  document.addEventListener("dragenter", function(event) {
+    if ( event.target.className == "droptarget" ) {
+      event.target.style.border = "10px dotted black";
+    }
+  });
+  
+  // By default, data/elements cannot be dropped in other elements. To allow a drop, we must prevent the default handling of the element
+  document.addEventListener("dragover", function(event) {
+    event.preventDefault();
+  });
+  
+  // When the draggable p element leaves the droptarget, reset the DIVS's border style
+  document.addEventListener("dragleave", function(event) {
+    if ( event.target.className == "droptarget" ) {
+      event.target.style.border = "";
+    }
+  });
+  
+  /* On drop - Prevent the browser default handling of the data (default is open as link on drop)
+  Reset the color of the output text and DIV's border color
+  Get the dragged data with the dataTransfer.getData() method
+  The dragged data is the id of the dragged element ("drag1")
+  Append the dragged element into the drop element
+  */
+  document.addEventListener("drop", function(event) {
+    event.preventDefault();
+    if ( event.target.className == "droptarget" ) {
+      document.getElementById("demo").style.color = "";
+      event.target.style.border = "";
+      var data = event.dataTransfer.getData("Text");
+      event.target.appendChild(document.getElementById(data));
+    }
+  });
 
-//element.addEventListener('drag / drop', (event) => {//Handle event});
-//element.addEventListener('load', (event) => {//Handle event});
-//element.addEventListener('focus', (event) => {//Handle event});
+//////////////////
+
+window.addEventListener('load', (event) => {
+    logo.style.fontSize = '50px';
+  });
+
+  /////////////
+
+  let nav2= document.querySelectorAll('nav a')[2];
+  nav2.addEventListener('focus', (event) => {
+    event.target.style.background = 'green';    
+  });
+  
+  nav2.addEventListener('blur', (event) => {
+    event.target.style.background = '';    
+  });
+  nav2.addEventListener('focusin', (event) => {
+    event.target.style.background = 'pink';    
+  });
+  
+  nav2.addEventListener('focusout', (event) => {
+    event.target.style.background = '';    
+  });
+//////////
+
+
 //element.addEventListener('resize', (event) => {//Handle event});
 //element.addEventListener('scroll', (event) => {//Handle event});
 //element.addEventListener('select', (event) => {//Handle event});
