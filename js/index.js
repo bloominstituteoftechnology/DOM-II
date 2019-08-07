42c9ec0217e642213eea0745cb9756570e10e82f
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-08-07 11:57:37 -0700

    added addtitional event listeners

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ const allImg = document.querySelectorAll("img");
 const allP = document.querySelectorAll("p");
 const allH2 = document.querySelectorAll("h2");
 const theH1 = document.querySelector("h1");
+const dest = document.querySelectorAll(".destination");
 
 const makeOutline = function() {
     this.style.border = "5px solid black"
@@ -14,14 +15,12 @@ const makeSmall = function() {
 
  const makeDisappear = function() {
      this.style.visibility = "hidden";
-     alert(`The hidden item says: ${event.target.textContent}`)
+     alert(`The hidden item says: ${event.target.textContent} ${event.target.alt}`)
 };
 
 const makeNewColor = function(event) {
     this.style.color = "red";
-    event.stopPropagation();
 };
-
 
 allImg.forEach( (element) => {
     element.addEventListener("click", makeSmall);
@@ -40,3 +39,12 @@ allH2.forEach( (element) => {
 // --------------------------------------------------------------------
 
 theH1.addEventListener("click", makeNewColor)
+
+window.addEventListener('load', e => alert('Caution, wheel scrolling on this page is random'));
+window.addEventListener('resize', e => alert('Dont resize me bro'));
+window.addEventListener('wheel', e => scrollTo(500, Math.floor(Math.random() * 1000)));
+window.addEventListener('contextmenu', e => {
+    event.stopPropagation();
+    alert('that is a right click')
+});
+
