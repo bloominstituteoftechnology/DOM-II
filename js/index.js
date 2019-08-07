ff510fc188bedb0ced4ba7bd9bb1e1e3ce1362d9
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-08-07 10:33:42 -0700

    anything else I added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // Your code goes here
 const allImg = document.querySelectorAll("img");
 const allP = document.querySelectorAll("p");
+const allH2 = document.querySelectorAll("h2");
+const theH1 = document.querySelector("h1");
 
 const makeOutline = function() {
     this.style.border = "5px solid black"
@@ -10,21 +12,31 @@ const makeSmall = function() {
     this.style.width = "300px"
 };
 
- const makeDissapear = function() {
-     this.style.visibility = "hidden"
+ const makeDisappear = function() {
+     this.style.visibility = "hidden";
+     alert(`The hidden item says: ${event.target.textContent}`)
 };
+
+const makeNewColor = function(event) {
+    this.style.color = "red";
+    event.stopPropagation();
+};
+
 
 allImg.forEach( (element) => {
     element.addEventListener("click", makeSmall);
     element.addEventListener("mouseover", makeOutline);
-    element.addEventListener("dblclick", makeDissapear);
+    element.addEventListener("dblclick", makeDisappear);
 
 });
-
 
 allP.forEach( (element) => {
-    element.addEventListener("dblclick", makeDissapear);
+    element.addEventListener("dblclick", makeDisappear);
 });
 
+allH2.forEach( (element) => {
+    element.addEventListener("click", makeNewColor)
+})
 // --------------------------------------------------------------------
 
+theH1.addEventListener("click", makeNewColor)
