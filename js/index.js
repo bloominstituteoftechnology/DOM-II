76ee9870083244cb1d60b7d6846b896ed852332d
Cameron Alvarado <cameronalvarado@Camerons-Macintosh.local>
2019-08-08 07:37:34 -0700

    added example for stopPropagation

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const allP = document.querySelectorAll("p");
 const allH2 = document.querySelectorAll("h2");
 const theH1 = document.querySelector("h1");
 const dest = document.querySelectorAll(".destination");
-const button = document.querySelector(".btn")
+const button = document.querySelector(".btn");
 
 const makeOutline = function() {
     this.style.border = "5px solid black"
@@ -22,6 +22,7 @@ const makeSmall = function() {
 const makeNewColor = function(event) {
     this.style.color = "red";
 };
+
 
 allImg.forEach( (element) => {
     element.addEventListener("click", makeSmall);
@@ -44,11 +45,31 @@ theH1.addEventListener("click", makeNewColor)
 window.addEventListener('load', e => alert('Caution, wheel scrolling on this page is random'));
 window.addEventListener('resize', e => alert('Dont resize me bro'));
 window.addEventListener('wheel', e => scrollTo(500, Math.floor(Math.random() * 1000)));
-window.addEventListener('scroll', e => {
-    open("https://www.google.com");
-    e.preventDefault()
-});
+// window.addEventListener('scroll', e => {
+//     open("https://www.google.com");
+//     e.preventDefault()
+// });
 window.addEventListener('contextmenu', e => {
     alert('that is a right click')
 });
 
+// -------------------- Stop Propogation -------------------------------
+
+const parent = document.querySelector(".content-pick");
+const child = parent.querySelector(".destination");
+
+const makeBGBlue = function(event) {
+    alert('content-pick section clicked');
+    this.style.backgroundColor = "blue";
+};
+
+const makeBGRed = function(event) {
+    event.stopPropagation();
+    alert('destination section clicked')
+    this.style.backgroundColor = "red";
+};
+
+parent.addEventListener('click', makeBGBlue);
+child.addEventListener('click', makeBGRed);
+
+// adding stopPropagation to the child's event listener prevents the parent's event listener from being triggered when the child's is. It allows the child to be "disowned" by the parent and the child is now independant and its parent is no longer affected.
