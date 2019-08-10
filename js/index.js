2ce0825e87a83dbe0f9297b862117aadd52298d7
chefboyrdeuce <jpldeuce@gmail.com>
2019-08-09 21:30:48 -0400

    3 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 // Your code goes here
+
+// 1. scroll event
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function doSomething(scroll_pos) {
+  // Do something with the scroll position
+  console.log("scroll event")
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+
+  if (!ticking) {
+    window.requestAnimationFrame(function() {
+      doSomething(last_known_scroll_position);
+      ticking = false;
+    });
+
+    ticking = true;
+  }
+});
+
+// 2. mouse over
+let test = document.getElementById("redNav");
+  
+// This handler will be executed every time the cursor
+// is moved over a different nav item
+test.addEventListener("mouseover", function( event ) {   
+  // highlight the mouseover target
+  event.target.style.color = "red";
+
+  // reset the color after a short delay
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+
+// 3. keydown
+function keydownFunction() {
+    alert("You pressed a key inside the input field");
+  }
