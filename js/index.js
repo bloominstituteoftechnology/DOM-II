f43c216c3613c6a062cd7cce259f2e723ee21636
AislynnEdmiston <davidandaislynn@gmail.com>
2019-08-10 12:11:04 -0400

    Started by creating a sign up alert box

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+//Event Listeners
+/*  
+ mouseover
+ keydown
+ wheel
+ drag / drop
+ load
+ focus
+ resize
+ scroll
+ select
+ dblclick */
+
+const signUpBtn = document.querySelectorAll(".btn");
+
+function signUp(){
+    prompt("Please enter your name here:", "Name");
+    if (confirm){
+        alert("Thank you for signing up!");
+    }
+};
+
+signUpBtn.forEach(function(element){
+    element.addEventListener("click", signUp);
+});
