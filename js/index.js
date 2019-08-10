2d27a47c21a387b023ca7af1f37585bd915c8419
spettigrew <sara.pettigrew5@gmail.com>
2019-08-09 20:10:02 -0600

    Some changes to DOM II js

--- js/index.js
+++ js/index.js
@@ -1,53 +1,55 @@
 // Your code goes here
-const nav = document.querySelectorAll('.main-navigation');
-nav.addEventListener('click', (event) => {
-    event.target.mouseover.color = 'yellow';
-    event.target.select
+const nav = document.querySelector('.main-navigation');
+nav.addEventListener('mouseover', (event) => {
+    console.log(event.target);
+    event.target.style.backgroundColor = 'yellow';
+    // event.target.select
 });
 
-const navLi = document.querySelectorAll('.nav-link');
-navLi = addEventListener('', () => {
-
+const home = document.querySelector('.intro');
+console.log(home);
+home.addEventListener('mousedown', function (event) {
+    event.target.style.backgroundColor = 'pink';
+    console.log(event);
 });
 
 const h1 = document.querySelector('.logo-heading');
-h1 = addEventListener('', () => {
-
+h1.addEventListener('dragover', (event) => {
+    event.target;
 });
 
 const intro = document.querySelector('.intro-img');
-intro.addEventListener('', () => {
-
-    event.mousemove.fade.color = 'white';
-});
+intro.addEventListener('drag', (event) => {
+    // event.target.drag;
+}, false);
 
 const text = document.querySelector('.text-content');
-text.addEventListener('', () => {
-    event
+text.addEventListener('scroll', (event) => {
+    even
 });
 
 const h2 = document.querySelector('.text-content h2');
-h2.addEventListener('', () => {
-
+h2.addEventListener('dblclick', (event) => {
+    event.currentTarget.style.color = 'blue';
 });
 
 const image = document.querySelector('.content-destination img');
-image.addEventListener('', () => {
-
+image.addEventListener('drop', (event) => {
+    event
 });
 
 const destination = document.querySelector('.destination');
-destination.addEventListener('', () => {
+destination.addEventListener('keypress', (event) => {
+    event.target.style.backgroundColor = 'orange';
 
 });
 
 const h4 = document.querySelector('.destination h4');
-h4.addEventListener('', () => {
-
+h4.addEventListener('keydown', (event) => {
+    event.target.style.backgroundColor = 'red';
 });
 
 const button = document.querySelector('.btn');
-button.addEventListener('', () => {
-
+button.addEventListener('click', (event) => {
+    event.target.style.color = 'black';
 });
-
