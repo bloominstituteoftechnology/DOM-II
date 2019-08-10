95a84dcee0270b8a170493c69cb9f87e6756ec69
spettigrew <sara.pettigrew5@gmail.com>
2019-08-10 15:58:50 -0600

    dom II event listeners completed

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
 const nav = document.querySelector('.main-navigation');
 nav.addEventListener('mouseover', (event) => {
     console.log(event.target);
-    event.target.style.backgroundColor = 'yellow';
+    event.target.style.backgroundColor = 'lightYellow';
     // event.target.select
 });
 
@@ -13,19 +13,15 @@ home.addEventListener('mousedown', function (event) {
     console.log(event);
 });
 
-const h1 = document.querySelector('.logo-heading');
-h1.addEventListener('dragover', (event) => {
-    event.target;
-});
+// const h1 = document.querySelector('.logo-heading');
+// h1.addEventListener('dragover', (event) => {
+//     event.target.style
+// });
 
-const intro = document.querySelector('.intro-img');
-intro.addEventListener('drag', (event) => {
-    // event.target.drag;
-}, false);
 
-const text = document.querySelector('.text-content');
-text.addEventListener('scroll', (event) => {
-    even
+const links = document.querySelector('.nav-link');
+links.addEventListener('mouseout', (event) => {
+    event.target.style.fontSize = '2rem'
 });
 
 const h2 = document.querySelector('.text-content h2');
@@ -33,23 +29,91 @@ h2.addEventListener('dblclick', (event) => {
     event.currentTarget.style.color = 'blue';
 });
 
-const image = document.querySelector('.content-destination img');
-image.addEventListener('drop', (event) => {
-    event
+const intros = document.querySelectorAll('.text-content p');
+intros.forEach(intro => {
+    intro.addEventListener('click', () => {
+    event.target.style.color = 'lightGreen';
+})
 });
+
+const image = document.querySelectorAll('img');
+let blur = 0;
+let lastKnownPosition = 0;
+window.addEventListener('scroll', () => {
+    if(lastKnownPosition < window.scrollY){
+        blur += 1
+    } else {
+    // if (lastKnownPosition > window.scrollY) {
+        blur -= 1
+}
+
+    lastKnownPosition = window.scrollY;
+
+    image.forEach(pic => pic.style.filter = `blur(${blur}px)`);
+});
+// image.addEventListener('drop', (event) => {
+//     event.target.
+
 
 const destination = document.querySelector('.destination');
-destination.addEventListener('keypress', (event) => {
-    event.target.style.backgroundColor = 'orange';
+document.addEventListener('keypress', (event) => {
+    destination.style.backgroundColor = 'orange';
 
 });
 
+// destination.forEach(dest => {
+//     dest.addEventListener('keypress', () => {
+//         destination.style.backgroundColor = 'orange';
+//     })
+// });
+
+const outputBox = document.createElement('p')
+document.querySelector('body').append(outputBox);
+document.addEventListener('keydown', event => {
+    outputBox.textContent += `${event.key}`
+})
+
+
 const h4 = document.querySelector('.destination h4');
-h4.addEventListener('keydown', (event) => {
-    event.target.style.backgroundColor = 'red';
+document.addEventListener('keydown', () => {
+    h4.style.backgroundColor = 'red';
 });
 
 const button = document.querySelector('.btn');
-button.addEventListener('click', (event) => {
-    event.target.style.color = 'black';
+document.addEventListener('click', (event) => {
+   button.style.backgroundColor = 'yellow';
 });
+
+
+// let dragged ;
+// introP.forEach(pTag => {
+//     pTag.daggable = true;
+//     pTag.classList.add('dropzone');
+// })
+
+// document.addEventListener('dragstart', (event) => {
+//     dragged.event.target;
+//     event.target.style.opacity = .5;
+// });
+
+// document.addEventListener('dragend', (event) => {
+//     dragged.event.target;
+//     event.target.style.opacity = .5;
+// });
+
+// document.addEventListener('dragover', (event) => {
+//     dragged.event.target;
+//     event.target.style.opacity = .5;
+// });
+
+// document.addEventListener('dragenter', (event) => {
+//     if(event.target.className == 'dropzone';
+//     event.target.style.opacity = .5;
+// });
+
+// document.addEventListener('dragleave', (event) => {
+//     dragged.event.target;
+//     event.target.style.opacity = .5;
+// });
+
+// introP = document.querySelector
