b0393dc1c119037e37a5695880fe221a42bde36f
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-09-02 14:53:00 -0700

    first commit - test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+
+// -- "mouseover" -- 
+const headerLogo = document.querySelector('.logo-heading');
+
+headerLogo.addEventListener('mouseover', function(event){
+    // console.log('MouseOver LOGO') // TEST
+    event.target.style.color = "gold"
+});
+
+// -- "keydown" -- 
+const newBod = document.querySelector("body");
+
+newBod.addEventListener('keydown', function(event){
+    // console.log("KEYDOWN") // TEST
+    event.target.style.background = "gold"
+});
+
+// -- "load" -- 
+const newNav = document.querySelectorAll("a");
+
+newNav.addEventListener("load", function(event){
+    event.target.style.color = "navy"
+});
+
+
+
+
+
