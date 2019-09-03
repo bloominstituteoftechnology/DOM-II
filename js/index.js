02db75f910bb8b50a169d649d6ed3389363b7295
Temitope Akinsoto <takinsoto@gmail.com>
2019-09-03 12:56:28 +0100

    feat: Add mouse over event to all images
    In this commit, I added an event handler to all images to listen for any for any mouse over
    effect. If the mouse is placed over any image on the website, the handler function toggles
    the image by applying pre-defined css rule sets

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let pageImages = document.querySelectorAll('img');
+pageImages.forEach(image => {
+    image.addEventListener('mouseover', e => {
+        console.log(e.target.classList.toggle('toggleImage'));
+    });
+})
