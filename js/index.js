64f201dea224163e14fc96e6852e8726027af585
Ayomide Jones <ayormeday@gmail.com>
2019-09-03 13:03:59 +0100

    nav tags set to red when mouse overs

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+let mainNav = document.querySelectorAll('.nav-link')
+
+mainNav.forEach(el => el.addEventListener('mouseover', () => {
+        el.style.color = 'red'   
+}));
