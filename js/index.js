ff3298755438d7f60b9253f01fddffc952e1b468
Cuza <baopham92@yahoo.com>
2019-09-03 16:13:14 -0400

    mouseout & mouseover event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+const allNodes = document.querySelectorAll('*');
+// Iterate unwanted / wanted index items
+allNodes.forEach(index => {
+    const action = (e) => {
+        const element = e.target;
+        let oldColor = "";
+
+        if (e.type === 'mouseover') {
+            // Store old color
+            oldColor = element.style.background
+
+            // Change to new color
+            return element.style.background = 'grey';
+            
+        } else if (e.type === 'mouseout') {
+            return element.style.background = oldColor;
+        }
+    }
+
+    // Event listeners
+    index.addEventListener('mouseover', action)
+    index.addEventListener('mouseout', action)
+})
