151c6204f35f015e7a769a98a201448f4a21b93d
Anthony Hill <hillan1152@gmail.com>
2019-09-03 19:34:08 -0400

    just started

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+// const smallBus = document.querySelector('.intro img');
+// smallBus.addEventListener('mouseover', e => {
+// })
+
+
+//mouse over
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseover', e => event.target.style.color = 'red');
+
+
+//key down
+
+
+
+//Load
+window.addEventListener('load', e => console.log('PAGE IS LOADED'));
+
+//dbl click
+const card = document.querySelector('btn');
+
+card.addEventListener('dblclick', function (e) {
+  card.e.toggle('large');
+});
