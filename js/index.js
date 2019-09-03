1782f21b35a8371b4267730abb60a8af895809c4
Steve Renner <che1111@hotmail.com>
2019-09-03 14:29:34 -0700

    Added Change Colors

--- js/index.js
+++ js/index.js
@@ -8,7 +8,6 @@ imgScale.addEventListener("mouseover", e => {
 
 // Zoom with mouse wheel on header
 function zoom(event) {
-    event.preventDefault()
     scale += event.deltaY * -0.01;
     scale = Math.min(Math.max(.125, scale), 4);
     el.style.transform = `scale(${scale})`;
@@ -17,15 +16,35 @@ let scale = 1;
 const el = document.querySelector('header');
 el.onwheel = zoom;
 
-const textBlur = document.querySelector('footer[type="textBlur"]');
-    textBlur.addEventListener('focusout', e => {
-        footer.target.style.background = 'purple';
-    });
+// Blur footer
+// const textBlur = document.querySelector('footer[type="textBlur"]');
+//     textBlur.addEventListener('focusout', e => {
+//         footer.target.style.background = 'purple';
+//     });
 
-const newBus = document.querySelectorAll('.img-content');
+// Change first non-header image
+const newBus = document.querySelectorAll('.lets-go');
 newBus.forEach(item => {
     item.addEventListener('click', e => {
     item.src= 'https://images.unsplash.com/photo-1525962898597-a4ae6402826e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60';
     item.alt ="New Bus"
     })
+});
+
+// Change text in footer
+window.addEventListener('resize', e => {
+    const changeTitle = document.querySelector('.footer');
+    changeTitle.textContent="*A sham of a company"
+})
+
+// Change color when clicking on first <p>
+window.onclick = changeColor;
+function changeColor() {
+    document.getElementsByTagName("p")[0].style.backgroundColor = "purple";
+}
+
+// Change color to green when clicken on Pick Your Destination
+const destinationColor = document.querySelector('.content-destination');
+destinationColor.addEventListener('click', (e) => {
+    destinationColor.style.background = "green";
 })
