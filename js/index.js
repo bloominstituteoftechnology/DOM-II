5f3477ac7cd287529fdc84b0d9f1d6f5c6212946
Steve Renner <che1111@hotmail.com>
2019-09-03 12:43:30 -0700

    Added Scale & Zoom

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const imgScale = document.querySelector('img');
+imgScale.addEventListener("mouseover", e => {
+    imgScale.style.transform = 'scale(1.5)';
+});
+
+function zoom(event) {
+    // event.preventDefault()
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    el.style.transform = `scale(${scale})`;
+}
+let scale = 1;
+const el = document.querySelector('header');
+el.onwheel = zoom;
