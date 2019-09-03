81913ca0b4bf5ba194fe272777f17fc49572403b
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-09-03 16:48:24 -0400

    5 Event Listeners

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@
 
 
 // Mouseover & Mouseout
-const navBar = document.querySelector(".nav");
-navBar.addEventListener("mouseover", (e) => {
+const navBar = document.querySelector('.nav');
+navBar.addEventListener('mouseover', (e) => {
     navBar.style.backgroundColor = 'white';
 });
 
@@ -28,3 +28,23 @@ function zoom(event) {
     let scale = 1;
     const el = document.querySelector('img');
     el.onwheel = zoom;
+
+
+// Dblclick
+const btn = document.querySelector('.btn');
+
+console.log(btn);
+
+btn.addEventListener('dblclick', function (e) {
+    btn.style.width = '250px';
+    btn.style.height = '50px';
+});
+
+
+// Click Count
+const click = document.querySelector('.btn');
+
+click.addEventListener('click', (e) => {
+  click.innerHTML = `Click count: ${event.detail}`;
+});
+
