f16394f959e05aa3bfec510c21cb745e0c1638ba
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-09-03 15:53:49 -0400

    wheel on top img

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
 // Your code goes here
 
 
-// Mouseover
+// Mouseover & Mouseout
 const navBar = document.querySelector(".nav");
 navBar.addEventListener("mouseover", (e) => {
-    navBar.style.backgroundColor = 'hotpink';
+    navBar.style.backgroundColor = 'white';
 });
+
+navBar.addEventListener('mouseout', (e) => {
+    navBar.style.backgroundColor = 'hotpink';
+  });
+
+
+// Wheel
+function zoom(event) {
+      event.preventDefault();
+    
+      scale += event.deltaY * -0.01;
+    
+      // Restrict scale
+      scale = Math.min(Math.max(.125, scale), 4);
+    
+      // Apply scale transform
+      el.style.transform = `scale(${scale})`;
+    }
+    
+    let scale = 1;
+    const el = document.querySelector('img');
+    el.onwheel = zoom;
