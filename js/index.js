bee25754fe354b4da011c79bb9de1726160f772e
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-09-03 12:46:24 -0700

    10 unique Event listeners

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 
 
-// // -- "mouseover" -- 
+// // -- (1) "mouseover" -- 
 const headerLogo = document.querySelector('.logo-heading');
 
 headerLogo.addEventListener('mouseover', function(event){
@@ -9,7 +9,7 @@ headerLogo.addEventListener('mouseover', function(event){
     event.target.style.color = "gold"
 });
 
-// -- "keydown" -- 
+// -- (2) "keydown" -- 
 const newBod = document.querySelector("body");
 
 newBod.addEventListener('keydown', function(event){
@@ -18,7 +18,7 @@ newBod.addEventListener('keydown', function(event){
     event.target.style.background = "gold"
 });
 
-// // -- 'mouseenter' -- 
+// // -- (3) 'mouseenter' -- 
 const mouseNone = document.querySelector('footer');
 
 mouseNone.addEventListener('mouseenter', function(event){
@@ -26,7 +26,7 @@ mouseNone.addEventListener('mouseenter', function(event){
     event.target.style.cssText = "display:none";
 });
 
-// // -- 'wheel' --
+// // -- 'wheel' -- // DO NOT UNDERSTAND WHEEL
 // const headerLogoWheel = document.querySelector('h2');
 // const paragraph = document.querySelector('p');
 
@@ -39,10 +39,16 @@ mouseNone.addEventListener('mouseenter', function(event){
 // // -- 'drag / drop' // 
 
 
-// // -- 'focus' // 
+// // -- (4) 'click' // 
+const newPlaneImg = document.querySelectorAll('img.img-fluid').forEach(item => {
+  item.addEventListener('click', event => {
+  item.src = 'https://dw8stlw9qt0iz.cloudfront.net/VLVYPikq_0ydHmRj89KV9HVqWTQ=/fit-in/1440x810/filters:format(jpeg):quality(75)/curiosity-data.s3.amazonaws.com/images/content/landscape/standard/991adb98-ffd0-4c64-c11f-9b05b643bd83.jpg';
+item.alt = "plane nosediving with wing on fire";
+  })
+});
 
 
-// -- 'scroll' // 
+// -- (5) 'scroll' // 
 const headerLogoScroll = document.querySelector('h1');
 const paragraph = document.querySelector('p');
 
@@ -52,41 +58,44 @@ document.addEventListener('scroll', function (event){
     paragraph.textContent = 'This is the Broke Bus!'
 });
 
-// -- 'select' // 
 
-
-// -- 'double-click' -- 
+// -- (6) 'double-click' -- 
 const logoDbl = document.querySelector('.home .intro h2');
 
 logoDbl.addEventListener("dblclick", function(event){
     event.target.style.cssText = "font-size:5rem;color:dodgerblue"
 })
 
-// -- "load" -- // specific to WINDOW 
-const onLoad = document.querySelectorAll("body");
+// // -- (7 & 8) Scaling with 'mouseenter' & 'mouseleave'
+const scaleUp = document.querySelectorAll('.content-destination img')
 
-window.addEventListener("load", setTimeout(function(){
-    alert('Do not ride the Broke Bus!'); }, 3000));
+scaleUp.forEach(item => {
+    item.addEventListener('mouseenter', event => {
+        item.style.transform = 'scale(1.1)';
+        item.style.transition = 'transform 0.5s';
+    })
+})
+
+const scaleDown = document.querySelectorAll(' .content-destination img');
+
+scaleDown.forEach(item => {
+    item.addEventListener('mouseleave', event => {
+        item.style.transform = 'scale(1)';
+        item.style.transition = 'transform 0.5s'
+    })
+})
+
+// -- (9) 'RESIZE' -- // 
+window.addEventListener('resize', event => {
+    const changeTitle = document.querySelectorAll('h2');
+    changeTitle[1].textContent = 'Try Walking!'
+    changeTitle[0].textContent = 'Ride a Steel Horse!'
+  });
 
 
-// -- BLANK -- 
-// const destination = document.querySelector('.destination');
-//     destination.addEventListener('click', function(){
-//         console.log("I'm in the div!")
-//     })
+// -- (10) "load" -- // specific to WINDOW 
+// const onLoad = document.querySelectorAll("body");
 
-//     const btn = document.querySelector('.btn');
-//     btn.addEventListener('click', function(event){
-//         console.log("I'm in the button")
-//     });
+// window.addEventListener("load", setTimeout(function(){
+//     alert('Do not ride the Broke Bus!'); }, 3000));
 
-// // -- 'resize' // 
-// const heightOutput = document.querySelector('#height');
-// const widthOutput = document.querySelector('#width');
-
-// function reportWindowSize() {
-//     heightOutput.textContent = window.innerHeight;
-//     widthOutput.textContent = window.innerWidth;
-// }
-
-// window.onresize = reportWindowSize;
