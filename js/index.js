5a427104ce5444c77eee3f79a282ee53cf771f77
Steve Renner <che1111@hotmail.com>
2019-09-03 13:41:00 -0700

    Added Blur and Change Photo

--- js/index.js
+++ js/index.js
@@ -1,11 +1,14 @@
 // Your code goes here
+
+// Mouseover bus image grows in size
 const imgScale = document.querySelector('img');
 imgScale.addEventListener("mouseover", e => {
     imgScale.style.transform = 'scale(1.5)';
 });
 
+// Zoom with mouse wheel on header
 function zoom(event) {
-    // event.preventDefault()
+    event.preventDefault()
     scale += event.deltaY * -0.01;
     scale = Math.min(Math.max(.125, scale), 4);
     el.style.transform = `scale(${scale})`;
@@ -13,3 +16,16 @@ function zoom(event) {
 let scale = 1;
 const el = document.querySelector('header');
 el.onwheel = zoom;
+
+const textBlur = document.querySelector('footer[type="textBlur"]');
+    textBlur.addEventListener('focusout', e => {
+        footer.target.style.background = 'purple';
+    });
+
+const newBus = document.querySelectorAll('.img-content');
+newBus.forEach(item => {
+    item.addEventListener('click', e => {
+    item.src= 'https://images.unsplash.com/photo-1525962898597-a4ae6402826e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60';
+    item.alt ="New Bus"
+    })
+})
