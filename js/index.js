b2b8cc3e32adecb36d2ab1519c7d0766e0dba70d
Manju KC <kcmanjuthapa@gmail.com>
2019-09-03 17:52:23 -0400

    added scroll eventListner

--- js/index.js
+++ js/index.js
@@ -88,3 +88,10 @@ introHeading.addEventListener("copy", function(e) {
     introHeading.innerText = "Don't copy me!"
 })
 
+//add spin animation to bottom image
+const destinationImg = document.querySelector(".content-destination img")
+window.addEventListener("scroll", function(e) {
+    TweenMax.to(destinationImg, 3, {
+        rotationY: 360
+    });
+});
