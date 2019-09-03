095b87711acbc00eba8a059096015389fb5a5b54
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-09-03 15:39:57 -0400

    nav bar minipulated

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+
+// Mouseover
+const navBar = document.querySelector(".nav");
+navBar.addEventListener("mouseover", (e) => {
+    navBar.style.backgroundColor = 'hotpink';
+});
