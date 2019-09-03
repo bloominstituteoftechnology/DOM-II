b43afcf14fe770a8b5df06bd9201b9e7e00ce222
Schrese <53787633+Schrese@users.noreply.github.com>
2019-09-03 18:16:54 -0400

    Just starting (got 3 done)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+//This makes the "fun bus" logo get GIANT 
+const logoScaleUp = document.querySelector('.logo-heading');
+    logoScaleUp.addEventListener('mouseenter', e => {
+    console.log('mouseover happened');
+    logoScaleUp.style.transform = 'scale(25)';
+});
+
+const logoScaleDown = document.querySelector('.logo-heading');
+    logoScaleDown.addEventListener('mouseleave', e => {
+        logoScaleDown.style.transform = 'scale(0.5)';
+});
+
+//This displays the window size in the top right
+const sizeChange = document.querySelector('.nav-link');
+    sizeChange.addEventListener('resize', e => {
+        sizeChange.style.size = 2;
+    });
+
