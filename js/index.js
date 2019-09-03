ea4dab286b70f304bb4d1c6ebe5844974e5ec63a
Sean <Sean@Witch-Doctor.home>
2019-09-03 16:45:31 -0400

    Event 1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+let navBar = document.querySelector('.nav')
+
+
+
+
+let navMouseOver = document.querySelectorAll('.nav-link');
+navMouseOver.forEach(item => {
+    item.addEventListener("mouseenter", event => {
+    event.target.style.color = 'purple';
+    })
+})
+navMouseOver.forEach(item => {
+    item.addEventListener("mouseleave", event =>{
+    event.target.style.color = 'orange';
+    })
+});
