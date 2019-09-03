5f3326513c98b5b125ddc14ceec9eab6bba9dab9
meera-andersen <meera.andersen@gmail.com>
2019-09-03 15:13:02 -0600

    added hover effects to the nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+/*
+* [ ] `mouseover`
+* [ ] `keydown`
+* [ ] `wheel`
+* [ ] `drag / drop`
+* [ ] `load`
+* [ ] `focus`
+* [ ] `resize`
+* [ ] `scroll`
+* [ ] `select`
+* [ ] `dblclick`
+*/
+
+const linkScaleUp = document.querySelectorAll(".nav-link");
+linkScaleUp.forEach(item => {
+    item.addEventListener('mouseenter', event => {
+        item.style.color = "#17A2B8";
+        item.style.transform = 'scale(1.2)';
+        item.style.transition = 'transform 0.3s';
+    })
+
+    item.addEventListener('mouseleave', event => {
+        item.style.color = "#212529";
+        item.style.transform = 'scale(1.0)';
+        item.style.transition = 'transform 0.3s';
+    })
+});
+
