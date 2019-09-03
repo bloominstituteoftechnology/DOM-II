6fd016cd6cfc1fc42c33ccf1124601e2f59610d2
Temitope Akinsoto <takinsoto@gmail.com>
2019-09-03 14:29:45 +0100

    feat: Add Keydown event listener
    In this commit, an event handler was added to the web page. The event listens a
    keydown event and triggers a callback which then plays a sound for each of the
    keys a-s-d-f-l-k letters

--- js/index.js
+++ js/index.js
@@ -4,4 +4,38 @@ pageImages.forEach(image => {
     image.addEventListener('mouseover', e => {
         console.log(e.target.classList.toggle('toggleImage'));
     });
-})
+});
+
+//on keydown
+
+document.body.addEventListener('keydown', e => {
+    let keyThatGotPressed = e.key;
+    let sound = new Audio('sounds/snare.mp3');
+    let sound1 = new Audio('sounds/crash.mp3');
+    let sound2 = new Audio('sounds/tom-1.mp3');
+    let sound3 = new Audio('sounds/tom-2.mp3');
+    let sound4 = new Audio('sounds/tom-3.mp3');
+    let sound5 = new Audio('sounds/tom-4.mp3');
+    let sound6 = new Audio('sounds/kick-bass.mp3');
+    if(keyThatGotPressed === 'a'){
+        sound.play();
+    } 
+    else if(keyThatGotPressed === 's'){
+        sound6.play();
+    }
+    else if(keyThatGotPressed === 'd') {
+        sound2.play()
+    }
+    else if(keyThatGotPressed === 'f') {
+        sound3.play()
+    }
+    else if(keyThatGotPressed === 'l') {
+        sound4.play()
+    }
+    else if(keyThatGotPressed === 'k'){
+        sound5.play()
+    }
+    else {
+        sound1.play();
+    }
+});
