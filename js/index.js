d34d0572428b4c761a38bde8fda6b80077721eb0
Sean <Sean@Witch-Doctor.home>
2019-09-03 16:55:57 -0400

    Event 2 - Menu Toggle

--- js/index.js
+++ js/index.js
@@ -1,6 +1,23 @@
 // Your code goes here
 
-let navBar = document.querySelector('.nav')
+let menuBtn = document.querySelector('.menu-btn');
+let menu = document.querySelector(".menu");
+let menuStatus = false;
+
+menu.style.marginLeft = '-500px';
+
+function menuToggle(){
+    if (menuStatus == false){
+        menu.style.marginLeft = '0px';
+        menuStatus = true;
+    }
+    else if (menuStatus == true){
+        menu.style.marginLeft = '-500px';
+        menuStatus = false;
+    }
+}
+
+menuBtn.onclick = menuToggle;
 
 
 
