93e1bf638886ac49218d1015b3f0da0b9783163f
Anthony Hill <hillan1152@gmail.com>
2019-09-04 09:44:01 -0400

    another one

--- js/index.js
+++ js/index.js
@@ -6,10 +6,20 @@
 
 //mouse over
 const logo = document.querySelector('.logo-heading');
-logo.addEventListener('mouseover', e => event.target.style.color = 'red');
+logo.addEventListener('mouseover', event => event.target.style.color = 'red');
 
 
 //key down
+const backgroundChange = document.querySelector('body');
+backgroundChange.addEventListener('keydown', e => {
+    backgroundChange.style.color = 'red';
+    e.stopPropagation();
+})
+
+backgroundChange.addEventListener('keyup', e => {
+    backgroundChange.style.color = 'black';
+    e.stopPropagation();
+})
 
 
 
@@ -17,8 +27,30 @@ logo.addEventListener('mouseover', e => event.target.style.color = 'red');
 window.addEventListener('load', e => console.log('PAGE IS LOADED'));
 
 //dbl click
-const card = document.querySelector('btn');
+const button = document.querySelectorAll('.content-pick .destination .btn');
+Array.from(button).forEach(function(button){
+    button.addEventListener("dblclick", function(e){
+        e.target.style.backgroundColor = 'red'
+    })
+})
 
-card.addEventListener('dblclick', function (e) {
-  card.e.toggle('large');
+
+//click
+let h2 = document.querySelector(".content-destination h2");
+h2.addEventListener('click', function(e) {
+    e.target.style.color = "blue";
 });
+
+//rotate
+const photos = document.querySelectorAll('img');
+
+
+//context menu (RIGHT CLICK)
+// const midText = document.querySelectorAll('.content-section p');
+// midText.forEach(text => {
+//     text.addEventListener('contextmenu', event => {
+//         event.preventDefault();
+//     })
+// })
+
+
