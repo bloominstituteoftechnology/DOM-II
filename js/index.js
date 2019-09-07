4837f4555eb06b5680187d06e4bb20689a3f2a16
Miley Wright <dishaliekwright@yahoo.com>
2019-09-07 12:11:50 -0400

    8/10 events completed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 // Your code goes here
 
 
+// window.addEventListener('load', (event) => {
+//     alert('YOU JUST WON A FREE TRIP!! PRESS OK TO ACCEPT!');
+// })
 
 const logo = document.querySelector('.logo-heading');
 logo.addEventListener('mouseover',(event) => {
@@ -13,6 +16,21 @@ logo.addEventListener('mouseleave',(event) => {
 logo.style.cursor = 'default';
  
 const firstImage = document.querySelector('.intro img');
-firstImage.addEventListener('keydown', (event) => {
-    event.target.
+firstImage.addEventListener('click', (event) => {
+    firstImage.src = 'https://images.unsplash.com/photo-1465172018141-c4e4fa823289?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=800';
+})
+
+function zoom(event) {
+    event.preventDefault();
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    size.style.transform = `scale(${scale})`;
+  }
+  let scale = 1;
+  const size = document.querySelector('div');
+  size.onwheel = zoom;
+
+document.querySelector('.btn').addEventListener('dblclick', (event) => {
+    event.target.style.backgroundColor = 'white';
+    event.target.style.color = '#17A2B8'
 })
