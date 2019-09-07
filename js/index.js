ef6c8d03d53be6d141877a03f04af625d2e60d59
Miley Wright <dishaliekwright@yahoo.com>
2019-09-06 23:14:32 -0400

    adding keydown event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+
+
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('mouseover',(event) => {
+    event.target.style.color = 'yellow';
+    
+})
+logo.addEventListener('mouseleave',(event) => {
+    event.target.style.color = 'red';
+})
+logo.style.cursor = 'default';
+ 
+const firstImage = document.querySelector('.intro img');
+firstImage.addEventListener('keydown', (event) => {
+    event.target.
+})
