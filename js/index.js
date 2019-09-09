4a274bad6b7717e164f6355741416a92885fd5b8
alberto90rodriguez <arodriguez.jr@outlook.com>
2019-09-09 14:00:43 -0500

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+//Used click to change the background on the buttons to red and then dblclick to change back to default color
+const button = document.querySelectorAll('.btn');
+button.forEach(function(element){
+    element.addEventListener('click', function(e){
+        // console.log('click works!')
+        e.target.style.background = 'red';
+    })
+    button.forEach(function(element){
+        element.addEventListener('dblclick', function(e){
+        //    console.log('dblclick works!')
+            e.target.style.background = '';
+        })
+    })
+
+    button.forEach(function(element){
+
+    })
+});
