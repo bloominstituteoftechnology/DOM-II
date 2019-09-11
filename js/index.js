46fe0fdc0cb35cad435f15cae053e0a9a9b8e562
Germaine Pannell <me@germainep.com>
2019-09-10 21:36:28 -0400

    added blur effect to images on mouseenter

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+const images = document.querySelectorAll('img');
+const page = document.querySelector('body');
+
+const imageArray = Array.from(images);
+
+imageArray.forEach(el => {
+  el.addEventListener('mouseenter', event => {
+    event.target.classList.add('blur');
+  });
+  el.addEventListener('mouseleave', event => {
+    event.target.classList.remove('blur');
+  });
+});
