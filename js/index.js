163914916fad5e2d7cc77d98f43ae3d9d1c03907
Chris Reyes <creyop1987@gmail.com>
2019-10-01 17:46:02 -0500

    mvp

--- js/index.js
+++ js/index.js
@@ -2,51 +2,46 @@
 
 const changeImg = document.querySelector('h1');
 changeImg.addEventListener("dblclick", () => {
-  changeImg.style.textTransform = "lowercase"
-  console.log(changeImg)
+    changeImg.style.textTransform = "lowercase"
+
 })
 
 const focusImg = document.querySelector('img');
 focusImg.addEventListener("mouseenter", () => {
-  focusImg.style.transform = "scale(1.9)"
-  console.log(focusImg)
-focusImg.addEventListener("mouseleave", () => {
-  focusImg.style.transform = "scale(1)"
-  console.log(focusImg)
-})
-})
+    focusImg.style.transform = "scale(1.9)"
 
+    focusImg.addEventListener("mouseleave", () => {
+        focusImg.style.transform = "scale(1)"
+    })
+})
 
 const navFocus = document.querySelector("a")
 navFocus.addEventListener("mouseover", () => {
     navFocus.style.textTransform = "lowercase"
-    console.log(navFocus)
+
 })
 
 const imageScale = document.querySelectorAll('.img-content')
 
-imageScale.forEach ((image)=>{
-image.addEventListener("click", () => {
-    image.style.transform = "scale(1.9)"
-    image.stlye.transition = "transform 0.5s"
+imageScale.forEach((image) => {
+    image.addEventListener("click", () => {
+        image.style.transform = "scale(1.9)"
+        image.stlye.transition = "transform 0.5s"
+    });
 
-    });    
- 
-image.addEventListener("wheel", () => {
-    image.style.transform = "scale(1)"
-    image.stlye.transition = "transform 0.5s"
-
-    });    
+    image.addEventListener("wheel", () => {
+        image.style.transform = "scale(1)"
+        image.stlye.transition = "transform 0.5s"
+    });
 });
 
 const buttonSpin = document.querySelectorAll('.btn')
 
-buttonSpin.forEach((button)=>{
-    button.addEventListener("click",() =>{
+buttonSpin.forEach((button) => {
+    button.addEventListener("click", () => {
         button.style.transform = "scale(1.9)"
-    button.stlye.transition = "transform 0.5s"
-
-    }); 
+        button.stlye.transition = "transform 0.5s"
+    });
 })
 
 const log = document.querySelector('.destination');
@@ -54,25 +49,23 @@ const log = document.querySelector('.destination');
 document.addEventListener('keypress', logKey);
 
 function logKey(e) {
-  log.textContent += ` ${e.code}`;
+    log.textContent += ` ${e.key}`;
 }
 
 window.addEventListener('load', () => {
     console.log('page is fully loaded');
-  });
+});
 
 const home = document.querySelector('body')
-  home.addEventListener('dblclick', e => {
+home.addEventListener('dblclick', e => {
     e.target.style.backgroundColor = 'blue'
     e.stopPropagation();
-    console.log(home);
-
-  })
+})
 
 const stopLink = document.querySelector("nav");
 
 stopLink.addEventListener("click", (event) => {
-  console.log("stopped link");
-  
-  event.preventDefault();
+    console.log("stopped link");
+
+    event.preventDefault();
 })
