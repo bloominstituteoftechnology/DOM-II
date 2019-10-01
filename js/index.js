76934cc008e9df0f3eba411e4a6987fa961b75be
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-01 11:27:10 -0700

    Added scroll changes on body, nav bar and nav-links

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 // Your code goes here
 const contentImg = document.querySelectorAll(".content-section img");
-console.log(contentImg);
+const bodyTag = document.querySelector("body");
+const navigation = document.querySelector(".main-navigation");
+const navLinks = document.querySelectorAll(".nav-link");
 
 // Mouse Over
 contentImg.forEach(img => {
@@ -15,5 +17,17 @@ contentImg.forEach(img => {
   img.addEventListener("mouseleave", () => {
     img.style.transform = "scale(1)";
     img.style.transition = "all 0.3s";
+  });
+});
+
+// Scroll
+window.addEventListener("scroll", () => {
+  bodyTag.style.backgroundColor = "#3c4857";
+  bodyTag.style.color = "#b35700";
+  navigation.style.backgroundColor = "#3c4857";
+  navigation.style.color = "#b35700";
+
+  navLinks.forEach(link => {
+    link.style.color = "#b35700";
   });
 });
