5973d999e91f81f518fba3e5ac3dc5ff1e3a9fae
Anthony Sutton <anthony.sutton95@gmail.com>
2019-10-01 11:09:58 -0700

    Added mouseover and mouseleave event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+const contentImg = document.querySelectorAll(".content-section img");
+console.log(contentImg);
+
+// Mouse Over
+contentImg.forEach(img => {
+  img.addEventListener("mouseover", () => {
+    img.style.transform = "scale(1.1)";
+    img.style.transition = "all 0.3s";
+  });
+});
+
+// Mouse Leave
+contentImg.forEach(img => {
+  img.addEventListener("mouseleave", () => {
+    img.style.transform = "scale(1)";
+    img.style.transition = "all 0.3s";
+  });
+});
