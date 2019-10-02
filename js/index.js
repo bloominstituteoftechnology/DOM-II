592c5394c913ff3b163343820e6426880538f594
Hunter5555 <hunter.m.allan@gmail.com>
2019-10-02 13:26:10 -0600

    hunter-allan

--- js/index.js
+++ js/index.js
@@ -1,1 +1,65 @@
-// Your code goes here
+const navA = document.querySelectorAll(".nav, a")
+document.querySelectorAll(".nav, a").forEach(el => {
+    el.addEventListener("mouseenter", () => {
+        el.style.transform = "scale(1.5)";
+        el.style.color= "gray";
+        el.style.transition = "all 0.3s";
+    })
+})
+document.querySelectorAll(".nav, a").forEach(el => {
+    el.addEventListener("mouseleave", () => {
+        el.style.transform = "scale(1)";
+        el.style.color= "white";
+        el.style.transition = "all 0.8s";
+    })
+})
+
+const textContentP = document.querySelector(".text-content, p")
+
+textContentP.addEventListener("click", () =>{
+    textContentP.style.color= "red";
+    textContentP.style.fontSize= "150%"
+})
+textContentP.addEventListener("dblclick", () =>{
+    textContentP.style.color= "blue";
+    textContentP.style.fontSize= "100%"
+})
+
+document.querySelectorAll("img").forEach(e => {
+    e.addEventListener("dblclick", () => {
+        e.style.display = "none";
+    })
+    e.addEventListener("click", () => {
+        e.style.transform  = 'rotate(360000deg)';
+        e.style.transition = "all 10s"
+        event.stopPropagation();
+    })
+
+})
+
+
+document.querySelectorAll('h2').forEach(e => {
+    e.addEventListener("mouseover", () => {
+        e.style.fontSize = "500%"
+    })
+    e.addEventListener("dblclick", () => {
+        e.style.fontSize = "1000%"
+    })
+})
+
+const body = document.querySelector("body");
+const par = document.querySelectorAll(".btn");
+
+window.addEventListener('scroll', () => {
+    body.style.backgroundColor="lightGreen";
+    textContentP.style.backgroundColor ="purple";
+    body.style.transform = "rotateZ(360deg)"
+    body.style.transition = "all 1s"
+  })
+
+
+  document.querySelectorAll("section").forEach(e => { e.addEventListener("click", () => {
+        e.style.transform  = 'rotateY(360deg)';
+        e.style.transition = "all 10s"
+    })
+})
