14b04e778da4b08d008d7a4021b8b14a371f769f
brianetaveras <taveras574@gmail.com>
2019-10-03 20:25:28 -0400

    Added event listener to change header image when clicked andn add parrots on hover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+let headerImage = document.querySelector('.intro img')
+let imgNum = 0
+headerImage.addEventListener('click', (e)=>{
+
+    e.target.setAttribute('src', 'https://picsum.photos/1000/300?random=' + imgNum)
+    imgNum++
+})
+
+
+let logoHeading = document.querySelector('.logo-heading')
+logoHeading.addEventListener('mouseover', (e)=>{
+    e.target.innerHTML = `<img width="32px" src="https://cultofthepartyparrot.com/parrots/hd/parrot.gif"> Fun Bus <img width="32px" src="https://cultofthepartyparrot.com/parrots/hd/parrot.gif">`
+})
+
+ 
