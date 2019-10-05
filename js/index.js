fda31bea5998288ef60c4cf0e16b2ea056e095ba
AdamMaley <adamemaley@yahoo.com>
2019-10-04 20:52:19 -0400

    made most of event listeners

--- js/index.js
+++ js/index.js
@@ -1,2 +1,68 @@
 // Your code goes here
-document.html.style.backgroundColor = "red";
+const logoMouseOver = document.querySelector(".logo-heading");
+logoMouseOver.addEventListener('mouseover', (e)=> {
+    e.target.style.backgroundColor = "green";
+});
+
+
+const fKey = document.querySelector("body");
+
+fKey.addEventListener("keydown", (e)=> {
+    e.target.style.backgroundColor = "red";
+});
+
+
+const mouseWheel = document.querySelector("body");
+
+mouseWheel.addEventListener("wheel", (e)=> {
+    e.target.style.backgroundColor = "yellow";
+});
+
+const doubleClick= document.querySelector("img");
+
+selectorText.addEventListener("dblclick", (e)=> {
+    e.target.style.color = "purple";
+});
+
+const mouseLeave = document.querySelector(".nav");
+
+mouseLeave.addEventListener("mouseover", (e)=> {
+    e.target.style.backgroundColor = "blue";
+});
+
+
+const pWhite = document.querySelector("p");
+
+pWhite.addEventListener("mouseleave", (e)=> {
+    e.target.style.backgroundColor = "white";
+});
+
+const navLinkBlack = document.querySelector(".nav-link");
+
+resizer.addEventListener("mouseenter", (e)=> {
+    e.target.style.backgroundColor = "black";
+});
+
+const resizer = document.querySelector(".nav");
+
+resizer.addEventListener("resize", (e)=> {
+    e.target.style.backgroundColor = "black";
+});
+
+const resizer = document.querySelector(".nav");
+
+resizer.addEventListener("resize", (e)=> {
+    e.target.style.backgroundColor = "black";
+});
+
+const resizer = document.querySelector(".nav");
+
+resizer.addEventListener("resize", (e)=> {
+    e.target.style.backgroundColor = "black";
+});
+
+const resizer = document.querySelector(".nav");
+
+resizer.addEventListener("resize", (e)=> {
+    e.target.style.backgroundColor = "black";
+});
