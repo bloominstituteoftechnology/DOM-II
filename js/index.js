95a6004b33c7151da304765d60ce80ff1bbabfdb
brianetaveras <taveras574@gmail.com>
2019-10-08 19:42:41 -0400

    Added the rest of the events

--- js/index.js
+++ js/index.js
@@ -8,6 +8,9 @@ headerImage.addEventListener('click', (e)=>{
     imgNum++
 })
 
+headerImage.addEventListener('dblclick', (e)=>{
+    welcome.textContent = "That's just rude! "
+})
 
 let logoHeading = document.querySelector('.logo-heading')
 logoHeading.addEventListener('mouseover', (e)=>{
@@ -15,15 +18,17 @@ logoHeading.addEventListener('mouseover', (e)=>{
 })
 
  let nav = document.querySelector('.main-navigation')
-nav.addEventListener('click', ()=>{
-    nav.getElementsByClassName.background = "#363636"
+nav.addEventListener('drag', ()=>{
     alert(`Yo! Don't poke me ther B!`)
 })
 
+
+
 let navLink = document.querySelectorAll('.nav .nav-link')
 navLink.forEach(link=>{
-    link.addEventListener('click', (e)=>{
+    link.addEventListener('wheel', (e)=>{
         e.stopPropagation()
+        e.preventDefault()
         e.target.innerHTML = `<img width="32px" src="https://cultofthepartyparrot.com/parrots/hd/parrot.gif">`
     })
 })
@@ -31,7 +36,34 @@ navLink.forEach(link=>{
 let welcome = document.querySelector('.intro h2')
 document.addEventListener('keydown', (e)=>{
     e.preventDefault()
-    welcome.innerHTML = `You pressed: '${e.key}' and the key code is: '${e.keyCode}'`
-   
-    
+    welcome.innerHTML = `You pressed: '${e.key}' and the key code is: '${e.keyCode}'` 
+})
+
+
+
+
+let letsGo = document.querySelector('.text-content h2')
+
+document.addEventListener('copy', (e)=>{
+    letsGo.textContent = 'YO, YO! Stop copying that!'
+})
+
+document.addEventListener('cut', (e)=>{
+    letsGo.textContent = 'Now you cutting!?'
+})
+
+document.addEventListener('mousedown', (e)=>{
+    letsGo.textContent = 'You have been pressing that for too long'
+})
+
+
+
+
+
+document.addEventListener('scroll', (e)=>{
+    logoHeading.innerHTML = 'Yeah, keep scrolling pal'
+})
+
+document.addEventListener('resize', (e)=>{
+    console.log('WHAT ARE YOU DOING?')
 })
