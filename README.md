7dd214d6b0c9d7fac8a318a3cd6b461172a86600
simonecarolynr <simonecarolynr@gmail.com>
2019-10-08 19:48:28 -0600

    almost done

--- README.md
+++ README.md
@@ -34,16 +34,16 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 ## Task 2: Create Unique Event Listeners
 
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
-	* [ ] `keydown`
-	* [ ] `wheel`
-	* [ ] `drag / drop`
-	* [ ] `load`
-	* [ ] `focus`
-	* [ ] `resize`
-	* [ ] `scroll`
+	* [x] `mouseover`
+	* [x] `mousenter`
+	* [x] `afterprint`
+	* [x] `blur`
+	* [x] `beforeprint`
+	* [x] `wheel`
+	* [x] `load`
+	* [x] `mouseleave`
 	* [ ] `select`
-	* [ ] `dblclick`
+	* [x] `dblclick`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,68 @@
 // Your code goes here
+//mouseover
+let nav = document.querySelectorAll('.nav .nav-link');
+console.log(nav)
+nav.forEach( n => {
+    n.addEventListener('mouseover', function(event){ event.target.style.color = 'red';
+})});
+
+//dblcclick
+let imgFun = document.querySelectorAll('img');
+console.log(imgFun)
+imgFun.forEach( i => {
+    console.log(i);
+    i.addEventListener('dblclick', function (event) {
+        console.log(event)
+        event.target.classList.toggle('large');
+    })
+});
+
+//select
+function logSelection(event) {
+    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+    console.log(`You selected: ${selection}`);
+}
+
+const select = document.querySelectorAll('p');
+select.forEach(paragraph => {
+    addEventListener('select', logSelection);
+})
+
+
+//mouse leave/mouse enter
+const mouseTarget = document.querySelectorAll('.nav .nav-link');
+
+mouseTarget.forEach(navLink => {
+    navLink.addEventListener('mouseenter', e => {
+        e.target.style.border = '5px dotted orange';
+    });
+
+    navLink.addEventListener('mouseleave', e => {
+        e.target.style.border = '1px solid dodgerblue';
+    });
+
+    navLink.addEventListener('click', e => {
+        e.preventDefault();
+    })
+});
+
+//alert/wheel
+
+window.addEventListener('wheel', (event) => {
+   alert('Ha Ha'); 
+});
+
+//beforeprint
+window.addEventListener('beforeprint', (event) => {
+    console.log('why');
+});
+
+//load
+window.addEventListener('load', (event) => {
+    console.log('page is loaded');
+});
+
+//afterprint
+  window.addEventListener('afterprint', (event) => {
+    console.log('still why');
+});
