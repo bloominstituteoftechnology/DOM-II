1daead3324d6f0255f5cc35ad728116d223fa609
Ana Araujo <actanaaraujo@gmail.com>
2019-10-08 18:57:20 -0700

    JS fies updated

--- js/index.js
+++ js/index.js
@@ -1,1 +1,105 @@
-// Your code goes here
+//intro mouseenter, keydown
+const busImg = document.querySelector('.intro img');
+const introSection = document.querySelector('.intro');
+const introHeading = document.querySelector('.intro h2');
+
+busImg.addEventListener('mouseenter', function(event) {
+    event.target.style.transform = 'scale(.8)';
+    event.target.style.transition = 'all .4s ease-out';
+    introSection.style.background = `rgba(237, 201, 175, .4)`;
+    introSection.style.transition = 'all .4s ease-out';
+});
+
+document.addEventListener('keydown', function(event){
+    //console.log(event);
+    introHeading.style.color = '#756345';
+    introHeading.style.fontSize = '5rem';
+});
+
+
+//text - content wheel event
+const textContent = document.querySelectorAll('.text-content');
+
+document.addEventListener('wheel', function(event){
+    //  textContent[0].style.background = '#fff9ef';
+    //  textContent[0].style.transition = 'all .3s ease-out';
+    //  textContent[0].style.transitionDelay = '3s';
+
+    //  textContent[1].style.background = '#fff9ef';
+    //  textContent[1].style.transition = 'all .3s ease-out';
+    //  textContent[1].style.transitionDelay = '3s';
+
+     for(let i = 0; i<textContent.length; i++){
+        textContent[i].style.background = '#fff9ef';
+        textContent[i].style.transition = 'all .3s ease-out';
+        textContent[i].style.transitionDelay = '3s'
+     }
+
+});
+
+//content-destination scroll, dblclick
+
+const contentDestination = document.querySelector('.content-destination');
+const scrolledHeading = document.querySelector('.content-destination h2');
+const destination = document.querySelectorAll('.destination p');
+
+contentDestination.addEventListener('dblclick', function(event){
+    console.log(event);
+    event.target.style.background = `rgba(237, 201, 175, .4)`;
+});
+
+window.addEventListener('scroll', function(){
+    //console.log(textContent[]);
+    for(let i = 0; i<3; i++){
+    destination[i].style.background = 'rgba(237, 201, 175, .4)';
+    }
+});
+
+
+//destination select
+
+
+
+const footerP = document.querySelector('footer p');
+
+footerP.addEventListener('mousedown', function(event){
+    footerP.style.color = 'brown';
+    TweenMax.to(introH2, 2, {rotation:1080});
+});
+
+
+//resize
+window.addEventListener('resize', function(event) {
+    footerP.style.background = 'green';
+});
+
+
+//stop propagation
+const nav = document.querySelector(".main-navigation .nav");
+console.log(nav);
+
+nav.addEventListener("dblclick", function(event) {
+  event.stopPropagation();
+  event.preventDefault();
+  event.target.style.fontSize = "40px";
+});
+
+ window.addEventListener('dblclick', function(){
+    alert('Dblclick');
+});
+
+
+//mouseup on buttons
+const button = document.querySelectorAll('.destination .btn');
+
+button.forEach(item => {
+    item.addEventListener('mouseup', function(event){
+        item.style.background = 'blue';
+    });
+});
+
+//changing background of the body
+
+footerP.addEventListener('click', function(event){
+    footerP.style.background = 'lightgreen';
+})
