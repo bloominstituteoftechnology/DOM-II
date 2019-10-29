e55e2a6cf31994daf0a38ac415a6e9a62b2f868a
josiah <josiahdamiwilliams@gmail.com>
2019-10-29 15:33:38 +0100

    Add click, mouseover and doubleclick event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const button1 = document.querySelector('#btn1');
+button1.addEventListener('click', function(event){
+  event.target.style.color = 'black';
+});
+
+const button2 = document.querySelector('#btn2');
+button2.addEventListener('mouseover', function(event){
+  event.target.style.background = 'black';
+});
+
+const button3 = document.querySelector('#btn3');
+button3.addEventListener('dblclick', function(event){
+  event.target.style.borderColor = 'red';
+});
