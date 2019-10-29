6ec5b9f3be8661f4c4fd6a09455e0d5492cfcb95
astubbings <53932400+astubbings@users.noreply.github.com>
2019-10-29 14:04:48 -0600

    first changes to setup pull

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+const funBusTextChange =  document.querySelector('.logo-heading');
+
+funBusTextChange.addEventListener("mouseenter", () => {
+    funBusTextChange.style.transform = 'scale(1.5)';
+    funBusTextChange.style.transition = 'all 500ms';
+    funBusTextChange.style.border ='2px solid black';
+    funBusTextChange.style.padding = '0 1%';
+    funBusTextChange.style.color = "darkorange";
+})
+
+funBusTextChange.addEventListener("mouseleave", () => {
+    funBusTextChange.style.transform = 'scale(1)';
+    funBusTextChange.style.transition = 'all 500ms';
+    funBusTextChange.style.border ='2px solid yellow';
+    funBusTextChange.style.padding = '1% 1% 0 1%';
+    funBusTextChange.style.color = "black";
+})
