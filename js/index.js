f1c3888515d8cd91e735c0404338f4f079fa52ce
Maxwell Beard <maxwelljamesbeard@gmail.com>
2019-10-29 12:45:19 +0000

    some event listeners added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+const welcome = document.querySelector('.intro');
+welcome.addEventListener('mouseover', event => event.target.style.color = 'orange');
+setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+ false;
+
+
+
+window.addEventListener('keydown', event => {
+    console.log(`Input detected keypress: ${event.key}`);
+});
+
+const riverBoat = document.querySelector('.img-fluid rounded');
+riverBoat.addEventListener('wheel', zoom);
