60c8fbf9dc39508fb270096b89dfcaf25def4d80
Jacob Washburn <jacobrwashburn@gmail.com>
2019-10-29 12:42:39 -0600

    added first one

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const logo = document.querySelector('.logo-heading');
+
+logo.addEventListener('mouseover', () => {
+   logo.style.color = 'green';
+   setTimeout(() => { logo.style.color = '' }, 600);
+})
