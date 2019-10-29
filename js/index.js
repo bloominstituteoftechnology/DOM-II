10124ffdf1240a5b93c1e0d1dd831ef568a7ce9a
jarrodskahill <jarrodskahill@gmail.com>
2019-10-29 13:14:52 -0700

    made picture disappear on hover and return on leave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+//Event 1
+const logo = document.querySelector("header img");
+logo.addEventListener("mouseover", () => {
+    logo.style.display= "none";
+})
+
+logo.addEventListener("mouseleave", () => {
+    logo.style.display = ""
+})
+
+// const navChange = document.getElementsByClassName("nav-link");
+// navChange.addEventListener("dblclick", () => {
+//     navChange.style.color = "brown"
+// })
+
+const darkMode = document.querySelector("body");
+const whiteText = document.querySelectorAll("h1", "h2", "h3","h4", "p")
+const acitvateDarkMode = document.getElementsByClassName("btn");
+acitvateDarkMode.addEventListener("click", () => {
+    darkMode.style.backgroundColor = "black";
+    whiteText.style.color = "white";
+})
