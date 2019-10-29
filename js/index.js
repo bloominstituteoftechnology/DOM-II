3045a14eabc0f34987e76b21db739e5a68df8ea1
sebastian-ojefors <godnoken@gmail.com>
2019-10-29 17:02:35 +0100

    initial commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+/* const test = document.querySelector(".intro");
+test.addEventListener("click", (event) => {
+   event.target.style.color = "red"; 
+});
+console.log(test); */
+
+const introImg = document.querySelector("img");
+introImg.addEventListener("mouseover", (event) => {
+    if (introImg.style.opacity == "1") {
+        event.target.style.opacity = "0.5";
+    } else {
+        event.target.style.opacity = "1";
+    }
+});
+
+/* window.addEventListener("keydown", event => {
+    if (event.target == "g") {
+        console.log("You picked the correct button!")
+    }
+})
+
+console.log(introImg.opacity) */
