2fe4e22d74aa00739e42509ac0ff06fe89c08fe6
josiah <josiahdamiwilliams@gmail.com>
2019-10-29 17:12:16 +0100

    mvp complete

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ const button1 = document.querySelector('#btn1');
 button1.addEventListener('click', function(event){
   event.target.style.color = 'black';
 });
-
+//stop propagation()
 const button2 = document.querySelector('#btn2');
 button2.addEventListener('mouseover', function(event){
   event.target.style.background = 'black';
@@ -11,48 +11,57 @@ event.stopPropagation();
 });
 
 const divClick = document.querySelector('#divBorder');
+
 divClick.addEventListener('click', function(event){
   event.currentTarget.style.border = '1px solid red';
 });
-
+//dblclick event listener
 const button3 = document.querySelector('#btn3');
 button3.addEventListener('dblclick', function(event){
   event.target.style.borderColor = 'red';
 });
-
+//mouseenter event listener
 const img1 = document.querySelector('#img1');
 img1.addEventListener('mouseenter', function(event){
   event.target.style.border = '2px dashed green';
 });
-
+//mouseleave event listener
 img1.addEventListener('mouseleave', function(event){
     event.target.style.border = 'none';
   })
-
+//mousedown event listener
 const img4 = document.querySelector('#img4');
 img4.addEventListener('mousedown', function(event){
   event.target.style.opacity = '0';
 });
-
+//mouseup event listener
 img4.addEventListener('mouseup', function(event){
   event.target.style.opacity = '1';
 });
-
+//copy event listener
 const p = document.querySelector('p');
 
 p.addEventListener('copy', function(event){
   alert('Why are you copying my paragraph!?')
 });
-
+//contextmenu
 const img2 = document.querySelector('#img2');
 img2.addEventListener('contextmenu', function(event){
   event.target.style.border = '2px solid yellow';
 });
-
+//mouseOut Listener
 img2.addEventListener('mouseout', function(event){
   event.target.style.border = 'none';
 });
 
-navlinks.addEventListener('click', (event) => {
-    event.preventDefault();
-}); 
+//Stop navigation items
+document.querySelectorAll(".nav-link").forEach(element => {
+    element.addEventListener("click", event => {
+      event.preventDefault();
+    });
+    
+//load event listener
+window.addEventListener("load", function(event) {
+    setTimeout(() => (alert("Are you ready? Because this webpage is.."), 0));
+})
+})
