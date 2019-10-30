77ab1aeeff1ebf4783cd32c6e2560b05c406a068
jarrodskahill <jarrodskahill@gmail.com>
2019-10-29 21:41:25 -0700

    finished 8 unique events

--- js/index.js
+++ js/index.js
@@ -4,20 +4,65 @@ const logo = document.querySelector("header img");
 logo.addEventListener("mouseover", () => {
     logo.style.display= "none";
 })
-
+//Event 2
 logo.addEventListener("mouseleave", () => {
     logo.style.display = ""
 })
 
-// const navChange = document.getElementsByClassName("nav-link");
-// navChange.addEventListener("dblclick", () => {
-//     navChange.style.color = "brown"
-// })
 
+//Event 3
 const darkMode = document.querySelector("body");
-const whiteText = document.querySelectorAll("h1", "h2", "h3","h4", "p")
-const acitvateDarkMode = document.getElementsByClassName("btn");
-acitvateDarkMode.addEventListener("click", () => {
+const botWhite = document.querySelectorAll("p, h2, h3, h4");
+const acitvateDarkMode = document.querySelector(".btn");
+acitvateDarkMode.addEventListener("click", (event) => {
     darkMode.style.backgroundColor = "black";
-    whiteText.style.color = "white";
+    botWhite.forEach(element => {
+        element.style.color="white";
+    })
+
 })
+// Event 4
+acitvateDarkMode.addEventListener("dblclick", (event) => {
+    darkMode.style.backgroundColor = "white";
+    botWhite.forEach(element => {
+        element.style.color="black";
+    })
+
+})
+// Event 5
+document.addEventListener("keydown", event);
+function event(e){
+    botWhite.forEach(element => {
+        element.style.color= "brown"
+    })
+    
+}
+// Event 6
+const sizeImg = document.querySelectorAll(".img-content")
+document.addEventListener("wheel", scroll);
+function scroll(e){
+    sizeImg.forEach(element => {
+        element.style.transform="scale(1.2)"
+    })
+}
+
+// Event 7
+const colorHeader = document.querySelector(".logo-heading");
+colorHeader.addEventListener("auxclick", (event) => {
+colorHeader.style.color= "brown"
+event.stopPropagation();
+})
+
+//Event 8
+const stopRefresh = document.querySelector(".nav");
+stopRefresh.addEventListener("click", (stop) => {
+    stop.preventDefault;
+})
+
+//Event 9
+
+
+
+
+
+
