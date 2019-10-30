e9f32a4f3049dd7c8d2d534e85fa09bb73bc5b66
Hester Knight <hesterknight@Hesters-MacBook-Pro.local>
2019-10-30 00:17:30 +0000

    one

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+//mouseover
+
+const title = document.getElementsByTagName('title');
+
+title.addEventListenter('mouseover', event => {
+	console.log('Fun Bus');
+	console.log(event);
+
+});
+
+
+
+
+ 
