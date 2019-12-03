86fcf1cb9f2955c521fd11c7d4174b5bc362afea
Eian Carter <eian@bricleir.com>
2019-12-03 14:53:10 -0800

    5 ev complete

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 // Your code goes here - 10 unique event listeners 
 
-// #1
+
 const logoExpand = document.querySelector('.logo-heading');
 //console.log(logoExpand);
+// #1
 logoExpand.addEventListener('mouseover', () => {
     logoExpand.style.transform = 'scale(1.7)';
     logoExpand.style.transition = 'transform 0.3s';
@@ -12,17 +13,31 @@ logoExpand.addEventListener('click', () => {
     logoExpand.style.transform = 'scale(1)';
     logoExpand.style.transition = 'transform 0.3s';
 })
-
-// #3 
+ 
 const busZoom = document.querySelector('.intro img');
 //console.log(busZoom);
+// #3
 busZoom.addEventListener('wheel', () => {
     busZoom.style.transform = `scale(2)`;
     busZoom.style.transition = 'transform 4.0s'
 })
-// #4
 busZoom.addEventListener('click', () => {
     busZoom.style.transform = `scale(1)`;
     busZoom.style.transition = 'transform 4.0s'
 })
+
+// #4 
+const navFocus = document.querySelectorAll('.nav-link').forEach(item => {
+    item.addEventListener('focus', event => {
+        item.style.background = 'red';
+        item.style.color = 'white';
+    })
+// #5
+    item.addEventListener('blur', event => {
+        item.style.background = '';
+        item.style.color = 'black';
+    } )
+});
+// console.log(titleSelect);
+
 
