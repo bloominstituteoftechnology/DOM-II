c4b7a1f9bdbf5b21a33f5e0c9e4561bf573716a9
igor <igoratakhanov@gmail.com>
2019-12-03 17:45:42 -0500

    mouseover on logo done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+// Mouseover on logo makes it move
+const moveLogo = gsap.timeline()
+
+moveLogo.to(".logo-heading", { duration: 1, x: 800 })
+    .to(".logo-heading", { duration: 1, x: 0 })
+
+moveLogo.pause()
+
+document.querySelector('.logo-heading').addEventListener('mouseover', () => {
+    moveLogo.play()
+})
+
+//
