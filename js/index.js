e32c051ddf5628e3399d5cf07df0ebb58e125396
Justin Price <mr.price@Justins-iMac.local>
2019-12-03 13:20:45 -0500

    2 listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+
+
+// 1st 2 unique event listeners- "mouseenter" & "mouseleave"
+ const navlink = document.querySelectorAll("a").forEach(navlink =>{navlink.addEventListener("mouseenter", () =>{
+    navlink.style.color = "hotpink";
+    navlink.style.backgroundColor = "gray";
+    navlink.style.padding= "3%";
+ })
+    navlink.addEventListener("mouseleave", ()=>{
+        navlink.style.color = "black";
+        navlink.style.backgroundColor = "white";
+      })
+})
+
+// 2nd 2 unique event listeners
+
+// 3rd 2 unique event listeners
+
+// 4th 2 unique event listeners
+
+// 5th 2 unique event listeners
