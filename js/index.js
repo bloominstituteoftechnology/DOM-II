5431daa1604bc28b3b8848ea42bb09f630d5d523
Aaron <anderseana@tutanota.com>
2019-12-03 15:31:43 -0600

    changed text color on key press

--- js/index.js
+++ js/index.js
@@ -1,21 +1,36 @@
 // GTFO!!
 //change image size.
 const keys = document.querySelector(".intro img")
-keys.addEventListener("click", (event) => {
+keys.addEventListener("mouseenter", (event) => {
     event.target.style.transform = "scale(2)";
-    event.target.style.transistion = "transform .3s"
+    event.target.style.transistion = "transform 5s"
 })
+keys.addEventListener("mouseleave", () => {
+    keys.style.transform = "scale(1)"
+  })
 
 //change color of h2 tags to blue.
 document.querySelectorAll("h2").forEach(el => {
-    el.addEventListener("click", function() {
+    el.addEventListener("click", () => {
       el.style.color= "blue";
     })
   })
 
-//change color of p tags to purple.
+// change color of p tags to purple.
   document.querySelectorAll("p").forEach(el => {
-    el.addEventListener("dblclick", function() {
+    el.addEventListener("dblclick", () => {
       el.style.color= "purple";
     })
   })
+
+  // on keypressed, change text color to green
+  const el = document.querySelector("body")
+    el.addEventListener("keydown", () => {
+      el.style.color= "green";
+    })
+//in key release, text color changes back to black
+    el.addEventListener("keyup", () => {
+      el.style.color= "black";
+    })
+
+    
