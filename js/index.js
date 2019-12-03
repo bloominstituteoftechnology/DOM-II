d14a965df628383e435dda2208aaa367de99c99e
Jose <Jrivera6869@yahoo.com>
2019-12-03 16:31:00 -0500

    added some styles, cant get Gsap to work will try later

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 // Your code goes here
+
+//Gasp imports
+// import { gsap } from "gsap";
+// import {CustomEase} from "..package/CustomEase.js";
+
+
+const funbus = document.querySelector(`h1`);
+
+funbus.addEventListener(`mouseover`,() => {
+funbus.style.transform = "scale(1.2)";
+funbus.style.transition = "transform 0.3s"
+funbus.style.color = `red`;
+// CustomWiggle.create("funWiggle", {wiggles: 10, type: "anticipate"});
+// gsap.to(".logo-heading", {duration: 2, rotation: 30, ease: "funWiggle"});
+
+})
+funbus.addEventListener("mouseleave", () => {
+    funbus.style.transform = "scale(1)"
+    funbus.style.color = `purple`
+  })
+
+  const navlinks = document.querySelectorAll(`.nav-link`)
+
+  navlinks.addEventListener(`mouseover`,() => {
+    navlinks.style.transform = "scale(1.2)";
+    navlinks.style.transition = "transform 0.3s"
+    navlinks.style.color = `red`;
+
+  })
+
