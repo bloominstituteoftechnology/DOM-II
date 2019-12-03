6aa2d16f2150e3db480f2fa00b8055e6ac4cd778
Aaron <anderseana@tutanota.com>
2019-12-03 14:46:42 -0600

    resized header img and changed font colors

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+// GTFO!!
+//change image size.
+const keys = document.querySelector(".intro img")
+keys.addEventListener("click", (event) => {
+    event.target.style.transform = "scale(2)";
+    event.target.style.transistion = "transform .3s"
+})
+
+//change color of h2 tags to blue.
+document.querySelectorAll("h2").forEach(el => {
+    el.addEventListener("click", function() {
+      el.style.color= "blue";
+    })
+  })
+
+//change color of p tags to purple.
+  document.querySelectorAll("p").forEach(el => {
+    el.addEventListener("dblclick", function() {
+      el.style.color= "purple";
+    })
+  })
