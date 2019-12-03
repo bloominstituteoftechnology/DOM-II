4c34822ce3a4927b02fbe36d8ab25823e1e02681
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2019-12-03 12:29:04 -0800

    Set up files and added a mouseover event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+
+const images = document.querySelectorAll('img');
+
+images.forEach( item => {
+    item.addEventListener('mouseover', function () {
+        item.style.transform = 'scale(1.1)';
+        item.style.transition = "transform 0.4s";
+        console.log('moused over!');
+    });
+
+    item.addEventListener('mouseleave', function (){
+        item.style.transform = 'scale(1.0)';
+item.style.zIndex = '.1';
+    })
+
+});
