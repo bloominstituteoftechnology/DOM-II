1444cd09a5efcd3332ccee2956ae68db7bbac85b
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2019-12-03 13:16:16 -0800

    added scroll event that moves fun bus icon across header

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 
-
+//image scale
 const images = document.querySelectorAll('img');
 
 images.forEach( item => {
@@ -15,4 +15,35 @@ images.forEach( item => {
 item.style.zIndex = '.1';
     })
 
+});
+
+// SCROLL EVENT LISTENERS
+
+
+let last_known_scroll_position = 0;
+let ticking = false;
+
+function doSomething(scroll_pos) {
+  // Do something with the scroll position
+}
+
+window.addEventListener('scroll', function(e) {
+  last_known_scroll_position = window.scrollY;
+  let funBusIcon = document.querySelector('.fun-bus-icon');
+
+
+  console.log('scrolllerrrr');
+  console.log(window.outerWidth);
+  funBusIcon.style.left = `${window.scrollY}px`;
+  console.log(window.scrollY)
+
+
+//   if (!ticking) {
+//     window.requestAnimationFrame(function() {
+//       doSomething(last_known_scroll_position);
+//       ticking = false;
+//     });
+
+//     ticking = true;
+//   }
 });
