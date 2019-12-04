c1892c395e6f3a975ebbc73fd452c1da8f0bf5e7
Humayun Rashid <humayun@Humayuns-iMac.local>
2019-12-04 10:21:46 -0500

    still working on problems. Stuck on drag and drop.

--- js/index.js
+++ js/index.js
@@ -11,5 +11,51 @@ btn.forEach(item => {
     })
 })
 
+// Mouseleave -- Changes Sign Me Up button color on mouseleave 
+btn.forEach(item =>{
+    item.addEventListener('mouseleave', e =>{
+        e.target.style.background ='#17A2B8'
+    })
+})
 
+
+// Keydown -- Alert modal once you type in keyboard
+const windowEvent = document.querySelector('body');
+windowEvent.addEventListener('keydown', () =>{
+    alert('Sup! How about you press some keys?!');
+})
+
+// Wheel
+const header = document.querySelector('header');
+header.addEventListener('wheel', e =>{
+    e.target.style.background = 'blue';
+})
+
+// // Drag & Drop
+// const introH2 = document.querySelector('.intro h2');
+// introH2.addEventListener('dragover', e =>{
+//     e.preventDefault();
+//     e.target.style.alignSelf = 'center';
+// })
+
+// Load -- Loads alert message upon page load 
+window.addEventListener('load', () =>{
+    alert('Welcome to Fun Bus! Come on and ride on the Magic School Bus!');
+})
+
+// Focus -- Select element to focus on
+const a = document.querySelectorAll('nav a');
+a.forEach(item =>{
+    item.addEventListener('focus', e=>{
+        e.target.style.color = 'red';
+        e.preventDefault();
+    })
+})
+
+// Blur --  
+a.forEach(item=>{
+    item.addEventListener('blur', e=>{
+        e.target.style.color = 'darkblue';
+    })
+})
 
