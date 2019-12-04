5dce0a2688ed47409b0420dbb075ccad6a2ca859
kika <anamonteiro430@gmail.com>
2019-12-04 10:53:09 +0000

    more events

--- js/index.js
+++ js/index.js
@@ -18,12 +18,13 @@ found.addEventListener('mouseout', () => {
 //scroll
 
 const [red, green, blue] = [98, 87, 81];
-const page = document.querySelector('body');
+const page = document.querySelector('header');
 
 window.addEventListener('scroll', () => {
   const y = 0.1 + (window.scrollY || window.pageYOffset) / 908;
   const [r, g, b] = [red / y, green / y, blue / y].map(Math.round);
   page.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+  page.style.color = '#aaa';
 });
 
 //click
@@ -35,9 +36,33 @@ signUp.forEach(button => {
   });
 });
 
-// const scaleUp = document.querySelectorAll('.card-img-top').forEach(item => {
-//   item.addEventListener('mouseenter', event => {
-//    item.style.transform = "scale(1.2)";
-//   item.style.transition = 'all 0.5s';
-//   })
-// })
+//resize
+
+window.addEventListener('resize', e => {
+  const changeText = document.querySelector('h2');
+  changeText.textContent = 'TOO SMALL!';
+});
+
+//wheel
+
+const zoom = document.querySelectorAll('img');
+zoom.forEach(img => {
+  img.addEventListener(
+    'wheel',
+    e => {
+      img.style.transform = 'scale(1.1)';
+      img.style.transition = 'all 0.5s';
+    },
+    { passive: true }
+  );
+});
+
+//create input
+
+const input = document.createElement('INPUT');
+input.setAttribute('type', 'text');
+input.style.width = '50px';
+
+const newHeader = document.querySelector('nav');
+
+newHeader.append(input);
