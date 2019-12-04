9693eb39cfa1a1959cb069b7a990c35171e64a4a
Justin Price <mr.price@Justins-iMac.local>
2019-12-03 20:43:42 -0500

    10 things

--- js/index.js
+++ js/index.js
@@ -1,22 +1,69 @@
 // Your code goes here
 
 
-// 1st 2 unique event listeners- "mouseenter" & "mouseleave"
- const navlink = document.querySelectorAll("a").forEach(navlink =>{navlink.addEventListener("mouseenter", () =>{
-    navlink.style.color = "hotpink";
-    navlink.style.backgroundColor = "gray";
-    navlink.style.padding= "3%";
- })
+// Mouse unique event listeners- "mouseenter" & "mouseleave" "mouseover" & "mouseout" & "click" & "dblclick"
+
+ const navlink = document.querySelectorAll("a").forEach(navlink =>{
+    navlink.addEventListener("mouseenter", () =>{
+        navlink.style.color = "hotpink";
+        navlink.style.backgroundColor = "gray";
+        navlink.style.padding= "3%";
+    })
     navlink.addEventListener("mouseleave", ()=>{
         navlink.style.color = "black";
         navlink.style.backgroundColor = "white";
       })
 })
 
-// 2nd 2 unique event listeners
+            // ********************************
 
-// 3rd 2 unique event listeners
 
-// 4th 2 unique event listeners
+const midimgs = document.querySelectorAll(".img-content").forEach(midimgs=>{
+    midimgs.addEventListener("click", ()=>{
+        midimgs.style.transform = "scale(1.5)";
+        midimgs.style.transition = "transform 0.3s" })
 
-// 5th 2 unique event listeners
+    midimgs.addEventListener("dblclick", () => {
+        midimgs.style.transform = "scale(1)";
+        midimgs.style.transition = "transform 0.3s" })
+  })
+
+            // ********************************
+
+
+const bottomimg = document.querySelector(".content-destination img");
+    bottomimg.addEventListener("mouseover", ()=>{
+        bottomimg.style.transform = "scale(2)";
+        bottomimg.style.transition = "transform 0.3s" })
+
+    bottomimg.addEventListener("mouseout", ()=>{
+        bottomimg.style.transform = "scale(1)";
+        bottomimg.style.transition = "transform 0.3s" })
+
+            // ********************************
+
+
+// View unique event listener "resize" & "scroll"
+
+const largeimgs = document.querySelector(".intro img");
+    window.addEventListener('resize', ()=>{
+        largeimgs.style.border = "thick solid lime";
+        largeimgs.style.borderRadius = "30px";
+    })
+            // ********************************     
+          
+const backg = document.querySelector("body");
+    document.addEventListener("scroll", ()=>{
+        backg.style.backgroundColor = "lightgray";
+    })
+
+// Keyboard unique event listeners "keydown" & "keyup"
+
+const mainmsg = document.querySelector(".intro h2");
+    document.addEventListener("keydown", ()=>{
+        mainmsg.style.backgroundColor = "hotpink";})
+
+    document.addEventListener("keyup", () => {
+        mainmsg.style.backgroundColor = "white"; })
+
+
