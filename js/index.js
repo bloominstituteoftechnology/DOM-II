e50ba7ec6966871a44af4f9b7a077eaadca823d4
Humayun Rashid <humayun@Humayuns-iMac.local>
2019-12-04 09:06:53 -0500

    Working on excercies

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+
+// Mouseover -- Changes Sign Me Up button color on mouseover
+const btn = document.querySelectorAll('.btn');
+btn.forEach(item => {
+    item.addEventListener('mouseover', e =>{
+        e.target.style.background = 'black';
+        e.target.style.color = 'white';
+        e.preventDefault();
+    })
+})
+
+
+
