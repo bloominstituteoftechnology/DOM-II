63da06d0ae0a9bb912193f58e8bd2256ff40efe2
skooger <adamjskooglambda@gmail.com>
2019-12-03 17:10:09 -0800

    added all the event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 // Your code goes here
+//Nav Animations
+document.querySelectorAll('nav a').forEach(item =>{
+    item.addEventListener("mouseenter", () =>{
+        item.style.transform = "scale(1.2)";
+        item.style.transition = "transform 0.3s"
+    })
+})
+
+document.querySelectorAll('nav a').forEach(item =>{
+    item.addEventListener("mouseleave", () =>{
+        item.style.transform = "scale(1)";
+        item.style.transition = "transform 0.3s"
+    })
+})
+//stop default
+document.querySelectorAll('nav a').forEach(item =>{
+    item.addEventListener("click", (event) =>{
+        event.preventDefault();
+    })
+})
+
+
+//load events
+window.addEventListener('load', (event) => {
+    console.log('page is fully loaded');
+  });
+
+//Sign up events
+document.querySelectorAll('.btn').forEach(item =>{
+    item.addEventListener("dblclick", () =>{
+        item.style.backgroundColor = "green";
+    })
+})
+
+document.querySelector('.intro').forEach(item =>{
+    item.addEventListener("keydown", () =>{
+        item.style.display = "none";
+    })
+})
+
