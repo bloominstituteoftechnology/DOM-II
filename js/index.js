75cc71d46bab9579031bc548398737242814c39c
Dani Blackwell <bleuxgenius@gmail.com>
2019-12-04 22:31:28 -0600

    update javascript
    added eventlisteners

--- js/index.js
+++ js/index.js
@@ -2,17 +2,14 @@
 
 
 
-const navBar = document.querySelector('header');
-
+const navBar = document.querySelector('.main-navigation');
 window.addEventListener('scroll', () => {
     navBar.style.background = '#C0C0C0';
     navBar.style.opacity = '0.8';
 });
-
-window.addEventListener('load',() => {
+window.addEventListener('load', () => {
     this.alert("Great News ! You've been Matched with an Excellent Traveling Advisor , You'll be on your Way ! ")
 })
-
 document.querySelector('.container > .intro > img').addEventListener('mouseover', mouseoverEL);
 
 document.querySelector('.container > .intro > img').addEventListener('mouseleave', mouseleaveEL); 
@@ -21,18 +18,33 @@ document.querySelector('.container > .intro > h2').addEventListener("dblclick", 
 
 document.querySelector('.container > .intro > h2').addEventListener('click', clickEL);
 
-document.querySelector('.container > .content-section > .text-content').addEventListener('mouseenter', mouseEnterEL);
+document.querySelector('.container > .content-destination > img').addEventListener("mouseover", mouseoverEL2);
+
+document.querySelector('.container > .content-destination > img').addEventListener('mouseleave', mouseleaveEl2);
+
 
 // pointerOut, PointerOver, 
-// MouseDown - mouse button is clicked down , MouseUp - when you realse the mouse clicker , mouseEnter, mouseLeave
+// MouseDown - mouse button is clicked down , 
+// MouseUp - when you realse the mouse clicker , 
+// mouseEnter, mouseLeave
 
 function mouseoverEL() {
     document.querySelector('.container > .intro > img').style.boxShadow = "2px 10px 8px grey";
     console.log('boxShawdow working');
 }
 
+function mouseoverEL2() {
+    document.querySelector('.container > .content-destination > img' ).style.boxShadow = "2px 10px 8px grey";
+    console.log('boxShawdow2 working');
+}
+
 function mouseleaveEL() {
     document.querySelector('.container > .intro > img').style.boxShadow = "none";
+    console.log('boxShadow disappeared');
+}
+
+function mouseleaveEl2() {
+    document.querySelector('.container > .content-destination > img').style.boxShadow = "none";
     console.log('boxShadow disappeared');
 }
 
@@ -41,12 +53,12 @@ function doubleClickEl() {
     console.log('italic working')
 }
 
+function doubleClickEl2() {
+document.querySelector('.container > content-destination > h2').style.fontStyle = 'italic';
+console.log('italic working')
+}
+
 function clickEL() {
     document.querySelector('.container > .intro > h2').style.fontStyle ='normal';
     console.warn("normal text")
-}
-
-function mouseEnterEL() {
-    document.querySelector('.container > .content-section > h2').style.boxShadow = '2px 10px 8px grey';
-    console.log('text shadow working');
 }
