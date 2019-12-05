8d2e370e4cd2a83efc239e8de9dca23d8589a5a5
MorbidMiyako <57117884+MorbidMiyako@users.noreply.github.com>
2019-12-05 18:48:48 +0100

    finished mvp

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,28 +1,37 @@
 
+let note = ""
+
 let first = document.querySelector(".block--red")
-let firstStyle = document.querySelector(".block--red").style.backgroundColor
 let firstColor = "red"
 
 let second = document.querySelector(".block--blue")
-let secondStyle = document.querySelector(".block--red").style.backgroundColor
+let secondStyle = document.querySelector(".block--blue").style.backgroundColor
 let secondColor = "blue"
 
 let third = document.querySelector(".block--green")
-let thirdStyle = document.querySelector(".block--red").style.backgroundColor
+let thirdStyle = document.querySelector(".block--green").style.backgroundColor
 let thirdColor = "green"
 
 let fourth = document.querySelector(".block--pink")
-let fourthStyle = document.querySelector(".block--red").style.backgroundColor
+let fourthStyle = document.querySelector(".block--pink").style.backgroundColor
 let fourthColor = "pink"
 
 let fifth = document.querySelector(".block--gray")
-let fifthStyle = document.querySelector(".block--red").style.backgroundColor
+let fifthStyle = document.querySelector(".block--gray").style.backgroundColor
 let fifthColor = "gray"
 
 second.addEventListener("click", () => {
+  console.log(firstColor)
+  console.log(secondColor)
+  console.log(secondColor)
+  console.log(note)
   note = firstColor
-  firstStyle = secondColor;
+  document.querySelector(".block--red").style.backgroundColor = secondColor;
   firstColor = secondColor;
-  secondStyle = note;
+  second.style.backgroundColor = note;
   secondColor = note;
+  console.log(firstColor)
+  console.log(secondColor)
+  console.log(secondColor)
+  console.log(note)
 })
