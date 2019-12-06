d84d5c631a59095d3f8c851f375065a5e8d51e6e
Tyler Alsop <tylerjalsop@gmail.com>
2019-12-05 20:00:32 -0700

    Added project MVP's to index.js just for reference

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
-// Your code goes here
+// Using your index.js file, create 10 unique event listeners. using your creativity to make the Fun Bus site more interactive. Here are some unique events you could try to use:
+//  mouseover
+//  keydown
+//  wheel
+//  drag / drop
+//  load
+//  focus
+//  resize
+//  scroll
+//  select
+//  dblclick
+// Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
+
+//  Nest two similar events somewhere in the site and prevent the event propagation properly
+//  Stop the navigation items from refreshing the page by using preventDefault()
