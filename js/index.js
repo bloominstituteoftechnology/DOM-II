827de4fc46499283234e21e61e0b1d125e357538
Adella <scratchglory@gmail.com>
2019-12-06 19:06:31 -0500

    made changes to h2, html

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 // Your code goes here
+const navBtn = document.querySelectorAll(".nav-link");
+const introH2 = document.querySelector(".intro h2");
+const html = document.querySelector("html");
+
+// mouseenter
+introH2.addEventListener("mouseenter", event => {
+  introH2.style.fontWeight = "bold";
+});
+
+//keydown -- make an alert
+html.addEventListener("keydown", event =>
+  alert("You pressed the down key, use the wheel/fingers")
+);
+
+//wheel
+html.addEventListener("wheel", event => {
+  html.style.cursor = "all-scroll";
+});
+
+//drag / drop
+// load
+//focus
+// resize
+// scroll
+// select
+// dbclick
+
+console.log(html);
