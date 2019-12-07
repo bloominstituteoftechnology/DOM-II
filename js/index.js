b5a825efb3f929c7c1f0267c4cb2108d86fd7941
o0WhiteTigerOo <supermanrobinhood1986@gmail.com>
2019-12-06 21:40:06 -0600

    updaes1

--- js/index.js
+++ js/index.js
@@ -8,3 +8,30 @@ topImg.addEventListener('mouseleave', function(){
     topImg.style.color= 'black';
 })
 
+const wheel1 = document.querySelector('.img-content');
+wheel1.addEventListener("mouseenter", function (){
+    wheel1.style.transform = 'scale(1.5)'
+    wheel1.style.transition = 'transform 0.3s'
+    
+})
+
+wheel1.addEventListener('mouseleave', function(){
+    wheel1.style.transform = 'scale(1)'
+})
+
+
+let counter = 0;
+const buttonall = document.querySelectorAll('.btn');
+buttonall.forEach((btn) => {
+    btn.addEventListener('click', function (event){
+        counter += 1;
+        // test.innerHTML = `Click count: ${event.detail}`;
+        updateAllBtns(`Click count: ${counter}`);
+    });
+});
+
+function updateAllBtns(innerHTML) {
+    buttonall.forEach(btn => btn.innerHTML = innerHTML);
+}
+
+
