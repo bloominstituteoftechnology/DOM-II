1e1dc01d0320a47aaab0059b216ded0090bcb5e3
Tyler Alsop <tylerjalsop@gmail.com>
2019-12-06 18:11:26 -0700

    Added Logo scaling and copy event background color change

--- js/index.js
+++ js/index.js
@@ -14,12 +14,43 @@
 //  Nest two similar events somewhere in the site and prevent the event propagation properly
 //  Stop the navigation items from refreshing the page by using preventDefault()
 
-const busImg = document.getElementsByTagName("img")[0]
+
+//navLinks scale
+
+const logo = document.querySelector(".logo-heading");
+
+console.log(logo)
+
+logo.addEventListener("mouseenter", (event) => {
+    logo.style.transform = "scale(1.5)";
+    logo.style.transition = "all 0.3s";
+})
+
+logo.addEventListener("mouseleave", (event) => {
+    logo.style.transform = "scale(1.0)";
+    logo.style.transition = "all 0.3s";
+})
+
+
+
+//Main Bus Img change
+
+const busImg = document.getElementsByTagName("img")[0];
 busImg.setAttribute("id", "header-img");
 busImg.addEventListener("mouseenter", (event) => {
-    busImg.setAttribute("src", "img/beach-sunset.jpg")
-})
+    busImg.setAttribute("src", "img/beach-sunset.jpg");
+});
 busImg.addEventListener("mouseleave", (event) => {
-    busImg.setAttribute("src", "img/fun-bus.jpg")
-})
+    busImg.setAttribute("src", "img/fun-bus.jpg");
+});
+
+
+//Text Copy Event
+const textContent = document.querySelector('p');
+
+textContent.addEventListener("copy", (event) => {
+    textContent.style.backgroundColor = "#e7c9f2";
+    textContent.style.transition = "all 0.5s"
+});
+
 
