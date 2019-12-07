3275517c692ae73506259505fe1e0210a6cfc8b4
o0WhiteTigerOo <supermanrobinhood1986@gmail.com>
2019-12-06 20:05:26 -0600

    dom 2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const topImg = document.querySelector('.intro');
+topImg.addEventListener('mouseover', function(){
+    topImg.style.color = 'blue';
+})
+
+topImg.addEventListener('mouseleave', function(){
+    topImg.style.color= 'black';
+})
+
