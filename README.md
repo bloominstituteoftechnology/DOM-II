b8109655bf47dbb4ad6ef542aad43a7c5d17b4b1
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2019-12-08 21:42:21 -0800

    project updated

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 ## Task 2: Create Unique Event Listeners
 
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
+	* [x] `mouseover`
 	* [ ] `keydown`
 	* [ ] `wheel`
 	* [ ] `drag / drop`
@@ -44,6 +44,9 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 	* [ ] `scroll`
 	* [ ] `select`
 	* [ ] `dblclick`
+	* [x] `click`
+	* [X] `mouseenter`
+	* [x] `mouseleave`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,66 @@
-// Your code goes here
+// mouseover
+const mouse = document.querySelector('.nav');
+mouse.addEventListener('mouseover', (event) => {
+    event.target.style.color = 'orange';
+    setTimeout((event) => {
+        event.target.style.color = "";
+    }, '500');
+}, false);
+
+// keydown
+const keyboardKey = document.querySelectorAll('p');
+keyboardKey[1].addEventListener('keydown', (event) => {
+    event.target.style.color = 'red';
+});
+
+//focus
+const focusNav = document.querySelector('.nav a');
+focusNav.addEventListener('focus', (event) => {
+    event.target.style.color = 'red';
+})
+
+// focusNav.forEach(navItems => {
+//     navItems.addEventListener('focus', (event) => {
+//         event.target.style.background = 'red';
+//     })
+// })
+
+// dblclick
+const imagesClicked = document.querySelectorAll('content-destination');
+imagesClicked.addEventListener('dblclick', () => {
+    console.log('image is double clicked.');
+});
+
+// resize
+// const reSize = document.querySelector('body');
+// reSize.addEventListener('resize', (event) => {
+//     event.target
+// })
+
+// load
+// window.addEventListener('load', () => {
+//     console.log('Page is loaded.')
+// });
+
+window.onload = (event) => {
+    console.log('page is fully loaded');
+};
+
+document.querySelectorAll('h2').forEach((wali) => {
+    wali.addEventListener('click', () => {
+        wali.style.color = '#d6cd1a';
+    })
+})
+
+const mirage = document.querySelector('.intro img');
+mirage.addEventListener('mouseenter', () => {
+    mirage.style.transform = 'scale(1.1)';
+    mirage.style.transition = 'all 0, 0.1s';
+})
+
+
+mirage.addEventListener('mouseleave', () => {
+    mirage.style.transform = 'scale(1.0)';
+    zoomOut.style.transition = 'all 0, 0.11s';
+})
+
