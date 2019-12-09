1df5f31dbdc498e883d4d742a60db5c9bfc448e8
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2019-12-09 13:51:06 -0800

    added-some-eventListener

--- js/index.js
+++ js/index.js
@@ -1,10 +1,47 @@
 // Your code goes here
-const busImage = document.querySelector('.intro img')
-  busImage.addEventListener('mouseenter',() =>{
-  catImage.style.transform = "scale(1.2)"
-  catImage.style.transition = "all 0,3s"
+
+const busImage = document.querySelector(".intro img")
+busImage.addEventListener("mouseenter",() =>{
+  busImage.style.transform = "scale(1.2)";
+  busImage.style.transition = "all 0,30s";
 });
-  busImg.addEventListener('mouseleave', (event) => {
-  busImg.style.transform = "scale(1.0)";
+const busImg = document.querySelector(".intro img") 
+busImage.addEventListener('mouseleave', () => {
+  busImg.style.transform = "scale(0.9)";
   busImg.style.transition = "all 0.3s";
-  });
+});
+
+document.querySelectorAll('h2').forEach(el => {
+    el.addEventListener('click',function(){
+        el.style.color = "red";
+    })
+})
+ 
+
+const destinationImg = document.querySelector(".content-destination img")
+destinationImg.addEventListener("mouseenter",() =>{
+    destinationImg.style.transform = "scale(1.2)";
+    destinationImg.transition = "all 0,30s";
+});
+const destination = document.querySelector(".content-destination img") 
+destination.addEventListener('mouseleave', () => {
+    destination.style.transform = "scale(0.9)";
+    destination.style.transition = "all 0.3s";
+});
+
+
+
+
+
+
+
+
+
+document.querySelectorAll(".img-content img").forEach(images => {
+    images.addEventListener("mouseenter",function(){
+    Image.style.transform = "scale(1.2)";
+    Image.style.transition = "all 0,30s";
+
+    })
+})
+
