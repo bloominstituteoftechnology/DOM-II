9564bd45c32372c4614d206ee748e3e5d226349e
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2019-12-10 09:31:56 -0800

    added-more-event

--- README.md
+++ README.md
@@ -34,9 +34,9 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 ## Task 2: Create Unique Event Listeners
 
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
+	* [X] `mouseover`
 	* [ ] `keydown`
-	* [ ] `wheel`
+	* [X] `wheel`
 	* [ ] `drag / drop`
 	* [ ] `load`
 	* [ ] `focus`
--- js/index.js
+++ js/index.js
@@ -16,7 +16,6 @@ document.querySelectorAll('h2').forEach(el => {
         el.style.color = "red";
     })
 })
- 
 
 const destinationImg = document.querySelector(".content-destination img")
 destinationImg.addEventListener("mouseenter",() =>{
@@ -29,19 +28,29 @@ destination.addEventListener('mouseleave', () => {
     destination.style.transition = "all 0.3s";
 });
 
+let nav = document.querySelector(".nav");
+nav.addEventListener("mouseenter", function( event ) {   
+  event.target.style.color = "purple";
+
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
+nav.addEventListener("mouseover", function( event ) {   
+  event.target.style.color = '#d2f05b';
+  setTimeout(function() {
+    event.target.style.color = "";
+  }, 500);
+}, false);
 
 
-
-
-
-
-
-
-document.querySelectorAll(".img-content img").forEach(images => {
-    images.addEventListener("mouseenter",function(){
-    Image.style.transform = "scale(1.2)";
-    Image.style.transition = "all 0,30s";
-
-    })
-})
-
+function zoom(event) {
+    event.preventDefault();
+  
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    el.style.transform = `scale(${scale})`;
+  }
+  let scale = 1;
+  const el = document.querySelector('div');
+  el.onwheel = zoom;
