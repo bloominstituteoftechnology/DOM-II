6128c479b24c923282dbe50ba34f43c3dae1dcec
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2019-12-10 19:28:21 -0500

    2/10

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+// 1
+const mapImg = document.querySelector('.img-content');
+mapImg.addEventListener('mouseenter', (event) => {
+mapImg.style.transform = "scale(1.1)";
+mapImg.style.transition = "all 0.3s";
+});
+
+// 2
+mapImg.addEventListener('mouseleave', (event) => {
+    mapImg.style.transform = "scale(1.0)";
+    mapImg.style.transition = "all 0.3s";
+});
+
+
