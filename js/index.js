434c78f1c795be88fb82ac79d8189b64031cd87d
Hester Knight <hesterknight@hesters-mbp.mynet>
2020-01-05 17:37:34 +0000

    m

--- js/index.js
+++ js/index.js
@@ -19,7 +19,23 @@ title.addEventListener('mouseover', (event) => {
  
 const anykey = document.querySelector('body');
 
+// when user presses any key on website the effects occur
+
 anykey.addEventListener('keydown', (event) => {
 	alert('KeyDown!');
 	event.target.style.backgroundColor = "coral";
+})
+
+const wheelevent = document.querySelector('body');
+
+// event fires when the user rotates a wheel button on a  a mouse aka like zooming in and out! or mouse scrolls 
+//if want to change font size or box size 
+
+wheelevent.addEventListener('wheel', (event) => {
+
+	event.target.style.fontSize = "25px";
+	event.target.style.backgroundColor = "lightgreen";
+
+
+
 })
