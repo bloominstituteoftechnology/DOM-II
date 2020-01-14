328784c2ab4097040ab30350d942da86e4eec4fc
carlsachs <sachscarl@gmail.com>
2020-01-14 18:37:50 -0500

    added a few more

--- js/index.js
+++ js/index.js
@@ -21,4 +21,28 @@ p1.addEventListener("dblclick", () => {
    p1.style.color = "blue" 
 });
 
-const
+const h2 = document.querySelectorAll("h2");
+
+h2[0].addEventListener("wheel", () => {
+    h2[0].style.color = " red "
+});
+
+const bg1 = document.querySelector(".text-content");
+
+bg1.addEventListener("mousedown", () => {
+    bg1.style.visibility = "hidden"
+});
+
+const pic = document.querySelectorAll("img");
+
+pic[1].addEventListener("mouseout", () => {
+    pic[1].style.webkitTransform = "rotate(45deg)";
+});
+
+pic[2].addEventListener("mouseup", () => {
+    pic[2].style.webkitTransform = "rotate(90deg)";
+});
+
+pic[3].addEventListener("mousemove", () => {
+    pic[3].style.webkitTransform = "rotate(65deg)"
+});
