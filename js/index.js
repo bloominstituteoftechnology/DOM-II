78b92937994df68c517b95f393a09b71b61f9811
alexis villaraza <alexis.borja@gmail.com>
2020-01-14 14:10:30 -0700

    added dblclick, mouseenter, mouseclick

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
 // Your code goes here
+//mouse click
+const bgChange = document.querySelector('.main-navigation')
+bgChange.addEventListener('click', () => {
+    bgChange.style.backgroundColor = 'cyan'
+})
+
+
+// mouse enter
+// const drive = document.querySelector('.img-content');
+// drive.addEventListener('mouseenter', () => { 
+//   gsap.to('.img-content', {
+//   duration: 1,
+//   filter: "none",
+//   x: -20,
+//   y: -10,
+//   // rotate: 360,
+//   yoyo: true,
+//   repeat: 1
+// })
+  
+// })
+
+const busAnimate = document.querySelector('img')
+busAnimate.addEventListener('mouseenter', () => {
+    busAnimate.style.transform = 'scale(1.5)';
+    busAnimate.style.transition = 'transform 0.5s'
+})
+busAnimate.addEventListener('mouseleave', () => {
+    busAnimate.style.transform = 'scale(.5)'
+  }) 
+
+  //dblclick
+  document.querySelectorAll('h2').forEach(e => {
+      e.addEventListener('dblclick', () => {
+          e.style.color = 'blue';
+      })
+  })
+
+//   
+const colorful = document.querySelector('.img-content');
+colorful.addEventListener('mouseenter', () => {
+    gsap.to('.img-content', {
+        filter: "none",
+    })
+})
