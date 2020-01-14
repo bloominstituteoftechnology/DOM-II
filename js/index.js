7b1f7bef94e23d7b0d28c5b441d8d8ff40faaa2d
Zach Stack <stackz.personal@gmail.com>
2020-01-14 15:32:40 -0800

    MVP

--- js/index.js
+++ js/index.js
@@ -22,4 +22,83 @@ const scaryPic = document.querySelectorAll('.img-content img').forEach((img) => 
     })
 })
 
-3//
+//3
+const html = document.querySelector('html');
+const beeswax = document.querySelectorAll('p').forEach((p) => {
+    html.addEventListener('auxclick', () => {
+        p.textContent = "Mind your business!!!";
+    })
+})
+
+//4
+
+const ohNo = document.querySelector('.intro img');
+window.addEventListener('resize', () =>{
+    ohNo.src = 'https://i.pinimg.com/originals/9a/0a/78/9a0a780018ca1aa855e8b5a2788b0d7a.png'
+})
+
+//5
+
+const redBtn = document.querySelector('.btn1');
+const body = document.querySelector('body');
+redBtn.addEventListener('mousedown', () => {
+    body.style.backgroundColor = 'red'
+})
+redBtn.addEventListener('mouseup', () => {
+    body.style.backgroundColor = 'white'
+})
+
+//6
+
+const greenBtn = document.querySelector('.btn2');
+greenBtn.addEventListener('click', () => {
+    body.style.color = 'green'
+})
+
+//7
+
+greenBtn.addEventListener('dblclick', () => {
+    body.style.color = 'black'
+})
+
+//8
+
+const blueBtn = document.querySelector('.btn3');
+blueBtn.addEventListener('auxclick', () => {
+    body.style.backgroundColor = 'blue'
+})
+
+blueBtn.addEventListener('dblclick', () => {
+    body.style.backgroundColor = 'white'
+})
+
+//9
+
+
+const lawn = document.querySelectorAll('p').forEach((p) => {
+    p.addEventListener('mousemove', () => {
+        p.textContent = "Get off my lawn!!!";
+    })
+})
+
+//10
+
+body.addEventListener('click', () => {
+    body.style.backgroundColor = 'fuchsia'
+})
+
+const bodyText = document.querySelector('.text-content1');
+
+bodyText.addEventListener('click', () => {
+    bodyText.style.backgroundColor = 'cyan';
+    event.stopPropagation();
+    // console.log("Working?")
+})
+
+
+//prevent default
+
+const stopNav = document.querySelector('.nav-link');
+stopNav.addEventListener('click', (event) => {
+    event.preventDefault();
+})
