20b9f6b53f38583593707d895d7d27411f98efd0
eoinlynchcodes <eoin95lynch@gmail.com>
2020-01-14 13:30:18 +0000

    Finished. Now to start yesterday's project that I missed.

--- js/index.js
+++ js/index.js
@@ -2,78 +2,73 @@
 
 //mouseOver
 
+const navChange = document.getElementsByClassName("nav-link")[0];
 
-const navChange = document.getElementsByClassName('nav-link')[0];
-
-const nav = document.getElementsByTagName('nav')[0];
+const nav = document.getElementsByTagName("nav")[0];
 // console.log(nav);
 
-navChange.addEventListener('mouseover', event => {
-    console.log(event);
-    console.log('See this? It works.');
-    console.log(nav);
-    nav.style.backgroundColor = "red";
+navChange.addEventListener("mouseover", event => {
+  console.log(event);
+  console.log("See this? It works.");
+  console.log(nav);
+  nav.style.backgroundColor = "red";
 });
 
 //keyDown
-
-
-
+document.addEventListener("keydown", event => {
+  alert("You pressed a key.");
+});
 
 //wheel
 
+document.addEventListener("wheel", event => {
+  console.log("We have a wheel event");
+});
 
 //drag or drop
 
+// See draggable.js...
 
 //load
-
-
+// document.addEventListener("load", event => {
+//   console.log("The page has loaded.");
+// });
 
 //focus
-
 
 //resize - Not working
 
 // const forMobile = window.getElementsByClassName('destination');
 
-    window.addEventListener('resize', event => {
-    console.log(event);
-    console.log('forMobile works!');
-    // div.destination.backgroundColor = "red";
+window.addEventListener("resize", event => {
+  console.log(event);
+  console.log("forMobile works!");
+  // div.destination.backgroundColor = "red";
 });
-
 
 //scroll
 // const theyAreScrolling = document.getElementsByClassName('mainContainer');
 
-window.addEventListener('scroll', function(event){
-    console.log(event + 'They\'re scrolling..');
-    // alert('Click OK if you have already read everything on this page.');
+window.addEventListener("scroll", function(event) {
+  console.log(event + "They're scrolling..");
+  // alert('Click OK if you have already read everything on this page.');
 });
 
 //select
 
-const goingToAnotherPage = document.getElementsByClassName('nav-link');
-
-goingToAnotherPage.addEventListener('select', event => {
-    console.log(event);
-    console.log('Yea, she works.');
-});
-
 //dblclick
 
-const firstSignUp = document.getElementsByClassName('btn')[0];
-firstSignUp.addEventListener('dblclick', event => {
-    alert('Don\'t break your mouse! 1');
+const firstSignUp = document.getElementsByClassName("btn")[0];
+firstSignUp.addEventListener("dblclick", event => {
+  alert("Don't break your mouse! 1");
 });
 
-const secondSignUp = document.getElementsByClassName('btn')[1];
-secondSignUp.addEventListener('dblclick', event => {
-    alert('Don\'t break your mouse! 2!');
+const secondSignUp = document.getElementsByClassName("btn")[1];
+secondSignUp.addEventListener("dblclick", event => {
+  alert("Don't break your mouse! 2!");
 });
 
-const thirdSignUp = document.getElementsByClassName('btn')[2];
-thirdSignUp.addEventListener('dblclick', event => {
-    alert('Don\'t break your mouse! 3!');
+const thirdSignUp = document.getElementsByClassName("btn")[2];
+thirdSignUp.addEventListener("dblclick", event => {
+  alert("Don't break your mouse! 3!");
 });
