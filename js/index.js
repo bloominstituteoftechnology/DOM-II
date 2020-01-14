6e2d14a659c93bb7a8e74ced3a1e34df6f9102e1
Anthony Johnson <wontons713@Anthonys-MacBook-Pro.local>
2020-01-14 15:55:30 -0700

    Almost done.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,57 @@
 // Your code goes here
+
+let busDrive = document.getElementsByTagName("img")
+    busDrive[0].addEventListener('mouseenter', () => {
+        busDrive[0].style.transform = "scale(0.8)"
+    })
+
+let mapRotate = document.getElementsByTagName('img')
+    mapRotate[1].addEventListener('mousemove', () => {
+        mapRotate[1].style.transform = 'rotate(70deg)'
+    })
+
+let cityRotate = document.getElementsByTagName('img')
+    cityRotate[2].addEventListener('click', () => {
+        cityRotate[2].style.transform = 'rotate(-70deg)'
+    })
+
+let textGrow = document.querySelectorAll('h2')
+    textGrow[0].addEventListener('mouseenter', () =>{
+        textGrow[0].style.fontSize = "10rem";
+    })
+  
+let bodyColorBackground = document.querySelector('body')
+    bodyColorBackground.addEventListener('mouseenter', () => {
+        bodyColorBackground.style.backgroundColor = "blue";    
+    })
+
+let enterTextColor = document.querySelector('body')
+    enterTextColor.addEventListener('mouseenter', () => {
+        enterTextColor.style.color = "red";
+    })
+
+let exitTextColor = document.querySelector('body')
+    exitTextColor.addEventListener('mouseleave', () => {
+        exitTextColor.style.color = "green";
+    })
+
+let navColorBackground = document.querySelector('.main-navigation')
+    navColorBackground.addEventListener('mouseenter', () => {
+        navColorBackground.style.backgroundColor = '#32CD32';
+    })
+
+let flipButton = document.querySelectorAll('btn')
+    .forEach(flipButton => {
+        flipButton.addEventListener('click', () => {
+            flipButton.style.transform = 'rotate(180deg)'
+        })
+    })
+
+    console.log(flipButton);
+        
+
+    
+    
+    
+
+ 
