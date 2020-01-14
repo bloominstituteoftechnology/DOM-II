cad281edaf201d5c0984b161b33c9c97a4639a12
Ike Otu <ikeotu@msn.com>
2020-01-14 15:03:59 -0800

    Initial commit, mouseenter and mouseleave events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+// mouseenter event
+
+let missedTheBus = document.querySelector('h1');
+missedTheBus.addEventListener('mouseenter', () => {
+    missedTheBus.style.backgroundColor = 'greenYellow';
+    missedTheBus.style.color = 'white';
+})
+
+// mouseleave event
+
+missedTheBus.addEventListener('mouseleave', () => {
+    alert("You're going to miss the fun!");
+})
