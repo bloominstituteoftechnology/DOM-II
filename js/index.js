95724382e5e1f2c25b6b74d3a3d5d2761cc92455
Alicia Petgrave <alicia.petgrave@gmail.com>
2020-01-14 17:40:37 -0500

    eight event listeners

--- js/index.js
+++ js/index.js
@@ -1,23 +1,24 @@
 // Your code goes here
 
-//1.
-// const imgSize = document.querySelector("intro img")
-// imgSize.style.transform = "scale(1.2)";
+//1. canges text
+const smallBoat = document.querySelector('.text-content p');
+smallBoat.addEventListener('mouseover', () => {
+smallBoat.textContent = "Wo0o0o0o0ow this works"
+})
 
-
-//2. change logo color
+//2. changes logo color
 const funLogo = document.querySelector('.logo-heading');
 funLogo.addEventListener("click", () => {
 funLogo.style.color = "red";
 })
 
-//3. change intro color
+//3. changes intro paragraph color
 const intro = document.querySelector('.intro p');
 intro.addEventListener('mouseenter', () => {
   intro.style.color = 'blue';
 })
 
-//4. change bus image
+//4. changes bus image
 const images = document.querySelector('.intro img');
  images.addEventListener('mouseover', () => {
    images.src ="img/destination.jpg";
@@ -30,14 +31,33 @@ const images = document.querySelector('.intro img');
  })
 
  //6. changes text on scroll NOT WORKING
- const scrollText = document.querySelector('.text-content p');
- scrollText.addEventListener('scroll', () => {
-scrollText.textContent = 'Wow, this is so cool!'
+//  const scrollText = document.querySelector('.text-content p');
+//  scrollText.addEventListener('scroll', () => {
+// scrollText.textContent = 'Wow, this is so cool!'
+//  })
+
+
+ //7. changes color of buttons
+ const signUp = document.querySelectorAll(".btn").forEach(stuff => {
+stuff.addEventListener("mousedown", () => {
+stuff.style.backgroundColor = "purple"
+})
  })
 
+ //8. changes body backgrond color
+ const poop = document.querySelector('body');
+poop.addEventListener("contextmenu", () => {
+poop.style.backgroundColor = "rebeccapurple";
+})
 
- //7. resize image
- const map = document.querySelector(".btn")
-map.addEventListener("mousedown", () => {
-map.style.backgroundColor = "purple"
+
+//9. resize bottom img
+const boat = document.querySelector(".content-destination img")
+boat.addEventListener("mouseenter", () => {
+  boat.style.transform ="scale(1.2)";
+  boat.style.transition = "transform 0.3s"
 } )
+boat.addEventListener("mouseleave", function() {
+  boat.style.transform = "scale(1)"
+  boat.style.transition = "transform 0.3s"
+}) 
