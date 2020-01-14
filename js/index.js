8373f0831cf8bfe63742813a6746a37d51404bb4
carlsachs <sachscarl@gmail.com>
2020-01-14 15:45:36 -0500

    forgot to fork heres less and 2 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const logo = document.querySelector('.logo-heading');
+
+logo.addEventListener("mouseover", () => {
+    logo.style.color = "green"
+});
+
+logo.addEventListener("mouseleave", () => {
+    logo.style.color = ""
+});
