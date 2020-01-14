b896af0c824ea6bfa7d692b1d82803a8050bcc71
carlsachs <sachscarl@gmail.com>
2020-01-14 17:54:46 -0500

    two more

--- js/index.js
+++ js/index.js
@@ -8,3 +8,17 @@ logo.addEventListener("mouseover", () => {
 logo.addEventListener("mouseleave", () => {
     logo.style.color = ""
 });
+
+const h1 = document.querySelector("img");
+
+h1.addEventListener("click", () => {
+    h1.style.transform = "scale(1.5)"
+});
+
+const p1 = document.querySelector('p');
+
+p1.addEventListener("dblclick", () => {
+   p1.style.color = "blue" 
+});
+
+const
