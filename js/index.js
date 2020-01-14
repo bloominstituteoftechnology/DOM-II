a1ebfe1939a6d8e2dc8d008823b81a373b46c574
unknown <Kat2bk@gmail.com>
2020-01-14 14:42:39 -0800

    added some addEvents to the navigation bar, h1 element, and image

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-// Your code goes here
+const ImgChange = document.querySelector("header img");
+ImgChange.addEventListener("click", () => {
+  ImgChange.src = "https://i.ytimg.com/vi/n_PRLmvdLT8/maxresdefault.jpg";
+});
+
+const fontChange = document.querySelector(".logo-heading");
+fontChange.addEventListener("mouseover", () => {
+  fontChange.style.color = "orange";
+});
+fontChange.addEventListener("mouseleave", () => {
+  fontChange.style.color = "black";
+});
+
+// const containerImages = document.querySelectorAll(".intro img");
+// containerImages.forEach(item => {
+//   item.addEventListener("click", () => {
+//     containerImages.style.transform = "scale(2)";
+//   });
+// });
+
+const anchors = document.querySelectorAll("header nav a");
+anchors.forEach(function(item) {
+  item.addEventListener("mouseup", e => {
+    item.style.transform = "scale(1.5)";
+  });
+});
+anchors.forEach(function(item) {
+  item.addEventListener("mousedown", e => {
+    item.style.transform = "scale(1)";
+  });
+});
+
+
+
+
