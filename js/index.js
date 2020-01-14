3e2ce4c19e96e2dfe823d0795d5d6239968c0101
Zach Stack <stackz.personal@gmail.com>
2020-01-14 12:38:15 -0800

    2 events done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+//1
+const headingWarp = document.querySelector('.logo-heading');
+
+headingWarp.addEventListener('mouseover', () =>{
+    headingWarp.textContent = 'Bun Fus';
+    
+});
+
+headingWarp.addEventListener('mouseout', () => {
+    headingWarp.textContent = 'Fun Bus'
+})
+
+//2
+const scaryPic = document.querySelectorAll('.img-content img').forEach((img) => {
+    img.addEventListener('mouseenter', () => {
+        img.src = 'https://i.ytimg.com/vi/a5xYjc_s0kI/maxresdefault.jpg';
+    })
+    img.addEventListener('mouseleave', () => {
+        img.src = 'https://smalbanynewyork.files.wordpress.com/2018/09/gotcha.png?w=645';
+    })
+})
+
+3//
