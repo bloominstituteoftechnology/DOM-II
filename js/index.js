06ebcaeda97b4d5df48d0d52a8ef1e0596abf691
Luis PM <luisp237@gmail.com>
2020-01-15 03:37:32 -0500

    added click event to logo heading

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+let myTest = document.querySelector('.logo-heading');
+myTest.addEventListener('click', () => {
+myTest.style.color ='red';
+});
+
+
+let headerImg = document.querySelector('.intro header');
+headerImg.addEventListener('mouseover', () => {
+    headerImg.
+})
