6dd8e063e432181377a75695f7c90456d64747c5
ranccm <ranimnapier@gmail.com>
2020-01-15 13:27:55 -0800

    event listners added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,47 @@
 // Your code goes here
+const logo = document.querySelector('.logo-heading')
+const welcome = document.querySelector('.intro h2')
+const image = document.getElementsByTagName('img')
+const destinations = document.querySelectorAll('.destination p')
+
+window.addEventListener('load', () => {
+    alert('welcome');
+})
+
+window.addEventListener('resize', () => {
+    console.log('Resizing');
+});
+
+logo.addEventListener('mouseenter', () => {
+    logo.style.transform = 'scale(1.5)';
+})
+
+logo.addEventListener('mouseleave', () => {
+    logo.style.transform = 'scale(1)';
+})
+
+logo.addEventListener('dblclick', (event) =>
+    logo.textContent = 'Not So Fun Bus'
+)
+
+welcome.addEventListener('click', (event) => {
+    welcome.style.textAlign = 'center';
+})
+
+document.addEventListener('keydown', (event) => {
+image[0].style.transform = 'rotate(180deg)';
+})
+
+document.addEventListener('keyup', (event) => {
+    image[0].style.transform = 'rotate(360deg)';
+})
+
+destinations.forEach(destination => {
+    destination.addEventListener('wheel', (event) => {
+        destination.style.fontSize = '2.2rem';
+    })
+})
+
+document.addEventListener('scroll', (event) => {
+        image[3].style.transform = 'rotate(180deg)';
+})
