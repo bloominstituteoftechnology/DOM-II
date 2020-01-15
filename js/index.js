a9f3fc7e898fb2444b420f3f8c474471f3b2d6c3
Luis PM <luisp237@gmail.com>
2020-01-15 04:40:07 -0500

    added dblclickevent and resize event

--- js/index.js
+++ js/index.js
@@ -7,14 +7,28 @@ myTest.addEventListener('click', () => {
     myTest.style.color ='red';
 });
 
-//mouse over event listener
+//mouse enter event listener
 let headerImg = document.querySelector('.intro img');
 headerImg.addEventListener('mouseenter', () => {
     // headerImg.style.borderRadius = '50%';
     headerImg.src = './img/evil-tele.jpg';
 });
 
+//mouse leave event listener
 headerImg.addEventListener('mouseleave', () => {
     // headerImg.style.borderRadius = '50%';
     headerImg.src = './img/fun-bus.jpg';
 });
+
+//dblclick evenListener
+let creepyImg1 = document.querySelector('.img-content img')
+creepyImg1.addEventListener('dblclick', () => {
+    creepyImg1.src = './img/scary1.jpg';
+});
+
+
+//resize window event listener
+window.addEventListener("resize", () => {
+    document.querySelector('html').style.backgroundColor = 'red';
+  });
+  
