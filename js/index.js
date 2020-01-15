7f914d8f3deac43612623c2a4e9e2bfe9acd9202
mastaryan <sizemore.ryan@gmail.com>
2020-01-15 14:27:17 -0500

    Mouse Over

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// Mouse Over
+const nav = document.querySelector(".nav");
+nav.addEventListener("mouseover", e => {
+  e.target.style.color = "lightgreen";
+});
+
+// Key Down
