391f25e6bfd67f4416f05ded1954bc3abe7d9856
Freelance Development <49110640+KejdaCelaj@users.noreply.github.com>
2020-01-15 06:08:54 -0500

    added explanatory comments

--- js/index.js
+++ js/index.js
@@ -1,35 +1,95 @@
 // Your code goes here
 
 // * [ ] `mouseover`
-const navigation = document.querySelectorAll("nav a");
+// mouse over the nav links
+const navigation = document.querySelectorAll(".nav-link");
 navigation.forEach(el => {
     el.addEventListener('mouseover', () => {
+        event.preventDefault();
         el.style.color = 'green';
     })
 })
 
+
 // * [ ] `mouseout`
+// mouse out of the nav links
 navigation.forEach(el => {
     el.addEventListener('mouseout', (event) => {
         el.style.color = 'black';
-        event.preventDefault();
     })
 })
 
-// * [ ] `keydown`
-document.addEventListener('keydown', () => {
-    document.querySelector('.main-navigation').style.background ='#FFE4B5';
-    document.querySelector('body').style.background = '#FFFFE0';
+
+// Stop Propagation
+//changing the background color of the intro class section 
+const intro = document.querySelector('.intro');
+intro.addEventListener('mouseover', () => {
+    intro.style.background = '#FAFAD2';
+})
+intro.addEventListener('mouseout', () => {
+    intro.style.background = '#ffffff';
 })
 
+// then using stoppropogation to cancel the section background change 
+// and zoom out only the image of .intro class when the image is moused over
+const prop = document.querySelector('.intro img');
+prop.addEventListener('mouseover', () => {
+    event.stopPropagation();
+    prop.style.transform = 'scale(0.9)'
+})
+
+prop.addEventListener('mouseout', () => {
+    event.stopPropagation();
+    prop.style.transform = 'scale(1)'
+})
+
+// changing the other sections
+const intro2 = document.querySelector('.content-destination');
+intro2.addEventListener('mouseover', () => {
+    intro2.style.background = '#FAFAD2';
+})
+intro2.addEventListener('mouseout', () => {
+    intro2.style.background = '#ffffff';
+})
+
+const intro3 = document.querySelector('.content-section');
+intro3.addEventListener('mouseover', () => {
+    intro3.style.background = '#FAFAD2';
+})
+intro3.addEventListener('mouseout', () => {
+    intro3.style.background = '#ffffff';
+})
+
+const intro4 = document.querySelector('.inverse-content');
+intro4.addEventListener('mouseover', () => {
+    intro4.style.background = '#FAFAD2';
+})
+intro4.addEventListener('mouseout', () => {
+    intro4.style.background = '#ffffff';
+})
+
+
+// * [ ] `keydown`
+// on key down the backround of the page changes
+document.addEventListener('keydown', (event) => {
+    document.querySelector('.main-navigation').style.background ='#FFE4B5';
+    document.querySelector('body').style.background = '#FFFFE0';
+    event.stopPropagation();
+})
+
+
 // * [ ] `keyup`
+// reset the background of the page on key up
 const headerColor = document.querySelector('.main-navigation');
-// document.addEventListener('keyup', (event) => {
-//     headerColor.style.background ='#ffffff';
-//     document.querySelector('body').style.background = '#ffffff';
-// })
+document.addEventListener('keyup', (event) => {
+    headerColor.style.background ='#ffffff';
+    document.querySelector('body').style.background = '#ffffff';
+})
+
 
 // * [ ] `wheel`
+// when wheeled over the images on the content-section class
+// the images will zoom out
 const image = document.querySelector('.img-content');
 const image2 = document.querySelector('.img-fluid');
 image.addEventListener('wheel', () => {
@@ -41,16 +101,10 @@ image2.addEventListener('wheel', () => {
     
 });
 
+
 // * [ ] `click`
+// button will change color and background
 const button = document.querySelectorAll('.btn');
-// for(var btn of button){
-//     this.btn.addEventListener('click', (event) => {
-//         // button.style.fontSize ="1.5rem";
-//         this.btn.style.color = 'red'; 
-//         this.btn.style.background = 'gray';
-    
-//     });
-// }
 button[0].addEventListener('click', (event) => {
     // button.style.fontSize ="1.5rem";
     button[0].style.color = 'red'; 
@@ -70,57 +124,56 @@ button[2].addEventListener('click', (event) => {
 
 });
 
+
 // * [ ] `load`
+//console will print a message when page is fully loaded
 window.addEventListener('load', (event) => {
     console.log('Fully loaded!');
-  });
+});
+
 
 // * [ ] `scroll`
+//the navigation bar will change the background
 window.addEventListener('scroll', () => {
     headerColor.style.background = "#FFFFE0";
 })
 
+
 // * [ ] `dblclick`
+//the titles and paragraphs on .destionaion class will change color
 const content = document.querySelectorAll(".destination h4");
 document.addEventListener("dblclick", () => {
     for(var cont of content){
         cont.style.color = "orange";
 
-    }
-    
-  })
+    };
+})
 
-  const content2 = document.querySelectorAll(".destination p");
+const content2 = document.querySelectorAll(".destination p");
 document.addEventListener("dblclick", () => {
     for(var cont of content2){
         cont.style.color = "brown";
 
-    }
-    
-  })
-
-  // * [ ] `mouseenter`
-var doc = document.querySelectorAll(".destination p");
-doc[0].addEventListener('mouseenter', () => {
-    console.log("uh oh, it entered");
+    };
 })
 
-content2[0].addEventListener('mouseenter', (event) => {
-    content2[0].style.transform = "scale(1.1)";
-    event.stopPropagation();
+
+// * [ ] `mouseenter`
+// paragraph's font size will change
+content2[0].addEventListener('mouseenter', () => {
+    content2[0].style.transform = "scale(0.9)";
 })
-content2[1].addEventListener('mouseenter', (event) => {
-    content2[1].style.transform = "scale(1.1)";
-    event.stopPropagation();
+content2[1].addEventListener('mouseenter', () => {
+    content2[1].style.transform = "scale(0.9)";
 })
-content2[2].addEventListener('mouseenter', (event) => {
-    content2[2].style.transform = "scale(1.1)";
-    event.stopPropagation();
+content2[2].addEventListener('mouseenter', () => {
+    content2[2].style.transform = "scale(0.9)";
 })
+
 
 // * [ ] `mouseout`
-
-content2[0].addEventListener('mouseout', (event) => {
+// paragraph's font size will reset
+content2[0].addEventListener('mouseout', () => {
     content2[0].style.transform = "scale(1)";
 })
 content2[1].addEventListener('mouseout', () => {
