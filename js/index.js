109dd269b136d26861f2e25e2d3f5cffa72643a7
Luis PM <luisp237@gmail.com>
2020-01-15 04:55:20 -0500

    changed mouseenter/mouseleave to affect different property. added scroll event

--- js/index.js
+++ js/index.js
@@ -9,26 +9,32 @@ myTest.addEventListener('click', () => {
 
 //mouse enter event listener
 let headerImg = document.querySelector('.intro img');
-headerImg.addEventListener('mouseenter', () => {
+window.addEventListener('scroll', () => {
     // headerImg.style.borderRadius = '50%';
     headerImg.src = './img/evil-tele.jpg';
 });
 
-//mouse leave event listener
-headerImg.addEventListener('mouseleave', () => {
-    // headerImg.style.borderRadius = '50%';
-    headerImg.src = './img/fun-bus.jpg';
-});
+
 
 //dblclick evenListener
-let creepyImg1 = document.querySelector('.img-content img')
+let creepyImg1 = document.querySelector('.img-content img');
 creepyImg1.addEventListener('dblclick', () => {
     creepyImg1.src = './img/scary1.jpg';
 });
 
+let creepyImg2 = document.querySelector('.inverse-content div img');
+creepyImg2.addEventListener("mouseenter", () => {
+    creepyImg2.src = './img/scary2.jpg';
+  });
 
-//resize window event listener
+//mouse leave event listener
+creepyImg2.addEventListener('mouseleave', () => {
+    // headerImg.style.borderRadius = '50%';
+    creepyImg2.src = './img/fun.jpg';
+});  
+
+
+//resize window event scroll
 window.addEventListener("resize", () => {
     document.querySelector('html').style.backgroundColor = 'red';
   });
-  
