02aff1c96146e70d69542b4eae081ddf8d0c1c71
Donna Ballew <ballewdd@gmail.com>
2020-01-17 15:39:19 -0800

    added fun-bus-city.jpg

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+// MOUSEOVER
+
+// KEYDOWN
+
+// WHEEL
+
+// DRAG / DROP
+
+// LOAD
+
+// FOCUS
+
+// RESIZE
+
+// RESIZE
+
+// SCROLL
+
+// SELECT
+
+//DBLCLICK
+const changeImage = document.querySelector(".intro img");
+console.log(changeImage);
+
+changeImage.addEventListener("dblclick", () => {
+  changeImage.src = "img/volkswagen.jpg";
+  // alt="white and teal volkswagen van die cast model"
+});
