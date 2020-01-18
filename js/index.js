92d0d62ef6f8364dcd54b8a2772ba59c02dd0878
Anthony <chitowncode@gmail.com>
2020-01-17 23:18:55 -0600

    I have added a mouseover event, mouseleave event and a double click event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+const busImage = document.querySelector('.intro img');
+
+busImage.addEventListener('mouseenter', () => {
+    busImage.style.transform = "scale(1.2)";
+    busImage.style.transition = "all 0.3s";
+})
+
+busImage.addEventListener('mouseleave', () => {
+    busImage.style.transform = "scale(1)"
+})
+
+const changeImage = document.querySelector('.intro img');
+changeImage.addEventListener('dblclick', () => {
+    changeImage.src = "./img/bus-fun.jpg";
+})
