2ac86359e573d345393529cba6fb6f134bc5cec6
thomas <t_huynh927@yahoo.com>
2020-01-17 21:02:58 -0800

    first commit, still working on MVP

--- README.md
+++ README.md
@@ -34,12 +34,12 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 ## Task 2: Create Unique Event Listeners
 
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
-	* [ ] `keydown`
-	* [ ] `wheel`
-	* [ ] `drag / drop`
-	* [ ] `load`
-	* [ ] `focus`
+	* [x] `mouseover`
+	* [x] `keydown`
+	* [x] `wheel`
+	* [x] `drag / drop`
+	* [x] `load`
+	* [x] `focus`
 	* [ ] `resize`
 	* [ ] `scroll`
 	* [ ] `select`
--- js/index.js
+++ js/index.js
@@ -1,1 +1,90 @@
 // Your code goes here
+
+
+const colors = ["pink", "green", "orange", "blue", "yellow", "purple", "teal", "dodgerblue", "cyan"]
+
+const navbar = Array.from(document.getElementsByClassName("nav-link"))
+
+const randomColor = () => {
+    return colors[Math.floor(Math.random() * colors.length)]
+}
+navbar.forEach((nav) => {
+    nav.addEventListener('mouseover', () => {
+        nav.style.color = randomColor()
+    })
+})
+
+document.addEventListener("keydown", (e) => {
+    if (e.code === 'KeyH') {
+        navbar[0].style.color = randomColor()
+    }
+
+    if (e.code === 'KeyA') {
+        navbar[1].style.color = randomColor()
+    }
+
+    if (e.code === 'KeyB') {
+        navbar[2].style.color = randomColor()
+    }
+
+    if (e.code === 'KeyC') {
+        navbar[3].style.color = randomColor()
+    }
+})
+
+const busImg = document.getElementsByTagName("img")[0]
+
+let scale = 1
+
+busImg.addEventListener("wheel", (e) => {
+    e.preventDefault()
+    
+    scale += event.deltaY * -0.001
+
+    e.target.style.transform = `scale(${scale})`
+})
+
+busImg.addEventListener("ondragover", (e) => {
+    e.preventDefault()
+})
+
+busImg.addEventListener("ondrop", (e) => {
+    e.preventDefault()
+    const data = e.dataTransfer.getData("h2")
+    console.log(data)
+    e.target.textContent = data
+})
+
+const introH2 = document.getElementById("h2-drag")
+
+introH2.draggable = "true"
+
+introH2.addEventListener("ondragstart", (e) => {
+    console.log(e)
+    e.dataTransfer.setData("h2", e.target.textContent)
+})
+
+const body = document.getElementsByTagName("body")[0]
+
+window.addEventListener("load", () => {
+    // let loading = document.createElement("h1")
+    // loading.textContent = "Loading..."
+    // document.getElementsByTagName("body")[0] = 
+    body.style.opacity = 0.5
+    setTimeout(() => {
+        
+        body.style.opacity = 1
+    }, 1000)
+    
+})
+
+Array.from(document.getElementsByClassName("destination")).forEach((dest) => {
+    dest.children[0].addEventListener("click", () => {
+        console.log(dest.children[2])
+        dest.children[2].focus()
+    })
+})
+
+window.addEventListener("resize", () => {
+    console.log(window.innerHeight, window.innerWidth)
+})
