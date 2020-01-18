652b8185323716cadb8b0b6ae57ef7d4b08605a6
whoIsJosh_ <joshua.c.luscombe@gmail.com>
2020-01-17 20:33:28 -0800

    Added some goofy event callbacks

--- js/index.js
+++ js/index.js
@@ -46,16 +46,47 @@ let themeToggle = event => {
 
 htmlBody.addEventListener("dblclick", themeToggle);
 
-let contentPickContainer = document.querySelector(".content-pick");
+let contentPickContainer = document.querySelector(".container.home");
+let mainButtonsContainer = document.createElement("div");
+mainButtonsContainer.classList.add("content-pick");
+mainButtonsContainer.style.flexDirection = "column";
+mainButtonsContainer.style.align = "center";
+
+contentPickContainer.appendChild(mainButtonsContainer);
 
 let addButton = event => {
   let newButton = document.createElement("div");
   let newButtonContainer = document.createElement("div");
   newButtonContainer.classList.add("destination");
   newButton.classList.add("btn");
-  newButton.textContent = "POOR DESIGN CHOICE";
+  newButton.textContent = "ENDLESS BUTTONS!";
   newButtonContainer.appendChild(newButton);
-  contentPickContainer.appendChild(newButtonContainer);
+  mainButtonsContainer.appendChild(newButtonContainer);
 };
 
 window.addEventListener("scroll", addButton);
+
+let middleImgContainer = document.querySelector(
+  ".content-section .img-content"
+);
+let middleImg = document.querySelector(".content-section img");
+let imageContentContainer = document.querySelector(".content-section");
+let moveImage = event => {
+  let clonedImgContainer = middleImgContainer.cloneNode(true);
+  console.log(clonedImgContainer);
+  imageContentContainer.appendChild(middleImg);
+};
+
+middleImg.addEventListener("drag", moveImage);
+
+let topParagraphContainer = document.querySelector(".intro");
+let topParagraph = document.querySelector(".intro p");
+let starTrekString =
+  "I can't. As much as I care about you, my first duty is to the ship. Our neural pathways have become accustomed to your sensory input patterns. The look in your eyes, I recognize it. You used to have it for me. Computer, lights up! Yesterday I did not know how to eat gagh. Maybe we better talk out here; the observation lounge has turned into a swamp. I've had twelve years to think about it. And if I had it to do over again, I would have grabbed the phaser and pointed it at you instead of them. In all trust, there is the possibility for betrayal. Damage report! What? We're not at all alike! We know you're dealing in stolen ore. But I wanna talk about the assassination attempt on Lieutenant Worf. Fate protects fools, little children and ships named Enterprise. When has justice ever been as simple as a rule book?";
+
+let changeText = () => {
+  console.log("we triggered a select event");
+  topParagraph.textContent = starTrekString;
+};
+
+topParagraph.addEventListener("auxclick", changeText);
