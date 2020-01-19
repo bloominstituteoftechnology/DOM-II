4d3fb6aff7fdc911bb4335435e91d833aa98e1f2
badhabits287 <56571933+badhabits287@users.noreply.github.com>
2020-01-19 12:17:36 -0500

    playing with functions -if - else

--- js/index.js
+++ js/index.js
@@ -1,4 +1,18 @@
 
+
+
+
+function myFunction(x) {
+  if (x.matches) { // If media query matches
+    document.body.style.backgroundColor = "yellow";
+  } else {
+   document.body.style.backgroundColor = "pink";
+  }
+}
+
+var x = window.matchMedia("(max-width: 700px)")
+myFunction(x) // Call listener function at run time
+x.addListener(myFunction) // Attach listener function on state changes
 //logo items
 
 const title = document.querySelector("body > header > div > h1")
@@ -104,17 +118,7 @@ busImg.style.margin = '18px'
 
 //Footer 
 
-function myFunction(x) {
-  if (x.matches) { // If media query matches
-    document.body.style.backgroundColor = "yellow";
-  } else {
-   document.body.style.backgroundColor = "pink";
-  }
-}
 
-var x = window.matchMedia("(max-width: 700px)")
-myFunction(x) // Call listener function at run time
-x.addListener(myFunction) // Attach listener function on state changes
 
 
 
