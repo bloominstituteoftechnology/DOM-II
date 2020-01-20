509d1889bc10dcf8418d684d21072dd31eb87481
DrewCDoll <drwcdoll@gmail.com>
2020-01-19 21:24:25 -0500

    initial commit with event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-// Your code goes here
+window.onload = function () {
+
+    const navText = document.querySelector('nav');
+    navText.onmouseenter = () => {
+        navText.style.backgroundColor = 'pink';
+        const navTextLinks = navText.querySelectorAll('a');
+        navTextLinks.forEach(item => item.style.color = 'white')};
+    navText.onmouseleave = () => {
+        navText.style.backgroundColor = 'white';
+        const navTextLinks = navText.querySelectorAll('a');
+        navTextLinks.forEach(item => item.style.color = 'black')};
+        ;
+
+    const firstLogo = document.querySelector('.logo-heading');
+    firstLogo.addEventListener('click', () => firstLogo.style.transform = 'rotateY(60deg)'); 
+    firstLogo.addEventListener('dblClick', () => firstLoro.style.transform = 'initial');
+    
+    
+    
+}
