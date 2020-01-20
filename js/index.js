cb22cfc089c9d3d8ed7f6bd505ddf8487030ec61
Mike Raysor <michaelraysor1@gmail.com>
2020-01-20 09:45:06 -0500

    updated styles in JS from original. code doesn't seem to work though. I have no bugs in my JS code applied, but there are bugs in the less footer.less and home-page.less files. I checked to see if the less compiler was active, and yes it is. I originally had a merging conflict that I've since resloved.

--- js/index.js
+++ js/index.js
@@ -1,2 +1,41 @@
 // Your code goes here
 
+unBusHeading = document.querySelector('.logo-heading');
+navBar = document.querySelector('.nav');
+navItems = document.querySelectorAll('.nav-link');
+headerH2 = document.querySelector('.intro h2');
+allParagraphs = document.querySelectorAll('p');
+
+// Receive a "Don't Try It" note.
+funBusHeading.addEventListener('click', (event) => {
+   alert('Dont Try it!')
+});
+
+funBusHeading.addEventListener('click', (event) => { 
+event.target.style.backgroundColor = 'green'; 
+
+});
+
+NavBar.addEventListener('click', (event) => {
+    alert('Almost There!')
+});
+
+
+//  Changes the color of each nav item at dblclick
+navItems.forEach(node => {
+    node.addEventListener('dblclick', function (event) {
+        // event.stopPropagation();
+        event.target.style.color = 'red';
+    })
+})
+
+
+headerH2.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'green';
+    event.target.style.borderColor = 'yellow';
+});
+
+//  alert when an image is copied
+allImages.addEventListener('mouseleave', () => {
+    allImages.style.transform = "scale(2)"
+})
