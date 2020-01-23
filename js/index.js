ce48bed8b321ecaffbb8242bcef372fad4b7e444
Spencer_Vail <vail.spencer@gmail.com>
2020-01-22 18:32:03 -0700

    mvp completed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,5 @@
 // Your code goes here
-
-//Unique Event Listeners
+ //Unique Event Listeners
   // #1
   let img = document.querySelector("header img");
   img.addEventListener("mouseenter", () => {
@@ -18,52 +17,44 @@ newImg.addEventListener("click", event => {
     newImg.src = "img/treasure map.jpg";
     event.stopPropagation();
 });
-
 //#4
 newImg.addEventListener("dblclick", event => {
     newImg.src = "img/adventure.jpg";
     event.stopPropagation();
 });
-
-
 //#5
-
 const dontCopy = document.querySelectorAll("p");
-
 dontCopy.forEach(para => {
   para.addEventListener("copy", () => {
     alert(`Woah! Did you make that? I don't think so. This is mine. You wouldn't steal a car would you?`);
   });
 });
-
 //#6
-
 window.addEventListener('load', ()=>{
     alert("You have arrived at the Fun Bus Travel Agency page! Have fun!")
 })
-
 //#7
-
 let theTag = document.querySelector(".nav-link");
 theTag.addEventListener('blur', (el)=>{
 theTag.style.backgroundColor = "black";
 })
-
 //#8
-
 const optionOne = document.querySelector('.container.home .content-pick .destination .btn');
-
 optionOne.addEventListener('click', event => {
     optionOne.style.backgroundColor = 'red'
 });
-
-
 //#9
-
-let backgroundClrChng = document.querySelector(".main-navigation");
+let backgroundColorChange = document.querySelector(".main-navigation");
 window.addEventListener("scroll", () => {
-    backgroundClrChng.style.backgroundColor = "orange"
+    backgroundColorChange.style.backgroundColor = "orange"
 });
-
-
-
+//#10
+let clickButton = document.querySelector(".destination. .btn");
+footerEvent.addEventListener('focus', (el)=>{
+  event.target.style.background= "red";
+})
+// Prevent Default
+const noRefresh = document.querySelector('.nav-link');
+noRefresh.addEventListener('click', (event) => {
+    event.preventDefault();
+})
