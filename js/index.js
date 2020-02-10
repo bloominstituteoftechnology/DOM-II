6984734cfc3601cd4c3e608fea9658a1c8f156ec
yirano <yirano@protonmail.ch>
2020-02-10 10:29:47 -0500

    Add fun logo animation event handler - bounce main image on double click

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+let logoHeading = document.querySelector(".logo-heading");
+logoHeading.addEventListener("mouseover", () => {
+  logoHeading.classList.add("funLogo");
+});
+logoHeading.addEventListener("mouseleave", () => {
+  logoHeading.classList.remove("funLogo");
+});
+
+let mainImg = document.querySelector(".intro img");
+mainImg.addEventListener("dblclick", () => {
+  mainImg.classList.toggle("funImg");
+});
