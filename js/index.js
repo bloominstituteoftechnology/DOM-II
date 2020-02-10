cfb6edbfb018f9cd3a2864a35054c4020be84fba
yirano <yirano@protonmail.ch>
2020-02-10 12:23:42 -0500

    Add flipping title on click event and buttons displaying diff message on click

--- js/index.js
+++ js/index.js
@@ -11,3 +11,21 @@ let mainImg = document.querySelector(".intro img");
 mainImg.addEventListener("dblclick", () => {
   mainImg.classList.toggle("funImg");
 });
+
+let funBusTitle = document.querySelector(".intro h2");
+funBusTitle.addEventListener("click", () => {
+  funBusTitle.classList.toggle("runawayText");
+});
+
+let btns = document.querySelectorAll("button");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", e => {
+    if (e.target === btns[0]) {
+      btns[0].textContent = "WE DON'T WANT YOU";
+    } else if (e.target === btns[1]) {
+      btns[1].textContent = "WHAT DO YOU WANT?";
+    } else {
+      btns[2].textContent = "I'm just kidding, come back.";
+    }
+  });
+}
