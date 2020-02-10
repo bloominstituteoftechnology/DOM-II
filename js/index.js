fdcb6619ddab13f20a20442a1f258aebcd3851c0
yirano <yirano@protonmail.ch>
2020-02-10 15:16:48 -0500

    Falling links

--- js/index.js
+++ js/index.js
@@ -1,43 +1,60 @@
 // Your code goes here
-let logoHeading = document.querySelector(".logo-heading");
-logoHeading.addEventListener("mouseover", () => {
-  logoHeading.classList.add("funLogo");
+let logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseover', () => {
+	logoHeading.classList.add('funLogo');
 });
-logoHeading.addEventListener("mouseleave", () => {
-  logoHeading.classList.remove("funLogo");
-});
-
-let mainImg = document.querySelector(".intro img");
-mainImg.addEventListener("dblclick", () => {
-  mainImg.classList.toggle("funImg");
+logoHeading.addEventListener('mouseleave', () => {
+	logoHeading.classList.remove('funLogo');
 });
 
-let funBusTitle = document.querySelector(".intro h2");
-funBusTitle.addEventListener("click", () => {
-  funBusTitle.classList.toggle("runawayText");
+let mainImg = document.querySelector('.intro img');
+mainImg.addEventListener('dblclick', () => {
+	mainImg.classList.toggle('funImg');
 });
 
-let btns = document.querySelectorAll("button");
+let funBusTitle = document.querySelector('.intro h2');
+funBusTitle.addEventListener('click', () => {
+	funBusTitle.classList.toggle('runawayText');
+});
+
+let btns = document.querySelectorAll('button');
 for (let i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", e => {
-    if (e.target === btns[0]) {
-      if (btns[0].textContent == "Sign Me Up!") {
-        btns[0].textContent = "WE DON'T WANT YOU";
-      } else {
-        btns[0].textContent = "Sign Me Up!";
-      }
-    } else if (e.target === btns[1]) {
-      if (btns[1].textContent == "Sign Me Up!") {
-        btns[1].textContent = "WHAT DO YOU WANT?";
-      } else {
-        btns[1].textContent = "Sign Me Up!";
-      }
-    } else {
-      if (btns[2].textContent == "Sign Me Up!") {
-        btns[2].textContent = "I'm just kidding, come back.";
-      } else {
-        btns[2].textContent = "Sign Me Up!";
-      }
-    }
-  });
+	btns[i].addEventListener('click', (e) => {
+		if (e.target === btns[0]) {
+			if (btns[0].textContent == 'Sign Me Up!') {
+				btns[0].textContent = "WE DON'T WANT YOU";
+			} else {
+				btns[0].textContent = 'Sign Me Up!';
+			}
+		} else if (e.target === btns[1]) {
+			if (btns[1].textContent == 'Sign Me Up!') {
+				btns[1].textContent = 'WHAT DO YOU WANT?';
+			} else {
+				btns[1].textContent = 'Sign Me Up!';
+			}
+		} else {
+			if (btns[2].textContent == 'Sign Me Up!') {
+				btns[2].textContent = "I'm just kidding, come back.";
+			} else {
+				btns[2].textContent = 'Sign Me Up!';
+			}
+		}
+	});
 }
+
+let fallingLinks = document.querySelectorAll('.nav-link');
+console.log(fallingLinks);
+let nav = document.querySelector('nav');
+
+nav.addEventListener('mouseenter', () => {
+	for (let i = 0; i < fallingLinks.length; i++) {
+		fallingLinks[i].style = `position: relative; animation: fallingLinks ${Math.floor(Math.random() * 1200 - 600) +
+			600}ms forwards`;
+	}
+});
+nav.addEventListener('mouseleave', () => {
+	for (let i = 0; i < fallingLinks.length; i++) {
+		fallingLinks[i].style = `position: relative; animation: risingLinks ${Math.floor(Math.random() * 1200 - 600) +
+			600}ms forwards`;
+	}
+});
