c0a082e533b69a918a685dd4bdb858805b8ccc62
vanesa-tamu <vahe5495@tamu.edu>
2020-02-12 14:31:51 -0600

    changed img when clicked

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+
+//changing background image!
+const adventureAwaitsImg = document.querySelector('.img-fluid')
+console.log(adventureAwaitsImg)
+adventureAwaitsImg.addEventListener('click', e => {
+    adventureAwaitsImg.src="https://static.scientificamerican.com/sciam/cache/file/BCC3BD1E-5DC0-4843-A841706AE575C694_source.jpg?w=590&h=800&39BBF62E-5F96-4C6A-A59590CCF416DA11"
+})
