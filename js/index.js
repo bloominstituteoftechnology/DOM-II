ce23e9cc5c44311c3a297aa8c5697a55b6e04536
John Oblenda <joandong2@gmail.com>
2020-02-13 11:18:46 -0500

    added event listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+document.getElementById("nav").addEventListener("mouseover", function(e) {   
+    e.target.style.color = "red";
+
+    setTimeout(function() {
+        e.target.style.color = "";
+    }, 1000);
+}, false);
+
+
+document.addEventListener('keydown', (e) => {
+    document.getElementById("keyDownP").textContent += ` ${e.code}`;
+    if(e.code === 'Backspace') {
+        document.getElementById("keyDownP").textContent = "";
+    }
+});
+
+document.getElementById("featImg").addEventListener('dblclick', (e) => {
+    e.target.classList.toggle('transform');
+});
