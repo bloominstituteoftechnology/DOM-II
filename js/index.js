395e4045bb2df30f2a5413d2465e626edf3b00c6
John Oblenda <joandong2@gmail.com>
2020-02-14 14:41:11 -0500

    MVP done

--- js/index.js
+++ js/index.js
@@ -1,4 +1,15 @@
 // Your code goes here
+// window.addEventListener('resize', () => {
+//     alert('STOPPPPPP!!!');
+// });
+
+window.addEventListener('load', () => {
+    setTimeout(function() {
+        document.querySelector('#loading').style.display = 'none';
+        document.querySelector('.home').style.display = 'inherit';
+    }, 3000);
+})
+
 document.addEventListener('scroll', () => {
     const bgColors = ['red', 'orange', 'blue', 'violet', 'black'];
     document.getElementById("main-navigation").style.backgroundColor = (this.scrollY === 0) ? 'white' : bgColors[Math.floor(Math.random() * bgColors.length) + 1];
@@ -25,14 +36,39 @@ document.querySelectorAll('img').forEach((image) => {
     });
 });
 
-document.querySelectorAll('h2').forEach((h2) => {
-    h2.addEventListener('click', (e) => {
+document.querySelector('.content-pick').addEventListener('click', (e) => {
+    e.target.style.background = 'green';
+});
+
+document.querySelectorAll('.destination').forEach((div) => {
+    div.addEventListener('click', (e) => {
         (e.target.style.fontWeight == 'bold') ? 
             e.target.style.fontWeight = 'normal' : 
             e.target.style.fontWeight = 'bold';
+            e.stopPropagation();
     });
 });
 
 document.addEventListener('wheel', (e) => {
     (e.deltaY < 0) ? document.querySelector('.container').style.border = 'unset' : document.querySelector('.container').style.border = '5px solid black';
 });
+
+document.querySelectorAll('.btn').forEach((btn) => {
+    btn.setAttribute('draggable', true);
+    btn.addEventListener('drag', (e) => {
+        e.target.style.backgroundColor = 'gray';  
+    });
+});
+
+document.querySelectorAll('p').forEach((p) => {
+    p.addEventListener('copy', (e) => {
+        alert('Rights Reserved');
+    });
+});
+
+// prevent nav from refreshing the page
+document.querySelectorAll('nav a').forEach((nav) => {
+    nav.addEventListener('click', (e) => {
+        e.preventDefault();
+    });
+})
