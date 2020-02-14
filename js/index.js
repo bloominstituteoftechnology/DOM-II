af0721c2dbd706effede6f514e22e1f14dd20913
John Oblenda <joandong2@gmail.com>
2020-02-13 21:43:38 -0500

    added few events

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Your code goes here
-document.getElementById("nav").addEventListener("mouseover", function(e) {   
+document.addEventListener('scroll', () => {
+    const bgColors = ['red', 'orange', 'blue', 'violet', 'black'];
+    document.getElementById("main-navigation").style.backgroundColor = (this.scrollY === 0) ? 'white' : bgColors[Math.floor(Math.random() * bgColors.length) + 1];
+});
+
+document.querySelector("#nav").addEventListener("mouseover", (e) => {   
     e.target.style.color = "red";
 
     setTimeout(function() {
@@ -7,14 +12,27 @@ document.getElementById("nav").addEventListener("mouseover", function(e) {
     }, 1000);
 }, false);
 
-
 document.addEventListener('keydown', (e) => {
-    document.getElementById("keyDownP").textContent += ` ${e.code}`;
+    document.querySelector("#keyDownP").textContent += ` ${e.code}`;
     if(e.code === 'Backspace') {
-        document.getElementById("keyDownP").textContent = "";
+        document.querySelector("#keyDownP").textContent = "";
     }
 });
 
-document.getElementById("featImg").addEventListener('dblclick', (e) => {
-    e.target.classList.toggle('transform');
+document.querySelectorAll('img').forEach((image) => {
+    image.addEventListener('dblclick', () => {
+        image.classList.toggle('transform');
+    });
+});
+
+document.querySelectorAll('h2').forEach((h2) => {
+    h2.addEventListener('click', (e) => {
+        (e.target.style.fontWeight == 'bold') ? 
+            e.target.style.fontWeight = 'normal' : 
+            e.target.style.fontWeight = 'bold';
+    });
+});
+
+document.addEventListener('wheel', (e) => {
+    (e.deltaY < 0) ? document.querySelector('.container').style.border = 'unset' : document.querySelector('.container').style.border = '5px solid black';
 });
