d24bee3465ff24f16001fee4d7972fbd3887e2f7
Ryan Erickson <rerickson21@yahoo.com>
2020-02-14 19:19:29 -0500

    Initial commit mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+// mouseenter event
+
+let missedTheBus = document.querySelector('h1');
+missedTheBus.addEventListener('mouseover', () => {
+    missedTheBus.style.backgroundColor = '#f4ff61';
+    missedTheBus.style.color = 'black';
+})
+
+// mouseleave event
+
+missedTheBus.addEventListener('mouseleave', () => {
+    alert("Don't miss the fun!");
+})
+
