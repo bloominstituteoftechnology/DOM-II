3386a7ef0c688a68a3a6175d12367a05b3daeac7
Ryan Erickson <rerickson21@yahoo.com>
2020-02-14 19:21:46 -0500

    Double click and resize

--- js/index.js
+++ js/index.js
@@ -13,3 +13,18 @@ missedTheBus.addEventListener('mouseleave', () => {
     alert("Don't miss the fun!");
 })
 
+// dblclick event
+
+let changePic = document.querySelector('.intro img');
+changePic.addEventListener('dblclick', () => {
+    changePic.src = 'https://files.constantcontact.com/726779ab001/79b55cdc-020c-4b53-8e96-d4c21ffdb181.jpg?a=1125872615244';
+})
+
+// resize event
+
+let newSize = document.querySelector('.intro img');
+
+window.addEventListener('resize', () => {
+    newSize.style.display = 'none';
+
+}) 
