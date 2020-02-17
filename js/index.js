ab069cbc3c0e628533f54cf349f65ff8b36bc1c3
Lindsey Henager <lindseyhenager@lindseys-air.attlocal.net>
2020-02-16 19:27:50 -0800

    added mousover on navi items

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-//Your code goes here
+// Your code goes here
+let navigationItems = document.querySelectorAll('.nav-link');
+
+navigationItems.forEach(item => {
+  item.addEventListener('mouseover', () => {   
+        item.style.color = '#17A2B8';
+    })
+  item.addEventListener('mouseleave', () => {   
+    item.style.color = '#000'; 
+})
+})
+
+let buttons = document.querySelectorAll('.btn').addEventListener('keydown', myFunction());
+
+function myFunction() {
+    buttons.querySelectorAll('.btn').style.backgroundColor = "red";
+}
+
