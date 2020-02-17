c7673a0311c225169865be54e6e4a4a71646371a
Kimberly Boyd <kimberlyboyd@Kimberlys-MacBook-Pro.local>
2020-02-17 18:58:49 -0500

    5 out of 10 done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,45 @@
 // Your code goes here
+
+// 1
+const logo = document.querySelector('.logo-heading');
+logo.addEventListener('click', (e) => {
+    logo.style.color = 'red';
+})
+
+// 2
+const busImage = document.querySelector('.intro img');
+busImage.addEventListener('mouseover', (e) => {
+    busImage.style.transform = 'scale(1.1)';
+})
+
+busImage.addEventListener('mouseleave', (e) => {
+    busImage.style.transform = 'scale(1.0)';
+})
+
+// 3
+const paragraph = document.querySelectorAll('p');
+paragraph.forEach(para => {
+    para.addEventListener('dblclick', (e) => {
+        para.style.backgroundColor = "yellow";
+    });
+});
+
+// 4
+
+const adventureImg = document.querySelector('.img-content img');
+adventureImg.addEventListener('mousemove', (e) => {
+    alert('Click the sign up button below!');
+});
+
+// 5
+
+const funImg = document.querySelector('.inverse-content img');
+funImg.addEventListener('drag', (e) => {
+    funImg.style.border = '5px solid red'
+});
+
+// 6
+
+
+
+
