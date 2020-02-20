525b4587d0016be04460eed1bb7039029bdd5ad9
Kim Ly Buck <kim.3993@gmail.com>
2020-02-19 18:05:55 -0600

    dom ii
    forgot to commit and push regualrly.

--- README.md
+++ README.md
@@ -21,38 +21,42 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 ## Task 1: Set Up LESS Preprocessor
 
-* [ ] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
+* [x] Verify that you have LESS installed correctly by running `lessc -v` in your terminal, if you don't get a version message back, reach out to your project manager for help.
 
-* [ ] Open your terminal and navigate to your preprocessing project by using the `cd` command
+* [x] Open your terminal and navigate to your preprocessing project by using the `cd` command
 
-* [ ] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
+* [x] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
 
-* [ ] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
+* [x] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
 
-* [ ] Once you see the red screen, you can delete that style and you're ready to start on the next task
+* [x] Once you see the red screen, you can delete that style and you're ready to start on the next task
 
 ## Task 2: Create Unique Event Listeners
 
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
-	* [ ] `mouseover`
-	* [ ] `keydown`
+	* [x] `mouseover`
+	* [x] `mouseout`
+	* [x] `keydown`
 	* [ ] `wheel`
-	* [ ] `drag / drop`
+	* [x] `drag / drop`
 	* [ ] `load`
 	* [ ] `focus`
-	* [ ] `resize`
-	* [ ] `scroll`
+	* [x] `resize`
+	* [x] `scroll`
 	* [ ] `select`
-	* [ ] `dblclick`
+	* [x] `dblclick`
+	* [x] `click`
+	* [x] `beforeprint`
+	* [x] `afterprint`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
-* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
-* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+* [x] Nest two similar events somewhere in the site and prevent the event propagation properly
+* [x] Stop the navigation items from refreshing the page by using `preventDefault()`
 
 ## Stretch Task:
 
-* [ ] Go look at [GSAP](https://greensock.com/) and implement the animations found in that library with your custom events.
+* [x] Go look at [GSAP](https://greensock.com/) and implement the animations found in that library with your custom events.
 
 ## Stretch assignment
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,123 @@
-// Your code goes here
+// *************************KIM BUCK 2020**************************************************
+// mouse over
+const logoHeader = document.querySelector('.main-navigation .container.nav-container h1');
+logoHeader.addEventListener('mouseover', (event) => {
+    event.target.style.transition = 'font-size 0.2s ease-in-out';
+    event.target.style.fontSize = '5rem'
+});
+
+logoHeader.addEventListener('mouseout', (event) => {
+    event.target.style.fontSize = '';
+});
+
+// beforeprint
+// add banner before print 
+const printBanner = document.createElement('div');
+printBanner.textContent = 'Printing the Fun Bus Page - KB';
+printBanner.style.textAlign = 'center';
+printBanner.style.fontSize = '8rem';
+window.addEventListener('beforeprint', () => {
+    document.body.prepend(printBanner);
+});
+
+// afterprint
+// take off banner after print
+window.addEventListener('afterprint', () => {
+    printBanner.remove();
+});
+
+// scroll
+document.addEventListener('scroll', () => {
+    logoHeader.style.transform = `rotate(${window.scrollY}deg)`;
+})
+
+// resize
+let resizeOpacityReset;
+window.addEventListener('resize', () => {
+    document.body.style.opacity = '0.5';
+    clearTimeout(resizeOpacityReset);
+    resizeOpacityReset = setTimeout(() => {
+        document.body.style.opacity = '';
+    }, 100);
+});
+
+// keydown
+document.addEventListener('keydown', event => {
+    if (event.key.toLowerCase() === 'f') {
+        const footer = document.querySelector('footer.footer');
+        footer.scrollIntoView();
+    }
+});
+
+// click sign me up
+const btns = document.querySelectorAll('.destination .btn');
+const clickHandler = () => {
+    const email = prompt('Enter your email:');
+    console.log(`You entered: %c${email}`, 'font-size: 2rem; color: orange');
+};
+btns.forEach(btn => {
+    btn.addEventListener('click', clickHandler);
+})
+
+// drag nav items
+let draggedEl;
+document.addEventListener('drag', event => {});
+document.addEventListener('dragstart', event => {
+    if (event.target.classList.contains('nav-link')) {
+        draggedEl = event.target;
+        draggedEl.style.opacity = '.5';
+    }
+})
+
+document.addEventListener('dragover', event => {
+    event.preventDefault();
+});
+
+document.addEventListener('dragend', event => {
+    event.target.style.opacity = '';
+})
+
+document.addEventListener('drop', event => {
+    event.preventDefault();
+    if (event.target.classList.contains('nav')) {
+        draggedEl.remove();
+        event.target.appendChild(draggedEl);
+        draggedEl = null;
+    }
+})
+
+// dblclick
+const introImage = document.querySelector('.intro img');
+const bodyImages = document.querySelectorAll('.img-content img');
+const adventureImage = bodyImages[0];
+const funImage = bodyImages[1];
+const destinationImage = document.querySelector('.content-destination img');
+
+// img flip handler
+const eventHandler = () => {
+    let temp = introImage.src;
+    introImage.src = destinationImage.src;
+    destinationImage.src = temp;
+
+    temp = adventureImage.src;
+    adventureImage.src = funImage.src;
+    funImage.src = temp;
+};
+
+const images = [introImage, adventureImage, funImage, destinationImage];
+
+images.forEach(img => {
+    img.addEventListener('dblclick', eventHandler);
+});
+
+// ***************************************************************************
+// prevent links from refreshing the page
+document.querySelectorAll('a').forEach(a => {
+    a.addEventListener('click', evt => evt.preventDefault());
+});
+
+// ******************************* STRETCH ********************************************
+// click on fun in the sun
+document.querySelector('.destination h4').addEventListener('click', () => {
+    gsap.to('.destination:first-child h4', {rotate: 150, duration: 1});
+});
