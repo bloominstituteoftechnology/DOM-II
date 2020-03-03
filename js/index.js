91c26b74c798afc3e2f62cc78cd826e94ab8925d
Mimi <mimihoang619@gmail.com>
2020-03-03 08:46:31 -0500

    project-check

--- js/index.js
+++ js/index.js
@@ -1,1 +1,56 @@
-// Your code goes here
+let intro = document.querySelector(".intro h2");
+intro.addEventListener("copy", (event) => {
+  alert("Intro is copied");
+});
+
+const keyDown = document.getElementsByClassName('.text-content');
+
+document.addEventListener('keydown', function(){
+    console.log("You pressed me!");
+});
+
+  let letsGo = document.querySelector(".img-content img");
+  letsGo.addEventListener("dblclick", (event) => {
+    event.preventDefault();
+    alert("WOOOOOOOOOOOO!!!");
+  });
+    
+  let imgRemove = document.querySelector('.img-content img');
+  
+    imgRemove.addEventListener('dblclick', event)
+  
+    imgRemove.addEventListener('dblclick', event => {
+       imgRemove.style.display = 'none';
+   });
+  
+  let destination = document.querySelectorAll(".destination h4");
+  destination[0].addEventListener("mousemove", (event) => {
+    event.target.style.color = "red";
+  });
+  destination[1].addEventListener("mousemove", (event) => {
+    event.target.style.color = "red";
+  });
+  destination[2].addEventListener("mousemove", (event) => {
+    event.target.style.color = "red";
+  });
+  
+  let pickYour = document.querySelector(".content-destination h2");
+  pickYour.addEventListener("drag", (event) => {
+    alert("Pick your destination is being dragged");
+  });
+  
+  let paragraph = document.querySelector('p')
+  paragraph.addEventListener('click', (event) => {
+      event.target.style.color = 'green';
+  });
+
+window.addEventListener('resize', function(){console.log('resize!')}, true); 
+
+var dragged;
+/* events fired on the draggable target */
+document.addEventListener("drag", function(event) {console.log('Drag!')}, false);
+
+const foot = document.querySelector('footer');
+ foot.addEventListener('mouseover', function(event) {
+    event.target.style.backgroundColor = 'pink';
+})
