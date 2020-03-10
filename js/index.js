1aa7053c3af5923b819f1be93acca3728fde6cd4
William Bell <wtb2804@gmail.com>
2020-03-10 17:38:43 -0400

    First commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
+// LECTURE NOTES
+let homeLink = document.getElementById('home-link')
 // Your code goes here
+console.log('something else still');
+homeLink.addEventListener(
+  'click', // string with the name of the event
+  () => {
+    // things we want happening when this
+    // link gets clicked
+    console.log('the link got clicked');
+  }
+)
+document.querySelector('nav').addEventListener('click', () => {
+  console.log('the nav got clicked');
+})
+
+
+// attach a 'click' event listener to the header element
+// and console.log('')
+document.querySelector('header').addEventListener('click', () => {
+  console.log('the header got clicked');
+});
+
+document.body.addEventListener('click', (stuff) => {
+  console.log(stuff);
+  console.log('the body of the page got clicked');
+});
