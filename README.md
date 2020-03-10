a51ee5ce7d83d05e07186019f5344e91457a556b
Jamie Sullivan <jamiesullivan@jamies-mbp.lan>
2020-03-10 16:33:19 -0700

    75% compelte

--- README.md
+++ README.md
@@ -25,11 +25,11 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 * [ ] Open your terminal and navigate to your preprocessing project by using the `cd` command
 
-* [ ] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
+* [ x] Once in your project's root folder, run the following command `less-watch-compiler less css index.less`
 
-* [ ] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
+* [ x] Verify your compiler is working correctly by changing the `background-color` on the `html` selector to `red` in your `index.less` file.
 
-* [ ] Once you see the red screen, you can delete that style and you're ready to start on the next task
+* [x ] Once you see the red screen, you can delete that style and you're ready to start on the next task
 
 ## Task 2: Create Unique Event Listeners
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,95 @@
 // Your code goes here
+
+
+gsap.registerEffect({
+    name: "fade",
+    defaults: {duration: 2}, //defaults get applied to the "config" object passed to the effect below
+    effect: (targets, config) => {
+        return gsap.to(targets, {duration: config.duration, opacity: 0});
+    }
+});
+
+//1
+homeLink.addEventListener(
+    'mouseover', //string with name of event
+    () => {
+        homeLink.style.color = 'pink';
+        // things we want to happen   
+       
+    }
+)
+
+//2
+homeLink.addEventListener(
+    'click', () => {
+        homeLink.style.color = 'red';
+        event.stopPropagation();
+    }
+)
+
+const wholePage = document.querySelector('html');
+
+//3
+document.addEventListener(
+    'scroll', () => {
+         wholePage.style.background = 'pink';
+         console.log("this is working")
+    }
+)
+
+const header = document.querySelector('header');
+
+
+//4
+header.addEventListener(
+    'click', () => {
+        header.style.background = "teal";
+    }
+)
+
+
+//5
+introText.addEventListener(
+    'mousemove', () => {
+        introText.style.color = 'teal';
+    })
+
+
+
+//6
+
+introText.addEventListener(
+    'mouseleave', () => {
+        introText.style.color = 'black';
+    }
+)
+
+
+const images = document.querySelectorAll("img")
+
+
+forEach(function(item) {
+    item.addEventListener("mouseenter", function() {
+      gsap.effects.fade(this);
+    });
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
