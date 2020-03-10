b658f402c40a3d24ee7463f5d37da18cc660f43d
Perezented <57721734+Perezented@users.noreply.github.com>
2020-03-10 11:29:33 -0500

    Working on this live. Initial commit.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+console.log("something lese still");
+
+const anchorTag = document.querySelectorAll("a");
+
+homeLink.addEventListener(
+    "click", //string with the name of the event
+    () => {
+        console.log("the link got clicked");
+    }
+);
