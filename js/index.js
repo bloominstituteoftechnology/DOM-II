7bcd6b1107e2b289691e096e070f49d9cb35af1c
jighreps <jipsga@gmail.com>
2020-03-10 16:26:25 -0700

    added in .preventDefault()

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@ document.querySelector("nav").addEventListener("click", event => {
   event.target.style.color = "red";
 });
 
-document.querySelector('header').addEventListener("mouseleave", event => {
+document.querySelector("header").addEventListener("mouseleave", event => {
   event.target.style.backgroundColor = "white";
 });
 
@@ -29,35 +29,40 @@ document.querySelector('header').addEventListener("mouseleave", event => {
 //  event.target.style.backgroundColor = 'red';
 // });
 
-document.body.addEventListener('wheel', event => {
-    event.target.style.backgroundColor = 'yellow';
-})
+document.body.addEventListener("wheel", event => {
+  event.target.style.backgroundColor = "yellow";
+});
 
-bus.addEventListener('dblclick', event =>{
-    event.target.style.display = "none";
-})
+bus.addEventListener("dblclick", event => {
+  event.target.style.display = "none";
+});
 
+bus.addEventListener("mouseenter", event => {
+  event.target.style.width = "500px";
+});
 
-bus.addEventListener('mouseenter', event => {
-    event.target.style.width = "500px";
-})
+bus.addEventListener("mouseout", event => {
+  event.target.style.width = "100%";
+});
 
-bus.addEventListener('mouseout', event => {
-    event.target.style.width = "100%";
-})
+document.querySelectorAll("p").forEach(function(item) {
+  item.addEventListener("copy", item => {
+    alert("NO COPY FOR YOU");
+  });
+});
 
+window.addEventListener("resize", event => {
+  bus.style.display = "none";
+});
 
-document.querySelectorAll('p').forEach(function (item){
-    item.addEventListener('copy', item =>{
-        alert("NO COPY FOR YOU"); 
-    })
- })
+document.body.addEventListener("contextmenu", event => {
+  alert("OBVIOUSLY, YOU HAVE MADE A RIGHT CLICKER");
+});
 
-window.addEventListener('resize', event => {
-    bus.style.display = 'none';
-})
+document.querySelector("nav").addEventListener("click", event => {
+  event.target.style.color = "red";
+});
 
-
-document.body.addEventListener('contextmenu', event => {
-    alert('OBVIOUSLY, YOU HAVE MADE A RIGHT CLICKER');
-})
+document.querySelector("nav").addEventListener("click", event => {
+  event.preventDefault();
+});
