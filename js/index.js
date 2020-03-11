103a3cd4a9bca23b5895b95ebb25870d3a48df74
DenisChuvilin <denischuv@gmail.com>
2020-03-10 19:45:50 -0700

    change color of text when scrolling and back to default on button up

--- js/index.js
+++ js/index.js
@@ -15,3 +15,22 @@ image.forEach(img => {
     img.style.transition = 'transform 1s';
   });
 });
+
+// change all p text on scroll 
+
+const textScroll = document.querySelectorAll('p');
+textScroll.forEach(txt => window.addEventListener('scroll', () => {
+    txt.style.color = 'orange';
+}))
+
+//change color back to black on up arrow key 
+const textDefault = document.querySelectorAll('p');
+textDefault.forEach(txt => addEventListener(
+    'keyup',
+    ( ) => {
+        txt.style.color = 'black';
+
+    }
+));
+
+// colors loop in 
