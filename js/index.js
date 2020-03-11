3ab5e590eb5f6fdc50d7283b726912277bcafee7
DenisChuvilin <denischuv@gmail.com>
2020-03-11 12:39:16 -0700

    added additional event listeners

--- js/index.js
+++ js/index.js
@@ -3,17 +3,17 @@
 // scale image and have it go back
 const image = document.querySelectorAll('img');
 image.forEach(img => {
-  img.addEventListener('mouseover', () => {
-    img.style.transform = 'scale(1.1)';
-    img.style.transition = 'transform 1s';
-  });
+    img.addEventListener('mouseenter', () => {
+        img.style.transform = 'scale(1.1)';
+        img.style.transition = 'transform 1s';
+    });
 });
 
 image.forEach(img => {
-  img.addEventListener('mouseleave', () => {
-    img.style.transform = 'scale(1)';
-    img.style.transition = 'transform 1s';
-  });
+    img.addEventListener('mouseleave', () => {
+        img.style.transform = 'scale(1)';
+        img.style.transition = 'transform 1s';
+    });
 });
 
 // change all p text on scroll 
@@ -27,10 +27,65 @@ textScroll.forEach(txt => window.addEventListener('scroll', () => {
 const textDefault = document.querySelectorAll('p');
 textDefault.forEach(txt => addEventListener(
     'keyup',
-    ( ) => {
+    () => {
         txt.style.color = 'black';
 
     }
 ));
 
-// colors loop in 
+// colors loop in nav bar
+const click = document.querySelectorAll('nav')[0];
+colors = ['hotpink', 'blue', 'teal', 'yellow'];
+
+let colorIndex = 0;
+click.addEventListener(
+    'click', e => {
+        e.target.style.color = colors[colorIndex];
+        colorIndex = (colorIndex + 1) % colors.length;
+    },
+    false
+);
+// Mouseover event on body with set timeout
+let test = document.querySelector('body');
+test.addEventListener(
+    'mouseover',
+    function (event) {
+        event.target.style.color = 'purple';
+
+        setTimeout(function () {
+            event.target.style.color = '';
+        }, 500)
+    },
+    false
+)
+// stop clicking nav
+const stopNav = document.querySelector('nav');
+stopNav.addEventListener('click', (event) => {
+    alert('stop clicking me!');
+    event.preventDefault();
+})
+
+// bottom click change color
+const clicks = document.querySelectorAll('.destination, .btn');
+clicks.forEach(btn => btn.addEventListener('click', (event) => {
+    btn.style.backgroundColor = 'yellow';
+    event.stopPropagation;
+}));
+
+// bottom click remove display
+clicks.forEach(btn => btn.addEventListener('dblclick', () => {
+    btn.style.display = 'none';
+    event.stopPropagation();
+}))
+
+// mousedown and mouseup
+
+let mouseDown = document.querySelector("h1");
+mouseDown.addEventListener("mousedown", function (e) {
+    e.target.style.backgroundColor = "red";
+})
+
+let mouseUp = document.querySelector("h1");
+mouseUp.addEventListener("mouseup", function (e) {
+    e.target.style.backgroundColor = "white";
+})
