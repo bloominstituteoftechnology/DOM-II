d73aa7fe28ae299ae0ce2897525afe7052edb671
James Brennan <creative44@Jamess-MacBook-Pro.local>
2020-03-10 22:46:31 -0400

    completed the challenge

--- README.md
+++ README.md
@@ -15,9 +15,9 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 **Follow these steps for completing your project.**
 
-* [ ] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
-* [ ] Add your project manager as a reviewer on the pull-request
-* [ ] Your project manager will count the project as complete by merging the branch back into master.
+* [x] Submit a Pull-Request to merge <firstName-lastName> Branch into master (student's  Repo). **Please don't merge your own pull request**
+* [x] Add your project manager as a reviewer on the pull-request
+* [x] Your project manager will count the project as complete by merging the branch back into master.
 
 ## Task 1: Set Up LESS Preprocessor
 
@@ -36,14 +36,14 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 * [ ] Using your [index.js file](js/index.js), create 10 [unique event listeners](https://developer.mozilla.org/en-US/docs/Web/Events). using your creativity to make the Fun Bus site more interactive.  Here are some unique events you could try to use: 
 	* [ ] `mouseover`
 	* [ ] `keydown`
-	* [ ] `wheel`
-	* [ ] `drag / drop`
+	* [x] `wheel`
+	* [x] `drag / drop`
 	* [ ] `load`
 	* [ ] `focus`
 	* [ ] `resize`
-	* [ ] `scroll`
+	* [ x `scroll`
 	* [ ] `select`
-	* [ ] `dblclick`
+	* [x] `dblclick`
 
 Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,79 @@
-// Your code goes here
+let variableFive = document.querySelector('.btn')
+    variableFive.addEventListener('click', (event) => {
+        event.preventDefault();     
+                                 //prevent default stops default function of object
+    })
+
+let variableSix = document.querySelector('footer.footer')
+    variableSix.addEventListener('click', (event) => {
+        variableSix.style.backgroundColor = 'red'   //red should not fire because of propogration
+        
+    })
+
+
+let variableSeven = document.querySelector('.intro')
+    variableSeven.addEventListener('click', (event) => {
+        variableSeven.style.backgroundColor = 'orange'    //orange should not fire because of propogation. Add true variable to make it fire first.
+    })
+
+
+let variableEight = document.querySelector('p')
+variableEight.addEventListener('click', (event) => {
+    variableEight.style.backgroundColor = 'orange'
+    event.stopPropagation();})
+
+
+
+let variableOne = document.querySelector('body')
+
+let variableTwo = document.querySelector('nav a')
+
+let variableThree = document.querySelector('h1')
+
+
+
+variableOne.addEventListener('mouseover', (event) => {
+    variableOne.style.backgroundColor = 'blue';
+})
+
+variableOne.addEventListener('mousedown', (event) => {
+    variableOne.style.backgroundColor = 'green';
+})
+
+variableOne.addEventListener('keydown', (event) => {
+    variableOne.style.backgroundColor = 'red';
+}) 
+
+variableTwo.addEventListener('dragend', (event) => {
+    variableTwo.style.backgroundColor = 'orange';
+}) 
+
+variableOne.addEventListener('mouseup', (event) => {
+    variableOne.style.backgroundColor = 'purple';
+})
+
+variableThree.addEventListener('wheel', (event) => {
+    variableThree.style.fontSize = '100px';
+})
+
+variableTwo.addEventListener('click', (event) => {
+    alert("you broke the site!")
+})
+
+variableThree.addEventListener('dblclick', (event) => {
+    variableThree.textContent = "You changed me!";
+})
+
+variableTwo.addEventListener('drag', (event) => {
+    variableTwo.textContent = "stop moving me!";
+})
+
+variableTwo.addEventListener('copy', (event) => {
+    variableTwo.textContent = "copied";
+})
+
+
+
+
+
+
