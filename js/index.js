110a8ba2d51d0c49179233043d01d66845e2e2d4
DenisChuvilin <denischuv@gmail.com>
2020-03-10 19:05:39 -0700

    increase image size and go back mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+// scale image and have it go back
+const image = document.querySelectorAll('img');
+image.forEach(img => {
+  img.addEventListener('mouseover', () => {
+    img.style.transform = 'scale(1.1)';
+    img.style.transition = 'transform 1s';
+  });
+});
+
+image.forEach(img => {
+  img.addEventListener('mouseleave', () => {
+    img.style.transform = 'scale(1)';
+    img.style.transition = 'transform 1s';
+  });
+});
