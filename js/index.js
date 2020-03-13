8d9e244dacf25aaf9303f7e90174216354c21752
Matt <makeshifthidalgo@hotmail.com>
2020-03-12 20:49:54 -0700

    preventDefault added to all links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+
+
+//preventDefault()
+const links = document.querySelectorAll("a");
+links.forEach(link => {
+    link.addEventListener("click", e => {
+        e.preventDefault();
+    })
+})
