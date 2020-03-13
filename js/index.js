e831aa80a36c8b1e14075d2fb2558f811e8a47c9
Mannie <carlos.alvarezberrios@gmail.com>
2020-03-12 23:35:49 -0400

    Added mouseover event to nav-links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const navLinks = document.querySelectorAll(".main-navigation .nav-container .nav .nav-link");
+
+navLinks.forEach(link => link.addEventListener("mouseover", (event) => {
+    event.target.style.transform = "scale(1.5)";
+    event.target.style.transition = "all 0.2s";
+}));
+
+navLinks.forEach(link => link.addEventListener("mouseout", (event) => {
+    event.target.style.transform = "scale(1)";
+}));
