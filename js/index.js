e32e333c08d958c5629240b960076d64c48dd819
Judy Ghashim <judyghashim@judys-mbp.attlocal.net>
2020-03-14 17:15:20 -0500

    adding 5-6

--- js/index.js
+++ js/index.js
@@ -21,7 +21,7 @@
 // })
 // Console.log(logoHeading);
 
-//------> Click
+//------> 1- Click
 const sectionNav = document.querySelector(".nav-link");
 console.log(sectionNav);
 
@@ -32,7 +32,7 @@ document.querySelectorAll(".nav-link").forEach(el => {
   });
 });
 
-// -----> scale
+// -----> 2- scale
 
 const letGo = document.querySelector(".img-content");
 console.log(letGo);
@@ -44,7 +44,7 @@ letGo.addEventListener("mouseenter", () => {
 letGo.addEventListener("mouseleave", () => {
   letGo.style.transform = "scale(1)";
 });
-// -----> flip
+// -----> 3- flip
 const logoHeading = document.querySelector(".logo-heading");
 console.log(logoHeading);
 logoHeading.addEventListener("dblclick", () => {
@@ -54,9 +54,40 @@ logoHeading.addEventListener("dblclick", () => {
     logoHeading.textContent = "Fun Bus";
   }
 });
-// -----> click
+// -----> 4- click
 const body = document.querySelector("body");
 body.addEventListener("click", () => {
   body.style.backgroundColor = "#FFF1F9";
   console.log("me last!");
+});
+
+// ------> 5- click event on multiple elements
+const secctionContent = document.querySelector("h2");
+console.log(secctionContent);
+document.querySelectorAll("h2").forEach(el => {
+  el.addEventListener("click", () => {
+    el.style.transition = "color 0.5s";
+    el.style.color = "#1A1E71";
+  });
+});
+
+// ------> 6- click event on multiple elements
+const textContent = document.querySelector("p");
+console.log(textContent);
+document.querySelectorAll("p").forEach(el => {
+  el.addEventListener("click", () => {
+    el.style.transition = "color 0.5s";
+    el.style.color = "#FF166A";
+  });
+});
+
+const catImage = document.querySelector(".img-fluid rounded");
+console.log(catImage);
+// mouse events
+catImage.addEventListener("mouseenter", () => {
+  catImage.style.transform = "scale(1.2)";
+  catImage.style.transition = "all 0.3s";
+});
+catImage.addEventListener("mouseleave", () => {
+  catImage.style.transform = "scale(1)";
 });
