687c648f274f74110af6087aee5a4cbc3c41fadc
Taja-House <ahlliahtaja@yahoo.com>
2020-03-13 18:36:13 -0700

    Created an event for navigation bar to change colors on hover

--- js/index.js
+++ js/index.js
@@ -1,2 +1,4 @@
 const onHover = event => event.target.style.color = "red";
 const navBar = document.querySelectorAll('a');
+navBar.forEach(anchors =>{ anchors.addEventListener('mouseover', onHover) });
+navBar.forEach(anchors =>{anchors.addEventListener('mouseleave', (event) => {event.target.style.color = "black"}) });
