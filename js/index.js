d694ed4222c1e393a9f108ae361ea26b1279af40
HDpearlman <HDpearlman@users.noreply.github.com>
2020-03-14 13:47:01 -0400

    added mouseenter/mouseleave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+const busImage = document.querySelector('img');
+
+busImage.addEventListener('mouseenter', () => {
+busImage.style.transform = 'scale(1.2)';
+busImage.style.transition = 'all 0.4s';
+})
+
+busImage.addEventListener('mouseleave', () => {
+    busImage.style.transform = 'scale(1)'
+    busImage.style.transition = 'all .4s'
+})
