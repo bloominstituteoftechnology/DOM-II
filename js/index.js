4a9831fbbf7e42beca5b5d94b9ae9fd6aaa13b46
Taja-House <ahlliahtaja@yahoo.com>
2020-03-13 19:32:03 -0700

    Started Styling h2s

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
-const onHover = event => event.target.style.color = "red";
+//Created a navBar onHover event
+const onHover = event => event.target.style.color = "orange";
 const navBar = document.querySelectorAll('a');
 navBar.forEach(anchors =>{ anchors.addEventListener('mouseover', onHover) });
 navBar.forEach(anchors =>{anchors.addEventListener('mouseleave', (event) => {event.target.style.color = "black"}) });
+
+//Styling for h2 content
+const hTwoContent = document.querySelectorAll('h2');
+hTwoContent.forEach(header =>{
+    header.style.textAlign = "center";
+});
