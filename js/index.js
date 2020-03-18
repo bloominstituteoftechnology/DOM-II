62d7c4e52e36f59ae1061642721181548fdbc860
Mike Raysor <michaelraysor1@gmail.com>
2020-03-18 00:19:14 -0400

    updated bugs and added 10 Event Listeners

--- js/index.js
+++ js/index.js
@@ -1,41 +1,75 @@
+
 // Your code goes here
 
-unBusHeading = document.querySelector('.logo-heading');
+
+
+
 navBar = document.querySelector('.nav');
 navItems = document.querySelectorAll('.nav-link');
 headerH2 = document.querySelector('.intro h2');
-allParagraphs = document.querySelectorAll('p');
+allParagraphs = document.querySelectorAll('.p');
+contentPick = document.querySelector('.content-pick');
+divH4 = document.querySelector('h4');
 
+
+divH4.addEventListener = ('click' , (event) => {
+    event.target.style.backgroundColor = "yellow" ; 
+});
+
+let funBusHeading = document.querySelector('.logo-heading');
 // Receive a "Don't Try It" note.
+console.log('hello') ;
+
 funBusHeading.addEventListener('click', (event) => {
-   alert('Dont Try it!')
+alert('Dont Try it!');
 });
 
-funBusHeading.addEventListener('click', (event) => { 
-event.target.style.backgroundColor = 'green'; 
-
+funBusHeading.addEventListener('click', (event) => {event.target.style.backgroundColor = 'yellow'; });
+navBar.addEventListener('click', (event) => {alert('Almost There!');
 });
 
-navBar.addEventListener('click', (event) => {
-    alert('Almost There!')
+contentPick.addEventListener('click', (event) => {alert('complete!')
 });
-
-
-//  Changes the color of each nav item at dblclick
-navItems.forEach(node => {
-    node.addEventListener('dblclick', function (event) {
-        // event.stopPropagation();
-        event.target.style.color = 'red';
-    })
-})
-
 
 headerH2.addEventListener('click', (event) => {
-    event.target.style.backgroundColor = 'green';
-    event.target.style.borderColor = 'yellow';
+event.target.style.backgroundColor = 'yellow';
+event.target.style.borderColor = 'yellow';
+
+});
+headerH2.addEventListener('mouseenter', () => {
+headerH2.style.transform = "scale(1.2)";
+headerH2.style.transition = "all 0.5s";
+
 });
 
+headerH2.addEventListener('mouseout', () => {
+headerH2.style.transform = "scale(1.1)";
+headerH2.style.transition = "all 0.5s";
+
+});
+
+
 //  alert when an image is copied
-allImages.addEventListener('mouseleave', () => {
-    allImages.style.transform = "scale(2)"
-})
+contentPick.addEventListener('mouseenter', () => {
+contentPick.style.transform = "scale(1.2)";
+contentPick.style.transition ="all 0.5s";
+
+});
+
+contentPick.addEventListener('mouseout', () => {
+contentPick.style.transform = "scale(1.1)";
+contentPick.style.transition = "all 0.5s";
+
+});
+
+funBusHeading.addEventListener('mouseenter', () => {
+funBusHeading.style.transform = "scale(1.2)";
+funBusHeading.style.transition = "all 0.5s";
+
+});
+
+funBusHeading.addEventListener('mouseout', () => {
+funBusHeading.style.transform = "scale(1.5)";
+funBusHeading.style.transition = "all 0.5s";
+
+});
