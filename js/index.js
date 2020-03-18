36334203df221e9431933c1881dbe5d0e7b0b91f
lex-marie790 <lexi.marie790@gmail.com>
2020-03-17 17:27:10 -0700

    mouseover element added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+ const doc = document;
+
+ // MOUSEOVER 
+
+//  const moNav = doc.querySelectorAll('.main-navigation');
+
+//  moNav.addEventListener('mouseover', function(event) {
+//      event.target.style.color = "violet";
+//  });
+
+const header = doc.querySelector('.main-navigation');
+
+// Bolden up our header to make it more prominent
+
+header.addEventListener('mouseover', (event) => {
+    // event.stopPropagation();
+    event.target.style.color = 'violet';
+});
