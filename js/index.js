ff5af00d795b0ffebd36e0a090cfd34accfa0f9c
Spacecase94 <chrisharwell72@gmail.com>
2020-03-28 09:22:33 -0400

    Added 5 of the 10 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+const getHeaderImg = document.querySelector('.intro img');
+const getHeadingTitle = document.querySelector('h1');
+const getTopHeading = document.querySelector('.intro h2');
+const getMiddleRightImg = document.querySelector('.img-content img');
+const getMiddleTopHeading = document.querySelector('.text-content h2');
+
+const setHeadingTitle = getHeadingTitle.addEventListener('click', (event) =>{
+   event.target.textContent = 'Adventure Land';
+});
+
+const setHeaderImg = getHeaderImg.addEventListener('dblclick', (event) => {
+   event.target.src = './img/naturePhoto.jpg';
+   event.target.style.width = '100%';
+   event.target.style.maxHeight = '60vh';
+});
+
+const setTopHeading = getTopHeading.addEventListener('mouseenter', (event) => {
+   event.target.textContent = 'Welcome to Adventure Land!';
+});
+
+const setMiddleRightImage = getMiddleRightImg.addEventListener('auxclick', (event) => {
+   event.target.src = './img/middle-right-image.jpg';
+});
+
+const setMiddleTopTextContent = getMiddleTopHeading.addEventListener('mouseleave', (event) => {
+   event.target.textContent = 'Lets See The Stars!';
+});
