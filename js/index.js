ce35bd457033253d6d5e845b6072d1aa16f90690
allan-pimble <allan.pimble@gmail.com>
2020-04-07 15:37:22 -0400

    Starting project...

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+/* 
+  copy
+  keypress
+  click
+  doubleclick
+  mouseenter
+  mouseleave
+  mouseover
+  * fullscreenchange - fullscreenchange - Bubbles = Yes
+  * scroll   - onscroll   - Bubbles = Yes
+  wheel
+  resize   - onresize   - Bubbles = No - window object only
+  focus    - onfocus    - Bubbles = No
+  blur     - onblur     - Bubbles = No
+  focusin  - onfocusin  - Bubbles = Yes
+  focusout - onfocusout - Bubbles = Yes
+*/
+
+// 1- Create a function called 'get'
+// that takes a CSS selector as its argument
+// and executes document.querySelector with the argument.
+const get = (selector) => {
+  return document.querySelector(selector)
+}
