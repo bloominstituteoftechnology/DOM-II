acd6561b65f1f114e0003e6807561daf317c6f39
allan-pimble <allan.pimble@gmail.com>
2020-04-07 17:21:04 -0400

    Created DOM selectors

--- js/index.js
+++ js/index.js
@@ -7,7 +7,6 @@
   mouseenter
   mouseleave
   mouseover
-  * fullscreenchange - fullscreenchange - Bubbles = Yes
   * scroll   - onscroll   - Bubbles = Yes
   wheel
   resize   - onresize   - Bubbles = No - window object only
@@ -20,6 +19,34 @@
 // 1- Create a function called 'get'
 // that takes a CSS selector as its argument
 // and executes document.querySelector with the argument.
-const get = (selector) => {
+const select = (selector) => {
   return document.querySelector(selector)
 }
+
+const selectAll = (selector) => {
+  return document.querySelectorAll(selector)
+}
+
+// Select the navbar
+const nav = select('.nav')
+
+// Select each image
+const imgFunBus = select(`[src='img/fun-bus.jpg']`)
+const imgAdventure = select(`[src='img/adventure.jpg']`)
+const imgFun = select(`[src='img/fun.jpg']`)
+const imgDestination = select(`[src='img/destination.jpg']`)
+
+// Select all and each destination
+const destination = selectAll('.destination')
+
+const funInTheSun = destination[0]
+const mountainExcursion = destination[1]
+const islandGetaway = destination[2]
+
+const attendance = (e) => alert(e.target.textContent)
+
+// Select all of the heading 2s
+const allH2s = selectAll('h2')
+allH2s.forEach(h2 => {
+  h2.addEventListener('click', attendance)
+})
