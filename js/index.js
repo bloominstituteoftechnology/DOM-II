5cf3a26ffc45e69a850684d4306c24f0995d2a4a
allan-pimble <allan.pimble@gmail.com>
2020-04-07 19:05:23 -0400

    Added 4 event handlers

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 /* 
   SOME EVENT OPTIONS:
-    copy, keypress, click, doubleclick, mouseenter, mouseleave, mouseover
-    scroll, onscroll, wheel, resize, focus, blur, focusin, focusout
+    copy, keypress, *click, *doubleclick, *mouseenter, *mouseleave, *mouseover
+    scroll, wheel, resize, focus, blur, focusin, focusout
 */
 
 // Query selector utilities
@@ -29,13 +29,29 @@ const imgDestination = select(src.destination)
 // Select all and each destination
 const destination = selectAll('.destination')
 
-const funInTheSun = destination[0]
+const funInTheSun = destination[0].firstChild
 const mountainExcursion = destination[1]
 const islandGetaway = destination[2]
 
+
+// CALLBACK FUNCTIONS
 // Display the textContent of the target calling this function
 const attendance = (e) => alert(e.target.textContent)
+const yellowBackground = (e) => e.target.style.backgroundColor = 'yellow'
+// const preventCopy = (e) => {
+// }
 
-// Select all of the h2s and add a click event listener
+const giveRoundCorners = (e) => {
+  e.target.classList.toggle('roundGreenBorders')
+}
+
+// Select all of the h2s
 const allH2s = selectAll('h2')
+
+// Event handlers
 allH2s.forEach(h2 => h2.addEventListener('click', attendance))
+imgFunBus.addEventListener('mouseover', giveRoundCorners)
+imgAdventure.addEventListener('mouseenter',giveRoundCorners)
+imgAdventure.addEventListener('mouseleave',giveRoundCorners)
+funInTheSun.addEventListener('doubleclick', yellowBackground)
+console.log(funInTheSun)
