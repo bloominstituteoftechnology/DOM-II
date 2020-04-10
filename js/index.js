15143013bb1d60bbbc99a7678f28aa750c54d78c
jacob <jacobcunningham96@yahoo.com>
2020-04-10 17:57:58 -0600

    added some eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
-// Your code goes here
+const home = document.querySelector('.nav-link-one');
+const button = document.querySelector('.button');
+const input = document.querySelector('.input');
+const funTimes = document.querySelector('.display');
+const form = document.querySelector('.form');
+localStorage.setItem('title', 'Fun Times!');
+home.addEventListener('click', () => alert('You are already on the homepage'));
+button.addEventListener('click', () => input.style.display = 'block');
+input.addEventListener('keyup', e => funTimes.innerHTML = e.target.value);
+form.addEventListener('submit', e =>  {
+    localStorage.setItem('time submitted', e.timeStamp)
+});
