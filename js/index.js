8b277af2628637489d99c51543860e12060e2931
jacob <jacobcunningham96@yahoo.com>
2020-04-10 18:17:18 -0600

    almost done

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@ const button = document.querySelector('.button');
 const input = document.querySelector('.input');
 const funTimes = document.querySelector('.display');
 const form = document.querySelector('.form');
+const fauxButton = document.querySelector('.btn');
+const destination = document.querySelector('.destination')
 localStorage.setItem('title', 'Fun Times!');
 home.addEventListener('click', () => alert('You are already on the homepage'));
 button.addEventListener('click', () => input.style.display = 'block');
@@ -10,3 +12,16 @@ input.addEventListener('keyup', e => funTimes.innerHTML = e.target.value);
 form.addEventListener('submit', e =>  {
     localStorage.setItem('time submitted', e.timeStamp)
 });
+fauxButton.addEventListener('dblclick', () => alert('Are you so bored that you are double clicking random things?'));
+destination.addEventListener('mouseenter', e => {
+    e.target.style.color = 'red';
+    setTimeout(() => {
+        e.target.style.color = '';
+    }, 500)
+}, false);
+destination.addEventListener('mouseover', e => {
+    e.target.style.color = 'teal';
+    setTimeout(() => {
+        e.target.style.color = '';
+    }, 500)
+}, false);
