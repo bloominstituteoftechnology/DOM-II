d1cf3f762bce06b4bfd7f650222d0261bb46756b
Cassandra Dew <cpdew2@gmail.com>
2020-04-12 15:36:19 -0400

    navigation listener

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
 // Your code goes here
-const navigation 
+const navigation = document.querySelectorAll('.nav')
+navigation.forEach(navigation => {
+navigation.addEventListener('mouseover', e => {
+e.target.style.border = '3px solid green';
+})
+})
