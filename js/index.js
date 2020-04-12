631b907f87f9c3c08ebb1c26a6570fd21365490c
Cassandra Dew <cpdew2@gmail.com>
2020-04-12 16:08:24 -0400

    logo heading

--- js/index.js
+++ js/index.js
@@ -17,3 +17,13 @@ button.forEach(button =>{
         e.target.style.border = '3px solid red';
     })
 })
+
+const letsGo = document.querySelector('.text-content h2')
+letsGo.addEventListener('resize', e => {
+    e.target.style.transform = 'rotate(360deg)';
+}) // not working
+
+const logoHeading = document.querySelectorAll('.logo-heading')
+logoHeading.addEventListener('drag', e => {
+    logoHeading.style.transform = 'rotate(180deg)';
+});
