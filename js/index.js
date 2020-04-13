07c49bcec9adf1f7f93ce087eb000717530ee081
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-04-12 20:54:03 -0400

    mouse enter and mouse leave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const bus = document.querySelector('.img-content');
+
+bus.addEventListener("mouseenter" , function(){
+
+bus.style.transform = "scale(1.6)";
+bus.style.transition = "transform 2s";
+
+})
+
+bus.addEventListener('mouseleave', function() {
+    bus.style.transform = "scale(1)";
+})
