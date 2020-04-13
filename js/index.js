1c7392f68adbdac452db9e8df356db67c4d8ebff
Valerie Vazquez <vnvazquez68@gmail.com>
2020-04-13 12:28:25 -0600

    dbl click picture enlargement

--- js/index.js
+++ js/index.js
@@ -15,17 +15,49 @@ navLink.forEach((element) => {
     })
 })
 
-/** header text slide in */
+/** h2 text rotate */
 
-let intro = document.querySelector('.intro h2');
-
-intro.addEventListener('mouseover', function() {
-    intro.style.transform = "rotateX(360deg)";
-    intro.style.transition = "transform 1.5s ";
+let intro = document.querySelectorAll('h2');
+intro.forEach((element) => {
+    element.addEventListener('mouseover', function(){
+        element.style.transform = "rotateX(360deg)";
+        element.style.transition = 'transform 1.5s';
+    })
+        element.addEventListener('mouseout', function(){
+        element.style.transform = "";
+    })
 })
 
-intro.addEventListener('mouseleave', function (){
-    intro.style.transform = "rotateX(360deg)";
+
+let p = document.querySelectorAll('p');
+p.forEach((click) => {
+    click.addEventListener ('click', function(){
+        if (click.style.backgroundColor === 'black'){
+            click.style.backgroundColor = 'white';
+            click.style.color = 'black';
+        }
+        else {
+            click.style.backgroundColor = 'black';
+            click.style.color = 'white';
+        }
+    
+    })
     
 })
 
+document.addEventListener('keydown', (event) => { alert('YOU PRESSED A KEY');
+  });
+
+
+const img = document.querySelectorAll('img');
+img.forEach((dbl) => {
+    dbl.addEventListener('dblclick', function (e) {
+      if (dbl.style.transform === 'scale(1.2)') {
+          dbl.style.transform = 'scale(1)';
+          dbl.style.transition = "1.2s";
+      } else {
+          dbl.style.transform = 'scale(1.2)';
+          dbl.style.transition = "1.2s";
+      }  
+})  
+});
