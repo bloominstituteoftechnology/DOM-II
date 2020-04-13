100a04d479a03f05edb250f449601e67d9d339e2
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-04-12 21:42:48 -0400

    keydown does not work

--- js/index.js
+++ js/index.js
@@ -11,3 +11,16 @@ roadMap.style.transition = "transform 2s";
 roadMap.addEventListener('mouseleave', function() {
     roadMap.style.transform = "scale(1)";
 })
+
+const bus = document.querySelector('.intro');
+bus.addEventListener("keydown", function() {
+bus.style.transform = "rotate(180deg)";
+//bus.style.transform = "transform 3s";
+
+})
+
+
+/*bus.img.addEventListener("keydown", function() {
+    bus.img.style.transform = "rotate(180deg)";
+    bus.img.style.transform = "transform 3s";
+    */
