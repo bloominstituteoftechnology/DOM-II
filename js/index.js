c8fdcfaf0352e80213addb03534bed9012263cf7
Mr. Jacky Charpentier <jcharp@fdu.edu>
2020-04-14 10:29:22 -0400

    forgot to save along the way

--- js/index.js
+++ js/index.js
@@ -2,74 +2,56 @@
 const roadMap = document.querySelectorAll('.img-content');
 roadMap.forEach((element) => {
 element.addEventListener("mouseenter" , function(){
-
     element.style.transform = "scale(1.6)";
     element.style.transition = "transform 2s";
 
 })
-
 element.addEventListener('mouseleave', function() {
     element.style.transform = "scale(1)";
 })
 })
-
-const intro = document.querySelector(".intro");
-
-document.addEventListener('keydown', function() {
-
-
-  intro.img.style.transform = "rotate(180deg)";
-})
-
 
 const body = document.querySelector('body');
  body.addEventListener('wheel', function(){
 
     body.style.transform = "scale(1.6)";
     body.style.transition = "2s";
-
-
     body.addEventListener('dblclick', function(){
-
-        body.style.transform = "scale(1)";
-
-    } )
-  //  
-
+    body.style.transform = "scale(1)";
+    } )  
  })
-
- body.addEventListener('keydown' , function(){
-
+    body.addEventListener('keydown' , function(){
     body.style.transform = 'rotate(180deg)';
  } )
- 
-body.addEventListener('keyup', function() {
+    body.addEventListener('keyup', function() {
     body.style.transform = 'rotate(0deg)';
 
 })
-/*
-roadMap.addEventListener("mouseenter" , function(){
 
-roadMap.style.transform = "scale(1.6)";
-roadMap.style.transition = "transform 2s";
-
-})
-
-roadMap.addEventListener('mouseleave', function() {
-    roadMap.style.transform = "scale(1)";
-})
+const navCont = document.querySelector('.nav-container');
 
 
+    var box = document.createElement('input');
+    box.type="text";
+    box.placeholder = "click here";
+    navCont.appendChild(box);
+    
+    const clickHere = document.querySelector('input[type="text"]');
 
-const bus = document.querySelector('.intro');
-bus.addEventListener("keydown", function() {
-bus.style.transform = "rotate(180deg)";
-//bus.style.transform = "transform 3s";
+    clickHere.addEventListener('focus', (event) => {
+        event.target.style.background ="pink";
+    })
 
-})
-*/
+    clickHere.addEventListener('blur', (event) => {
+        event.target.style.background = "";
+    })
 
-/*bus.img.addEventListener("keydown", function() {
-    bus.img.style.transform = "rotate(180deg)";
-    bus.img.style.transform = "transform 3s";
-    */
+    window.addEventListener('scroll', () => {
+        const scrollable = document.documentElement.scrollHeight - window.innerHeight;
+        const scrolled = window.scrollY;
+
+        if (scrolled === scrollable) {
+            alert('IT IS TIME TO PLAN YOUR TRIP.  CALL US AT 1-800-555-5555');
+        }
+
+    })
