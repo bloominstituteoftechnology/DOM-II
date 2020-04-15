66cfa1136f95d31fdd0e6f66a18dc7e7e3f6fb27
BryanPAnderson <AndersonBryan53@gmail.com>
2020-04-14 20:13:28 -0400

    final

--- js/index.js
+++ js/index.js
@@ -1,35 +1,70 @@
 // Your code goes here
-//1
-const firstImg = document.querySelector(".fun bus");
-firstImg.addEventListener("mouseenter", function(){
-    firstImg.style.transform = "scale(1.6)";
-    firstImg.style.transition = "Transform 1s";
-})
 
-firstImg.addEventListener("mouseleave", function(){
-    firstImg.style.transform = "scale(1)";
-})
-
-//2
-const allTitles = document.querySelectorAll("h2");
-allTitles.addEventListener("mouseover", function(){
-    allTitles.style.color = "cornflowerblue"
-})
-
-//3
-const header = document.querySelector('.main-navigation');
-
-const navlinks = document.querySelector('.nav');
-
-navlinks.addEventListener('mouseover', (event) => {
-    event.target.classList.toggle('active');
-});
-navlinks.addEventListener('mouseout', (event) => {
-    event.target.classList.toggle('active');
-});
-
-//4
-header.addEventListener('mouseover', (event) => {
+window.addEventListener("load", function(event) {
+    alert("The page has now loaded! Have Fun!");
+    });
+  
+  const funBus = document.querySelector("#img1");
+  funBus.addEventListener("mouseenter", function(){
+    funBus.style.transform = "scale(1.4)";
+    funBus.style.transition = "transform 1s";
+  })
+  
+  const button1 = document.querySelector('#btn1');
+  button1.addEventListener('click', function(event){
+    event.target.style.color = 'black';
     event.stopPropagation();
-    event.target.style.fontWeight = 'bold';
-});
+  });
+  
+  const divClick = document.querySelector('#divBorder');
+  divClick.addEventListener('click', function(event){
+    event.currentTarget.style.border = '1px solid red';
+  });
+
+  const button2 = document.querySelector('#btn2');
+  button2.addEventListener('mouseover', function(event){
+    event.target.style.background = 'black';
+  });
+  
+  const button3 = document.querySelector('#btn3');
+  button3.addEventListener('dblclick', function(event){
+    event.target.style.borderColor = 'red';
+  });
+  
+  const img1 = document.querySelector('#img1');
+  img1.addEventListener('mouseenter', function(event){
+    event.target.style.border = '2px dashed green';
+  });
+  
+  img1.addEventListener('mouseleave', function(event){
+    event.target.style.border = 'none';
+  });
+  
+  const img4 = document.querySelector('#img4');
+  img4.addEventListener('mousedown', function(event){
+    event.target.style.opacity = '0';
+  });
+  
+  img4.addEventListener('mouseup', function(event){
+    event.target.style.opacity = '1';
+  });
+  
+  const p = document.querySelector('p');
+  p.addEventListener('copy', function(event){
+    alert('Why are you copying my paragraph!?')
+  });
+  
+  const img2 = document.querySelector('#img2');
+  img2.addEventListener('contextmenu', function(event){
+    event.target.style.border = '2px solid yellow';
+  });
+  
+  img2.addEventListener('mouseout', function(event){
+    event.target.style.border = 'none';
+  });
+  
+  const navStop = document.querySelector('nav');
+  navStop.addEventListener('click', function(event){
+    event.preventDefault();
+    alert(`You can't click that!`);
+  });
