cf4267bc0fc693d96bf6ea77fa88004c0b1d61f4
Impulse2020 <irishht@gmail.com>
2020-05-05 16:25:23 -0400

    Layout ideas in and prompts in comments in index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
-// Your code goes here
+/* Your code goes here
+/ Using your [index.js file](js/index.js), create 10 [unique event listeners]
+(https://developer.mozilla.org/en-US/docs/Web/Events). 
+using your creativity to make the Fun Bus site more interactive.  
+Here are some unique events you could try to use: 
+
+*/
+//mouseover
+
+
+
+//keydown
+
+
+
+//wheel
+
+
+//drag / drop
+
+
+// load
+
+
+
+// focus
+
+
+
+// resize
+
+
+//scroll
+
+
+//select
+
+
+
+// duble click
+
+
+
+
+
+//Using the 10 unique events, find ways to update the DOM in creative ways. For example you could change colors, animate objects, remove objects, etc.
+/*
+* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly
+* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+*/
