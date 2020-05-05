48cc5b280f4334c0eadbafb6c187690ae8ba9b9b
Impulse2020 <irishht@gmail.com>
2020-05-05 17:53:27 -0400

    Adds a mouseover event that gives a gold border and moves the fun bus img

--- js/index.js
+++ js/index.js
@@ -6,8 +6,19 @@ Here are some unique events you could try to use:
 
 */
 //mouseover
+let bus = document.getElementById('bus');
+
+bus.addEventListener("mouseover", function(event){
+    event.target.style.border = '25px solid gold';
+    event.target.style.margin = '5% 0%';
+
+    setTimeout(function(){
+    event.target.style.border = '0px';
+    event.target.style.margin = '0%';
+    }, 1500);
 
 
+})
 
 //keydown
 
