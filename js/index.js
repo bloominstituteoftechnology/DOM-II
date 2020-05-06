64a749c7c7cc1cf2ae6e89a0cd8f7bef3eff7d0f
KSClopton <kyle.clopton@outlook.com>
2020-05-05 20:26:03 -0400

    More tasks finished. Two to go

--- js/index.js
+++ js/index.js
@@ -5,10 +5,17 @@
 function get(selector){
     return document.querySelector(selector)
 }
-
+function getAll(selector){
+    return Array.from(document.querySelectorAll(selector))
+}
 let navContainer = get('.nav-container')
 let logoHead = get('.logo-heading')
 let bodyWheel = get('.home')
+let headerDrag = getAll('h2')
+let bodyColor = get('body')
+let getForm = get('.form input')
+let getParagraphs = getAll('p')
+let clickables = getAll('a')
 
 // * [ ] `mouseover`
 
@@ -39,9 +46,53 @@ function turnGrey(event){
 bodyWheel.addEventListener('wheel', turnGrey)
 
 // * [ ] `drag / drop`
+
+// make headings draggable
+
+headerDrag.forEach(item => {item.setAttribute('draggable', true)});
+
+function dragHeading(event){
+    headerDrag.forEach((item) => item.style.color = 'red')
+}
+
+headerDrag.forEach(element => {element.addEventListener('drag', dragHeading)});
+
 // * [ ] `load`
+
+
+// headerIntro.addEventListener('loadstart', makeColorChange)
+
 // * [ ] `focus`
+
+
+function changeForm(event){
+    
+    getForm.style.border = 'red 2px solid'
+}
+getForm.addEventListener('focus', changeForm)
+
 // * [ ] `resize`
+
+
+
 // * [ ] `scroll`
+
+function turnGreen(event){
+    navContainer.style.backgroundColor = 'green'
+}
+window.addEventListener('scroll', turnGreen)
+
 // * [ ] `select`
+function turnRed(event){
+    getForm.style.color = 'red'
+}
+
+getForm.addEventListener('select', turnRed)
+
 // * [ ] `dblclick`
+
+function addBorder(event){
+    clickables.forEach((element) => element.style.border = 'orange 2px solid')
+}
+
+clickables.forEach(item => {item.addEventListener('dblclick', addBorder)});
