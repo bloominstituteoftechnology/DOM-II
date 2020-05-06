6fcbc7c708133cd6693da10d06d9201340d7f066
Kirk Tolliver <kirktolliver28@gmail.com>
2020-05-05 21:20:12 -0500

    Merging this branch to master test

--- js/index.js
+++ js/index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+
+const logoEvent = document.querySelector('.logo-heading');
+
+//This is a comment that i am making to  commit to my branch
+logoEvent.addEventListener('dblclick', (e)=> { logoEvent.classList.toggle('large') });
