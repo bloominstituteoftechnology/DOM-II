a842356d3b136544c00a7adb1f3f7918838e643d
JenVest2020 <jenvest2019@gmail.com>
2020-05-07 23:24:37 -0500

    added 2 evEL's to my navigation

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-let links = document.querySelector('nav-link');
+let links = document.querySelectorAll('.nav-link');
 links.forEach(text => {
     text.addEventListener('mouseover', event => {
         event.target.style.textDecoration = 'underline'
+    });
+});
+links.forEach(text => {
+    text.addEventListener('mouseout', event => {
+        event.target.style.textDecoration = 'none'
     });
 });
