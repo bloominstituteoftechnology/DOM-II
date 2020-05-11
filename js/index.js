42de14df75052652fac7dfdfcb4d0145a48635f2
Robel Mengistu <62121236+robelv2020@users.noreply.github.com>
2020-05-10 19:15:05 -0600

    Added mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+// Mouseover event
+const test = document.querySelector('.nav-link')
+test.addEventListener('mouseover', () => {
+  test.innerHTML = 'Travel';
+})
+
+// MouseLeave
