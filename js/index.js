da73a03e4eb915d905729e1fd59724887df57e50
tbbcoach <58452640+tbbcoach@users.noreply.github.com>
2020-06-04 21:48:47 -0400

    started adding events to page.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+const body = document.querySelector('body');
+body.addEventListener('mouseover', () => {
+    body.style.backgroundColor = 'lime';
+
+    body.addEventListener('mouseleave', () => {
+        body.style.backgroundColor = 'white';
+})
+});
+
+
