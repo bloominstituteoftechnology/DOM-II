f5ddf13ce8bb56eb1b001c48ac93ef1b4d4965da
noahbibb21 <noahbibb90@gmail.com>
2020-06-06 14:01:31 -0400

    starting to get somewhere

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+const navLinks = document.querySelector('a')
+const busImg = document.querySelector('.intro img')
+const destination = document.querySelector('.destination');
+const footer = document.querySelector('.footer')
+const body = document.querySelector('body')
+const btn = document.querySelector('.btn')
+const middleImage = document.querySelector('.content-destination img');
+const headerContainer = document.querySelector('.intro');
+const destinationText = document.querySelectorAll('.destination');
+
+// 1.
+navLinks.addEventListener('mouseover', () => {
+    console.log('mouse went over')
+    navLinks.style.color = 'yellow';
+});
+
+// 2. 
+navLinks.addEventListener('mouseout', () => {
+    console.log('mouse went out')
+    navLinks.style.color = 'black';
+})  
