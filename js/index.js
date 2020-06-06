10b18013620f3510744208b9095e3d3e2c2ce328
Donavan Terranova <dterran2@outlook.com>
2020-06-05 19:01:13 -0600

    finished assignments but need to work on two chunks of code

--- js/index.js
+++ js/index.js
@@ -17,17 +17,43 @@ navBar.forEach((element) => {
     event.target.style.color = 'black';
     })
 });
-// const buss = document.querySelector('.intro img')
-// buss.addEventListener('keydown', (e) => {
-//     if ( )
-// } )
+const headerImg = document.querySelectorAll('img');
+headerImg.forEach((element) => {
+    element.addEventListener('mousedown', (event) => {
+    event.target.style.transform = 'scale(1.2)';
+    })
+});
+headerImg.forEach((element) => {
+    element.addEventListener('dblclick', (event) => {
+    event.target.style.transform = 'scale(1)';
+    })
+});
 
-// keydown
-// Wheel
-// Drag
-// load
-// focus
-// resize
-// scroll
+const welcome = document.querySelector('.intro h2')
+welcome.addEventListener('keydown', (e) => {
+    if (e === 'k') {
+    welcome.style.visibility = 'hidden';    
+    }
+} );
+
+const letGo = document.querySelector('.content-section .text-content h2')
+letGo.addEventListener('onwheel', (e) => {
+    e.target.style.visibility = 'hiden';
+});
+
+const footerHeaders = document.querySelectorAll('.content-pick .destination p')
+footerHeaders.forEach((element) => {
+    element.addEventListener('mouseenter', (event) =>{
+        event.target.style.color = 'red';
+    })
+});
+
+footerHeaders.forEach((element) => {
+    element.addEventListener('mousedown', (event) => {
+    event.target.style.transform = 'scale(1.2)';
+    })
+});
+
+
 
 
