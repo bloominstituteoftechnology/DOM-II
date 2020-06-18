19862628fa082f3ce74c0c6b9097c6132f472762
unknown <clay2000lbs@gmail.com>
2020-06-18 17:41:26 -0500

    completeled task 1-2

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+let headLine = document.querySelector('h1');
+headLine.addEventListener('mouseover', (event) => {headLine.style.color = 'cyan';});
+
+let footer = document.querySelector('footer');
+footer.addEventListener('dblclick', (event) => {footer.style.backgroundColor = 'red';});
