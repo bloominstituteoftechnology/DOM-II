9ab6845f95e9d2b6bda7161b33a09d2fa0df8688
kevin lam <kevin@kevins-MacBook-Pro-3.local>
2020-07-07 12:29:12 -0700

    MVP started

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 // Your code goes here
+
+//mouseover
+const links = document.querySelectorAll('a')
+
+links.forEach(link => {
+    link.addEventListener('mouseover',() => {
+        link.style.color = 'aqua'
+    })
+})
+
+//keydown
+document.addEventListener('keydown', (e) => {
+    if(e.keyCode === 27 || e.key === 'Escape'){
+        alert('There is no escape.')
+    }
+})
