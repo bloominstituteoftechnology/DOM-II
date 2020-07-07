b10a9ee0e647665c530e0dacb4f8e932eebeb3b8
unknown <anatoliy.a.k.85@gmail.com>
2020-07-07 16:12:34 -0400

    added click and wheel

--- js/index.js
+++ js/index.js
@@ -13,9 +13,23 @@
       event.target.style.color = "";
     }, 500);
   }, false);
- //`keydown`
+ //`click`
+ const header = document.querySelector('h1');
+console.log(header)
+header.addEventListener('click', (event) => {
+  event.target.style.color = 'purple';    
+});
+
  //`wheel`
+ const header2 = document.querySelector('h2');
+ console.log(header2)
+ header2.addEventListener("wheel", myFunction);
+
+function myFunction() {
+  this.style.fontSize = "45px";
+}
  //`load`
+ 
  //`focus`
  //`resize`
  //`scroll`
