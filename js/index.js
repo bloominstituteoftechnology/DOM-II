395440c9c74fafaf0a6b7b10e11009fb4d4a6974
unknown <anatoliy.a.k.85@gmail.com>
2020-07-07 15:40:37 -0400

    added color to nav links

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+
+
+
+ //`mouseover`
+ const NavLink = document.querySelector("nav");
+ console.log(NavLink)
+ NavLink.addEventListener("mouseover", function( event ) {   
+    event.target.style.color = "turquoise";
+  
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+ //`keydown`
+ //`wheel`
+ //`load`
+ //`focus`
+ //`resize`
+ //`scroll`
+ //`select`
+ //`dblclick`
