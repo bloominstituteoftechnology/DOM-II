f36de23cd47985a578aa267f4c44874a8e00826c
Cisco <cisco@franciscos-mbp.home>
2020-07-10 20:31:56 -0700

    1.1

--- js/index.js
+++ js/index.js
@@ -1,1 +1,43 @@
 // Your code goes here
+
+
+
+console.log("mouse enter event");
+const busImage = document.querySelector("img")
+busImage.addEventListener('mouseenter', () => {
+    busImage.style.transform = "scale(1.05)"
+
+})
+//
+console.log("mouse leave event")
+busImage.addEventListener('mouseleave', () => {
+    busImage.style.transform = "scale(1)"
+  
+})
+
+
+//
+const mapPic = document.querySelector('.img-content img')
+mapPic.addEventListener('dblclick', () => {
+    mapPic.src = "https://images.unsplash.com/photo-1469854523086-cc02fe5d8800?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1608&q=80"
+})
+
+//
+mapPic.addEventListener('click', () => {
+    mapPic.src = 'https://images.unsplash.com/photo-1536685712909-6ac3633e0812?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80'
+})
+//
+
+let textColor = document.querySelector('html')
+textColor.addEventListener('dblclick', (event) => {
+    event.target.style.color = "green"
+})
+//
+textColor.addEventListener('click', (event) => {
+    event.target.style.color = "black"
+})
+//
+const mouseOver = document.querySelector('footer')
+mouseOver.addEventListener('mouseenter', () => {
+   mouseOver.stlye.color = "white"
+})
