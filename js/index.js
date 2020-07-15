456b06306250869774bea4c3f1ae5dbd4cf2f59c
Brianna Larkins <blarkins123@gmail.com>
2020-07-15 09:21:15 -0700

    alomst there

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 // Your code goes here
 const allImg = document.querySelectorAll('img')
-//console.log(allImg)
+
 allImg.forEach(item => {
     item.addEventListener('mouseover', event => {
-      event.target.style.border = "solid black"
+      event.target.style.border = "solid purple"
       setTimeout(function() {
         event.target.style.border = "";
       }, 1000);
@@ -21,4 +21,36 @@ allImg.forEach(item => {
     }, 500);
   }, false);
 
-  
+
+document.addEventListener('keydown', (e) => {
+  if(e.keyCode === 27 || e.key === 'Backspace'){
+      alert('The time is NOW.')
+  }
+}) 
+
+const p = document.querySelectorAll('p')
+window.addEventListener('load',() => {
+    setTimeout(function(){
+        p.forEach(instance => {
+            instance.textContent = 'Its never too late!'
+        })
+        },5000)
+})
+
+window.addEventListener('resize', () => {
+  document.querySelector('body').style.backgroundColor = 'green'
+})
+
+const imgs = document.querySelectorAll('img')
+
+window.addEventListener('scroll', () => {
+    imgs.forEach(img => {
+        img.style.display = 'none'
+    })
+})
+document.addEventListener('wheel', (event) => {
+  if(event.deltaY>0)
+   document.body.style.backgroundColor = 'yellow';
+   if(event.deltaY<0)
+   document.body.style.backgroundColor = 'blue';
+  })
