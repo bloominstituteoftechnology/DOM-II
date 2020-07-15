18519daf887118101d0750e9a8e7fa84924cd0ae
Zack-Ballenger <zack.ballenger@tutanota.com>
2020-07-15 05:54:39 -0600

    Ahhhhh lost all my work, redoing project, just finished 1 of 10 events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-// Your code goes here
+// 1.) Mouse events
+
+
+const navStuff = document.querySelector('.nav');
+navStuff.addEventListener('mouseover', (event) => {
+
+navStuff.style.transform = 'scale(1.3)';
+navStuff.style.transition = 'transform 1s';
+
+
+});
+
+navStuff.addEventListener('mouseleave',(event) => {
+    navStuff.style.transform = 'scale(1)';
+    navStuff.style.transition = 'transform 1s';
+});
+
+// 2.) 
+
+
