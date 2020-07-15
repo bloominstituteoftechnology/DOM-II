4faf2c469640b9778ed15ad1e02d3472dca4ab83
Brianna Larkins <blarkins123@gmail.com>
2020-07-14 18:24:42 -0700

    not done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+const allImg = document.querySelectorAll('img')
+//console.log(allImg)
+allImg.forEach(item => {
+    item.addEventListener('mouseover', event => {
+      event.target.style.border = "solid black"
+      setTimeout(function() {
+        event.target.style.border = "";
+      }, 1000);
+    })
+
+  })
+
+  const NavLink = document.querySelector("nav");
+ console.log(NavLink)
+ NavLink.addEventListener("mouseover", function( event ) {   
+    event.target.style.color = "Red";
+
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
+
+  
