9d68ee029e5d4703807c2befade5a26cea21e493
A <jesus.e.estrada@hotmail.com>
2020-08-04 22:59:53 -0500

    completed mvp

--- js/index.js
+++ js/index.js
@@ -3,34 +3,13 @@
 const funBus = document.querySelector('h1.logo-heading');
 const allLinks = document.querySelectorAll('nav a');
 const allImgs = document.querySelectorAll('img');
-const allBtns = document.querySelectorAll('.btn');
+const allHeaderTwo = document.querySelectorAll('h2');
+const allHeaderFour = document.querySelectorAll('h4')
 const body = document.querySelector('body');
+const headerScroll = document.querySelector('header')
+const allBtns = document.querySelectorAll('.btn')
+const h2Text = document.querySelector('.content-destination h2')
 
-
-/******************** EVENT FUNCTIONS *****************/
-
-function mouseOvr(event) {
-  event.target.style.color = 'orange';
-}
-
-function keyDown(event) {
-  if (event.key === 'Escape') {
-    funBus.style.color = 'blue'
-  }
-}
-
-function dblClick(event) {
-
-  if (event.target.style.border) {
-    event.target.style.border = 'none'
-  } else {
-    event.target.style.border = '2px dashed green'
-  }
-}
-
-function useScroll(event) {
-  body.style.background = 'orange';
-}
 /***************** EVENT LISTENERS ******************/
 
 allLinks.forEach(el => {
@@ -41,11 +20,86 @@ allImgs.forEach(el => {
   el.addEventListener('dblclick', dblClick)
 })
 
-allBtns.forEach(el => el{
-  el.addEventListener('')
-})
+document.addEventListener('scroll', useScroll)
+
+window.addEventListener('resize', useResize)
 
 document.addEventListener('keydown', keyDown)
 
-body.addEventListener('wheel', useScroll)
+body.addEventListener('wheel', useWheel)
+
+window.addEventListener('load', useLoad)
+
+document.addEventListener('mouseup', useMouseUp)
+
+document.addEventListener('mousedown', useMouseDwn)
+
+h2Text.addEventListener('mouseenter', useMouseEntr)
+/******************** EVENT FUNCTIONS *****************/
+
+allLinks.forEach(el => {
+  el.addEventListener('click', event => {
+    console.log('test');
+    event.preventDefault()
+  })
+})
+
+function mouseOvr(event) {
+  event.target.style.color = 'orange';
+}
+
+function keyDown(event) {
+  if (event.key === 'A') {
+    funBus.style.color = 'blue'
+  }
+}
+
+function dblClick(event) {
+
+  if (event.target.style.border === '2px dashed green') {
+    event.target.style.border = 'none'
+  } else {
+    event.target.style.border = '2px dashed green'
+  }
+}
+
+function useWheel(event) {
+
+  body.style.background = 'orange';
+}
+
+function useResize(event) {
+  allHeaderTwo.forEach(el => {
+    el.style.border = '2px solid purple'
+  })
+}
+
+function useLoad(event) {
+  allHeaderFour.forEach(el => {
+    el.style.color = 'blue';
+  })
+}
+
+function useScroll(event) {
+  headerScroll.style.background = 'pink';
+}
+
+function useMouseDwn(event) {
+  allBtns.forEach(el => {
+    el.style.background = 'pink';
+  })
+}
+
+function useMouseUp(event) {
+
+  allBtns.forEach(el => {
+    el.style.background = 'black';
+  })
+}
+
+function useMouseEntr(event) {
+  event.target.style.color = 'blue';
+}
+
+
 
