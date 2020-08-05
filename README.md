f68e5353a414560b69747d7a31d34f32d8616ef6
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-08-05 16:23:23 +0200

    finshed mvp

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
   * `dblclick`
   * `drag / drop`
 
-Note: Drag and drop is a bit more advanced than the others as it's not actually a single type of event but several, working together.
+Note: Drag and drop is a bit more advanced than the others: it's not actually a single type of event but several types that need to work together.
 
 * [ ] Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
 * [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
--- js/index.js
+++ js/index.js
@@ -1,1 +1,83 @@
 // Your code goes here
+
+//h2
+const titles = document.querySelectorAll('h2')
+
+titles.forEach(element => {
+    element.addEventListener('mouseenter', () => {
+        element.style.color = 'pink';
+        element.style.fontSize = '50px';
+    })
+});
+
+titles.forEach(element => {
+    element.addEventListener('mouseleave', () => {
+        element.style.color = 'black';
+        element.style.fontSize = '3.6rem';
+    })
+});
+
+
+//p 
+const paragraph = document.querySelectorAll('p')
+
+paragraph.forEach(element => {
+    element.addEventListener('mouseover', () => {
+        element.style.color = 'blue';
+    })
+});
+
+paragraph.forEach(element => {
+    element.addEventListener('mouseout', () => {
+        element.style.color = 'brown';
+    })
+});
+
+//button
+const button = document.querySelectorAll('.btn')
+
+button.forEach(element => {
+    element.addEventListener('dblclick', () => {
+        element.style.color = 'green';
+    })
+});
+
+//nav
+const nav = document.querySelectorAll('a')
+
+nav.forEach(element => {
+    element.addEventListener('click', () => {
+        element.style.transform = 'scale(1.3)';
+    })
+    element.addEventListener('focus', (event) => {
+        event.target.style.background = 'pink';  
+    })
+});
+
+
+//img 
+const img = document.querySelectorAll('img')
+
+img.forEach(element => {
+    element.addEventListener('mouseenter', () => {
+        element.style.transform = 'scale(1.2)';
+        element.style.transition= 'all 0.3s';
+    })
+});
+
+img.forEach(element => {
+element.addEventListener("mouseleave", () => {
+    element.style.transform ="scale(1)";
+})
+});
+
+
+//h4 
+const h4 = document.querySelectorAll('h4')
+
+h4.forEach(element => {
+    element.addEventListener('wheel', (event) => {
+        event.target.style.background = 'gray';
+        
+    })
+});
