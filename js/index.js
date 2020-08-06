1ec6e7b9d3afd2c5a080e39de780a869ea2072cf
tarmes <traarmes@gmail.com>
2020-08-06 17:50:29 -0400

    mouseenter

--- js/index.js
+++ js/index.js
@@ -1,2 +1,18 @@
 // Your code goes here
-hello
+// MouseOver
+
+const headChange = document.querySelector('.intro h2')
+console.log(headChange);
+function mouseOver(event) {
+    event.target.style.color = 'red';
+}
+headChange.addEventListener('mouseenter', mouseOver)
+// Keydown
+// Wheel
+// Load
+// Focus
+// Resize
+// Scroll
+// Select
+// DoubleClick
+// Drag/Drop
