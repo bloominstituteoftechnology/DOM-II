16eeec630710ce05f60dbbd9e6baacc66e1e00d9
halliwell2046 <halliwell2046@yahoo.com>
2020-08-07 16:47:36 -0400

    click

--- js/index.js
+++ js/index.js
@@ -6,9 +6,16 @@ bus.addEventListener('mouseover', (event) => {
 });
 
 const cool = document.querySelector('.main-navigation');
-console.log(cool);
+// console.log(cool);
 
 cool.addEventListener('dblclick', (event) => {
     event.target.style.backgroundColor = 'purple';
 });
   
+const adventure = document.querySelector('.img-content');
+//  console.log(adventure);
+
+adventure.addEventListener('click', (event) => {
+    event.target.src = 'https://images.unsplash.com/photo-1496154704336-6c88a94a019a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
+});
+
