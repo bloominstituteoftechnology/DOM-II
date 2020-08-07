69b7a3e94a8d6b8c8df306585eb9f9c6040b27f4
halliwell2046 <halliwell2046@yahoo.com>
2020-08-07 17:03:41 -0400

    resize

--- js/index.js
+++ js/index.js
@@ -31,3 +31,12 @@ tinyBus.addEventListener('mouseleave', () => {
     tinyBus.style.transform = 'scale(1)';
     tinyBus.style.transition = 'transform 1s';
 });
+
+const body = document.querySelectorAll('img');
+// console.log(body);
+
+window.addEventListener('resize', () => {
+    body.forEach((pic) => {
+        pic.src = 'https://images.unsplash.com/photo-1478359900967-91ec0c6edc60?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=80';
+    });
+});
