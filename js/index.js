b38e0cdc2981d09de899dfb1073f314f62158a82
halliwell2046 <halliwell2046@yahoo.com>
2020-08-07 16:34:49 -0400

    dblclick

--- js/index.js
+++ js/index.js
@@ -4,3 +4,11 @@ const bus = document.querySelector('.intro');
 bus.addEventListener('mouseover', (event) => {
     event.target.style.backgroundColor = 'black';
 });
+
+const cool = document.querySelector('.main-navigation');
+console.log(cool);
+
+cool.addEventListener('dblclick', (event) => {
+    event.target.style.backgroundColor = 'purple';
+});
+  
