7056148c1b2b7e8346da2c800dfbf012257ca3fe
halliwell2046 <halliwell2046@yahoo.com>
2020-08-07 16:52:19 -0400

    mouseenter

--- js/index.js
+++ js/index.js
@@ -19,3 +19,10 @@ adventure.addEventListener('click', (event) => {
     event.target.src = 'https://images.unsplash.com/photo-1496154704336-6c88a94a019a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
 });
 
+const tinyBus = document.querySelector('.intro img');
+// console.log(tinyBus);
+
+tinyBus.addEventListener('mouseenter', () => {
+    tinyBus.style.transform = 'scale(1.2)';
+    tinyBus.style.transition = 'transform 1s';
+});
