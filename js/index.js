91c6598bc88e278d7eb92431420cbe2f114b8167
unknown <clay2000lbs@gmail.com>
2020-08-11 18:58:55 -0500

    added events

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 // Your code goes here
 let headLine = document.querySelector('h1');
 headLine.addEventListener('mouseover', (event) => {headLine.style.color = 'cyan';});
+headLine.addEventListener('dblclick', (event) => {headLine.style.color = 'black';});
 
 let footer = document.querySelector('footer');
+footer.addEventListener('mouseover', (event) => {footer.style.backgroundColor = 'green';});
 footer.addEventListener('dblclick', (event) => {footer.style.backgroundColor = 'red';});
+
+
+let h2Highlight = document.querySelector('h2');
+h2Highlight.addEventListener('mouseover', (event) => {h2Highlight.style.color = 'green';});
+
+let pHighlight = document.querySelector('body p');
+pHighlight.addEventListener('mouseover', (event) => {pHighlight.style.color = 'darkgreen';});
+
+let btnClick = document.querySelectorAll('body btn');
+btnClick.addEventListener('dblclick', (event) => {headLine.style.backgroundColor = 'black';});
+
