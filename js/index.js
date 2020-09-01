28d2989cc50f6d80c6de397bfe3d73b9d9ff209b
Simone Ballard <simone.a.ballard@gmail.com>
2020-09-01 14:32:12 -0600

    sample codes added into the js folder

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+test.addEventListener("mouseover", function( event ) {   
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+  
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
