6c6ff9c3f0bb47af963343a5dbc1e0feebe545e7
Simone Ballard <simone.a.ballard@gmail.com>
2020-09-01 19:58:11 -0600

    all code in, some testing still required

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@
 
 // MOUSEOVER STYLE
 
+
 document.addEventListener("mouseover", function (event) {   
     event.target.style.color = "orange";
     setTimeout(function() {
@@ -9,25 +10,43 @@ document.addEventListener("mouseover", function (event) {
     }, 200);
   }, false);
 
-  const signupButton = document.querySelector('.content-pick .destination .btn');
+//MOUSE ENTER_LEAVE
 
-signupButton.addEventListener("mouseover", function (event) {
-  event.target.style.color = "green"; 
+var enterText = 0;
+var leaveText = 0;
+
+const mouseTarget = document.getElementByClassName('content-destination');
+
+mouseTarget.addEventListener('mouseenter', e => {
+  mouseTarget.style.border = '5px dotted orange';
+  enterText++;
 });
+
+mouseTarget.addEventListener('mouseleave', e => {
+  mouseTarget.style.border = '1px solid #333';
+  leaveText++;
+});
+
+
+//CLICK
+
+const signupButton = document.querySelectorAll('.content-pick .destination .btn');
+
+signupButton.addEventListener('click', (event) => { event.target.style.backgroundColor = 'black'; });
 
 
 // KEYDOWN 
 
-document.addEventListener('keydown', function (event) {
+document.addEventListener('keydown', (event) => {
     if (event.key === 'Esc') {
-       console.log('I see you wanna have fun');
+       console.log('I see you wanna have fun')
         }
-    }
+    });
 
 // WHEEL // I don't have a mouse with a wheel so this will be harder for me to test**
 
-var scale = 1;
-const wheel = document.querySelector('div');
+let scale = 1;
+const wheel = document.querySelectorAll('div p');
 wheel.onwheel = zoom;
 
 function zoominclose(event) {
@@ -39,15 +58,19 @@ function zoominclose(event) {
   
 }
 
-
 // LOAD
 
-window.addEventListener('load', function (event) => {
-  console.log('this page is now fully loaded');
-})
+window.addEventListener('load', (event) => {
+  console.log('this page is now fully loaded')
+});
 
-// FOCUS
+// ONLINE 
 
+window.addEventListener('online', (event) => {
+  console.log("You are now connected to the network.");
+});
+
+// FOCUS // this one is also difficult for me to test**
 
 const links = document.querySelector('.nav .nav-link');
 
@@ -60,19 +83,19 @@ links.addEventListener('blur', (event) => {
 });
 
 
-//RESIZE
-const heightOutput = document.querySelector('#height');
-const widthOutput = document.querySelector('#width');
+//RESIZE ATTEMPT
+// const heightOutput = document.querySelector('#height');
+// const widthOutput = document.querySelector('#width');
 
-function reportWindowSize() {
-  heightOutput.textContent = window.innerHeight;
-  widthOutput.textContent = window.innerWidth;
-}
+// function reportWindowSize() {
+//   heightOutput.textContent = window.innerHeight;
+//   widthOutput.textContent = window.innerWidth;
+// }
 
-window.onresize = reportWindowSize;
+// window.onresize = reportWindowSize;
 
-window.addEventListener('resize', reportWindowSize);
-
+// window.addEventListener('resize', reportWindowSize);
+// NOTE THIS REQUIRES ALTERING THE HTML TO CREATE HEIGHT AND WINDOW CLASSES
 
 //SCROLL
 
@@ -92,21 +115,23 @@ window.addEventListener('scroll', function(e) {
 // SELECT
 
 function navSelection(event) {
-  const nav = document.getElementById('.main-navigation');
+  const nav = document.getElementsByClassName('main-navigation');
   const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
   nav.textContent = `You selected: ${selection}`;
 }
 
-const input = document.querySelector('input');
-input.addEventListener('select', logSelection);
+nav.addEventListener('select', navSelection);
 
 // DBLCLICK
 
-const card = document.querySelector('.main-navigation .container nav-container .logo-heading');
+const title = document.querySelector('.main-navigation .container nav-container .logo-heading');
 
-card.addEventListener('dblclick', function (e) {
-  card.classList.toggle('large');
+title.addEventListener('dblclick', function (e) {
+  title.classList.toggle('large');
 });
 
+// DRAG & DROP
+
+//* not attempted yet *//
 
 
