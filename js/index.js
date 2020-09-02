ac0905c8add1e4a15d087270206f4f46ef1483e2
Alexandra Kay <alexandrakay@Alexandras-MacBook-Pro.local>
2020-09-02 17:38:10 -0400

    forgot to fork project and submit it. sorry, was an odff day yesterday. I copied from my unforked version and will submit this now.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,98 @@
 // Your code goes here
+// Your code goes here
+window.addEventListener("load", function(event) {
+    alert("howdy howdy howdy");
+    });
+
+const button1 = document.querySelector('#btn1');
+button1.addEventListener('click', function(event){
+  event.target.style.color = 'blue';
+});
+
+const button2 = document.querySelector('#btn2');
+const doc = document;
+const header = doc.querySelector('.main-navigation');
+
+
+
+header.addEventListener('mouseover', (event) => {
+    event.stopPropagation();
+    event.target.style.fontWeight = 'bold';
+});
+
+button2.addEventListener('mouseover', function(event){
+  event.target.style.background = 'black';
+});
+
+const button3 = document.querySelector('#btn3');
+button3.addEventListener('dblclick', function(event){
+  event.target.style.borderColor = 'red';
+});
+const img1 = document.querySelector('#img1');
+img1.addEventListener('mouseenter', function(event){
+  event.target.style.border = '8px dashed blue';
+});
+
+
+img3.addEventListener('mouseout', function(event){
+  event.target.style.border = 'none';
+});
+
+doc.addEventListener('keydown', (event) => {
+    const keydown = event.key;
+    alert('Do not touch that key! ' + keydown + ' do not do');
+});
+
+const password = document.querySelector('input[type="password"]');
+password.addEventListener('blur', (event) => {
+    password.style.background = 'skyblue';    
+  })
+
+const input = document.querySelector('input[type="text"]');
+input.addEventListener('blur', (event) => {
+    input.style.background = 'lightblue';    
+  })
+
+  document.addEventListener('keydown', function (event){
+    if(event.target === 'ArrowUp')
+    scrollToTop();
+  });
+
+
+  const buttons = document.getElementsByClassName('btn');
+buttons[1].addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'red';
+    event.stopPropagation();
+})
+
+const funBus = document.querySelector('.intro')
+funBus.addEventListener('wheel', function zoom(event) {
+    event.preventDefault;
+    console.log('Number of times you scrolled on this Heading');
+})
+
+
+
+const destination = document.querySelector('.destination')
+const destinationH4 = destination.querySelector('h4')
+
+destinationH4.addEventListener('click', function(event){
+  event.stopPropagation()
+  event.target.style.color = 'green'
+})
+destination.addEventListener('click', function(event){
+  event.target.style.color = 'purple'
+}) 
+const sectionText = document.querySelectorAll('section p')
+
+sectionText.forEach(function(item){
+  item.addEventListener('mouseenter', function(event){
+    event.target.style.backgroundColor = 'grey'
+  })
+})
+
+sectionText.forEach(function(item){
+  item.addEventListener('mouseleave', function(event){
+    event.target.style.backgroundColor = ''
+  })
+})
