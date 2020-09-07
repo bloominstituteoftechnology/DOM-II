3bedb20a2a4d8478d95ced8309e9886b682c37b5
cameronyoung94 <youngcameron0794@gmail.com>
2020-09-07 00:56:21 -0400

    made 6 different event listeners

--- js/index.js
+++ js/index.js
@@ -1,2 +1,33 @@
 // Your code goes here
-j
+const newH1 = document.querySelector("h1")
+    newH1.addEventListener('dblclick', (event) => {
+    event.target.style.color = 'red'
+})
+
+const newNav = document.querySelectorAll(".nav")
+    newNav.forEach(navigation => {
+    navigation.addEventListener('click', (event) => {
+    event.target.style.color = "blue"
+        })
+    })
+
+const newBusIMg = document.querySelector(".intro img")
+    newBusIMg.addEventListener('mouseover', () => {
+        newBusIMg.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSUwIKCooICeQqedLMh3eq_YaS-0SmQwgskTg&usqp=CAU"
+    })
+
+const newH2 = document.querySelector(".intro h2")
+    newH2.addEventListener('mouseenter', () => {
+        newH2.textContent = "PARTY BUS WOOOO"
+    })
+
+const h2LetsGo = document.querySelector(".content-section h2")
+    h2LetsGo.addEventListener('mouseout', () => {
+        h2LetsGo.style.color = "blue"
+    })
+
+const middleImg = document.querySelector(".img-content img")
+    middleImg.addEventListener('contextmenu', () => {
+        middleImg.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQfNikreL2sfwO_bXUSwV-CDu8X129UqWRnEw&usqp=CAU"
+    })
+
