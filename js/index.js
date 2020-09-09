9ca9611eac8562ac1800da442fc756fbc401b3f9
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-09-08 17:34:47 -0700

    8 event handlers

--- js/index.js
+++ js/index.js
@@ -1,13 +1,30 @@
 
 let bus = document.getElementById('bus');
 let busF = true;
+
+// Try to make a sprite image of the funbus in the fixed header
+var img = new Image(40,40);   // Create new img element
+img.addEventListener('load', function(e) {
+  // execute drawImage statements here
+      var canvas  = document.getElementById("funbus");
+      var context = canvas.getContext('2d');
+      img.onload = function (e){
+      context.drawImage(img, 30, 30);
+
+      }
+      img.src = '../img/fun-bus.jpg'; // Set source path 
+      
+}, false);
+
+
+// Double click the fun bus to make it smaller
+
 bus.addEventListener("dblclick", (e) => {
     if(busF){
         e.target.style.width = '50%';
         e.target.style.height = '100%';
         e.target.style.marginLeft = '25%';
         e.target.style.marginRight = '25%';
-    // TODO: Clip bus as a bus sprite and transition it down x cord
         busF= false;
     }else{
         e.target.style.width = '100%';
@@ -22,11 +39,11 @@ bus.addEventListener("dblclick", (e) => {
 
 
 
-// let himg = new Image('200','200');
+// Mouse over the bus to move it after its been double clicked
 let backward = false;
 bus.addEventListener("mouseover", (e) => {
     if(!busF){
-        let start = Date.now();
+        // let start = Date.now();
         e.target.style.margin = '0% 0% 0% 0%';
         e.target.style.animateFillMode = 'backwards';
 
@@ -42,7 +59,8 @@ bus.addEventListener("mouseover", (e) => {
             
             iterations: 1
           })
-
+          
+          busF=true;
        
 
         // if(backward){
@@ -50,10 +68,15 @@ bus.addEventListener("mouseover", (e) => {
         //  e.target.style.transform = 'translateX(100%)';
         //  e.target.style.transition = 'all 3s linear'
         // }
+    }else{
+        e.target.style.width = '100%';
+          e.target.style.height = '100%';
+          e.target.style.marginLeft = '0%';
+          e.target.style.marginRight = '0%';
     }
 });
 
-
+// Click the destination header for a world globe gif
 let selectDst = true;
 let selectDestination = document.querySelector('.content-destination');
 selectDestination.addEventListener('click', (e) => {
@@ -84,6 +107,58 @@ selectDestination.addEventListener('click', (e) => {
     
     
 },false );
+
+// Mouse over the image and rotate it while the pointer is on the image
+//TODO Error Find Out why Now the image appears to be fixed while scrolling down
+let degs = 5;
+let logoMove = document.getElementById('mouseme');
+logoMove.addEventListener('mousemove', e => {
+  logoMove.style.transform = 'rotate('+degs+'deg)';
+  if(degs <= 360){
+    degs = degs + 5;
+  }else{
+    degs = 5;
+  }
+});
+logoMove.addEventListener('mouseleave', e => {
+    logoMove.style.transform = 'rotate(0deg)';
+});
+
+
+// Change all the h2s to red on mouse enter or black on mouse out
+let h2Sel = document.querySelectorAll('h2');
+h2Sel[0].addEventListener('mouseenter', (e) =>{
+    e.target.style.color = 'red';
+});
+
+h2Sel[0].addEventListener('mouseout', (e) =>{
+    e.target.style.color  = 'black';
+});
+
+h2Sel[1].addEventListener('mouseenter', (e) =>{
+    e.target.style.color = 'red';
+});
+
+h2Sel[1].addEventListener('mouseout', (e) =>{
+    e.target.style.color  = 'black';
+});
+
+
+h2Sel[2].addEventListener('mouseenter', (e) =>{
+    e.target.style.color = 'red';
+});
+
+h2Sel[2].addEventListener('mouseout', (e) =>{
+    e.target.style.color  = 'black';
+});
+
+h2Sel[3].addEventListener('mouseenter', (e) =>{
+    e.target.style.color = 'red';
+});
+
+h2Sel[3].addEventListener('mouseout', (e) =>{
+    e.target.style.color  = 'black';
+});
 
 
 
