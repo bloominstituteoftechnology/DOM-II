248724b57e2de5d29b8da196e77d11e589e744c4
Brianna Larkins <blarkins123@gmail.com>
2020-09-08 20:11:06 -0700

    all 10 done

--- js/index.js
+++ js/index.js
@@ -11,6 +11,15 @@ allImg.forEach(item => {
 
   })
 
+  let img = document.querySelector('img')
+  img.addEventListener('mouseenter', e => {
+      img.src = "https://media0.giphy.com/media/3BMsdRXVELShWzUcXj/giphy.gif"
+  })
+  
+  img.addEventListener('drag', e => {
+      img.src = "https://frogblogmanchester.files.wordpress.com/2014/04/agalychnis-annae.jpg"
+  })
+
   const NavLink = document.querySelector("nav");
  console.log(NavLink)
  NavLink.addEventListener("mouseover", function( event ) {   
@@ -19,8 +28,7 @@ allImg.forEach(item => {
     setTimeout(function() {
       event.target.style.color = "";
     }, 500);
-  }, false);
-
+  }, false)
 
 document.addEventListener('keydown', (e) => {
   if(e.keyCode === 27 || e.key === 'Backspace'){
@@ -28,13 +36,24 @@ document.addEventListener('keydown', (e) => {
   }
 }) 
 
+const h1 = document.querySelector('h1')
+h1.addEventListener("click", event => {
+  event.target.textContent = "The Ocean Disappers";
+
+});
+h1.addEventListener("dblclick", event => {
+  event.target.textContent = "When the moonlight fades";
+  event.preventDefault();
+  event.stopPropagation();
+});
+
 const p = document.querySelectorAll('p')
 window.addEventListener('load',() => {
     setTimeout(function(){
         p.forEach(instance => {
             instance.textContent = 'Its never too late!'
         })
-        },5000)
+      },5000)
 })
 
 window.addEventListener('resize', () => {
@@ -53,4 +72,5 @@ document.addEventListener('wheel', (event) => {
    document.body.style.backgroundColor = 'yellow';
    if(event.deltaY<0)
    document.body.style.backgroundColor = 'blue';
-  })
+})
+
