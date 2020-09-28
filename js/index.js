918bedb79c9b45b03cd56cf2382503ec78f3ea89
Christopher Girvin <bugbountychrisg8691@gmail.com>
2020-09-28 15:00:52 -0400

    done up to/including resize

--- js/index.js
+++ js/index.js
@@ -1,1 +1,60 @@
 // Your code goes here
+
+// mouseover
+const nav = document.querySelector("nav");
+Array.from(nav.children).forEach(child => {
+    child.addEventListener("mouseover", function() {
+        child.style.color = "blue";
+        child.style.transform = "scale(1.2)"
+    })
+})
+
+// keydown
+document.addEventListener("keydown", (e) => {
+    const footer = document.querySelector("footer");
+    const spanKeyDown = document.createElement("span");
+    spanKeyDown.textContent = e.keyCode;
+    footer.appendChild(spanKeyDown);
+});
+
+// wheel
+function zoom(e) {
+    e.preventDefault();
+    scale += e.deltaY * -0.01;
+
+    scale = Math.min(Math.max(.125, scale), 4);
+
+    heading.style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+const heading = document.querySelector(".nav-container");
+heading.onwheel = zoom;
+
+// load
+const busImgContainer = document.querySelector(".intro");
+busImgContainer.children[0].addEventListener("load", function() {
+    busImgContainer.children[0].style.transform = "scale(0.6)";
+})
+
+// focus
+const inputEl = document.createElement("input");
+inputEl.value = "Insert Text";
+const home = document.querySelector(".home");
+home.appendChild(inputEl);
+function foc(e) {
+    e.target.style.background = "yellow";
+}
+const inputElAdded = document.querySelector(".home").children[document.querySelector(".home").children.length - 1].addEventListener("focus", foc);
+
+const resizeLog = document.createElement("div");
+const heightLog = document.createElement("p");
+const widthLog = document.createElement("p");
+resizeLog.appendChild(heightLog);
+resizeLog.appendChild(widthLog);
+home.appendChild(resizeLog);
+
+window.onresize = function() {
+    heightLog.textContent = window.innerHeight;
+    widthLog.textContent = window.innerWidth;
+}
