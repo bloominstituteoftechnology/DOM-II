bb3ff867d005820c04a7afdd38241bfce776e005
NDCT94 <nathan.dumas01@gmail.com>
2020-09-29 15:55:05 -0400

    about halfway to mvp

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-// Your code goes here
+// Disabling nav
+document.querySelectorAll('a').forEach(item => item.addEventListener('click', e => {
+    e.preventDefault()
+}))
+
+
+// Click
+document.querySelector('h1').addEventListener('click', e => {
+    e.target.innerHTML = 'Sad Bus'
+    e.target.style.color = 'orange'
+})
+
+// Mouseover
+document.querySelectorAll('p').forEach(item => item.addEventListener('mouseover', e => {
+    e.target.style.color = 'pink'
+}))
+
+// Mouseout
+document.querySelectorAll('p').forEach(item => item.addEventListener('mouseout', e => {
+    e.target.style.color = 'black'
+}))
