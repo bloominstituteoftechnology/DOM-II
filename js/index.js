3c186cef94c3b001dde5b1b03547767199960433
NDCT94 <nathan.dumas01@gmail.com>
2020-09-29 17:23:35 -0400

    mvp

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 // Disabling nav
-document.querySelectorAll('a').forEach(item => item.addEventListener('click', e => {
+const navLinks = document.querySelectorAll('a')
+navLinks.forEach(item => item.addEventListener('click', e => {
     e.preventDefault()
 }))
 
-
 // Click
-document.querySelector('h1').addEventListener('click', e => {
-    e.target.innerHTML = 'Sad Bus'
+const h1 = document.querySelector('h1')
+h1.addEventListener('click', e => {
+    e.target.innerHTML = 'Sad Bus' 
     e.target.style.color = 'orange'
+})
+
+// dblclick
+h1.addEventListener('dblclick', e => {
+    e.target.innerHTML = 'Fun Bus'
+    e.target.style.color = 'Black'
 })
 
 // Mouseover
@@ -19,3 +26,33 @@ document.querySelectorAll('p').forEach(item => item.addEventListener('mouseover'
 document.querySelectorAll('p').forEach(item => item.addEventListener('mouseout', e => {
     e.target.style.color = 'black'
 }))
+
+// Keydown
+document.body.addEventListener('keydown', e => {
+    e.target.style.background = 'grey'
+})
+
+// Scroll
+window.addEventListener('scroll', e => {
+    document.body.style.background = '#d0bdf4'
+})
+
+// Focus
+window.addEventListener('focus', e => {
+    document.querySelector('footer').style.background = 'royalblue'
+})
+
+// Load
+window.addEventListener('load', e => {
+    alert('Welcome')
+})
+
+// Resize
+window.addEventListener('resize', e => {
+    document.querySelector('header').style.background = 'purple'
+})
+
+// Wheel
+window.addEventListener('wheel', e => {
+    document.querySelectorAll('h2').forEach(item => item.style.color = 'gold')
+})
