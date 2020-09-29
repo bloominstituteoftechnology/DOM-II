6b67036efe33979522de4f7d280df776ad814fd3
seth-bradshaw <seth.bradshaw27@gmail.com>
2020-09-29 13:59:16 -0600

    Targeted the 10 elements successfully

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-// Your code goes here
+//selecting following elements from DOM
+//h1
+const h1 = document.querySelector('h1')
+//h2's
+const h2 = document.querySelectorAll('h2')
+//h4
+const h4 = document.querySelector('h4')
+//nav
+const navLinks = document.querySelector('nav')
+//img's
+const busImg = document.querySelector('.intro img')
+const mapImg = document.querySelector('.img-content img')
+const boatImg1 = document.querySelector('.inverse-content img')
+const boatImg2 = document.querySelector('.content-destination img')
+//P's
+const p = document.querySelectorAll('p')
+const footerP = document.querySelector('footer p')
+
