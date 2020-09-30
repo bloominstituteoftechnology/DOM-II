efd9e5034a6e13f1134e37b812bd583730fe4e43
seth-bradshaw <seth.bradshaw27@gmail.com>
2020-09-29 20:55:07 -0600

    Finished assignment

--- js/index.js
+++ js/index.js
@@ -4,17 +4,17 @@ const h1 = document.querySelector('h1')//done
 //h2's
 const h2 = document.querySelector('h2')//done
 //h4
-const h4 = document.querySelector('h4')
+const h4 = document.querySelector('h4')//done
 //nav
 const navLinks = document.querySelector('.nav')//done
 //img's
 const busImg = document.querySelector('.intro')//done
-const mapImg = document.querySelector('.img-content img')
-const boatImg1 = document.querySelector('.inverse-content img')
-const boatImg2 = document.querySelector('.content-destination img')
+const mapImg = document.querySelector('.img-content')//done
+const boatImg1 = document.querySelector('.inverse-content')//done
+const boatImg2 = document.querySelector('.content-destination')//done
 //P's
 const p = document.querySelector('.container .intro p')//done
-const footerP = document.querySelector('footer p')
+const footer = document.querySelector('footer')
 
 h1.addEventListener('click', event => {
     h1.style.color = 'yellow'
@@ -24,9 +24,8 @@ document.addEventListener('keyup', event => {
     h2.style.color = 'blue'  
 })
 
-p.addEventListener('mouseover', (event) => {
-    console.log('test')
-    p.style.background = 'pink'
+h4.addEventListener('mousemove', event => {
+    h4.style.background = 'purple'
 })
 
 navLinks.addEventListener('keydown', event => {
@@ -40,6 +39,21 @@ busImg.addEventListener('dblclick', event => {
 })
 
 mapImg.addEventListener('mousedown', event => {
-
+    mapImg.style.background = 'red'
 })
-.
+
+boatImg1.addEventListener('wheel', event => {
+    boatImg1.style.background = 'cyan'
+})
+
+document.addEventListener('keypress', event => {
+    boatImg2.style.background = 'red'
+})
+
+p.addEventListener('mouseover', (event) => {
+    p.style.background = 'pink'
+})
+
+document.addEventListener('mouseout', event => {
+    footer.style.background = 'brown'
+})
