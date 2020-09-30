9cdc3985158054ac928161c0de28c3f009a8dc94
justin-jones <52481185+justinemanueljones@users.noreply.github.com>
2020-09-29 21:13:20 -0400

    mvp w notes

--- README.md
+++ README.md
@@ -27,16 +27,16 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 ### Task 2a: Create listeners for 10 types of events
 
 * [ ] Using your [index.js file](js/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, remove objects, etc. Here are some event types you could try to use:
-  * `mouseover`
-  * `keydown`
+  <!-- * `mouseover` -->
+  <!-- * `keydown` -->
   * `wheel`
-  * `load`
-  * `focus`
-  * `resize`
+  <!-- * `load` -->
+  <!-- * `focus` -->
+  <!-- * `resize` -->
   * `scroll`
   * `select`
-  * `dblclick`
-  * `drag / drop`
+  <!-- * `dblclick` -->
+  <!-- * `drag / drop` -->
 
 Note: Drag and drop is a bit more advanced than the others: it's not actually a single type of event but several types that need to work together.
 
--- js/index.js
+++ js/index.js
@@ -1,1 +1,61 @@
 // Your code goes here
+// This handler will be executed only once when the cursor
+// moves over the unordered list
+const h1 = document.querySelector('h1');
+h1.onmouseover = logMouseOver;
+h1.onmouseout = logMouseOut;
+
+
+//Mouse Over
+function logMouseOver() {
+  h1.innerHTML = 'mouse over detected(1)';
+}
+
+
+//Mouse Out
+function logMouseOut() {
+  h1.innerHTML = 'mouse out detected(2)';
+}
+
+//Keydown & Logs Key
+const input = document.querySelector('input');
+const log = document.getElementById('log');
+
+input.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  log.textContent += ` ${e.code}`;
+}
+
+//dblclick
+const card = document.querySelector('aside');
+
+card.addEventListener('dblclick', function (e) {
+  card.classList.toggle('large');
+});     
+
+// resize
+const heightOutput = document.querySelector('#height');
+const widthOutput = document.querySelector('#width');
+
+function reportWindowSize() {
+  heightOutput.textContent = window.innerHeight;
+  widthOutput.textContent = window.innerWidth;
+}
+
+window.onresize = reportWindowSize;
+
+
+//obload
+window.onload = (e) => {
+  console.log('page is fully loaded');
+};
+
+
+function getEventType(event) {
+  const log = document.getElementById('log');
+  log.innerText = event.type + '\n' + log.innerText;
+}
+
+// Mouse events
+document.addEventListener('click', getEventType, false);    
