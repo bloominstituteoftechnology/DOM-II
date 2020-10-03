42bcbbd2a21e68fbcf004aa304dbd4d0f971cd9c
Jonah D. Hadlock <jonah-hadlock@lambdastudents.com>
2020-10-02 18:36:30 -0600

    I now have 4 done

--- js/index.js
+++ js/index.js
@@ -1,17 +1,25 @@
 // Your code goes here
 // 1 KEY DOWN
 document.addEventListener('keydown', function(event) {
-    window.alert("The page is fully loaded! Enter at your own risk....");
+    if (event.keyCode === 72 /*H*/ ) {
+        window.alert("The page is fully loaded! Enter at your own risk....");
+    } else(window.alert("You don't know which key to press; you need an education! Didn't your mother raise you right?!"));
 });
 
-// 2 CLICK COUNTER
+//2 MOUSEUP 
+const mouse1 = document.querySelector('.intro');
+mouse1.addEventListener('mouseup', function() {
+    window.alert('You discovered the intro. You get a gold star!');
+})
+
+// 3 CLICK COUNTER
 const clickEvent = document.querySelector('.btn');
 
 clickEvent.addEventListener('click', event => {
     clickEvent.innerHTML = `Click count: ${event.detail}`;
 });
 
-// 3 MOUSEENTER/LEAVE FUN BUS
+// 4 MOUSEENTER/LEAVE FUN BUS
 const logoHeader = document.querySelector('.logo-heading');
 logoHeader.addEventListener('mouseenter', function() {
     logoHeader.style.color = "#ed246a";
