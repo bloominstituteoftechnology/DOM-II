95d3df77633d46aa883865e7f6b75bf772142c71
Jonah D. Hadlock <64397503+jonahdhadlock@users.noreply.github.com>
2020-10-02 18:57:04 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Your code goes here
-// 1 KEY DOWN
+// 1-2 KEY DOWN
 document.addEventListener('keydown', function(event) {
     if (event.keyCode === 72 /*H*/ ) {
         window.alert("The page is fully loaded! Enter at your own risk....");
     } else(window.alert("You don't know which key to press; you need an education! Didn't your mother raise you right?!"));
 });
 
-//2 MOUSEUP 
+//3-4 MOUSEUP 
 const mouse1 = document.querySelector('.intro');
 mouse1.addEventListener('mouseup', function() {
     window.alert('You discovered the intro. You get a gold star!');
 })
 
-// 3 CLICK COUNTER
+// 5 CLICK COUNTER
 const clickEvent = document.querySelector('.btn');
 
 clickEvent.addEventListener('click', event => {
     clickEvent.innerHTML = `Click count: ${event.detail}`;
 });
 
-// 4 MOUSEENTER/LEAVE FUN BUS
+// 6-7 MOUSEENTER/LEAVE FUN BUS
 const logoHeader = document.querySelector('.logo-heading');
 logoHeader.addEventListener('mouseenter', function() {
     logoHeader.style.color = "#ed246a";
@@ -27,3 +27,5 @@ logoHeader.addEventListener('mouseenter', function() {
         logoHeader.style.color = "black";
     });
 });
+
+// 8 ERROR
