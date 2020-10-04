7b056a6353b992010e1bcb5fa443f15723e95a02
Greyson <greyson.r.hamilton@gmail.com>
2020-10-03 22:53:27 -0400

    codetest

--- js/index.js
+++ js/index.js
@@ -1,14 +1,15 @@
 // Your code goes here
-
+// bubbles
 const mouseOn = document.querySelectorAll('nav').forEach(a => {
 nav.addEventListener("mouseover", (e) => {
     e.style.transform = scale("2.0");
     e.style.textDecoration = "none";
 });
 
+// bubbles
 const keyCode = document.getElementByName('body');
 keyCode.addEventListener("keydown", (e) => {
-    if (e.isComposing || e.keyCode === help) {
+    if (e.isComposing || e.keyCode === 32) {
     return;
 }
     keyCode.style.backgroundColor = 'honey';
@@ -16,6 +17,7 @@ keyCode.addEventListener("keydown", (e) => {
 
 let scale = 1;
 
+// bubbles
 const wheel = document.getElementsByClassName("img-content");
 wheel.addEventListener("wheel", (e) => {
     if (e.deltaY < 0) {
@@ -27,10 +29,43 @@ wheel.addEventListener("wheel", (e) => {
     wheel.style.transform = `scale(${scale})`;
 });
 
-const load = 
-// focus
-// resize
-// scroll
-// select
-// dblclick
-// drag, drop
+// no bubbles
+const load = document.querySelector('footer p');
+load.addEventListener('DOMContentLoaded', (e) => {
+load.style.textAlign = "center";
+});
+
+// no bubbles
+const focus = document.querySelector('.text-content p');
+focus.addEventListener('focus', (e) => {
+
+});
+
+// no bubbles
+let i = 0;
+const resize = document.querySelector('footer');
+resize.addEventListener('resize', (e) => {
+    resize.append(document.createElement('p').textContent = `This window has been resized ${change} times`);
+    let change = i += 1;
+});
+
+
+const scroll = document.getElementByName('body');
+scroll.addEventListener('scroll', (e) => {
+scroll.style.backgroundColor = "black" ? "white" : "black";
+});
+
+
+const select = document.getElementByName('h1');
+select.addEventListener('select', (e) => {
+    select.style.textContent = "This is the fun-vee!"
+});
+
+// bubbles
+const dblclick = document.querySelector('.logo-heading');
+dblclick.addEventListener('dblclick', (e) => { 
+dblclick.style.border = "5px dashed #85e085"
+});
+
+
+// dragDrop
