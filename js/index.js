b2e21ae9f992698d00788e93bf4bd77d6be5cb31
Allan <allanoliveira.dev@gmail.com>
2020-10-04 02:55:51 -0300

    completed assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
-// Your code goes here
+// click
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('click', () => logoHeading.style.color = 'red');
+
+// dblclick
+const footer = document.querySelector('.footer p');
+footer.addEventListener('dblclick', () => footer.style.fontSize = '30px');
+
+// mouseover 
+const busImage = document.querySelector('.intro img');
+busImage.addEventListener('mouseover', () => busImage.style.border = '3px solid black');
+
+// mouseleave
+busImage.addEventListener('mouseleave', () => busImage.style.border = 'none');
+
+// keydown
+document.addEventListener('keydown', () => document.body.style.backgroundColor = 'blue');
+
+// keyup
+document.addEventListener('keyup', () => document.body.style.backgroundColor = 'white');
+
+// wheel
+const colors = ['red', 'blue', 'brown', 'yellow', 'orange', 'purple'];
+const random = Math.round(Math.random() * 5);
+const body = document.querySelector('body');
+body.addEventListener('wheel', () => body.style.backgroundColor = colors[random]);
+
+// load
+const navLinks = document.querySelectorAll('.nav a');
+window.addEventListener('load', () => navLinks.forEach(nav => nav.style.color = 'green'));
+
+// focus
+const input = document.querySelector('.focus');
+const img = document.querySelectorAll('img');
+input.addEventListener('focus', () => img.forEach(el => el.style.border = '3px solid blue'));
+
+// select
+const select = document.querySelector('.select');
+const output = document.querySelector('.output');
+output.style.fontSize = "40px";
+select.addEventListener('select', (event) => output.textContent = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd))
