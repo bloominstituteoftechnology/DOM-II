292325432ec76963b413e1b504f678ea71d480ae
halliwell2046 <halliwell2046@yahoo.com>
2020-10-04 13:20:23 -0400

    stretch move

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -8,4 +8,15 @@ const moveToTop = (e) => {
 
 rocket.forEach(block => {
     block.addEventListener('click', moveToTop)
+  
+  const right = (e) => {
+            e.target.style = 'transition: margin-left, 4s; margin-left: 1000px';
+        }
+
+        const left = (e) => {
+            e.target.style = 'transition: margin-right, 4s; margin-right: 8000px';
+        }
+
+        block.addEventListener('mousedown', right);
+        block.addEventListener('mouseup', left);
 });
