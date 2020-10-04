3430dcc692f5b5f1cbf1e1c608ad1ad3f1238afc
Allan <allanoliveira.dev@gmail.com>
2020-10-03 22:27:31 -0300

    created mouseover event listener

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@ const logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener('click', () => logoHeading.style.color = 'red');
 
 // dblclick
-
 const footer = document.querySelector('.footer p');
 footer.addEventListener('dblclick', () => footer.style.fontSize = '30px');
+
+// mouseover 
+const busImage = document.querySelector('.intro img');
+busImage.addEventListener('mouseover', () => busImage.style.border = '3px solid black');
