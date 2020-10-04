191b50aa837dd0cbe9e1097a50ca9984819bf5b8
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-04 16:47:26 -0400

    setup the JS page

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
-// Your code goes here
+// Ten different types of event listeners. Examples: mouseover, keydown, wheel, load, focus, resize, scroll, select, dblclick, drag / drop.
+// Then, nest two similar events somewhere in the site and prevent the event propagation properly. ((not all events bubble!))
+// Stop navigation items from refreshing the page by using preventDefault().
+
+//1
+
+//2
+
+//3
+
+//4
+
+//5
+
+//6
+
+//7
+
+//8
+
+//9
+
+//10
