63ab06af399365a8210d72f7d253716939e21514
Allan <allanoliveira.dev@gmail.com>
2020-10-03 22:24:27 -0300

    created dblclick event listner

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
 // click
 const logoHeading = document.querySelector('.logo-heading');
 logoHeading.addEventListener('click', () => logoHeading.style.color = 'red');
+
+// dblclick
+
+const footer = document.querySelector('.footer p');
+footer.addEventListener('dblclick', () => footer.style.fontSize = '30px');
