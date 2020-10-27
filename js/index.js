0c9ea4bb9291407a5c66321873825652f54adbb9
DavidHall-Code <david_m_hall@outlook.com>
2020-10-27 17:53:41 -0400

    Worked on focus, scroll and mouse over.

--- js/index.js
+++ js/index.js
@@ -38,10 +38,95 @@ window.addEventListener('keydown', (event) => {
         document.body.style.visibility = 'visible'
     }
 
-    if (event.key == 'w') {
+        console.log(event.key)
+})
+
+// Change the body color when a key is released
+window.addEventListener('keyup', (event) => {
+    if (event.key == 'a') {
         document.body.style.background = 'white'
         document.body.style.visibility = 'visible'
     }
 
+    if (event.key == 'o') {
+        document.body.style.background = 'white'
+        document.body.style.visibility = 'visible'
+    }
+
+    if (event.key == 'r') {
+        document.body.style.background = 'white'
+        document.body.style.visibility = 'visible'
+    }
+
+    if (event.key == 'y') {
+        document.body.style.background = 'white'
+        document.body.style.visibility = 'visible'
+    }
+
+    if (event.key == 'b') {
+        document.body.style.background = 'white'
+        document.body.style.visibility = 'visible'
+    }
+
+   
     console.log(event.key)
 })
+
+const navigation = document.querySelectorAll('.nav-link');
+navigation[2].href = "https://www.w3schools.com/cssref/css_colors.asp";
+navigation[2].addEventListener('click', (event) => {
+    event.preventDefault();
+});
+
+navigation.forEach((element) => {
+    element.style.padding = "1.5rem";
+    element.style.textDecoration = "none";
+});
+
+const navBar = document.querySelector('.nav');
+navBar.style.padding = "3rem";
+navBar.style.displayFlex = "flex";
+navBar.style.justifyContent = "space-between";
+navBar.style.alignItems = "space-between";
+navigation.forEach((element) => {
+    element.style.fontSize = "1.3rem";
+    element.style.color = "black";
+    
+});
+
+//console.log(navBar);
+navigation.forEach((element) => element.addEventListener('mouseover', (event) => {
+    element.style.transform = "scale(2)";
+    element.style.background = "red";
+    element.style.color = "goldenrod";
+    
+}));
+
+navigation.forEach((element) => {
+    element.addEventListener('mouseout', (event) => {
+        element.style.transform = "none";
+        element.style.background = "white";
+        element.style.color = "#00BFFF";
+        
+    })
+});
+
+// Focus Event
+window.addEventListener('focus', (event) => {
+    console.log('Our minds are one you can focus')
+})
+
+// Scroll Event
+window.addEventListener('scroll', (event) => {
+    //alert('You are scrolling the page')
+    console.log('You found the mouse wheel')
+})
+
+//Mouseover Event
+window.addEventListener('mouseover', (event) => {
+    document.h2.style.color = 'blue'
+})
+
+
+//WHEEL
+
