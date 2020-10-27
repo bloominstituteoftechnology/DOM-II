04e11a820a9b9799f6cb4899e09aa92a6a3309ba
Diego Roman <diegormnv209@gmail.com>
2020-10-27 13:37:07 -0700

    used mouseover and mouse out on nav to change size and color

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const nav = document.querySelector('.nav');
+nav.addEventListener('mouseover', (event) => {
+    event.target.style.fontSize= 'x-large';
+    event.target.style.color= '#17A2B8';
+}); 
+nav.addEventListener('mouseout',(event) => {
+    event.target.style = 'initial';
+
+}); 
