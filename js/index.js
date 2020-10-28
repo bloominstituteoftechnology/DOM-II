78e41290b8af221a554d65a452c1c81c073a8bf4
Justin Justin <jbry0723@gmail.com>
2020-10-28 08:54:33 -0400

    finished assignement

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // Your code goes here
 let topBar=document.querySelector(".main-navigation")
-let navButton=document.querySelectorAll(".nav-link")
+
 
 
 topBar.addEventListener("mouseover",(event)=>{
     topBar.classList.add("topMouseOver")
-    navButton.classList.add("topMouseOver")
+    
     
 })
 
@@ -13,12 +13,83 @@ topBar.addEventListener("mouseout",(event)=>{
     topBar.classList.remove("topMouseOver")})
 
 
+let dBody=document.querySelector("body")
 
-document.body.addEventListener("wheel",(event)=>{
-document.body.style.backgroundColor="red"
+function scrollFunction(event){
+    dBody.classList.add("rBody")
+    setInterval(()=>dBody.classList.toggle("gBody"),1000);
+    dBody.removeEventListener("wheel",(scrollFunction))
+}
 
-})
+
+dBody.addEventListener("wheel",(scrollFunction))
 
 document.body.addEventListener("keydown",(event)=>{
     document.body.style.color="green"
+})
+
+let navLinks=document.querySelectorAll(".nav-link")
+
+Array.from(navLinks).forEach(function (link){
+    
+    link.addEventListener("dblclick", function (event){
+        
+        event.target.style.fontSize="5rem"
+        
+    })
+})
+
+
+
+
+dBody.addEventListener("click", (event)=>{
+    dBody.style.color="red"
+})
+
+topBar.addEventListener("click", function(event){
+    event.stopImmediatePropagation()
+})
+
+window=document.querySelector("window")
+
+window.addEventListener("resize",(event)=>{
+    dBody.style.color="black"
+})
+let busWords=document.querySelector(".logo-heading")
+let busPic=document.querySelector(".busPic")
+
+busPic.addEventListener("click",(event)=>{
+    busWords.style.fontSize="+22rem"
+   
+})
+
+let footer=document.querySelector(".footer")
+
+footer.classList.add("rBody")
+
+window.addEventListener("mousemove",(event)=>{
+    footer.classList.toggle("gBody")
+})
+
+let welcome=document.querySelector(".intro h2")
+
+welcome.addEventListener("copy",(event)=>{
+    busWords.style.fontSize="+6rem"
+})
+
+let footerp=document.querySelector(".footer p")
+
+
+footerp.addEventListener("cut",(event)=>{
+    busWords.style.fontSize="+9rem"
+})
+
+let sunButton=document.querySelector(".destination .btn")
+
+sunButton.addEventListener("contextmenu",(event)=>{
+    sunButton.style.backgroundColor="purple"
+})
+
+window.addEventListener("keyup",(event)=>{
+    dBody.style.color="red"
 })
