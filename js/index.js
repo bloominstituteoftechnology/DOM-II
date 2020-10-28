f9773c049f0dce888499bfee963d6f052121c5b9
Justin Justin <jbry0723@gmail.com>
2020-10-27 23:42:45 -0400

    Going to be late

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+let topBar=document.querySelector(".main-navigation")
+let navButton=document.querySelectorAll(".nav-link")
+
+
+topBar.addEventListener("mouseover",(event)=>{
+    topBar.classList.add("topMouseOver")
+    navButton.classList.add("topMouseOver")
+    
+})
+
+topBar.addEventListener("mouseout",(event)=>{
+    topBar.classList.remove("topMouseOver")})
+
+
+
+document.body.addEventListener("wheel",(event)=>{
+document.body.style.backgroundColor="red"
+
+})
+
+document.body.addEventListener("keydown",(event)=>{
+    document.body.style.color="green"
+})
