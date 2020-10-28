1464fa5ed1f4ef24dd8485ef657581dea8a22ec6
cmirza <mail@cmirza.com>
2020-10-27 17:19:07 -0700

    MVP Complete

--- js/index.js
+++ js/index.js
@@ -58,3 +58,32 @@ window.addEventListener('keydown', () => {
     alert('Don\'t do that!');
 });
 
+// (8) Console out when scrolling in main navigation, use preventDefault() to stop scrolling
+const scrollNav = document.querySelector('.main-navigation')
+scrollNav.addEventListener('wheel', (event) => {
+    console.log('SCROLL!');
+    event.preventDefault();
+});
+
+// (9) Set background to green when resize, set back to white when done
+const resizeBody = document.querySelector('body');
+let resizeTimer;
+window.addEventListener('resize', () => {
+    resizeBody.style.backgroundColor = 'green';
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+        resizeBody.style.backgroundColor = 'white';
+    }, 250);
+});
+
+// (10) Double click on content destination image to scale, use stopPropagation to prevent nested function from scaling parent element
+const destination = document.querySelector('.content-destination ');
+const destinationImage = document.querySelector('.content-destination img');
+destinationImage.addEventListener('dblclick', (event) => {
+    destinationImage.style.transform = 'scale(1.5)';
+    event.stopPropagation();
+    destination.addEventListener('dblclick', () => {
+        destination.style.transform = 'scale(1.5)';
+        event.stopPropagation();
+    });
+});
