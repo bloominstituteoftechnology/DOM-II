fa58ec5ec775ba6efca76a1c42de4f746a216fb5
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2020-10-30 17:33:06 -0600

    Up to eight unique events and did the stop propagation part

--- js/index.js
+++ js/index.js
@@ -11,3 +11,23 @@ const Welcome = document.querySelector('.intro h2')
 Welcome.addEventListener('dblclick', e => e.target.style.color = 'red')
 Welcome.addEventListener('dblclick', e => e.target.style.fontStyle = 'italic')
 
+const Html = document.querySelector('header')
+Html.addEventListener('wheel', e => e.target.style.backgroundColor = 'blue')
+
+const Pic = document.querySelector('.content-destination img')
+Pic.addEventListener('contextmenu', e => e.target.style.marginTop = '15rem')
+
+const Foot = document.querySelector('.footer')
+Foot.addEventListener('mousedown', e => e.target.style.backgroundColor = 'purple')
+Foot.addEventListener('mouseup', e => e.target.style.backgroundColor = 'orange')
+
+
+ 
+// stop Propagation
+const LetsGo = document.querySelector('.content-section')
+LetsGo.addEventListener('mouseover', e => e.target.style.border = '5px solid orange')
+
+const map = document.querySelector('.content-section img')
+map.addEventListener('mouseover', e => e.target.style.border = '3px dashed red')
+map.addEventListener('mouseover', e => e.stopPropagation())
+
