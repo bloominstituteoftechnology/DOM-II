cbda3da2ac7d547622401384198fefbd2e8fdbb9
Mitchell Knudsen <mitch@Mitchells-MacBook-Pro.local>
2020-10-30 00:49:04 -0600

    Made event listeners for three elements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+
+const Bus = document.querySelector('.intro img')
+Bus.addEventListener('click', e => alert('Hey! Don\'t touch the bus!'))
+
+const Fun = document.querySelector('.logo-heading')
+Fun.addEventListener('mouseover', e => e.target.style.fontSize = '6rem')
+Fun.addEventListener('mouseleave', e => e.target.style.fontSize = '3rem')
+
+const Welcome = document.querySelector('.intro h2')
+Welcome.addEventListener('dblclick', e => e.target.style.color = 'red')
+Welcome.addEventListener('dblclick', e => e.target.style.fontStyle = 'italic')
+
