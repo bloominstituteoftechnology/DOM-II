6d366b3f2cf4d4a725ef9126099c7c58b820cd34
Pauline Haren <pharen07@gmail.com>
2020-10-31 20:52:24 -0500

    add click, mousemove, onload, and copy events--and add stop prop on footer elements--and add preventdefault for nav links

--- README.md
+++ README.md
@@ -31,17 +31,17 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
   * [x] `keydown`
   * [x] `mouseleave`
   * [x] `dblclick`
-  * [] `focus`
-  * [] `resize`
-  * [] `scroll`
-  * [] `select`
-  * [] `dblclick`
-  * [] `drag / drop`
+  * [x] `click`
+  * [x] `resize`
+  * [x] `scroll`
+  * [x] `mousemove`
+  * [x] `onload`
+  * [x] `copy`
 
 Note: Drag and drop is a bit more advanced than the others: it's not actually a single type of event but several types that need to work together.
 
-* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
-* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+* [x] Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
+* [x] Stop the navigation items from refreshing the page by using `preventDefault()`
 
 
 
--- js/index.js
+++ js/index.js
@@ -24,13 +24,56 @@ const headerIMG = document.querySelector('.intro img')
     })
 
 // Window Resize Background Color
-let background = document.querySelector('body')
+const background = document.querySelector('body')
 window.addEventListener('resize', () => {
     background.style.backgroundColor = "#89cff0"
 })
 
 // Scroll Background Color Nav Bar
-let navBar = document.querySelector('.main-navigation')
+const navBar = document.querySelector('.main-navigation')
 window.addEventListener('scroll', () => {
     navBar.style.backgroundColor = '#89cff0'
+})
+
+// H2 Click
+const h2 = document.querySelector('.intro h2')
+h2.addEventListener('click', () => {
+    h2.style.color = 'purple'
+    h2.style.fontSize = '50px'
+})
+
+// Intro Paragraph Mousemove
+const intro = document.querySelector('.intro p')
+intro.addEventListener('mousemove', () => {
+    intro.style.fontSize = '30px'
+})
+
+// Window Onload
+window.onload = function() {
+    alert('Welcome to the site')
+}
+
+// No Copy
+background.addEventListener('copy', () => {
+    alert('Thanks for wanting to save that piece of info.')
+})
+
+//Footer Stop Propagation
+const div = document.querySelector('.footer div')
+div.addEventListener('click', () => {
+    div.style.backgroundColor = 'purple'
+})
+
+const copyright = document.querySelector('.footer p')
+copyright.addEventListener('click', () => {
+    copyright.style.color = '#ffd800'
+    event.stopPropagation()
+})
+
+// Nav Prevent Default
+const navLinks = document.querySelectorAll('.nav-link')
+navLinks.forEach(navigation => {
+    navigation.addEventListener('click', (event) => {
+        event.preventDefault()
+    })
 })
