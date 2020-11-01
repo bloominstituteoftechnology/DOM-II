d1a5bc2cba13b2debd1b8181d31493256c998a35
travis-mcminimy <tmcminimy@gmail.com>
2020-11-01 12:47:25 -0800

    ugh

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+const introPic = document.querySelector('.intro img');
+const navLinks = document.querySelectorAll('nav a');
+const signupButtons = document.querySelectorAll('.btn');
+const sectionHeaders = document.querySelectorAll('.text-content h2');
+const footer = document.querySelector('.footer')
+Array.from(signupButtons).forEach((event) => {
+    event.addEventListener("mouseover", (event) => {
+        event.target.style.fontWeight = "900"
+    });
+    event.addEventListener('mouseout', (event) => {
+        event.target.style.fontWeight = "400"
+    });
+});
+
+Array.from(sectionHeaders).forEach((event) => {
+    event.addEventListener("mouseover", (event) => {
+        event.target.style.color = "blue"
+    });
+    event.addEventListener('mouseout', (event) => {
+        event.target.style.color = "black"
+    });
+});
+
+Array.from(navLinks).forEach((item) => {
+    item.addEventListener('dblclick', (event) => {
+        event.target.style.color = "pink"
+    });
+});
