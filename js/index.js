1eea0a41e7a539b4250f5d0a8ffe896d1c60c2df
Louis Stevens <loustevenshere@gmail.com>
2020-12-01 12:47:32 -0500

    Take 2

--- js/index.js
+++ js/index.js
@@ -1,33 +1,73 @@
+//Code
 
-// Your code goes here
-console.log("Hello world");
 
-const head = document.querySelector(".main-navigation");
-//console.log(head);
-const funBus = head.querySelector("h1");
-funBus.addEventListener("mouseover", (element) => element.target.style.color = "yellow");
-const links = head.querySelector("a");
-//Array.from(links).forEach((element) => element.addEvenetListener("mouseover", (e) => e.target.style.color = "blue"));
 
-const webBody = document.querySelector("body");
-webBody.addEventListener('keydown', (e) => alert('Dont touch anything'));
 
-const funBusImg = document.querySelector(".intro img")
-funBusImg.addEventListener('wheel',function zoom(event) {
-    event.preventDefault();
-    let scale = 1;
-    scale += event.deltaY * -0.02;
+
+// // Your code goes here
+// console.log("Hello world");
+
+// const head = document.querySelector(".main-navigation");
+// //console.log(head);
+// const funBus = head.querySelector("h1");
+// funBus.addEventListener("mouseover", (element) => element.target.style.color = "yellow");
+// const links = head.querySelector("a");
+// //Array.from(links).forEach((element) => element.addEvenetListener("mouseover", (e) => e.target.style.color = "blue"));
+
+// //webBody.addEventListener('keydown', (e) => alert('Dont touch anything'));
+// const webBody = document.querySelector("body");
+// console.log(webBody);
+// webBody.addEventListener('keydown', (event) => {
+//   if (event.key === "a") {
+//     webBody.style.backgroundColor ="blue"
+//   }
+// });
+
+// const funBusImg = document.querySelector(".intro img")
+// funBusImg.addEventListener('wheel',function zoom(event) {
+//     event.preventDefault();
+//     let scale = 1;
+//     scale += event.deltaY * -0.02;
   
-    // Restrict scale
-    scale = Math.min(Math.max(.125, scale), 4);
+//     // Restrict scale
+//     scale = Math.min(Math.max(.125, scale), 4);
   
-    // Apply scale transform
-    funBusImg.style.transform = `scale(${scale})`;
-  });
+//     // Apply scale transform
+//     funBusImg.style.transform = `scale(${scale})`;
+//   });
 
-  webBody.addEventListener('load', (event) => console.log('page is fully loaded'));
+//   const letsGo = document.querySelector(".text-content");
+//   letsGo.addEventListener('click', function changeColor(element) {
+//     letsGo.style.backgroundColor = "black";
+//   })
+//   letsGo.addEventListener('dblclick', function changeBack(element) {
+//     letsGo.style.backgroundColor = "white";
+//   });
+  
 
-  webBody.addEventListener('click', (event) => event.backgroundColor = "green");
+//   const mainNav = document.querySelector(".main-navigation");
+//   const logo = mainNav.querySelector("h1");
+//   logo.addEventListener("click", (event) => logo.innerText = "Bus Fun");
+
+
+//   const navLinks = mainNav.querySelectorAll("a");
+//   navLinks.forEach(element => {
+//     element.addEventListener("mouseover", (event) => {
+//       event.style.backgroundColor = "green";
+//     })
+//   });
+
+
+// funBusImg.addEventListener('dblclick', (event) => {
+  
+// })
+
+
+
+
+
+
+
 
 
 
