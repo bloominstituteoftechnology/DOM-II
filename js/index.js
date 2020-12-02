11637386bd32634114693bab07968438a7f2138f
Brennen O'Connor <brennenoconnor@Brennens-MacBook-Pro.local>
2020-12-01 19:52:42 -0500

    fun bus

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+const h1 = document.querySelector("h1");
+h1.addEventListener("mouseover", function (event) {
+  event.target.style.color = "green";
+});
+
+const body = document.querySelector("body");
+body.addEventListener("keydown", function (event) {
+  if (event.keyCode == 66) {
+    h1.innerHTML = "Dope Bus";
+  }
+});
+
+const container = document.querySelectorAll('.container.home')
+body.addEventListener('wheel', function() {
+    container[0].style.background = 'red';
+})
+
