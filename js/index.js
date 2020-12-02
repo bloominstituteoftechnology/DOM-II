1365f09ff68566a8fbdce20a86bc5a0d53d55987
Matthew Larson <maflarson@gmail.com>
2020-12-01 18:48:14 -0700

    did some shit

--- js/index.js
+++ js/index.js
@@ -27,4 +27,20 @@ const navLinksT = document.querySelector('nav');
 navLinksT.addEventListener('dblclick', function(event){
     event.target.style.fontWeight = "100"
 })
-
+//example of something to do 
+const link = document.querySelectorAll('h2');
+Array.from(link).forEach((item) => {
+    item.addEventListener('mouseenter', (e) => {
+        item.style.color = 'red';
+        e.target.style.fontSize = '6rem'
+    }, false);
+})
+Array.from(link).forEach((item) => {
+    item.addEventListener('mouseleave', (e) => {
+        e.target.style.color = '#666666';
+        e.target.style.fontSize = '';
+    setTimeout((e) => {
+        item.style.color = ''
+    }, 600)    
+    }, false)
+})
