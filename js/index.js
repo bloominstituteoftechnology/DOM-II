0ee6e94d839ceca3278cb38ee949e1a63af00e30
george-hatzigeorgio <georgehatzigeorgio@zohomail.com>
2020-12-02 09:19:40 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -1,29 +1,74 @@
-const navLinks = document.querySelectorAll("a" )
+// const { render } = require("less");
+
+var navLinks = document.querySelectorAll(".nav-link" )
 
 
+//   console.log(navLinks)
 
 
-Array.from(navLinks).forEach(function (link) {
-    link.addEventListener("click", function (event) {
-       event.target.style.backGroundColor = "red"
+ navLinks.forEach(function(element){
+   element.addEventListener("click", function (event) {
+        event.target.style.fontSize = "3rem"
+        // console.log("its working")
+
     });
+ });
+
+
+ const headers = document.querySelectorAll("h2")
+ headers.forEach(function(element){
+    element.addEventListener("click", function (event) {
+         event.target.style.fontSize = "4rem"
+ 
+     });
   });
-  const headerClass = document.querySelector(".intro")
-  const header = headerClass.querySelector("h2")
+  window.addEventListener('scroll',(event) => {
+    console.log('Scrolling...');
+});
+const div = document.querySelector(".text-content")
+ const para = document.querySelector("p")
+ function reportWindowSize() {
+    div.textContent = window.innerHeight;
+    para.textContent = window.innerWidth;
+    console.log("its working")
+   }
   
+   window.onresize = reportWindowSize;
+ window.addEventListener('resize', reportWindowSize);
 
-  header.addEventListener("click",function(event){
-      event.target.style.fontSize = "3rem"
-      
-      
-  })
-
-
-  const div = document.querySelector(".text-content")
-
-  const paragraph = div.querySelector("p")
-  paragraph.addEventListener(`dblclick`, function(event){
-   event.target.style.fontsize = "3.8rem"
-   
-  })
+ function logSelection(event) {
+    const log = document.getElementById('log');
+    const selection = event.target.value.substring(event.target.selectionStart, event.target.selectionEnd);
+    log.textContent = `You selected: ${selection}`;
+  }
   
+  const input = document.querySelector('input');
+  input.addEventListener('select', logSelection);
+
+ const paragraph = document.querySelectorAll("p")
+  document.addEventListener('keydown', logKey);
+
+function logKey(e) {
+  paragraph.textContent += ` ${e.code}`;
+  console.log("it works")
+}
+
+const headings = document.querySelectorAll("h4")
+ headings.addEventListener("mouseenter", function(event){
+     event.target.style.color = "blue"
+     setTimeout(function(){
+         event.target.style.color ="";
+     },600)
+ }, false)
+ headings.addEventListener("mouseover", function( event ) {   
+    event.target.style.color = "orange";
+     setTimeout(function() {
+         event.target.style.color="";
+     },600)
+ },false)
+
+const divs =document.querySelectorAll(".btn")
+
+ divs.addEventListener("dblclick",function (e){
+   divs.classList.toggle("large")
+ });
