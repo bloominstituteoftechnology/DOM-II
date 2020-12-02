80ea0c6995a176585ec3af034f86a265a796bba8
Andrew Sohrabi <andrewsohrabi@Andrews-MacBook-Pro.local>
2020-12-02 01:50:39 -0800

    completed 2/10 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+
+const navElement = document.querySelector('nav');
+const logoHeading = document.querySelector('.logo-heading');
+
+navElement.addEventListener('click', function (event) {
+    navElement.style.backgroundColor = "red";
+})
+
+logoHeading.addEventListener('mouseover', function(event) {
+    logoHeading.style.backgroundColor = "seagreen";
+})
+
+// add event listener to undo changes once i leave page
+
+
+// CANT GET KEYDOWN ESCAPE TO CHANGE IMAGE
+const funBusImgs = document.querySelectorAll('img');
+funBusImgs[0].addEventListener('keydown', function (event) {
+    if (event.key === "Escape") {
+        funBusImgs[0].src = "https://images.pexels.com/photos/1091470/pexels-photo-1091470.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    }
+   
+})
+
