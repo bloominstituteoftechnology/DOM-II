7e495e5af75b3c80fe1f8c8521152c27639a9477
DenisDiaz21 <71994529+DenisDiaz21@users.noreply.github.com>
2020-12-06 22:16:06 -0500

    added 1 event to big IMG

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+// 1. Disappearing Image!
+function disappear (){
+    mainImg.style.visibility = 'hidden';
+
+}
+function appear (){
+    mainImg.style.visibility = 'visible';
+}
+
+let mainImg = document.querySelector("#bigBusImage");
+mainImg.addEventListener('mouseover', disappear);
+mainImg.addEventListener('mouseout', appear);
