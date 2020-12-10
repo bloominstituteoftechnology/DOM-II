19e67b917361de904f7008ddf5288c60a97f84a4
Ethan Karp <ethansmainemail@gmail.com>
2020-12-10 16:27:38 -0700

    added 3 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+// 1
+const logoHeading = document.querySelector('.logo-heading');
+logoHeading.addEventListener('mouseenter', event => {
+    logoHeading.classList.toggle('toggle-style');
+    // debugger
+})
+logoHeading.addEventListener('mouseleave', event => {
+    logoHeading.classList.toggle('toggle-style');
+})
+
+// 2
+const nav = document.querySelector('.nav');
+document.addEventListener('keydown', event => {
+    nav.classList.toggle('toggle-style')
+})
+
+// 3
+const img = document.querySelector('img');
+document.addEventListener('wheel', event => {  
+    img.classList.toggle('toggle-radius');
+})
+
