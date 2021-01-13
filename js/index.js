3592589e483e4811a90c2aac6dfd5d35a4c310c1
Leon Nasswetter <leon.nasswetter@gmail.com>
2021-01-12 17:08:29 -0800

    mvp finished

--- js/index.js
+++ js/index.js
@@ -7,13 +7,25 @@ const nav1 = document.querySelector("nav a");
 const nav2 = nav1.nextElementSibling;
 const nav3 = nav2.nextElementSibling;
 const nav4 = nav3.nextElementSibling;
+// fun bus nav
+const funBus = document.querySelector(".logo-heading")
 
 // Img selectors
-const header = document.querySelector("header");
+const header = document.querySelector(".intro");
 const headerImg = header.querySelector("img");
+const destination = document.querySelector(".content-destination");
+const destImg = destination.querySelector("img");
+const letsGo = document.querySelector(".content-section");
+const letsGoImg = letsGo.querySelector("img");
+
 
 // Text selector
-const text = document.querySelector("p")
+const introText = document.querySelector(".intro");
+const welcomeText = introText.querySelector("h2");
+
+
+
+
 
 //home hover
 nav1.addEventListener("mouseover", function(event) {
@@ -42,7 +54,37 @@ function zoom(event) {
 scale = 1;
 nav3.onwheel = zoom;
 
-//selecting 
-text.addEventListener("select", function(event) {
-    text.style.color = "orange";
+//scroll
+document.addEventListener("scroll", function(event) {
+    body.style.backgroundColor = "gray";
 });
+
+//keydown anywhere to make img disappear
+document.addEventListener("keydown", function(event){
+    headerImg.style.opacity = 0;
+});
+//keyup to make img appear with low opacity
+document.addEventListener("keyup", function(event){
+    headerImg.style.opacity = 0.5;
+});
+
+//fun bus double click
+funBus.addEventListener("dblclick", function(event) {
+    funBus.textContent = "Double Fun Bus";
+});
+
+//destination img mouseleave
+destImg.addEventListener("mouseleave", function(event) {
+    body.style.backgroundColor = "white";
+});
+
+//mouseup on lets go img
+letsGo.addEventListener("mouseup", function(event) {
+    body.style.backgroundColor = "green";
+});
+
+//winow resize
+window.onresize = function(){
+    body.style.color = "pink";
+    body.style.backgroundColor = "black";
+}
