e133b7fd8731129459b52ee8fac9a6db336da620
Nick Allen <nwestallen@gmail.com>
2021-01-12 20:20:56 -0500

    successfully used stopPropagation()

--- js/index.js
+++ js/index.js
@@ -3,7 +3,11 @@ let header = document.querySelector('header');
 header.addEventListener('mouseenter', () => {header.style.backgroundColor = 'skyblue'});
 header.addEventListener('mouseleave', (event) => {event.currentTarget.style.backgroundColor = 'white'});
 
-document.addEventListener('click', (event) => {event.target.style.backgroundColor = 'orange'}); 
+let navItems = document.querySelectorAll('.nav-link');
+navItems.forEach((link) => link.addEventListener('click', (event) => {event.target.style.backgroundColor = 'orange';
+event.stopPropagation();}));
+header.addEventListener('click', (event) => event.currentTarget.style.backgroundColor = 'green');
+//document.addEventListener('click', (event) => event.target.style.backgroundColor = 'orange'); 
 document.addEventListener('keydown', () => {document.body.style.color = 'blue'});
 document.addEventListener('keyup', () => {document.body.style.color = 'black'});
 document.addEventListener('scroll', () => {document.body.style.backgroundColor = 'yellow'});
@@ -17,5 +21,4 @@ let busImg = document.querySelector('header img');
 busImg.addEventListener('drag', (event) => event.target.style.width = '100px');
 busImg.addEventListener('dragend', (event) => event.target.style.width = '100%');
 
-let navItems = document.querySelectorAll('nav-link');
 navItems.forEach((link) => link.addEventListener('click', (event) => event.preventDefault()));
