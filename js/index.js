51e4468f9b2e80ec309a320d5067518b9117550f
Bansa Laura <weidiwu207@gmail.com>
2021-01-12 19:07:38 -0500

    color change

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Your code goes here
 let headings = document.querySelector('h2');
 headings.addEventListener('mouseOver', function(event){
-
+headings.style.color = "red";
 })
+console.log(headings);
+debugger;
