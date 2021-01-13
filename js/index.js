46e4f024a28afebc536ab844823592004d4c8132
Trenten grede <Trenten.grede@gmail.com>
2021-01-12 23:14:00 -0600

    added all selection tags necessary to control any piece of the page with eventlisteners

--- js/index.js
+++ js/index.js
@@ -1,32 +1,99 @@
 // Your code goes here
 let headerBar = document.querySelector('.nav-container')
-console.log(headerBar)
+console.log('HeaderBar: ', headerBar)
 let headerText = document.querySelector('.logo-heading')
-console.log(headerText)
+console.log('HeaderText: ',headerText)
 let navBarList = document.querySelector('.nav')
-console.log(navBarList)
+console.log('navBarList: ', navBarList)
 let navItems = document.querySelectorAll('.nav-link')
-console.log(navItems)
+console.log('navItems: ', navItems)
 
 let fullContainer = document.querySelector('.home')
-console.log(fullContainer)
+console.log('fullContainer: ', fullContainer)
 
 let intro = document.querySelector('.intro')
-console.log(intro)
+console.log('intro: ', intro)
 let introImg = document.querySelector('.intro img')
-console.log(introImg)
+console.log('introImg: ', introImg)
 let introHead = document.querySelector('.intro h2')
-console.log(introHead)
+console.log('introHead: ', introHead)
 let introText = document.querySelector('.intro p')
-console.log(introText)
+console.log('introText: ', introText)
 
 let content = document.querySelector('.content-section')
-console.log(content)
+console.log('content: ', content)
 let contentAllText = document.querySelector('.text-content')
-console.log(contentAllText)
+console.log('contentAllText: ', contentAllText)
 let contentHeadText = document.querySelector('.text-content h2')
-console.log(contentHeadText)
+console.log('contentHeadText: ', contentHeadText)
 let contentP1Text = document.querySelector('.text-content p')
-console.log(contentP1Text)
-let contentP2Text = document.querySelector('.text-content p')
-console.log(contentP2Text)
+console.log('contentP1Text: ', contentP1Text)
+let contentP2Text = contentP1Text.nextElementSibling
+console.log('contentP2Text: ', contentP2Text)
+let contentImgDiv = content.querySelector('.img-content')
+console.log('contentImgDiv: ', contentImgDiv)
+let contentImg = contentImgDiv.querySelector('img')
+console.log('contentImg: ', contentImg)
+
+let inverseContent = document.querySelector('.inverse-content')
+console.log('inverseContent: ', inverseContent)
+let inverseContentAllText = inverseContent.querySelector('.text-content')
+console.log('inverseContentAllText: ', inverseContentAllText)
+let inverseContentHeadText = inverseContentAllText.querySelector('h2')
+console.log('inverseContentHeadText: ', inverseContentHeadText)
+let inverseContentP1Text = inverseContentAllText.querySelector('p')
+console.log('inverseContentP1Text: ', inverseContentP1Text)
+let inverseContentP2Text = inverseContentP1Text.nextElementSibling
+console.log('inverseContentP2Text: ', inverseContentP2Text)
+let inverseContentImgDiv = inverseContent.querySelector('.img-content')
+console.log('inverseContentImgDiv: ', inverseContentImgDiv)
+let inverseContentImg = inverseContentImgDiv.querySelector('img')
+console.log('inverseContentImg: ', inverseContentImg)
+
+let destination = document.querySelector('.content-destination')
+console.log('destination: ', destination)
+let destinationHeader = destination.querySelector('h2')
+console.log('destinationHeader: ', destinationHeader)
+let destinationText = destinationHeader.nextElementSibling
+console.log('destinationText: ', destinationText)
+
+let pickDestination = document.querySelector('.content-pick')
+console.log('pickDestination: ', pickDestination)
+
+let destination1 = pickDestination.querySelector('.destination')
+console.log('destination1: ', destination1)
+let destination1Head = destination1.querySelector('h4')
+console.log('destination1Head: ', destination1Head)
+let destination1Text = destination1Head.nextElementSibling
+console.log('destination1Text: ', destination1Text)
+let destination1Button = destination1Text.nextElementSibling
+console.log('destination1Button: ', destination1Button)
+
+let destination2 = destination1.nextElementSibling
+console.log('destination2: ', destination2)
+let destination2Head = destination2.querySelector('h4')
+console.log('destination2Head: ', destination2Head)
+let destination2Text = destination2Head.nextElementSibling
+console.log('destination2Text: ', destination2Text)
+let destination2Button = destination2Text.nextElementSibling
+console.log('destination2Button: ', destination2Button)
+
+
+let destination3 = destination2.nextElementSibling
+console.log('destination3: ', destination3)
+let destination3Head = destination3.querySelector('h4')
+console.log('destination3Head: ', destination3Head)
+let destination3Text = destination3Head.nextElementSibling
+console.log('destination3Text: ', destination3Text)
+let destination3Button = destination3Text.nextElementSibling
+console.log('destination3Button: ', destination3Button)
+
+let footer = document.querySelector('footer')
+console.log('footer: ', footer)
+let footerText = footer.querySelector('p')
+console.log('footerText: ', footerText)
+
+
+
+
+
