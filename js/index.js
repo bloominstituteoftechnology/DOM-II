d06d71496adca0017a5d98e630ce052b3bbbd7c7
Christopher Greene <greenebasecamp@gmail.com>
2021-01-12 23:55:44 -0500

    Playing with complex stuff... how to get images to move around?

--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 const navLinks = document.querySelectorAll(".nav-link");
 const logoHeading = document.querySelector("h1");
 const secondaryTitles = document.querySelectorAll("h2");
+const busImg = document.querySelectorAll("img");
+
+
 
 logoHeading.addEventListener("click", function(e){
     let newColor = e.target.style.color
@@ -16,7 +19,7 @@ logoHeading.addEventListener("click", function(e){
 
 navLinks.forEach(item => {
     item.addEventListener("mouseover", function(e){
-        e.target.style.color = 'green'
+        e.target.style.color = 'red'
     });
 });
 
@@ -24,5 +27,12 @@ secondaryTitles.forEach(item => {
     item.addEventListener("click", function(e){
         e.target.style.color = 'pink'
         e.target.style.fontSize = '4rem'
-    })
-})
+    });
+});
+
+busImg.forEach(item => {
+    item.addEventListener("mouseover", function(e){
+        e.target.style.marginRight = "0px";
+    });
+});
+
