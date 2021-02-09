6f2383d4cec090d8e27437da2f5bfbed730a8b8f
L-Steinmacher <dasai.sacul@gmail.com>
2021-02-09 15:03:52 -0800

    50% done with event listeners

--- js/index.js
+++ js/index.js
@@ -3,12 +3,45 @@
 
 const funLogo = document.querySelector('.logo-heading');
 const funImg = document.querySelector('.intro Img');
-const selectP = document.querySelector('p');
+const destination = document.querySelector("content-destination");
 const destinationP = document.querySelector('.destination');
 const button = document.querySelector('.btn');
+const footer = document.querySelector('.footer p');
+const head = document.querySelector('head');
+
 const pressed = [];
 const lucasCode = 'lucas';
 
+let scale = 1;
+let x = 0;
+
+footer
+
+destination.addEventListener('scroll', (){
+
+});
+
+window.addEventListener('resize', ()=>{
+    let txt = x +=1;
+
+    funImg.innerHTML = txt;
+    console.log(txt);
+});
+
+funImg.addEventListener('mousewheel', (e) => {
+    e.preventDefault
+
+    scale += e.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    funImg.style.transform = `scale(${scale})`;
+})
+
+funLogo.addEventListener('mouseover', (e) => {
+    e.target.style.color = 'blue';
+    setTimeout(function() {
+        e.target.style.color = "";
+      }, 500);
+},false)
 
 window.addEventListener('keyup', (e) =>{
     pressed.push(e.key);
@@ -18,6 +51,17 @@ window.addEventListener('keyup', (e) =>{
     }
 });
 
-window.addEventListener('keydown',(e) =>{
-    
+document.addEventListener('keydown',(e) =>{
+    if(e.key === 'Escape'){
+        alert("Don't you just want to escape? ");
+    }
+});
+
+window.addEventListener('load', ()=>{
+    console.log('hey')
+    setTimeout(() => {alert('Welcome To Your Life!');},3000);
 })
+
+
+
+
