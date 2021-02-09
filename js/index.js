a5473885db8785144db2e3cc3d5a88fb2ce9fb8d
ALDO <aldo@aldos-mbp.attlocal.net>
2021-02-09 12:18:58 -0800

    first commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// Your code goes here
+/* ### Task 2: Create listeners for 10 types of events
+
+* [ ] Using your [index.js file](js/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, remove objects, etc. Here are some event types you could try to use:
+  * `mouseover`
+  * `keydown`
+  * `wheel`
+  * `load`
+  * `focus`
+  * `resize`
+  * `scroll`
+  * `select`
+  * `dblclick`
+  * `drag / drop`
+
+Note: Drag and drop is a bit more advanced than the others: it's not actually a single type of event but several types that need to work together.
+
+* [ ] Nest two similar events somewhere in the site and prevent the event propagation properly. Remember not all event types bubble.
+* [ ] Stop the navigation items from refreshing the page by using `preventDefault()`
+*/
