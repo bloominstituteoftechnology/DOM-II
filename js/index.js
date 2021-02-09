402173c2397cfeb82981fc2c32bdae3454d064df
Timmy Nguyen <timmy-thai-nguyen@lambdastudents.com>
2021-02-09 13:21:57 -0800

    quick save

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+// link variables
+const main = document.querySelector( ".main-navigation" );
+const links = document.querySelectorAll (".nav-link");
+const destinations = document.querySelector(".destination");
+
+
+// add Event Listerers, variables can have multiple event listeners
+
+main.addEventListener( "mouseover", function ( evt ) {
+    main.style.background = "green";
+}, 500);
+main.addEventListener( "mouseleave", function ( evt ) {
+    main.style.background = "red";
+}, 500);
+links.addEventListener( "dblclick", function ( evt ) {
+    main.style.background = "blue";
+});
