58a2a022f277cb83df23088003045ebf52838c40
Ryan Ishimaru <rishimaru23@gmail.com>
2021-02-09 18:58:54 -0800

    intial changes

--- js/index.js
+++ js/index.js
@@ -1,1 +1,40 @@
 // Your code goes here
+const logoHeading = document.querySelector("h1"); // changes logo color of fun bus 
+logoHeading.addEventListener("click",() => {
+    logoHeading.style.color = "yellow";
+});
+
+logoHeading.addEventListener ("dblclick", () => { // logo click
+alert ("Welcome!")
+});
+
+const button1 = document.querySelector('.destination');
+button1.addEventListener('click', () => {
+    button1.style.backgroundColor = "yellow"; //button
+});
+
+const button2 = document.querySelector(".btn");
+button2.addEventListener('mouseup', (event) => {
+    button2.style.backgroundColor = "green";
+    console.log(event);
+    event.stopPropagation();
+});
+
+const button3 = document.querySelector(".btn:nth-of-type(2)");
+button3.addEventListener('mouseup', ()=>{
+    button3.style.backgroundColor = "blue";
+});
+
+const stopNav = document.querySelector("a");
+stopNav.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log("No longer refreshing");
+});
+
+document.querySelectorAll("a").forEach(a => {
+    a.addEventListener("click", function() {
+      a.style.color= "yellow";
+    });
+  });
+
+
