b46ffaa8aa9db8a278e8804f54862107d294f392
L-Steinmacher <dasai.sacul@gmail.com>
2021-02-09 16:59:53 -0800

    almost finished

--- js/index.js
+++ js/index.js
@@ -4,22 +4,48 @@
 const funLogo = document.querySelector('.logo-heading');
 const funImg = document.querySelector('.intro Img');
 const destination = document.querySelector("content-destination");
-const destinationP = document.querySelector('.destination');
+const destinationP = document.querySelector('.destination p');
 const button = document.querySelector('.btn');
 const footer = document.querySelector('.footer p');
 const head = document.querySelector('head');
+const navLinks = document.querySelectorAll('.nav')
+const body = document.querySelector('body')
 
 const pressed = [];
 const lucasCode = 'lucas';
 
 let scale = 1;
 let x = 0;
+let hue = 0;
 
-footer
 
-destination.addEventListener('scroll', (){
 
-});
+
+
+destinationP.querySelector('select', (e) => {
+    const selection = e.target.value.substring(e.target.selectionStart, e.target.selectionEnd);
+    console.log(`You selected ${selection}`)
+})
+
+window.addEventListener('scroll', () => {
+    console.log('Hello')
+    hue++
+    body.style.background = `hsl(${hue}, 100%, 50%)`
+    if(hue > 360){
+        hue = 0;
+    }
+})
+
+Array.from(navLinks).forEach(link =>{
+    link.addEventListener('click', (e) => {
+        e.preventDefault()
+    })
+})
+
+footer.addEventListener('dblclick', () => {
+    console.log('present')
+    footer.innerHTML = 'try typing "lucas"!'
+})
 
 window.addEventListener('resize', ()=>{
     let txt = x +=1;
@@ -48,8 +74,10 @@ window.addEventListener('keyup', (e) =>{
     pressed.splice(-lucasCode.length -1, pressed.lengh - lucasCode.length);
     if (pressed.join('').includes(lucasCode)){
         console.log('BwaaaaHAHAHAH!')
+        cornify_add()
     }
 });
+
 
 document.addEventListener('keydown',(e) =>{
     if(e.key === 'Escape'){
