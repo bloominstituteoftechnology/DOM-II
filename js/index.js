b47529cf6c28c880dfbc750ec10db6ca2a824d5b
axe <axe@Jake.local>
2021-02-14 20:53:55 -0700

    i have completed the assignment

--- js/index.js
+++ js/index.js
@@ -1,1 +1,37 @@
 // Your code goes here
+const navText = document.querySelector(".main-navigation").addEventListener(click, function(thisIsTheNav));
+
+const expandedImage = document.addEventListener('fullscreenchange', (click) => {
+    if (document.fullscreenElement) {
+      console.log(`Element: ${document.intro.img.id} entered full-screen mode.`);
+    } else {
+      console.log('Leaving full-screen mode.');
+    }
+  });
+  
+const navFocus = document.querySelector('nav');
+
+  password.addEventListener('focus', (hover) => {
+    event.target.style.background = 'pink';
+  });
+  
+  password.addEventListener('blur', (hover) => {
+    event.target.style.background = 'white';
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function thisIsTheNav() {
+    alert ("This is the nav bar!");
+}
