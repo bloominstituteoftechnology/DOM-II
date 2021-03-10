337d43315ed2e46165ed63302154e51e3f609720
Lara Hilborn <larahilborn@Laras-MacBook-Air.local>
2021-03-09 20:50:17 -0500

    added 5 changes to the DOM

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 // Your code goes here
+
+const button = document.querySelector('.btn');
+
+button.addEventListener('click', event => {
+    button.textContent = `click count: ${event.detail}`;
+});
+
+
+const password = document.querySelector('input[type="password"]');
+
+password.addEventListener('focus', (event) => {
+  event.target.style.background = 'lightblue';
+});
+
+password.addEventListener('blur', (event) => {
+  event.target.style.background = '';
+});
