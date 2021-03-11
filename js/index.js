45ad2c3131e557d6071be79664a320cebbe3bd10
emmalouiseread <73863740+emmalouiseread@users.noreply.github.com>
2021-03-10 22:13:05 -0500

    fin

--- js/index.js
+++ js/index.js
@@ -1,51 +1,85 @@
-// Your code goes here
-// mouseover
+// callback functions
 
-let mouseover = document.getElementById("a");
+const colorChanger = function(element) {
+    element.preventDefault();
+    const {target} = element;
+    target.style.color = "pink";
+}
 
-mouseover.addEventListener("mouseenter", function (event) {
-    event.target.style.color = "purple";
+const colorReturn = function(element) {
+    const {target} = element;
+    target.style.color = "black";
+}
+
+const textChanger = function(element) {
+    element.preventDefault();
+    const {target} = element;
+    target.innerHTML = "whoop";
+}
+
+const rounderCorners = function(element) {
+    const {target} = element;
+    target.style.borderRadius = "10%"
+}
+
+// retrieve all of the elements
+
+let navBar = document.querySelector(".nav");
+let navList = navBar.querySelectorAll(".nav-link");
+let header = document.querySelector(".intro");
+let headerTitle = header.querySelector("h2");
+let headerText = header.querySelector("p");
+let textContent = document.querySelector(".text-content");
+let textContentPara = textContent.querySelector("p");
+let footer = document.querySelecotr(".footer p");
+let bus = header.querySelector("img");
+
+
+// mouseenter
+
+navList.forEach(a => {
+    a.addEventListener("mouseenter", colorChanger)
+});
+
+// keydown
+
+headerTitle.addEventListener("keydown", colorChanger);
+
+// wheel
+
+headerText.addEventListener("wheel", colorChanger);
+
+// scroll
+
+textContentPara.forEach(a => {
+    a.addEventListener("scroll", textChanger);
 })
 
-let w = document.getElementById("b");
+// click
 
-w.addEventListener("wheel", function (event) {
-    event.style.fontSize = "35px";
-})
+footer.addEventListener("click", colorChanger);
 
-w.addEventListener('load', (event) => {
-    console.log('page is fully loaded');
-  });
+// resize
 
- w.addEventListener('click', () => {
-    log.textContent ='';
-    window.setTimeout(() => {
-        window.location.w(true);
-    }, 200);
-  });
+footer.addEventListener("resize", textChanger);
 
-  let um = document.getElementById("c");
+// reload 
 
-  um.addEventListener('focus', (event) {
-      event.target.style.background = 'pink';
-  })
+footer.addEventListener("reload", colorChanger);
 
-  um.addEventListener('blur', (event) => {
-    event.target.style.background = '';
-  }, true);
+// mousemove
 
-  let last_known_scroll_position = 0;
-  let ticking = false;
-  
-  document.addEventListener('scroll', function(e) {
-    last_known_scroll_position = window.scrollY;
-  
-    if (!ticking) {
-      window.requestAnimationFrame(function() {
-        doSomething(last_known_scroll_position);
-        ticking = false;
-      });
-  
-      ticking = true;
-    }
-  });
+bus.addEventListener("mousemove", rounderCorners);
+
+// mouseleave
+
+headerTitle.addEventListener("mouseleave", colorReturn);
+
+
+
+
+
+
+
+
+
