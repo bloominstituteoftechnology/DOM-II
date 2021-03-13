542fd55cce52ac5844c3be0c0eaa705150cfcf5f
Cynthia Coronado <cynthiacoronado@Cynthias-Air.hsd1.ca.comcast.net>
2021-03-13 15:40:20 -0800

    added alert to dblclick and tried wheel

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 // Your code goes here
 
 //mouseover 
-
 let navItems = document.querySelectorAll('.nav-link')
 navItems.forEach(function (element) {
     element.addEventListener('mouseover', turnPurple)
@@ -12,9 +11,13 @@ navItems.forEach(function (element) {
         }, 500);
     }
 })
+navItems.forEach(function (event) {
+    event.style.color = 'pink'
+})
+
 
 //click
-let navClick = document.querySelectorAll('.btn')
+let navClick = document.querySelectorAll('.nav-link')
 navClick.forEach(function (element) {
     element.addEventListener('click', changeNavColor)
     function changeNavColor(event) {
@@ -27,9 +30,20 @@ let doubleClick = document.querySelectorAll('.btn')
 doubleClick.forEach(function (element) {
     element.addEventListener('dblclick', changeColor)
     function changeColor(event) {
-        event.target.style.color = 'yellow'
+        event.target.style.color = 'white'
+        alert('Join Now')
     }
 })
+doubleClick.forEach(function (event) {
+    event.style.color = 'black'
+})
+
+//wheel
+let makeTextLarge = doument.querySelector('.h1')
+makeTextLarge.addEventListener('wheel', function (event) {
+    event.target.style.fontsize = '100px'
+})
+
 
 
 
