53cee50b1535f1de62b82cecc569d91eb9b346c8
Charles May <charlie@Charless-MacBook-Pro.local>
2021-03-13 07:46:54 -0600

    plan created

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
+/* eslint-disable no-unused-vars */
 // Your code goes here
+// first, let's select some things I want to interact with
+
+// signup button
+const signUp = document.querySelectorAll('#btn');
+
+
+// listner 1: click on signUp button
+
+// listner 2: keydown the esc key to leave the signup pop-up
+
+// listner 3: mouseover the map to display something
+
+// listner 4: wheel
+
+// listner 5: load
+
+// listner 6: focus
+
+// listner 7: select
+
+// listner 8: dblclick
+
+// listner 9: resize
+
+// listner 10: look something up
