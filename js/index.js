8a45d80c171d3afd36707bbaaa43f9101039bba1
Cynthia Coronado <cynthiacoronado@Cynthias-Air.hsd1.ca.comcast.net>
2021-03-13 12:56:32 -0800

    mouseover and wheel

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
 // Your code goes here
+
+//mouseover 
+var navColor = document.querySelector("#nav-id");
+navColor.addEventListener('mouseover', turnPurple)
+function turnPurple(event) {
+    event.target.style.color = 'purple'
+    setTimeout(function () {
+        event.target.style.color = "";
+    }, 500);
+}
+
+//wheel 
+var image = document.querySelector("img[src='img/adventure.jpg']")
+image.onwheel = zoom
+
+    // //load
+    // let imgLoad = document.querySelector('.img-content')
+    // imgLoad.load();
+
+    //focus
+    // focusButton = function getFocus() {
+    //     document.getElementsByClassName(".btn").focus();
+    // }
