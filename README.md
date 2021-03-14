8b7d89774faad576ff636d53bad4ce296d3dff0e
Stan Tudor <admin@Mac.local>
2021-03-14 16:30:11 -0700

    final project updates

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ Fun Bus wants you to make their site more interactive. They are relying on you t
 
 ### Task 2: Create listeners for 10 types of events
 
-* [ ] Using your [index.js file](js/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, remove objects, etc. Here are some event types you could try to use:
+* [x] Using your [index.js file](js/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, remove objects, etc. Here are some event types you could try to use:
   * `mouseover`
   * `keydown`
   * `wheel`
@@ -54,4 +54,4 @@ Note: Drag and drop is a bit more advanced than the others: it's not actually a 
 
 **Follow these steps for completing your project.**
 
-* [ ] Submit a Pull-Request to merge `<firstName-lastName>` Branch into `main` (student's  Repo). **Please don't merge your own pull request**
+* [x] Submit a Pull-Request to merge `<firstName-lastName>` Branch into `main` (student's  Repo). **Please don't merge your own pull request**
--- js/index.js
+++ js/index.js
@@ -12,9 +12,14 @@ const imgOutGo = document.querySelector('.img-content');
 const imgInAwaits = document.querySelector('.inverse-content img');
 const imgOutAwaits = document.querySelector('.inverse-content img');
 const button = document.querySelectorAll('.btn');
+const bkColor = document.getElementsByClassName('destination');
+const destination = document.querySelector('.intro p')
+const dest = document.querySelector('.content-destination p');
 
 
-// create a new LOGO element (IMAGE NOT SHOWING)
+
+// create a new LOGO element 
+// (IMAGE NOT SHOWING, driving me crazy, but crazy is good...:)
 logoStan.src = 'img/stan.png'
 // style the LOGO element 
 logoStan.style.marginTop = '40px';
@@ -26,32 +31,33 @@ logoStan.style.borderRadius = '50%';
 logoSection.prepend(logoStan);
 
 
-// create a new H2 element (OK)
+// create a new H2 element 
 subHeadLine.textContent = 'Click the yellow bus'
-// style ...
+// style it...
 subHeadLine.style.fontSize = '2.5rem'
 subHeadLine.style.color = 'slategrey'
 subHeadLine.style.marginBottom = '20px';
-// append ...
+// append it...
 headerEl.appendChild(subHeadLine);
 
-
-// change background color 
-const x = document.getElementsByClassName('destination');
-for (let i = 0; i < x.length; i++) {
-    x[i].style.backgroundColor = 'lightgrey';
-    x[i].style.padding = '10px';
+// change CTA background color 
+for (let i = 0; i < bkColor.length; i++) {
+    bkColor[i].style.backgroundColor = 'yellow';
+    bkColor[i].style.padding = '10px';
+    bkColor[i].style.borderRadius = '50%';
 };
 
 
-// Change 'p' color when select
-const destination = document.querySelector('.content-destination p')
-destination.addEventListener('mousedown', () => {
+// PARAGRAPHS - Mouse Events
+destination.addEventListener('mouseover', () => {
     destination.style.color = 'violet';
-})
+});
+dest.addEventListener('mousedown', () => {
+    dest.style.color = 'green';
+});
 destination.addEventListener('mouseup', () => {
-    destination.style.color = 'blue'
-})
+    destination.style.color = 'red'
+});
 
 
 // Window E V E N T  * * * 
@@ -60,14 +66,14 @@ window.addEventListener('load', event => {
 });
 
 
-// Alert on the bus
+// ALERT on the Bus
 let funBus = document.querySelector('.intro img');
 funBus.addEventListener('click', event => {
     alert('CONGRATULATIONS... You clicked the BUS!')
 });
 
 
-// Hero banner img transform * * * ZOOM ZOOM
+// Hero banner img TRANSFORM * * * ZOOM ZOOM
 imgInHero.addEventListener('mouseenter', () => {
     imgInHero.style.transform = 'scale(1.1)';
     imgInHero.style.transition = 'all 0.5s';
@@ -75,7 +81,8 @@ imgInHero.addEventListener('mouseenter', () => {
 imgOutHero.addEventListener('mouseleave', () => {
     imgOutHero.style.transform = 'scale(1)';
 });
-// Let's Go! img... ZOOM ZOOM
+
+// Let's Go! img TRANFORM ... ZOOM ZOOM
 imgInGo.addEventListener('mouseenter', () => {
     imgInGo.style.transform = 'scale(1.1)';
     imgInGo.style.transition = 'all 0.5s';
@@ -83,7 +90,7 @@ imgInGo.addEventListener('mouseenter', () => {
 imgOutGo.addEventListener('mouseleave', () => {
     imgOutGo.style.transform = 'scale(1)';
 });
-// Adventure Awaits img... ZOOM ZOOM
+// Adventure Awaits img TRANFORM ... ZOOM ZOOM
 imgInAwaits.addEventListener('mouseenter', () => {
     imgInAwaits.style.transform = 'scale(1.1)';
     imgInAwaits.style.transition = 'all 0.5s';
@@ -93,23 +100,24 @@ imgOutAwaits.addEventListener('mouseleave', () => {
 });
 
 
-// Sign me up buttons (take visitors to registration pages)
-for (let i = 0; i < x.length; i++)
+// Sign me up BUTTONS event details (needs more events...)
+for (let i = 0; i < bkColor.length; i++)
     button[i].addEventListener('click', event => {
-    button[i].textContent = `Click: ${event.detail}`;
-    console.log(`Fun Bus ${event.detail} in the sun`);
+        button[i].textContent = `Click: ${event.detail}`;
+        console.log(`Fun Bus ${event.detail} in the sun`);
     });
 
 
 
-// Window scrolling + alert (can alert/award a visitor for reading the entire page)
+// Window scrolling + A L E R T !!!
+// (can alert/award a visitor for reading the entire page)
+
 // window.addEventListener('scroll', () => {
 //     console.log('window.scrollY', window.scrollY);
 //     if (window.scrollY >= 1009) {
 //         alert('Congratulations, you are a TOP SCROLLER');
 //     }
 // });
-
 
 //              S T R E C H 
 // ______________ No Time _______________
