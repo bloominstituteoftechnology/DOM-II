8bb226f0a72c28620e41fecaafc666e06940c8b9
Nicole Pizzano <nicolepizzano@Nicoles-iMac.local>
2021-03-14 18:16:15 -0400

    saves

--- js/index.js
+++ js/index.js
@@ -1,1 +1,84 @@
 // Your code goes here
+
+let highlight = document.querySelector('container-home');
+highlight.addEventListener('hover', (event) => {event.target.style.backgroundColor = "pink"; })
+
+
+// 1. for the nav info, changing th ecolors of the links.
+
+const navigation = document.querySelector('.nav')
+ navigation.addEventListener('mouseover', (event) => {
+     if(event.target.style.color === "green"){
+         event.target.style.color = "black"
+     }else {
+         event.target.style.color = "green";
+     }
+ })
+
+
+// 2.turns section lightblue colored when moused upon.
+
+let button = document.querySelector('.content-section');
+ button.addEventListener('mouseenter', (event) => {
+     event.target.style.backgroundColor = 'lightBlue'; });
+
+
+// 3.turns entirety of page pink colored when moused over!
+
+     const body = document.querySelector('body')
+     body.addEventListener("mouseenter", (event) => {
+         event.target.style.backgroundColor = "pink"
+ })
+
+ // 4. double click to spin the image.
+
+ const busPicture = document.querySelectorAll('img')[2]
+ busPicture.addEventListener("mouseover", event =>{
+     event.target.style.transform = "skewy(360deg)";
+     event.target.style.transition = "transform 0.5s";
+ })
+
+// .
+//  document.getElementById("btn").addEventListener("click", function() {
+//     document.getElementById("demo").innerHTML = "Hello World";
+//   });
+
+//   const button = document.querySelector('btn')
+//   button.addEventListener("click", (event) => {
+//       event.target.style.color = "green"
+// })
+
+
+// 5. scroll info in the window
+
+window.addEventListener('scroll', function() {
+	console.log('they see me scrollin);
+});
+// 6. 
+// const button = document.querySelector('btn');
+// button.addEventListener('mouseover', function(){
+//   alert('submit info')
+// })
+
+// 6.password blur/hide feature i added in
+
+const password = document.querySelector('input[type="password"]');
+
+password.addEventListener('focus', (event) => {
+  event.target.style.background = 'pink';
+});
+
+password.addEventListener('blur', (event) => {
+  event.target.style.background = '';
+});
+
+// 7.for the button at the bottom that i added in. creates an alert
+
+const submitButton = document.querySelector('button');
+submitButton.addEventListener('click', (event) =>{
+  alert("wooooah, mondo thanks bromosapien!!")
+})
+
+// 8.
+
+
