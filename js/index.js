a32b1d5b185135106676b4b17e4ef37b4afcedd7
Greg Brewton <gregory-brewton@lambdastudents.com>
2021-03-14 22:06:31 -0600

    Adds more event listeners

--- js/index.js
+++ js/index.js
@@ -1,22 +1,39 @@
 // Your code goes here
-function taskLog(taskID, input, color = "#007AAF") {
-  console.log(`%cTask ${taskID}`, `color: ${color}`);
-  console.log(input);
-}
 const navLinks = document.querySelectorAll(".nav-link");
+const images = document.querySelectorAll("img");
+const coordSpan = document.createElement("span");
 
 navLinks.forEach((link) => {
   link.addEventListener("mouseover", () => link.classList.add("zoom"));
   link.addEventListener("mouseout", () => link.classList.remove("zoom"));
+  link.addEventListener("mousedown", () => link.classList.remove("zoom"));
+  link.addEventListener("mouseup", () => link.classList.add("zoom"));
 });
 
-const images = document.querySelectorAll("img");
 images.forEach((image) => {
   image.addEventListener("click", () => {
-      if(document.fullscreenElement){
-          document.exitFullscreen();
-      } else {
-          image.requestFullscreen();
-      }
+    document.fullscreenElement
+      ? document.exitFullscreen()
+      : image.requestFullscreen();
+  });
+  image.addEventListener("mousedown", () => {
+    image.classList.add("zoomOut");
+  });
+  image.addEventListener("mouseup", () => {
+    image.classList.remove("zoomOut");
   });
 });
+window.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.alert(`You clicked X:${e.clientX}, Y:${e.clientY}`);
+});
+window.addEventListener("mousemove", (e) => {
+  const xPos = e.clientX;
+  const yPos = e.clientY;
+  coordSpan.innerText = `X:${xPos}\nY:${yPos}`;
+});
+coordSpan.style.position = "absolute";
+coordSpan.innerText = "testing";
+coordSpan.style.top = "2em";
+coordSpan.style.left = "2em";
+document.querySelector("body").appendChild(coordSpan);
