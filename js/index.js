a78c7bffcc80ebd4e6a52c37bf9fe560dffa05cb
Greg Brewton <gregory-brewton@lambdastudents.com>
2021-03-14 22:29:12 -0600

    Adds scroll listener

--- js/index.js
+++ js/index.js
@@ -2,12 +2,17 @@
 const navLinks = document.querySelectorAll(".nav-link");
 const images = document.querySelectorAll("img");
 const coordSpan = document.createElement("span");
+const scrollSpan = document.createElement("span");
 
 navLinks.forEach((link) => {
   link.addEventListener("mouseover", () => link.classList.add("zoom"));
   link.addEventListener("mouseout", () => link.classList.remove("zoom"));
   link.addEventListener("mousedown", () => link.classList.remove("zoom"));
   link.addEventListener("mouseup", () => link.classList.add("zoom"));
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.alert(`${e.target} clicked!`);
+  });
 });
 
 images.forEach((image) => {
@@ -30,10 +35,10 @@ window.addEventListener("click", (e) => {
 window.addEventListener("mousemove", (e) => {
   const xPos = e.clientX;
   const yPos = e.clientY;
-  coordSpan.innerText = `X:${xPos}\nY:${yPos}`;
+  coordSpan.innerText = `COORD\nX:${xPos}\nY:${yPos}`;
 });
-coordSpan.style.position = "absolute";
-coordSpan.innerText = "testing";
+coordSpan.style.position = "fixed";
+coordSpan.innerText = "coord";
 coordSpan.style.top = "2em";
 coordSpan.style.left = "2em";
 document.querySelector("body").appendChild(coordSpan);
@@ -41,3 +46,12 @@ document.querySelector("body").appendChild(coordSpan);
 window.addEventListener("load", () =>
   console.log("%cPage Loaded!", "color: limegreen")
 );
+
+scrollSpan.style.position = "fixed";
+scrollSpan.innerText = "scroll";
+scrollSpan.style.top = "2em";
+scrollSpan.style.right = "2em";
+document.querySelector("body").appendChild(scrollSpan);
+window.addEventListener("scroll", () => {
+  scrollSpan.innerText = `SCROLL\nX:${scrollX}\nY:${scrollY}`;
+});
