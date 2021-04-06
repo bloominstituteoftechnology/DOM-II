48888efd8a0ce207143494c007f5ef3d63bfa8f3
ferengitooth <ehitsamill@gmail.com>
2021-04-06 15:53:25 -0700

    keydown and mousever events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+const title = document.querySelector('title');
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+const destination = document.querySelector('.destination')
+const letsGoImage = document.querySelector('.content-section .img-content')
+
+body.addEventListener("keydown", event => {
+    body.style.backgroundColor = 'cyan';
+})
+
+destination.addEventListener("mouseover", event => {
+    destination.style.backgroundColor = 'purple';
+})
+
