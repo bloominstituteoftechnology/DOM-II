d4f0a41a172553c92b6212da3c1413fec543ef14
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-04-06 17:01:17 -0600

    update to index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,49 @@
 // Your code goes here
+// MOUSEOVER
+
+const nav = document.querySelectorAll('.nav-link');
+console.log(nav);
+
+Array.from(document.links).forEach(function(link){
+    link.addEventListener('mouseover', function(event){
+        link.style.color = 'red';
+    })
+})
+
+// AUXCLICK
+const footer = document.querySelector('.footer p');
+
+footer.addEventListener('auxclick', function(event){
+    event.target.style.background = 'yellow';
+});
+
+
+// DBLCLICK
+const h2 = document.querySelector('h2');
+
+h2.addEventListener('dblclick', function(e){
+    h2.style.fontSize = '2rem';
+   
+});
+
+// COPY
+document.addEventListener('copy', function (event){
+    console.log('copy action has been taken');
+})
+// KEYDOWN
+document.addEventListener('keydown', function(event){
+    console.log('keyboard has been used.');
+})
+// CLICK
+const button = document.querySelectorAll('.btn');
+
+Array.from(button).forEach(function(btn){
+    btn.addEventListener('click', function(event){
+        btn.style.color = 'green';
+    })
+})
+
+// POINTOVER
+document.addEventListener('pointerover', function (event){
+    console.log('Pointover has occured');
+})
