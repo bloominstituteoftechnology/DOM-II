aa3b1a8ca906c9ac5a7432e3cfe71977187a4a99
adamhunt <adam-hunt@lambdastudents.com>
2021-04-06 18:21:30 -0500

    5

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
-// Your code goes here//
+const navBar = document.querySelector("nav");
+console.log(navBar);
+
+navBar.addEventListener("mouseenter", function(event) {
+    event.target.style.color = "red";
+
+setTimeout(function() {
+    event.target.style.color = "";
+}, 500);
+}, false);
+
+navBar.addEventListener("mouseover", function(event) {
+    event.target.style.color = "blue";
+
+setTimeout(function() {
+    event.target.style.color = "";
+}, 1000);
+}, false);
+
+navBar.addEventListener("mmouseover", function(event) {
+    event.target.style.color="green"
+} )
