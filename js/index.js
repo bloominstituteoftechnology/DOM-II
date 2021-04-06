73c66d0c36290aa446f03cbece8ee7396ce9d76b
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-04-06 19:59:08 -0400

    Completed 3/10 task - Pushing 'w' will change the 'p'text color to white

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 //Elements from the DOM
 const signUpBtn = document.querySelector(".btn");
 const firstImg = document.querySelector('#first-img');
-console.log(signUpBtn);
+const paragraph = document.querySelectorAll('p')
 
 //One
 signUpBtn.addEventListener("click",function(event){
@@ -13,5 +13,13 @@ signUpBtn.addEventListener("click",function(event){
 
 //two
 firstImg.addEventListener("mouseover",function(event){
-firstImg.style.filter = "grayscale(100%)";
+    firstImg.style.filter = "grayscale(100%)";
 } )
+
+// three
+document.addEventListener("keydown",function(event){
+    if(event.key === "w"){
+        console.log(paragraph);
+        paragraph.forEach(item => item.style.color = "white");
+    }
+})
