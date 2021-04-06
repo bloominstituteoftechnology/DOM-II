ee4d805be8763d0f45a304265dca55ad6dcb2b2e
thekid510 <76237067+thekid510@users.noreply.github.com>
2021-04-06 14:47:29 -0700

    1 event done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const buttons = document.querySelectorAll(".btn");
+    buttons.forEach(btn => {btn.addEventListener("click",
+    (event)=> {event.target.style.backgroundColor = 'red';});
+
+    })
