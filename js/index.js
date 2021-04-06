d16dbae701f2696a94beceabae5c97944423a14d
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-04-06 19:45:35 -0400

    Completed task 2/10 - Mouse over first img adds a grayscale filter

--- js/index.js
+++ js/index.js
@@ -1,5 +1,8 @@
 // Your code goes here
+
+//Elements from the DOM
 const signUpBtn = document.querySelector(".btn");
+const firstImg = document.querySelector('#first-img');
 console.log(signUpBtn);
 
 //One
@@ -8,3 +11,7 @@ signUpBtn.addEventListener("click",function(event){
 })
 
 
+//two
+firstImg.addEventListener("mouseover",function(event){
+firstImg.style.filter = "grayscale(100%)";
+} )
