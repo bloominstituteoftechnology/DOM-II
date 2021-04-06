e9cfed12e80315f7e33d0c649007973dc959dc6d
ricardo-pena <ricardo.penagarcia06@gmail.com>
2021-04-06 19:30:11 -0400

    One done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+const signUpBtn = document.querySelector(".btn");
+console.log(signUpBtn);
+
+//One
+signUpBtn.addEventListener("click",function(event){
+    signUpBtn.style.background = 'red';
+})
+
+
