af0d7a0e8764d46474e585d9666ece6e6c512882
nwraimo <raimo.nick9@gmail.com>
2021-04-06 16:21:00 -0700

    working

--- js/index.js
+++ js/index.js
@@ -1,1 +1,4 @@
 // Your code goes here
+const launchButton = document.querySelector("#launchButton");
+const confirmButton = document.querySelector("#confirmButton");
+const cancelButton = document.querySelector("#cancelButton");
