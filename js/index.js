214685b90f0db95a83cbad19d34fd1e443debc43
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-04-11 21:22:38 -0500

    made a hover effect on nav-link buttons

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+const highlight = document.querySelector('.nav')
+
+highlight.addEventListener('mouseover',function(event)
+{
+    event.target.style.color = 'red'
+})
+highlight.addEventListener('mouseout',function(event)
+{
+    event.target.style.color = 'black'
+})
