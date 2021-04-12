3ff658d4218ee7806d4daa1d165d29eddb6e5d86
Purefallen11 <53922216+Purefallen11@users.noreply.github.com>
2021-04-11 22:03:14 -0500

    made second event listener, rotate logo on top nav

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
 // Your code goes here
 
+
+//1st event, change color of nav links
 const highlight = document.querySelector('.nav')
 
 highlight.addEventListener('mouseover',function(event)
@@ -10,3 +12,14 @@ highlight.addEventListener('mouseout',function(event)
 {
     event.target.style.color = 'black'
 })
+
+
+//2nd event, rotate logo
+const turnAround = document.querySelector('.logo-heading')
+
+turnAround.addEventListener('dblclick',clockWise,false)
+
+function clockWise(event){
+    event.target.style.transform = 'rotate(20deg)'
+}
+
