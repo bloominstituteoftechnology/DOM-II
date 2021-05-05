1f28b299f7f59f36b8a18e448ca86be4c6b837d0
Fernando Velasco <fervelgo@gmail.com>
2021-05-04 23:14:09 -0500

    added multiple eventlisteners

--- js/index.js
+++ js/index.js
@@ -2,20 +2,20 @@
 
 // * `mouseover` -
 //   * `keydown` -
-//   * `wheel`
+//   * `wheel`-
 //   * `load` 
 //   * `focus` -
-//   * `resize`
+//   * `resize` -
 //   * `scroll` -
 //   * `select`
-//   * `dblclick`
+//   * `dblclick`-
 //   * `drag / drop`
 
 //Selectors
 
 //Nav
 let navButtons = document.querySelector('nav a');
-//Header
+//HeaderImg
 let topImage = document.querySelector('.intro img');
 //Mid Content Section
 let letsGo = document.querySelector('.content-section h2'); //checar si este selector también  modifica el otro content-section h2 o sólo Let's go.
@@ -37,3 +37,31 @@ navButtons.addEventListener('mouseover', (event) => {
     event.target.style.text-shadow = 2px 2px lightgrey;
 });
 
+//HeaderImg
+let lastScrollPositionY = 0;
+topImage.addEventListener('scroll', (event) => {
+    event.lastScrollPositionY = window.scrollY;
+});
+
+function resize (event) {
+    event.target.style.width = 100%;
+    event.target.style.height = 30%;
+}
+
+topImage.addEventListener('resize', resize);
+
+////Mid Content Section
+contentSectionImg.addEventListener('click', (event)=> {
+    event.target.style.opacity = .9;
+});
+//Buttons
+function changeTextColor(event) {
+    event.target.style.color = blue;
+}
+
+signUpButton.addEventListener('wheel', changeTextColor);
+
+//Footer
+footerText.addEventListener('dblclick', (event) => {
+    event.target.style.font-size = 15px;
+})
