dbeb0032ebb65cd5bb9cb8f1b59010aa81c5df6d
Apryl Smith <aprylsmith@Ryles-iMac.attlocal.net>
2021-05-09 12:06:17 -0400

    add search button and input field

--- js/index.js
+++ js/index.js
@@ -1,11 +1,13 @@
 // Your code goes here
-const image = document.querySelectorAll('img');
+const allImg = document.querySelectorAll('img');
 const bottom = document.querySelector('footer');
-const main1 = document.querySelector('body');
+const body = document.querySelector('body');
 const firstSec = document.querySelector('.intro');
 const firstSecImg = document.querySelector('.intro > img');
 const firstSecHead = document.querySelector('.intro > h2');
 const firstSecPara = document.querySelector('.intro > p');
+const navLinks = document.querySelector('.nav');
+const navContainer = document.querySelector('.nav-container');
 
 //using a forEach on all links in the document after converting them into an array using array.from()
 const allLinks = document.links
@@ -22,23 +24,27 @@ allLinksArray.forEach(link => {
     link.addEventListener('focus', e => {
         link.style.backgroundColor = "#FFEBCD";
     })
+
+    link.addEventListener('focusout', e => {
+        link.style.backgroundColor = "white";
+    })
 })
 
      
-for(let i=0; i < image.length; i += 1){
-image[i].addEventListener('click', e => {
-    image[i].style.borderRadius = "50%";
+for(let i=0; i < allImg.length; i += 1){
+allImg[i].addEventListener('click', e => {
+    allImg[i].style.borderRadius = "50%";
 });
 }
 
-main1.addEventListener('keydown', e => {
+body.addEventListener('keydown', e => {
     if(event.key === "ArrowDown"){
-        main1.style.background = "linear-gradient(to bottom, #fff 30%, #FFEBCD)";
+        body.style.background = "linear-gradient(to bottom, #fff 30%, #FFEBCD)";
     }
 });
 
-main1.addEventListener('wheel', e => {
-    main1.style.background = "linear-gradient(to bottom, #fff 30%, #FFEBCD)";
+body.addEventListener('wheel', e => {
+    body.style.background = "linear-gradient(to bottom, #fff 30%, #FFEBCD)";
     firstSec.style.display = "flex";
     firstSec.style.flexDirection ="column";
     firstSec.style.alignItems ="center";
@@ -48,8 +54,26 @@ main1.addEventListener('wheel', e => {
     firstSec.style.height = "20%";
 });
 
-//const navBorder = document.querySelector('.main-navigation');
+
 const navBorder = document.querySelector('.main-navigation');
 document.addEventListener('scroll', e => {
     navBorder.style.borderBottom = "2px solid #C0C0C0";
 })
+
+firstSecImg.addEventListener('dblclick', e => {
+    firstSecImg.style.border = "2px solid #c0c0c0";
+});
+
+const inputField = document.createElement('input');
+document.querySelector('.nav-container').appendChild(inputField)
+inputField.style.width = "10%";
+
+const inputBtn = document.createElement('button');
+document.querySelector('.nav-container').appendChild(inputBtn)
+inputBtn.innerHTML = "SEARCH";
+inputBtn.style.backgroundColor = "#17A2B8";
+inputBtn.style.border = "none";
+inputBtn.style.color= "white";
+inputBtn.style.padding = "12px 20px";
+inputBtn.style.textAlign= "center";
+inputBtn.style.fontSize = "10px";
