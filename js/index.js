e72391fef516a36be5a69958197e309a50ebcbcf
Greg Brewton <gbrewton1980@gmail.com>
2021-05-09 19:29:10 -0600

    Refactors code + Semantic args + Comments + DRYer code

--- js/index.js
+++ js/index.js
@@ -14,42 +14,80 @@ scrollSpan.style.right = "2em";
 document.querySelector("body").appendChild(coordSpan);
 document.querySelector("body").appendChild(scrollSpan);
 
-navLinks.forEach((link) => {
-  link.addEventListener("mouseover", () => link.classList.add("zoom"));
-  link.addEventListener("mouseout", () => link.classList.remove("zoom"));
-  link.addEventListener("mousedown", () => link.classList.remove("zoom"));
-  link.addEventListener("mouseup", () => link.classList.add("zoom"));
-  link.addEventListener("click", (e) => {
-    e.preventDefault();
-    window.alert(`${e.target} clicked!`);
+const toggleZoom = (link) => {
+  // destructure object provided as argument
+  const { element, value } = link;
+  // add or remove zoom class
+  switch (value) {
+    case true:
+      element.classList.add("zoom");
+      break;
+    default:
+      element.classList.remove("zoom");
+      break;
+  }
+};
+
+const addEventListeners = (link) => {
+  // enlarge button when moused over
+  link.addEventListener("mouseover", () =>
+    toggleZoom({ element: link, value: true })
+  );
+  // return to normal size when cursor moves away from element
+  link.addEventListener("mouseout", () =>
+    toggleZoom({ element: link, value: false })
+  );
+
+  // make button animate when clicked, keeping it small until you let the mouse button go
+  link.addEventListener("mousedown", () =>
+    toggleZoom({ element: link, value: false })
+  );
+  link.addEventListener("mouseup", () =>
+    toggleZoom({ element: link, value: true })
+  );
+
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    console.log(`${event.target} clicked!`);
   });
+};
+
+navLinks.forEach((link) => {
+  addEventListeners(link);
 });
+
 images.forEach((image) => {
-  image.addEventListener("click", (e) => {
-    e.stopPropagation();
+  image.setAttribute("title", "Click me!");
+  image.addEventListener("click", (event) => {
+    event.stopPropagation();
     document.fullscreenElement
       ? document.exitFullscreen()
       : image.requestFullscreen();
   });
   image.addEventListener("mousedown", () => {
+    // scale image while mouse held down for a "bounce" effect on fullscreen selection
     image.classList.add("zoomOut");
   });
   image.addEventListener("mouseup", () => {
     image.classList.remove("zoomOut");
   });
 });
-window.addEventListener("click", (e) => {
-  e.preventDefault();
-  window.alert(`You clicked X:${e.clientX}, Y:${e.clientY}`);
+
+window.addEventListener("click", (event) => {
+  event.preventDefault();
+  console.log(`You clicked X:${event.clientX}, Y:${event.clientY}`);
 });
-window.addEventListener("mousemove", (e) => {
-  const xPos = e.clientX;
-  const yPos = e.clientY;
+
+window.addEventListener("mousemove", (event) => {
+  const xPos = event.clientX;
+  const yPos = event.clientY;
   coordSpan.innerText = `COORD\nX:${xPos}\nY:${yPos}`;
 });
+
 window.addEventListener("load", () =>
   console.log("%cPage Loaded!", "color: limegreen")
 );
+
 window.addEventListener("scroll", () => {
   scrollSpan.innerText = `SCROLL\nX:${scrollX}\nY:${scrollY}`;
 });
