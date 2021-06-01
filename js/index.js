390f5feb0d712c2c82cbbae11d800de714c114f6
Fernando Velasco <fervelgo@gmail.com>
2021-06-01 16:17:41 -0600

    corrected basic mistakes, added new triggers

--- js/index.js
+++ js/index.js
@@ -14,27 +14,27 @@
 //Selectors
 
 //Nav
-let navButtons = document.querySelector('nav a');
+const navButtons = document.querySelector('nav a');
 //HeaderImg
-let topImage = document.querySelector('.intro img');
+const topImage = document.querySelector('.intro img');
 //Mid Content Section
-let letsGo = document.querySelector('.content-section h2'); //checar si este selector también  modifica el otro content-section h2 o sólo Let's go.
-let contentSectionImg = document.querySelector('.content-section img')
+const letsGo = document.querySelector('.content-section h2'); //checar si este selector también  modifica el otro content-section h2 o sólo Let's go.
+const contentSectionImg = document.querySelector('.content-section img')
 //Content Destination
-let sectionDest = document.querySelector('.home .content-destination')
+const sectionDest = document.querySelector('.home .content-destination')
 //Content-Pick bottom
-let topLogo = document.querySelector('.home .content-pick');
-let signUpButton = document.querySelector('.btn');
+const topLogo = document.querySelector('.home .content-pick');
+const signUpButton = document.querySelector('.btn');
 //Footer
-let footerText = document.querySelector('.footer')
+const footerText = document.querySelector('.footer')
 
 //Event Listeners and Methods
 
 //Nav
 navButtons.addEventListener('mouseover', (event) => {
-    event.target.style.color = "blue";
-    event.target.style.font-size = 10px;
-    event.target.style.text-shadow = 2px 2px lightgrey;
+    event.target.style.color = "blue"
+    //event.target.style.font-size = 10px
+    //event.target.style.text-shadow = 2px 2px lightgrey
 });
 
 //HeaderImg
@@ -43,25 +43,33 @@ topImage.addEventListener('scroll', (event) => {
     event.lastScrollPositionY = window.scrollY;
 });
 
-function resize (event) {
-    event.target.style.width = 100%;
-    event.target.style.height = 30%;
+function resize (event) {   
+   console.log("resize!")
 }
 
 topImage.addEventListener('resize', resize);
 
+//letsGo
+
+letsGo.addEventListener('click', (event) => {
+    event.target.style.color = 'red';
+    event.target.style.fontSize = 10;
+})
+
+
 ////Mid Content Section
 contentSectionImg.addEventListener('click', (event)=> {
     event.target.style.opacity = .9;
-});
+})
+
 //Buttons
 function changeTextColor(event) {
-    event.target.style.color = blue;
+    event.target.style.color = "blue";
 }
 
 signUpButton.addEventListener('wheel', changeTextColor);
 
 //Footer
 footerText.addEventListener('dblclick', (event) => {
-    event.target.style.font-size = 15px;
+    event.target.style.fontSize = 15;
 })
