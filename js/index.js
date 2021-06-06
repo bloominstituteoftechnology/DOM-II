ae57dbfe091118e8ebed585ac8ffcdb9c7c640ba
unknown <jlarson129@outlook.com>
2021-06-06 14:20:28 -0400

    added mouseover event to move bus across screen

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 // Your code goes here
+const logo = document.querySelector('.logo-heading')
+let run = true;
+logo.addEventListener('mouseover', event => {
+    if (run === true) {
+        let elem = document.getElementById("bus"),
+            speed = 15,
+            currentPos = 0;
+        elem.style.top = window.screen.height/2 + "px";
+        elem.style.left = 0+"px";
+        run = false;
+        elem.style.display = "block";
+        elem.style.position = "fixed";
+        let motionInterval = setInterval(function() {
+            currentPos += speed;
+            if (currentPos >= window.screen.width && speed > 0) {
+            currentPos = 0;
+            speed = -1;
+            elem.style.left = 0+"px";
+            elem.style.display = "none";
+            run = true;
+            }
+            if (currentPos <= 0 && speed < 0) {
+            clearInterval(motionInterval);
+            }
+            elem.style.left = currentPos+"px";
+        },30);
+    }
+});
+
+const busimg = document.querySelector('#sandbus');
+
+busimg.addEventListener('click', event => {
+    var honk = document.getElementById("honk");
+    honk.play();
+})
