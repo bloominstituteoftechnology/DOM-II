7d8f286fb50832064fb171b6e24e44bc9fc44456
Collin-Falstad <wildkard91@gmail.com>
2021-06-06 15:16:47 -0400

    added scroll background color change event

--- js/index.js
+++ js/index.js
@@ -1,8 +1,20 @@
 // Your code goes here
+// * `mouseover`
+// * `keydown`
+// * `wheel`
+// * `load`
+// * `focus`
+// * `resize`
+// * `scroll`
+// * `select`
+// * `dblclick`
+// * `drag / drop`
+
+
+//adds subtext to the main header if mouse over
 const logoImg = document.querySelector("h1.logo-heading")
 const logoSub = document.querySelector("h1.logo-subtext")
 
-//adds subtext to the main header if mouse over
 logoImg.addEventListener('mouseover', () => {
 logoSub.classList.remove('off')
 })
@@ -12,28 +24,39 @@ logoImg.addEventListener('mouseout', () => {
     })
 
 //add interactivity to nav bar upon clicks
-    const  navItem = document.querySelectorAll('a')
+const  navItem = document.querySelectorAll('a')
    
-    navItem[0].addEventListener("click", e => {
-        e.target.style.fontSize = "4rem"
-    })
+navItem[0].addEventListener("click", e => {
+    e.target.style.fontSize = "4rem"
+})
 
-    navItem[1].addEventListener("click", e => {
-        e.target.style.color = "green"
-    })
+navItem[1].addEventListener("click", e => {
+    e.target.style.color = "green"
+})
 
-    navItem[2].addEventListener("click", e => {
-        e.target.style.color = "pink"
-    })
+navItem[2].addEventListener("click", e => {
+    e.target.style.color = "pink"
+})
 
-    navItem[3].addEventListener("click", e => {
-        e.target.style.color = "blue"
-    })
+navItem[3].addEventListener("click", e => {
+    e.target.style.color = "blue"
+})
 
-    // const  navItem = document.querySelector('.nav')
-    // navItem1 = navItem.firstElementChild
-    // navItem1.addEventListener("mouseover", e => {
-    //     e.target.style.fontSize = "4rem"
-    // })
+//double click to remove image
+
+// const advImg = document.getElementsByClassName("img-content")
+
+// advImg.addEventListener("dblclick", e => {
+//     e.target.style.display = "none"
+// })
 
 
+//add color change to body with scroll wheel
+bodyScreen = document.querySelector("body")
+bodyScreen.addEventListener("wheel" , () => {
+    bodyScreen.style.backgroundColor = "blue"
+    bodyScreen.style.color = "white"
+    logoImg.style.color = "black"
+    logoSub.style.color = "black"
+    // stopPropogation()
+})
