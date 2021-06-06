928716c984777e1f0aeae05794f06ae59ea07087
Collin-Falstad <wildkard91@gmail.com>
2021-06-06 15:03:22 -0400

    added interactivity to nav bar

--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 // Your code goes here
 const logoImg = document.querySelector("h1.logo-heading")
 const logoSub = document.querySelector("h1.logo-subtext")
-const navItem = document.querySelectorAll("nav.nav-link")
-
-
 
 //adds subtext to the main header if mouse over
 logoImg.addEventListener('mouseover', () => {
@@ -14,6 +11,29 @@ logoImg.addEventListener('mouseout', () => {
     logoSub.classList.add('off')
     })
 
-navItem.addEventListener('click', () => {
-})
+//add interactivity to nav bar upon clicks
+    const  navItem = document.querySelectorAll('a')
+   
+    navItem[0].addEventListener("click", e => {
+        e.target.style.fontSize = "4rem"
+    })
+
+    navItem[1].addEventListener("click", e => {
+        e.target.style.color = "green"
+    })
+
+    navItem[2].addEventListener("click", e => {
+        e.target.style.color = "pink"
+    })
+
+    navItem[3].addEventListener("click", e => {
+        e.target.style.color = "blue"
+    })
+
+    // const  navItem = document.querySelector('.nav')
+    // navItem1 = navItem.firstElementChild
+    // navItem1.addEventListener("mouseover", e => {
+    //     e.target.style.fontSize = "4rem"
+    // })
+
 
