a31e298051fa1bdbf169fcd22b420657d7015737
Collin-Falstad <wildkard91@gmail.com>
2021-06-06 14:21:20 -0400

    added event listener for header subtext

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+const logoImg = document.querySelector("h1.logo-heading")
+const logoSub = document.querySelector("h1.logo-subtext")
+const navItem = document.querySelectorAll("nav.nav-link")
+
+
+
+//adds subtext to the main header if mouse over
+logoImg.addEventListener('mouseover', () => {
+logoSub.classList.remove('off')
+})
+
+logoImg.addEventListener('mouseout', () => {
+    logoSub.classList.add('off')
+    })
+
+navItem.addEventListener('click', () => {
+})
+
