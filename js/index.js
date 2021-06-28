ce77f3706a7a5b5484898c3811a078ff7698e218
George Cavazos <cavazosgeorge@yahoo.com>
2021-06-28 11:51:29 -0400

    add link color event && image click event

--- js/index.js
+++ js/index.js
@@ -12,3 +12,26 @@ const content = document.querySelectorAll(".content-section");
 const img = document.querySelectorAll("img");
 const destination = document.querySelectorAll(".destination");
 const footer = document.querySelector("footer");
+
+// Event listeners
+
+const allLinks = document.links;
+const allLinksArray = Array.from(allLinks);
+
+allLinksArray.forEach((link) => {
+  link.addEventListener("mouseover", (event) => {
+    link.style.color = "crimson";
+  });
+});
+
+allLinksArray.forEach((link) => {
+  link.addEventListener("mouseout", (event) => {
+    link.style.color = "green";
+  });
+});
+
+for (let i = 0; img.length; i++) {
+  img[i].addEventListener("click", (event) => {
+    img[i].style.borderRadius = "50%";
+  });
+}
