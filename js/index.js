020138d8c30dbdcca419bf8a16a195af56e44737
George Cavazos <cavazosgeorge@yahoo.com>
2021-06-28 10:55:46 -0400

    "Add selectors"

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+// Selectors
+const intro = document.querySelector(".intro");
+const introImg = document.querySelectorAll(".intro > img");
+const introHeader = document.querySelector(".intro > h2");
+const introPara = document.querySelector(".intro > p");
+const navLinks = document.querySelector(".nav");
+const navContainer = document.querySelector(".nav-container");
+const body = document.querySelector("body");
+const content = document.querySelectorAll(".content-section");
+const img = document.querySelectorAll("img");
+const destination = document.querySelectorAll(".destination");
+const footer = document.querySelector("footer");
