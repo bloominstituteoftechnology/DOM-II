5af123d8545b553581b6f11e3e0464ef48d3a18d
Rick <rmansfield@realpeoplesratings.com>
2021-07-03 09:59:52 -0500

    Set up modal

--- js/index.js
+++ js/index.js
@@ -11,5 +11,45 @@ console.log(headerH1
         event.target.style.backgroundColor = 'white';
     });
 
-//2 Keydown 
-//
+//2 Keydown <h4> "Sign Me Up!"
+//Step 1 add modal
+const newParentDivModal = document.createElement('div');
+console.log(newParentDivModal);
+newParentDivModal.classList.add('modal');
+newParentDivModal.classList.add('off');
+
+const divChild1 = document.createElement('div');
+console.log(divChild1);
+divChild1.classList.add('modal-opacity');
+newParentDivModal.appendChild(divChild1);
+
+const divChild2 = document.createElement('div');
+console.log(divChild2);
+divChild2.classList.add('modal-dialog');
+newParentDivModal.appendChild(divChild2);
+
+const messageSure = document.createElement('h4');
+messageSure.setAttribute('id', "youSure");
+console.log(messageSure);
+
+
+const buttonY = document.createElement('button');
+console.log(buttonY);
+buttonY.setAttribute('id', "confirmationButton");
+buttonY.textContent= `Yes, let's rock`;
+
+const buttonN = document.createElement('button');
+buttonN.setAttribute('id', "cancelButton");
+buttonN.textContent = "No take me back";
+console.log(buttonN);
+
+
+// const signMeUph4 = document.querySelectorAll('h4');
+// console.log(signMeUph4);
+// signMeUph4.forEach((elem) => {
+//     elem.classList.add('launchAlert');
+// });
+// signMeUph4[0].classList.add('message0');
+// signMeUph4[1].classList.add('message1');
+// signMeUph4[2].classList.add('message2');
+
