5b2e69701dc1e033d85b7c705a4971ee495993d7
Kyle Andrews <andrewskr90@gmail.com>
2021-07-06 16:55:09 -0700

    2 events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+//ideas:
+//  mirror photo while mousedown
+//  links change color and become bold while onmouseover then onmouseout
+//  window.alert, or window.prompt this pops up a window with an alert, that way you don't need to ajust html
+
+const images = document.querySelectorAll('img')
+
+images.forEach(function(ele) {
+    ele.addEventListener('mouseenter', e => e.target.style.transform = 'scaleX(-1)')
+})
+images.forEach(function(ele) {
+    ele.addEventListener('mouseleave', e => e.target.style.transform = 'scaleX(1)')
+})
+
+const title = document.querySelector('.logo-heading')
+
+
+title.addEventListener('click', e => e.target.style.backgroundColor = 'gold')
+
