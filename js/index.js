6c2fcd8fe00b9bd4891cb3ef98060f2acf529f66
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-07-06 14:41:14 -0600

    Finished three more tasks.

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
 let nav_links = document.querySelectorAll(".nav-link");
 nav_links.forEach((element) => {
   element.addEventListener("mouseover", function (event) {
-    this["style"]["color"] = "purple";
+    this.style.color = "purple";
   });
   element.addEventListener("mouseout", function (event) {
     element.removeAttribute("style");
@@ -14,21 +14,39 @@ nav_links.forEach((element) => {
 //2
 let logo_heading = document.querySelector(".logo-heading");
 logo_heading.addEventListener("click", function (event) {
-  this["style"]["color"] = "red";
+  this.style.color = "red";
 });
 
 //3
 logo_heading.addEventListener("dblclick", function (event) {
-  this["style"]["color"] = "blue";
+  this.style.color = "blue";
 });
 
 //4
 let intro = document.querySelector(".intro");
 intro.addEventListener("mouseover", function (event) {
-  this["style"]["color"] = "orange";
+  this.style.color = "orange";
 });
 intro.addEventListener("mouseout", function (event) {
   intro.removeAttribute("style");
 });
 
 //5
+
+let text = document.querySelector(".text-content");
+text.addEventListener(`select`, function (event) {
+  text.style.color = `blue`;
+});
+
+//6
+
+let page = window;
+page.addEventListener("load", function (event) {
+  alert(`Press 'N' to toggle between night mode`);
+});
+
+//7
+
+text.addEventListener("click", function (event) {
+  alert("Time for fun!");
+});
