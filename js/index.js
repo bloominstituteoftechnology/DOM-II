44c19961d93c11ac95b3f911f5a21c81bb501f8d
Rory Roberts <82188608+Rory-Ray@users.noreply.github.com>
2021-07-06 14:03:57 -0600

    First four down.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+
+//1
+let nav_links = document.querySelectorAll(".nav-link");
+nav_links.forEach((element) => {
+  element.addEventListener("mouseover", function (event) {
+    this["style"]["color"] = "purple";
+  });
+  element.addEventListener("mouseout", function (event) {
+    element.removeAttribute("style");
+  });
+});
+
+//2
+let logo_heading = document.querySelector(".logo-heading");
+logo_heading.addEventListener("click", function (event) {
+  this["style"]["color"] = "red";
+});
+
+//3
+logo_heading.addEventListener("dblclick", function (event) {
+  this["style"]["color"] = "blue";
+});
+
+//4
+let intro = document.querySelector(".intro");
+intro.addEventListener("mouseover", function (event) {
+  this["style"]["color"] = "orange";
+});
+intro.addEventListener("mouseout", function (event) {
+  intro.removeAttribute("style");
+});
+
+//5
