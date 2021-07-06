88f6abc1cda6a4bcade84bf4a80dd0828b5d17b3
Klove Adams <kalebsxott@gmail.com>
2021-07-06 18:17:29 -0400

    almost there

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-// Your code goes here
+const header = document.querySelector("header.main-navigation")
+window.addEventListener("load", (_e) => {
+    header.style.backgroundColor = "mistyrose"
+})
+const buttons = document.querySelector(".btn")
+buttons.addEventListener("click", (_e) => {
+    buttons.style.backgroundColor = "red"
+})
+buttons.addEventListener("dblclick", (_e) => {
+    buttons.style.backgroundColor = "#17A2B8"
+})
+header.addEventListener("mouseover", (_e) => {
+    header.style.backgroundColor = "white"
+})
+const welcome = document.querySelector(".intro h2")
+window.addEventListener("keydown", (event) => {
+    if(event.keyCode === 90) {
+        return welcome.style.fontSize = "4.5rem"
+    }
+})
+window.addEventListener("focus", (_e) => {
+    welcome.style.fontSize = "3.2rem"
+})
+const bodyText = document.querySelector("body")
+bodyText.addEventListener("wheel", (_e) => {
+    bodyText.style.color = "#4f4f4f"
+})
+window.addEventListener("resize", (_e) => {
+    bodyText.style.backgroundColor = "mistyrose"
+})
+const cp = document.querySelector(".destination")
+window.addEventListener("scroll", (_e) => {
+    cp.style.color = "green"
+})
+window.addEventListener("select", (_e) => {
+    bodyText.style.color = "red"
+})
+// addEventListener("select")
+
