84259335e24766acb8313bb2a1a9bdfbff708714
juliane1999 <lastjuliane@gmail.com>
2021-07-06 22:59:05 -0700

    second commit

--- js/index.js
+++ js/index.js
@@ -1,2 +1,21 @@
 // Your code goes here
-document.querySelector('.nav-link').addEventListener
+
+const container = document.querySelector('.home');
+console.log(container);
+
+const buttonAlert = document.querySelectorAll('section.content-pick .destination .btn')
+
+
+function alert (){
+    buttonAlert.addEventListener('click', alert);
+    alert ('Signing Up!');
+}
+
+function alert (){
+    buttonAlert.addEventListener('click', function(){
+        this.style.backgroundColor='red';
+    })
+}
+
+
+
