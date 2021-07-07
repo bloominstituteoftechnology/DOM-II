aab14c89fd68601957264bd368ce2a5732bcf807
KRQuinn <kr.quinn82@gmail.com>
2021-07-06 21:45:40 -0700

    more work done.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // Your code goes here
+
+
 document.addEventListener("mouseover", element => {
     element.target.style.color = "#8FBC8F";
 })
@@ -8,17 +10,30 @@ document.addEventListener("mouseout", element => {
 
 
 let imgCont = document.querySelectorAll(".img-content");
-document.addEventListener("keydown ", element => {
+document.addEventListener("keydown ", function() {
     imgCont.forEach(element => {
         element.style.opacity = "0.2 ";
     })
 })
 
-document.addEventListener("wheel", element => {
-    
+
+window.addEventListener("load", event => {
+    document.querySelector("body").style.backgroundColor = "#B0E0E6";
 })
 
-document.addEventListener("load", element => {
-    
+
+let p = document.querySelectorAll("p");
+document.addEventListener("wheel", element => {
+    if(element.deltaY < 0){
+        p.forEach(e => {
+            e.style.fontSize = ".5rem";
+        })
+    }
+    else if (element.deltaY > 0){
+        p.forEach(e => {
+            e.style.fontSize = "2rem";
+        })
+    }
 })
+
 
