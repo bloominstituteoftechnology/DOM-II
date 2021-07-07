0344b536be3e6368ea2a5c63c72e35178df6869a
KRQuinn <kr.quinn82@gmail.com>
2021-07-06 19:01:48 -0700

    2 listener events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 // Your code goes here
+document.addEventListener("mouseover", element => {
+    element.target.style.color = "gray";
+})
+document.addEventListener("mouseout", element => {
+    element.target.style.color = "black";
+})
+
+
+
+document.addEventListener("keydown", element => {
+
+})
+
+document.addEventListener("wheel", element => {
+    
+})
+
+document.addEventListener("load", element => {
+    
+})
+
