fb32fdb850b4e00c4dcff4de2bcf93b44de1f721
KRQuinn <kr.quinn82@gmail.com>
2021-07-06 21:25:17 -0700

    3 listeners active

--- js/index.js
+++ js/index.js
@@ -1,15 +1,17 @@
 // Your code goes here
 document.addEventListener("mouseover", element => {
-    element.target.style.color = "gray";
+    element.target.style.color = "#8FBC8F";
 })
 document.addEventListener("mouseout", element => {
     element.target.style.color = "black";
 })
 
 
-
-document.addEventListener("keydown", element => {
-
+let imgCont = document.querySelectorAll(".img-content");
+document.addEventListener("keydown ", element => {
+    imgCont.forEach(element => {
+        element.style.opacity = "0.2 ";
+    })
 })
 
 document.addEventListener("wheel", element => {
