04bbfabaf0f4928b0fddaf8b6f13e9c36fa5c338
Richard Oh <n24512@protonmail.com>
2021-08-02 19:14:19 -0500

    update, one more event handler to go

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 // mouseenter and mouseleave event on the main picture
+// turns all "Fun Bus" texts pink
 const introContainer = document.querySelector('.intro');
 const mainNavigation = document.querySelector('.main-navigation');
 const busImg = document.querySelector('.intro img');
@@ -43,7 +44,8 @@ btnsArray[2].addEventListener('mouseleave', event => {
     btnsArray[2].textContent = 'Sign Me Up!';
 });
 
-// keydown event on the 'd' and 'l' key to toggle dark/light mode
+// keydown event on the 'd' and 'l' key 
+// toggles dark/light mode for page
 document.addEventListener('keydown', event => {
     if (event.key === 'd') {
         document.body.style = 'color: white; background-color: #111111';
@@ -53,27 +55,96 @@ document.addEventListener('keydown', event => {
         document.body.style = '';
         event.stopPropagation();
     }
-})
+});
 
-// busImg.addEventListener('mouseenter', event => {
-//     introContainer.style.overflow = 'hidden';
-//     busImg.style.transition = 'transform';
-//     busImg.style.transform = 'scale(1.5)';
-// });
-// busImg.addEventListener('mouseleave', event => {
-//     busImg.style.transition = 'transform';
-//     busImg.style.transform = 'scale(1)';
-// });
+// click key event on the last image
+// replaces h2 of last section to a random location name
+const destinationDiv = document.querySelector('.content-destination')
+const destinationImg = document.querySelector('.content-destination img');
+const destinationH2 = document.querySelector('.content-destination h2')
 
-// const linksAndHeaders = document.getElementsByTagName('a h1 h2 h4');
-// linksAndHeaders.addEventListener('keydown', event => {
-//     const colors = ['red', 'blue', 'green', 'black', 'purple'];
-//     let randomColors = Math.floor(Math.random() * 5);
-//     linksAndHeaders.style.color = 'red'
-// })
+destinationImg.addEventListener('mouseenter', event => {
+    destinationH2.style.textDecoration = 'underline';
+    destinationH2.textContent = 'Click The Image!';
+    destinationImg.style.border = 'solid 5px red';
+});
+destinationImg.addEventListener('mouseleave', event => {
+    destinationH2.style.textDecoration = '';
+    destinationH2.textContent = 'Pick Your Destination';
+    destinationImg.style.border = '';
+});
+destinationImg.addEventListener('click', event => {
+    const destinations = [
+        'Rio de Janeiro', 
+        'Thailand', 
+        'Hawaii', 
+        'Jamaica', 
+        'Santorini',
+        'Miami',
+        'Cancun',
+        'Bali',
+        'San Diego',
+        'Maldives'
+    ]
+    function randomNumber() {
+        return Math.floor(Math.random() * 10);
+    }
+    destinationH2.textContent = destinations[randomNumber()];
+});
 
-// const middleTwoImgs = document.querySelectorAll('.img-content img');
+// draggable
+const middleTwoImgs = document.querySelectorAll('.img-content img');
+middleTwoImgs[0].setAttribute('draggable', true);
+middleTwoImgs[1].draggable = true;
+const middleTwoDivs = document.querySelectorAll('.img-content');
 
-// middleTwoImgs.addEventListener('wheel', event => {
-//     middleTwoImgs.style.rotate = 
-// })
+middleTwoImgs[0].addEventListener('drag', event => {
+}, false);
+middleTwoImgs[1].addEventListener('drag', event => {
+}, false);
+middleTwoImgs[0].addEventListener('dragstart', event => {
+    middleTwoImgs[0] = event.target;
+    middleTwoImgs[0].style.opacity = '0.4';
+}, false);
+middleTwoImgs[1].addEventListener('dragstart', event => {
+    middleTwoImgs[1] = event.target;
+    middleTwoImgs[1].style.opacity = '0.4';
+}, false);
+middleTwoImgs[0].addEventListener('dragend', event => {
+    middleTwoImgs[0].style.opacity = '';
+}, false);
+middleTwoImgs[1].addEventListener('dragend', event => {
+    middleTwoImgs[1].style.opacity = '';
+}, false);
+middleTwoImgs[0].addEventListener('dragover', event => {
+    event.preventDefault();
+}, false);
+middleTwoImgs[1].addEventListener('dragover', event => {
+    event.preventDefault();
+}, false);
+middleTwoImgs[0].addEventListener('dragenter', event => {
+    if (event.target.className == 'img-content') {
+        event.target.style.border = 'dashed red 5px';
+    }
+}, false);
+middleTwoImgs[1].addEventListener('dragenter', event => {
+    if (event.target.className == 'img-content') {
+        event.target.style.border = 'dashed red 5px';
+    }
+}, false);
+middleTwoImgs[0].addEventListener('drop', event => {
+    event.preventDefault();
+    if (event.target.className == 'img-content') {
+        event.target.style.background = '';
+        middleTwoImgs[0].parentNode.removeChild(middleTwoImgs[0]);
+        event.target.appendChild(middleTwoImgs[0]);
+    }
+}, false);
+middleTwoImgs[1].addEventListener('drop', event => {
+    event.preventDefault();
+    if (event.target.className == 'img-content') {
+        event.target.style.background = '';
+        middleTwoImgs[1].parentNode.removeChild(middleTwoImgs[1]);
+        event.target.appendChild(middleTwoImgs[1]);
+    }
+}, false);
