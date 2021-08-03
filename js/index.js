7892c97491ce967e68abbe5a1c214a3ead61b7f0
vgaranin <vasilii_garanin@laptevfamily.com>
2021-08-03 18:45:28 -0500

    First commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+window.addEventListener('load', event => 
+{
+    console.log(`${event.type} happened`);
+});
+
+let navA = document.querySelectorAll('.nav a');
+
+navA[0].addEventListener('click', event => 
+{
+    event.target.style.backgroundColor = "yellow";
+    document.h1;
+});
