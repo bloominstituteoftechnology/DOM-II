7f4d3208b16ba24c7fb357f74dd9e09da6bdf46b
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-08-03 15:19:47 -0600

    Added two event listeners: Click and mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 // Your code goes here
+
+// Fun Bus - Click
+const funBusTitle = document.querySelector(".logo-heading");
+
+function turnBlue() {
+    
+    if(funBusTitle.style.color !== "blue"){
+        funBusTitle.style.color = "blue";
+    } else if (funBusTitle.style.color === "blue"){
+        funBusTitle.style.color = "#000000";
+    } 
+}
+
+funBusTitle.addEventListener("click", turnBlue);
+
+
+// Welcome To Fun Bus! - Mouseover 
+const introTitle = document.querySelector(".intro h2")
+
+function randomRGB() {
+    const randomR = Math.floor(Math.random()*255);
+    const randomG = Math.floor(Math.random()*255);
+    const randomB = Math.floor(Math.random()*255);
+    introTitle.style.color = `rgb(${randomR}, ${randomG}, ${randomB})`;
+}
+
+introTitle.addEventListener("mouseover", randomRGB);
+
+console.log(randomRGB());
