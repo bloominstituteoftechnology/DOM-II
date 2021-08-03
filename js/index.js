b377b70c089e1bb85273eec1f0d5e3f66845e336
AdamI19 <broadscars@gmail.com>
2021-08-03 12:38:52 -0600

    updated project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
 // Your code goes here
+const navlink = document.querySelector('.nav-link');
+const btnclass = document.querySelector('.btn');
+const Destination = document.querySelector('.destination');
+const Text = document.querySelector('.text-content')
+
+document.addEventListener('click', function(event) {
+    if (event.target === navlink) {
+        console.log("Clicked the Nav button!")
+    } else {
+        console.log("Clicked somewhere else...")
+    }
+})
