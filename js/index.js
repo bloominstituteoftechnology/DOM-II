18a1a87ef63ef98f68792f2341f0cb636868f6ac
luisbryandr <luisbdominguezr@gmail.com>
2021-08-03 19:57:44 -0400

    selected some elements to add event listeners to and did my first one

--- js/index.js
+++ js/index.js
@@ -1,15 +1,25 @@
 // Your code goes here
 // I believe that in order to change something I need to select it first so I will start by selecting 10 items which may or may not be the ones I end up creating listeners for
-const bigSur = document.querySelector('.main-navigation')
-const lilSur = document.querySelector('.nav')
+const mainNav = document.querySelector('.main-navigation')
+const genNav = document.querySelector('nav')
 const signMeUpButtons = document.querySelectorAll('.btn')
 const pickYour = document.querySelector('.content-destination')
 const destination = document.querySelectorAll('.destination')
 
+const funBus = document.querySelector('.logo-heading')
+console.log(funBus);
+
 //next going to create an event listener like we did in the guided project
-signMeUpButtons.onclick = function(event){
-    console.log('one of the buttons was clicked')
-}
-signMeUpButtons.addEventListener('click', function (event){
+// signMeUpButtons.onclick = function(event){
+//     console.log('one of the buttons was clicked')
+// }
+// signMeUpButtons.addEventListener('click', function (event){
     
-})
+// })
+
+function changeTitle (){
+    console.log('mouse over Fun Bus');
+    funBus.textContent = 'The Funnest';
+};
+
+funBus.addEventListener('mouseover', changeTitle)
