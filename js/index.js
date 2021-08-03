84ebacb79d73128f9f3d85819e74c52bc0b9122f
Brent Besselievre <brent@Brents-MacBook-Pro.local>
2021-08-03 17:02:01 -0600

    Added double click event listener

--- js/index.js
+++ js/index.js
@@ -18,13 +18,27 @@ funBusTitle.addEventListener("click", turnBlue);
 // Welcome To Fun Bus! - Mouseover 
 const introTitle = document.querySelector(".intro h2")
 
-function randomRGB() {
+function randomRGBIntro() {
     const randomR = Math.floor(Math.random()*255);
     const randomG = Math.floor(Math.random()*255);
     const randomB = Math.floor(Math.random()*255);
     introTitle.style.color = `rgb(${randomR}, ${randomG}, ${randomB})`;
 }
 
-introTitle.addEventListener("mouseover", randomRGB);
+introTitle.addEventListener("mouseover", randomRGBIntro);
 
-console.log(randomRGB());
+
+// Nav Bar - double click
+const navBar = document.querySelector(".nav-link");
+
+console.log(navBar);
+
+function changeFontSize(){
+    if(navBar.style.fontSize !== "5rem"){
+        navBar.style.fontSize = "5rem"
+    } else if (navBar.style.fontSize === "5rem"){
+        navBar.style.fontSize = "1.6rem";
+    }
+}
+
+navBar.addEventListener("dblclick", changeFontSize);
