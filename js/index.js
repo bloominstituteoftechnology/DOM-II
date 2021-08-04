dcb0f490338804d1c7e3fb417a3452b0c3bb8983
vgaranin <vasilii_garanin@laptevfamily.com>
2021-08-03 23:07:28 -0500

    h1 tag edited

--- js/index.js
+++ js/index.js
@@ -1,13 +1,41 @@
 // Your code goes here
-window.addEventListener('load', event => 
-{
-    console.log(`${event.type} happened`);
-});
+
 
 let navA = document.querySelectorAll('.nav a');
+let allImg = document.querySelectorAll(" img");
+const h1Tag = document.querySelector('h1');
+
+h1Tag.addEventListener('mouseover', event =>
+{
+    h1Tag.textContent = "Fun Bus Here For you!";
+    h1Tag.style.fontSize = "6em";
+});
+h1Tag.addEventListener('mouseout', event =>
+{
+    h1Tag.textContent = "Fun Bus";
+    h1Tag.style.fontSize = "2em";
+});
 
 navA[0].addEventListener('click', event => 
 {
     event.target.style.backgroundColor = "yellow";
-    document.h1;
+    if (navA[0].backgroundColor === "yellow" || navA[0].backgroundColor === "red")
+    {
+        navA[0].backgroundColor === "green";
+    } else if (navA[0].backgroundColor === green)
+    {
+        navA[0].backgroundColor === "red";
+    }
+});
+
+navA[1].addEventListener('mouseover', event =>
+{
+    navA[1].style.backgroundColor = "purple";
+    navA[1].style.color = "white";
+});
+
+navA[1].addEventListener('mouseout', event =>
+{
+    navA[1].style.backgroundColor = "white";
+    navA[1].style.color = 'rgb(51, 0, 102)';
 });
