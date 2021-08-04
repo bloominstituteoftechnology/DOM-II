d2e19d3526ac50f31f7f2d289865e857eb8f49a9
jonahjtr <86381032+jonahjtr@users.noreply.github.com>
2021-08-03 23:03:19 -0500

    made it a little bit cleaner

--- js/index.js
+++ js/index.js
@@ -73,3 +73,40 @@ const el = document.querySelector('.hh');
 el.onwheel = zoom;
   
 el.addEventListener('wheel', zoom);
+
+// const form = document.querySelector('.hey')
+
+// form.addEventListener('blur', (event) => {
+//     event.target.style.background = '';
+//   }, true);
+
+var enterEventCount = 0;
+var leaveEventCount = 0;
+const mouseTarget = document.getElementById('mouseTarget');
+const unorderedList = document.getElementById('unorderedList');
+
+mouseTarget.addEventListener('mouseenter', e => {
+  mouseTarget.style.border = '5px dotted orange';
+  enterEventCount++;
+  addListItem('This is mouseenter event ' + enterEventCount + '.');
+});
+
+mouseTarget.addEventListener('mouseleave', e => {
+  mouseTarget.style.border = '1px solid #333';
+  leaveEventCount++;
+  addListItem('This is mouseleave event ' + leaveEventCount + '.');
+});
+
+function addListItem(text) {
+  // Create a new text node using the supplied text
+  var newTextNode = document.createTextNode(text);
+
+  // Create a new li element
+  var newListItem = document.createElement("li");
+
+  // Add the text node to the li element
+  newListItem.appendChild(newTextNode);
+
+  // Add the newly created list item to list
+  unorderedList.appendChild(newListItem);
+}
