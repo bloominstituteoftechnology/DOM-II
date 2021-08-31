3b6111c9834737dd9c319d12e1c9d5d1d86f04ce
andrewhunt21 <andrewhunt21@icloud.com>
2021-08-31 15:02:53 -0700

    started project

--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+const navigation = document.querySelector('.nav');
+
+
+
+
+navigation.addEventListener("mouseover", function(event){
+    event.target.style.color = "orange";
+});
