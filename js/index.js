a7fce1a0ba0d59f9310e0e0357c1dd06944d585f
LukeSchmude <schmude2002@gmail.com>
2021-08-31 16:14:05 -0500

    first push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-// Your code goes here
+
+//Brings up a window that welcomes you the website once the page fully loads
+
+window.addEventListener('load', (event) => {
+    alert
+})
+
+// changes the color of the text in the nav when the mouse is over it and moves away
+
+const mainNav = document.querySelector('.nav')
+
+mainNav.addEventListener('mouseover', function(event) { 
+    event.target.style.color = 'red';
+})
+mainNav.addEventListener('mouseout', function(event) {
+    event.target.style.color = 'black'
+})
+
+
+//Adds a border around the first image on the page when the mouse is over it
+
+const imgFun = document.querySelector('.intro img')
+
+imgFun.addEventListener('mouseenter', function(event) {
+    event.target.style.border = '1px solid red'
+})
+imgFun.addEventListener('mouseleave', function(event) {
+    event.target.style.border = ''
+})
+
+
+
+const topContentHead = document.querySelector('.content-section .text-content h2')
+
+topContentHead.addEventListener
