d13bbd9da648de64bc082cf2d25b4400caa2d45d
Lance Turbes <waywardpooch@users.noreply.github.com>
2021-08-31 13:21:42 -0700

    finished adding at least 10 interactive elements

--- js/index.js
+++ js/index.js
@@ -17,6 +17,7 @@ const header = document.querySelector("header");
 const siteButtons = document.querySelectorAll("div .btn");
 const busPhoto = document.querySelector(".intro img");
 const siteBody = document.querySelector("body");
+const paragraphs = document.querySelectorAll("p");
 
 console.log(siteButtons);
 
@@ -48,23 +49,57 @@ siteButtons.forEach((button) => {
   });
 });
 
-// Event Listener 4 (Press any key for an alert message)
+// Event Listener 4 (Press any key down to make the background green)
 document.addEventListener("keydown", (event) => {
-  alert("You pressed a key!");
+  siteBody.style.backgroundColor = "seagreen";
+  event.stopPropagation();
 });
 
-// Event Listener 5 (Double click the bus photo for an alert message)
+// Event Listener 5 (Let go of any key to reset the background color to white)
+document.addEventListener("keyup", (event) => {
+  siteBody.style.backgroundColor = "white";
+  event.stopPropagation();
+});
+
+// Event Listener 6 (Double click the bus photo for an alert message)
 busPhoto.addEventListener("dblclick", (event) => {
   alert("You double-clicked the bus photo!");
+  event.stopPropagation();
 });
 
-// Event Listener 6 (Scroll down from the top to make the background light yellow)
+// Event Listener 7 (Scroll down from the top to make the background light yellow)
 document.addEventListener("scroll", (event) => {
   siteBody.style.transition = "1s";
   siteBody.style.backgroundColor = "lightyellow";
+  event.stopPropagation();
 });
 
-// Event Listener 7 (Alert the user when the site loads)
+// Event Listener 8 (Alert the user when the site loads)
 window.addEventListener("load", (event) => {
   alert("The site has been loaded! Welcome!");
+  event.stopPropagation();
+});
+
+// Event Listener 9 (Put a red border around the bus photo when hovered)
+busPhoto.addEventListener("mouseenter", (event) => {
+  busPhoto.style.border = "solid 3px red";
+  event.stopPropagation();
+});
+busPhoto.addEventListener("mouseleave", (event) => {
+  busPhoto.style.border = "solid 0px red";
+  event.stopPropagation();
+});
+
+// Event Listener 10 (Display an alert message when the browser is resized)
+window.addEventListener("resize", (event) => {
+  alert("browser resized");
+  event.stopPropagation();
+});
+
+// Event Listener 11 (Display an alert message when copying text)
+paragraphs.forEach((paragraph) => {
+  paragraph.addEventListener("copy", (event) => {
+    alert("copied text");
+    event.stopPropagation();
+  });
 });
