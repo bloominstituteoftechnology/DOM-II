e6b2381f30957708d2707c56191e1dde0ee098de
Anastasia Lapteva <anastasia_laptev@hotmail.com>
2021-08-31 17:16:22 -0700

    added some event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+let navLink = document.querySelectorAll(".nav-link");
+
+navLink[0].addEventListener("mouseover", (event) =>
+{
+    navLink[0].style.backgroundColor = "orange";
+});
+
+navLink[0].addEventListener("mouseout", function ()
+{
+    navLink[0].style.backgroundColor = "white";
+});
+
