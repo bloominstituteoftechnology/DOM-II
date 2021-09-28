b01ebd571c78509b5c24bc2990671f044e115894
astephens-fsd <astephens.pm@icloud.com>
2021-09-28 15:28:02 -0700

    added .off class, worked on mouseover and mouse leave

--- js/index.js
+++ js/index.js
@@ -1,1 +1,27 @@
 // Your code goes here
+
+//mouseover
+Array.from(document.links).forEach(link => {
+    link.addEventListener("mouseenter", function(event) {
+        event.target.style.color = "orange";
+        setTimeout(function() {
+            event.target.style.color = "";
+        }, 300);
+    },false);
+})
+
+//mouseleave
+var enterEventCount = 0;
+var leaveEventCount = 0;
+const navContent = document.querySelector('.nav-container');
+const links = document.querySelectorAll('a');
+
+navContent.addEventListener('mouseleave', function(event) {
+    console.log('nav mouseleave event');
+    event.target.style.backgroundColor ='teal';
+    event.target.style.color = 'white';
+    enterEventCount++;
+    links.forEach(link => {
+        link.style.color = 'white';
+    })
+});
