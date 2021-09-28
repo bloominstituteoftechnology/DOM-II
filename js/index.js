bf53b1c03f0b4e9f9ad88e1c1a9153d4f2f9c65a
Ryan Howard <ryanghoward@protonmail.com>
2021-09-28 14:21:56 -0700

    4 - Key Down Added

--- js/index.js
+++ js/index.js
@@ -1,16 +1,32 @@
 // --------------------------- EVENT LISTENERS ------------------------ //
 
+// 1. Mouseover -------------------------------------------------
+
 const mouseOver = document.querySelector('.logo-heading');
 mouseOver.addEventListener('mouseover', function(event){
-    event.target.style.color = 'red';
+    event.target.style.color = 'aquamarine';
     setTimeout(function() {
         event.target.style.color = "";
     }, 500);
 });
 
+// 2. Load --------------------------------------------------------
+
 window.addEventListener('load', (event) => {
-    console.log('The page has successfully loaded');
-  });
+    console.log('Locked and Loaded');
+});
 
+// 3. Click --------------------------------------------------------
 
+const buttonClick = document.querySelector('.btn');
+buttonClick.addEventListener('click', event => {
+    buttonClick.textContent = `Boop! ${event.detail}`;
+});
 
+// 4. Keydown ---------------------------------------------------------
+
+let body = document.querySelector('body');
+body.addEventListener('keydown', (event) => {
+    event.target.style.color = 'gray';
+    event.target.style.backgroundColor = 'black';
+});
