3b8a0706721c523b814e47ed528e8c666bbcd239
Lia Moua <moua0061@yahoo.com>
2021-09-28 18:52:58 -0400

    completed key down

--- js/index.js
+++ js/index.js
@@ -4,5 +4,11 @@
 const mouseOverHeading = document.querySelector('header h2');
 mouseOverHeading.addEventListener('mouseenter', function(event){
     event.target.style.color = 'purple';
-}
-)
+})
+
+//keydown
+const boddy = document.querySelectorAll('body');
+boddy[0].addEventListener("keydown", function(){
+    boddy[0].style.color = "darkgray";
+    console.log("your keydown works!")
+})
