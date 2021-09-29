1789988549644282540d4153d3d6193aea26b64d
Hughes <hughespham2002the2nd@yahoo.com>
2021-09-28 20:00:00 -0500

    6/10 done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,50 @@
-// Your code goes here
+links.addEventListener('mouseover', function(event) {
+    event.target.style.color = 'red';
+    setTimeout(function() {
+        event.target.style.color = '';
+    }, 500);
+}, false);
+
+document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape"){
+        document.classList.add("off");
+    }
+});
+const mainNavigation = document.querySelector('.main-navigation');
+mainNavigation.style.position = "fixed";
+
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.style.justifyContent = "space-evenly";
+
+const butt = document.querySelectorAll('.btn');
+butt.style.border = "yellow";
+butt.style.cursor = "pointer";
+
+const images = document.querySelectorAll('.img-content');
+
+images.forEach(imgs => {
+    imgs.addEventListener("mouseover", () => {
+        imgs.style.transform = "rotate(90deg)";
+    });
+});
+
+images.forEach(imgs => {
+    imgs.addEventListener("mouseleave", () => {
+        imgs.style.transform = "rotate(270deg)";
+    });
+});
+
+const resizer = document.querySelector('.content-destination');
+
+function reportWindowSize() {
+  resizer.textContent = window.innerHeight;
+  resizer.textContent = window.innerWidth;
+}
+
+window.onresize = reportWindowSize;
+
+const doubleClick = document.querySelector('p');
+
+doubleClick.addEventListener('dblclick', function() {
+    doubleClick.classList.toggle('.large');
+})
