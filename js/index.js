4fa6261fec23d7b124ce7034ddba8a40cc562ce1
Cheyenne Bowman <cheyennebowman1212123@gmail.com>
2021-09-28 19:44:58 -0500

    all event listeners

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 // Your code goes here
-const signButton = document.querySelectorAll('.btn');
+const signButtons = document.querySelectorAll('.btn');
 const navButton = document.querySelectorAll('nav a');
 const containerH2 = document.querySelectorAll('h2');
 const logo = document.querySelector('.logo-heading');
@@ -9,6 +9,25 @@ const h4 = document.createElement('h4')
 const home = document.querySelector('.home')
 const welcome = document.querySelector('.intro h2')
 const letsGo = document.querySelector('.text-content h2')
+const destination = document.querySelector('.content-destination h2')
+const destinationValue = document.querySelector('.content-destination')
+const style = document.createElement('style')
+
+style.innerHTML = `
+    .btn {
+        background-color: black;
+        color: white;
+        display: block;
+        height: 40px;
+        line-height: 40px;
+        text-decoration: none;
+        width: 100px;
+        text-align: center;
+        cursor: pointer;
+    }
+`
+document.head.appendChild(style)
+
 const user = {
     name: ''
 }
@@ -34,6 +53,7 @@ const detectOnline = () => {
     onlineH4.textContent = 'Yay! You are online!'
 }
 window.addEventListener('online', detectOnline())
+
 // Nav button colors
 navButton.onclick = function(event) {
     navButton.setAttribute('style', 'color: red')
@@ -64,6 +84,52 @@ const getExcited = () => {
     letsGo.insertAdjacentHTML('afterend', ' I am sooo excited!')
 }
 letsGo.addEventListener('dblclick', getExcited)
+
+// Destination Input
+const destinationInput = document.createElement('INPUT')
+const destinationStrong = document.createElement('strong')
+destinationInput.setAttribute('type', 'text')
+destinationInput.setAttribute('value', 'Where do you want to go?')
+ 
+destination.appendChild(destinationInput)
+destination.appendChild(destinationStrong)
+
+const updateDestination = (e) => {
+    const targetValue = e.target.value
+    if (targetValue.length > 3) {
+        destinationStrong.textContent = ` I love ${e.target.value} too!`
+    }
+}
+destinationInput.addEventListener('input', updateDestination)
+
+// Change color on focus
+const changeInputColorAndRest = (e) => {
+destinationInput.setAttribute('value', '')
+    e.target.style.background = 'pink'
+}
+destinationInput.addEventListener('focus', changeInputColorAndRest)
+
+// ALert if input is empty
+const emptyInputAlert = (e) => {
+    const targetValue = e.target.value
+    destinationInput.setAttribute('value', 'Where do you want to go?')
+    e.target.style.background = ''
+    if (targetValue.length === 0) {
+        alert('Please tell me where you want to go.')
+    }
+}
+destinationInput.addEventListener('blur', emptyInputAlert)
+
+// Alert on signup
+const buttons = Array.from(signButtons)
+const handleClick = () => {
+    alert('Sorry the sign up page is out of order')
+}
+
+buttons.forEach((button) => {
+    button.addEventListener('click', handleClick)
+})
+
 
 
 
