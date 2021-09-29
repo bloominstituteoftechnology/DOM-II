75bd87bfc19b75ee34fec78de89277e8cf70997b
Ryan Howard <ryanghoward@protonmail.com>
2021-09-28 17:07:20 -0700

    Random save

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@
 // 1. Mouseover -------------------------------------------------
 
 const mouseOver = document.querySelector('.logo-heading');
-mouseOver.addEventListener('mouseover', (event) => {
+mouseOver.addEventListener('mouseover', event => {
     event.target.style.color = 'aquamarine';
     setTimeout(function() {
         event.target.style.color = "";
@@ -12,20 +12,20 @@ mouseOver.addEventListener('mouseover', (event) => {
 
 const busImg = document.querySelector('.intro img');
 busImg.classList.add('bus');
-busImg.addEventListener('mouseover', (event) => {
-    busImg.setAttribute('src', 'https://images.unsplash.com/photo-1632778931175-128809d8facc?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80');
+busImg.addEventListener('mouseover', event => {
+    busImg.setAttribute('src', 'https://images.unsplash.com/photo-1537110008491-de25aefaf46a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1074&q=80');
 });
 
 // 2. Mouseleave ---------------------------------------------------
 
 const busImgReturn = document.querySelector('.intro img');
-busImgReturn.addEventListener('mouseleave', (event) => {
+busImgReturn.addEventListener('mouseleave', event => {
     busImgReturn.setAttribute('src', 'img/fun-bus.jpg');
 });
 
 // 3. Load --------------------------------------------------------
 
-window.addEventListener('load', (event) => {
+window.addEventListener('load', event => {
     console.log('Locked and Loaded');
 });
 
@@ -36,27 +36,37 @@ buttonClick.addEventListener('click', event => {
     buttonClick.textContent = `Boop! ${event.detail}`;
 });
 
-// 5. Key-down ---------------------------------------------------------
+// 5. Key-down & Key-up ---------------------------------------------------------
 
 const keyDown = document.querySelector('body');
-keyDown.addEventListener('keydown', (event) => {
+keyDown.addEventListener('keydown', event => {
     event.target.style.color = 'gray';
     event.target.style.backgroundColor = 'black';
 });
 
-// 6. Key-up ---------------------------------------------------------
-
 const keyUp = document.querySelector('body');
-keyUp.addEventListener('keyup', (event) => {
+keyUp.addEventListener('keyup', event => {
     event.target.style.color = 'black';
     event.target.style.backgroundColor = 'white';
 });
 
-// 7. Focus ------------------------------------------------------------
+// Pointer Down ----------------------------------------------------------------
 
-const title = document.querySelector('title:nth-of-type(1)');
-title.addEventListener('focus', (event) => {
-    event.target.style.color = 'blue';
-    event.target.style.backgroundColor = 'pink';
-}); 
+const pointerDown = document.querySelector('p');
+pointerDown.addEventListener('pointerdown', event => {
+    console.log('Pointer down event');
+});
 
+// // 6. Blur ------------------------------------------------------------
+
+// const title = document.querySelector('div, img');
+// title.addEventListener('blur', (event) => {
+//     event.target.background = ' ';
+// }); 
+
+// // 7. Wheel -----------------------------------------------------------
+
+// const adventureImageWheel = document.querySelector('img:nth-of-type(3)');
+// adventureImageWheel.addEventListener('wheel', event => {
+//     adventureImageWheel.setAttribute('src', 'img/adventure.jpg');
+// });
