bb6299b0ef1dd4ca494b34a4678aaeefb29c6bcd
Nithya Narayanan <n.nithya@pm.me>
2021-10-26 15:39:10 -0700

    Completed Click.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Your code goes here
 //onmouseOver
-const imgSelected = document.querySelector('.img-content');
+const imgSelected = document.querySelector('div.img-content');
 imgSelected.onmouseover = imgMouseOver;
 imgSelected.onmouseout = imgMouseOut;
 
@@ -21,3 +21,13 @@ document.addEventListener('keydown', anyKey);
 function anyKey(){
     pageName.style.backgroundColor = "Yellow";
 }
+
+//click
+
+const pageLand = document.querySelector(".intro h2");
+
+pageLand.addEventListener('click', function(event){
+    if (event.target === pageLand){
+        console.log("You have come to the right place")
+    }
+})
