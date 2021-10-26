608f85b621a69de3707a6a51f3e86394506803b7
MNoble2014 <michealsimpson2013@gmail.com>
2021-10-26 16:02:15 -0400

    event listener 1 added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 // Your code goes here
+// Random color to "Fun Bus" text when clicked
+let headingAnimation = document.querySelector(".logo-heading");
+function colorGenerator() {
+    let min = 0;
+    let max = 300;
+
+    let colorR = Math.floor(Math.random() * (max-min + 1)) + min;
+    let colorG = Math.floor(Math.random() * (max-min + 1)) + min;
+    let colorB = Math.floor(Math.random() * (max-min + 1)) + min;
+
+    return `rgb(${colorR}, ${colorG}, ${colorB})`;
+}
+
+headingAnimation.addEventListener('click', () => {
+    headingAnimation.style.transitionProperty = 'color';
+    headingAnimation.style.transition = 'ease-in';
+    headingAnimation.style.color = colorGenerator()
+})
+
+headingAnimation.addEventListener('click', () => {
+    headingAnimation.style.transitionProperty = 'color';
+    headingAnimation.style.transition = '5s ease-out';
+    headingAnimation.style.color = colorGenerator();
+})
