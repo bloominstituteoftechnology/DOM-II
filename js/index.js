23b059919cf52d02123c2d69476daeb20737703d
Ryan Howard <ryanghoward@protonmail.com>
2021-10-26 12:31:19 -0700

    First Save. Load and Click events functional

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-// Your code goes here
+//* 1. Load
+window.addEventListener('load', () => {
+    console.log('Congrats, the page has loaded!')
+});
+
+//* 2. Click
+const buttonClick = document.querySelectorAll('.btn');
+buttonClick[0].addEventListener('click', e => {
+    buttonClick[0].textContent = `Hey, don't click that! ${e.detail}`
+})
