5884495db5de44a156551d42a2ba09c57a81c8b1
Amethyst Williams <zmithic@gmail.com>
2021-10-26 16:25:27 -0500

    Prevented nav from refreshing page, added mouseoer event, added keydown event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
-// Your code goes here
+//Testing to see if everything works properly
+/*
+const logo = document.querySelector('h1');
+logo.textContent = 'Please work';
+It works lol*/
+
+//Prevent navigation from refreshing page
+document.querySelectorAll('.nav-link').forEach(elem => {
+    elem.addEventListener('click', function(e) {
+        e.stopPropagation();
+    })
+});
+
+//Make all h2s turn purple when moused over for a short period of time
+document.querySelectorAll('off').forEach(elem => {
+    elem.addEventListener('mouseover', function(e) {
+        e.target.style.color = 'purple';
+        setTimeout(function() {
+            e.target.style.color = '';
+          }, 500);
+    })
+}, false);
+
+//Change the title to "There is No Escape" when escape key is pressed and then go back to normal after a short period of time. Also turns red because spooky
+const pageTitle = document.querySelector('h1');
+
+function escKey(evt) {
+    if (evt.key === 'Escape') {
+        pageTitle.textContent = 'There is No Escape';
+        pageTitle.style.color = 'red';
+        setTimeout(function() {
+            pageTitle.textContent = 'Fun Bus';
+            pageTitle.style.color = '';
+          }, 500);
+    }
+};
+
+document.addEventListener('keydown', escKey);
