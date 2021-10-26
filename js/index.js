70bcc3f92d28c520dd3d61c84033ccebb36ab5d6
Amethyst Williams <zmithic@gmail.com>
2021-10-26 17:46:58 -0500

    Added double click, focus/blur, copy, select, scroll, and drag event listners

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ document.querySelectorAll('.nav-link').forEach(elem => {
 });
 
 //Make all h2s turn purple when moused over for a short period of time
-document.querySelectorAll('off').forEach(elem => {
+document.querySelectorAll('h2').forEach(elem => {
     elem.addEventListener('mouseover', function(e) {
         e.target.style.color = 'purple';
         setTimeout(function() {
@@ -36,3 +36,81 @@ function escKey(evt) {
 };
 
 document.addEventListener('keydown', escKey);
+
+//Add listener to images that makes them zoom in and out with wheel
+const busImg = document.querySelector('.intro img')
+
+//Ensure the busImg will always be centered
+busImg.style.display = 'block';
+busImg.style.margin = '0 auto';
+
+//Trying to make the zoom function
+//Bro idk lmao moving on
+
+//Double-click boat image to shrink it
+const boatImg = document.querySelector('.content-destination img');
+
+//Keep image centered
+boatImg.style.display = 'block';
+boatImg.style.margin = '0 auto';
+
+boatImg.addEventListener('dblclick', function(e) {
+    boatImg.style.height = '100px';
+    boatImg.style.width = 'auto';
+});
+
+//test to see if i selected it
+/*
+boatImg.style.height = '500px';
+I did lol*/
+
+//Turn background grey when out of focus
+function unfocused() {
+    document.body.style.backgroundColor = 'grey';
+    pageTitle.textContent = "Where'd you go!?"
+}
+  
+function focused() {
+    document.body.style.backgroundColor = 'white';
+    pageTitle.textContent = "Fun Bus"
+}
+  
+window.addEventListener('blur', unfocused);
+window.addEventListener('focus', focused);
+
+//console.log when the user copies text
+document.addEventListener('copy', function() {
+    console.log('The user has copied something.')
+})
+
+//Make the title self aware that you're selecting things
+document.addEventListener('selectstart', function() {
+    pageTitle.textContent = 'Hey, stop selecting that!'
+    setTimeout(function() {
+        pageTitle.textContent = 'Fun Bus';  
+      }, 2000);
+});
+
+//Make a dumb pun in the console when scrolling.
+document.addEventListener('scroll', function(e) {
+    console.log('They see me scrollin');
+});
+
+//Make the website upset when you drag an image + Make things draggable
+let dragged;
+
+document.addEventListener('dragstart', function(e) {
+    dragged = e.target;
+    e.target.style.opacity = '0.25';
+    pageTitle.textContent = 'Give that back!'
+}, false);
+
+document.addEventListener('dragend', function(e) {
+    e.target.style.opacity = '';
+    pageTitle.textContent = 'Hmph!'
+    setTimeout(function() {
+        pageTitle.textContent = 'Fun Bus';  
+      }, 1000);
+}, false);
+
+//Make something goofy happen when you move the pointer
