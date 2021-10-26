505d02c468d43ec5dc2baf185d5eaf4992f4ce00
James_Calvo <15jcalvo@gmail.com>
2021-10-26 13:42:43 -0400

    4 events added

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+//Links turn red on click and revert on doubleclick
+let navLinks = document.querySelectorAll('nav a');
+let navLinksArray = Array.from(navLinks);
+for(let i = 0; i < navLinksArray.length; i++){
+    navLinksArray[i].addEventListener('click', (event) => {event.target.style.color = 'red'; });
+}
+for(let i = 0; i < navLinksArray.length; i++){
+    navLinksArray[i].addEventListener('dblclick', (event) => {event.target.style.color = 'blue'; });
+}
+//minimize photos on click, resize on mouseover
+let images = document.querySelectorAll('img');
+let imagesArray = Array.from(images);
+for(let i = 0; i < imagesArray.length; i++){
+        imagesArray[i].addEventListener('click', (event) => {event.target.height = '50'; });
+}
+for(let i = 0; i < imagesArray.length; i++){
+    imagesArray[i].addEventListener('mouseover', (event) => {event.target.height = "300"; });
+}
