09d6d34b8583ed7e5e3dfb07f8259f08bd289f28
Krystle Mitchell <mitchell.krystle2@gmail.com>
2021-10-26 14:21:38 -0700

    added Event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
-// Your code goes here
+const button = document.querySelector('.destination .btn');
+
+button.addEventListener('click', function(){
+    button.textContent = ('hey you clicked me!');
+    button
+});
