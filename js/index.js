9a83924d92d9e38e75d42730d253c64f0745fa4c
Nyamekye Annor <nyamekyeannor@Nyamekyes-MacBook-Pro.local>
2021-10-26 16:54:47 -0500

    first push

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-// Your code goes here
+// This is alerting "Hello World when the first H2's is mousedover"
+const h2Elements = document.querySelectorAll("h2");
+h2Elements[0].addEventListener("mouseover", h2);
+function h2() {
+  alert ("Hello World!");
+}
+
+
+// This turns the .logo-heading bluse when clicked 
+const logoHeading = document.querySelector(".logo-heading");
+logoHeading.addEventListener("click", logo);
+function logo() {
+    logoHeading.style.color = "blue"
+}
