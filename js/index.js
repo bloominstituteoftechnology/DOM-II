3e0a9088fc8d06c0c4b3e6f6678c80a74ac8cfd0
UrukuTelal <ao.bier1101@gmail.com>
2021-10-26 14:02:25 -0700

    some done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+const logoHeading = document.querySelector('.logo-heading');
+const links = document.querySelectorAll('a');
+const intro = document.querySelector('.intro');
+const container = document.querySelector('.container');
+
+let randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+
+//random color for Fun Bus
+function changeFunBusColor(){
+    randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+
+        logoHeading.style.color = randomColor;
+            
+}
+logoHeading.addEventListener('mouseover', changeFunBusColor);
+
+//random link color
+Array.from(links).forEach(link => {
+    
+    function linkClick(){
+     randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+
+        link.style.color = randomColor;
+    }
+    link.addEventListener('click', linkClick)        
+    })
+
+function changeIntro(){
+    randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+    intro.style.backgroundColor = randomColor;
+}
+intro.addEventListener('mouseover', changeIntro);
+
