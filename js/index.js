b12de5d4d2617704f8a45c81f3ef7642afce2b91
quavo <qua.thesage19@gmail.com>
2021-10-26 14:26:05 -0500

    Finished 1st event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+const imgChange = document.querySelector('.intro img');
+imgChange.addEventListener('mouseover', function(evt) {
+    evt.target.style.boxShadow = "10px 15px 2px #DEB887";
+});
+console.log(imgChange)
