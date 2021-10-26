8a61868ff082a4b159a4f7d4c29e0bf579eaad51
quavo <qua.thesage19@gmail.com>
2021-10-26 15:38:24 -0500

    finished tasks 3 through 5

--- js/index.js
+++ js/index.js
@@ -9,10 +9,22 @@ imgChange.addEventListener('mouseover', function(evt) {
 const navChange = document.querySelectorAll('.nav .nav-link');
  Array.from(navChange).forEach(change =>{
     change.addEventListener('click', function(evt) {
-        evt.target.style.color = '#DEB887';
+        evt.target.style.color = '#808080';
     });
-
-    change.removeEventListener('click', function(evt) {
-        evt.target.style.color = "black";
-    })
  });
+
+ //Event 3
+window.addEventListener('load', function() {
+     console.log('The page has loaded successfully.')
+ });
+
+ // Event 4
+ const secondImage = document.querySelector('.logo-heading');
+ secondImage.addEventListener('dblclick', function(event) {
+     event.target.style.fontSize = "6rem";
+ });
+
+ //Event 5
+ document.body.addEventListener('wheel', function(evt) {
+     evt.target.style.backgroundColor = "#DEB887";
+ })
