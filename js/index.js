152a3405b3c115974774867b60b82b12b5d94daf
Ryan Howard <ryanghoward@protonmail.com>
2021-10-26 12:40:35 -0700

    MouseOver event added to Fun Bus logo

--- js/index.js
+++ js/index.js
@@ -7,4 +7,13 @@ window.addEventListener('load', () => {
 const buttonClick = document.querySelectorAll('.btn');
 buttonClick[0].addEventListener('click', e => {
     buttonClick[0].textContent = `Hey, don't click that! ${e.detail}`
-})
+});
+
+//* 3. MouseOver
+const mouseOver = document.querySelector('.logo-heading');
+mouseOver.addEventListener('mouseover', e => {
+    e.target.style.color = 'purple';
+    setTimeout(function () {
+        e.target.style.color = '';
+    }, 1000);
+});
