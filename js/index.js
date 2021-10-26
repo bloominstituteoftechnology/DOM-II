99406d99db68f9f10b7118af9e2c9003cdd05147
Laila Arkadan <lailaarkadan@Lailas-MacBook-Air.local>
2021-10-26 14:50:45 -0600

    started writing the code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // Your code goes here
+
+const logo = document.querySelector('.logo-heading');
+const mainNav = document.querySelector('.main-navigation');
+const navBar = document.querySelectorAll('.nav-link');
+const footer = document.querySelector('.footer');
+const footer1 = document.querySelector(".footer p");
+const footBack= document.querySelectorAll("footer p");
+const imgs = document.querySelectorAll("img");
+const body = document.querySelector("body");
+const btns = document.querySelectorAll('.btn');
+const btn = document.querySelector(".btn");
+const nav = document.querySelectorAll('a');
+const para = document.querySelectorAll('p');
+const h1 = document.querySelector('h1');
+const h2 = document.querySelectorAll('h2');
+const h4 = document.querySelectorAll('h4');
+
+
