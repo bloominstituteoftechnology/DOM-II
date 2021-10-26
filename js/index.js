8928ad3eb2a9b100687b5539816e733bdfee210c
MNoble2014 <michealsimpson2013@gmail.com>
2021-10-26 16:11:44 -0400

    event listener 2 added

--- js/index.js
+++ js/index.js
@@ -23,3 +23,22 @@ headingAnimation.addEventListener('click', () => {
     headingAnimation.style.transition = '5s ease-out';
     headingAnimation.style.color = colorGenerator();
 })
+
+//Add color to h2 headings when moused over
+let h2Animation = document.querySelectorAll('h2');
+
+h2Animation.forEach((element) => {
+    element.addEventListener('mouseover', (event) => {
+        element.style.transitionProperty = 'color';
+        element.style.transition = 'ease-in';
+        element.style.color = 'purple';
+    });
+    element.addEventListener('mouseout', (event) => {
+        element.style.transitionProperty ='color';
+        element.style.transition = '3s ease-out';
+        element.style.color = 'black';
+    });
+    element.addEventListener('click', (event) => {
+        event.preventDefault()
+    });
+});
