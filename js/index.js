ba247312951ac8b2b47a56d26eb2b4491db88edc
Nithya Narayanan <n.nithya@pm.me>
2021-10-26 14:13:58 -0700

    Added onmouseover.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+//onmouseOver
+const imgSelected = document.querySelector('.img-content');
+imgSelected.onmouseover = imgMouseOver;
+imgSelected.onmouseout = imgMouseOut;
+
+function imgMouseOver() {
+  imgSelected.style.color = "purple";
+  imgSelected.textContent = 'You Turned me purple!';
+}
+
+function imgMouseOut(){
+    imgSelected.style.color = "Black";
+    imgSelected.textContent = 'Back to Normal';
+}
+//
