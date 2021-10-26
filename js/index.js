9ef414841be8e489cdc3b673899e180f45114dbb
quavo <qua.thesage19@gmail.com>
2021-10-26 14:46:20 -0500

    Finished event 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 // Your code goes here
+// Event 1
 const imgChange = document.querySelector('.intro img');
 imgChange.addEventListener('mouseover', function(evt) {
     evt.target.style.boxShadow = "10px 15px 2px #DEB887";
 });
-console.log(imgChange)
+
+// Event 2
+const navChange = document.querySelectorAll('.nav .nav-link');
+ Array.from(navChange).forEach(change =>{
+    change.addEventListener('click', function(evt) {
+        evt.target.style.color = '#DEB887';
+    });
+
+    change.removeEventListener('click', function(evt) {
+        evt.target.style.color = "black";
+    })
+ });
