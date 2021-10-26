18a57bd440a8cb7b2fbb732b6b8d98d6477fddc1
Ryan Howard <ryanghoward@protonmail.com>
2021-10-26 12:58:52 -0700

    Keyup and Keydown events added

--- js/index.js
+++ js/index.js
@@ -1,19 +1,33 @@
-//* 1. Load
+//! 1. Load
 window.addEventListener('load', () => {
     console.log('Congrats, the page has loaded!')
 });
 
-//* 2. Click
+//! 2. Click
 const buttonClick = document.querySelectorAll('.btn');
 buttonClick[0].addEventListener('click', e => {
-    buttonClick[0].textContent = `Hey, don't click that! ${e.detail}`
+    buttonClick[0].textContent = `Hey, don't click me! ${e.detail}`
 });
 
-//* 3. MouseOver
+//! 3. MouseOver
 const mouseOver = document.querySelector('.logo-heading');
 mouseOver.addEventListener('mouseover', e => {
     e.target.style.color = 'purple';
     setTimeout(function () {
         e.target.style.color = '';
-    }, 1000);
+    }, 5000);
 });
+
+//! 4. Key Down
+const keyDown = document.querySelector('body');
+keyDown.addEventListener('keydown', e => {
+    e.target.style.color = '#8899A6';
+    e.target.style.backgroundColor = '#15202B';
+});
+
+//! 5. Key Up
+const keyUp = document.querySelector('body');
+keyUp.addEventListener('keyup', e => {
+    e.target.style.color = 'black';
+    e.target.style.backgroundColor = 'white';
+})
