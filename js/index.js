4cedba548133fe74cae373848cb91c03e2414cce
Nyamekye Annor <nyamekyeannor@Nyamekyes-MacBook-Pro.local>
2021-10-26 19:56:14 -0500

    another push

--- js/index.js
+++ js/index.js
@@ -12,3 +12,26 @@ logoHeading.addEventListener("click", logo);
 function logo() {
     logoHeading.style.color = "blue"
 }
+
+// This turns the entire text on the page red
+const entirePage = document.querySelector("body");
+entirePage.addEventListener("keydown", body);
+function body() {
+  entirePage.style.color = "red"
+}
+
+// This will alert "DOM is ready" When I refresh the page
+function ready(){alert('DOM is ready');}
+document.addEventListener('DOMContentLoaded', ready);
+
+// This will turn .text-content orange when hovered
+const text = document.querySelector(".text-content")
+text.addEventListener("mouseendter", mouse)
+function mouse(){
+    text.style.color ="orange"
+}
+
+// This will tun my a link (Home) red when i scroll
+const nav = document.querySelector("a");
+window.addEventListener('scroll', (e) => (nav.style.color = "red"))
+
