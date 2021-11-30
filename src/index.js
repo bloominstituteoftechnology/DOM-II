3ec662f3771e19e142c86c3b95f181f90658b32b
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2021-11-30 16:51:37 -0500

    added mouse over event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 import './less/index.less'
 
 // Your code goes here!
+const title = document.querySelector('h1');
+console.log(title.textContent);
+title.addEventListener("mouseover", function( event ) {
+    // highlight the mouseover target
+    event.target.style.color = "hotpink";
+  
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 500);
+  }, false);
