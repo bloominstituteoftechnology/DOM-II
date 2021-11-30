4704ca4f2632971e20b4f8f9ffc91da8b63fb676
Andrew Nelson <Realseal177@gmail.com>
2021-11-30 14:41:02 -0800

    2 done, keydown and load

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//keydown event
+const logo = document.querySelector('.logo-heading');
+
+function fKey(evt) {
+    if (evt.key === 'f') {
+        logo.textContent = "Hella Fun Freakin Bus";
+    }
+};
+document.addEventListener('keydown', fKey);
+
+//load event 
+window.addEventListener('load', (evt) => {
+    alert('This page is loaded! Just like you will be on this Hella Fun Freakin Bus!');
+    
+})
+
+
+
+
