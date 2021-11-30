d03e9d59a930b61f13093ff99b4ab17237bd73a6
Brett Smith <brettsmith212@gmail.com>
2021-11-30 12:23:51 -0800

    2 event listeners added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+
+const logo = document.querySelector("h1.logo-heading");
+const homeImg = document.querySelector(".intro img");
+
+logo.addEventListener("mouseover", (e) => {
+  e.target.style.color = "red";
+});
+
+homeImg.addEventListener("dblclick", (e) => {
+  e.target.style.borderRadius = "10px";
+  e.target.style.transform = "scale(1.1)";
+});
