88ca907216f4ed2fdfbdcdb8c674d2c0a6b3ab7b
Chris Kelley <chriskelley85@gmail.com>
2021-11-30 13:13:09 -0600

    thru 1 event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//1 - underline nav links
+const navLinks = document.querySelectorAll('.nav-link');
+navLinks.forEach(function(link){
+    link.addEventListener('mouseover', function() {
+        link.style.textDecoration = 'underline';
+    })
+})
