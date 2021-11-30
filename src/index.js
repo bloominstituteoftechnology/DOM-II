822948330427eda7836b07a19111ab8009e60261
Richard Dowd <richarddowd@Richards-MacBook-Pro.local>
2021-11-30 14:26:42 -0800

    have one event listener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// mouseover
+const logo = document.querySelector('.logo-heading');
+
+const mouseBlue = () => {
+    logo.style.backgroundColor = 'dodgerblue';
+}
+
+logo.addEventListener('mouseover', mouseBlue)
+
+const mouseOut = () => {
+    logo.style.backgroundColor = '';
+}
+logo.addEventListener('mouseout', mouseOut)
