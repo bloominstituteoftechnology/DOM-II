5df33842e9363224c3489c7c129a1206b165a2f7
Ant Man <34754995+aaronamendez@users.noreply.github.com>
2021-11-30 17:52:25 -0500

    Finished 2 events and added a prevent default.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+const header = document.querySelector("header");
+function hoverColor(e) {
+	e.target.style.backgroundColor = "red";
+	// console.log(e);
+}
+header.addEventListener("mouseover", hoverColor);
+
+const aTags = document.querySelectorAll("a");
+aTags.forEach((el) => {
+	el.addEventListener("click", function (e) {
+		e.preventDefault();
+		e.target.style.color = "blue";
+	});
+});
