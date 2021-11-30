caa95150355a3c5cbaa70103cef3d9a28bf18cec
lomelo_x <aalomelo@gmail.com>
2021-11-30 14:21:00 -0600

    Trigger 5: KeyDown

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './less/index.less'
 // Your code goes here!
 const links = document.querySelectorAll("a");
 const title = document.querySelector('.logo-heading')
+const body = document.querySelector('body')
 //Trigger 1: Mouse Over
 links.forEach(link => {
     link.addEventListener("mouseover", function(event) {
@@ -32,3 +33,23 @@ title.addEventListener("mouseenter", function(event) {
 title.addEventListener("mouseleave", function(event) {
         event.target.textContent = 'Fun Bus';
     });
+
+//Trigger 5: KeyDown  
+function sixKey(event) {
+    if (event.key === "6") {
+        body.style.backgroundColor = 'black';
+    } else {
+    body.style.backgroundColor = 'white';
+    }
+}
+document.addEventListener("keydown", sixKey);
+
+//Trigger 6:  
+
+//Trigger 7:  
+
+//Trigger 8:  
+
+//Trigger 9:  
+
+//Trigger 10:
