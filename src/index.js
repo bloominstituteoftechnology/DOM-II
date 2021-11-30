2b22ca9ded76a8786f8d043752756abd2d55a99c
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2021-11-30 17:46:45 -0500

    added mouseenter and mouse leave even listener

--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ logo.addEventListener("mouseover", function (event) {
   event.target.style.color = "green";
   event.target.style.fontSize = "80px";
 });
+const navBar = document.querySelector("nav");
+navBar.addEventListener("mouseenter", function (ev) {
+  ev.target.style.backgroundColor = "orange";
+  ev.target.style.fontStyle = "italic";
+});
+navBar.addEventListener("mouseleave", function (ev) {
+  ev.target.style.backgroundColor = "white";
+  ev.target.style.fontStyle = "normal";
+});
