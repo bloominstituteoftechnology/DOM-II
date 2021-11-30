ccbc529d3acdf51e80d6a99d45a4fd835c24db5d
Carlton Lauckern <reed@Carltons-MacBook-Air.local>
2021-11-30 15:30:49 -0800

    added-wheel

--- src/index.js
+++ src/index.js
@@ -2,20 +2,101 @@ import './less/index.less'
 
 
 // Your code goes here!
-const mouseOver = document.querySelectorAll('a');
+//display secret message when user clicks map photo
+const logoHead = document.querySelector('.logo-heading');
 
-const keyDown = document.querySelector('body');
+const mousePink = () => {
+    logoHead.style.backgroundColor = 'pink';
+}
 
-const wheelTurn = document.querySelector('footer');
+logoHead.addEventListener('mouseover', mousePink)
 
-const loadTopage = document.querySelector('img');
+const mouseOut = () => {
+    logoHead.style.backgroundColor = '';
+}
 
-const focusOntext = document.querySelector('.text-content');
+logoHead.addEventListener('mouseout', mouseOut);
 
-const resizeWindow = document.querySelector('*');
+const mapPhoto = document.querySelector('.img-content');
+mapPhoto.addEventListener('click', displayMessage)
 
-const scroll = document.querySelector('section');
+function displayMessage() {
+    alert ("Congratulations! Secret Found!")
+}
 
-const select = document.querySelector('src');
+//navigate through Nav Bar with Keyboard
+const nav = document.querySelector('nav');
 
-const onContextMenu = document.querySelector('html');
+const home = nav.firstElementChild;
+const about = home.nextElementSibling;
+const blog = about.nextElementSibling;
+const contact = blog.nextElementSibling;
+
+// //nav to home
+// document.addEventListener('keydown', pressOneForHome)
+
+// function pressOneForHome (evt) {
+//     evt.preventDefault();
+//     evt.stopPropagation();
+//     if (evt.key === '1') {
+//         window.location.href = home.getAttribute('href');
+//     }
+// }
+// function pressTwoForAbout (evt) {
+//     evt.preventDefault();
+//     evt.stopPropagation();
+//     if (evt.key === '2') {
+//         window.location.href = about.getAttribute('href');
+//     }
+// }
+// //nav to blog
+// document.addEventListener('keydown', pressThreeForBlog)
+
+// function pressThreeForBlog (evt) {
+//     evt.preventDefault();
+//     evt.stopPropagation();
+//     if (evt.key === '3') {
+//         window.location.href = blog.getAttribute('href');
+//     }
+// }
+
+// //nav to contact
+// document.addEventListener('keydown', pressFourForContact)
+
+// function pressFourForContact (evt) {
+//     evt.preventDefault();
+//     evt.stopPropagation();
+//     if (evt.key === '4') {
+//         window.location.href = contact.getAttribute('href');
+//     }
+// }
+
+
+
+//wheel
+function zoom(event) {
+    event.preventDefault();
+    scale += event.deltaY * -0.01;
+    scale = Math.min(Math.max(.125, scale), 4);
+    el.style.transform = `scale(${scale})`;
+}
+
+let scale = 1;
+const el = document.querySelector('img');
+el.onwheel = zoom;
+
+//load
+
+el.addEventListener('load', function(){
+    alert('Page is loaded loaded')
+})
+
+const images =  document.querySelectorAll('.img-content img')
+
+const bigger = (event,img) => {
+    img.style.width = '60%';
+}
+
+images.forEach(img => {
+    img.addEventListener('dblclick', (event) => bigger(event,img))
+});
