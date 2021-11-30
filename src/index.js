58e9d52a04aa04ad3cba4206d365d1a4b5b7a36d
David Conley <dconley1989@gmail.com>
2021-11-30 13:50:41 -0700

    added style to the logo header when you mouse over it

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+const funBusHeader = document.querySelector("h1");
+
+function linkHover() {
+  funBusHeader.style.color = "yellow";
+}
+
+funBusHeader.addEventListener("mouseover", linkHover);
