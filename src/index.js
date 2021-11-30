2a303dabf5d171c644dcb0a19e3b33cc3723bb7f
Berenika Ahmetaj <ahmetajberenika@gmail.com>
2021-11-30 14:31:16 -0500

    added mouseover eventlistener

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
+const logo = document.querySelector(".logo-heading");
+logo.addEventListener("mouseover", function (event) {
+  event.target.style.color = "green";
+  event.target.style.fontSize = "80px";
+});
