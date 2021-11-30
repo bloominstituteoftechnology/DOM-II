c9aa96f346b2edb0aebc69cb56ae57290ef75425
lomelo_x <aalomelo@gmail.com>
2021-11-30 14:48:21 -0600

    Trigger 7: Wheel

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './less/index.less'
 const links = document.querySelectorAll("a");
 const title = document.querySelector('.logo-heading')
 const body = document.querySelector('body')
-const img = document.querySelector('img')
+const headImg = document.querySelector('img')
+
 //Trigger 1: Mouse Over
 links.forEach(link => {
     link.addEventListener("mouseover", function(event) {
@@ -17,46 +18,51 @@ links.forEach(link => {
 
 //Trigger 2: Click
 links.forEach(link => {
-        link.addEventListener("click", function(event) {
-            event.target.style.color = "red";
-            setTimeout(function() {
-                event.target.style.color = "";
+    link.addEventListener("click", function(event) {
+        event.target.style.color = "red";
+        setTimeout(function() {
+            event.target.style.color = "";
             }, 500);
-        }, false);
-    })
+    }, false);
+})
 
 //Trigger 3: Mouse Enter
 title.addEventListener("mouseenter", function(event) {
-        event.target.textContent = 'Bun Fus';
-    });
+    event.target.textContent = 'Bun Fus';
+});
 
 //Trigger 4: Mouse Leave
 title.addEventListener("mouseleave", function(event) {
-        event.target.textContent = 'Fun Bus';
-    });
+    event.target.textContent = 'Fun Bus';
+});
 
 //Trigger 5: KeyDown  
 function sixKeyDown(event) {
     if (event.key === "6") {
         body.style.backgroundColor = 'black';
-    } else {
-    body.style.backgroundColor = 'white';
-    }
+    }
 }
 document.addEventListener("keydown", sixKeyDown);
 
 //Trigger 6: KeyUp
 function sixKeyUp(event) {
-        if (event.key === "6") {
-            body.style.backgroundColor = 'white';
-        } else {
+    if (event.key === "6") {
         body.style.backgroundColor = 'white';
-        }
     }
+}
 document.addEventListener("keyup", sixKeyUp);
 
-//Trigger 7:  
-
+//Trigger 7: Wheel
+function zoom(event) {
+    event.preventDefault();
+    scale += event.deltaY * -0.01;
+    // Restrict scale
+    scale = Math.min(Math.max(1, scale), 2);
+    // Apply scale transform
+    headImg.style.transform = `scale(${scale})`;
+}
+let scale = 1;
+headImg.onwheel = zoom;
 //Trigger 8:  
 
 //Trigger 9:  
