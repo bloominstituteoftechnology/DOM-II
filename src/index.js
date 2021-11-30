a0af7f581be7e3291a0270be84c7a41839ee8f33
Carlton Lauckern <reed@Carltons-MacBook-Air.local>
2021-11-30 12:54:44 -0800

    quyery-selectors

--- src/index.js
+++ src/index.js
@@ -2,3 +2,20 @@ import './less/index.less'
 
 
 // Your code goes here!
+const mouseOver = document.querySelectorAll('a');
+
+const keyDown = document.querySelector('body');
+
+const wheelTurn = document.querySelector('footer');
+
+const loadTopage = document.querySelector('img');
+
+const focusOntext = document.querySelector('.text-content');
+
+const resizeWindow = document.querySelector('*');
+
+const scroll = document.querySelector('section');
+
+const select = document.querySelector('src');
+
+const onContextMenu = document.querySelector('html');
