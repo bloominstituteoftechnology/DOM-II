9fbd7e918f5b46dd8dea67dfdbf364666c71fb24
rstott87 <rstott87@yahoo.com>
2021-11-29 19:52:51 -0500

    completed first step

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// 1.) changes text of "Fun Bus" on hover
+const logoHeading = document.querySelector(".logo-heading");
+
+logoHeading.addEventListener('mouseover', (event) => {
+    event.target.innerHTML = 'Bad Bus';
+        setTimeout(function() {
+        event.target.innerHTML = 'Fun Bus';
+    }, 500);
+}, false); 
+
+//2.) changes nav bar to red 
+const xc = document.querySelector('nav');
+xc.addEventListener('click', (event) => {
+    event.target.style.backgroundColor = 'red';
+})
+
+
+
+
+//logoHeading.addEventListener('blur', (event) => {
+ //   event.target.innerHTML = 'xx';
+  //});
