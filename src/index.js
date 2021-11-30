44c284b57b18d2711d696aed652794ab662fba17
Tuan147 <90153951+Tuan147@users.noreply.github.com>
2021-11-30 17:02:55 -0500

    added in 6 event listeners so far

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import './less/index.less'
 
 // Your code goes here!
+
+const links = document.querySelectorAll("a");
+links.forEach(link => {
+    link.addEventListener("click", function(event) {
+        event.target.style.fontWeight = "bold";
+        event.preventDefault();
+    })
+});
+
+const imgs = document.querySelectorAll("img");
+imgs.forEach(img => {
+    img.addEventListener("mouseover", function (evt) {
+        evt.target.style.border = "red 10px solid";
+    })
+});
+
+const h2s = document.querySelectorAll(".h2");
+h2s.forEach(h2 => {
+    h2.addEventListener("dblclick", evt => {
+        evt.target.style.textDecoration = "underline";
+    })
+});
+
+const ps = document.querySelectorAll(".p");
+ps.forEach(p => {
+    p.addEventListener("keydown", event => {
+        if (event.isComposing || event.keyCode === 229){
+            return console.alert("Help!");
+        }
+    })
+});
+
+const buttons = document.querySelectorAll("btn");
+buttons.forEach(btn => {
+    btn.addEventListener("focusin", evt => {
+        evt.target.style.background = "purple";
+    })
+    btn.addEventListener("focusout", evt => {
+        evt.target.style.background = "";
+    });
+});
