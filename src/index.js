a44b0d9c594797157c311a0e67f2b1c6680c30e3
rstott87 <rstott87@yahoo.com>
2021-11-30 19:19:35 -0500

    Final changes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 //./less/index.less'
 
+//const e = require("express");
+
 // Your code goes here!
 
-// 1.) changes text of "Fun Bus" on hover
+// 1.) MOUSEOVER EVENT changes text of "Fun Bus" on hover
 const logoHeading = document.querySelector(".logo-heading");
 
 logoHeading.addEventListener('mouseover', (event) => {
@@ -12,13 +14,14 @@ logoHeading.addEventListener('mouseover', (event) => {
     }, 500);
 }, false); 
 
-//2.) changes nav bar to red 
+//2.) CLICK EVENT changes nav bar to red on 
+
 const xc = document.querySelector('nav');
 xc.addEventListener('click', (event) => {
     event.target.style.backgroundColor = 'red';
 })
 
-//3. makes bus photo bigger or smaller
+//3. ZOOM EVENT makes bus photo bigger or smaller
 const busPocc = document.querySelector('.intro img'); 
 
 function zoom(event) {
@@ -35,7 +38,7 @@ function zoom(event) {
   let scale = 1;
 busPocc.addEventListener('wheel', zoom);
 
-//4 random color on key press in body
+//4 and 5 KEYUP EVENT and KEYDOWN EVENT - random color on key press in body
 
 function random_bg_color() {
     var x = Math.floor(Math.random() * 256);
@@ -48,28 +51,65 @@ function random_bg_color() {
 const keyPress = document.querySelector("body")
 keyPress.addEventListener('keydown',random_bg_color)
 
-// 5 
+
 const keyPressUp = document.querySelector("body");
 keyPressUp.addEventListener('keyup' , () =>{
     keyPressUp.style.backgroundColor = null
 })
 
-//5
+//6 RESIZE EVENT - changes h2 element to report window size.
 
 const heightOutput= document.querySelector(".text-content h2")
 
 
 function reportWindowSize() {
-  heightOutput.textContent = ` Window height is ${window.innerHeight}`;
+  heightOutput.textContent = ` Window height is ${window.innerHeight}. Double Click to return to orinal text`;
 }
 
 window.onresize = reportWindowSize;
 
-//6
+//7 DOUBLECLICK EVENT - returns above h2 to oringal text on double click
 
 
 const original= document.querySelector(".text-content h2")  
-original.addEventListener('dblclick'()=>{
-    original.textContent = "return"
+original.addEventListener('dblclick', () =>{
+    original.textContent = "Let's Go!"
 })
+
+
+//8 and 9 MOUSEENTER and MOUSELEAVE events. Changes all text in p's to italices when mouse enters like hover
+// changes all text back to normal when mouse "leaves"
+
+const focus  = document.querySelectorAll('p');
+
+focus.forEach((el) => {
+    el.addEventListener('mouseenter', () => {
+        el.style.fontStyle = "italic"
+    })
+
+    el.addEventListener('mouseleave', () => {
+        el.style.fontStyle = "normal"
+    })
+});
+
+
+//10 and 11  MOUSEUP and MOUSEDOWN events. Changes background color of h4's to blue on down click, back
+//to white on release or mouseup 
+
+const mouseDownUp = document.querySelectorAll("h4");
+
+mouseDownUp.forEach((down)=>{
+    down.addEventListener('mousedown', () => {
+        down.style.backgroundColor = "blue";
+    })
+    down.addEventListener('mouseup', () => {
+        down.style.backgroundColor = "white";
+    })
+});
+
+
+
+
+
+
 
