d4d5c8dd044312a1ad33fdf37c325d54bc7dcee8
Britt Dalby <91482954+brittney-dalby@users.noreply.github.com>
2021-11-30 18:16:35 -0700

    worked through another event

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import './less/index.less'
 
-// Your code goes here!
-const colorChangeNav = document.querySelector('.main-navigation');
-colorChangeNav.addEventListener('mouseover', function(){
-    colorChangeNav.style.backgroundColor = 'light blue';
+//change color of title when mouse goes over it
+const colorChangeTitle = document.querySelector('.main-navigation');
+colorChangeTitle.addEventListener('mouseenter', function(event){
+    event.target.style.color = 'purple';
 });
-colorChangeNav.addEventListener('mouseout', function(){
-    colorChangeNav.style.backgroundColor = 'pink';
-});
+// colorChangeNav.addEventListener('mouseout', function(){
+//     colorChangeNav.style.backgroundColor = 'pink';
+// });
 
+//change color of background when page clicked
+const colorChangeNav = document.querySelector('p');
 
+function random(number) {
+    return Math.floor(Math.random() * (number+1));
+}
+colorChangeNav.onclick = function() {
+    const randomColor = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
+    document.body.style.backgroundColor = randomColor;
+}
