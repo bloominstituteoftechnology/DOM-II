38678f5f37dd7338508f681374d7e9b0dffb4613
Kayla Sade <sade@Kaylas-MacBook-Pro.local>
2021-11-30 20:42:05 -0500

    added alert when enter key is down

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// Title
 const title = document.querySelector('h1');
 console.log(title.textContent);
 title.addEventListener("mouseover", function( event ) {
-    // highlight the mouseover target
-    event.target.style.color = "hotpink";
-  
-    // reset the color after a short delay
+    event.target.style.color = "hotpink";  
     setTimeout(function() {
       event.target.style.color = "";
-    }, 500);
+    }, 400);
   }, false);
+
+// Tab key fn
+const printKey = function(e) {
+    // console.log(e.key);
+    if(e.key === "Enter") {
+        alert("Warning! The fun bus can be extremely fun!")
+    }
+}
+
+document.addEventListener("keydown", printKey);
+
+
+
