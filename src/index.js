1f13c608419d311331dc5a602919130f67ffda99
Visnu <vgcaisse@gmail.com>
2021-11-30 22:27:49 -0500

    got a couple done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// -------------------------One-----------------------------
+window.addEventListener('load', event => {
+    console.log(`${event.type} happened`);
+});
+// -------------------------Two-----------------------------
+// const buttonsClick = document.querySelectorAll(".btn");
+
+// function clickButton() {
+//     console.log('None of these buttons work');
+// }
+
+// buttonsClick.addEventListener("click", clickButton);
+// -------------------------Three-----------------------------
+// const button = document.querySelector('.btn');
+
+// button.addEventListener('focus', (event) => {
+//   event.target.style.background = 'pink';
+// });
+
+// button.addEventListener('blur', (event) => {
+//   event.target.style.background = '';
+// });
+
+// -------------------------Four-----------------------------
+// const links = document.querySelectorAll("#");
+
+// links.forEach(link => {
+//   link.addEventListener("click", function(event) {
+//     event.preventDefault();
+//   });
+// });
+
+// -------------------------Five-----------------------------
+window.addEventListener('scroll', event => {
+    console.log(`${event.type} happened`);
+});
+
+// -------------------------Six-----------------------------
+const button = document.querySelector('.btn'); 
+button.addEventListener('mouseover', event => {
+    console.log(`${event.type} happened`);
+});
+
+// -------------------------Seven-----------------------------
+
+
+// -------------------------Eight-----------------------------
+
+
+// -------------------------Nine-----------------------------
+
+
+// -------------------------Ten-----------------------------
+
+
