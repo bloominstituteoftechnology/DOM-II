1214f9d1b780ed1dcf94ca58646c449f07bcf206
Shamir Gibbons <nonshalant@shamirs-air.myfiosgateway.com>
2021-11-30 20:35:30 -0500

    update after 20 minutes

--- src/index.js
+++ src/index.js
@@ -2,29 +2,55 @@ import './less/index.less'
 
 // Your code goes here!
 const navBar = document.querySelectorAll('.nav-link');
-const header = document.querySelector('header')
+const header = document.querySelector('header');
+const navLinks = document.querySelectorAll('nav a');
+const buttons = document.querySelectorAll('.btn');
+const footerr = document.querySelector('.footer p')
 
-function colorChange(){
-    document.body.style.backgroundColor = 'grey';
-}
+const newLinks = Array.from(navLinks);
 
 navBar.forEach(item => item.addEventListener('click', colorChange));
+
+buttons.forEach(item => item.addEventListener('click', buttonsPressed))
+
+function colorChange(item){
+    document.body.style.backgroundColor = 'grey';
+
+}
 
 function scrollBy (){
     if( window.scrollY > 45 ){
         header.style.backgroundColor = 'black';
         header.style.color = 'whitesmoke';
+        for(let link of navLinks){
+            link.style.color = 'whitesmoke';
+        }
     }else{
-        header.style.backgroundColor = 'whitesmoke';
+        header.style.backgroundColor = 'white';
+        header.style.color = 'black';
         header.style.transition = '.5s';
+        for(let link of navLinks){
+            link.style.color = 'black';
+        }
     }
 }
 
 function alerted(){
-    alert('Welcome to FUN BUS :D')
+    alert('Welcome to FUN BUS :D');
     
 }
 
+function annoyingAlert (){
+    alert('dont mind just being annoying');
+
+}
+
+function buttonsPressed(){
+//    footerr.style.font-size = 'green';
+}
+
+
+window.addEventListener('animationend', annoyingAlert)
 window.addEventListener('scroll', scrollBy);
-window.addEventListener('load', alerted)
+// window.addEventListener('load', alerted);
 
