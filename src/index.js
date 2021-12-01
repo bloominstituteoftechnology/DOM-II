cafd0c139d5ced1d989d3063ba8a30df001ff1ce
AnneKGoodfriend <AnneKGoodfriend@gmail.com>
2021-11-30 23:19:36 -0500

    9 down

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import './less/index.less'
 
-// Your code goes here!
+// mouseover
+const menu = document.querySelector(".main-navigation");
+menu.addEventListener("mouseover", (event) =>{
+    menu.style.backgroundColor = 'pink';
+})
+
+//click
+const introImage = document.querySelector(".intro img")
+introImage.addEventListener("click", (event) =>{
+    event.target.style.filter = "blur(7px)";
+})
+
+//dblclick
+introImage.addEventListener("dblclick", (event) =>{
+    introImage.style.filter = "blur(0px)";
+})
+
+//keydown
+document.addEventListener("keydown", (event) =>{
+    //console.log(event);
+    if(event.key === "x"){     
+        menu.style.borderColor = "red";
+        }
+})
+
+//resize
+const image1 = document.querySelector(".img-content img")
+window.addEventListener('resize', (event) =>{
+    image1.style.filter = "grayscale(100%)";    
+});
+
+//scroll
+const image2 = document.querySelector('.img-fluid')
+window.addEventListener('scroll', (event) =>{
+    image2.style.filter = "grayscale(100%)";     
+});
+
+//mousenter
+image1.addEventListener('mouseenter', (event) => { 
+    image1.style.filter = "grayscale(0%)";  
+  });
+
+//mouseleave
+image1.addEventListener('mouseleave', (event) => { 
+    image1.style.filter = "blur(2px)";  
+  });
+
+  
+
+
+
