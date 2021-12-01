b7ed5def933fd436a2c2829d38024eba584342df
Jessica Kitchen <jess@Jessicas-Mini.lan1>
2021-11-30 19:49:24 -0500

    completed 2 event listeners

--- src/index.js
+++ src/index.js
@@ -1,3 +1,49 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//click event
+const homeButton = document.querySelector('a')
+function changeColor(){
+  homeButton.style.color = 'purple';  
+  homeButton.style.fontSize = '5rem';
+}
+
+homeButton.addEventListener('click', changeColor);
+
+//double click event
+const welcomeFunBus = document.querySelector('.intro h2');
+
+function funBus (){
+    welcomeFunBus.style.fontSize = '2rem';
+}
+
+//load event
+
+
+
+//scroll event
+
+
+
+//wheel event
+
+
+
+//blur event
+
+
+
+//mouseup event
+
+
+
+//mouseover event
+
+
+
+//mouseout event
+
+
+
+//keydown event
