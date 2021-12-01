ceaef6f69bf17370fe0b54e525ebd19e4cb966ec
Ken Smith <kennethologist@gmail.com>
2021-12-01 11:01:40 -0500

    Made variable declarations for items

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './less/index.less'
 
 // Your code goes here!
+const logo = document.querySelector('.logo-heading');
+const nav = document.querySelector('nav');
+const introImg = document.querySelector('.intro > img');
+const introText = document.querySelector('.intro > p');
+const imgAdv = document.querySelector('section > .img-content > img');
+const imgDestination = document.querySelector('.content-destination . img');
+const btnFunSunSignup = document.querySelector('.destination > .btn')
+const copyright = document.querySelector('.footer > p');
+
