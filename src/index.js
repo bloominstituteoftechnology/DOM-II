fb3c852f370f056397a645c181732fb9a6de323a
Britt Dalby <91482954+brittney-dalby@users.noreply.github.com>
2021-11-30 17:21:47 -0700

    Working through first event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './less/index.less'
 
 // Your code goes here!
+const colorChangeNav = document.querySelector('.main-navigation');
+colorChangeNav.addEventListener('mouseover', function(){
+    colorChangeNav.style.backgroundColor = 'light blue';
+});
+colorChangeNav.addEventListener('mouseout', function(){
+    colorChangeNav.style.backgroundColor = 'pink';
+});
+
+
