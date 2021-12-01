9d1bfd71cfce0df3e8b8aaddcf89f24719c6b233
Britt Dalby <91482954+brittney-dalby@users.noreply.github.com>
2021-12-01 08:19:56 -0700

    did as many as I could

--- src/index.js
+++ src/index.js
@@ -3,19 +3,29 @@ import './less/index.less'
 //change color of title when mouse goes over it
 const colorChangeTitle = document.querySelector('.main-navigation');
 colorChangeTitle.addEventListener('mouseenter', function(event){
-    event.target.style.color = 'purple';
+    event.target.style.backgroundColor = 'pink';
 });
-// colorChangeNav.addEventListener('mouseout', function(){
-//     colorChangeNav.style.backgroundColor = 'pink';
-// });
+colorChangeTitle.addEventListener('mouseout', function(event){
+    event.target.style.backgroundColor = 'white';
+});
 
 //change color of background when page clicked
-const colorChangeNav = document.querySelector('p');
+const colorChangePara = document.querySelector('p');
 
 function random(number) {
     return Math.floor(Math.random() * (number+1));
 }
-colorChangeNav.onclick = function() {
+colorChangePara.onclick = function() {
     const randomColor = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
     document.body.style.backgroundColor = randomColor;
-}
+};
+
+//change the size of an image when clicked
+// const imageEnlarged = document.querySelector('.img-content');
+
+// imageEnlarged.onclick = function() {
+//     imageEnlarged.style.width = '200%';
+//     imageEnlarged.style.height  = 'auto';
+// };
+
+//
