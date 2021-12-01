87e3cfcf40c4a50cceca8315a0d5be39e54ced08
Kenneth Mercer <kmercer5439@gmail.com>
2021-11-30 22:46:09 -0600

    2 / 10 complete (i know im running late)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//Mouseover
+const introh2 = document.querySelector('.intro h2');
+console.log(introh2);
+
+introh2.addEventListener("mouseenter", function(event){
+    event.target.style.color = 'yellow';
+    setTimeout(function() {
+        event.target.style.color = "";
+      }, 500);
+    }, false);
+
+//Double Click
+const midImg = document.querySelector('.img-content img');
+console.log(midImg);
+
+midImg.addEventListener('dblclick', function (e) {
+  midImg.style.opacity = "0.5";
+  midImg.style.border = "10px dotted black";
+  setTimeout(function() {
+    midImg.style.opacity = "";
+    midImg.style.border = "";
+  }, 500);
+}, false);
