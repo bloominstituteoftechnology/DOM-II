449c68eae156a2d1dc5b7032d3c8c8687640077b
Shamir Gibbons <nonshalant@Shamirs-Air.lan>
2021-11-30 19:21:04 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './less/index.less'
 
 // Your code goes here!
+const navBar = document.querySelectorAll('.nav-link');
+const header = document.querySelector('header')
+
+function colorChange(){
+    document.body.style.backgroundColor = 'grey';
+}
+
+navBar.forEach(item => item.addEventListener('click', colorChange));
+
+function scrollBy (){
+    if( window.scrollY > 45 ){
+        header.style.backgroundColor = 'black';
+        header.style.color = 'whitesmoke';
+    }else{
+        header.style.backgroundColor = 'whitesmoke';
+        header.style.transition = '.5s';
+    }
+}
+
+window.addEventListener('scroll', scrollBy);
+
+
