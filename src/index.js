eb4589f45e7a728b1f946e1b7f13faa4699f9f15
scotty-yu <scotty.yu@yahoo.com>
2021-11-30 20:52:55 -0800

    progress

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import './less/index.less'
 
 // Your code goes here!
+const nav = document.querySelector('.nav');
+nav.addEventListener("mouseover", function( event ) {
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+  
+    // reset the color after a short delay
+    setTimeout(function() {
+      event.target.style.color = "";
+    }, 200);
+  }, false);
+
+nav.addEventListener('click', function(event){
+    button.textContent = `Click count: ${event.detail}`;
+});
