7e880ea62b6fb1355eef6b84043bf11f26e66cbe
Visnu <vgcaisse@gmail.com>
2021-11-30 23:05:36 -0500

    got a couple more done, starting to understand a bit

--- src/index.js
+++ src/index.js
@@ -7,39 +7,11 @@ window.addEventListener('load', event => {
     console.log(`${event.type} happened`);
 });
 // -------------------------Two-----------------------------
-// const buttonsClick = document.querySelectorAll(".btn");
-
-// function clickButton() {
-//     console.log('None of these buttons work');
-// }
-
-// buttonsClick.addEventListener("click", clickButton);
-// -------------------------Three-----------------------------
-// const button = document.querySelector('.btn');
-
-// button.addEventListener('focus', (event) => {
-//   event.target.style.background = 'pink';
-// });
-
-// button.addEventListener('blur', (event) => {
-//   event.target.style.background = '';
-// });
-
-// -------------------------Four-----------------------------
-// const links = document.querySelectorAll("#");
-
-// links.forEach(link => {
-//   link.addEventListener("click", function(event) {
-//     event.preventDefault();
-//   });
-// });
-
-// -------------------------Five-----------------------------
 window.addEventListener('scroll', event => {
     console.log(`${event.type} happened`);
 });
 
-// -------------------------Six-----------------------------
+// -------------------------Three-----------------------------
 const button = document.querySelectorAll('.btn'); 
 // button.addEventListener('mouseover', event => {
 //     console.log(`${event.type} happened`);
@@ -48,13 +20,28 @@ const button = document.querySelectorAll('.btn');
 button.forEach(button => {
     button.addEventListener('mouseover', function(event) {
         console.log(`${event.type} happened`);
-    })
-})
+    });
+});
 
-// -------------------------Seven-----------------------------
+// -------------------------Four-----------------------------
 window.addEventListener('keydown', event => {
     console.log(`${event.type} happened`);
 });
+
+// -------------------------Five-----------------------------
+button.forEach(button => {
+    button.addEventListener('dblclick', function(event) {
+        console.log(`event passing through ${event.currentTarget.nodeName}`);
+    });
+});
+
+// -------------------------Six-----------------------------
+window.addEventListener('select', event => {
+    console.log(`${event.currentTarget} was selected`);
+});
+
+// -------------------------Seven-----------------------------
+
 // -------------------------Eight-----------------------------
 
 
