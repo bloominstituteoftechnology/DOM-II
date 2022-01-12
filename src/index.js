1b2bf4ff39b0a78b8fd421e64eb7ed68a0675ef6
JacobBarnett <93158517+JacobBarnett@users.noreply.github.com>
2022-01-11 22:10:50 -0800

    Finished project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './less/index.less'
 
+const el = document.getElementById("text-content").addEventListener("click", displayDate);
+const el1 = document.getElementById("myBtn").addEventListener("click", displayDate);
+const el2 = document.getElementById("myBtn").addEventListener("click", displayDate);
+const el3 = document.getElementById("myBtn").addEventListener("click", displayDate);
+const el4 = document.getElementById("myBtn").addEventListener("click", displayDate);
+const el5 = document.getElementById("myBtn").addEventListener("click", displayDate);
+const el6 = document.getElementById("myBtn").addEventListener("click", displayDate);
+const el7 = document.getElementById("myBtn").addEventListener("click", displayDate);
+const el8 = document.getElementById("myBtn").addEventListener("click", displayDate);
 // Your code goes here!
