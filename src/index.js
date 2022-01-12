ec08ebbe6979b2ffb28f193789b386eb2e3ac312
James Robertson <Jor4291@gmail.com>
2022-01-11 19:04:26 -0600

    I cant believe I forgot to push

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './less/index.less'
 
 // Your code goes here!
+const navLink = document.querySelector('.nav-link')
+
+
+navLink.addEventListener("onClick", function(event) {
+    event.target.style.color = "red"
+
+});
