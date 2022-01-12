1fcd4d66cef01b89044e520e6ac84a36cce52367
James Robertson <Jor4291@gmail.com>
2022-01-11 21:13:01 -0600

    more progress, final bug fixes

--- src/index.js
+++ src/index.js
@@ -33,6 +33,26 @@ document.body.addEventListener('dblclick', evt =>{
 // Keydown
 window.addEventListener('keydown', evt =>{
   if (evt.key === 6){
-      document.body.innerHTML = "NO MORE FUN BUS"
+      document.body.innerHTML = "<h1> NO MORE FUN BUS</h1>"   
   }  
 })
+
+// mousemove
+document.body.addEventListener('mousemove', evt => {
+    const { clientX, clientY } = evt
+});
+
+
+// mouse enter&leave
+
+const destinations = document.querySelectorAll('.destination')
+for (let destination of destinations) {
+    destination.addEventListener('mouseenter', ()=>{
+        destination.style.fontWeight = 'bold'
+    })
+    destination.addEventListener('mouseleave', ()=>{
+        setTimeout(() =>{
+            destination.style.fontWeight = 'initial'
+        }, 500)
+    })
+}
