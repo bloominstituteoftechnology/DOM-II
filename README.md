2e6ccab2e61ded81f4bb5c8f1a994d89bc6da3ae
Rebecca Stone <you@example.com>
2022-02-08 17:44:47 -0600

    5 events have been added to the JavaScript. will start to clean up functions

--- README.md
+++ README.md
@@ -27,6 +27,7 @@ Do not **move or rename any files** in this project. The website's source files 
 * [ ] Using your [index.js file](src/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, add DOM elements, remove them, etc.
 
 * [ ] Here are some event types you could try to use:
+  * [X] `mousemove`
   * [ ] `mouseover`
   * [ ] `keydown`
   * [ ] `wheel`
@@ -35,7 +36,8 @@ Do not **move or rename any files** in this project. The website's source files 
   * [ ] `resize`
   * [ ] `scroll`
   * [ ] `select`
-  * [ ] `dblclick`
+  * [X] `click`
+  * [X] `dblclick`
   * [ ] `drag / drop`
 
 Note: Drag and drop is a bit more advanced than the others. It's not actually a single type of event but several types that need to work together.
--- src/index.js
+++ src/index.js
@@ -4,17 +4,18 @@ import './less/index.less'
 
 
 
-//Changes the body background Color when mousemoves in header image
+//1. Changes the body background Color when mousemoves in header image
 const backgroundChanger = document.querySelector(".container .intro img");
 
-document.body.style.backgroundSize = "cover";
+
 backgroundChanger.addEventListener("mousemove", function (e) {
   let x = e.offsetX;
   let y = e.offsetY;
+  document.body.style.backgroundSize = "cover";
   document.body.style.backgroundColor = `rgb(${x}, ${y}, ${x - y})`;
 });
 
-//Changes the body font Color when clicking the nav links
+//2. Changes the body font Color when clicking the nav links
 
 const nav = document.querySelector(".nav")
 
@@ -27,4 +28,34 @@ function randomColor() {
   const newColor = Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.color = "#" + newColor;
 }
+
+//3. This area is for the first sign me up button
+const placePicker = document.querySelector(".content-pick");
+
+placePicker.children[0].children[2].addEventListener("mouseenter", buttonGone);
+
+function buttonGone(e){
+     e.target.style.display = "none";
+}
+
+//4. Changes the middle sign me up button text
+placePicker.children[1].children[2].addEventListener("click", clickAgain)
+placePicker.children[1].children[2].addEventListener("dblclick", clickTwice);
+
+function clickAgain(e){
+     e.target.textContent = "Hmm, Try a double click";
+}
+
+function clickTwice(e) {
+  e.target.textContent = "Sorry, You can't come.";
+}
+//5. This area is for the third sign me up button
+
+placePicker.children[2].children[2].addEventListener("mouseup", (e) => {
+  e.target.style.background = "pink";
+  e.target.style.fontWeight = "bold";
+  e.target.style.color = "black";
+});
+
+placePicker.children[2].children[2].addEventListener("mouseleave", buttonGone);
 
