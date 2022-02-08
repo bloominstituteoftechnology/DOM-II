2e86bcd797271b517c7ac92c42763888342b6216
renee.troiani@gmail.com <renee.troiani@gmail.com>
2022-02-08 13:38:12 -0700

    added mouseover and keydown events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './less/index.less'
 
 // Your code goes here!
+const welcome = document.querySelector('.intro h2');
+const words = document.querySelectorAll('p');
+
+
+function mouseOver () {
+    welcome.style.color = 'blue';
+}
+welcome.addEventListener('mouseover', mouseOver);
+
+function mouseOut() {
+    welcome.style.color = 'black';
+}
+
+welcome.addEventListener('mouseout', mouseOut);
+
+
+function oneKey (event) {
+    if (event.key === '1') {
+        words.forEach(elem => elem.style.fontSize = '2rem');
+    }
+}
+document.addEventListener('keydown', oneKey);
