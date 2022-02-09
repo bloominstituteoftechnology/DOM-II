c4b3ca3b042b045f73a3840c26841a5feb76b324
kim5981 <kimberlyruthrodriguez@gmail.com>
2022-02-08 22:48:40 -0800

    added code to mouse over event

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 import './less/index.less'
 
 // Your code goes here!
+
+//!------------------------ MOUSE OVER EVENT --------------------!
+//mouse over nav bar and change the color of text when moused over
+
+let navHover = document.querySelectorAll(".nav-link");
+
+navHover.addEventListener("mouseover", function(event) {
+    //highlight the text/change font color
+    event.target.style.color = "pink";
+
+    //reset color after timeout
+    setTimeout( function () {
+        event.target.style.color = "";
+    }, 500);
+}, false);
+
+
+//!------------------------ KEY DOWN EVENT -----------------------!
+
+//!------------------------ WHEEL EVENT ----------------------------!
+
+//!------------------------ LOAD EVENT -----------------------------!
+
+//!------------------------ FOCUS EVENT ----------------------------!
+
+//!------------------------ RESIZE EVENT ----------------------------!
+
+//!------------------------ SCROLL EVENT ----------------------------!
+
+//!------------------------ SELECT EVENT ----------------------------!
+
+//!------------------------ DOUBLE CLICK EVENT --------------------!
+
+//!------------------------ DRAG/DROP EVENT -----------------------!
