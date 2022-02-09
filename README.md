012cb500691a1c854576de53862f1b300faff462
Rebecca Stone <you@example.com>
2022-02-08 20:50:25 -0600

    finished all tasks except preventDefault

--- README.md
+++ README.md
@@ -26,8 +26,7 @@ Do not **move or rename any files** in this project. The website's source files 
 
 * [ ] Using your [index.js file](src/index.js), create [event listeners](https://developer.mozilla.org/en-US/docs/Web/Events) of at least 10 _different_ types. You must Use your creativity to make the Fun Bus site more interactive. For example you could change colors, animate objects, add DOM elements, remove them, etc.
 
-* [ ] Here are some event types you could try to use:
-  * [X] `mousemove`
+* [] Here are some event types you could try to use:
   * [ ] `mouseover`
   * [ ] `keydown`
   * [ ] `wheel`
@@ -36,8 +35,7 @@ Do not **move or rename any files** in this project. The website's source files 
   * [ ] `resize`
   * [ ] `scroll`
   * [ ] `select`
-  * [X] `click`
-  * [X] `dblclick`
+  * [ ] `dblclick`
   * [ ] `drag / drop`
 
 Note: Drag and drop is a bit more advanced than the others. It's not actually a single type of event but several types that need to work together.
--- src/index.js
+++ src/index.js
@@ -1,61 +1,121 @@
-import './less/index.less'
+import "./less/index.less";
 
 // Your code goes here!
 
+//These are my DOM selectors
+const busImg = document.querySelector(".container .intro img");
+const nav = document.querySelector(".nav");
+const placePicker = document.querySelector(".content-pick");
+const destinations = document.querySelector(".content-destination");
+const adventureAwaits = document.querySelector(".inverse-content");
+const contentSection = document.querySelector(".content-section");
+const introSectionH2 = document.querySelector(".intro h2");
+const introSection = document.querySelector(".intro p");
+const title = document.querySelector(".logo-heading");
+const footer = document.querySelector(".footer");
 
-
-//1. Changes the body background Color when mousemoves in header image
-const backgroundChanger = document.querySelector(".container .intro img");
-
-
-backgroundChanger.addEventListener("mousemove", function (e) {
+//These are my re-useable functions
+function locationBodyColor(e) {
   let x = e.offsetX;
   let y = e.offsetY;
   document.body.style.backgroundSize = "cover";
   document.body.style.backgroundColor = `rgb(${x}, ${y}, ${x - y})`;
-});
+}
 
-//2. Changes the body font Color when clicking the nav links
-
-const nav = document.querySelector(".nav")
-
-nav.children[0].addEventListener("click", randomColor);
-nav.children[1].addEventListener("click", randomColor);
-nav.children[2].addEventListener("click", randomColor);
-nav.children[3].addEventListener("click", randomColor);
-
-function randomColor() {
+function randomBodyColor() {
   const newColor = Math.floor(Math.random() * 16777215).toString(16);
   document.body.style.color = "#" + newColor;
 }
 
-//3. This area is for the first sign me up button
-const placePicker = document.querySelector(".content-pick");
-
-placePicker.children[0].children[2].addEventListener("mouseenter", buttonGone);
-
-function buttonGone(e){
-     e.target.style.display = "none";
+function makeDisappear(e) {
+  e.target.style.display = "none";
 }
 
-//4. Changes the middle sign me up button text
-placePicker.children[1].children[2].addEventListener("click", clickAgain)
-placePicker.children[1].children[2].addEventListener("dblclick", clickTwice);
-
-function clickAgain(e){
-     e.target.textContent = "Hmm, Try a double click";
+function firstClick(e) {
+  e.target.textContent = "Hmm, Try a double click";
+  e.target.style.fontWeight = "bold";
+  e.target.style.border = "2px solid black";
+  e.target.style.textAlign = "center";
 }
 
 function clickTwice(e) {
-  e.target.textContent = "Sorry, You can't come.";
+  e.target.textContent = "Maybe one click again?";
+  e.target.textAlign = "center";
 }
-//5. This area is for the third sign me up button
 
-placePicker.children[2].children[2].addEventListener("mouseup", (e) => {
-  e.target.style.background = "pink";
+function selector(e) {
+  let newColor1 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  let newColor2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  e.target.style.background = newColor1;
+  e.target.style.color = newColor2;
+  e.target.style.border = `2px solid ${newColor2}`;
   e.target.style.fontWeight = "bold";
-  e.target.style.color = "black";
-});
+}
 
-placePicker.children[2].children[2].addEventListener("mouseleave", buttonGone);
+function byeBye (e){
+    if(placePicker.children[0].children[2].style.display === "none" && placePicker.children[1].children[2].style.display === "none" && placePicker.children[2].children[2].style.display === "none")
+    makeDisappear(e);
+    comeAgain();
+}
 
+function comeAgain(){
+    if(title.style.display === "none"){
+        footer.children[0].textContent = "We Hope You Come Back Soon!";
+        footer.children[0].style.fontWeight = "bold";
+        footer.children[0].textAlign = "center";
+    }
+}
+
+//1. Changes the body background Color when mousemove in bus image
+busImg.addEventListener("mousemove", locationBodyColor);
+
+//2. Changes the body font Color when clicking the nav links
+nav.children[0].addEventListener("click", randomBodyColor);
+nav.children[1].addEventListener("click", randomBodyColor);
+nav.children[2].addEventListener("click", randomBodyColor);
+nav.children[3].addEventListener("click", randomBodyColor);
+
+//3. makes the first sign me up button disappear when mouseenter
+placePicker.children[0].children[2].addEventListener(
+  "mouseenter",
+  makeDisappear
+);
+
+//4. Changes the middle sign me up button text on click
+placePicker.children[1].children[2].addEventListener("click", firstClick);
+
+//5. Changes the middle sign me up button text again on double click
+placePicker.children[1].children[2].addEventListener("dblclick", clickTwice);
+
+//6. makes the middle sign me up button disappear on mouseout
+placePicker.children[1].children[2].addEventListener(
+    "mouseout", makeDisappear
+);
+
+//7. Changes the last sign me up button background/font weight/color on mousedown
+placePicker.children[2].children[2].addEventListener("mousedown", selector);
+
+//8. Changes the last sign me up button background/font weight/color on mouseup
+placePicker.children[2].children[2].addEventListener("mouseup", selector);
+
+//9. Makes the last sign me up button disappear when mouseleave
+placePicker.children[2].children[2].addEventListener(
+  "mouseleave",
+  makeDisappear
+);
+
+//10. Elements disappear after mouse track through, or clicks, if all buttons have disappeared
+
+placePicker.addEventListener("mousemove", byeBye);
+destinations.addEventListener("mousemove", byeBye);
+adventureAwaits.addEventListener("mouseout", byeBye);
+contentSection.addEventListener("mousemove", byeBye);
+introSectionH2.addEventListener("mousemove", byeBye);
+introSection.addEventListener("mousemove", byeBye);
+busImg.addEventListener("mouseout", byeBye);
+nav.children[0].addEventListener("click", byeBye);
+nav.children[1].addEventListener("click", byeBye);
+nav.children[2].addEventListener("click", byeBye);
+nav.children[3].addEventListener("click", byeBye);
+title.addEventListener("mouseenter", byeBye);
+footer.preventDefault();("click", comeBack);
