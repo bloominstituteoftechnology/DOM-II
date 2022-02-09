507afc7d0db86f5988e510634d422f0a254659e6
Joshmclemore <josh_mclemore@yahoo.com>
2022-02-08 18:04:40 -0600

    Finally accomplished 2 working changes. 8 more to go.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './less/index.less'
 
 // Your code goes here!
+
+// Selecting Elements to change
+const navButtons = document.querySelectorAll(".nav a");
+console.log(navButtons);
+const title = document.querySelector("h1");
+const introImg = document.querySelector(".intro img");
+
+// Creating Events with .addEventListener
+
+// function greyBackground() {
+//     EventTarget.style.color = 'grey'
+// }
+
+// navButtons.addEventListener("mouseover", greyBackground);
+
+navButtons.forEach(element => {
+    element.addEventListener('mouseover', (event) => {
+        event.target.style.backgroundColor = 'lightgrey';}
+)});
+
+title.addEventListener('click', event => {
+    title.textContent += ` Is Taking Off!!`;
+})
+
