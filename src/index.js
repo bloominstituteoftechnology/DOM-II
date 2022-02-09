4b3984aad40d08942081c295abe93e91014c7db6
Joshmclemore <josh_mclemore@yahoo.com>
2022-02-08 19:34:17 -0600

    I have completed 9 out of 10 events

--- src/index.js
+++ src/index.js
@@ -22,22 +22,75 @@ navButtons.forEach(element => {
         event.target.style.backgroundColor = 'lightgrey';}
 )});
 
+// click
 title.addEventListener('click', event => {
     title.textContent += ` Is Taking Off!!`;
 })
 
+// dblclick
 introImg.addEventListener('dblclick', function (event){
     introImg.style.border = '5rem';
 })
 
+//load on my own
 window.addEventListener('load', (event) => {
     console.log('Good job loading');
   });
 
-const source = document.querySelector('div.source');
+// keydown
+window.addEventListener('keydown', evt => {
+    if (evt.key == 6){
+        document.body.innerHTML = '<h1>Sent to File 13<h1>'
+    }
+})
 
-source.addEventListener('copy', (event) => {
-    const selection = document.getSelection();
-    event.clipboardData.setData('text/plain', selection.toString().toUpperCase());
-    event.preventDefault();
-});
+// Load
+window.onload = function (evt) {
+    const heading = document.querySelector('h1')
+    heading.textContent = 'READY TO GO'
+
+// Copy
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+            .then(text => {
+                heading.textContent += text
+            })
+    })
+
+    document.body.addEventListener('click', evt => {
+        evt.target.classList.toggle('mirror')
+    })
+
+// mousemove
+
+document.body.addEventListener('mousemove', evt => {
+    const {clientX, clientY} = evt
+    console.log(`mouse is at ${clientX}, ${clientY}`)
+})
+
+// mouseenter
+
+// mouseleave
+
+const destinations = document.querySelectorAll('.destination')
+for (let destination of destinations) {
+    destination.addEventListener('mouseenter', () => {
+        destination.style.fontWeight = 'bold'
+    })
+    destination.addEventListener('mouseleave', () => {
+        destination.style.fontWeight = 'initial'
+    })
+}
+
+
+}
+
+
+// Prevent Default
+
+Array.from(document.links).forEach(link => {
+    link.addEventListener("click", function(evt) {
+        evt.preventDefault();
+        console.log(`The ${evt.target.textContent} link doesn't work, but I'm sure glad you came.`)
+    })
+})
