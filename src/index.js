712ef045c4e0baefe7493e9fe9af54cf41b61202
stonebreaker1080 <stonebreaker1080@protonmail.com>
2022-02-09 20:41:31 -0500

    submission

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './less/index.less'
 
 // Your code goes here!
+const logo = document.querySelector(".logo-heading")
+
+logo.addEventListener("mouseover", function() {
+    logo.style.backgroundColor = "red"
+})
+
+document.addEventListener("keydown", function(){
+    logo.style.backgroundColor = "blue"
+})
+
+const topImages = document.querySelectorAll("div.img-content img")
+
+function reduceImages(){
+    topImages.forEach(item => {
+        item.setAttribute("style", "width:25%;")
+    })
+}
+
+document.addEventListener("wheel", reduceImages)
