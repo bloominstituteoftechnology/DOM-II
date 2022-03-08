05488d52f7e4a5a926582fb7e609864763eec4f0
Gage Clancy <gclancy121@gmail.com>
2022-03-08 16:46:57 -0600

    added 3/10 effects

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './less/index.less'
 
 // Your code goes here!
+const header = document.querySelector('.main-navigation');
+function headerColor() {
+    header.style.backgroundColor='green';
+}
+function headerColorRestore() {
+    header.style.backgroundColor='white';
+}
+header.addEventListener('mouseover', headerColor);
+header.addEventListener('mouseout', headerColorRestore);
+function tellWhatKeyPressed(evt) {
+    if (evt.key === ' ') {
+        console.log("You pressed the spacebar!");
+    } else {
+        console.log(`You pressed the ${evt.key} key!`);
+    }
+    
+}
+document.addEventListener('keydown', tellWhatKeyPressed);
