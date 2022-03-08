6c215f56d5215a6ae8e086a08eae0eb96f5a0089
Ryan Berry <rberry16@hotmail.com>
2022-03-08 15:32:35 -0700

    first event listener added

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import './less/index.less'
 
 // Your code goes here!
+const header = document.querySelector('.logo-heading');
+const NavLinks = document.querySelectorAll('.nav-link');
+Array.from(NavLinks).forEach(link => link.addEventListener('click', evt => {
+    evt.preventDefault();
+    link.classList.remove('.nav-link')
+}))
+
+function headerBold(evt){
+    header.textContent = 'Super Fun!'
+}
+
+header.addEventListener('click', headerBold);
