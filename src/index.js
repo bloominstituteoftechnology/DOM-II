1c689ae1701320fca28783687e58898290883c16
Ryan Berry <rberry16@hotmail.com>
2022-03-08 19:33:23 -0700

    event listeners 2, 3, and 4 completed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import './less/index.less'
 
 // Your code goes here!
 const header = document.querySelector('.logo-heading');
-const NavLinks = document.querySelectorAll('.nav-link');
-Array.from(NavLinks).forEach(link => link.addEventListener('click', evt => {
-    evt.preventDefault();
-    link.classList.remove('.nav-link')
-}))
+const headerImg = document.querySelector('.home img');
 
-function headerBold(evt){
+function imgChange (evt) {
+    headerImg.src = 'https://cdn4.onpurpose.org/media/images/flying_bus.2e16d0ba.fill-1200x630.png';
+}
+
+function imgReturn (evt) {
+    headerImg.src = 'http://localhost:9000/img/fun-bus.jpg'
+}
+
+function spaceKey(evt) {
+    if (evt.key === 'Escape') {
+        header.textContent = 'Fun Bus';
+    }
+}
+
+document.addEventListener('keydown', spaceKey);
+
+headerImg.addEventListener('mouseenter', imgChange);
+headerImg.addEventListener('mouseleave', imgReturn);
+
+function headerFun(evt){
     header.textContent = 'Super Fun!'
 }
 
-header.addEventListener('click', headerBold);
+header.addEventListener('click', headerFun);
