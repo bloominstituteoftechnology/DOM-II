a8d5661be00f182984347f940ab65a5c6f4163de
Jericho-West <97463076+Jericho-West@users.noreply.github.com>
2022-03-08 21:51:50 -0500

    first 2

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 import './less/index.less'
 
 // Your code goes here!
+
+let pancake = document.querySelector('.btn')
+
+function canbake() {
+    console.log("Click ^v^")
+}
+
+pancake.addEventListener('click', canbake)
+
+
+let kp = document.querySelector('.btn')
+
+function kpf() {
+    let mathy = Math.floor(Math.random() * 3)
+
+    if (mathy === 1){
+    kp.textContent = "Sign Me Up!"
+} else if (mathy === 2){
+    kp.textContent = "Sign Me Up?"
+} else {
+    kp.textContent = "Sign Me Up :D"
+} 
+
+}
+
+kp.addEventListener('mouseover', kpf)
+
