4d986d3953b602f8915b089cbf2af7e08d782c35
Jericho-West <97463076+Jericho-West@users.noreply.github.com>
2022-03-08 22:38:44 -0500

    3rd

--- src/index.js
+++ src/index.js
@@ -28,3 +28,44 @@ function kpf() {
 
 kp.addEventListener('mouseover', kpf)
 
+
+function kdf() {
+    for (let i = 0; i < 50; i++) {
+
+        
+
+    let fun = document.querySelectorAll('div')
+
+    let fun2 = Math.floor(Math.random() * 10)
+    if (fun2 === 0) {
+        fun2 = "red"
+    } else if (fun2 === 1) {
+        fun2 = "lime"
+    } else if (fun2 === 2) {
+        fun2 = "yellow"
+    } else if (fun2 === 3) {
+        fun2 = "white"
+    } else if (fun2 === 4) {
+        fun2 = "orange"
+    } else if (fun2 === 5) {
+        fun2 = "coral"
+    } else if (fun2 === 6) {
+        fun2 = "tan"
+    } else if (fun2 === 7) {
+        fun2 = "pink"
+    } else if (fun2 === 8) {
+        fun2 = "salmon"
+    } else if (fun2 === 9) {
+        fun2 = "magenta"
+    } else {
+        fun = "blue"
+    }
+
+    
+    
+
+    fun[Math.floor(Math.random() * 12)].style.backgroundColor = (fun2)
+}
+}
+
+document.onkeydown = kdf;
