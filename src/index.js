d5ac9010f91d89f65487b6a2fbc8506960fc6259
allrightsreserved89 <100737435+allrightsreserved89@users.noreply.github.com>
2022-06-07 18:39:03 -0500

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,51 @@
 import './less/index.less'
 
 // Your code goes here!
+console.log('hello world')
+
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired! Ready to go!`)
+    const heading = document.querySelector('h1')
+    heading.textContent= 'READY TO GO!!'
+
+
+    window.addEventListener('copy',() =>{
+        navigator.clipboard.readText()
+        .then(text =>{
+            heading.textContent += text
+        })
+    })
+
+    document.body.addEventListener('click',evt =>{
+    evt.target.classList.toggle('mirror')
+
+    })
+
+    document.body.addEventListener('dblclick', evt => {
+        evt.target.innerHTML=''
+    })
+
+    window.addEventListener('keydown',evt =>{
+        if(evt.key==6){
+            document.body.innerHTML='<h1>YOU RAN ORDER 66</h1>'
+        }
+    })
+
+    document.body.addEventListener('mouseover',evt=>{
+        const {clientX,clientY} = evt
+
+    })
+
+    const destinations = document.querySelectorAll('.destination')
+    for (let destination of destinations){
+        destination.addEventListener('mouserenter', ()=>{
+            destination.style.fontWeight='bold'    
+        })
+        destination.addEventListener('mouseleave',()=>{
+            setTimeout(()=>{
+            destination.style.fontWeight='initial'  
+            },500)
+
+        })
+    }
+}
