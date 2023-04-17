3304b6189c6e80b3274f369c8440a0dea84e6ef5
Orlando Castillo <omaddoc@gmail.com>
2023-04-17 02:35:57 -0400

    added 2 events

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-import './less/index.less'
+import './less/index.less';
 
 // Your code goes here!
+document.querySelector('.nav').addEventListener('mouseover', (event) => {
+  event.target.style.color = 'red';
+});
+
+document.addEventListener('keydown', () => {
+  document
+    .querySelector('.main-navigation')
+    .classList.toggle('main-navigation-keydown');
+});
