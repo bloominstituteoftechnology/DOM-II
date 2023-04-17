eeb17e8d12c51a8c00a388b212f21997196e827a
Orlando Castillo <omaddoc@gmail.com>
2023-04-17 10:38:10 -0400

    MVP

--- src/index.js
+++ src/index.js
@@ -14,3 +14,31 @@ document.addEventListener('keydown', () => {
 document.querySelector('.intro img').addEventListener('wheel', () => {
   document.querySelector('.intro img').style.transform = 'rotate(180deg)';
 });
+
+window.addEventListener('load', () => {
+  alert('Welcome to the Fun Bus website!');
+});
+
+document.querySelector('.nav-link').addEventListener('focus', (event) => {
+  event.target.style.background = 'yellow';
+});
+
+window.addEventListener('resize', () => {
+  document.querySelector('.logo-heading').style.fontSize = '2rem';
+});
+
+document
+  .querySelector('.content-section')
+  .addEventListener('select', (event) => {
+    event.target.style.background = 'lightblue';
+  });
+
+document.querySelector('.btn').addEventListener('dblclick', (event) => {
+  event.target.textContent = 'Clicked!';
+});
+
+document.querySelectorAll('.destination').forEach((destination) => {
+  destination.addEventListener('dblclick', (event) => {
+    event.target.style.backgroundColor = 'lightgreen';
+  });
+});
